e645aad30c29b02cbe647a751f441f8793d4f6e3
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-11 21:46:14 +0200

    Mon, 21:46, 5 files, M1 iMac
     M .config/starship/starship.toml
     M .config/starship/starship_terminus.toml
     M .gitignore
     M .zsh_history
     M "Mac Migration Scripts/2 quick setup on new mac.sh"

--- Mac Migration Scripts/2 quick setup on new mac.sh
+++ Mac Migration Scripts/2 quick setup on new mac.sh
@@ -4,3 +4,9 @@ cd ~ || exit 1
 git clone git@github.com:chrisgrieser/dotfiles.git
 zsh ~/dotfiles/symlink-creation.sh
 
+
+cd ~/dotfiles/Alfred.alfredpreferences/workflows/ || exit 1
+git clone git@github.com:chrisgrieser/shimmering-obsidian.git
+git clone git@github.com:chrisgrieser/alfred-bibtex-citation-picker.git
+git clone git@github.com:chrisgrieser/pdf-annotation-extractor-alfred.git
+
