a1ae14ce268c7d871f8cdce6c48a0d697b200196
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-11 14:09:07 +0200

    patch

--- git-dotfile-backup.sh
+++ git-dotfile-backup.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
 # go to script location (the script should be located in the git repository)
-cd "$(dirname "$0")" || exit
-
-device_name=$(hostname | cut -d"." -f1)
+THIS_LOCATION="$(dirname "$0")"
+cd "$THIS_LOCATION" || exit 1
+#-------------------------------------------------------------------------------
+device_name=$(scutil --get ComputerName | cut -d" " -f2-)
 details="$(git status --porcelain)"
 filesChanged="$(echo "$details" | wc -l | tr -d ' ')"
 
@@ -17,8 +18,18 @@ else
 fi
 
 git add -A
-# no full date needed, since GitHub shows it
+# no full date needed, since git shows it already
 git commit -m "$(date +"%a, %H:%M"), $changeType, $device_name" -m "$details"
 
 git pull
 git push
+
+#-------------------------------------------------------------------------------
+# pull repos
+
+git pull
+cd "$THIS_LOCATION/Alfred.alfredpreferences/workflows/user.workflow.41B90DCD-A99E-4943-A19A-E91859557FB0/" || exit 1
+git pull
+cd "$THIS_LOCATION/Alfred.alfredpreferences/workflows/user.workflow.D02FCDA1-EA32-4486-B5A6-09B42C44677C/" || exit 1
+git pull
+cd "$THIS_LOCATION/Alfred.alfredpreferences/workflows/user.workflow.765354AA-49F0-4CB1-8DB0-EA4BE2DB09F8/" || exit 1
