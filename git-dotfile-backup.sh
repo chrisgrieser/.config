b2ac11cf6bbe1ff50e17d20c74d743f2430b51a4
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-11 14:14:49 +0200

    fixes for dotfile repo move

--- git-dotfile-backup.sh
+++ git-dotfile-backup.sh
@@ -4,25 +4,25 @@
 THIS_LOCATION="$(dirname "$0")"
 cd "$THIS_LOCATION" || exit 1
 #-------------------------------------------------------------------------------
-device_name=$(scutil --get ComputerName | cut -d" " -f2-)
-details="$(git status --porcelain)"
-filesChanged="$(echo "$details" | wc -l | tr -d ' ')"
+# device_name=$(scutil --get ComputerName | cut -d" " -f2-)
+# details="$(git status --porcelain)"
+# filesChanged="$(echo "$details" | wc -l | tr -d ' ')"
 
-if [[ "$filesChanged" == 0 ]] ; then
-	# abort if there haven't been changes
-	exit 0
-elif [[ "$filesChanged" == 1 ]] ; then
-	changeType="$filesChanged file"
-else
-	changeType="$filesChanged files"
-fi
+# if [[ "$filesChanged" == 0 ]] ; then
+# 	# abort if there haven't been changes
+# 	exit 0
+# elif [[ "$filesChanged" == 1 ]] ; then
+# 	changeType="$filesChanged file"
+# else
+# 	changeType="$filesChanged files"
+# fi
 
-git add -A
-# no full date needed, since git shows it already
-git commit -m "$(date +"%a, %H:%M"), $changeType, $device_name" -m "$details"
+# git add -A
+# # no full date needed, since git shows it already
+# git commit -m "$(date +"%a, %H:%M"), $changeType, $device_name" -m "$details"
 
-git pull
-git push
+# git pull
+# git push
 
 #-------------------------------------------------------------------------------
 # pull repos
