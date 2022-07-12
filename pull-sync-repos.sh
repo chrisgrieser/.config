75411bcc59e29ec9d40c0c03b32ef5dcd83f7ea5
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-12 18:05:59 +0200

    Tue, 18:05, 3 files, M1 iMac
     M hammerspoon/menubar.lua
     M hammerspoon/system-and-cron.lua
     M pull-sync-repos.sh

--- pull-sync-repos.sh
+++ pull-sync-repos.sh
@@ -1,18 +1,17 @@
 #!/bin/zsh
 
-set -e  # exit with 1 if any command fails
-
 # pull dotfiles repo
 THIS_LOCATION="$(dirname "$0")"
-cd "$THIS_LOCATION"
-git pull
+cd "$THIS_LOCATION" || exit 1
+git pull || exit 1
 
 # pull Alfred repos
-cd "Alfred.alfredpreferences/workflows"
-cd "./shimmering-obsidian"
-git pull
-cd "../alfred-bibtex-citation-picker"
-git pull
-cd "../pdf-annotation-extractor-alfred"
-git pull
+cd "Alfred.alfredpreferences/workflows" || exit 1
+cd "./shimmering-obsidian" || exit 1
+git pull || exit 1
+cd "../alfred-bibtex-citation-picker" || exit 1
+git pull || exit 1
+cd "../pdf-annotation-extractor-alfred" || exit 1
+git pull || exit 1
 
+# INFO: not using 'set -e' so that more meaningful output msgs are created
