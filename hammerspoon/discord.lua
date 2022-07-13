750044730791736c79be01fccb588d48c51059c9
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-14 00:27:25 +0200

    Thu, 00:27, 5 files, Mac mini
    M  "Cron Jobs/some.log"
    M  espanso/match/emojis-and-ascii.yml
    M  hammerspoon/discord.lua
    MM hammerspoon/system-and-cron.lua
    M  pull-sync-repos.sh

--- hammerspoon/discord.lua
+++ hammerspoon/discord.lua
@@ -3,6 +3,7 @@ require("utils")
 function discordWatcher(appName, eventType)
 	if appName ~= "Discord" then return end
 
+
 	-- on launch, open OMG Server instead of friends (who needs friends if you have Obsidian?)
 	-- and reconnect Obsidian's Discord Rich Presence (Obsidian launch already covered by RP Plugin)
 	if eventType == hs.application.watcher.launched then
@@ -13,19 +14,19 @@ function discordWatcher(appName, eventType)
 				hs.application("Discord"):activate()
 			end)
 		end
+	end
 
 	-- when Discord is focused, enclose URL in clipboard with <>
-	elseif eventType == hs.application.watcher.activated then
-		local clipb = hs.pasteboard.getContents()
-		if not clipb then return end
+	-- when Discord is unfocused, removes <> from URL in clipboard
+	local clipb = hs.pasteboard.getContents()
+	if not (clipb) then return end
+	if eventType == hs.application.watcher.activated then
 		local hasURL = string.match(clipb, '^https?%S+$')
 		if (hasURL) then
 			hs.pasteboard.setContents("<"..clipb..">")
 		end
-
-	-- when Discord is unfocused, removes <> from URL in clipboard
 	elseif eventType == hs.application.watcher.deactivated then
-		local clipb = hs.pasteboard.getContents()
+
 		local hasEnclosedURL = string.match(clipb, '^<https?%S+>$')
 		if (hasEnclosedURL) then
 			clipb = clipb:sub(2, -2) -- remove first & last character
