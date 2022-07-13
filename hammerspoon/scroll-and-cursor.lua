bec0b26ed1483c4e358d506c629f1c21fe904a7a
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-13 10:25:08 +0200

    Wed, 10:25, 1 file, M1 iMac
     M hammerspoon/scroll-and-cursor.lua

--- hammerspoon/scroll-and-cursor.lua
+++ hammerspoon/scroll-and-cursor.lua
@@ -5,25 +5,13 @@ require("utils")
 -- have to be done here, since when send from Karabiner, gets caught by the
 -- pagedown/up listener from Hammerspoon in `twitterific-iina.lua`
 
-highlightsScrollAmount = 16
-
-function highlightsAppScroll (amount)
-		local screen = hs.mouse.getCurrentScreen()
-		local highlightsWin = hs.application("Highlights"):mainWindow():frame()
-
-		local centerPos = {
-			x = highlightsWin.x + highlightsWin.w * 0.5,
-			y = highlightsWin.y + highlightsWin.h * 0.5,
-		}
-		hs.mouse.setRelativePosition(centerPos)
-
-		hs.eventtap.scrollWheel({0, amount}, {})
-
-		local pseudoHiddenPos = {
-			x = screen:frame().w - 1, -- -1 to keep it on the current screen
-			y = screen:frame().h * 0.75,
-		}
-		hs.mouse.absolutePosition(pseudoHiddenPos)
+function pseudoHideCursor ()
+	local screen = hs.mouse.getCurrentScreen()
+	local pos = {
+		x = screen:frame().w - 1, -- -1 to keep it on the current screen
+		y = screen:frame().h * 0.75,
+	}
+	hs.mouse.setRelativePosition(pos, screen)
 end
 
 function scrollDown ()
@@ -49,22 +37,32 @@ hotkey({"alt"}, "J", scrollDown, nil, scrollDown)
 hotkey({"alt"}, "K", scrollUp, nil, scrollUp)
 
 --------------------------------------------------------------------------------
--- CURSOR HIDING
+
+-- HIGHLIGHTS Scroll
+highlightsScrollAmount = 16
+function highlightsAppScroll (amount)
+		local highlightsWin = hs.application("Highlights"):mainWindow():frame()
+		local centerPos = {
+			x = highlightsWin.x + highlightsWin.w * 0.5,
+			y = highlightsWin.y + highlightsWin.h * 0.5,
+		}
+		hs.mouse.setRelativePosition(centerPos)
+
+		hs.eventtap.scrollWheel({0, amount}, {})
+		pseudoHideCursor()
+end
+
+-- CURSOR HIDING in Brave
 -- when Alacritty activates, hide cursor
 -- when Brave activates and j or k is pressed for the first time, hide cursor
-function hidingCursor(key)
+function hidingCursorInBrowser(key)
 	keystroke({}, key, 1, hs.application("Brave Browser"))
-	local screen = hs.mouse.getCurrentScreen()
-	local pos = {
-		x = screen:frame().w - 1, -- -1 to keep it on the current screen
-		y = screen:frame().h * 0.75,
-	}
-	hs.mouse.setRelativePosition(pos, screen)
+	pseudoHideCursor()
 	jHidesCursor:disable() -- so it only works the first time
 	kHidesCursor:disable()
 end
-jHidesCursor = hotkey({},"j", function() hidingCursor("J") end)
-kHidesCursor = hotkey({},"k", function() hidingCursor("K") end)
+jHidesCursor = hotkey({},"j", function() hidingCursorInBrowser("J") end)
+kHidesCursor = hotkey({},"k", function() hidingCursorInBrowser("K") end)
 jHidesCursor:disable()
 kHidesCursor:disable()
 
