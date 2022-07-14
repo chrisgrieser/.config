7151068629814ce7fa37c04a58d767291163d2de
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-14 02:42:00 +0200

    Thu, 02:42, 3 files, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/info.plist
     M hammerspoon/menubar.lua
    ?? Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/git-doc-search.js

---
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/git-doc-search.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+const alfredMatcher = (str) => str.replace (/[-()_.]/g, " ") + " " + str + " ";
+
+const workArray = JSON.parse(app.doShellScript('curl -s "https://api.github.com/repos/git/git/git/trees/master?recursive=1"'))
+	.tree
+	.filter(file => file.path.startsWith("Documentation/"))
+	.map(file => {
+		const subsite = file.path
+			.slice(14) // eslint-disable-line no-magic-numbers
+			.replace(/\.\w+$/, ""); // remove extension
+
+		const displayTitle = subsite.replaceAll("-", " ");
+
+		return {
+			"title": displayTitle,
+			"match": alfredMatcher (subsite),
+			"arg": `https://git-scm.com/docs/${subsite}`,
+			"uid": subsite,
+		};
+	});
+
+JSON.stringify({ items: workArray });
