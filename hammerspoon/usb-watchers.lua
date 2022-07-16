399fe33f48d1f0ada03f0e38a709364b647ad6b0
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-16 23:47:24 +0200

    Sat, 23:47, 1 file, M1 iMac
     M hammerspoon/usb-watchers.lua

--- hammerspoon/usb-watchers.lua
+++ hammerspoon/usb-watchers.lua
@@ -2,8 +2,11 @@ require("utils")
 
 function openSwimAdded (device)
 	if not(device.eventType == "added" and device.productName == "LC8234xx_17S EVK") then return end
-
-	notify ("connected: openSwim")
+	hs.execute([[
+		PODCAST_LOCATION=~"/Library/Group Containers/243LU875E5.groups.com.apple.podcasts/Library/Cache/"
+		rsync "$PODCAST_LOCATION"/*.mp3 "/Volumes/OpenSwim/"
+	]])
+	notify ("Podcasts moved.")
 end
 
 openSwimWatcher = hs.usb.watcher.new(openSwimAdded)
