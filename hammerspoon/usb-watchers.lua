487fafd4506714a4af7d219731b2fd42ba7c2693
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-16 17:18:39 +0200

    Sat, 17:18, 1 file, M1 iMac
     M hammerspoon/usb-watchers.lua

--- hammerspoon/usb-watchers.lua
+++ hammerspoon/usb-watchers.lua
@@ -1,6 +1,7 @@
 require("utils")
 
 function openSwimUSB (device)
+	notify(device.eventType)
 	if device.eventType == "added" then
 		notify ("connected: "..device.productName)
 	elseif device.eventType == "removed" then
