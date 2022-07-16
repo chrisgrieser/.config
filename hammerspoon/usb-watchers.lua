ff7462c877b01bcc348bfbe325529b508b4ade3e
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-17 00:07:45 +0200

    Sun, 00:07, 1 file, M1 iMac
     M hammerspoon/usb-watchers.lua

--- hammerspoon/usb-watchers.lua
+++ hammerspoon/usb-watchers.lua
@@ -3,7 +3,7 @@ require("utils")
 function openSwimAdded (device)
 	if not(device.eventType == "added" and device.productName == "LC8234xx_17S EVK") then return end
 	notify("device added.")
-	hs.task.new(os.getenv("HOME").."dotfiles/hammerspoon/cp-podcasts.zsh", function (exitCode, _, stdErr) -- wrapped like this, since hs.task objects can only be run one time
+	hs.task.new(os.getenv("HOME").."/dotfiles/hammerspoon/cp-podcasts.zsh", function (exitCode, _, stdErr) -- wrapped like this, since hs.task objects can only be run one time
 		if exitCode == 0 then notify ("✅ podcast sync finished.")
 		else notify("⚠️️ podcast sync error"..stdErr) end
 	end):start()
