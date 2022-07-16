49eb005e330c8c9e16f1343251bd1b4102d5e276
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-17 00:33:02 +0200

    Sun, 00:33, 3 files, M1 iMac
     M "Cron Jobs/some.log"
     M hammerspoon/cp-podcasts.sh
     M hammerspoon/usb-watchers.lua

--- hammerspoon/cp-podcasts.sh
+++ hammerspoon/cp-podcasts.sh
@@ -3,11 +3,11 @@ PODCAST_LOCATION=~"/Library/Group Containers/243LU875E5.groups.com.apple.podcast
 PODCAST_TARGET="/Volumes/OpenSwim/"
 
 i=1
-while [[ ! -e "PODCAST_TARGET" ]] && [[ $i -lt 10 ]]; do
+while [[ ! -e "$PODCAST_TARGET" ]] && [[ $i -lt 10 ]]; do
 	sleep 0.5
 	i=$((i+1))
 done
-[[ -e "PODCAST_TARGET" ]] || exit 1
+[[ -e "$PODCAST_TARGET" ]] || exit 1
 
-cp -n "$PODCAST_LOCATION"/*.mp3 "$PODCAST_TARGET"
+cp -n "$PODCAST_LOCATION"/*.mp3 "$PODCAST_TARGET" || true
 open "$PODCAST_TARGET"
--- hammerspoon/usb-watchers.lua
+++ hammerspoon/usb-watchers.lua
@@ -3,16 +3,17 @@ require("utils")
 podcastSyncScript = os.getenv("HOME").."/dotfiles/hammerspoon/cp-podcasts.sh"
 function openSwimAdded (device)
 	if not(device.eventType == "added" and device.productName == "LC8234xx_17S EVK") then return end
+
+	notify ("⏳ Starting Podcast Sync…")
 	hs.task.new(podcastSyncScript, function (exitCode, _, stdErr) -- wrapped like this, since hs.task objects can only be run one time
 		if exitCode == 0 then
 			notify ("✅ podcast sync finished")
 			log("✅ podcast sync finished", "$HOME/dotfiles/Cron Jobs/some.log")
 		else
 			notify("⚠️️ podcast sync error"..stdErr)
-			log("✅ podcast sync error"..stdErr, "$HOME/dotfiles/Cron Jobs/some.log")
+			log("⚠️️ podcast sync error"..stdErr, "$HOME/dotfiles/Cron Jobs/some.log")
 		end
 	end):start()
-
 end
 
 openSwimWatcher = hs.usb.watcher.new(openSwimAdded)
