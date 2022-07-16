bfb9b04ee5c0b9597ba143b74b7230294caeaba4
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-17 00:19:36 +0200

    Sun, 00:19, 2 files, M1 iMac
     M hammerspoon/cp-podcasts.sh
     M hammerspoon/usb-watchers.lua

--- hammerspoon/usb-watchers.lua
+++ hammerspoon/usb-watchers.lua
@@ -1,12 +1,16 @@
 require("utils")
-
+--------------------------------------------------------------------------------
+podcastSyncScript = os.getenv("HOME").."/dotfiles/hammerspoon/cp-podcasts.sh"
 function openSwimAdded (device)
 	if not(device.eventType == "added" and device.productName == "LC8234xx_17S EVK") then return end
-	notify("device added.")
-	local podcastSyncScript = os.getenv("HOME").."/dotfiles/hammerspoon/cp-podcasts.sh"
 	hs.task.new(podcastSyncScript, function (exitCode, _, stdErr) -- wrapped like this, since hs.task objects can only be run one time
-		if exitCode == 0 then notify ("✅ podcast sync finished.")
-		else notify("⚠️️ podcast sync error"..stdErr) end
+		if exitCode == 0 then
+			notify ("✅ podcast sync finished")
+			log("✅ podcast sync finished", "$HOME/dotfiles/Cron Jobs/some.log")
+		else
+			notify("⚠️️ podcast sync error"..stdErr)
+			log("✅ podcast sync error"..stdErr, "$HOME/dotfiles/Cron Jobs/some.log")
+		end
 	end):start()
 
 end
