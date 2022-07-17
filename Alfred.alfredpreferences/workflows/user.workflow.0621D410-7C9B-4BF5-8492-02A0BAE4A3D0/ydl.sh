8aea3bb444201978a8a8b6b30ff58d687514e605
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-17 13:01:05 +0200

    Sun, 13:01, 4 files, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/ydl.sh
    ?? .config/youtube-dl/config
    ?? Alfred.alfredpreferences/workflows/user.workflow.8DECF5D8-77FD-40FE-B718-01DFB43ABC53/info.plist

--- Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/ydl.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/ydl.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env zsh
-# shellcheck disable=SC2154
 export PATH=/usr/local/bin/:/opt/homebrew/bin/:$PATH
-CURRENT_TAB=$(osacript -e 'tell application "Brave Browser" to return URL of active tab of front window')
+CURRENT_TAB=$(osascript -e 'tell application "Brave Browser" to return URL of active tab of front window')
 
-download_location="${download_location/#~/$HOME}"
+download_location="${download_location/#\~/$HOME}"
 cd "$download_location" || exit 1
 
-# shellcheck disable=SC2086
-youtube-dl --format $output_format "$CURRENT_TAB" 2>&1
+youtube-dl --quiet "$CURRENT_TAB" 2>&1
