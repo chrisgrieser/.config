a5c2b12a9ae10928cea5478a9273a8fc795606ad
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-17 13:46:05 +0200

    Sun, 13:46, 2 files, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/ydl.sh

--- Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/ydl.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/ydl.sh
@@ -3,7 +3,11 @@ export PATH=/usr/local/bin/:/opt/homebrew/bin/:$PATH
 download_location="${download_location/#\~/$HOME}"
 CURRENT_TAB=$(osascript -e 'tell application "Brave Browser" to return URL of active tab of front window')
 
-[[ -z "$CURRENT_TAB" ]] && exit 1
+[[ -n "$CURRENT_TAB" ]] || exit 1
 cd "$download_location" || exit 1
 
-youtube-dl "$CURRENT_TAB" 2>&1
+if youtube-dl --quiet "$CURRENT_TAB" ; then
+	echo "✅ Download finished."
+else
+	echo "❌ Not possible"
+fi
