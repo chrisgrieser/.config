8bd6963b061003da066e7dccd1c69a0eb0d96254
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-17 20:16:13 +0200

    Sun, 20:16, 8 files, M1 iMac
     M .config/karabiner/assets/complex_modifications/hyper-and-meh.json
     M .config/karabiner/assets/complex_modifications/hyper-and-meh.yaml
     M .config/karabiner/assets/complex_modifications/utility-hotkeys.json
     M .config/karabiner/assets/complex_modifications/utility-hotkeys.yaml
     M .config/karabiner/karabiner.json
     M Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/yaml2json.sh
     M "Sublime User Folder/Preferences.sublime-settings"

--- Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/yaml2json.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/yaml2json.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env zsh
-
+export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 cd ~/.config/karabiner/assets/complex_modifications/ || exit 1
 
 for f in *.yaml ; do
   f=$(basename "$f" .yaml)
   yq -o=json '.' "$f.yaml" > "$f.json"
-done
-
-for f in *.yml ; do
-  f=$(basename "$f" .yml)
-  yq -o=json '.' "$f.yml" > "$f.json"
 done
 
