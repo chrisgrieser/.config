5b861f9eefe6e6c261d6a7ee0568dc13c98e7bf0
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-17 13:16:05 +0200

    Sun, 13:16, 4 files, M1 iMac
     M .config/youtube-dl/config
     M Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/ydl.sh
     D Alfred.alfredpreferences/workflows/user.workflow.8DECF5D8-77FD-40FE-B718-01DFB43ABC53/info.plist

--- Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/ydl.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/ydl.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 export PATH=/usr/local/bin/:/opt/homebrew/bin/:$PATH
+download_location="${download_location/#\~/$HOME}"
 CURRENT_TAB=$(osascript -e 'tell application "Brave Browser" to return URL of active tab of front window')
 
-download_location="${download_location/#\~/$HOME}"
+[[ -z "$CURRENT_TAB" ]] && exit 1
 cd "$download_location" || exit 1
 
-youtube-dl --quiet "$CURRENT_TAB" 2>&1
+youtube-dl "$CURRENT_TAB" 2>&1
