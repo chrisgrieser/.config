43f1877b68d88bf54ef7e4e9f3000307d52051c5
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-18 10:49:27 +0200

    Mon, 10:49, 1 file, Mac mini
     M hammerspoon/twitterrific-iina.lua

--- hammerspoon/twitterrific-iina.lua
+++ hammerspoon/twitterrific-iina.lua
@@ -46,7 +46,8 @@ function homeAction ()
 	if appIsRunning("IINA") then
 		keystroke({}, "Space", 1, hs.application("IINA"))
 	elseif appIsRunning("zoom.us") then
-		keystroke({"shift", ""}, "Space", 1, hs.application("zoom.us"))
+		-- toggle mute
+		keystroke({"shift", "command"}, "A", 1, hs.application("zoom.us"))
 	else
 		hs.application("Twitterrific"):activate()
 	end
