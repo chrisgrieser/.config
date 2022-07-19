3911779a4ed2c5391a33fd9991afe1e6ea361a60
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-19 17:15:17 +0200

    Tue, 17:15, 6 files, M1 iMac
     M Alfred.alfredpreferences/preferences/workflows/prefs.plist
     M Alfred.alfredpreferences/workflows/user.workflow.10965751-E224-4556-9321-4FC7494B6B6A/citekey-selection.js
     M Alfred.alfredpreferences/workflows/user.workflow.10965751-E224-4556-9321-4FC7494B6B6A/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.71B41CE9-9AAB-43B7-A1D7-482FC1AE71AD/info.plist
     M espanso/match/snippets.yml
     M pandoc/main-bibliography.bib

--- Alfred.alfredpreferences/workflows/user.workflow.10965751-E224-4556-9321-4FC7494B6B6A/citekey-selection.js
+++ Alfred.alfredpreferences/workflows/user.workflow.10965751-E224-4556-9321-4FC7494B6B6A/citekey-selection.js
@@ -4,31 +4,25 @@ function run (argv){
 	ObjC.import('stdlib');
 
 	//import variables
-	var input = argv.join("");
-	var pdf_path = $.getenv('pdf_path');
-	var alfred_bar_length = $.getenv('alfred_bar_length');
+	const input = argv.join("");
+	const pdfPath = $.getenv('pdf_path');
+	const alfredBarLength = $.getenv('alfred_bar_length');
 
-	var possible_entries = input.split(";");
+	const possibleEntries = input.split(";");
 	let jsonArray = [];
-	possible_entries.forEach(entry => {
+	possibleEntries.forEach(entry => {
 		let citekey = entry.split("§")[0];
 		let title = entry.split("§")[1];
-       //shorten, if title too long
- 		if (title.length > alfred_bar_length){
- 			title = title.substring(0, alfred_bar_length);
- 			title = title + "...";
- 		}
-		let renamed_file = pdf_path.replace (/(.*\/).*(_.*)/,"$1" + citekey + "$2");
+		//shorten, if title too long
+		if (title.length > alfredBarLength){
+			title = title.substring(0, alfredBarLength);
+			title = title + "...";
+		}
+		let renamedFile = pdfPath.replace (/(.*\/).*(_.*)/,"$1" + citekey + "$2");
 		jsonArray.push ({
 			'title': title,
 			'subtitle': citekey,
-			'arg': renamed_file,
-			"mods": {
-			   "shift": {
-			      "arg": citekey,
-			      "subtitle": "⇧: Open in BibDesk"
-			   },
-			}
+			'arg': renamedFile,
 		})
 	});
 
