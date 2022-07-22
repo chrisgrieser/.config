b62057e465b95ee69cf4a5f76ed2f17bf01c90a8
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-22 21:36:53 +0200

    Fri, 21:36, 4 files, M1 iMac
     M .config/alacritty/alacritty.yml
     M zsh/.zshrc
     M zsh/keybindings.zsh
    ?? zsh/vi-mode.zsh

---
+++ zsh/vi-mode.zsh
@@ -0,0 +1,23 @@
+# Enable vi mode
+bindkey -v
+
+# Change cursor shape for different vi modes, by @Jack of some quantity of trades
+function zle-keymap-select () {
+    case $KEYMAP in
+        vicmd) echo -ne '\e[1 q';;      # block
+        viins|main) echo -ne '\e[5 q';; # beam
+    esac
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q' # Use beam shape cursor on startup.
+preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.Use beam shape cursor on startup.
+
+
+#-------------------------------------------------------------------------------
+
+# ℹ️ prompt styling based on vi mode in starship config
