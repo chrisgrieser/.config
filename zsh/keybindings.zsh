1c17d5747c2a0542273458dcb619f7c2192595dd
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-22 21:21:53 +0200

    Fri, 21:21, 3 files, M1 iMac
     M .config/alacritty/alacritty.yml
     M zsh/.zshrc
     M zsh/keybindings.zsh

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,20 +1,6 @@
 # Enable vi mode
-# shellcheck disable=SC1085,SC1073,SC1009
 bindkey -v
 
-function zle-keymap-select () {
-    case $KEYMAP in
-    ¦   vicmd) echo -ne '\e[1 q';;      # block
-    ¦   viins|main) echo -ne '\e[5 q';; # beam
-    esac
-}
-zle -N zle-keymap-select
-zle-line-init() {
-    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-    echo -ne "\e[5 q"
-}
-zle -N zle-line-init
-echo -ne '\e[5 q' # Use beam shape cursor on startup.
 
 #-------------------------------------------------------------------------------
 bindkey "^P" copyLocation
