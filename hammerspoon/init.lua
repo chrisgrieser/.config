825e5f36ef796182da696f7fc62a241e4cf3a314
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-23 18:11:40 +0200

    Sat, 18:11, 1 file, M1 iMac
     M hammerspoon/init.lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -37,7 +37,7 @@ if isAtOffice() then systemWake() end
 dotfileSyncMenuBar:setClickCallback(function ()
 	local lastCommit = hs.execute('git log -1 --format=%ar')
 	lastCommit = trim(lastCommit)
-	dotfileSyncMenuBar:setTooltip(lastCommit)
 	local minTillSync = math.floor(repoSyncTimer:nextTrigger() / 60)
 	notify("last commit "..lastCommit.."\r\rnext sync in "..tostring(minTillSync).." min")
 end)
+
