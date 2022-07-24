1f625452eb77fddd94fa7cf90746c08e7bca3960
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-24 10:46:09 +0200

    Sun, 10:46, 1 file, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.js

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.js
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.js
@@ -1,16 +1,29 @@
 #!/usr/bin/env osascript -l JavaScript
 
-const app = Application.currentApplication();
-app.includeStandardAdditions = true;
-function finderSelection () {
-	const selection = decodeURI(Application("Finder").selection()[0]?.url()).slice(7);
-	if (selection === "undefined") return ""; // no selection
-	return selection;
-}
+function run () {
+	const app = Application.currentApplication();
+	app.includeStandardAdditions = true;
 
-function run (argv){
-		join('');
-!argv
-}
+	function finderSelection () {
+		const selection = decodeURI(Application("Finder").selection()[0]?.url()).slice(7);
+		if (selection === "undefined") return ""; // no selection
+		return selection;
+	}
 
-app.doShellScript('ls');
+	const filePathRegex = /(\/.*)\/(.*\.(\w+))$/;
+
+	//------------------------------------------------------------------------------
+
+	const selection = finderSelection();
+	if (!selection) return;
+
+	const isRegularFile = Boolean(selection.match(/\/.*\.\w+$/));
+	if (!isRegularFile) return; // not a directory
+
+	const parentFolder = selection.replace(filePathRegex, "$1");
+	const fileName = selection.replace(filePathRegex, "$2");
+	const ext = selection.replace(filePathRegex, "$3");
+	const fileNameSave = fileName.replaceAll(".", "-");
+
+	app.doShellScript(`cd "${parentFolder}" ; git rev-parse --git-dir`)
+}
