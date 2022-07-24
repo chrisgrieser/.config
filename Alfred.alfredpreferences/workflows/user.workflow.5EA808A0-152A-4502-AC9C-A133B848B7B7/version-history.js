48a3def0bcc5303e0ddd99b4f9afc00b9acf94e4
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-24 11:01:10 +0200

    Sun, 11:01, 2 files, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.js

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.js
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.js
@@ -5,10 +5,12 @@ function run () {
 	app.includeStandardAdditions = true;
 
 	function finderSelection () {
-		const selection = decodeURI(Application("Finder").selection()[0]?.url()).slice(7);
-		if (selection === "undefined") return ""; // no selection
-		return selection;
+		const sel = decodeURI(Application("Finder").selection()[0]?.url()).slice(7);
+		if (sel === "undefined") return ""; // no selection
+		return sel;
 	}
+
+	const alfredMatcher = (str) => str.replace (/[-()_.]/g, " ") + " " + str + " ";
 
 	const filePathRegex = /(\/.*)\/(.*\.(\w+))$/;
 
@@ -18,12 +20,28 @@ function run () {
 	if (!selection) return;
 
 	const isRegularFile = Boolean(selection.match(/\/.*\.\w+$/));
-	if (!isRegularFile) return; // not a directory
+	if (!isRegularFile) return;
 
 	const parentFolder = selection.replace(filePathRegex, "$1");
 	const fileName = selection.replace(filePathRegex, "$2");
 	const ext = selection.replace(filePathRegex, "$3");
 	const fileNameSave = fileName.replaceAll(".", "-");
+	const fileIcon = { "type": "fileicon", "path": selection };
 
-	app.doShellScript(`cd "${parentFolder}" ; git rev-parse --git-dir`)
+	const isGitRepo = app.doShellScript(`cd "${parentFolder}" ; git rev-parse --git-dir || echo "not a git directory"`) === ".git";
+	if (!isGitRepo) return;
+
+	const commitHashArr = app.doShellScript(`cd "${parentFolder}" ; git log --pretty=format:%h "${selection}"`)
+		.split("\r")
+		.map(hash => {
+			return {
+				"title": hash,
+				"match": alfredMatcher (hash),
+				"subtitle": hash,
+				"icon": fileIcon,
+				"arg": hash,
+			};
+		})
+	return JSON.stringify({ items: commitHashArr });
 }
+
