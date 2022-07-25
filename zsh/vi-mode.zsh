9a686513f6df91a22bb8c50527407e8d5ae90d6f
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-25 11:15:04 +0200

    Mon, 11:15, 2 files, M1 iMac
     M zsh/general_and_plugin_configs.zsh
     M zsh/vi-mode.zsh

--- zsh/vi-mode.zsh
+++ zsh/vi-mode.zsh
@@ -15,22 +15,52 @@ zle-line-init() {
 }
 zle -N zle-line-init
 echo -ne '\e[5 q' # Use beam shape cursor on startup.
-preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.Use beam shape cursor on startup.
-#-------------------------------------------------------------------------------
-# Display all commands for Normal/Insert Mode
-# bindkey -M vicmd
-# bindkey -M viins
-bindkey -M viins 'jj' vi-cmd-mode
-bindkey -M vicmd 'L' vi-end-of-line
-bindkey -M vicmd 'H' vi-first-non-blank
-bindkey -M vicmd 'U' redo
-bindkey -M vicmd ' ' 'ciw'
-bindkey -M vicmd 'Y' 'y\$'
-bindkey -M vicmd '-' vi-history-search-backward
-bindkey -M vicmd '?' show-vim-commands
-
+preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt. Use beam shape cursor on startup.
 
 #-------------------------------------------------------------------------------
 
 # ℹ️ prompt styling based on vi mode in starship config
 # possible, but not compatible when using the snippet to style the cursor
+
+#-------------------------------------------------------------------------------
+# https://jindalakshett.medium.com/zsh-vim-%EF%B8%8F-eafdef2183c4
+# ci", ci', ci`, di", etc
+autoload -U select-quoted
+zle -N select-quoted
+for m in visual viopp; do
+	for c in {a,i}{\',\",\`}; do
+		bindkey -M $m $c select-quoted
+	done
+done
+
+# ci{, ci(, ci<, di{, etc
+autoload -U select-bracketed
+zle -N select-bracketed
+for m in visual viopp; do
+	for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+		bindkey -M $m $c select-bracketed
+	done
+done
+
+# fix backspace
+bindkey "^?" backward-delete-char
+
+# in hundredth's of seconds (default: 0.4 seconds)
+export KEYTIMEOUT=1
+
+#-------------------------------------------------------------------------------
+# Display all commands for Normal/Insert Mode
+# bindkey -M vicmd
+# bindkey -M viins
+bindkey -M vicmd 'L' vi-end-of-line
+bindkey -M vicmd 'H' vi-first-non-blank
+bindkey -M vicmd 'U' redo
+bindkey -M vicmd ' ' '"ciw"'
+bindkey -M vicmd 'Y' "y\$"
+bindkey -M vicmd '-' vi-history-search-backward
+bindkey -M vicmd '?' show-vim-commands
+
+show-vim-commands () {
+	bindkey -M vicmd
+}
+zle -N show-vim-commands
