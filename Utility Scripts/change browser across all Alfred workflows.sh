41898c4d2a4f1f198f1d162c38b690dd13a2b177
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-25 15:00:33 +0200

    Mon, 15:00, 2 files, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.js
     M "Utility Scripts/change browser across all Alfred workflows.sh"

--- Utility Scripts/change browser across all Alfred workflows.sh
+++ Utility Scripts/change browser across all Alfred workflows.sh
@@ -1,15 +1,12 @@
 #!/bin/zsh
 
 
-# TODO: need to change brwoser across hammerspoon configs as well
+# TODO: need to change browser across hammerspoon configs as well
 
-cd "$ZSH_DOTFILE_LOCATION/.." || return 1
+cd "$DOTFILE_FOLDER/.." || return 1
 
 # replace (insert Browsers here)
 fd "_\[Browser\]" | xargs -I{} sed -i '.bak' 's/Brave Browser/Vivaldi/g' "{}"
-
-# finicky
-sed -i '.bak' 's/Brave Browser/Vivaldi/g' .finicky.js
 
 # replace (insert Browsers PATHS here)
 fd "_\[Browser-Path\]" | xargs -I{} sed -i '.bak' 's/BraveSoftware\/Brave Browser/BraveSoftware\/Brave-Browser/g' "{}"
