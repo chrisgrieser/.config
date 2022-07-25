c329fc46551be4c1d0a3d563dec31ff402053685
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-25 11:15:04 +0200

    Mon, 11:15, 2 files, M1 iMac
     M zsh/general_and_plugin_configs.zsh
     M zsh/vi-mode.zsh

--- zsh/general_and_plugin_configs.zsh
+++ zsh/general_and_plugin_configs.zsh
@@ -41,9 +41,6 @@ export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 
-# in hundredth's of seconds (default: 0.4 seconds)
-export KEYTIMEOUT=1
-
 # OPTIONS --- (`man zshoptions` to see all options)
 setopt AUTO_CD # pure directory = cd into it
 setopt INTERACTIVE_COMMENTS # comments in interactive mode (useful für copypasting)
