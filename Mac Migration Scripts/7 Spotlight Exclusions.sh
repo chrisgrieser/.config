b73753ce5701beb57294e4147a00165e00266a2a
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-27 21:18:22 +0200

    Wed, 21:18, 6 files, M1 iMac
     M "Mac Migration Scripts/7 Spotlight Exclusions.sh"
     D "Utility Scripts/Inoreader CSS/Custom Inoreader CSS.css"
     D "Utility Scripts/Inoreader CSS/Screenshot 2022-02-27 12.34.10.png"
     M hammerspoon/filesystem-watchers.lua
     M zsh/terminal_utils.zsh
    ?? "Utility Scripts/Custom Inoreader CSS.css"

--- Mac Migration Scripts/7 Spotlight Exclusions.sh
+++ Mac Migration Scripts/7 Spotlight Exclusions.sh
@@ -4,7 +4,7 @@
 
 SPOTLIGHT_CONFIG="/System/Volumes/Data/.Spotlight-V100/VolumeConfiguration.plist"
 
-# restart spotlight indexing (this also writing exclusions again)
+# restart spotlight indexing (this also requires writing exclusions again)
 
 sudo mdutil -E -i on /
 sudo rm -R "$SPOTLIGHT_CONFIG"
@@ -13,14 +13,15 @@ sleep 1
 
 sudo plutil -insert Exclusions.0 -string '/Applications/Utilities/' "$SPOTLIGHT_CONFIG"
 sudo plutil -insert Exclusions.0 -string '/Applications/Cisco' "$SPOTLIGHT_CONFIG"
-sudo plutil -insert Exclusions.0 -string ~'/Dotfiles/Alfred.alfredpreferences/workflows/' "$SPOTLIGHT_CONFIG"
+sudo plutil -insert Exclusions.0 -string ~'/dotfiles/Alfred.alfredpreferences/workflows/' "$SPOTLIGHT_CONFIG"
 sudo plutil -insert Exclusions.0 -string ~'/Library/Mobile Documents/com~apple~CloudDocs/Dotfolder/Backups/' "$SPOTLIGHT_CONFIG"
 sudo plutil -insert Exclusions.0 -string ~'/Library/Mobile Documents/com~apple~CloudDocs/.Trash/' "$SPOTLIGHT_CONFIG"
 sudo plutil -insert Exclusions.0 -string ~'/Library/Mobile Documents/com~apple~CloudDocs/Academia/PhD Data/' "$SPOTLIGHT_CONFIG"
 sudo plutil -insert Exclusions.0 -string "/Volumes/Externe A" "$SPOTLIGHT_CONFIG"
 sudo plutil -insert Exclusions.0 -string "/Volumes/Externe B" "$SPOTLIGHT_CONFIG"
 sudo plutil -insert Exclusions.0 -string "/Volumes/Externe C" "$SPOTLIGHT_CONFIG"
-
+sudo plutil -insert Exclusions.0 -string ~"/dotfiles/Utility Scripts/less-bottom.app" "$SPOTLIGHT_CONFIG"
+sudo plutil -insert Exclusions.0 -string ~"/dotfiles/Utility Scripts/Obsidian-Opener.app" "$SPOTLIGHT_CONFIG"
 
 #-------------------------------------------------------------------------------
 
