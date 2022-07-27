e6afd6a6991364a1b77a20a2f2bed4ff843575b2
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-27 14:07:30 +0200

    Wed, 14:07, 3 files, M1 iMac
     M Alfred.alfredpreferences/preferences/local/921c5d099cfd13a0f6191145281cd13334b1ba9d/workflows/debugger/prefs.plist
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/restore.sh
     D Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.sh

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/restore.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/restore.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env zsh
-hash=$(echo "$*" | cut d";" -f1)
-FULL_PATH=$(echo "$*" | cut d";" -f2)
+set -e
+hash=$(echo "$*" | cut -d";" -f1)
+FULL_PATH=$(echo "$*" | cut -d";" -f2)
 
 dir="$(dirname "$FULL_PATH")"
 file="$(basename "$FULL_PATH")"
+cd "$dir"
 
-cd "$dir" || exit 1
 git checkout "$hash" -- "$file"
-
+git commit -m "Restored '$file' (from: $hash)"
 open "$file"
