d87166bb2d3feb555ce4d57e30359b2c14e114fb
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-27 16:07:30 +0200

    Wed, 16:07, 1 file, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.js

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.js
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.js
@@ -18,17 +18,8 @@ function run (argv) {
 		return JSON.stringify({ items: item });
 	}
 
-	function padLeft (str, target) {
-		const diff = target - str.length;
-		if (diff > 0) {
-			for (let i = 1; i <= diff; i++) {
-				str += " ";
-			}
-		}
-		return str;
-	}
-
 	//------------------------------------------------------------------------------
+
 	const query = argv.join("");
 
 	let FULL_PATH;
@@ -54,58 +45,53 @@ function run (argv) {
 	const FIRST_RUN = query === "";
 	let FIRST_ITEM = true;
 	let historyMatches;
-	let logLines;
-	if (FIRST_RUN) {
-		logLines = app.doShellScript(`cd "${PARENT_FOLDER}" ; git log --pretty=format:"%h;%ah;%s;%an" --numstat "${FULL_PATH}"`)
-			.split("\r\r")
-			.map(commit => commit.replaceAll("\r", ";"));
-	} else {
-		logLines = app.doShellScript(`cd "${PARENT_FOLDER}" ; git log --pretty=format:%h "${FULL_PATH}"`)
-			.split("\r");
-	}
 
 	// show all versions of file with commit message, author. Sorted by commit date.
 	if (FIRST_RUN) {
-		historyMatches = logLines.map(line => {
-			const commitHash = line.split(";")[0];
-			const displayDate = line.split(";")[1]; // results from `extraOptions`
-			const commitMsg = line.split(";")[2]; //             ^
-			const author = line.split(";")[3]; //                ^
-			const numstat = line.split(";")[4].match(/\d+/g); // ^
+		historyMatches = app.doShellScript(`cd "${PARENT_FOLDER}" ; git log --pretty=format:"%h;%ah;%s;%an" --numstat "${FULL_PATH}"`)
+			.split("\r\r")
+			.map(commit => commit.replaceAll("\r", ";"))
+			.map(line => {
+				const commitHash = line.split(";")[0];
+				const displayDate = line.split(";")[1]; // results from `extraOptions`
+				const commitMsg = line.split(";")[2]; //             ^
+				const author = line.split(";")[3]; //                ^
+				const numstat = line.split(";")[4].match(/\d+/g); // ^
+				const changes = Number(numstat[0]) + Number(numstat[1]);
 
-			const changes = Number(numstat[0]) + Number(numstat[1]);
-			const displayChanges = padLeft(changes.toString(), 3);
+				const subtitle = `${changes}  ▪︎  ${commitMsg}  ▪︎  ${author}`;
 
-			const subtitle = `${displayChanges}  ▪︎  ${commitMsg}  ▪︎  ${author}`;
+				let appendix = "";
+				if (FIRST_ITEM) {
+					appendix = "  ▪︎  " + FILE_NAME;
+					FIRST_ITEM = false;
+				}
 
-			let appendix = "";
-			if (FIRST_ITEM) {
-				appendix = "  ▪︎  " + FILE_NAME;
-				FIRST_ITEM = false;
-			}
-
-			return {
-				"title": displayDate + appendix,
-				"subtitle": subtitle,
-				"mods": {
-					"alt": {
-						"arg": commitHash,
-						"subtitle": `${commitHash}    (⌥: Copy)`
+				return {
+					"title": displayDate + appendix,
+					"subtitle": subtitle,
+					"mods": {
+						"alt": {
+							"arg": commitHash,
+							"subtitle": `${commitHash}    (⌥: Copy)`
+						},
 					},
-				},
-				"icon": FILE_ICON,
-				"arg": `${commitHash};${FULL_PATH};0`,
-			};
-
-		});
+					"icon": FILE_ICON,
+					"arg": `${commitHash};${FULL_PATH};0`,
+				};
+			});
 
 	// search patches for change (git log -G) & display git commit info for matched versions
 	} else {
+
+		const ripgrepInstalled = app.doShellScript('which rg || echo "no"') !== "no";
+		const grepEngine = ripgrepInstalled ? "rg" : "grep";
+
 		historyMatches = app.doShellScript(`cd "${PARENT_FOLDER}" ; git log -G"${query}" --regexp-ignore-case --pretty=%h -- "${FULL_PATH}"`)
 			.split("\r")
 			.map(commitHash => {
 				const displayDate = app.doShellScript(`cd "${PARENT_FOLDER}" ; git show -s --format=%ah ${commitHash}`);
-				const grepMatch = app.doShellScript(`cd "${PARENT_FOLDER}" ; git show "${commitHash}:./${FILE_NAME}" | grep "${query}" --max-count=1 --ignore-case --line-number || true`);
+				const grepMatch = app.doShellScript(`cd "${PARENT_FOLDER}" ; git show "${commitHash}:./${FILE_NAME}" | ${grepEngine} "${query}" --max-count=1 --ignore-case --line-number || true`);
 				let line;
 				let firstMatch;
 				if (grepMatch) {
