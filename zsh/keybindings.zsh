a5f50aa162a4fb3d38bb8388fb480e6e1357e83b
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-28 12:21:14 +0200

    Thu, 12:21, 1 file, M1 iMac
     M zsh/keybindings.zsh

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -8,29 +8,24 @@ bindkey "^Y" redo
 bindkey "^K" kill-line
 bindkey "^U" vi-kill-line
 
-# [alt+arrow] - move word forward or backward (like in Mac)
+# [alt+arrow] - move word forward or backward (like on Mac)
 bindkey "^[[1;3C" forward-word
 bindkey "^[[1;3D" backward-word
 
 # zsh-autosuggest
-bindkey '^X' autosuggest-execute
-bindkey '^C' autosuggest-accept
+bindkey '^X' autosuggest-execute # e[x]ecute
+bindkey '^C' autosuggest-accept # [c]omplete
 
-# -----------------------------------
+#-------------------------------------------------------------------------------
 # `bindkey -M main` to show existing keybinds
 # there `^[` usually means escape
 # some bindings with '^' are reserved (^M=enter, ^I=tab, ^[[Z = shift+tab)
-# -----------------------------------
+#-------------------------------------------------------------------------------
 
 copyLocation () {
 	pwd | pbcopy
 }
 zle -N copyLocation
-
-quitSession () {
-	exit
-}
-zle -N quitSession
 
 # https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/copybuffer/copybuffer.plugin.zsh
 copyBuffer () {
