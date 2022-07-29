153dbbf8008e7f97421ed5e12e527224f31fbd99
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-29 12:33:58 +0200

    Fri, 12:33, 5 files, M1 iMac
     M .config/alacritty/alacritty.yml
     M Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/info.plist
     M "Sublime User Folder/.neovintageousrc"
     M zsh/keybindings.zsh
     M zsh/vi-mode.zsh

--- zsh/vi-mode.zsh
+++ zsh/vi-mode.zsh
@@ -1,26 +1,15 @@
+# Good primer on how to bind hotkeys in the ZSH
+# https://thevaluable.dev/zsh-line-editor-configuration-mouseless/
+#-------------------------------------------------------------------------------
+
 # Enable vi mode
 bindkey -v
 
-# Change cursor shape for different vi modes, by @Jack of some quantity of trades
-function zle-keymap-select () {
-    case $KEYMAP in
-        vicmd) echo -ne '\e[1 q';;      # block
-        viins|main) echo -ne '\e[5 q';; # beam
-    esac
-}
-zle -N zle-keymap-select
-zle-line-init() {
-    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-    echo -ne "\e[5 q"
-}
-zle -N zle-line-init
-echo -ne '\e[5 q' # Use beam shape cursor on startup.
-preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt. Use beam shape cursor on startup.
+# fix backspace
+bindkey "^?" backward-delete-char
 
-#-------------------------------------------------------------------------------
-
-# ℹ️ prompt styling based on vi mode in starship config
-# possible, but not compatible when using the snippet to style the cursor
+# in hundredth's of seconds (default: 0.4 seconds)
+export KEYTIMEOUT=1
 
 #-------------------------------------------------------------------------------
 # https://jindalakshett.medium.com/zsh-vim-%EF%B8%8F-eafdef2183c4
@@ -42,18 +31,39 @@ for m in visual viopp; do
 	done
 done
 
-# fix backspace
-bindkey "^?" backward-delete-char
-
-# in hundredth's of seconds (default: 0.4 seconds)
-export KEYTIMEOUT=1
-
 #-------------------------------------------------------------------------------
 # Display all commands for Normal/Insert Mode
 # bindkey -M vicmd
 # bindkey -M viins
+# bindkey -M visual
 bindkey -M vicmd 'L' vi-end-of-line
 bindkey -M vicmd 'H' vi-first-non-blank
 bindkey -M vicmd 'U' redo
+bindkey -M vicmd -s ' ' 'ciw'
+bindkey -M vicmd -s 'ö' 'xp' # transpose (move character to the right)
+bindkey -M vicmd -s 'Ö' 'xhhp' # reversed transpose (move character to the left)
+# shift-space has to be bound to daw via alacritty.yml, cause shell does not accept shift-space :(
+  # - { key: Space, mods: shift, chars: '^]'}
+bindkey -M vicmd -s '?' "ibindkey -M vicmd^M" # this properly re-shows the prompt after execution
 
-alias \?="bindkey -M vicmd"
+
+#-------------------------------------------------------------------------------
+
+# INFO: prompt styling based on vi mode in starship config
+# possible, but not compatible when using the snippet to style the cursor
+
+# Change cursor shape for different vi modes
+function zle-keymap-select () {
+    case $KEYMAP in
+        vicmd) echo -ne '\e[1 q';;      # block
+        viins|main) echo -ne '\e[5 q';; # beam
+    esac
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q' # Use beam shape cursor on startup.
+preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt. Use beam shape cursor on startup.
