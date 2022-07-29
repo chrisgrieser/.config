8ff8a0c227db325d055b84c1be42f0c8ee7f0400
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-29 20:08:11 +0200

    Fri, 20:08, 5 files, M1 iMac
     M Alfred.alfredpreferences/themes/theme.custom.D4B57AE8-9E80-4D3F-8F6F-AE008A8626DD/theme.json
     M Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/icon.png
     M Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/spotify-playlists.js
     M Alfred.alfredpreferences/workflows/user.workflow.CADB0A87-F125-441A-AECF-F8619A6D3ED5/info.plist

--- Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/spotify-playlists.js
+++ Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/spotify-playlists.js
@@ -4,12 +4,15 @@ ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 const alfredMatcher = (str) => str.replace (/[-()_.]/g, " ") + " " + str + " ";
+const currentlyPlaying = app.doShellScript("spt playback --status --format=%p").trim();
 
 const playlists = app.doShellScript("spt list --playlists --format=%p --limit=50")
 	.split("\r")
 	.map(playlist => {
+		let suffix = "";
+		if (playlist === currentlyPlaying) suffix = " ▶️";
 		return {
-			"title": playlist,
+			"title": playlist + suffix,
 			"match": alfredMatcher (playlist),
 			"arg": playlist,
 			"uid": playlist,
