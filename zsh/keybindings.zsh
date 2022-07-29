6dfac8436bf46346ef406b6892e8ff3f89703ae0
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-29 12:53:57 +0200

    Fri, 12:53, 2 files, M1 iMac
     M zsh/keybindings.zsh
     M zsh/vi-mode.zsh

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,12 +1,15 @@
-bindkey "^P" copyLocation
-bindkey "^B" copyBuffer
+# built in zle functions
 bindkey "^A" beginning-of-buffer-or-history
 bindkey "^E" end-of-buffer-or-history
-
 bindkey "^Z" undo
 bindkey "^Y" redo
 bindkey "^K" kill-line
 bindkey "^U" vi-kill-line
+
+# custom ZLE funtions
+bindkey "^P" copyLocation
+bindkey "^B" copyBuffer
+bindkey '“' quote-args # alt+2 → quote all args
 
 # [alt+arrow] - move word forward or backward (like on Mac)
 bindkey "^[[1;3C" forward-word
@@ -25,11 +28,18 @@ bindkey '^Y' autosuggest-accept # [y]ank the completion
 
 copyLocation () {
 	pwd | pbcopy
+	zle -M "'$PWD' copied."
 }
 zle -N copyLocation
 
 # https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/copybuffer/copybuffer.plugin.zsh
 copyBuffer () {
 	printf "%s" "$BUFFER" | pbcopy
+	zle -M "Buffer copied."
 }
 zle -N copyBuffer
+
+quote-args() {
+    BUFFER="$(echo "$BUFFER" | sed 's/ / "/' | sed 's/$/"/' )"
+}
+zle -N quote-args
