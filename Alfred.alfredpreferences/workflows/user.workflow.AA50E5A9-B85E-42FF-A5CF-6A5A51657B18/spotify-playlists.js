f87daa4c26e0b98b49cd2a88e4a87bf07d724575
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-29 20:28:11 +0200

    Fri, 20:28, 4 files, M1 iMac
     M .config/spotify-tui/.spotify_token_cache.json
     M Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/spotify-playlists.js
     M espanso/match/alfred.yml

--- Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/spotify-playlists.js
+++ Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/spotify-playlists.js
@@ -6,17 +6,25 @@ app.includeStandardAdditions = true;
 const alfredMatcher = (str) => str.replace (/[-()_.]/g, " ") + " " + str + " ";
 const currentlyPlaying = app.doShellScript("spt playback --status --format=%p").trim();
 
-const playlists = app.doShellScript("spt list --playlists --format=%p --limit=50")
+const playlists = app.doShellScript("spt list --playlists --limit=50")
 	.split("\r")
 	.map(playlist => {
+		const id = playlist.match(/\w+(?=\))/)[0];
+		const name = playlist.split(" (")[0];
 		let suffix = "";
 		if (playlist === currentlyPlaying) suffix = " ▶️";
 		return {
-			"title": playlist + suffix,
-			"match": alfredMatcher (playlist),
-			"arg": playlist,
-			"uid": playlist,
+			"title": name + suffix,
+			"match": alfredMatcher (name),
+			"mods": { "cmd": { "arg": id } },
+			"arg": name,
+			"uid": id,
 		};
+	})
+	.sort((a, b) => {
+		if (a.title.endsWith(" ▶️") && !b.title.endsWith(" ▶️")) return 1;
+		if (!a.title.endsWith(" ▶️") && b.title.endsWith(" ▶️")) return -1;
+		if (a.title.endsWith(" ▶️") && b.title.endsWith(" ▶️")) return 0;
 	});
 
 JSON.stringify({ items: playlists });
