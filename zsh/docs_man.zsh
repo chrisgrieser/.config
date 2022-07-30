ef7af36b6e17ef9d9b6cae66874ef5ac34ff9b99
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-30 13:55:46 +0200

    Sat, 13:55, 4 files, M1 iMac
     D "Browser Extension Settings/uBlacklist Subscriptions.txt"
     D unused/mouse-click-trickery.yaml
     M zsh/docs_man.zsh
    ?? "Browser Extension Settings/uBlacklist.txt"

--- zsh/docs_man.zsh
+++ zsh/docs_man.zsh
@@ -8,6 +8,7 @@ function cc () {
 	echo "$CHEAT_CODE_ONLY" | pbcopy
 }
 
+# Better Man
 # first arg: command
 # second arg: search term
 function man () {
@@ -17,14 +18,17 @@ function man () {
 		echo "Command '$1' not installed." ; return 1
  	fi
 
+ 	local isBuiltIn=false
+ 	[[ "$(which "$1")" =~ "built-in" ]] && isBuiltIn=true
+
 	# run in subshell to surpress output
- 	if [[ "$(which "$1")" =~ "built-in" ]] && [[ -z "$2" ]] ; then
+ 	if [[ $isBuiltIn == true ]] && [[ -z "$2" ]] ; then
  		(alacritty --option=window.decorations=full --title="built-in help: $1" \
  			--command less /usr/share/zsh/*/help/$1 &)
- 	elif [[ "$(which "$1")" =~ "built-in" ]] && [[ -n "$2" ]] ; then
+ 	elif [[ $isBuiltIn == true ]] && [[ -n "$2" ]] ; then
  		(alacritty --option=window.decorations=full --title="built-in help: $1" \
  			--command less --pattern=$2 /usr/share/zsh/*/help/$1 &)
-	elif [[ ! "$(which "$1")" =~ "built-in" ]] && [[ -z "$2" ]] ; then
+	elif [[ $isBuiltIn == false ]] && [[ -z "$2" ]] ; then
 		(alacritty --option=window.decorations=full --title="man: $1" \
 			--command man "$1" &)
 	else
@@ -32,7 +36,6 @@ function man () {
 			--command man "$1" -P "/usr/bin/less -is --pattern=$2" &)
 	fi
 }
-
 
 # colorize less https://wiki.archlinux.org/index.php/Color_output_in_console#less .
 export LESS_TERMCAP_mb=$'\E[1;31m'     # begin bold
@@ -42,5 +45,5 @@ export LESS_TERMCAP_us=$'\E[1;34m'     # begin underline
 export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
 
 # Pager-specific settings
-export LESS='-R --incsearch --ignore-case --HILITE-UNREAD --window=-3 --quit-at-eof --quit-if-one-screen --no-init --tilde'
+export LESS='-R --incsearch --ignore-case --HILITE-UNREAD --window=-3 --quit-if-one-screen --no-init --tilde'
 
