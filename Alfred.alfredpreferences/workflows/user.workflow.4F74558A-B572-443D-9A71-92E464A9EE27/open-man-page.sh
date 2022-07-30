7a173602c7d984e3a498ae7574526b4fbc067207
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-30 13:35:46 +0200

    Sat, 13:35, 5 files, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.4F74558A-B572-443D-9A71-92E464A9EE27/open-man-page.sh
     M Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/info.plist
     D "unused/uBlacklist Subscriptions.txt"
     M zsh/docs_man.zsh
    ?? "Browser Extension Settings/uBlacklist Subscriptions.txt"

--- Alfred.alfredpreferences/workflows/user.workflow.4F74558A-B572-443D-9A71-92E464A9EE27/open-man-page.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.4F74558A-B572-443D-9A71-92E464A9EE27/open-man-page.sh
@@ -1,21 +1,10 @@
 #!/usr/bin/env zsh
+# shellcheck disable=SC1091
 
-# to import LESS settings like colorization
 DOTFILE_FOLDER=~/dotfiles
 source "$DOTFILE_FOLDER/zsh/docs_man.zsh"
 
-# INPUT terms
 ONE=$(echo "$*" | cut -d" " -f1)
 [[ "$*" =~ " " ]] && TWO=$(echo "$*" | cut -d" " -f2)
 
-if [[ -z "$TWO" ]] ; then
-	alacritty \
-		--option=window.decorations=full \
-		--title="man $ONE" \
-		--command man "$ONE" &
-else
-	alacritty \
-		--option=window.decorations=full \
-		--title="man $ONE" \
-		--command man "$ONE" -P "/usr/bin/less -is --pattern=$TWO" &
-fi
+man "$ONE" "$TWO"
