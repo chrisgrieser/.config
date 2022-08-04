8720ecf923e2833d0cc7fe67070bc92fa3510cb8
🤖🕒 <automated@cron.job>
2022-08-04 23:07:35 +0200

    1 file, M1 iMac

--- Alfred.alfredpreferences/workflows/user.workflow.71B41CE9-9AAB-43B7-A1D7-482FC1AE71AD/recent-items.js
+++ Alfred.alfredpreferences/workflows/user.workflow.71B41CE9-9AAB-43B7-A1D7-482FC1AE71AD/recent-items.js
@@ -3,33 +3,59 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 const alfredMatcher = (str) => str.replace (/[-()_.]/g, " ") + " " + str + " ";
 
+// eslint-disable-next-line quotes
 const recentItems = app.doShellScript(`osascript -e 'tell application "System Events" to tell (process 1 where frontmost is true) to set allItems to every menu item of menu of menu item "Recent Items" of menu 1 of menu bar 1'`)
 	.split(", ")
 	.map(item => item.slice(10).split(" of ")[0]);
 
 const start = recentItems.indexOf("Documents") + 1;
 const end = recentItems.indexOf("Servers") - 1;
-
-// workaround with id necessary, since only file names, but not file paths are
-// saved in the menu, so that the Ids needs to be used to emulate a click in
-// the next applescript step
-
 let i = 0;
-const recentItemsMap = recentItems
+
+// eslint-disable-next-line quotes
+const recentFolders = app.doShellScript(`osascript -e 'tell application "System Events" to tell (process "Finder") to set allItems to every menu item of menu of menu item "Recent Folders" of menu "Go" of menu bar 1'`)
+	.split(", ")
+	.slice(0, -2)
+	.map(item => item.slice(10).split(" of ")[0])
 	.map(item => {
 		i++;
-		return { name: item, id: i };
+		return { "name": item, "id": i, "type": "d" };
+	});
+
+i = 0;
+const recentItemsMap = recentItems
+	.map(item => {
+		// workaround with id necessary, since only file names, but not file paths are
+		// saved in the menu, so that the Ids needs to be used to emulate a click in
+		// the next applescript step
+		i++;
+		return { "name": item, "id": i, "type": "f" };
 	})
 	.slice(start, end)
-	.filter(item => !item.name.includes("“"))
+	.filter(item => !item.name.includes("“"));
+
+const recentAll = [...recentItemsMap, ...recentFolders]
 	.map(item => {
+		let altMod;
+		if (item.type === "f") {
+			altMod = {
+				"valid": true,
+				"arg": item.id + 1 // id + 1 = reveal in Finder
+			};
+		} else {
+			altMod = {
+				"valid": false,
+			};
+		}
+
 		return {
 			"title": item.name,
+			"uid": item.name,
 			"match": alfredMatcher (item.name),
-			"arg": item.id,
-			"mods": { "alt": { "arg": item.id + 1 } }, // id + 1 = reveal in Finder
+			"mods": { "alt": altMod },
+			"arg": item.type + item.id.toString(),
 		};
 	});
 
-JSON.stringify({ items: recentItemsMap });
+JSON.stringify({ items: recentAll });
 
