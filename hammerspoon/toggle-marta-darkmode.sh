133f2aaf985c6518f7e7acfbbf5717fd8e364f49
🤖🕒 <automated@cron.job>
2022-08-06 16:42:26 +0200

    1 file, M1 iMac

--- hammerspoon/toggle-marta-darkmode.sh
+++ hammerspoon/toggle-marta-darkmode.sh
@@ -3,7 +3,6 @@ DARK_THEME=Kon
 LIGHT_THEME=Classic
 TO_DARK=$1
 
-
 if test "$TO_DARK" = "true" ; then
 	grep -q "theme \"$DARK_THEME\"" "$MARTA_CONFIG" && return
 	sed -i '' "s/ theme \"$LIGHT_THEME\"/ theme \"$DARK_THEME\"/" "$MARTA_CONFIG"
