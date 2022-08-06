733e4f4e9ad86e46569cde07f02ef5028592f775
🤖🕒 <automated@cron.job>
2022-08-06 16:24:54 +0200

    3 files, M1 iMac

--- hammerspoon/system-and-cron.lua
+++ hammerspoon/system-and-cron.lua
@@ -202,6 +202,27 @@ function toggleDarkMode ()
 			end if
 		end tell
 	]])
+
+	hs.execute([[
+		MARTA_CONFIG=~"/Library/Application Support/org.yanex.marta/conf.marco"
+		DARK_THEME=Kon
+		LIGHT_THEME=Classic
+		TO_DARK=$1
+
+		if test "$TO_DARK" = "true" ; then
+			grep -q "theme \"$DARK_THEME\"" "$MARTA_CONFIG" && return
+			sed -i '' "s/ theme \"$LIGHT_THEME\"/ theme \"$DARK_THEME\"/" "$MARTA_CONFIG"
+		else
+			grep -q "theme \"$LIGHT_THEME\"" "$MARTA_CONFIG" && return
+			sed -i '' "s/ theme \"$DARK_THEME\"/ theme \"$LIGHT_THEME\"/" "$MARTA_CONFIG"
+		fi
+
+		if pgrep "Marta" &> /dev/null ; then
+			killall "Marta"
+			sleep 0.2
+			open -a "Marta"
+		fi
+	]])
 end
 
 function isDarkMode()
