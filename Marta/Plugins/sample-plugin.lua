36c59168936ecfa8f171042a8b491aa3a6024a40
🤖🕒 <automated@cron.job>
2022-08-06 18:30:05 +0200

    6 files, M1 iMac

---
+++ Marta/Plugins/sample-plugin.lua
@@ -0,0 +1,16 @@
+plugin {
+	id = "de.chris-grieser.sample-plugin",
+	name = "Sample Plugin",
+	apiVersion = "2.1",
+	author = "pseudometa aka Chris Grieser",
+	email = "73286100+chrisgrieser@users.noreply.github.com",
+	url = "https://chris-grieser.de/"
+}
+
+action {
+	id = "action",
+	name = "Hello world",
+	apply = function()
+		martax.alert("Hello, world!")
+	end
+}
