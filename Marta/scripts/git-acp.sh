14b70edd5922aeea47153a9eae06cc88fcc3457e
🤖📂 <marta@file.explorer>
2022-08-07 01:17:40 +0200

    2 files, M1 iMac

--- Marta/scripts/git-acp.sh
+++ Marta/scripts/git-acp.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env zsh
-git add -A
-git commit -m "patch" --author
+device_name=$(scutil --get ComputerName | cut -d" " -f2-)
+filesChanged="$(git status --porcelain | wc -l | tr -d ' ')"
+
+if [[ "$filesChanged" == 0 ]] ; then
+	exit 0
+elif [[ "$filesChanged" == 1 ]] ; then
+	changeType="$filesChanged file"
+else
+	changeType="$filesChanged files"
+fi
+msg="$changeType, $device_name"
+
+git add -A && git commit -m "$msg" --author="🤖📂<marta@file.explorer>"
+git pull
+git push
