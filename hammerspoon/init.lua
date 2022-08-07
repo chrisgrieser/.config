f53432ee5a30a729eb9aa95289ba540c23aecc00
🤖🕒 <automated@cron.job>
2022-08-07 13:52:26 +0200

    1 file, M1 iMac

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -32,3 +32,29 @@ reloadAllMenubarItems()
 if isAtOffice() then systemWake() end
 
 notify("Config reloaded")
+
+--------------------------------------------------------------------------------
+
+choices = {
+	{
+		["text"] = "First Choice",
+		["subText"] = "This is the subtext of the first choice",
+		["uuid"] = "0001"
+	},
+	{
+		["text"] = "Second Option",
+		["subText"] = "I wonder what I should type here?",
+		["uuid"] = "Bbbb"
+	},
+		{ ["text"] = hs.styledtext.new("Third Possibility", {font={size=18}, color=hs.drawing.color.definedCollections.hammerspoon.green}),
+		["subText"] = "What a lot of choosing there is going on here!",
+		["uuid"] = "III3"
+	},
+}
+
+
+testChooser = hs.chooser.new(function()
+	notify ("test")
+end)
+testChooser:choices(choices)
+
