66570508c411bf10b6eef4b57d77f4b04b90ec2b
🤖🕒 <automated@cron.job>
2022-08-08 14:36:34 +0200

    1 file, M1 iMac

--- hammerspoon/app-switcher.lua
+++ hammerspoon/app-switcher.lua
@@ -2,6 +2,8 @@ require("utils")
 
 switcher = hs.window.switcher.new()
 switcher.ui.showThumbnails = false
+switcher.ui.showTitles = false
+switcher.ui.showSelectedTitle = false
 
 hotkey({"alt"}, "tab", function() switcher:next() end)
 hotkey({"alt", "shift"}, "tab", function() switcher:next() end)
