179d7a74565ae0f6d830866ebfbf29434aa5150b
🤖🕒 <automated@cron.job>
2022-08-08 19:42:14 +0200

    3 files, M1 iMac

--- Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/playback.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/playback.sh
@@ -2,7 +2,13 @@
 # shellcheck disable=SC2086,SC2154
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 
+# run in subshell to avoid output, only pass if for notification
 notification=$(spt playback --$1 --format="$format")
 
+# if shuffle isn't active, activate it
+flags=$(spt playback --status --format=%f)
+[[ ! "$flags" =~ "🔀" ]] && spt playback --shuffle
+
+# if not paused, then show notification
 current_status="$(spt playback --status --format=%s)"
 [[ "$current_status" != "⏸" ]] && echo -n "$notification"
