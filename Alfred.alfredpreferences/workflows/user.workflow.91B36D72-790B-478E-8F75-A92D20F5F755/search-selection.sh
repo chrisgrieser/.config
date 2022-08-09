653fc58713f5e3c1b59409618874da3e03759066
🤖🕒 <automated@cron.job>
2022-08-09 15:46:55 +0200

    2 files, M1 iMac

--- Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/search-selection.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/search-selection.sh
@@ -1,15 +1,24 @@
 #!/bin/zsh
 
 # IF SELECTION IS...
-# file path: reveal it
+# file path: reveal it in file explorer
 # directory path: open it
 # url: open in Browser
 # email: send to that adress
 # some other text: google it
 # empty: do nothing
 
+
 SEL="$*"
-SEL="${SEL/#\~/$HOME}"
+if [[ -z "$SEL" ]] ; then
+	PREV_CLIPBOARD=$(pbpaste)
+	osascript -e 'tell application "System Events" to keystroke "c" using {command down}'
+	sleep 0.1
+	SEL=$(pbpaste)
+fi
+
+SEL=$(echo "$SEL" | xargs echo -n) # trims whitespace
+SEL="${SEL/#\~/$HOME}" # resolve ~
 
 if [[ -f "$SEL" ]]; then
 	open -R "$SEL"
@@ -22,4 +31,8 @@ elif [[ "$SEL" =~ "@" ]]; then
 	open "mailto:$SEL"
 elif [[ -n "$SEL" ]]; then
 	open "https://www.google.com/search?q=$SEL"
+fi
+
+if [[ -n "$PREV_CLIPBOARD" ]] ; then
+	echo "$PREV_CLIPBOARD" | pbcopy
 fi
