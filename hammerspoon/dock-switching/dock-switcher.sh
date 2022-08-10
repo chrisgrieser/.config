b9a63922180196fed46589e0320c4ccc298d975b
🤖🕒 <automated@cron.job>
2022-08-10 12:11:33 +0200

    6 files, M1 iMac

--- hammerspoon/dock-switching/dock-switcher.sh
+++ hammerspoon/dock-switching/dock-switcher.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env zsh
 
 PLIST_LOCATION=$(dirname "$0")
-rm ~/Library/Preferences/com.apple.dock.plist
-cp -a "$PLIST_LOCATION/$1.plist" ~/Library/Preferences/com.apple.dock.plist
-defaults import com.apple.dock ~/Library/Preferences/com.apple.dock.plist
+MODE="$1"
+LAYOUT="$2"
+DOCK_PLIST=~"/Library/Preferences/com.apple.dock.plist"
+
+rm "$DOCK_PLIST"
+cp -a "$PLIST_LOCATION/$1.plist" "$DOCK_PLIST"
+defaults import com.apple.dock "$DOCK_PLIST"
 sleep 0.1
 killall Dock
+
+# cp -a ~/Library/Preferences/com.apple.dock.plist "$LAYOUT.plist"
+# auch in hammerspoon anpassen
