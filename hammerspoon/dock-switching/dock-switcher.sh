30932fe3bd463e79ea2f5e4dca64502166da735e
🤖🕒 <automated@cron.job>
2022-08-11 11:10:00 +0200

    5 files, M1 iMac

--- hammerspoon/dock-switching/dock-switcher.sh
+++ hammerspoon/dock-switching/dock-switcher.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env zsh
 
-PLIST_LOCATION=$(dirname "$0")
+DATA_DIR=$(dirname "$0")
 MODE="$1"
 LAYOUT="$2"
 DOCK_PLIST=~"/Library/Preferences/com.apple.dock.plist"
 
 if [[ "$MODE" == "--load" ]]; then
+	if [[ -z "$LAYOUT" ]] ; then
+		echo "Layout to load is missing."
+		exit 1
+	fi
 	rm "$DOCK_PLIST"
-	cp -a "$PLIST_LOCATION/$LAYOUT.plist" "$DOCK_PLIST"
+	cp -a "$DATA_DIR/$LAYOUT.plist" "$DOCK_PLIST"
 	defaults import com.apple.dock "$DOCK_PLIST"
 	sleep 0.1
 	killall Dock
+	touch "$DATA_DIR/current_$LAYOUT"
 elif [[ "$MODE" == "--save" ]]; then
+	if [[ -z "$LAYOUT" ]] ; then
+		LAYOUT=$(ls "$DATA_DIR" | grep "current" | cut -c9-)
+	fi
 	cp -fa ~/Library/Preferences/com.apple.dock.plist "$LAYOUT.plist"
 else
-	echo "Not a option."
+	echo "Not a valid option."
 	exit 1
 fi
 
