7ad94d6ef3d8121adacfb210df88ac7bc0d68f3f
🤖🕒 <automated@cron.job>
2022-08-17 18:43:03 +0200

    6 files, iMac Leuthingerweg

--- quick-start.sh
+++ quick-start.sh
@@ -1,3 +1,15 @@
+# to get the config started
+
+cd ~ || exit 1
+git clone git@github.com:chrisgrieser/dotfiles.git
+
+cd ~/dotfiles/Alfred.alfredpreferences/workflows/ || exit 1
+git clone git@github.com:chrisgrieser/shimmering-obsidian.git
+git clone git@github.com:chrisgrieser/alfred-bibtex-citation-picker.git
+git clone git@github.com:chrisgrieser/pdf-annotation-extractor-alfred.git
+
+#-------------------------------------------------------------------------------
+
 # create all symlinks in the appropriate locations for everything
 #-------------------------------------------------------------------------------
 DOTFILE_FOLDER="$(dirname "$0")"
