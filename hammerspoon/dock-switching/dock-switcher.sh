a8258349e94d4418c778bd969b37010211495dee
🤖🕒 <automated@cron.job>
2022-08-17 20:57:55 +0200

    8 files, iMac Leuthingerweg

--- hammerspoon/dock-switching/dock-switcher.sh
+++ hammerspoon/dock-switching/dock-switcher.sh
@@ -15,14 +15,13 @@ if [[ "$MODE" == "--load" ]]; then
 	defaults import com.apple.dock "$DOCK_PLIST"
 	sleep 0.1
 	killall Dock
-	touch "$DATA_DIR/current_$LAYOUT"
+	echo "$LAYOUT" > "$DATA_DIR/current"
 elif [[ "$MODE" == "--save" ]]; then
 	if [[ -z "$LAYOUT" ]] ; then
 		LAYOUT=$(ls "$DATA_DIR" | grep "current_" | cut -c9-)
 		echo "Saved Layout '$LAYOUT'"
 	fi
 	cp -f ~/Library/Preferences/com.apple.dock.plist "$LAYOUT.plist"
-	touch "$DATA_DIR/current_$LAYOUT"
 else
 	echo "Not a valid option."
 	exit 1
--- zsh/.zlogin
+++ zsh/.zlogin
@@ -1,8 +1,10 @@
 # shellcheck disable=SC2164,SC1009,SC1073,SC1056
 
-clear # messages when launching via Alfred
+clear # messages for when launching via Alfred
 
-if [[ "$TERMINAL" == "Alacritty" ]] ; then
+DEVICE_NAME=$(scutil --get ComputerName | cut -d" " -f2-)
+
+if [[ "$TERMINAL" == "Alacritty" ]] && [[ ! "$DEVICE_NAME" =~ "Leuthingerweg" ]] ; then
 	# clear
 	# 33% art, 66% fortune
 	if [[ $((RANDOM%3)) == 1 ]] ; then
