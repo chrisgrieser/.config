b8717a4a2dccd6c8f1e9bc1d0721324723ed84bd
🤖🕒 <automated@cron.job>
2022-08-18 21:16:35 +0200

    2 files, iMac Leuthingerweg

--- Alfred.alfredpreferences/workflows/user.workflow.4F74558A-B572-443D-9A71-92E464A9EE27/dotfile-bkp.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.4F74558A-B572-443D-9A71-92E464A9EE27/dotfile-bkp.sh
@@ -9,7 +9,9 @@ dotfile_folder="${dotfile_folder/#\~/$HOME}"
 # https://unix.stackexchange.com/questions/245856/zip-a-file-without-including-the-parent-directory
 [[ -e "$dotfile_bkp_destination" ]] || mkdir -p "$dotfile_bkp_destination"
 cd "$dotfile_folder" || exit 1
-zip -r "$backup_path" ./*
+
+# not explicitly matching all hidden files to not backup the git folder
+zip -r --quiet "$backup_path" ./* ./.{searchlink,vimrc,config,gitignore}
 
 # restrict number of backups
 # shellcheck disable=SC2154
