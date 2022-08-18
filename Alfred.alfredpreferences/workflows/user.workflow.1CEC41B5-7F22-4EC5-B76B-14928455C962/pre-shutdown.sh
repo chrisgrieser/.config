39087bc9ceef2cc100c207b4978d1c24fd27f1a8
🤖🕒 <automated@cron.job>
2022-08-18 22:54:16 +0200

    3 files, iMac Leuthingerweg

--- Alfred.alfredpreferences/workflows/user.workflow.1CEC41B5-7F22-4EC5-B76B-14928455C962/pre-shutdown.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.1CEC41B5-7F22-4EC5-B76B-14928455C962/pre-shutdown.sh
@@ -5,4 +5,5 @@ filesChanged="$(git status --porcelain | wc -l | tr -d ' ')"
 if [[ -n "$filesChanged" ]] ; then
 	echo "📴 pre-shutdown-sync" >> ~"/dotfiles/Cron Jobs/sync.log"
 	zsh ~/dotfiles/git-dotfile-sync.sh
+	zsh ~"/Main Vault/Meta/git vault backup.sh"
 fi
