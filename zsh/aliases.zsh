4e6c28cabd62fd0497393739528290ab7a5940c3
🤖🕒 <automated@cron.job>
2022-08-19 15:55:22 +0200

    4 files, iMac Leuthingerweg

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -47,10 +47,16 @@ alias size="du -sh . ./* ./.* | sort -rh | sed 's/\\.\\///'" # size of files in 
 
 # Suffix Aliases
 # = default command to act upon the filetype, when is is entered
+# ' and " are there to also open quoted things
 # without preceding command (analogous to `setopt AUTO_CD` but for files)
 alias -s {css,ts,js,yml,json,plist,xml,md}='bat'
-# alias -s {pdf,png,jpg,jpeg}="qlmanage -p &> /dev/null"
-alias -s {pdf,png,jpg,jpeg}="open"
+alias -s {css,ts,js,yml,json,plist,xml,md}\"='bat'
+alias -s {css,ts,js,yml,json,plist,xml,md}\'='bat'
+alias -s {pdf,png,jpg,jpeg,tiff}="qlmanage -p &> /dev/null"
+alias -s {pdf,png,jpg,jpeg,tiff}\"="qlmanage -p &> /dev/null"
+alias -s {pdf,png,jpg,jpeg,tiff}\'="qlmanage -p &> /dev/null"
 
 # open log files in less and scrolled to the bottom
 alias -s log="less +G"
+alias -s log\'="less +G"
+alias -s log\"="less +G"
