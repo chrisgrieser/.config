9e12a3ea435d0d9c64c4fe21c619f2b33e5ae595
🤖🕒 <automated@cron.job>
2022-08-19 13:06:38 +0200

    2 files, iMac Leuthingerweg

--- zsh/general_and_plugin_configs.zsh
+++ zsh/general_and_plugin_configs.zsh
@@ -33,8 +33,8 @@ setopt AUTO_CD # pure directory = cd into it
 setopt INTERACTIVE_COMMENTS # comments in interactive mode (useful für copypasting)
 
 # MAGIC ENTER
-export MAGIC_ENTER_GIT_COMMAND="git status --short ; broot"
-export MAGIC_ENTER_OTHER_COMMAND="broot"
+export MAGIC_ENTER_GIT_COMMAND="git-sbroot"
+export MAGIC_ENTER_OTHER_COMMAND="sbroot"
 
 function directoryInspect (){
 	if command git rev-parse --is-inside-work-tree &>/dev/null ; then
