d7b11226814e81b8157cb1170523cc3e0a4aaa8f
🤖🕒 <automated@cron.job>
2022-08-19 10:26:38 +0200

    13 files, iMac Leuthingerweg

--- zsh/terminal_utils.zsh
+++ zsh/terminal_utils.zsh
@@ -11,27 +11,27 @@ function separator (){
 
 # Quick Open File
 # (or change directory if a folder is selected)
-function o (){
-	local INPUT="$*"
+# function o (){
+# 	local INPUT="$*"
 
-	# skip `fzf` if file is fully named (e.g. through tab-completion)
-	[[ -f "$INPUT" ]] && { open "$INPUT" ; return }
-	[[ -d "$INPUT" ]] && { z "$INPUT" ; runMagicEnter ; return }
+# 	# skip `fzf` if file is fully named (e.g. through tab-completion)
+# 	[[ -f "$INPUT" ]] && { open "$INPUT" ; return }
+# 	[[ -d "$INPUT" ]] && { z "$INPUT" ; runMagicEnter ; return }
 
-	local SELECTED
-	SELECTED=$(fd --hidden | fzf \
-	           -0 -1 \
-	           --query "$INPUT" \
-	           --preview "if [[ -d {} ]] ; then exa ; else ; bat --color=always --style=snip --wrap=character --tabs=2 --line-range=:\$FZF_PREVIEW_LINES --terminal-width=\$FZF_PREVIEW_COLUMNS {} ; fi" \
-	           )
-	[[ -z "$SELECTED" ]] && return 130 # abort if no selection
+# 	local SELECTED
+# 	SELECTED=$(fd --hidden | fzf \
+# 	           -0 -1 \
+# 	           --query "$INPUT" \
+# 	           --preview "if [[ -d {} ]] ; then exa ; else ; bat --color=always --style=snip --wrap=character --tabs=2 --line-range=:\$FZF_PREVIEW_LINES --terminal-width=\$FZF_PREVIEW_COLUMNS {} ; fi" \
+# 	           )
+# 	[[ -z "$SELECTED" ]] && return 130 # abort if no selection
 
-	if [[ -d "$SELECTED" ]] ; then
-		z "$SELECTED"
-	else
-		open "$SELECTED"
-	fi
-}
+# 	if [[ -d "$SELECTED" ]] ; then
+# 		z "$SELECTED"
+# 	else
+# 		open "$SELECTED"
+# 	fi
+# }
 
 # smarter z/cd
 # (also alternative to https://blog.meain.io/2019/automatically-ls-after-cd/)
