00bbe92a02568cb77c878dbb5fe180bc06aadcdf
🤖🕒 <automated@cron.job>
2022-08-21 18:46:31 +0200

    8 files, iMac Leuthingerweg

---
+++ Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/pseudo-firenvim.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+subl_cli="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+
+osascript -e '
+	tell application "System Events"
+		keystroke "a" using {command down}
+		keystroke "c" using {command down}
+	end tell'
+
+sleep 0.05
+pbpaste | "$subl_cli" --wait --new-window | pbcopy
+sleep 0.05
+
+osascript -e 'tell application "System Events" to keystroke "v" using {command down}'
