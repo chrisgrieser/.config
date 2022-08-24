5590ecbc30d4c2c6b2c1fd6cea5ab605ce37c6b7
🤖 automated <cron@job>
2022-08-24 19:36:19 +0200

    iMac Helmholtz (8 files)

--- Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/volume-control.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/volume-control.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 # shellcheck disable=SC2154
 
+currentDevice=$(spt playback --format=%d)
 currentVolume=$(spt playback --format=%v)
 if [[ "$*" == "up" ]] ; then
 	newVolume=$((currentVolume + vol_increment))
@@ -8,7 +9,7 @@ else
 	newVolume=$((currentVolume - vol_increment))
 fi
 
-spt playback --volume="$newVolume" &> /dev/null
+spt playback --volume="$newVolume" --device="$currentDevice" &> /dev/null
 
 # for notification
 currentVolume=$(spt playback --format=%v)
