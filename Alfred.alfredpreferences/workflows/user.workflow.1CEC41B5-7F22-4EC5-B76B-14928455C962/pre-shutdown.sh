cc6e9801b283ae86ad291c285fb7d5f31c3d97be
🤖 automated <cron@job>
2022-08-24 15:11:29 +0200

    iMac Leuthingerweg (1 file)

--- Alfred.alfredpreferences/workflows/user.workflow.1CEC41B5-7F22-4EC5-B76B-14928455C962/pre-shutdown.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.1CEC41B5-7F22-4EC5-B76B-14928455C962/pre-shutdown.sh
@@ -2,10 +2,9 @@
 cd ~/dotfiles/ || exit 1
 if [[ -n "$(git status --porcelain)" ]] ; then
 	echo "📴 pre-shutdown-sync dotfiles" >> ~'/dotfiles/hammerspoon/logs/sync.log'
-	zsh ~"/dotfiles/git-dotfile-sync.sh"
-
+	zsh ~"/dotfiles/git-dotfile-sync.sh" &> /dev/null
 	if [[ -n "$(git status --porcelain)" ]] ; then
-		echo "Dotfile-Repo not clean." # f
+		echo "Dotfile-Repo not clean."
 		exit 1
 	fi
 fi
@@ -13,7 +12,7 @@ fi
 cd ~"/Main Vault/" || exit 1
 if [[ -n "$(git status --porcelain)" ]] ; then
 	echo "📴 pre-shutdown-sync vault" >> ~'/dotfiles/hammerspoon/logs/sync.log'
-	zsh ~"/Main Vault/Meta/git-vault-sync.sh"
+	zsh ~"/Main Vault/Meta/git-vault-sync.sh" &> /dev/null
 	if [[ -n "$(git status --porcelain)" ]] ; then
 		echo "Vault-Repo not clean."
 		exit 1
