dbb6277f8001ec258d7153dc8f9c4209597823cd
🤖 automated <cron@job>
2022-08-24 15:07:36 +0200

    iMac Leuthingerweg (2 files)

--- Alfred.alfredpreferences/workflows/user.workflow.1CEC41B5-7F22-4EC5-B76B-14928455C962/pre-shutdown.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.1CEC41B5-7F22-4EC5-B76B-14928455C962/pre-shutdown.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env zsh
 cd ~/dotfiles/ || exit 1
-
 filesChanged="$(git status --porcelain | wc -l | tr -d ' ')"
 if [[ -n "$filesChanged" ]] ; then
-	echo "📴 pre-shutdown-sync" >> ~'/dotfiles/hammerspoon/logs/sync.log'
+	echo "📴 pre-shutdown-sync dotfiles" >> ~'/dotfiles/hammerspoon/logs/sync.log'
 	zsh ~"/dotfiles/git-dotfile-sync.sh"
+fi
+
+cd ~"/Main Vault/" || exit 1
+filesChanged="$(git status --porcelain | wc -l | tr -d ' ')"
+if [[ -n "$filesChanged" ]] ; then
+	echo "📴 pre-shutdown-sync vault" >> ~'/dotfiles/hammerspoon/logs/sync.log'
 	zsh ~"/Main Vault/Meta/git-vault-sync.sh"
 fi
+
+#-------------------------------------------------------------------------------
+
