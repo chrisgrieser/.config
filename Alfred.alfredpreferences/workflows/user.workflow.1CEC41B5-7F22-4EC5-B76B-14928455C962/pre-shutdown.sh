39b1a41b7c68a0a8e416321506ad0f13c16d607d
🤖 automated <cron@job>
2022-08-24 15:09:04 +0200

    iMac Leuthingerweg (1 file)

--- Alfred.alfredpreferences/workflows/user.workflow.1CEC41B5-7F22-4EC5-B76B-14928455C962/pre-shutdown.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.1CEC41B5-7F22-4EC5-B76B-14928455C962/pre-shutdown.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env zsh
 cd ~/dotfiles/ || exit 1
-filesChanged="$(git status --porcelain | wc -l | tr -d ' ')"
-if [[ -n "$filesChanged" ]] ; then
+if [[ -n "$(git status --porcelain)" ]] ; then
 	echo "📴 pre-shutdown-sync dotfiles" >> ~'/dotfiles/hammerspoon/logs/sync.log'
 	zsh ~"/dotfiles/git-dotfile-sync.sh"
+
+	if [[ -n "$(git status --porcelain)" ]] ; then
+		echo "Dotfile-Repo not clean."
+		exit 1
+	fi
 fi
 
 cd ~"/Main Vault/" || exit 1
-filesChanged="$(git status --porcelain | wc -l | tr -d ' ')"
-if [[ -n "$filesChanged" ]] ; then
+if [[ -n "$(git status --porcelain)" ]] ; then
 	echo "📴 pre-shutdown-sync vault" >> ~'/dotfiles/hammerspoon/logs/sync.log'
 	zsh ~"/Main Vault/Meta/git-vault-sync.sh"
+	if [[ -n "$(git status --porcelain)" ]] ; then
+		echo "Vault-Repo not clean."
+		exit 1
+	fi
 fi
 
-#-------------------------------------------------------------------------------
+echo "success"
 
