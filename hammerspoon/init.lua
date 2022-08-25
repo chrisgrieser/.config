ad830090609fe673612acd35aaa5e7bec1b42abd
🤖 automated <cron@job>
2022-08-25 11:36:06 +0200

    iMac Helmholtz (4 files)

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -19,11 +19,10 @@ require("dark-mode")
 require("app-specific-behavior")
 require("twitterrific-controls")
 if isAtMother() then require("hot-corner-action") end
--- require("app-switcher")
 
 -- START
-systemStart()
-reloadAllMenubarItems()
+reloadAllMenubarItems() ---@diagnostic disable-line: undefined-global
+systemStart() ---@diagnostic disable-line: undefined-global
 notify("Config reloaded")
 
 
