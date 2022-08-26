33da543d42dd88ca55277a1b094e71caf091f62a
🤖 automated <cron@job>
2022-08-26 18:59:41 +0200

    iMac Helmholtz (2 files)

--- zsh/terminal_utils.zsh
+++ zsh/terminal_utils.zsh
@@ -42,14 +42,14 @@ function t(){
 	local orignal=$(alacritty-colorscheme status | cut -d. -f1)
 	local input="$*"
 	read -r -d '' colordemo << EOM
-\033[1;30mblack  \033[0m  \033[1;40mblack\033[0m
-\033[1;31mred    \033[0m  \033[1;41mred\033[0m
-\033[1;32mgreen  \033[0m  \033[1;42mgreen\033[0m
-\033[1;33myellow \033[0m  \033[1;43m\033[1;30myellow\033[0m
-\033[1;34mblue   \033[0m  \033[1;44mblue\033[0m
-\033[1;35mmagenta\033[0m  \033[1;45mmagenta\033[0m
-\033[1;36mcyan   \033[0m  \033[1;46m\033[1;30mcyan\033[0m
-\033[1;37mwhite  \033[0m  \033[1;47m\033[1;30mwhite\033[0m
+\033[1;30mblack  \033[0m  \033[1;40mblack   \033[0m
+\033[1;31mred    \033[0m  \033[1;41mred     \033[0m
+\033[1;32mgreen  \033[0m  \033[1;42mgreen   \033[0m
+\033[1;33myellow \033[0m  \033[1;43m\033[1;30myellow  \033[0m
+\033[1;34mblue   \033[0m  \033[1;44mblue    \033[0m
+\033[1;35mmagenta\033[0m  \033[1;45mmagenta \033[0m
+\033[1;36mcyan   \033[0m  \033[1;46m\033[1;30mcyan    \033[0m
+\033[1;37mwhite  \033[0m  \033[1;47m\033[1;30mwhite   \033[0m
 EOM
 
 	# --preview-window=0 results in a hidden preview window, with the preview
@@ -64,7 +64,7 @@ EOM
 					--layout=reverse \
 					--info=inline \
 					--preview-window="right,70%,border-left" \
-					--preview="alacritty-colorscheme apply {}.yaml || alacritty-colorscheme apply {}yml ;echo \"\n$colordemo\"" \
+					--preview="alacritty-colorscheme apply {}.yaml || alacritty-colorscheme apply {}.yml ;echo \"\n$colordemo\"" \
 	         )
 
 	# re-apply original color scheme when aborting
