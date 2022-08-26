ddbee23ef88758b6f767dbce2235cdd5eebf6f5f
🤖 automated <cron@job>
2022-08-26 16:18:41 +0200

    iMac Helmholtz (1 file)

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -26,28 +26,10 @@ systemStart() ---@diagnostic disable-line: undefined-global
 notify("Config reloaded")
 
 
-
-
-function mouseHighlight()
-	 -- Delete an existing highlight if it exists
-	 if mouseCircle then
-		mouseCircle:delete()
-		if mouseCircleTimer then
-			mouseCircleTimer:stop()
-		end
-	 end
-	 -- Get the current co-ordinates of the mouse pointer
-	 mousepoint = hs.mouse.absolutePosition()
-	 -- Prepare a big red circle around the mouse pointer
-	 mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
-	 mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
-	 mouseCircle:setFill(false)
-	 mouseCircle:setStrokeWidth(5)
-	 mouseCircle:show()
-
-	 -- Set a timer to delete the circle after 3 seconds
-	 mouseCircleTimer = hs.timer.doAfter(3, function()
-													 mouseCircle:delete()
-													 mouseCircle = nil
-												 end)
- end
+menubarSubline = hs.drawing.rectangle({x=0, y=24, w=1920, h=1})
+menubarSubline:setStrokeColor(hs.drawing.color.black)
+menubarSubline:setFill(true)
+menubarSubline:setStrokeWidth(2)
+menubarSubline:show()
+-- menubarSubline:delete()
+menubarSubline = nill
