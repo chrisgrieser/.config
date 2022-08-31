a0297bedf1a7902db2761cce5da950b885a093bc
🤖 automated <cron@job>
2022-08-31 17:43:50 +0200

    iMac Helmholtz (3)

--- Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/playback.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.AA50E5A9-B85E-42FF-A5CF-6A5A51657B18/playback.sh
@@ -3,7 +3,12 @@
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 
 # run in subshell to avoid output, only pass if for notification
-notification=$(spt playback --$1 --format="$format")
+notification=$(spt playback --$1 --format="$format" 2>&1)
+# shellcheck disable=SC2181
+if [[ "$?" != "0" ]] ; then
+	echo -n "$notification"
+	exit 1
+fi
 
 # if shuffle isn't active, activate it
 flags=$(spt playback --status --format=%f)
