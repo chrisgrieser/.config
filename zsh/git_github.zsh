3b66309c16ed0040e6cca028b2557d837c328e1d
🤖 automated <cron@job>
2022-09-04 11:38:55 +0200

    iMac Helmholtz (2)

--- zsh/git_github.zsh
+++ zsh/git_github.zsh
@@ -91,7 +91,13 @@ function gittree(){
 	)
 }
 
+function gg(){
+	git branch --format='%(refname:short)'
+
+}
+
 alias gc="git checkout"
+alias gg="git checkout -" # go to last branch, analogues to `zz` switching to last directory
 alias add="git add -A"
 alias commit="git commit -m"
 alias push="git push"
