40ea8fbfb10ac6d178d7eec5832b6cdd5e26f6aa
🤖 automated <cron@job>
2022-09-07 14:43:24 +0200

    Mac mini (5)

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -9,18 +9,18 @@ require("splits")
 
 require("scroll-and-cursor")
 require("menubar")
--- require("system-and-cron")
+require("system-and-cron")
 require("filesystem-watchers")
-require("usb-watchers")
+if isIMacAtHome() then require("usb-watchers") end
 
 require("app-specific-behavior")
 require("twitterrific-controls")
 if isAtMother() then require("hot-corner-action") end
 
--- menubarLine() ---@diagnostic disable-line: undefined-global
--- reloadAllMenubarItems() ---@diagnostic disable-line: undefined-global
--- systemStart() ---@diagnostic disable-line: undefined-global
--- killIfRunning("Finder")
+menubarLine() ---@diagnostic disable-line: undefined-global
+reloadAllMenubarItems() ---@diagnostic disable-line: undefined-global
+killIfRunning("Finder")
+
 notify("Config reloaded")
 
-
+systemStart() ---@diagnostic disable-line: undefined-global
