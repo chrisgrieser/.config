f2691b2c702c5b0174e8aa421fcf5c6b4ed32b86
🤖 automated <cron@job>
2022-09-09 00:26:24 +0200

    iMac Helmholtz (4)

--- Alfred.alfredpreferences/workflows/user.workflow.2C71D560-C064-4035-BF73-525C405E349F/add-to-espanso-spellings.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.2C71D560-C064-4035-BF73-525C405E349F/add-to-espanso-spellings.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env zsh
 
-input="$*"
-var1=${input%-*}
-var2=${input#*-}
+spell_config="$HOME/dotfiles/espanso/match/spelling.yml"
+sublcli="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+
+wrong=$(echo "$*" | cut -d" " -f1)
+correct=$(echo "$*" | cut -d" " -f2)
+
+{ echo "  - trigger: $wrong" ;
+  echo "    replace: $correct" ;
+  echo "    word: true";        } >> "$spell_config"
+
+lines=$(wc -l "$spell_config")
+
+"$sublcli" "$spell_config:$lines"
