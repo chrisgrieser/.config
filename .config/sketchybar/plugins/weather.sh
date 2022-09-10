ba5c0cf207851eb2664010661f5045d8a832cc7f
🤖 automated <cron@job>
2022-09-10 12:16:00 +0200

    iMac Helmholtz (12)

--- .config/sketchybar/plugins/weather.sh
+++ .config/sketchybar/plugins/weather.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-weather=$(curl "https://wttr.in/Berlin?format=1" | cut -d"+" -f2)
+weather=$(curl "https://wttr.in/Berlin?format=1" | cut -d"+" -f2 | tr -d "C")
 [[ "$weather" =~ "Unknown" ]] && weather="–"
 sketchybar --set "$NAME" label="$weather"
 
--- hammerspoon/dark-mode.lua
+++ hammerspoon/dark-mode.lua
@@ -58,7 +58,7 @@ function toggleDarkMode ()
 
 	hs.application(prevApp):activate()
 	holeCover() ---@diagnostic disable-line: undefined-global
-	hs.execute("brew services restart sketchybar") -- restart instead of reload to load colors
+	hs.execute("export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH ; brew services restart sketchybar") -- restart instead of reload to load colors
 end
 
 function isDarkMode()
