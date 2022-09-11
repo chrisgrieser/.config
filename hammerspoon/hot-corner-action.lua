c040c6ff559f290a3b602fdc5002b737c7020477
🤖 automated <cron@job>
2022-09-11 19:12:14 +0200

    iMac Helmholtz (10)

--- hammerspoon/hot-corner-action.lua
+++ hammerspoon/hot-corner-action.lua
@@ -2,29 +2,18 @@
 --------------------------------------------------------------------------------
 
 -- HOT CORNER Use "Quick Note" as Pseudo Hot Corner Action
--- to trigger something else instead
-hs.execute("defaults write com.apple.dock wvous-br-corner -int 14")
+
+-- REQUIREMENTS:
+-- - hs.execute("defaults write com.apple.dock wvous-br-corner -int 14")
+-- - only Accessibility Keyboard being enabled in the Accessibility Shortcuts
+
 function hotcornerWatcher(appName, eventType)
 	if not(eventType == aw.activated and appName == "Notes") then return end
 
 	hs.application("Notes"):kill9()
-	hs.shortcuts.run("Keyboard on-screen")
+	-- using hs.eventtap.keyStroke somehow does not seem to work properly
+	-- triggering via  Shotcuts ro menubar Gui also does not seem to be reliable
+	hs.osascript.applescript('tell application "System Events" to key code 96 using {command down, option down}')
 end
 hotcornerEmulation = hs.application.watcher.new(hotcornerWatcher)
 hotcornerEmulation:start()
-
--- function listShortcuts()
--- 	local shortcuts = hs.shortcuts.list()
--- 	for i = 1, #shortcuts do
--- 		shortcuts[i].text = shortcuts[i].name
--- 		shortcuts[i].name = nil
--- 	end
--- 	return shortcuts
--- end
-
--- shortcutsChooser = hs.chooser.new(function(selectedItem)
--- 	hs.shortcuts.run(selectedItem.text)
--- 	notify (selectedItem.text)
--- end)
--- shortcutsChooser:choices(listShortcuts)
-
