86ba5b35357a4c95a0672b307c4714f756af31ac
🤖 automated <cron@job>
2022-09-14 00:51:44 +0200

    iMac Home (1)

--- Utility Scripts/web-article-scrapper.sh
+++ Utility Scripts/web-article-scrapper.sh
@@ -7,14 +7,31 @@ OUTPUT_FOLDER="$2"
 [[ -z "$OUTPUT_FOLDER" ]] && OUTPUT_FOLDER="."
 
 # install dependencies
-command mercury-parser || npm -g install @postlight/mercury-parser
-command turndown-cli || npm -g install turndown-cli
-command yq || brew install yq # could also use `jq`
-command markdownlint || brew install yq
+if ! command mercury-parser ; then
+	echo "mercury-parser not installed."
+	echo "install: npm -g install @postlight/mercury-parser"
+fi
+if ! command turndown-cli ; then
+	echo "turndown-cli not installed."
+	echo "install: npm -g install turndown-cli"
+fi
+if ! command yq ; then
+	echo "yq not installed."
+	echo "install: brew install yq"
+fi
+if ! command mercury-parser ; then
+	echo "mercury-parser not installed."
+	echo "install: npm -g install @postlight/mercury-parser"
+fi
+if ! command markdownlint ; then
+	echo "markdownlint not installed."
+	echo "install: brew markdownlint-cli"
+fi
 if ! command gather ; then
-	echo "gather-cli not installed. "
+	echo "gather-cli not installed."
 	echo "Install from here: https://github.com/ttscoff/gather-cli"
 	echo "(brew tap requires 12gb Xcode install, so download the package instead…)"
+	exit 1
 fi
 
 # Mercury Reader Version
@@ -28,12 +45,12 @@ excerpt=$(echo "$parsed_data" | yq .excerpt)
 word_count=$(echo "$parsed_data" | yq .word_count)
 
 frontmatter=$(echo "---" ; echo "title: $title" ; echo "author: $author" ; echo "date: $date_published" ; echo "excerpt: $excerpt" echo "word: $word_count" ; echo "source: $URL" ; echo "---" ; echo)
-turndown-cli --head=2 --hr=2 --bullet=2 --code=2 --em=2 --strong=2 temp.html
+turndown-cli --head=2 --hr=2 --bullet=2 --code=2 temp.html
 rm ./*.html ./*.md
 output1="$frontmatter\n$content"
 
 # Gather Version
-output2=$(gather --inline-links --include-title --metadata-yaml --no-include-source "$URL" | markdownlint --fix --quiet --stdin)
+output2=$(gather --inline-links --no-include-source "$URL" | markdownlint --fix --quiet --stdin)
 
 # Output & Diff
 # (Diff as a form of quality control)
