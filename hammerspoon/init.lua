4b7dbd14ac5ebd698849c091c556b336fa09c740
🤖 automated <cron@job>
2022-09-15 15:23:59 +0200

    iMac Home (9)

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -22,15 +22,16 @@ systemStart() ---@diagnostic disable-line: undefined-global
 --------------------------------------------------------------------------------
 -- https://github.com/dbalatero/VimMode.spoon#configuration
 
--- VimMode = hs.loadSpoon('VimMode')
--- vim = VimMode:new()
--- vim
--- 	:disableForApp('Sublime Text')
--- 	:disableForApp('alacritty')
--- 	:disableForApp('Obsidian')
--- 	:setAlertFont('Recursive')
--- 	:bindHotKeys({ enter = {{'cmd'}, 'f19'} }) -- Karabiner: tap left-opt
--- 	:enableBetaFeature('block_cursor_overlay')
+VimMode = hs.loadSpoon('VimMode')
+vim = VimMode:new()
+vim:setAlertFont('Recursive')
+	:bindHotKeys({ enter = {{'cmd'}, 'f19'} }) -- Karabiner: tap left-opt
+	:enableBetaFeature('block_cursor_overlay')
+
+	:disableForApp('Sublime Text')
+	:disableForApp('alacritty')
+	:disableForApp('Alacritty')
+	:disableForApp('Obsidian')
 
 --------------------------------------------------------------------------------
 
