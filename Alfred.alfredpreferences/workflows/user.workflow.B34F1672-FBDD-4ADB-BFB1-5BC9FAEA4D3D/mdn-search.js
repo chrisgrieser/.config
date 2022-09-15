a5e1f4f978c914cc794f6c812919d4b85ea17909
🤖 automated <cron@job>
2022-09-15 21:25:35 +0200

    iMac Home (7)

--- Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/mdn-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/mdn-search.js
@@ -8,19 +8,31 @@ function run (argv) {
 	const onlineJSON = (url) => JSON.parse (app.doShellScript(`curl -sL '${url}'`));
 
 	//------------------------------------------------------------------------------
-	const query = argv.join("");
-	const baseURI =
-	const resultsJSON = onlineJSON("https://developer.mozilla.org/api/v1/search?q=" + query)
-		.documents
-		.map(item => {
 
+	const query = argv.join("");
+	const baseURL = "https://developer.mozilla.org";
+	const searchAPI = "https://developer.mozilla.org/api/v1/search?q=";
+
+	const resultsArr = onlineJSON(searchAPI + query).documents;
+	let output = [];
+
+	if (resultsArr.length === 0) {
+		output = [{
+			"title": "No documents found.",
+			"valid": false,
+			"arg": "no"
+		}];
+	} else {
+		output = resultsArr.map(item => {
+			const url = baseURL + item.mdn_url;
 			return {
-				"title": item,
-				"match": alfredMatcher (item),
-				"subtitle": item,
-				"arg": item,
+				"title": item.title,
+				"match": alfredMatcher(item.title),
+				"subtitle": item.summary,
+				"arg": url,
 			};
 		});
+	}
 
-	return JSON.stringify({ items: resultsJSON });
+	return JSON.stringify({ items: output });
 }
