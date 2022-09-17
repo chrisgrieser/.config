5b543ff88b22669ea0f4957a533241789e8b334f
🤖 automated <cron@job>
2022-09-18 00:54:18 +0200

    iMac Home (10)

--- Marta/scripts/cut.sh
+++ Marta/scripts/cut.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env zsh
 
-# clear from previous attempts
 pseudo_clipboard="/tmp/pseudo-clipboard/"
+
+# clear from previous attempts
 rm -r "$pseudo_clipboard"
 mkdir "$pseudo_clipboard"
 
 for arg in "$@" ; do
-	cp "$arg" "$pseudo_clipboard"
+	cp -R "$arg" "$pseudo_clipboard"
 	mv "$arg" "$HOME/.Trash" # leave copy in trash
 done
 
-osascript "/Users/chrisgrieser/dotfiles/Marta/scripts/copy-to-clipboard.applescript" "$pseudo_clipboard/*"
+osascript "/Users/chrisgrieser/dotfiles/Marta/scripts/copy-to-clipboard.applescript" "$pseudo_clipboard/"*
 
 afplay "/System/Library/Sounds/bottle.aiff"
