ad647b9dae12dfd695304f3082cada772e57c8e0
🤖 automated <cron@job>
2022-09-18 21:04:50 +0200

    iMac Home (6)

--- Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/restart-espanso.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/restart-espanso.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env zsh
 
-if pgrep -x "espanso" > /dev/null; then
-	killall "espanso" # needs to be lowercase
-	sleep 1
-fi
+# if pgrep -x "espanso" > /dev/null; then
+# 	killall "espanso" # needs to be lowercase
+# 	sleep 1
+# fi
 
-open -a "Espanso"
-echo "🟨 Espanso restarted"
+# open -a "Espanso"
+# echo "🟨 Espanso restarted"
+
+echo "inactive"
