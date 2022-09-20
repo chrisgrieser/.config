39f99abd0eee1f0c879819bf589d8e38f41f1efd
🤖 automated <cron@job>
2022-09-20 16:27:27 +0200

    iMac Home (1)

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -25,7 +25,8 @@ vim = VimMode:new()
 vim:setAlertFont('SF Mono')
 	:bindHotKeys({ enter = {{'cmd'}, 'f19'} }) -- Karabiner: tap left-opt
 	:enableBetaFeature('block_cursor_overlay')
-	:disableForApp('Sublime Text')
+
+vim:disableForApp('Sublime Text')
 	:disableForApp('alacritty')
 	:disableForApp('Alacritty')
 	:disableForApp('Obsidian')
