f0cb693deb3c6f330c3e0272506c2b80b47df51b
🤖 automated <cron@job>
2022-09-22 16:02:18 +0200

    iMac Home (3)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,34 +4,41 @@
 -- vim.o: global-scoped
 -- vim.bo: buffer-scoped
 -- vim.wo: window-scoped
+-- vim.opt: set option
+-- vim.g: global variables
 
 --------------------------------------------------------------------------------
 -- SETTINGS
 
 -- search
-vim.o.smartcase = true
-vim.o.incsearch = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
 
 -- tabs & indentation
-vim.bo.autoindent = true
-vim.bo.smartindent = true
-vim.o.tabstop = 3
-vim.o.softtabstop = 3
-vim.o.shiftwidth = 3
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 3
+vim.opt.softtabstop = 3
+vim.opt.shiftwidth = 3
 
 -- gutter
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.wo.signcolumn = 'yes'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
 
 -- status bar
-vim.o.showmode = true
-
--- misc
-vim.wo.wrap = false
 
 --------------------------------------------------------------------------------
 
 -- KEYBINDINGS
 
 vim.g.mapleader = ','
+
+local key_mapper = function(mode, key, result)
+	vim.opt.nvim_set_keymap(
+			mode,
+			key,
+			result,
+			{noremap = true, silent = true}
+		)
+end
