ccf427a798e5c180552418f3326ac7043b4417be
🤖 automated <cron@job>
2022-09-24 22:10:58 +0200

    iMac Mother (5)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,8 @@
 -- https://bryankegley.me/posts/nvim-getting-started/
--- https://neovim.io/doc/user/vim_diff.html
 --------------------------------------------------------------------------------
 
--- required for homebrew installs (where the runtimepath is in the homebrew dir)
-vim.opt.runtimepath:append(', "~/.config/nvim/lua"') 
+vim.opt.runtimepath:append(', "~/.config/nvim/lua"') -- required for homebrew installs (where the runtimepath is in the homebrew dir)
+vim.opt.packpath:append(', "~/.config/nvim/plugins"')
 
 require("options")
 require("keybindings")
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,25 @@
 --------------------------------------------------------------------------------
 -- PACKER SETUP
 
+-- https://bryankegley.me/posts/nvim-getting-started/
+
+-- ensure that packer is installed
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	vim.api.nvim_command('!git clone --depth=1 https://github.com/wbthomason/packer.nvim '..install_path)
+	vim.api.nvim_command 'packadd packer.nvim'
+end
+vim.cmd('packadd packer.nvim')
+local packer = require('packer')
+local util = require('packer.util')
+packer.init({
+	package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+
+packer.startup(function (use)
+	-- add you plugins here like:
+	-- use 'neovim/nvim-lspconfig'
+end )
 
 --------------------------------------------------------------------------------
 -- CRUCIAL PLUGINS
