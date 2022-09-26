607f1a0f5cfd6b4dd7270931d16e15beeb90f8d8
🤖 automated <cron@job>
2022-09-26 18:44:31 +0200

    iMac Mother (7)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,8 @@
--- https://bryankegley.me/posts/nvim-getting-started/
---------------------------------------------------------------------------------
 
-vim.opt.runtimepath:append(', "~/.config/nvim/lua"') -- required for homebrew installs (where the runtimepath is in the homebrew dir)
+vim.opt.runtimepath:append(', "~/.config/nvim/lua"') -- apparently required for homebrew installs where the runtimepath is missing the .config directory?!
 
 require("options")
 require("keybindings")
 require("plugins")
 
--------------------------------------------------------------------------------
 
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,25 +1,43 @@
--- PACKER SETUP
-
--- ensure that packer is installed https://bryankegley.me/posts/nvim-getting-started/
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.notify("Packer not installed. Installing Packer...", vim.log.levels.INFO)
-	vim.cmd('!git clone --depth=1 https://github.com/wbthomason/packer.nvim '..install_path)
+-- auto-install packer if not installed https://github.com/wbthomason/packer.nvim#bootstrapping
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		print("Packer not installed. Installing...")
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
 end
-vim.cmd('packadd packer.nvim')
-local packer = require('packer')
-local util = require('packer.util')
-packer.init({
-	package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+
+local packer_bootstrap = ensure_packer()
+
+--------------------------------------------------------------------------------
+-- re-sync packer if this file changes -> https://www.reddit.com/r/neovim/comments/vqjz87/comment/ievd4tx/?utm_source=share&utm_medium=web2x&context=3
+local group = vim.api.nvim_create_augroup("packer_user_config", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+	command = "source <afile> | PackerSync",
+	pattern = "plugins.lua", -- the name of your plugins file
+	group = group,
 })
+
+-- Protected call to make sure that packer is installed
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then return end
 
 --------------------------------------------------------------------------------
 
-packer.startup(function (use)
-
+return packer.startup(function(use)
 	use 'wbthomason/packer.nvim' -- packer manages itself
+	-- color scheme
+	use 'folke/tokyonight.nvim'
 
-	use 'folke/tokyonight.nvim' -- color scheme
-end )
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		packer.sync()
+	end
+end)
 
 
