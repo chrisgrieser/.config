d5c0482d4e804aa101fc8d3a88f7ec8c9d3a5a33
🤖 automated <cron@job>
2022-09-27 17:24:14 +0200

    iMac Mother (5)

---
+++ .config/nvim/lua/plugin-list.lua
@@ -0,0 +1,36 @@
+function pluginList (use)
+	use 'wbthomason/packer.nvim' -- packer manages itself
+
+	-- Appearance
+	use 'folke/tokyonight.nvim' -- color scheme
+	use "lukas-reineke/indent-blankline.nvim" -- indentation guides
+	use "unblevable/quick-scope" -- highlight for f and t movements
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+	-- Utility
+	use 'farmergreg/vim-lastplace' -- remember last cursor position on file re-opening
+	use {
+		'nvim-telescope/telescope.nvim', -- fuzzy finder
+		requires = {
+			'nvim-lua/plenary.nvim', -- requirement
+			'kyazdani42/nvim-web-devicons' --icons for fuzzy finder
+		}
+	}
+
+	-- Editing
+	use 'tpope/vim-commentary' -- comments
+	use 'tpope/vim-surround' -- surround with punctuation
+	use 'wellle/targets.vim' -- various text object improvements
+	use 'michaeljsmith/vim-indent-object' -- indention-based text objects
+
+	-- check out later?
+	-- use {
+	-- 	'nvim-treesitter/nvim-treesitter-context',
+	-- 	requires = {'nvim-treesitter/nvim-treesitter'},
+	-- }
+end
+
+
+
