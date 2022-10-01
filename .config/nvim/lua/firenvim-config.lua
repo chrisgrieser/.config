c78ac0cbbb58551d2eafd890effd1cdc97d47e44
👨‍💻 pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-10-02 01:59:35 +0200

    patch

--- .config/nvim/lua/firenvim-config.lua
+++ .config/nvim/lua/firenvim-config.lua
@@ -1,13 +1,29 @@
 require("utils")
+--------------------------------------------------------------------------------
 
-opt.laststatus = 0
+-- spelling
+opt.spell = true
+opt.spelllang = "en_us"
+
+-- wrapping and related options
+opt.wrap = true -- soft wrap
+opt.linebreak = true -- do not break words for soft wrap
+keymap({"n", "v"}, "j", "gj")
+keymap({"n", "v"}, "k", "gk")
+keymap({"n", "v"}, "H", "g^")
+keymap({"n", "v"}, "L", "g$")
+opt.colorcolumn = ''
+
+-- Appearance
 cmd[[colorscheme tokyonight]]
+opt.laststatus = 0
+opt.textwidth = 0
 
 -- relevant for spellcheck suggestions
 require("telescope").setup {
 	defaults = {
 		mappings = {
-			i = { ["<Esc>"] = require('telescope.actions').close, -- close w/ one esc }, }
+			i = { ["<Esc>"] = require('telescope.actions').close }, -- close w/ one esc
 		},
 	}
 }
