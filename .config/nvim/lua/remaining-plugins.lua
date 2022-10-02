d2fa43b7cd10f2e54cb70fa2ff95a9c0355ac5c7
🤖 automated <cron@job>
2022-10-02 21:24:16 +0200

    iMac Home (2)

--- .config/nvim/lua/remaining-plugins.lua
+++ .config/nvim/lua/remaining-plugins.lua
@@ -20,24 +20,26 @@ autocmd("FileType", {
 g.user_emmet_mode='i'
 
 -- Wilder
+-- INFO: "UpdateRemotePlugins" may be necessary https://github.com/gelguy/wilder.nvim/issues/158
+
 local wilder = require('wilder')
 wilder.setup({modes = {':', '/', '?'}})
-
-wilder.set_option('pipeline', {
-	wilder.branch(
-		wilder.cmdline_pipeline(),
-		wilder.search_pipeline()
-	),
-})
-
 wilder.set_option('renderer', wilder.popupmenu_renderer(
 	wilder.popupmenu_border_theme({
 		highlighter = wilder.basic_highlighter(),
 		min_width = '30%',
-		left = {' ', wilder.popupmenu_devicons()},
 		min_height = '50%',
+		max_height = '50%',
+		left = {' ', wilder.popupmenu_devicons()},
 		reverse = 0,
 		border = 'rounded',
+		highlights = {
+			accent = wilder.make_hl('WilderAccent', 'Pmenu', {{foreground = 'Magenta'}, {foreground = 'Magenta'}, {foreground = '#f4468f'}}),
+		},
 	})
 ))
+
+cmd[[highlight WilderAccent ctermfg=LightMagenta]]
+
+
 
---
+++ .config/nvim/lua/wilder-config.lua
@@ -0,0 +1,27 @@
+-- Wilder
+-- INFO: "UpdateRemotePlugins" may be necessary https://github.com/gelguy/wilder.nvim/issues/158
+
+local wilder = require('wilder')
+wilder.setup({modes = {':', '/', '?'}})
+wilder.set_option('renderer', wilder.popupmenu_renderer(
+	wilder.popupmenu_border_theme({
+		highlighter = wilder.basic_highlighter(),
+		min_width = '30%',
+		min_height = '50%',
+		max_height = '50%',
+		left = {' ', wilder.popupmenu_devicons()},
+		reverse = 0,
+		border = 'rounded',
+		highlights = {
+			accent = wilder.make_hl('WilderAccent', 'Pmenu', {
+				{foreground = 'Magenta'}, -- term
+				{foreground = 'Magenta'}, -- cterm
+				{foreground = '#f4468f'} -- gui
+			}),
+		},
+	})
+))
+
+
+
+
