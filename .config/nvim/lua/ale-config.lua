884f84a353ae38835db9af19bd244f588754bafe
🤖 automated <cron@job>
2022-10-04 18:10:54 +0200

    iMac Home (2)

--- .config/nvim/lua/ale-config.lua
+++ .config/nvim/lua/ale-config.lua
@@ -4,7 +4,25 @@ g.ale_disable_lsp = 1 -- lsp already via coc
 
 g.ale_cursor_detail = 1 -- open popup instead of echoing the linter msg in the cmdline
 g.ale_close_preview_on_insert = 1
+g.ale_detail_to_floating_preview = 1
 
+g.ale_use_global_executables = 1 -- | can be set to `1` in your vimrc file to make
 
- keymap("n", "ga","<Plug>(ale_fix)")
- keymap("n", "ga",":ALEFixSuggest<CR>") -- suggest fixers
+--------------------------------------------------------------------------------
+
+g.ale_linters = {
+	javascript = {"eslint"},
+	typescript = {"eslint"},
+	css = {"stylelint"},
+	markdown = {"markdownlint"},
+	python = {"markdownlint"},
+	zsh = {"shellcheck"}
+}
+
+--------------------------------------------------------------------------------
+
+keymap("n", "<leader>f","<Plug>(ale_fix)") -- fix single instance
+keymap("n", "<leader>L","<Plug>(ale_lint)") -- line current file
+
+keymap("n", "gl","<Plug>(ale_next_wrap)")
+keymap("n", "gL","<Plug>(ale_previous_wrap)")
