8293668bd1e5a1efc9ef32fe67c8abc0157111ba
🤖 automated <cron@job>
2022-10-05 14:00:55 +0200

    iMac Home (5)

--- .config/nvim/lua/ale-config.lua
+++ .config/nvim/lua/ale-config.lua
@@ -6,12 +6,13 @@ g.ale_disable_lsp = 1
 -- INFO: setting for redirecting coc output to ALE in "coc-settings.json"
 -- https://github.com/dense-analysis/ale#5iii-how-can-i-use-ale-and-cocnvim-together
 --------------------------------------------------------------------------------
-g.ale_cursor_detail = 1 -- open popup instead msg
-g.ale_close_preview_on_insert = 1
+g.ale_cursor_detail = 0 -- open popup instead msg
 g.ale_detail_to_floating_preview = 0 -- bottom panel
+g.ale_set_balloons = 1 -- hover
 
 g.ale_use_global_executables = 1 -- globally installed listers
 
+-- https://github.com/dense-analysis/ale/blob/master/doc/ale-sh.txt
 
 --------------------------------------------------------------------------------
 
