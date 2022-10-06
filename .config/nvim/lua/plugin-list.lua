55014112661c35baeec55270256dad6ab125be76
🤖 automated <cron@job>
2022-10-07 00:26:26 +0200

    iMac Home (4)

--- .config/nvim/lua/plugin-list.lua
+++ .config/nvim/lua/plugin-list.lua
@@ -41,8 +41,8 @@ function PluginList (use)
 	use 'nvim-lualine/lualine.nvim' -- status bar
 	use 'airblade/vim-gitgutter' -- changes in gutter
 	use 'f-person/auto-dark-mode.nvim' -- auto-toggle themes with OS dark/light mode
-	-- use 'norcalli/nvim-colorizer.lua' -- color previews
-	use 'brenoprata10/nvim-highlight-colors'
+	-- use 'brenoprata10/nvim-highlight-colors'-- color previews
+	use 'uga-rosa/ccc.nvim'
 
 	-- File Management & Switching
 	use 'tpope/vim-eunuch' -- file operation utilities
