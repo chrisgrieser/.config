7d1dcaae424c981e8cd64142d3105056c45f85a6
🤖 automated <cron@job>
2022-10-08 17:35:03 +0200

    iMac Home (3)

--- .config/nvim/lua/mason-config.lua
+++ .config/nvim/lua/mason-config.lua
@@ -20,3 +20,36 @@ require("mason-lspconfig").setup({
 	},
 })
 
+-- Mappings.
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', 'ge', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', 'gE', vim.diagnostic.goto_prev, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap=true, silent=true, buffer=bufnr }
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', 'gD', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', '<leader>R', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<leader>,', vim.lsp.buf.code_action, bufopts)
+end
+
+local lsp_flags = {
+	debounce_text_changes = 150, -- This is the default in Nvim 0.7+
+}
+require('lspconfig')['tsserver'].setup{
+	on_attach = on_attach,
+	flags = lsp_flags,
+}
+require('lspconfig')['sumneko_lua'].setup{
+	on_attach = on_attach,
+	flags = lsp_flags,
+}
