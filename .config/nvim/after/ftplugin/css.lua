e470326643865014d81f228878c508558ea1cf6c
🤖 automated <cron@job>
2022-10-10 18:46:04 +0200

    iMac Home (3)

--- .config/nvim/after/ftplugin/css.lua
+++ .config/nvim/after/ftplugin/css.lua
@@ -19,5 +19,5 @@ cmd[[setlocal iskeyword+=-]]
 -- various other solutions are described here: https://github.com/vim/vim/issues/2790
 -- however, using treesitter, this is less of an issue, but treesitter css
 -- highlighting isn't good yet, so...
-
+keymap("n", "zz", ":syntax sync fromstart<CR>", {buffer = true})
 
