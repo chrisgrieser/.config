1d26427867d0301f0df021073d57dedc13b4e3a5
🤖 automated <cron@job>
2022-10-11 12:11:34 +0200

    iMac Home (4)

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,17 +1,17 @@
 -- hide URLs and other formatting
 -- TODO: figure out how to *only* conceal markdown links
 -- https://www.reddit.com/r/vim/comments/h8pgor/til_conceal_in_vim/
-wo.conceallevel = 1
+opt.conceallevel = 1
 
 -- spelling
-wo.spell = true
+opt.spell = true
 
 -- wrapping and related options
-wo.wrap = true -- soft wrap
-wo.linebreak = true -- do not break words for soft wrap
+opt.wrap = true -- soft wrap
+opt.linebreak = true -- do not break words for soft wrap
 keymap({"n", "v"}, "j", "gj")
 keymap({"n", "v"}, "k", "gk")
 keymap({"n", "v"}, "H", "g^")
 keymap({"n", "v"}, "L", "g$")
-wo.colorcolumn = ''
+opt.colorcolumn = ''
 
