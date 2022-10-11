b6f949bc19dace9eff5da50c0014416fc82666dd
🤖 automated <cron@job>
2022-10-11 15:11:33 +0200

    iMac Home (3)

--- .config/nvim/lua/remaining-plugins.lua
+++ .config/nvim/lua/remaining-plugins.lua
@@ -1,3 +1,6 @@
+require("utils")
+--------------------------------------------------------------------------------
+
 -- netrw
 g.netrw_list_hide= '.*\\.DS_Store$,^./$' -- hide files created by macOS & current directory
 g.netrw_banner = 0 -- no ugly top banner
@@ -28,12 +31,13 @@ g.undotree_SetFocusWhenToggle = 1
 g.undotree_ShortIndicators = 1 -- for the relative date
 g.undotree_HelpLine = 0 -- 0 hides the "Press ? for help"
 cmd[[ function! g:Undotree_CustomMap()
-	nmap <buffer> <C-j> <plug>UndotreePreviousState
-	nmap <buffer> <C-k> <plug>UndotreeNextState
-	nmap <buffer> J jjjjjjj
-	nmap <buffer> K 7k
-	nmap <buffer> U <plug>UndotreeRedo
-	endfunc ]]
+nmap <buffer> <C-j> <plug>UndotreePreviousState
+nmap <buffer> <C-k> <plug>UndotreeNextState
+nmap <buffer> J jjjjjjj
+nmap <buffer> K 7k
+nmap <buffer> U <plug>UndotreeRedo
+endfunc
+]]
 
 -- Symbol outline
 require("symbols-outline").setup{
@@ -53,4 +57,65 @@ require("symbols-outline").setup{
 	},
 	lsp_blacklist = {},
 	symbol_blacklist = {"Enum", "EnumMember"},
+}
+
+-- whichkey
+opt.timeoutlen = 500
+require("which-key").setup {
+	plugins = {
+		marks = true, -- shows a list of your marks on ' and `
+		registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+		presets = {
+			operators = true,
+			motions = true,
+			text_objects = true,
+			windows = true, -- default bindings on <c-w>
+			nav = true, -- misc bindings to work with windows
+			z = true, -- bindings for folds, spelling and others prefixed with z
+			g = true, -- bindings for prefixed with g
+		},
+	},
+	-- add operators that will trigger motion and text object completion
+	-- to enable all native operators, set the preset / operators plugin above
+	operators = {
+		q = "Comment",
+		['ö'] = "Override",
+	},
+	key_labels = {
+		["<space>"] = "␣",
+		["<cr>"] = "↵",
+		["<tab>"] = "↹",
+	},
+	icons = {
+		breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+		separator = "➜", -- symbol used between a key and it's label
+		group = "+", -- symbol prepended to a group
+	},
+	popup_mappings = {
+		scroll_down = '<c-d>', -- binding to scroll down inside the popup
+		scroll_up = '<c-u>', -- binding to scroll up inside the popup
+	},
+	window = {
+		border = borderStyle, -- none, single, double, shadow
+		position = "bottom", -- bottom, top
+		margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+		padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+	},
+	layout = {
+		height = { min = 4, max = 25 }, -- min and max height of the columns
+		width = { min = 20, max = 50 }, -- min and max width of the columns
+		spacing = 3, -- spacing between columns
+		align = "left", -- align columns left, center or right
+	},
+	show_help = true, -- show help message on the command line when the popup is visible
+	triggers = "auto", -- automatically setup triggers
+	-- triggers = {"<leader>"} -- or specify a list manually
+	triggers_blacklist = {
+		-- list of mode / prefixes that should never be hooked by WhichKey
+		-- this is mostly relevant for key maps that start with a native binding
+		-- most people should not need to change this
+		i = { "j", "k" },
+		v = { "j", "k" },
+	},
+
 }
