b5ff6205a01b21b453124023b8345366f263db01
🤖 automated <cron@job>
2022-10-14 04:17:00 +0200

    iMac Home (3)

--- .config/nvim/lua/telescope-config.lua
+++ .config/nvim/lua/telescope-config.lua
@@ -1,5 +1,12 @@
 -- https://github.com/nvim-telescope/telescope.nvim#telescope-setup-structure
 
+local smallIvy = require("telescope.themes").get_ivy{
+	prompt_prefix = "  ",
+	initial_mode = "normal",
+	results_title = false,
+	layout_config = { bottom_pane = { height = 0.3 } }
+}
+
 require("telescope").setup {
 	defaults = {
 		selection_caret = "⟐ ",
@@ -36,35 +43,19 @@ require("telescope").setup {
 		lsp_document_symbols = { prompt_prefix='* ', show_line=false},
 		treesitter = { show_line=false, prompt_prefix='🌳' },
 		keymaps = { prompt_prefix='🔘' },
-		help_tags = { prompt_prefix=':h ' },
-		commands = { prompt_prefix=': ' },
+		find_files = { cwd='%:p:h', prompt_prefix=' ', hidden=true },
 		oldfiles = { prompt_prefix=' ' },
 		highlights = { prompt_prefix='🎨' },
-		marks = { prompt_prefix="' " },
 		buffers = {prompt_prefix='📑',ignore_current_buffer = true},
 		live_grep = {cwd='%:p:h', disable_coordinates=true, prompt_prefix='🔎'},
-		current_buffer_fuzzy_find = { prompt_prefix='🔍' },
-		spell_suggest = ( require('telescope.themes').get_ivy{
-			layout_config = {
-				bottom_pane = {
-					height = 0.3
-				}
-			}
-		} ),
-		colorscheme = { enable_preview = true, prompt_prefix='🎨', layout_strategy = "bottom_pane" },
-		find_files = { cwd='%:p:h', prompt_prefix=' ', hidden=true },
+		spell_suggest = ( smallIvy ),
+		colorscheme = { enable_preview = true, prompt_prefix='🎨' },
 	},
 	extensions = {
-		["ui-select"] = {
-			require("telescope.themes").get_ivy{
-				prompt_prefix = "  ",
-				initial_mode = "normal",
-				results_title = false,
-				layout_config = { bottom_pane = { height = 0.25 } }
-			}
-		}
+		["ui-select"] = { smallIvy }
 	}
 }
+
 
 -- use telescope for selections like code actions
 require("telescope").load_extension("ui-select")
