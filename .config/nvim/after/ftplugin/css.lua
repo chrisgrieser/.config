138a0a45d295151006847177f83289728c87e7fe
🤖 automated <cron@job>
2022-10-19 17:45:46 +0200

    iMac Home (4)

--- .config/nvim/after/ftplugin/css.lua
+++ .config/nvim/after/ftplugin/css.lua
@@ -18,4 +18,22 @@ cmd[[setlocal iskeyword+=-]]
 -- however, using treesitter, this is less of an issue, but treesitter css
 -- highlighting isn't good yet, so...
 keymap("n", "zz", ":syntax sync fromstart<CR>", {buffer = true})
+keymap("i", ",,", "<Plug>(emmet-expand-abbr)", {silent = true, buffer = true})
+
+
+keymap("n", "<leader>v", '^Ellct;') -- change [v]alue key
+keymap("n", "<leader>c", 'mzlEF.yEEp`z') -- double [c]lass under cursor
+keymap("n", "<leader>C", 'lF.d/[.\\s]<CR>:nohl<CR>') -- delete class under cursor
+
+-- toggle !important
+keymap("n", "<leader>i", function ()
+	local lineContent = fn.getline('.')
+	if lineContent:find("!important") then
+		lineContent = lineContent:gsub(" !important", "")
+	else
+		lineContent = lineContent:gsub(";", " !important;")
+	end
+	fn.setline(".", lineContent)
+	---@diagnostic enable: undefined-field, param-type-mismatch
+end, {buffer = true})
 
