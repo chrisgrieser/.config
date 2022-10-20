8af094ce5a837391df3fdbf88acd80f228118123
🤖 automated <cron@job>
2022-10-20 14:05:45 +0200

    iMac Home (1)

--- Alfred.alfredpreferences/workflows/user.workflow.0B6B4D67-DBC0-45DA-B720-71363DEBF31C/move-file-to-front-marta-dir.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.0B6B4D67-DBC0-45DA-B720-71363DEBF31C/move-file-to-front-marta-dir.sh
@@ -10,9 +10,13 @@ if [[ "$FRONT_APP" =~ "Finder" ]]; then
 		return POSIX path of pathToOpen
 	end tell')
 elif [[ "$FRONT_APP" =~ "Marta" ]]; then
-	TARGET_DIR=$(osascript -e 'tell 
-		if (count windows) is not 0 then set pathToOpen to target of window 1 as alias
-		return POSIX path of pathToOpen
-	end tell')
+	TARGET_DIR=$(osascript -e '
+		tell application "System Events" to set martaProcess to first process whose name is "Marta"
+		tell martaProcess to return name of front window
+	')
+	TARGET_DIR=${TARGET_DIR:11:-1}
 fi
+
 [[ -d "$TARGET_DIR" ]] || exit 1
+
+mv "$FILE_TO_MOVE" "$TARGET_DIR"
