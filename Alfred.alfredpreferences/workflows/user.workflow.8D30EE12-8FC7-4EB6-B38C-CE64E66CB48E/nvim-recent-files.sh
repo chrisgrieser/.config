8d9cf757cd2929ca6ed9b2538396ea69fea8926c
🤖 automated <cron@job>
2022-10-21 17:25:43 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/user.workflow.8D30EE12-8FC7-4EB6-B38C-CE64E66CB48E/nvim-recent-files.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.8D30EE12-8FC7-4EB6-B38C-CE64E66CB48E/nvim-recent-files.sh
@@ -4,6 +4,6 @@ temp=/tmp/oldfiles.txt
 [[ -e "$temp" ]] && rm "$temp"
 nvim -c "redir > $temp | silent oldfiles | redir end | q"
 
-cut -d" " -f2 "$temp" | while read -r line; do # only output existing files
+cut -d" " -f2- "$temp" | while read -r line; do # only output existing files
 	[[ ! -f "$line" ]] || echo "$line"
 done
