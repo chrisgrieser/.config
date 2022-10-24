a7a5a4242cb39a32c6e9c7124bcfc2c9274b8f1d
🤖 automated <cron@job>
2022-10-25 00:18:31 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/user.workflow.2C71D560-C064-4035-BF73-525C405E349F/add-to-espanso-spellings.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.2C71D560-C064-4035-BF73-525C405E349F/add-to-espanso-spellings.sh
@@ -9,6 +9,7 @@ correct=$(echo "$*" | cut -d" " -f2)
   echo "    replace: $correct" ;
   echo "    word: true";        } >> "$spell_config"
 
-lines=$(wc -l "$spell_config")
+LINE_NO=$(wc -l "$spell_config")
 
-alacritty --command nvim +$LINE_NO "$spell_config"
+# workaround for https://github.com/neovide/neovide/issues/1604
+open "$spell_config" --env=LINE=$LINE_NO
