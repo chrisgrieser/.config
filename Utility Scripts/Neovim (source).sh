896931ceb414bf44ca34fd828140ab528d4438a1
🤖 automated <cron@job>
2022-10-26 18:30:48 +0200

    iMac Home (5)

--- Utility Scripts/Neovim (source).sh
+++ Utility Scripts/Neovim (source).sh
@@ -1,3 +1,4 @@
+#!/bin/zsh
 # INFO: on every change of the Neovim.app, it needs to be granted Accessibility
 # permissions again to be able to send keystrokes
 
@@ -8,20 +9,20 @@ export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 if pgrep "neovide" ; then
 	prevClipboard="$(pbpaste)"
 	echo "$1" | pbcopy
-	osascript -e 'tell application "Neovide" to activate
+	osascript -e "tell application \"Neovide\" to activate
 		delay 0.07
-		tell application "System Events"
+		tell application \"System Events\"
 			key code 53
-			keystroke ":e "
-			keystroke "v" using {command down}
+			keystroke \":e +$LINE \"
+			keystroke \"v\" using {command down}
 			delay 0.05
 			keystroke return
-		end tell'
+		end tell"
 	echo "$prevClipboard" | pbcopy
 
 # workaround for: https://github.com/neovide/neovide/issues/1604
 else
-	if [[ -z "$LINE" ]] ; then
+	if [[ -z "$LINE" ]] ; then # $LINE is set via `open --env=LINE=num`
 		neovide "$1"
 	else
 		neovide "+$LINE" "$1"
