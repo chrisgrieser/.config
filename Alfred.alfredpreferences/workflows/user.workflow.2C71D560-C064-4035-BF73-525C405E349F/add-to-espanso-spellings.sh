8b26b17f5778466e3149ec0d5f13a3b60f447693
🤖 automated <cron@job>
2022-10-26 20:30:48 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/user.workflow.2C71D560-C064-4035-BF73-525C405E349F/add-to-espanso-spellings.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.2C71D560-C064-4035-BF73-525C405E349F/add-to-espanso-spellings.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 spell_config="$HOME/dotfiles/espanso/match/spelling.yml"
+wc -l "$spell_config"
 
 wrong=$(echo "$*" | cut -d" " -f1)
 correct=$(echo "$*" | cut -d" " -f2)
@@ -9,7 +10,8 @@ correct=$(echo "$*" | cut -d" " -f2)
   echo "    replace: $correct" ;
   echo "    word: true";        } >> "$spell_config"
 
-LINE_NO=$(wc -l "$spell_config")
+LINE_NO=$(wc -l "$spell_config" | tr -s " " | cut -d" " -f2)
 
 # workaround for https://github.com/neovide/neovide/issues/1604
 open "$spell_config" --env=LINE=$LINE_NO
+
