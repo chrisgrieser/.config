a4d219b0d0ba8500f2d7d49865fd51e3f144783e
🤖 automated <cron@job>
2022-10-27 17:21:41 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/ydl.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.0621D410-7C9B-4BF5-8492-02A0BAE4A3D0/ydl.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 export PATH=/usr/local/bin/:/opt/homebrew/bin/:$PATH
+
 download_location="${download_location/#\~/$HOME}"
 CURRENT_TAB=$(osascript -e 'tell application "Brave Browser" to return URL of active tab of front window')
 
 [[ -n "$CURRENT_TAB" ]] || exit 1
 cd "$download_location" || exit 1
 
-if youtube-dl --quiet "$CURRENT_TAB" ; then
+if yt-dlp --quiet "$CURRENT_TAB" ; then
 	echo "✅ Download finished."
 else
 	echo "❌ Not possible"
