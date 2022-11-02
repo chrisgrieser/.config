b645e5d80e5883a7aa9d4bd4cc165a2a6c624599
🤖 automated <cron@job>
2022-11-02 22:09:00 +0100

    iMac Home (3)

--- .config/nvim/lua/duplicate.lua
+++ .config/nvim/lua/duplicate.lua
@@ -1,12 +1,50 @@
-local fn = vim.fn
-local api = vim.api
-local getCursor = vim.api.nvim_win_get_cursor(0)
---------------------------------------------------------------------------------
+---@diagnostic disable: param-type-mismatch, undefined-field
+local getline = vim.fn.getline
+local append = vim.fn.append
+local getCursor = vim.api.nvim_win_get_cursor
+local setCursor = vim.api.nvim_win_set_cursor
 
-function duplicate()
-	local line = fn.getline(".") ---@diagnostic disable-line: param-type-mismatch
-	fn.append(".", line) ---@diagnostic disable-line: param-type-mismatch
+local dup = {}
+
+function dup.setup(options) ---@diagnostic disable-line: unused-local
+	-- placeholder for potential future options
+end
+
+function dup.duplicateLine()
+	local line = getline(".") 
+	append(".", line)
 	local lineNum = getCursor(0)[1] + 1 -- line down
 	local colNum = getCursor(0)[2]
+	setCursor(0, {lineNum, colNum})
+end
+
+function dup.duplicateVisual()
+	local prevReg = vim.fn.getreg("z")
+	cmd[[silent! "zy`]"zp]]
+	vim.fn.setreg("z", prevReg)
+end
+
+function dup.smartDuplicateLine()
+	local line = getline(".")
+	if line:find("top") then line = line:gsub("top", "bottom")
+	elseif line:find("bottom") then line = line:gsub("bottom", "top")
+	elseif line:find("right") then line = line:gsub("right", "left")
+	elseif line:find("left") then line = line:gsub("left", "right")
+	elseif line:find("height") and not(line:find("line-height")) then
+		line = line:gsub("height", "width")
+	elseif line:find("width") and not(line:find("border-width")) and not(line:find("outline-width")) then
+		line = line:gsub("width", "height")
+	end
+	append(".", line)
+
+	-- cursor movement
+	local lineNum = getCursor(0)[1] + 1 -- line down
+	local colNum = getCursor(0)[2]
+	local _, valuePos = line:find(": ?")
+	if valuePos then -- if line was changed, move cursor to value of the property
+		colNum = valuePos 
+	end 
 	api.nvim_win_set_cursor(0, {lineNum, colNum})
 end
+
+return dup
