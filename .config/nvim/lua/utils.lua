49f254918cf9e8411abcb07d855f848ba9396d18
🤖 automated <cron@job>
2022-11-05 16:10:45 +0100

    iMac Home (2)

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,5 +1,5 @@
-opt = vim.opt -- global option
-g = vim.g -- global variable
+opt = vim.opt -- global options
+g = vim.g -- global variables
 api = vim.api
 fn = vim.fn
 cmd = vim.cmd
@@ -11,6 +11,29 @@ autocmd = vim.api.nvim_create_autocmd
 telescope = require("telescope.builtin") -- requires loading extensions first
 keymap = vim.keymap.set
 home = vim.fn.expand("~")
+
+--------------------------------------------------------------------------------
+
+-- General Lua Utility
+-- https://www.lua.org/manual/5.4/manual.html#pdf-string.gmatch
+
+---@param str string
+---@param separator string uses Lua Pattern, so requires escaping
+---@return table
+function split(str, separator)
+	str = str .. separator
+	local output = {}
+	for i in str:gmatch("(.-)" .. separator) do
+		table.insert(output, i)
+	end
+	return output
+end
+
+blubb = "bla.bli.blu"
+test = split(blubb, "b")[2]
+print(test)
+
+--------------------------------------------------------------------------------
 
 -- :setlocal does not have a direct access via the vim-module, it seems https://neovim.io/doc/user/lua.html#lua-vim-setlocal
 function setlocal(option, value)
@@ -25,11 +48,14 @@ end
 function inspectFn(obj)
 	vim.pretty_print(fn.luaeval(obj))
 end
-cmd[[:command! -nargs=1 I lua inspectFn(<f-args>)]]
+
+cmd [[:command! -nargs=1 I lua inspectFn(<f-args>)]]
 
 function isGui()
 	return g.neovide or g.goneovim
 end
+
+--------------------------------------------------------------------------------
 
 commonFiletypes = {
 	"lua",
