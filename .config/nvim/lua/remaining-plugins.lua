28d4ac2ed30a8dacea4a1802ce30bcb710d8cdb3
🤖 automated <cron@job>
2022-11-09 14:07:02 +0100

    iMac Home (2)

--- .config/nvim/lua/remaining-plugins.lua
+++ .config/nvim/lua/remaining-plugins.lua
@@ -57,27 +57,20 @@ cmd[[highlight def link QuickScopePrimary CurSearch]]
 
 --------------------------------------------------------------------------------
 
-
+-- Comments.nvim Undo
 ---Textobject for adjacent commented lines = qu for uncommenting
 -- https://github.com/numToStr/Comment.nvim/issues/22#issuecomment-1272569139
 local function commented_lines_textobject()
 	local U = require("Comment.utils")
 	local cl = vim.api.nvim_win_get_cursor(0)[1] -- current line
 	local range = { srow = cl, scol = 0, erow = cl, ecol = 0 }
-	local ctx = {
-		ctype = U.ctype.linewise,
-		range = range,
-	}
+	local ctx = { ctype = U.ctype.linewise, range = range }
 	local cstr = require("Comment.ft").calculate(ctx) or vim.bo.commentstring
 	local ll, rr = U.unwrap_cstr(cstr)
 	local padding = true
 	local is_commented = U.is_commented(ll, rr, padding)
-
 	local line = vim.api.nvim_buf_get_lines(0, cl - 1, cl, false)
-	if next(line) == nil or not is_commented(line[1]) then
-		return
-	end
-
+	if next(line) == nil or not is_commented(line[1]) then return end
 	local rs, re = cl, cl -- range start and end
 	repeat
 		rs = rs - 1
@@ -89,7 +82,6 @@ local function commented_lines_textobject()
 		line = vim.api.nvim_buf_get_lines(0, re - 1, re, false)
 	until next(line) == nil or not is_commented(line[1])
 	re = re - 1
-
 	vim.fn.execute("normal! " .. rs .. "GV" .. re .. "G")
 end
 
