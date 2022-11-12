cb538b8d873922083f6bbcd82242334330db75ef
🤖 automated <cron@job>
2022-11-12 14:37:01 +0100

    iMac Home (7)

--- Alfred.alfredpreferences/workflows/user.workflow.51A15AB5-67D8-4BE2-9E08-65644CEFF0B6/anycomplete.js
+++ Alfred.alfredpreferences/workflows/user.workflow.51A15AB5-67D8-4BE2-9E08-65644CEFF0B6/anycomplete.js
@@ -1,35 +1,32 @@
 #!/usr/bin/env osascript -l JavaScript
-function run (
+function run(argv) {
+	ObjC.import("stdlib");
+	const app = Application.currentApplication();
+	app.includeStandardAdditions = true;
 
-)
+	// function alfredMatcher(str) {
+	// 	const clean = str.replace(/-/g, " ");
+	// 	return [clean, str].join(" ");
+	// }
 
-ObjC.import("stdlib");
-const app = Application.currentApplication();
-app.includeStandardAdditions = true;
+	const onlineJSON = (url) => JSON.parse(app.doShellScript(`curl -s "${url}"`));
 
-function alfredMatcher(str) {
-	const clean = str.replace(/[-()_.:#]/g, " ");
-	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
-	return [clean, camelCaseSeperated, str].join(" ");
+	//──────────────────────────────────────────────────────────────────────────────
+
+	const baseURL = "https://duckduckgo.com/ac/?q=";
+	const query = argv.join("");
+
+	//──────────────────────────────────────────────────────────────────────────────
+
+	const jsonArray = onlineJSON(baseURL + query)
+		.map(item => {
+			const completion = item.phrase;
+			return {
+				"title": completion,
+				"match": completion,
+				"arg": completion,
+			};
+		});
+
+	return JSON.stringify({ items: jsonArray });
 }
-
-const onlineJSON = (url) => JSON.parse(app.doShellScript(`curl -s "${url}"`));
-
-//──────────────────────────────────────────────────────────────────────────────
-
-const baseURL = "https://duckduckgo.com/ac/?q=";
-const query = argv.join("");
-
-//──────────────────────────────────────────────────────────────────────────────
-
-const jsonArray = onlineJSON(baseURL + query)
-	.map(item => {
-		const completion = item.phrase;
-		return {
-			"title": completion,
-			"match": alfredMatcher (completion),
-			"arg": completion,
-		};
-	});
-
-JSON.stringify({ items: jsonArray });
