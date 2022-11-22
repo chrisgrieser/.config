53a7bc8e80532f7bb1fe63c1971bd8e47014d758
🤖 automated <cron@job>
2022-11-22 20:39:29 +0100

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/show-diff.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/show-diff.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env zsh
-sublcli="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" # using full path makes this work even if `subl` hasn't been added to PATH
 
 hash=$(echo "$*" | cut -d";" -f1)
 FULL_PATH=$(echo "$*" | cut -d";" -f2)
@@ -10,6 +9,6 @@ OLD="/tmp/$hash.$EXT"
 cd "$(dirname "$FULL_PATH")" || exit 1
 [[ ! -e "$OLD" ]] && git show "$hash:./$FILE" > "$OLD"
 
-diff --unified --ignore-all-space "$OLD" "$FULL_PATH" | tail -n+3 > "/tmp/$hash.diff"
+diff --side-by-side --ignore-all-space "$OLD" "$FULL_PATH" | tail -n+3 > "/tmp/$hash.diff"
 
-"$sublcli" "/tmp/$hash.diff"
+alacritty --command nvim "/tmp/$hash.diff"
