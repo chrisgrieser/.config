0aa7b1112fbd615800bfa4a3cda80bdf8a158556
🤖 automated <cron@job>
2022-11-27 10:00:14 +0100

    iMac Home (27)

--- nvim/after/ftplugin/json.lua
+++ nvim/after/ftplugin/json.lua
@@ -1,22 +1,11 @@
 require("utils")
 --------------------------------------------------------------------------------
 
--- use 2 spaces instead of tabs
-bo.shiftwidth = 2
-bo.tabstop = 2
-bo.softtabstop = 2
 bo.expandtab = true
-
 setlocal("listchars", "tab: >")
 
 -- hides quotes in JSON, making it actually more readable
 wo.conceallevel = 2
-
-keymap("n", "<D-p>", function ()
-	local jsonpath = require("jsonpath").get()
-	fn.setreg("+", jsonpath)
-	vim.notify(jsonpath)
-end, {buffer = true})
 
 -- JSON-[b]eautify line/selection, requires `yq`
 keymap("n", "<D-b>", ":.!yq -o=json<CR>", {buffer = true})
