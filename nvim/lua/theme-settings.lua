34dbac2989954b833b6015e1d947551ea3d786a5
🤖 automated <cron@job>
2022-11-29 17:52:31 +0100

    iMac Home (4)

--- nvim/lua/theme-settings.lua
+++ nvim/lua/theme-settings.lua
@@ -67,6 +67,7 @@ local function themeModifications()
 		end
 		cmd("highlight GitSignsChange guifg=#acaa62")
 		cmd("highlight GitSignsAdd guifg=#7fcc82")
+		cmd("highlight! def link UfoFoldedBg Cursor")
 	elseif theme == "dawnfox" then
 		cmd [[highlight IndentBlanklineChar guifg=#deccba]]
 		cmd [[highlight VertSplit guifg=#b29b84]]
