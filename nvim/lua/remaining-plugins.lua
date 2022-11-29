38956e5b2829ee53c3854a5a6795f8635058b672
🤖 automated <cron@job>
2022-11-29 19:12:30 +0100

    iMac Home (1)

--- nvim/lua/remaining-plugins.lua
+++ nvim/lua/remaining-plugins.lua
@@ -6,7 +6,7 @@ g.netrw_browse_split = 0
 g.netrw_list_hide = ".*\\.DS_Store$,^./$,^../$" -- hide files created by macOS & current directory
 g.netrw_banner = 0 -- no ugly top banner
 g.netrw_liststyle = 3 -- tree style as default
-g.netrw_winsize = 30
+g.netrw_winsize = 30 -- width
 g.netrw_localcopydircmd = "cp -r" -- so copy work with directories
 cmd [[highlight! def link netrwTreeBar IndentBlankLineChar]]
 
@@ -17,10 +17,13 @@ cmd [[highlight! def link netrwTreeBar IndentBlankLineChar]]
 g.undotree_WindowLayout = 4 -- split to the right
 g.undotree_SplitWidth = 25
 g.undotree_DiffpanelHeight = 10
+
+g.undotree_DiffCommand = "diff --unified"
+g.undotree_DiffAutoOpen = 0
+
 g.undotree_SetFocusWhenToggle = 1
 g.undotree_ShortIndicators = 1 -- short relative dates
 g.undotree_HelpLine = 1 -- 0 = hides the "Press ? for help"
-g.undotree_DiffAutoOpen = 0
 
 function g.Undotree_CustomMap()
 	local opts = {buffer = true, silent = true, nowait = true}
