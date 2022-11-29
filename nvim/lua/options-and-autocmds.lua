9b4390c93023c8240af90a55d518e04241965468
🤖 automated <cron@job>
2022-11-30 00:32:30 +0100

    iMac Home (1)

--- nvim/lua/options-and-autocmds.lua
+++ nvim/lua/options-and-autocmds.lua
@@ -128,13 +128,13 @@ opt.cmdheight = 0
 --------------------------------------------------------------------------------
 -- FOLDING
 -- use treesitter folding
--- opt.foldexpr = "nvim_treesitter#foldexpr()"
--- opt.foldmethod = "expr"
-opt.foldmethod = "indent"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldmethod = "expr"
 
 opt.foldenable = false -- do not fold at start
 opt.foldminlines = 2
 opt.foldnestmax = 3
+opt.foldlevel = 99
 
 -- keep folds on save https://stackoverflow.com/questions/37552913/vim-how-to-keep-folds-on-save
 augroup("rememberFolds", {})
