99faaa1e759eab63f40427947decd09f8ea409ba
🤖 automated <cron@job>
2022-12-05 21:16:11 +0100

    iMac Home (3)

--- sketchybar/git-sync.sh
+++ sketchybar/git-sync.sh
@@ -9,7 +9,7 @@ vaultfiles=$(git status --porcelain)
 passPath="$PASSWORD_STORE_DIR"
 [[ -z "$passPath" ]] && passPath="$HOME/.password-store"
 cd "$passPath" || configError="repo-path wrong"
-passfiles=$(git status --porcelain)
+passfiles=$(git status --porcelain --branch | grep -Eo "\d") # to check for ahead/behind instead of untracked
 
 if [[ "$dotfiles" =~ " m " ]]; then # changes in submodules
 	icon="*🔁"
