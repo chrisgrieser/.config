893bd90be44cb53921c8212580ed2cd5fda0e017
🤖 automated <cron@job>
2022-12-05 18:36:11 +0100

    iMac Home (7)

--- Alfred.alfredpreferences/workflows/user.workflow.6895BF0E-0186-46D1-A741-E068CE16BBA3/pass-cli-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.6895BF0E-0186-46D1-A741-E068CE16BBA3/pass-cli-search.js
@@ -1,45 +1,44 @@
 #!/usr/bin/env osascript -l JavaScript
-ObjC.import("stdlib");
-const app = Application.currentApplication();
-app.includeStandardAdditions = true;
+function run(argv) {
+	const passwordStore = argv[0];
 
-function alfredMatcher(str) {
-	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
-	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
-	return [clean, camelCaseSeperated, str].join(" ");
-}
-const fileExists = (filePath) => Application("Finder").exists(Path(filePath));
+	ObjC.import("stdlib");
+	const app = Application.currentApplication();
+	app.includeStandardAdditions = true;
 
-//──────────────────────────────────────────────────────────────────────────────
-// const passwordStore = $.getenv("password_store").replace(/^~/, app.pathTo("home folder"));
-const jsonArray = [];
+	function alfredMatcher(str) {
+		const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
+		const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
+		return [clean, camelCaseSeperated, str].join(" ");
+	}
+	const fileExists = (filePath) => Application("Finder").exists(Path(filePath));
 
-ObjC.import("Foundation");
-const environment = $.NSProcessInfo.processInfo.environment.js;
-const passwordStore = "PWD" in environment ? environment.PWD.js : undefined; /* eslint-disable-line no-undefined */
+	//──────────────────────────────────────────────────────────────────────────────
+	const jsonArray = [];
 
-if (fileExists(passwordStore)) {
-	app.doShellScript(`cd "${passwordStore}" ; find . -name "*.gpg"`)
-		.split("\r")
-		.forEach(gpgFile => {
-			const id = gpgFile.slice(2, -4);
-			const parts = id.split("/");
-			const name = parts.pop();
-			const group = parts.join("/");
+	if (fileExists(passwordStore)) {
+		app.doShellScript(`cd "${passwordStore}" ; find . -name "*.gpg"`)
+			.split("\r")
+			.forEach(gpgFile => {
+				const id = gpgFile.slice(2, -4);
+				const parts = id.split("/");
+				const name = parts.pop();
+				const group = parts.join("/");
 
-			jsonArray.push({
-				"title": name,
-				"subtitle": group,
-				"match": alfredMatcher(id),
-				"arg": id,
-				"uid": id,
+				jsonArray.push({
+					"title": name,
+					"subtitle": group,
+					"match": alfredMatcher(id),
+					"arg": id,
+					"uid": id,
+				});
 			});
+	} else {
+		jsonArray.push({
+			"title": "⚠️ Password Store does not exist",
+			"subtitle": passwordStore,
+			"valid": false,
 		});
-} else {
-	jsonArray.push({
-		"title": "⚠️ Password Store does not exist",
-		"subtitle": passwordStore,
-		"valid": false,
-	});
+	}
+	return JSON.stringify({ items: jsonArray });
 }
-JSON.stringify({ items: jsonArray });
