6265bd0ef252ae94b4f4bd354660870a188bc510
🤖 automated <cron@job>
2022-12-05 20:16:11 +0100

    iMac Home (5)

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,6 +15,7 @@ alias .....="z ../../../.."
 alias r='exec zsh' # do not reload with source ~/.zshrc, https://github.com/ohmyzsh/ohmyzsh/wiki/FAQ#how-do-i-reload-the-zshrc-file
 alias q='exit'
 alias notify="osascript -e 'display notification \"\" with title \"Terminal Process finished.\" subtitle \"\" sound name \"\"'"
+alias p='pass'
 
 # verbosity nets
 alias rm='rm -v'
