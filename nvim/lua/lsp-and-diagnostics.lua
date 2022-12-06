f15d7f59c26e7020c1c0e5f2f516201a44d770ea
🤖 automated <cron@job>
2022-12-06 22:42:52 +0100

    iMac Home (4)

--- nvim/lua/lsp-and-diagnostics.lua
+++ nvim/lua/lsp-and-diagnostics.lua
@@ -158,7 +158,8 @@ local function on_attach(client, bufnr)
 		keymap({"n", "x", "i"}, "<D-s>", function()
 			vim.lsp.buf.format {async = true}
 			if bo.filetype == "javascript" or bo.filetype == "typescript" then
-				cmd [[update! | EslintFixAll]] -- eslint-lsp
+				cmd [[update!]]
+				cmd [[EslintFixAll]] -- eslint-lsp
 			end
 			cmd [[write!]]
 		end, bufopts)
