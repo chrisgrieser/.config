64d02159373c6d5412d0ac646aaab449c5c71176
🤖 automated <cron@job>
2022-12-07 13:03:05 +0100

    iMac Home (1)

--- nvim/lua/remaining-plugins.lua
+++ nvim/lua/remaining-plugins.lua
@@ -28,4 +28,21 @@ autocmd("FileType", {
 })
 
 --------------------------------------------------------------------------------
+-- https://neovim.io/doc/user/luvref.html#luv-fs-event-handle
 
+
+local w = vim.loop.new_fs_event()
+local function on_change(err, fname, status)
+	-- Do work...
+	vim.api.nvim_command("checktime")
+	-- Debounce: stop/start.
+	w:stop()
+	watch_file(fname)
+end
+
+local function watch_file(fname)
+	local fullpath = vim.api.nvim_call_function("fnamemodify", {fname, ":p"})
+	w:start(fullpath, {}, vim.schedule_wrap(function(...)
+		on_change(...)
+	end))
+end
