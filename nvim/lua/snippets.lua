05242b517bcdd70800f269877e8eca7d4a3b1a28
🤖 automated <cron@job>
2022-12-09 14:28:23 +0100

    iMac Home (5)

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -362,14 +362,14 @@ add("javascript", {
 add("yaml", {
 	snip("delay (Karabiner)", [[
 	- {key_code: vk_none, hold_down_milliseconds: ${1:50}}
-	$0]]),
+	]]),
 	snip("to (Karabiner)", [[
 	to:
 		- {key_code: ${1:key}, modifiers: [${2:command}]}
-	$0]]),
+	]]),
 	snip("from (Karabiner)", [[
 	from: {key_code: ${1:key}, modifiers: {mandatory: [${2:command}]}}
-	$0]]),
+	]]),
 })
 
 --------------------------------------------------------------------------------
