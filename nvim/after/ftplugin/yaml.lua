09920f7047072aa8adcf5e1fb88edb88e8693c34
🤖 automated <cron@job>
2022-12-10 16:04:20 +0100

    iMac Home (4)

--- nvim/after/ftplugin/yaml.lua
+++ nvim/after/ftplugin/yaml.lua
@@ -6,5 +6,5 @@ bo.shiftwidth = 2
 bo.tabstop = 2
 bo.softtabstop = 2
 bo.expandtab = true
-
+setlocal("backspace", "start,eol,indent") -- restrict insert mode backspace behavior
 setlocal("listchars", "tab: >")
