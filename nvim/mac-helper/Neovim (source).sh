bddc917a0a579a9ab24bf4d2cb7823972828f80c
🤖 automated <cron@job>
2022-12-10 10:40:01 +0100

    iMac Home (7)

--- nvim/mac-helper/Neovim (source).sh
+++ nvim/mac-helper/Neovim (source).sh
@@ -4,15 +4,12 @@ export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 # this script, bundles as .app via Automator, ensures you can open a file from
 # anywhere in macOS and it will open in the currently existing Neovide instance
 
-if [[ -z "$LINE" ]]; then # $LINE is set via `open --env=LINE=num`
-	fileAndLn="$1"
-else
-	fileAndLn="+$LINE $1"
-fi
+file="$1"
+[[ -z "$LINE" ]] && LINE="+$LINE" # $LINE is set via `open --env=LINE=num`
 
 if pgrep "neovide"; then
-	echo "cmd.edit[[$fileAndLn]]" >"/tmp/nvim-automation" # this part requires the setup in /lua/file-watcher.lua
+	echo "cmd.edit [[$LINE $file]]" >"/tmp/nvim-automation" # this part requires the setup in /lua/file-watcher.lua
 	osascript -e 'tell application "Neovide" to activate'
 else
-	neovide "$fileAndLn"
+	neovide $LINE "$file"
 fi
