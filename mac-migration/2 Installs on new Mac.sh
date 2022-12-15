de930f2a6040822edb6e2fd7fd94bbf8fafc07c2
🤖 automated <cron@job>
2022-12-15 16:55:23 +0100

    iMac Home (3)

--- mac-migration/2 Installs on new Mac.sh
+++ mac-migration/2 Installs on new Mac.sh
@@ -5,6 +5,8 @@
 # INSTALLS
 sudo -v
 
+sudo gem install anystyle-cli
+
 pip3 install pdfannots
 pip3 install alacritty-colorscheme
 
@@ -18,11 +20,11 @@ brew bundle install --no-quarantine --verbose --no-lock --file ~'/Desktop/Brewfi
 
 # Uninstall unneeded Mac Default apps
 open -a "Appcleaner" \
-    "/Applications/Numbers.app" \
-    "/Applications/Pages.app/" \
-    "/Applications/GarageBand.app" \
-    "/Applications/Keynote.app" \
-    "/Applications/iMovie.app"
+	"/Applications/Numbers.app" \
+	"/Applications/Pages.app/" \
+	"/Applications/GarageBand.app" \
+	"/Applications/Keynote.app" \
+	"/Applications/iMovie.app"
 
 # NPM https://stackoverflow.com/a/41199625
 # shellcheck disable=SC2002
@@ -59,11 +61,11 @@ defaults write "org.hammerspoon.Hammerspoon" "SUEnableAutomaticChecks" 1
 font_size=19
 c_css_location=~'/Library/Application Support/name.abuchen.portfolio.product/workspace/.metadata/.plugins/name.abuchen.portfolio.ui/'
 mkdir -p "$c_css_location"
-printf "%s" "{\nfont-size: ""$font_size"";\n}" >> "$c_css_location"/custom.css
+printf "%s" "{\nfont-size: ""$font_size"";\n}" >>"$c_css_location"/custom.css
 
 # Steam UI https://tp69.blog/2020/02/11/how-to-zoom-the-steam-client/
 steamDataPath=~"/Library/Application Support/Steam/Steam.AppBundle/Steam/Contents/MacOS"
 newSkinPath="$steamDataPath""/skins/Bigger UI"
 mkdir -p "$newSkinPath"/resource/styles/
 cp "$steamDataPath"/resource/styles/steam.styles "$newSkinPath"/resource/styles/
-echo ":root { zoom: \"1.5\"; }" > "$newSkinPath"/resource/webkit.css
+echo ":root { zoom: \"1.5\"; }" >"$newSkinPath"/resource/webkit.css
