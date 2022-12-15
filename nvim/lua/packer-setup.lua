336fd42de7148b3d8ec826cde065771b49a13c38
🤖 automated <cron@job>
2022-12-15 17:15:23 +0100

    iMac Home (4)

--- nvim/lua/packer-setup.lua
+++ nvim/lua/packer-setup.lua
@@ -1,30 +1,30 @@
 -- INFO: file should *not* be named "packer.lua", since `require("packer")`
--- would be ambigious
+-- would be ambiguous
 --------------------------------------------------------------------------------
 
 -- auto-install packer if not installed https://github.com/wbthomason/packer.nvim#bootstrapping
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 	print("Packer not installed. Installing...")
-	vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	vim.fn.system {"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path}
 	vim.cmd [[packadd packer.nvim]]
 end
 
 -- Protected call to prevent error when Packer is not installed
 local status_ok, packer = pcall(require, "packer")
-if (not status_ok) then return end
+if not status_ok then return end
 
 -- Load plugins and display packer as popup
-packer.startup{
+packer.startup {
 	require("plugin-list").PluginList,
 	config = {
 		display = {
-			open_fn = function() return require('packer.util').float{border = borderStyle} end,
+			open_fn = function() return require("packer.util").float {border = borderStyle} end,
 			prompt_border = borderStyle,
 			compact = true,
 			show_all_info = false,
 		},
-		snapshot_path = vim.fn.stdpath("config").."/packer-snapshots/",
+		snapshot_path = vim.fn.stdpath("config") .. "/packer-snapshots/",
 		autoremove = true, -- remove unused plugins without prompting user
 	},
 }
