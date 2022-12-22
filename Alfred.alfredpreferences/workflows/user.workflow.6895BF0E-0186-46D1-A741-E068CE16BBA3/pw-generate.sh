8d51827e76664301b779419c3c8a66894b030ac3
🤖 automated <cron@job>
2022-12-22 17:29:01 +0100

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/user.workflow.6895BF0E-0186-46D1-A741-E068CE16BBA3/pw-generate.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.6895BF0E-0186-46D1-A741-E068CE16BBA3/pw-generate.sh
@@ -2,5 +2,9 @@
 # shellcheck disable=2154
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 
-pass generate --clip --no-symbols "$folder/$*" | tail -n1
+# lowercase & kebab-case
+entry_name=${*/ /-}
+entry_name=${entry_name:l}
+
+pass generate --clip --no-symbols "$folder/$entry_name" | tail -n1
 [[ "$auto_push" == "1" ]] && pass git push &>/dev/null
