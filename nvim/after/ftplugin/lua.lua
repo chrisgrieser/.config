0a39ddbc93302a0670821d75844536564913c95b
🤖 automated <cron@job>
2022-12-24 12:34:06 +0100

    iMac Mother (6)

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -9,5 +9,5 @@ keymap("n", "gR", function()
 	normal('"zya"vi"') -- yank and keep selection for quick replacement when done
 	local pattern = fn.getreg("z"):match('"(.*)"')
 	local url = "https://gitspartv.github.io/lua-patterns/?pattern=" .. pattern
-	os.execute("open '" .. url .. "'") -- opening method on macOS
-end, { desc = "Open next lua pattern in lua pattern viewer" })
+	fn.system("open '" .. url .. "'") -- opening method on macOS
+end, { desc = "Open next lua pattern in lua pattern viewer", buffer = true})
