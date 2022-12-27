91d0800296a98ec0a537a482c4d7a26467de839e
🤖 automated <cron@job>
2022-12-27 02:33:54 +0100

    iMac Mother (5)

--- nvim/lua/config/user-commands.lua
+++ nvim/lua/config/user-commands.lua
@@ -5,8 +5,8 @@ local newCommand = vim.api.nvim_create_user_command
 -- `:SwapDeleteAll` deletes all swap files
 newCommand("SwapDeleteAll", function(_)
 	local swapdir = vimDataDir .. "swap/"
-	local out = fn.system([[rm -vf "]]..swapdir..[["* ]])
-	vim.notify("Deleted:\n"..out)
+	local out = fn.system([[rm -vf "]] .. swapdir .. [["* ]])
+	vim.notify("Deleted:\n" .. out)
 end, {})
 
 -- `:ViewDir` opens the nvim view directory
@@ -16,16 +16,13 @@ newCommand("ViewDir", function(_)
 end, {})
 
 -- `:PluginDir` opens the nvim data path, where mason and lazy install their stuff
-newCommand("PluginDir", function(_)
-	fn.system('open "' .. fn.stdpath("data") .. '"')
-end, {})
+newCommand("PluginDir", function(_) fn.system('open "' .. fn.stdpath("data") .. '"') end, {})
 
--- `:I` inspects the passed lua object
-newCommand("I", function(ctx)
-	local output = vim.inspect(fn.luaeval(ctx.args))
-	vim.notify(output, vim.log.levels.INFO, {
-		-- enable highlighting in the notification
-		on_open = function(win)
+-- `:I` or `<leader>i` inspects the passed lua object / selection
+local function inspect(str)
+	local output = vim.inspect(fn.luaeval(str))
+	vim.notify(output, logTrace, {
+		on_open = function(win) -- enable highlighting in the notification
 			local outputIsStr = output:find('^"') and output:find('"$')
 			if not outputIsStr then
 				local buf = api.nvim_win_get_buf(win)
@@ -33,11 +30,16 @@ newCommand("I", function(ctx)
 			end
 		end,
 	})
-end, { nargs = "+" })
+end
+keymap("x", "<leader>i", function()
+	normal('"zy')
+	inspect(fn.getreg("z"))
+end, { desc = "inspect selection" })
+newCommand("I", function(ctx) inspect(ctx.args) end, { nargs = "+" })
 
 -- `:II` inspects the passed object and puts it into a new buffer, https://www.reddit.com/r/neovim/comments/zhweuc/comment/izo9br1/?utm_source=share&utm_medium=web2x&context=3
 newCommand("II", function(ctx)
-	local output = "out = "..vim.inspect(fn.luaeval(ctx.args))
+	local output = "out = " .. vim.inspect(fn.luaeval(ctx.args))
 	local lines = vim.split(output, "\n", { plain = true }) ---@diagnostic disable-line: param-type-mismatch
 	cmd.vsplit()
 	cmd.ene()
