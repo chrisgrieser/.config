a8c01a0745091c0c7a85bdff7e45253c642ad837
🤖 automated <cron@job>
2022-12-30 03:33:49 +0100

    iMac Home (4)

--- browser-extension-configs/surfing-keys.js
+++ browser-extension-configs/surfing-keys.js
@@ -1,27 +1,22 @@
 /* global api, settings, window, document */
 
 // Compatibility Prefix
-const {
-	imap,
-	imapkey,
-	map,
-	mapkey,
-	removeSearchAlias,
-	unmap,
-	unmapAllExcept,
-	vmapkey,
-	vunmap,
-} = api;
+const { imap, imapkey, map, mapkey, removeSearchAlias, unmap, unmapAllExcept, vmapkey, vunmap } = api;
 
 //──────────────────────────────────────────────────────────────────────────────
 
 // ---- SETTINGS ----
 // https://github.com/brookhong/Surfingkeys#edit-your-own-settings
-// Hints.setCharacters("asdfgyuiopqwertnmzxcvb");
 settings.focusAfterClosed = "last";
 settings.scrollStepSize = 300;
 settings.tabsThreshold = 7;
 settings.modeAfterYank = "Normal";
+settings.hintAlign = "left";
+settings.theme = `
+	#sk_status, #sk_find {
+		font-size: 20pt;
+	}
+}`;
 
 //──────────────────────────────────────────────────────────────────────────────
 
@@ -50,11 +45,17 @@ map(">", ">>"); // move tab to the right
 
 map("F", "C"); // Open Hint in new tab
 
+map("B", "ab"); // bookmark
+map("X", ";dh"); // delete bookmark
+map("m", "<Alt-m>"); // mute tab
+map("M", ";pm"); // markdown preview
+
 map("ye", "yv"); // yank text of an element
 
 map("wv", "W"); // move tab to new window (vsplit with Hammerspoon)
+map("wm", ";gw"); // merge all windows to current one
 
-map("gi", "i"); // enter insert field
+map("gi", "I"); // enter insert field
 map("a", "p"); // disable for one key
 
 map("h", "S"); // History Back/Forward
@@ -63,22 +64,19 @@ map("l", "D");
 map("H", "[["); // Next/Prev Page
 map("L", "]]");
 
-map("-", "/"); // find
+map("I", ";j"); // inspector
 
-map("ge", ";u"); // Edit current URL
+map("-", "/"); // find
+map("+", "*"); // find selection
+
+map("ge", ";U"); // Edit current URL
 map(",", ";e"); // Settings
 map("t", "T"); // choose tab via hint
 
 map("p", "cc"); // open URL from clipboard or selection
-// Open Clipboard URL in current tab
-// mapkey("P", "Open the clipboard's URL in the current tab", () => {
-// 	Clipboard.read(function (response) {
-// 		window.location.href = response.data;
-// 	});
-// });
 
 // toggle fullscreen, mainly because of YouTube
-mapkey("F", "Fullscreen", function () {
+mapkey("Z", "Fullscreen", function () {
 	if (window.fullScreen) {
 		document.exitFullscreen();
 	} else {
@@ -97,3 +95,90 @@ removeSearchAlias("w", "s");
 removeSearchAlias("y", "s");
 removeSearchAlias("s", "s");
 removeSearchAlias("e", "s");
+
+unmap(";ql");
+unmap("]]");
+unmap("[[");
+unmap("S");
+unmap("W");
+unmap("R");
+unmap("C");
+unmap("W");
+unmap("yG");
+unmap("yS");
+unmap(";w");
+unmap("P");
+unmap("D");
+unmap("yj");
+unmap("ys");
+unmap("yv");
+unmap("yma");
+unmap("U");
+unmap("cs");
+unmap("cS");
+unmap("$");
+unmap("0");
+unmap("<Alt-i>");
+unmap("I");
+unmap("q");
+unmap(";m");
+unmap(";fs");
+unmap(";di");
+unmap("gf");
+unmap("cf");
+unmap("yT");
+unmap("gxp");
+unmap(";U");
+unmap(";u");
+unmap("<Ctrl-6>");
+unmap("gx0");
+unmap("gx$");
+unmap("<Alt-p>");
+unmap("<Alt-m>");
+unmap("gxt");
+unmap("gxT");
+unmap("gT");
+unmap("gt");
+unmap("g?");
+unmap("g#");
+unmap("on");
+unmap("on");
+unmap("zr");
+unmap("zi");
+unmap("zo");
+unmap(";gt");
+unmap(";e");
+unmap(";v");
+unmap("yq");
+unmap("yQ");
+unmap("yh");
+unmap(";yQ");
+unmap("d");
+unmap("cp");
+unmap(";pa");
+unmap(";pf");
+unmap(";pj");
+unmap(";pp");
+unmap("cc");
+unmap(";yh");
+unmap(";dh");
+unmap("gs");
+unmap("gn");
+unmap("gb");
+unmap("gd");
+unmap("<Ctrl-'>");
+unmap("zv");
+unmap(";pb");
+unmap(";gw");
+unmap("gxx");
+unmap("zt");
+unmap(";pm");
+unmap("zb");
+unmap(";pc");
+unmap(";ps");
+unmap(";pd");
+unmap(";ap");
+unmap(";j");
+unmap("<Ctrl-Alt-i>");
+unmap("<Ctrl-h>");
+unmap("<Ctrl-j>");
