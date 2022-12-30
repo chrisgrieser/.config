881ecff4daabc9d684a728fd97f1ae4a1b7b9f50
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-12-30 05:23:16 +0100

    chore

--- nvim/lua/config/user-commands.lua
+++ nvim/lua/config/user-commands.lua
@@ -41,11 +41,12 @@ newCommand("I", function(ctx) inspect(ctx.args) end, { nargs = "+" })
 
 -- `:II` inspects the passed object and puts it into a new buffer, https://www.reddit.com/r/neovim/comments/zhweuc/comment/izo9br1/?utm_source=share&utm_medium=web2x&context=3
 newCommand("II", function(ctx)
-	os.remove("/tmp/nvim-cmd-output.lua")
+	os.remove("/tmp/nvim-cmd-output")
 	local output = "out = " .. vim.inspect(fn.luaeval(ctx.args))
 	local lines = vim.split(output, "\n", { plain = true }) ---@diagnostic disable-line: param-type-mismatch
 	cmd.vsplit()
 	cmd.ene()
+	bo.filetype = "lua"
 	api.nvim_buf_set_lines(0, 0, -1, false, lines)
 	cmd.write { "/tmp/nvim-cmd-output.lua", bang = true }
 end, { nargs = "+" })
