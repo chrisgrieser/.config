659ad3225cead63eb59044216e57574ee5448a2d
🤖 automated <cron@job>
2022-12-30 06:58:42 +0100

    iMac Home (6)

--- zsh/git_github.zsh
+++ zsh/git_github.zsh
@@ -196,9 +196,13 @@ function rel() {
 # search for [g]it [d]eleted [f]ile -> https://stackoverflow.com/a/42582877
 function gdf() {
 	local deleted_path deletion_commit
-	# alternative method: `git rev-list -n 1 HEAD -- "**/*$1*"` to get the commit
-	# of a deleted file
+
+	# goto git root
+	r=$(git rev-parse --git-dir) && r=$(cd "$r" && pwd)/ && cd "${r%%/.git/*}"
+
+	# alternative method: `git rev-list -n 1 HEAD -- "**/*$1*"` to get the commit of a deleted file
 	deleted_path=$(git log --diff-filter=D --summary | grep delete | grep -i "$*" | cut -d" " -f5-)
+
 	if [[ $(echo "$deleted_path" | wc -l) -gt 1 ]]; then
 		echo "🔍 multiple files found: "
 		echo "$deleted_path"
