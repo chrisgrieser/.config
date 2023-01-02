dbc7c2035e3c153768651277d4c82b1bc4169ec9
🤖 automated <cron@job>
2023-01-02 14:51:33 +0100

    iMac Home (3)

--- sketchybar/git-sync.sh
+++ sketchybar/git-sync.sh
@@ -1,34 +1,16 @@
 #!/usr/bin/env zsh
 
-# shellcheck disable=2034
-GIT_OPTIONAL_LOCKS=0
-
 # INFO to prevent constantly calling `git status`, which prevents other git processes
 # from running due to lock (happens sometimes even with optional locks)
-# FROM_PATCHWATCHER gets set when called from hammerspoon. 
+# FROM_PATCHWATCHER gets set when called from hammerspoon.
 # WARN running a git command on a path watcher trigger leads to an infinite loop
-# since git commands create index lock files, which again trigger the path 
+# since git commands create index lock files, which again trigger the path
 # watcher, therefore this workaround seems necessary
-if [[ "$FROM_PATHWATCHER" -eq 1 ]]; then
-	sketchybar --set "$NAME" icon="🔁"
-	exit 0
-fi
-
-configError=""
-
-cd "$DOTFILE_FOLDER" || configError="repo-path wrong"
-dotfiles=$(git status --porcelain)
-
-cd "$VAULT_PATH" || configError="repo-path wrong"
-vaultfiles=$(git status --porcelain)
-
-passPath="$PASSWORD_STORE_DIR"
-[[ -z "$passPath" ]] && passPath="$HOME/.password-store"
-cd "$passPath" || configError="repo-path wrong"
-passfiles=$(git status --porcelain --branch | grep -Eo "\d") # to check for ahead/behind instead of untracked
-
-if [[ -n "$dotfiles" ]] || [[ -n "$vaultfiles" ]] || [[ -n "$passfiles" ]]; then
+if [[ "$DIRTY" -eq 1 ]]; then
 	icon="🔁"
+elif [[ "$DIRTY" -eq 2 ]]; then
+	icon="repo-path wrong"
+else
+	icon=""
 fi
-
-sketchybar --set "$NAME" icon="$icon$configError"
+sketchybar --set "$NAME" icon="$icon"
