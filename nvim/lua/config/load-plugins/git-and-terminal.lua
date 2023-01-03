ae62f0bc37a4b0f423809260b1b52a1db5c86db1
🤖 automated <cron@job>
2023-01-03 09:16:28 +0100

    iMac Home (2)

--- nvim/lua/config/load-plugins/git-and-terminal.lua
+++ nvim/lua/config/load-plugins/git-and-terminal.lua
@@ -29,11 +29,27 @@ return {
 		dependencies = "nvim-lua/plenary.nvim",
 		cmd = { "DiffviewFileHistory", "DiffviewOpen" },
 		config = function()
+			local actions = require("diffview.actions")
 			require("diffview").setup {
+				enhanced_diff_hl = true,
 				file_history_panel = {
 					win_config = { height = 5 },
 				},
+				keymaps = {
+					file_history_panel = {
+						{ "n", "<CR>", false }, -- avoid conflict with buffer switcher
+						{ "n", "?", actions.help("file_history_panel") },
+					},
+				},
 			}
+			augroup("diffview", {})
+			autocmd("DiffviewDiffBufRead", {
+				group = "diffview",
+				pattern = "DiffviewFileHistory",
+				callback = function()
+					vim.keymap.set("n", "<CR>", "<C-w>w", {buffer = true}) 	
+				end
+			})
 		end,
 	},
 }
