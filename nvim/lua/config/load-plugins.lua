bd6e98230598b51a3bfb9cf3a416f0855b8fd5e6
🤖 automated <cron@job>
2023-01-03 08:56:28 +0100

    iMac Home (4)

--- nvim/lua/config/load-plugins.lua
+++ nvim/lua/config/load-plugins.lua
@@ -65,39 +65,6 @@ return {
 		},
 	},
 
-	-- Terminal & Git
-	{
-		"TimUntersberger/neogit",
-		dependencies = "nvim-lua/plenary.nvim",
-		cmd = "Neogit",
-		config = function()
-			require("neogit").setup {
-				disable_insert_on_commit = false, -- false = start commit msgs in insert mode
-				disable_commit_confirmation = false,
-				integrations = { diffview = true }, -- diffview plugin
-				signs = {
-					section = { "", "" },
-					item = { "", "" },
-				},
-			}
-		end,
-	},
-	{ "metakirby5/codi.vim", cmd = { "Codi", "CodiNew", "CodiExpand" } }, -- only coderunner with virtual text
-	{
-		"akinsho/toggleterm.nvim",
-		cmd = { "ToggleTerm", "ToggleTermSendVisualSelection" },
-		config = function() require("toggleterm").setup() end,
-	},
-	{
-		"sindrets/diffview.nvim",
-		dependencies = "nvim-lua/plenary.nvim",
-		cmd = { "DiffviewFileHistory", "DiffviewOpen" },
-		config = function()
-			require("diffview").setup {
-				file_history_panel = { win_config = { height = 5 } },
-			}
-		end,
-	},
 
 	{
 		"axieax/urlview.nvim",
