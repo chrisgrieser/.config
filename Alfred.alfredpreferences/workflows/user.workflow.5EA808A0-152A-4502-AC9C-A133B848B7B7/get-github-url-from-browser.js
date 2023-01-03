89866655a0ea71f70cc46560d98df40a6fb5be9d
🤖 automated <cron@job>
2023-01-03 14:50:34 +0100

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/get-github-url-from-browser.js
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/get-github-url-from-browser.js
@@ -15,8 +15,8 @@ function run() {
 		return "You need a supported browser as your frontmost app.";
 	}
 
-	if (!url.match(/^https?:\/\/github\.com\/\w+\/\w+/)) {
-		return "Not a GitHub Repo.";
-	}
-	return url;
+	// check if repo-url and truncate non-repo part
+	const githubURL = url.match(/^https?:\/\/github\.com\/\w+\/\w+/);
+	if (!githubURL) return "Not a GitHub Repo.";
+	return githubURL[0];
 }
