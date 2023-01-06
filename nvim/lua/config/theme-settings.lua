bf1707a545fa019c5d9f58f84fab9dbc2df06ff3
🤖 automated <cron@job>
2023-01-06 16:11:33 +0100

    iMac Home (2)

--- nvim/lua/config/theme-settings.lua
+++ nvim/lua/config/theme-settings.lua
@@ -1,121 +1,39 @@
--- CONFIG
--- local lightTheme = "rose-pine"
--- local darkTheme = "rose-pine"
-local lightTheme = "dawnfox"
--- local darkTheme = "tokyonight-moon"
-local darkTheme = "zephyr"
--- local lightTheme = "melange"
--- local darkTheme = "oxocarbon"
-local darkTransparency = 0.95
-local lightTransparency = 0.94
+	-- local lightTheme = "rose-pine"
+	-- local darkTheme = "rose-pine"
+	local lightTheme = "dawnfox"
+	-- local darkTheme = "tokyonight-moon"
+	local darkTheme = "zephyr"
+	-- local lightTheme = "melange"
+	-- local darkTheme = "oxocarbon"
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-require("config.utils")
----@param hlgroupfrom string
----@param hlgroupto string
-local function linkHighlight(hlgroupfrom, hlgroupto)
-	vim.cmd.highlight { "def link " .. hlgroupfrom .. " " .. hlgroupto, bang = true }
-end
+	local darkTransparency = 0.95
+	local lightTransparency = 0.94
 
----@param hlgroup string
----@param changes string
-local function setHighlight(hlgroup, changes) vim.cmd.highlight(hlgroup .. " " .. changes) end
---------------------------------------------------------------------------------
-
--- Annotations
-linkHighlight("myAnnotations", "Todo")
-fn.matchadd(
-	"myAnnotations",
-	[[\<\(BUG\|WARN\|WIP\|TODO\|WTF\|HACK\|INFO\|NOTE\|WARNING\|FIX\|REQUIRED\)\>]]
-)
-
---------------------------------------------------------------------------------
--- CUSTOM HIGHLIGHTS & Theme Customization
-
-local function customHighlights()
-	local highlights = {
-		"DiagnosticUnderlineError",
-		"DiagnosticUnderlineWarn",
-		"DiagnosticUnderlineHint",
-		"DiagnosticUnderlineInfo",
-		"SpellLocal",
-		"SpellRare",
-		"SpellCap",
-		"SpellBad",
-	}
-	for _, v in pairs(highlights) do
-		s etHighlight(v, "gui=underline cterm=underline")
+local function themeSettings()
+	local cmd = vim.cmd
+	---@param hlgroupfrom string
+	---@param hlgroupto string
+	local function linkHighlight(hlgroupfrom, hlgroupto)
+		vim.cmd.highlight { "def link " .. hlgroupfrom .. " " .. hlgroupto, bang = true }
 	end
 
-	-- active indent
-	linkHighlight("IndentBlanklineContextChar", "Comment")
+	---@param hlgroup string
+	---@param changes string
+	local function setHighlight(hlgroup, changes) vim.cmd.highlight(hlgroup .. " " .. changes) end
 
-	-- URLs
-	setHighlight("urls", "cterm=underline gui=underline")
-	fn.matchadd("urls", [[http[s]\?:\/\/[[:alnum:]%\/_#.\-?:=&]*]])
+	-----------------------------------------------------------------------------
 
-	-- rainbow brackets without aggressive red
-	setHighlight("rainbowcol1", " guifg=#7e8a95")
+	-- Annotations
+	linkHighlight("myAnnotations", "Todo")
+	fn.matchadd(
+		"myAnnotations",
+		[[\<\(BUG\|WARN\|WIP\|TODO\|WTF\|HACK\|INFO\|NOTE\|WARNING\|FIX\|REQUIRED\)\>]]
+	)
 
-	-- more visible matchparens
-	setHighlight("MatchParen", " gui=underdotted cterm=underdotted")
+	--------------------------------------------------------------------------------
+	-- CUSTOM HIGHLIGHTS & Theme Customization
 
-	-- Codi
-	linkHighlight("CodiVirtualText", "Comment")
-
-	-- treesittter refactor focus
-	setHighlight("TSDefinition", " term=underline gui=underdotted")
-	setHighlight("TSDefinitionUsage", " term=underline gui=underdotted")
-end
-
-local function themeModifications()
-	local mode = opt.background:get()
-	local theme = g.colors_name
-
-	-- tokyonight
-	if theme == "tokyonight" then
-		-- HACK bugfix for https://github.com/neovim/neovim/issues/20456
-		linkHighlight("luaParenError.highlight", "NormalFloat")
-		linkHighlight("luaParenError", "NormalFloat")
-		local modes = { "normal", "visual", "insert", "terminal", "replace", "command", "inactive" }
-		for _, v in pairs(modes) do
-			setHighlight("lualine_y_diff_modified_" .. v, "guifg=#acaa62")
-			setHighlight("lualine_y_diff_added_" .. v, "guifg=#8cbf8e")
-		end
-		setHighlight("GitSignsChange", "guifg=#acaa62")
-		setHighlight("GitSignsAdd", "guifg=#7fcc82")
-
-	-- oxocarbon
-	elseif theme == "oxocarbon" then
-		linkHighlight("FloatTitle", "TelescopePromptTitle")
-		linkHighlight("@function", "@function.builtin")
-
-	-- rose-pine
-	elseif theme == "rose-pine" then
-		linkHighlight("IndentBlanklineChar", "FloatBorder")
-		local blueHlgroups = {
-			"@keyword",
-			"@include",
-			"@exception",
-			"@repeat",
-			"@conditional",
-			"Conditional",
-			"@string.escape",
-		}
-		for _, hlgroup in pairs(blueHlgroups) do
-			setHighlight(hlgroup, "guifg=#4fa1c3")
-		end
-
-	-- kanagawa
-	elseif theme == "kanagawa" then
-		-- setHighlight("ScrollView", "guibg=#303050")
-		setHighlight("VirtColumn", "guifg=#323036")
-		linkHighlight("UfoFoldedBg", "Folded")
-
-	-- zephyr
-	elseif theme == "zephyr" then
-		setHighlight("IncSearch", "guifg=#FFFFFF")
+	function customHighlights()
 		local highlights = {
 			"DiagnosticUnderlineError",
 			"DiagnosticUnderlineWarn",
@@ -130,53 +48,134 @@ local function themeModifications()
 			setHighlight(v, "gui=underline cterm=underline")
 		end
 
-	-- dawnfox
-	elseif theme == "dawnfox" then
-		setHighlight("IndentBlanklineChar", "guifg=#deccba")
-		setHighlight("VertSplit", "guifg=#b29b84")
+		-- active indent
+		linkHighlight("IndentBlanklineContextChar", "Comment")
 
-	-- melange
-	elseif theme == "melange" then
-		linkHighlight("Todo", "IncSearch")
-		if mode == "light" then
-			linkHighlight("NonText", "Conceal")
-			linkHighlight("NotifyINFOIcon", "@define")
-			linkHighlight("NotifyINFOTitle", "@define")
-			linkHighlight("NotifyINFOBody", "@define")
+		-- URLs
+		setHighlight("urls", "cterm=underline gui=underline")
+		fn.matchadd("urls", [[http[s]\?:\/\/[[:alnum:]%\/_#.\-?:=&]*]])
+
+		-- rainbow brackets without aggressive red
+		setHighlight("rainbowcol1", " guifg=#7e8a95")
+
+		-- more visible matchparens
+		setHighlight("MatchParen", " gui=underdotted cterm=underdotted")
+
+		-- Codi
+		linkHighlight("CodiVirtualText", "Comment")
+
+		-- treesittter refactor focus
+		setHighlight("TSDefinition", " term=underline gui=underdotted")
+		setHighlight("TSDefinitionUsage", " term=underline gui=underdotted")
+	end
+
+	local function themeModifications()
+		local mode = opt.background:get()
+		local theme = g.colors_name
+
+		-- tokyonight
+		if theme == "tokyonight" then
+			-- HACK bugfix for https://github.com/neovim/neovim/issues/20456
+			linkHighlight("luaParenError.highlight", "NormalFloat")
+			linkHighlight("luaParenError", "NormalFloat")
+			local modes = { "normal", "visual", "insert", "terminal", "replace", "command", "inactive" }
+			for _, v in pairs(modes) do
+				setHighlight("lualine_y_diff_modified_" .. v, "guifg=#acaa62")
+				setHighlight("lualine_y_diff_added_" .. v, "guifg=#8cbf8e")
+			end
+			setHighlight("GitSignsChange", "guifg=#acaa62")
+			setHighlight("GitSignsAdd", "guifg=#7fcc82")
+
+		-- oxocarbon
+		elseif theme == "oxocarbon" then
+			linkHighlight("FloatTitle", "TelescopePromptTitle")
+			linkHighlight("@function", "@function.builtin")
+
+		-- rose-pine
+		elseif theme == "rose-pine" then
+			linkHighlight("IndentBlanklineChar", "FloatBorder")
+			local blueHlgroups = {
+				"@keyword",
+				"@include",
+				"@exception",
+				"@repeat",
+				"@conditional",
+				"Conditional",
+				"@string.escape",
+			}
+			for _, hlgroup in pairs(blueHlgroups) do
+				setHighlight(hlgroup, "guifg=#4fa1c3")
+			end
+
+		-- kanagawa
+		elseif theme == "kanagawa" then
+			-- setHighlight("ScrollView", "guibg=#303050")
+			setHighlight("VirtColumn", "guifg=#323036")
+			linkHighlight("UfoFoldedBg", "Folded")
+
+		-- zephyr
+		elseif theme == "zephyr" then
+			setHighlight("IncSearch", "guifg=#FFFFFF")
+			linkHighlight("TabLineSel", "lualine_a_normal")
+			linkHighlight("TabLineFill", "lualine_c_normal")
+
+		-- dawnfox
+		elseif theme == "dawnfox" then
+			setHighlight("IndentBlanklineChar", "guifg=#deccba")
+			setHighlight("VertSplit", "guifg=#b29b84")
+
+		-- melange
+		elseif theme == "melange" then
+			linkHighlight("Todo", "IncSearch")
+			if mode == "light" then
+				linkHighlight("NonText", "Conceal")
+				linkHighlight("NotifyINFOIcon", "@define")
+				linkHighlight("NotifyINFOTitle", "@define")
+				linkHighlight("NotifyINFOBody", "@define")
+			end
 		end
 	end
-end
 
-augroup("themeChange", {})
-autocmd("ColorScheme", {
-	group = "themeChange",
-	callback = function()
-		themeModifications()
-		customHighlights()
-	end,
-})
+	augroup("themeChange", {})
+	autocmd("ColorScheme", {
+		group = "themeChange",
+		callback = function()
+			themeModifications()
+			customHighlights()
+		end,
+	})
 
---------------------------------------------------------------------------------
--- DARK MODE / LIGHT MODE
--- functions not local, so they can be accessed via file watcher
-function setDarkTheme()
-	opt.background = "dark" ---@diagnostic disable-line: assign-type-mismatch
-	g.neovide_transparency = darkTransparency
-	cmd.highlight("clear") -- needs to be set before colorscheme https://github.com/folke/lazy.nvim/issues/40
-	cmd.colorscheme(darkTheme)
-end
+	--------------------------------------------------------------------------------
+	-- DARK MODE / LIGHT MODE
+	-- functions not local, so they can be accessed via file watcher
+	function setDarkTheme()
+		opt.background = "dark" ---@diagnostic disable-line: assign-type-mismatch
+		g.neovide_transparency = darkTransparency
+		cmd.highlight("clear") -- needs to be set before colorscheme https://github.com/folke/lazy.nvim/issues/40
+		cmd.colorscheme(darkTheme)
+	end
 
-function setLightTheme()
-	opt.background = "light" ---@diagnostic disable-line: assign-type-mismatch
-	g.neovide_transparency = lightTransparency
-	cmd.highlight("clear")
-	cmd.colorscheme(lightTheme)
-end
+	function setLightTheme()
+		opt.background = "light" ---@diagnostic disable-line: assign-type-mismatch
+		g.neovide_transparency = lightTransparency
+		cmd.highlight("clear")
+		cmd.colorscheme(lightTheme)
+	end
 
--- set dark or light mode on neovim startup (requires macos)
-local macOStheme = fn.system([[defaults read -g AppleInterfaceStyle]])
-if macOStheme:find("Dark") then
-	setDarkTheme()
-else
-	setLightTheme()
+	-- set dark or light mode on neovim startup (requires macos)
+	local macOStheme = fn.system([[defaults read -g AppleInterfaceStyle]])
+	if macOStheme:find("Dark") then
+		setDarkTheme()
+	else
+		setLightTheme()
+	end
 end
+return {
+	"EdenEast/nightfox.nvim",
+	"folke/tokyonight.nvim",
+	-- "rose-pine/neovim",
+	-- "rebelot/kanagawa.nvim",
+	-- "nyoom-engineering/oxocarbon.nvim",
+	"glepnir/zephyr-nvim",
+	"savq/melange",
+}
