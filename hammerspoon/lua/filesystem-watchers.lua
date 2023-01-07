441b8ac70dc1fa2c478a14033eaab07cc78eaf83
🤖 automated <cron@job>
2023-01-07 23:07:29 +0100

    iMac Home (5)

--- hammerspoon/lua/filesystem-watchers.lua
+++ hammerspoon/lua/filesystem-watchers.lua
@@ -9,22 +9,28 @@ local home = os.getenv("HOME")
 
 -- BRAVE Bookmarks synced to Chrome Bookmarks (needed for Alfred)
 local browserFolder = home .. "/Library/Application Support/BraveSoftware/Brave-Browser/"
-bookmarkWatcher = pw(browserFolder .. "Default/Bookmarks", function()
-	hs.execute("BROWSER_FOLDER='" .. browserFolder .. "' ; " .. [[
+bookmarkWatcher = pw(
+	browserFolder .. "Default/Bookmarks",
+	function()
+		hs.execute("BROWSER_FOLDER='" .. browserFolder .. "' ; " .. [[
 		mkdir -p "$HOME/Library/Application Support/Google/Chrome/Default"
 		cp "$BROWSER_FOLDER/Default/Bookmarks" "$HOME/Library/Application Support/Google/Chrome/Default/Bookmarks"
 		cp "$BROWSER_FOLDER/Local State" "$HOME/Library/Application Support/Google/Chrome/Local State"
 	]])
-end):start()
+	end
+):start()
 
 --------------------------------------------------------------------------------
 
 -- Download Folder Badge
 -- requires "fileicon" being installed
 local downloadFolder = home .. "/Downloaded"
-downloadFolderWatcher = pw(downloadFolder, function()
-	hs.execute("zsh ./helpers/download-folder-badge/download-folder-icon.sh " .. downloadFolder)
-end):start()
+downloadFolderWatcher = pw(
+	downloadFolder,
+	function()
+		hs.execute("zsh ./helpers/download-folder-badge/download-folder-icon.sh " .. downloadFolder)
+	end
+):start()
 
 --------------------------------------------------------------------------------
 
@@ -83,39 +89,50 @@ fileHubWatcher = pw(fileHub, function(paths)
 		if fileName:sub(-15) == ".alfredworkflow" or fileName:sub(-4) == ".ics" then
 			runWithDelays(3, function() os.rename(file, home .. "/.Trash/" .. fileName) end)
 
-			-- ublacklist
+		-- ublacklist
 		elseif fileName == "ublacklist-settings.json" then
 			os.rename(file, browserSettings .. fileName)
 			notify(fileName .. " filed away.")
 
-			-- adguard backup
+		-- adguard backup
 		elseif fileName:match(".*_adg_ext_settings_.*%.json") then
 			os.rename(file, browserSettings .. "adguard-settings.json")
 			notify("AdGuard backup filed away.")
 
-			-- tampermonkey backup
+		-- sponsor block
+		elseif fileName:match("SponsorBlockConfig_.*%.json") then
+			os.rename(file, browserSettings .. "SponsorBlockConfig.json")
+			notify("SpondorBlockConfig filed away.")
+
+		-- tampermonkey backup
 		elseif fileName:match("tampermonkey%-backup-.+%.txt") then
 			os.rename(file, browserSettings .. "tampermonkey-settings.json")
 			notify("TamperMonkey backup filed away.")
 
-			-- watch later .urls from the office
+		-- watch later .urls from the office
 		elseif fileName:sub(-4) == ".url" and isIMacAtHome() then
 			os.rename(file, home .. "/Downloaded/" .. fileName)
 			notify("Watch Later URL moved to Video Downloads.")
 
-			-- visualised keyboard layouts
-		elseif fileName:match("base%-keyboard%-layout%.%w+") or fileName:match("app%-switcher%-layout%.%w+") or
-			fileName:match("vimrc%-remapping%.%w+") or fileName:match("marta%-key%-bindings%.%w+") or
-			fileName:match("hyper%-bindings%-layout%.%w+") or fileName:match("single%-keystroke%-bindings%.%w+")
+		-- visualised keyboard layouts
+		elseif
+			fileName:match("base%-keyboard%-layout%.%w+")
+			or fileName:match("app%-switcher%-layout%.%w+")
+			or fileName:match("vimrc%-remapping%.%w+")
+			or fileName:match("marta%-key%-bindings%.%w+")
+			or fileName:match("hyper%-bindings%-layout%.%w+")
+			or fileName:match("single%-keystroke%-bindings%.%w+")
 		then
 			os.rename(file, dotfilesFolder .. "/visualized-keyboard-layout/" .. fileName)
 			notify("Visualized Keyboard Layout filed away.")
 
 			-- Finder vim mode
 		elseif fileName:match("finder%-vim%-cheatsheet%.%w+") then
-			os.rename(file, home .. "/Library/Mobile Documents/com~apple~CloudDocs/Repos/finder-vim-mode/" .. fileName)
+			os.rename(
+				file,
+				home .. "/Library/Mobile Documents/com~apple~CloudDocs/Repos/finder-vim-mode/" .. fileName
+			)
 			notify("Finder Vim Layout filed away.")
-
 		end
 	end
 end):start()
@@ -127,8 +144,7 @@ obsiAlphaWatcher = pw(fileHub, function(files)
 		-- needs delay and crdownload check, since the renaming is sometimes not picked up by hammerspoon
 		if not (file:match("%.crdownload$") or file:match("%.asar%.gz$")) then return end
 		runWithDelays(0.5, function()
-			hs.execute(
-				[[cd "]] .. fileHub .. [[" || exit 1
+			hs.execute([[cd "]] .. fileHub .. [[" || exit 1
 				test -f obsidian-*.*.*.asar.gz || exit 1
 				killall Obsidian
 				mv obsidian-*.*.*.asar.gz "$HOME/Library/Application Support/obsidian/"
@@ -136,10 +152,9 @@ obsiAlphaWatcher = pw(fileHub, function(files)
 				rm obsidian-*.*.*.asar
 				gunzip obsidian-*.*.*.asar.gz
 				while pgrep -q "Obsidian" ; do sleep 0.1; done
-				open -a "Obsidian"]]
-			)
+				open -a "Obsidian"]])
 			-- close the created tab
-			applescript [[
+			applescript([[
 				tell application "Brave Browser"
 					set window_list to every window
 					repeat with the_window in window_list
@@ -152,7 +167,7 @@ obsiAlphaWatcher = pw(fileHub, function(files)
 						end repeat
 					end repeat
 				end tell
-			]]
+			]])
 		end)
 	end
 end):start()
