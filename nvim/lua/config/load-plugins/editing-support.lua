ba0568cb091a4273e8b0d6f5e660cb930586e7d4
🤖 automated <cron@job>
2023-01-08 13:07:28 +0100

    iMac Home (5)

--- nvim/lua/config/load-plugins/editing-support.lua
+++ nvim/lua/config/load-plugins/editing-support.lua
@@ -65,45 +65,4 @@ return {
 		lazy = true,
 		config = function() require("substitute").setup() end,
 	},
-	{
-		"chrisgrieser/nvim-recorder", -- better macros
-		dev = true,
-		keys = {
-			{ "9", nil, desc = " Play Macro" },
-			{ "8", nil, desc = "/ Breakpoint", remap = false },
-			{ "0", nil, desc = " Start/Stop Recording" },
-		},
-		config = function()
-			require("recorder").setup {
-				clear = true,
-				logLevel = vim.log.levels.TRACE,
-				mapping = {
-					startStopRecording = "0",
-					playMacro = "9",
-					editMacro = "c0",
-					switchSlot = "<C-0>",
-					addBreakPoint = "8",
-				},
-				dapSharedKeymaps = true,
-			}
-			local topSeparators = isGui() and { left = "", right = "" } or { left = "", right = "" }
-
-			-- INFO inserting needed, to not disrupt existing lualine-segment set by dap
-			lualineZ = require("lualine").get_config().winbar.lualine_z or {}
-			lualineY = require("lualine").get_config().winbar.lualine_y or {}
-			table.insert(lualineZ, 
-				{ require("recorder").recordingStatus, section_separators = topSeparators }
-			)
-			table.insert(lualineY, 
-				{ require("recorder").displaySlots, section_separators = topSeparators }
-			)
-
-			require("lualine").setup {
-				winbar = {
-					lualine_y = lualineY,
-					lualine_z = lualineZ,
-				},
-			}
-		end,
-	},
 }
---
+++ nvim/lua/config/load-plugins/macros.lua
@@ -0,0 +1,44 @@
+return {
+	{
+		"chrisgrieser/nvim-recorder", 
+		dev = true,
+		keys = {
+			{ "9", nil, desc = "/ Continue/Play" },
+			{ "8", nil, desc = "/ Breakpoint" },
+			{ "0", nil, desc = " Start/Stop Recording" },
+		},
+		config = function()
+			require("recorder").setup {
+				clear = true,
+				logLevel = vim.log.levels.TRACE,
+				mapping = {
+					startStopRecording = "0",
+					playMacro = "9",
+					editMacro = "c0",
+					switchSlot = "<C-0>",
+					addBreakPoint = "8",
+				},
+				dapSharedKeymaps = true,
+			}
+
+			local topSeparators = isGui() and { left = "", right = "" } or { left = "", right = "" }
+
+			-- INFO inserting needed, to not disrupt existing lualine-segment set by dap
+			lualineZ = require("lualine").get_config().winbar.lualine_z or {}
+			lualineY = require("lualine").get_config().winbar.lualine_y or {}
+			table.insert(lualineZ, 
+				{ require("recorder").recordingStatus, section_separators = topSeparators }
+			)
+			table.insert(lualineY, 
+				{ require("recorder").displaySlots, section_separators = topSeparators }
+			)
+
+			require("lualine").setup {
+				winbar = {
+					lualine_y = lualineY,
+					lualine_z = lualineZ,
+				},
+			}
+		end,
+	},
+}
