bac558ea9a5dda759a90296927fe79590bfecded
🤖 automated <cron@job>
2023-01-08 14:49:56 +0100

    iMac Home (9)

---
+++ nvim/after/ftplugin/python.lua
@@ -0,0 +1,9 @@
+require("config.utils")
+--------------------------------------------------------------------------------
+
+-- python forces us to use spaces :'(
+bo.expandtab = true
+bo.shiftwidth = 4
+bo.tabstop = 4
+bo.softtabstop = 4
+wo.listchars = "tab: >"
--- sketchybar/numberOfDrafts.py
+++ sketchybar/numberOfDrafts.py
@@ -11,12 +11,23 @@ TAG_TO_EXCLUDE_2 = sys.argv[2]
 
 
 def search_draft():
-	str_path_db = expanduser("~") + "/Library/Group Containers/GTFQ98J4YG.com.agiletortoise.Drafts/DraftStore.sqlite"
+    str_path_db = (
+        expanduser("~")
+        + "/Library/Group Containers/GTFQ98J4YG.com.agiletortoise.Drafts/DraftStore.sqlite"
+    )
 
-	with closing(sqlite3.connect(str_path_db)) as connection:
-		with closing(connection.cursor()) as cursor:
-			rows = cursor.execute(("select ZUUID, ZFOLDER, ZCACHED_TAGS from main.ZMANAGEDDRAFT where ZFOLDER = 0 and ZCACHED_TAGS != 'ZZZ" + TAG_TO_EXCLUDE_1 + "ZZZ' and ZCACHED_TAGS != 'ZZZ" + TAG_TO_EXCLUDE_2 + "ZZZ';")).fetchall()
-			return rows
+    with closing(sqlite3.connect(str_path_db)) as connection:
+        with closing(connection.cursor()) as cursor:
+            rows = cursor.execute(
+                (
+                    "select ZUUID, ZFOLDER, ZCACHED_TAGS from main.ZMANAGEDDRAFT where ZFOLDER = 0 and ZCACHED_TAGS != 'ZZZ"
+                    + TAG_TO_EXCLUDE_1
+                    + "ZZZ' and ZCACHED_TAGS != 'ZZZ"
+                    + TAG_TO_EXCLUDE_2
+                    + "ZZZ';"
+                )
+            ).fetchall()
+            return rows
 
 
 INBOX_DRAFTS = search_draft()
