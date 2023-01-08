fd2f90b7f3b4b4a50ee422a513e143e8deda0cef
🤖 automated <cron@job>
2023-01-08 15:35:32 +0100

    iMac Home (3)

--- nvim/lua/config/test.lua
+++ nvim/lua/config/test.lua
@@ -1,7 +1,13 @@
-local a = {
-   1,
-   2,
-   3,
-}
+local counter = 0
 
-if true then print("foobar") end
+function __dot_repeat(motion)
+	if motion == nil then
+		vim.o.operatorfunc = "v:lua.__dot_repeat"
+		return "g@"
+	end
+
+	print("counter:", counter, "motion:", motion)
+	counter = counter + 1
+end
+
+vim.keymap.set("n", "gt", __dot_repeat, { expr = true })
