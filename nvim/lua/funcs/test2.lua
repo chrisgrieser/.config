ebb4e4cbf4bbf1daf1754543e569a9e00098c0a8
🤖 automated <cron@job>
2023-01-10 18:36:07 +0100

    iMac Home (3)

--- nvim/lua/funcs/test2.lua
+++
@@ -1,8 +0,0 @@
-
-local marks = { "A", "B" }
-local globalMarks = fn.getmarklist()
-globalMarks = vim.tbl_filter(
-	function(item) return vim.tbl_contains(marks, item.mark:sub(2, 2)) end,
-	globalMarks
-)
-vim.pretty_print(globalMarks)
