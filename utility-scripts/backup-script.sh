4c689cccd094a6d064bc8771135c915d3a73d04f
🤖 automated <cron@job>
2023-01-16 14:50:52 +0100

    iMac Home (10)

--- utility-scripts/backup-script.sh
+++ utility-scripts/backup-script.sh
@@ -1,11 +1,15 @@
 #!/bin/zsh
+
 DEVICE_NAME="$(scutil --get ComputerName)"
 
-# Backup location
-VOLUME_NAME="$*"
-BACKUP_DEST="${VOLUME_NAME/#\~/$HOME}/Backup_$DEVICE_NAME"
+VOLUME_NAME="$(df -h | grep -io "\s/Volumes/.*" | cut -c2-)"
+if [[ $(echo "$VOLUME_NAME" | wc -l) -ne 1 ]] ; then echo "More than one volume connected." ; exit 1 ; fi
+
+BACKUP_DEST="$VOLUME_NAME/Backup_$DEVICE_NAME"
 mkdir -p "$BACKUP_DEST"
 cd "$BACKUP_DEST" || exit 1
+
+#───────────────────────────────────────────────────────────────────────────────
 
 # Log (on the Mac)
 LOG_LOCATION="$(dirname "$0")/backup.log"
@@ -26,27 +30,18 @@ function bkp() {
 # WARNING each command has to sync to individual folders, since otherwise
 # the --delete option will override the previous contents
 # INFO All paths needs to end with a slash
+# INFO locations defined in zshenv
 bkp "$HOME/Library/Preferences/" ./Library/Preferences
 bkp "$HOME/RomComs/" ./Homefolder/RomComs
 bkp "$DOTFILE_FOLDER" ./Homefolder/.config
 bkp "$VAULT_PATH" ./Homefolder/main-vault
 bkp "$ICLOUD" ./iCloud-Folder
-bkp "$PASSWORD_STORE_DIR" ./.password-store
-bkp "$HOME/.gnupg/" ./.gnupg
+bkp "$PASSWORD_STORE_DIR" ./Homefolder/password-store
+bkp "$HOME/.gnupg/" ./Homefolder/gnupg
 
 #───────────────────────────────────────────────────────────────────────────────
 
 echo "----------------------------------------------------"
-
-# Brew Dumps
-BREWDUMP_PATH="$BACKUP_DEST/installed-apps-and-packages"
-mkdir -p "$BREWDUMP_PATH"
-brew bundle dump --force --file "$BREWDUMP_PATH/Brewfile_$DEVICE_NAME"
-npm list -g --parseable | sed "1d" | sed -E "s/.*\///" >"$BREWDUMP_PATH/NPMfile_$DEVICE_NAME"
-pip3 list --not-required | tail -n+3 | grep -vE "Pillow|pip|pybind|setuptools|six|wheel" | cut -d" " -f1 >"$BREWDUMP_PATH/Pip3file_$DEVICE_NAME"
-echo "Brewfile, NPM-File, and Pip3-File dumped at \"$BREWDUMP_PATH\""
-
-#───────────────────────────────────────────────────────────────────────────────
 
 # Log (on Mac)
 echo "completed: $(date '+%H:%M')" >>"$LOG_LOCATION"
