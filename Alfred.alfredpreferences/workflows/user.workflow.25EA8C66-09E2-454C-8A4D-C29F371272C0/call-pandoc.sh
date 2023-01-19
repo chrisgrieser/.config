9f68244c88ba0681bc2720db0f2461367a8dceca
🤖 automated <cron@job>
2023-01-19 14:15:44 +0100

    mini Office (8)

--- Alfred.alfredpreferences/workflows/user.workflow.25EA8C66-09E2-454C-8A4D-C29F371272C0/call-pandoc.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.25EA8C66-09E2-454C-8A4D-C29F371272C0/call-pandoc.sh
@@ -9,32 +9,15 @@ function md2docx () {
 	pandoc \
 		"$INPUT_FILE" \
 		--output="$OUTPUT_FILE" \
-		--defaults=Paper-Word \
+		--defaults="md2docx" \
 		--metadata=date:"$(date "+%d. %B %Y")" \
 	&& open -R "$OUTPUT_FILE" \
 	&& open "$OUTPUT_FILE"
 }
 
-
+# copy / pipe output, e.g. for information on missing citekeys
 output=$(md2docx "$*" 2>&1)
 echo "$output" | pbcopy
 echo "$output"
 
-# function docx2md () {
-# 	cd "$(dirname "$*")" || return
-# 	INPUT_FILE="$(basename "$*")"
-# 	OUTPUT_FILE="${INPUT_FILE%.*}_imported.md"
-#
-# 	pandoc \
-# 		"$INPUT_FILE" \
-# 		--output="$OUTPUT_FILE" \
-# 		--defaults=docx2md
-#
-# 	mv ./attachments/media/* ./attachments/
-# 	rmdir ./attachments/media/
-# 	sed -i '' 's/\/media\//\//g' "$OUTPUT_FILE"
-# 	sed -i '' 's/„/"/' "$OUTPUT_FILE"
-#
-# 	open -R "$OUTPUT_FILE"
-# }
 
