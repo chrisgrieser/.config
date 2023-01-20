ae78acbbda61753355ec9ca6e9b108dfc6b66947
🤖 automated <cron@job>
2023-01-20 11:45:14 +0100

    iMac Home (4)

--- hammerspoon/helpers/dock-switching/dock-switcher.sh
+++ hammerspoon/helpers/dock-switching/dock-switcher.sh
@@ -3,7 +3,8 @@
 DATA_DIR=$(dirname "$0")
 MODE="$1"
 LAYOUT="$2"
-DOCK_PLIST=~"/Library/Preferences/com.apple.dock.plist"
+DOCK_PLIST="$HOME/Library/Preferences/com.apple.dock.plist"
+DOCK_CONFIG="$DATA_DIR/$LAYOUT.plist" 
 
 #-------------------------------------------------------------------------------
 
@@ -11,22 +12,22 @@ if [[ "$MODE" == "--load" ]]; then
 	if [[ -z "$LAYOUT" ]] ; then
 		echo "Layout to load is missing."
 		exit 1
-	elif [[ ! -e "$DATA_DIR/$LAYOUT.plist" ]]; then
+	elif [[ ! -e "$DOCK_CONFIG" ]]; then
 		echo "Layout '$LAYOUT' does not exist."
 		exit 1
 	fi
 	rm "$DOCK_PLIST" || exit 1
-	cp -a "$DATA_DIR/$LAYOUT.plist" "$DOCK_PLIST"
+	cp -a "$DOCK_CONFIG" "$DOCK_PLIST"
 	defaults import com.apple.dock "$DOCK_PLIST"
 	sleep 0.1
 	killall Dock
 	echo "Loaded layout '$LAYOUT'"
 elif [[ "$MODE" == "--save" ]]; then
 	if [[ -z "$LAYOUT" ]] ; then
-		echo "No layout to save is missing."
+		echo "Layout to save is missing."
 		exit 1
 	fi
-	cp -f "$DOCK_PLIST" "$DATA_DIR/$LAYOUT.plist"
+	cp -f "$DOCK_PLIST" "$DOCK_CONFIG"
 	echo "Saved as '$LAYOUT'"
 else
 	echo "Not a valid option."
