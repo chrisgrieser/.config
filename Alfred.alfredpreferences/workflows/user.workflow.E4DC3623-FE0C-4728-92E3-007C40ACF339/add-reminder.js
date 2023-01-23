7ce1a9ef46742a098b92c1d5d3c37a13cb0ad6d3
🤖 automated <cron@job>
2023-01-24 00:29:50 +0100

    iMac Home (6)

--- Alfred.alfredpreferences/workflows/user.workflow.E4DC3623-FE0C-4728-92E3-007C40ACF339/add-reminder.js
+++ Alfred.alfredpreferences/workflows/user.workflow.E4DC3623-FE0C-4728-92E3-007C40ACF339/add-reminder.js
@@ -3,8 +3,8 @@ function run(argv) {
 	const today = new Date();
 	const weekdayNum = today.getDay();
 
-	const daysUntilMonday = weekdayNum === 1 ? 7 : (8 - weekdayNum) % 7;
-	const daysUntilWedday = weekdayNum === 3 ? 7 : (10 - weekdayNum) % 7;
+	const daysUntilMonday = weekdayNum === 1 ? 7 : (1 - weekdayNum + 7) % 7;
+	const daysUntilWedday = weekdayNum === 3 ? 7 : (3 - weekdayNum + 7) % 7;
 	const daysUntilSunday = weekdayNum === 0 ? 7 : 7 - weekdayNum;
 
 	const query = argv[0];
@@ -12,36 +12,43 @@ function run(argv) {
 	const jsonArray = [
 		{
 			title: "Tomorrow",
+			variables: { selection: "Tomorrow" }, // label for notification
 			arg: 1,
 		},
 		{
 			title: "In 2 Days",
+			variables: { selection: "In 2 Days" },
 			arg: 2,
 		},
 		{
 			title: "In 7 days",
+			variables: { selection: "In 7 days" },
 			arg: 7,
 		},
 		{
 			title: "Next Monday",
+			variables: { selection: "Next Monday" },
 			arg: daysUntilMonday,
 		},
 		{
 			title: "Next Wednesday",
+			variables: { selection: "Next Wednesday" },
 			arg: daysUntilWedday,
 		},
 		{
 			title: "Next Sunday",
+			variables: { selection: "Next Sunday" },
 			arg: daysUntilSunday,
 		},
 		{
 			title: "In 2 weeks",
+			variables: { selection: "In 2 weeks" },
 			arg: 14,
 		},
 	];
 
 	return JSON.stringify({
-		variables: { eventText: query },
+		variables: { reminderText: query },
 		items: jsonArray,
 	});
 }
