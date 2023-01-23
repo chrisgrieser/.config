9f22970a68a01b6f911bdeed60d8cc3687d16c03
🤖 automated <cron@job>
2023-01-23 23:49:51 +0100

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/user.workflow.E4DC3623-FE0C-4728-92E3-007C40ACF339/add-reminder.js
+++ Alfred.alfredpreferences/workflows/user.workflow.E4DC3623-FE0C-4728-92E3-007C40ACF339/add-reminder.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env osascript -l JavaScript
 function run(argv) {
-
 	const today = new Date();
 	const weekdayNum = today.getDay();
-	const daysUntilMonday = (7 - weekdayNum).toString;
-	const daysUntilSunday = (6 - weekdayNum).toString;
+	const daysUntilMonday = (7 - weekdayNum).toString();
+	const daysUntilSunday = (7 - weekdayNum).toString();
+	const daysUntilWednesday = (6 - weekdayNum).toString();
 	const query = argv[0];
 
 	const jsonArray = [
@@ -17,16 +17,24 @@ function run(argv) {
 			arg: "2",
 		},
 		{
-			title: "In 3 Days",
-			arg: "3",
+			title: "In 7 days",
+			arg: "7",
 		},
 		{
 			title: "Next Monday",
 			arg: daysUntilMonday,
 		},
 		{
+			title: "Next Wednesday",
+			arg: daysUntilWednesday,
+		},
+		{
 			title: "Next Sunday",
 			arg: daysUntilSunday,
+		},
+		{
+			title: "In 2 weeks",
+			arg: "14",
 		},
 	];
 
