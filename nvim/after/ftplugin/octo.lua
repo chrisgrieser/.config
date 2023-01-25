25f69679662310d0194c7d5f046ac7e43dd3e94f
🤖 automated <cron@job>
2023-01-26 00:35:41 +0100

    iMac Home (4)

--- nvim/after/ftplugin/octo.lua
+++ nvim/after/ftplugin/octo.lua
@@ -1,7 +1,10 @@
+---@diagnostic disable: param-type-mismatch
 require("config.utils")
 --------------------------------------------------------------------------------
 
 -- decrease line length without zen mode plugins 
-vim.opt_local.signcolumn = "yes:9"
-
-opt_local.colorcolumn = ""
+vim.defer_fn(function ()
+	wo.showbreak=""
+	opt_local.colorcolumn = ""
+	opt_local.signcolumn = "yes:2"
+end, 100)
