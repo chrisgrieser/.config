0bf465b6feda3daf47702fbdd531c8f5d282af61
🤖 automated <cron@job>
2023-01-25 23:15:41 +0100

    iMac Home (5)

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/my-github-issues.js
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/my-github-issues.js
@@ -10,21 +10,27 @@ function run(argv) {
 		return [clean, camelCaseSeperated, str].join(" ");
 	}
 
-
 	//──────────────────────────────────────────────────────────────────────────────
 
 	const token = argv[0];
 	const username = $.getenv("github_username");
 	const issues = JSON.parse(
-		app.doShellScript(`echo "${token}" | gh search issues --with-token --involves=${username} --json="repository,title,url,number,state"`),
+		app.doShellScript(
+			`echo "${token}" | gh auth login --with-token ; gh search issues --involves=${username} --json="repository,title,url,number,state,commentsCount"`,
+		),
 	).map(item => {
+		const icon = item.state === "open" ? "🟣" : "🟢"
+		const repo = item.repository.nameWithOwner
+		const comments = item.commentsCount > 0 ? item.commentsCount.toString() + " "
+
+		if (issue.comments !== "0") comments = "   💬 " + issue.comments;
+
 		return {
-			title: item,
-			match: alfredMatcher(item),
-			subtitle: item,
-			arg: item,
+			title: `${icon} ${item.title}`,
+			subtitle: `#${item.number} ${repo}`,
+			match: alfredMatcher(item.title),
+			arg: item.url,
 		};
 	});
-	JSON.stringify({ items: issues });
-
+	return JSON.stringify({ items: issues });
 }
