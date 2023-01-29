3b78b2b1aa8b5f8dae7952ed4db7cf51d918ef88
🤖 automated <cron@job>
2023-01-29 02:39:01 +0100

    iMac Home (36)

--- Alfred.alfredpreferences/workflows/user.workflow.2C71D560-C064-4035-BF73-525C405E349F/anycomplete.js
+++ Alfred.alfredpreferences/workflows/user.workflow.2C71D560-C064-4035-BF73-525C405E349F/anycomplete.js
@@ -1,30 +1,19 @@
 #!/usr/bin/env osascript -l JavaScript
+
 function run(argv) {
 	ObjC.import("stdlib");
 	const app = Application.currentApplication();
 	app.includeStandardAdditions = true;
-
-	// function alfredMatcher(str) {
-	// 	const clean = str.replace(/-/g, " ");
-	// 	return [clean, str].join(" ");
-	// }
-
-	const onlineJSON = (url) => JSON.parse(app.doShellScript(`curl -s "${url}"`));
-
 	//──────────────────────────────────────────────────────────────────────────────
 
 	const baseURL = "https://duckduckgo.com/ac/?q=";
 	const query = argv.join("");
 
-	//──────────────────────────────────────────────────────────────────────────────
-
-	const jsonArray = onlineJSON(baseURL + query)
+	const jsonArray = JSON.parse(app.doShellScript(`curl -s "${baseURL}${query}"`))
 		.map(item => {
-			const completion = item.phrase;
 			return {
-				"title": completion,
-				"match": completion,
-				"arg": completion,
+				"title": item.phrase,
+				"arg": item.phrase,
 			};
 		});
 
