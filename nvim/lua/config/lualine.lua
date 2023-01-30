65b9fad2bee2083845fc79bb57d03b6fc31e03cb
🤖 automated <cron@job>
2023-01-31 00:03:49 +0100

    iMac Home (6)

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -87,8 +87,8 @@ local function currentFile()
 	local icon = bo.modifiable and "%% " or " "
 	local ft = bo.filetype
 	if bo.buftype == "terminal" then
-		local mode = fn.mode() == "n" and "[N]" or ""
-		return " Terminal " .. mode
+		local mode = fn.mode() == "n" and " [N]" or ""
+		return " Terminal" .. mode
 	elseif bo.buftype == "nofile" then
 		if curFile == "" then curFile = ft end
 		return " " .. curFile -- e.g. Codi
