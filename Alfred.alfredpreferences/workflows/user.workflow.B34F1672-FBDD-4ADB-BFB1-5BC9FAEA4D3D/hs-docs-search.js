3aa6a6f352585c2e9e7400e5f8133c65a105076e
🤖 automated <cron@job>
2023-02-04 01:21:38 +0100

    iMac Home (8)

--- Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/hs-docs-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/hs-docs-search.js
@@ -2,12 +2,10 @@
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
-const alfredMatcher = (str) => str.replace (/[-()_.:]/g, " ")
-	+ " " + str + " "
-	+ str.replace(/([A-Z])/g, " $1"); // match parts of CamelCase
+const alfredMatcher = str => str.replace(/[-()_.:]/g, " ") + " " + str + " " + str.replace(/([A-Z])/g, " $1"); // match parts of CamelCase
 
 ObjC.import("Foundation");
-function readFile (path, encoding) {
+function readFile(path, encoding) {
 	if (!encoding) encoding = $.NSUTF8StringEncoding;
 	const fm = $.NSFileManager.defaultManager;
 	const data = fm.contentsAtPath(path);
@@ -26,47 +24,36 @@ categoryArr.forEach(category => {
 	const children = category.items.length;
 	// categories
 	workArray.push({
-		"title": category.name,
-		"subtitle": `${children} ▪︎ ${category.desc}`,
-		"match": alfredMatcher (category.name),
-		"arg": `https://www.hammerspoon.org/docs/${category.name}.html`,
-		"uid": category.name,
-		"mods": {
-			"alt": {
-				"arg": category.name,
-				"subtitle": `⌥: Paste ${category.name}`,
-			},
-		},
+		title: category.name,
+		subtitle: `${children} ▪︎ ${category.desc}`,
+		match: alfredMatcher(category.name),
+		arg: `https://www.hammerspoon.org/docs/${category.name}.html`,
+		uid: category.name,
+		mods: { alt: { arg: category.name } },
 	});
 
 	// categories items
 	category.items.forEach(catItem => {
 		const shortdef = catItem.def.split("->")[0].trim();
 		workArray.push({
-			"title": catItem.def,
-			"subtitle": catItem.desc,
-			"match": alfredMatcher (shortdef),
-			"arg": `https://www.hammerspoon.org/docs/${category.name}.html#${catItem.name}`,
-			"uid": `${category.name}_${catItem.name}`,
-			"mods": {
-				"alt": {
-					"arg": shortdef,
-					"subtitle": `⌥: Paste ${shortdef}`,
-				},
-			},
-
+			title: catItem.def,
+			subtitle: catItem.desc,
+			match: alfredMatcher(shortdef),
+			arg: `https://www.hammerspoon.org/docs/${category.name}.html#${catItem.name}`,
+			uid: `${category.name}_${catItem.name}`,
+			mods: { alt: { arg: shortdef, }, },
 		});
 	});
 });
 workArray.push({
-	"title": "Getting Started",
-	"match": "getting started examples",
-	"arg": "https://www.hammerspoon.org/go/",
-	"uid": "getting-started",
-	"mods": {
-		"alt": {
-			"valid": false,
-			"subtitle": "❌",
+	title: "Getting Started",
+	match: "getting started examples",
+	arg: "https://www.hammerspoon.org/go/",
+	uid: "getting-started",
+	mods: {
+		alt: {
+			valid: false,
+			subtitle: "❌",
 		},
 	},
 });
