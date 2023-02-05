846a3af3652b1ea4165e8896e37d0a1263b168f7
🤖 automated <cron@job>
2023-02-05 16:03:48 +0100

    iMac Home (3)

--- hammerspoon/lua/twitter.lua
+++ hammerspoon/lua/twitter.lua
@@ -60,34 +60,6 @@ local function moveCurWinToOtherDisplay()
 	end)
 end
 
-local function pagedownAction()
-	if #hs.screen.allScreens() > 1 then
-		moveCurWinToOtherDisplay()
-	-- disable in Finder due to conflict with finder vim mode
-	elseif appIsRunning("Twitter") and not frontAppName() == "Finder" then 
-		keystroke({}, "down", 1, app("Twitter")) -- tweet down
-	end
-end
-
-local function pageupAction()
-	if #hs.screen.allScreens() > 1 then
-		moveCurWinToOtherDisplay()
-	elseif appIsRunning("Twitter") and not frontAppName() == "Finder" then
-		keystroke({}, "up", 1, app("Twitter")) -- tweet up
-	end
-end
-
-local function endAction()
-	if appIsRunning("zoom.us") then
-		alert("📹/⛔") -- toggle video
-		keystroke({ "shift", "command" }, "V", 1, app("zoom.us"))
-		return
-	elseif appIsRunning("Twitter") then
-		keystroke({ "command" }, "K", 1, app("Twitter")) -- open tweet
-		app("Twitter"):activate() -- so media windows come to the foreground
-	end
-end
-
 local function homeAction()
 	if appIsRunning("zoom.us") then
 		alert("🔈/🔇") -- toggle mute
@@ -99,7 +71,6 @@ end
 
 -- Hotkeys
 hotkey({}, "f6", moveCurWinToOtherDisplay) -- for apple keyboard
-hotkey({}, "pagedown", pagedownAction, nil, pagedownAction)
-hotkey({}, "pageup", pageupAction, nil, pageupAction)
+hotkey(hyper, "pagedown", moveCurWinToOtherDisplay)
+hotkey(hyper, "pageup", moveCurWinToOtherDisplay)
 hotkey({}, "home", homeAction)
-hotkey({}, "end", endAction)
