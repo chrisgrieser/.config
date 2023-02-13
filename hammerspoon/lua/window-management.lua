a56ef3b8e8e0770377d23fb53b50fc82bc1007c9
🤖 automated <cron@job>
2023-02-13 10:50:28 +0100

    iMac Home (2)

--- hammerspoon/lua/window-management.lua
+++ hammerspoon/lua/window-management.lua
@@ -154,6 +154,7 @@ end
 function AutoTile(windowFilter)
 	if not windowFilter then return end
 	local wins = windowFilter:getWindows()
+	notify("#wins:" + tostring(#wins))
 
 	if #wins == 0 and frontAppName() == "Finder" then
 		-- prevent quitting when window is created imminently
@@ -162,7 +163,8 @@ function AutoTile(windowFilter)
 			-- 2) check if window count has changed in the meantime
 			-- 3) delay needs to be high enough since e.g. during quitting fullscreen
 			-- mode, Hammerspoon temporarily cannot detect Finder windows (sic!)
-			-- if #windowFilter:getWindows() == 0 then app("Finder"):kill() end
+			if #windowFilter:getWindows() == 0 then app("Finder"):kill() end
+			notify("beep")
 		end)
 	elseif #wins == 1 then
 		if isProjector() then
