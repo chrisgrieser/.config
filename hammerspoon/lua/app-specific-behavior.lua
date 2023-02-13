8934b2c9749af647ec3651adee29056c2057e758
🤖 automated <cron@job>
2023-02-13 10:53:39 +0100

    iMac Home (2)

--- hammerspoon/lua/app-specific-behavior.lua
+++ hammerspoon/lua/app-specific-behavior.lua
@@ -267,6 +267,7 @@ Wf_finder = wf.new("Finder")
 FinderAppWatcher = aw.new(function(appName, eventType, finderAppObj)
 	if not (appName == "Finder") then return end
 	if not (#finderAppObj:allWindows() > 0) then return end
+	notify("beep")
 
 	if eventType == aw.activated then
 		AutoTile(Wf_finder) -- also triggered via app-watcher, since windows created in the bg do not always trigger window filters
