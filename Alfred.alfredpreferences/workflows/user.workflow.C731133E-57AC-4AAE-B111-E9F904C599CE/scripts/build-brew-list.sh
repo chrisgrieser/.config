e9f4120a5a6240eec111e57b6753dafaf90eb12b
🤖 automated <cron@job>
2023-02-17 10:48:15 +0100

    iMac Home (15)

---
+++ Alfred.alfredpreferences/workflows/user.workflow.C731133E-57AC-4AAE-B111-E9F904C599CE/scripts/build-brew-list.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
+
+# shellcheck disable=2154
+mkdir -p "$alfred_workflow_cache"
+cache="${alfred_workflow_cache}/cache.json"
+
+# build cache on first run
+[[ ! -f "$cache" ]] && osascript -l JavaScript "./scripts/build-brew-list.js" >"$cache"
+cat "$cache"
+
