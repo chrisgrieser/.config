b020c967e4786334b14b8af25302ec3aa1fc1446
🤖 automated <cron@job>
2023-02-17 22:34:42 +0100

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/user.workflow.C731133E-57AC-4AAE-B111-E9F904C599CE/scripts/update-cache.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.C731133E-57AC-4AAE-B111-E9F904C599CE/scripts/update-cache.sh
@@ -4,5 +4,8 @@ export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 # check if cache is old
 # shellcheck disable=2154
 cache="${alfred_workflow_cache}/cache.json"
-[[ -z "$(find "${cache}" -mtime -1)" ]] && osascript -l JavaScript "./scripts/build-brew-list.js" >"$cache"
+updating="${alfred_workflow_cache}/is-updating"
 
+touch "$updating"
+[[ -z "$(find "${cache}" -mtime -1)" ]] && osascript -l JavaScript "./scripts/build-brew-list.js" >"$cache"
+rm -f "$updating"
