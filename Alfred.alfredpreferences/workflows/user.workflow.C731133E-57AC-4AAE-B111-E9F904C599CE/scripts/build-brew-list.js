853479c6598c1b989cc92990ae4be2e3a6694ef5
🤖 automated <cron@job>
2023-02-17 23:14:41 +0100

    iMac Home (5)

--- Alfred.alfredpreferences/workflows/user.workflow.C731133E-57AC-4AAE-B111-E9F904C599CE/scripts/build-brew-list.js
+++ Alfred.alfredpreferences/workflows/user.workflow.C731133E-57AC-4AAE-B111-E9F904C599CE/scripts/build-brew-list.js
@@ -2,6 +2,7 @@
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
+const home = app.pathTo("home folder");
 
 function alfredMatcher(str) {
 	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
@@ -9,12 +10,26 @@ function alfredMatcher(str) {
 	return [clean, camelCaseSeperated, str].join(" ");
 }
 
+function readFile(path) {
+	const fm = $.NSFileManager.defaultManager;
+	const data = fm.contentsAtPath(path);
+	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
+	return ObjC.unwrap(str);
+}
+
+const fileExists = filePath => Application("Finder").exists(Path(filePath));
+
 //──────────────────────────────────────────────────────────────────────────────
-// INFO https://formulae.brew.sh/docs/api/
 
 const jsonArray = [];
-const casks = JSON.parse(app.doShellScript(`curl -sL "https://formulae.brew.sh/api/cask.json"`));
-const formula = JSON.parse(app.doShellScript(`curl -sL "https://formulae.brew.sh/api/formula.json"`));
+
+// INFO https://formulae.brew.sh/docs/api/
+const caskJson = home + "/Library/Caches/Homebrew/api/cask.json";
+const formulaJson = home + "/Library/Caches/Homebrew/api/formula.json";
+if (!fileExists(caskJson) || !fileExists(formulaJson)) app.doShellScript(`brew update`);
+
+const casks = JSON.parse(readFile(caskJson));
+const formula = JSON.parse(readFile(formulaJson));
 
 casks.forEach(item => {
 	item = item.token;
