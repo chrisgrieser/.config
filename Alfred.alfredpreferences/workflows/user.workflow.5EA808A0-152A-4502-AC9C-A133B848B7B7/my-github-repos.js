a638f594e5cac368d2e3f37d99373be1cedd0df9
🤖 automated <cron@job>
2023-02-18 20:59:06 +0100

    iMac Home (2)

---
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/my-github-repos.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env osascript -l JavaScript
+/* eslint-disable complexity */
+function run() {
+	ObjC.import("stdlib");
+	const app = Application.currentApplication();
+	app.includeStandardAdditions = true;
+
+	function alfredMatcher(str) {
+		const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
+		const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
+		return [clean, camelCaseSeperated, str].join(" ");
+	}
+
+	//──────────────────────────────────────────────────────────────────────────────
+
+	const resultsNumber = $.getenv("results_number");
+	const username = $.getenv("github_username");
+	const issues = JSON.parse(
+		app.doShellScript(`curl -sL "https://api.github.com/search/issues?q=involves:${username}&per_page=${resultsNumber}"`),
+	).items.map(item => {
+		const isPR = Boolean(item.pull_request)
+		const merged = Boolean(item.pull_request?.merged_at)
+		const title = item.title;
+		const repo = item.repository_url.match(/[^/]+\/[^/]+$/)[0];
+		const comments = item.comments > 0 ? "💬 " + item.comments.toString() : "";
+
+		let icon; // also lists PRs due to --include-prs
+		if (item.state === "open" && isPR) icon = "🟦 ";
+		else if (item.state === "closed" && isPR && merged) icon = "🟨 ";
+		else if (item.state === "closed" && isPR && !merged) icon = "🟥 ";
+		else if (item.state === "closed" && !isPR) icon = "🟣 ";
+		else if (item.state === "open" && !isPR) icon = "🟢 ";
+		if (title.toLowerCase().includes("request") || title.includes("FR")) icon += "🙏 ";
+		if (title.toLowerCase().includes("bug")) icon += "🪲 ";
+
+		let matcher = alfredMatcher(item.title) + " " + alfredMatcher(repo);
+		if (isPR) matcher += " pr";
+
+		return {
+			title: icon + title,
+			subtitle: `#${item.number}  ${repo}   ${comments}`,
+			match: matcher,
+			arg: item.html_url,
+		};
+	});
+	return JSON.stringify({ items: issues });
+}
