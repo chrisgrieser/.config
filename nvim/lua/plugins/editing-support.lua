fe163f4e51e747b6efeb5e6b6255b02c581b3980
🤖 automated <cron@job>
2023-02-18 15:07:40 +0100

    iMac Home (5)

--- nvim/lua/plugins/editing-support.lua
+++ nvim/lua/plugins/editing-support.lua
@@ -10,9 +10,8 @@ return {
 	{ "bkad/CamelCaseMotion", event = "BufReadPost" },
 	{
 		"mbbill/undotree",
-		keys = "<leader>u",
+		cmd = "UndotreeToggle",
 		init = function()
-			vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>", { desc = "Undotree" })
 			vim.g.undotree_WindowLayout = 3
 			vim.g.undotree_DiffpanelHeight = 8
 			vim.g.undotree_ShortIndicators = 1
@@ -21,6 +20,14 @@ return {
 			vim.g.undotree_SetFocusWhenToggle = 1
 			vim.g.undotree_DiffCommand = "delta"
 			vim.g.undotree_HelpLine = 1
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "undotree",
+				callback = function()
+					vim.keymap.set("n", "<D-w>", ":UndotreeToggle<CR>", { buffer = true })	
+					vim.opt_local.listchars = "space: "
+					-- vim.opt_local.listchars = {space = " "}
+				end,
+			})
 		end,
 	},
 
