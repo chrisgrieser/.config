bef9c3ff7462f7b237de499cc05b567b46dc5598
🤖 automated <cron@job>
2023-02-18 02:47:41 +0100

    iMac Home (7)

--- utility-scripts/change-browser-across-configs.sh
+++ utility-scripts/change-browser-across-configs.sh
@@ -31,3 +31,5 @@ rg "$FROM_BROWSER_PATH" --files-with-matches \
 	| grep -v "/user.workflow.3BF713ED-02D0-4127-8126-26E36BF15CFC/" \
 	| grep -v "$0" \
 	| xargs -I {} sed -i '' "s|$FROM_BROWSER_PATH|$TO_BROWSER_PATH|g" '{}' 
+
+# TODO afterwards, restart Hammerspoon
