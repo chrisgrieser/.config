a11119b27cc80cd8d698ef36e273c0ddcd7ce5ad
🤖 automated <cron@job>
2023-02-18 16:27:39 +0100

    iMac Home (6)

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -12,11 +12,10 @@ require("lua.system-and-cron")
 require("lua.filesystem-watchers")
 require("lua.app-specific-behavior")
 require("lua.twitter")
--- require("lua.notification-watcher")
+require("lua.hardware-periphery")
 
 if IsIMacAtHome() or IsAtMother() then
 	require("lua.hot-corner-action")
-	require("lua.usb-watchers")
 end
 
 HoleCover()
