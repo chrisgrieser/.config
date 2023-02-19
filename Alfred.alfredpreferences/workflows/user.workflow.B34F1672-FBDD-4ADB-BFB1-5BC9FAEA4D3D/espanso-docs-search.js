315cc60b636f0e4e6b023a4198b724ce1d82c22f
🤖 automated <cron@job>
2023-02-19 18:39:10 +0100

    iMac Home (211)

--- Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/espanso-docs-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/espanso-docs-search.js
@@ -4,10 +4,11 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 const alfredMatcher = (str) => str.replace (/[-()_./]/g, " ") + " " + str + " ";
 
+//──────────────────────────────────────────────────────────────────────────────
+
 const workArray = JSON.parse(app.doShellScript('curl -s "https://api.github.com/repos/espanso/website/git/trees/main?recursive=1"'))
 	.tree
-	.filter(file => /^docs\/.*\.mdx?$/.test(file.path))
-	.filter(file => !file.path.includes("/_"))
+	.filter(file => /^docs\/.*\.mdx?$/.test(file.path) && !file.path.includes("/_"))
 	.map(file => {
 		const site = file.path
 			.slice(5) // remove "docs/"
