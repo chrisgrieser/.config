6be1f97fd7c6a6e3a028cc813f46dcde60aeb7a6
🤖 automated <cron@job>
2023-02-20 12:19:27 +0100

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/user.workflow.27AEFE2B-5249-48CE-A035-08E3252923EC/installed-browser-extensions.js
+++ Alfred.alfredpreferences/workflows/user.workflow.27AEFE2B-5249-48CE-A035-08E3252923EC/installed-browser-extensions.js
@@ -25,26 +25,28 @@ const jsonArray = app
 	.doShellScript(`find "${extensionFolder}" -name "manifest.json"`)
 	.split("\r")
 	.map(manifestPath => {
-		const id = manifestPath.replace(/.*Extensions\/(\w+)\/.*/, "$1") 
+		const root = manifestPath.slice(0, -13) /* eslint-disable-line no-magic-numbers */
+		const id = root.replace(/.*Extensions\/(\w+)\/.*/, "$1");
 		const manifest = JSON.parse(readFile(manifestPath));
 		const description = manifest.description.startsWith("__MSG_") ? "" : manifest.description;
-		const root = 
 
 		let name = manifest.name;
 		if (name.startsWith("__MSG_") && manifest.short_name) name = manifest.short_name;
 		if (name.startsWith("__MSG_")) {
-			const messagesPath = manifestPath.replace(/manifest.json$/, "_locales/en/messages.json");
-			const messagesJson = JSON.parse(readFile(messagesPath));
+			const messagesJson = JSON.parse(readFile(root + "_locales/en/messages.json"));
 			name = messagesJson.extensionName?.message ? messagesJson.extensionName.message : "[name missing]";
 		}
 
-		const iconPath =  manifest.icons["128"];
+		const iconPath = root + manifest.icons["128"];
+		const optionsSubPath = manifest.options_ui?.page ? manifest.options_ui.page : "";
+		const emoji = optionsSubPath === "" ? "" : " ⚙️";
 
 		return {
 			title: name,
-			subtitle: iconPath,
+			subtitle: emoji,
 			match: alfredMatcher(name),
 			icon: { path: iconPath },
+			valid: optionsSubPath === "",
 			arg: id,
 			uid: id,
 		};
