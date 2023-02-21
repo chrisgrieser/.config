1c61350c71830c109637dc2ca449aed033c2dae2
🤖 automated <cron@job>
2023-02-21 16:26:20 +0100

    iMac Home (9)

--- Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/yq-docs-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/yq-docs-search.js
@@ -4,23 +4,22 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 
 function alfredMatcher(str) {
-	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
-	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
-	return [clean, camelCaseSeperated, str].join(" ");
+	return str.replace(/[-()_.:#/\\;,[\]]/g, " ");
 }
 
 //──────────────────────────────────────────────────────────────────────────────
 
 const docsURL = "https://api.github.com/repos/mikefarah/yq/git/trees/master?recursive=1";
 const baseURL = "https://mikefarah.gitbook.io/yq/";
+const docPathRegex = /^pkg\/yqlib\/doc\/(.*)\.md$/i;
 
 const workArray = JSON.parse(app.doShellScript(`curl -s "${docsURL}"`))
-	.tree.filter(file => /^pkg\/yqlib\/doc\/.*\.md/.test(file.path))
+	.tree.filter(file => docPathRegex.test(file.path) && !file.path.includes("/headers/"))
 	.map(entry => {
-		const subsite = entry.path.slice(4, -4);
+		const subsite = entry.path.replace(docPathRegex, "$1");
 		const category = subsite.split("/")[0];
 		const displayTitle = subsite.split("/")[1];
-		const url = `${baseURL}/${subsite}.html`
+		const url = `${baseURL}/${subsite}`;
 
 		return {
 			title: displayTitle,
