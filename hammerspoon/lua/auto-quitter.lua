41459fa87b1a871a7b0442c00feae1f9c6d38ea3
🤖 automated <cron@job>
2023-02-23 16:37:59 +0100

    iMac Home (5)

---
+++ hammerspoon/lua/auto-quitter.lua
@@ -0,0 +1,25 @@
+require("lua.utils")
+--------------------------------------------------------------------------------
+-- inspired by the macOS app "quitter"
+
+---table containing all apps with their last activation time
+IdleApps = {}
+
+---log times when an app has been deactivated
+DeactivationWatcher = Aw.new(function(appName, eventType)
+	if eventType == Aw.deactivated then
+		local now = os.time()
+		IdleApps[appName] = now
+	elseif eventType == Aw.activated then
+		IdleApps[appName] = nil -- removes active apps from the table
+	end
+end):start()
+
+---check every ten second for idle apps
+hs.timer.doEvery(10, function ()
+	local now = os.time()
+	if IdleApps["Spotify"] then
+		local diff = IdleApps["Spotify"] - now
+		local treshhold = 60
+	end
+end)
