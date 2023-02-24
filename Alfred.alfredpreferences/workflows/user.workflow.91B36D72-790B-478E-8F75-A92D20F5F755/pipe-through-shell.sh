ac5befeb58b986062150a7eb5598852b9abc07bc
🤖 automated <cron@job>
2023-02-24 14:12:52 +0100

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/pipe-through-shell.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/pipe-through-shell.sh
@@ -2,10 +2,20 @@
 # shellcheck disable=2154
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 
-cmd="$*"
-result=$(bash -c "$(cat << EOF | ${cmd}
+#───────────────────────────────────────────────────────────────────────────────
+# Based on
+# https://medium.com/@gareth.stretton/obsidian-do-almost-anything-really-with-system-commands-b496ffd0679c
+# https://medium.com/@gareth.stretton/obsidian-part-2-system-commands-cdc20836a2b8
+
+#───────────────────────────────────────────────────────────────────────────────
+
+pipe_cmd="$*"
+result="$(cat << EOF | ${pipe_cmd}
 ${selection}
 EOF
-)")
+)"
 
-echo -n "$result"
+# paste via clipboard
+echo -n "$result" | pbcopy
+sleep 0.1
+osascript -e 'tell application "System Events" to keystroke "v" using {command down}'
