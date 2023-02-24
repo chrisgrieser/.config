8ae12761913a6d284e2ebaae0b4df30a7ca7eeef
🤖 automated <cron@job>
2023-02-24 14:32:52 +0100

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/pipe-through-shell.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/pipe-through-shell.sh
@@ -1,19 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 # shellcheck disable=2154
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
-
 #───────────────────────────────────────────────────────────────────────────────
 # Based on
 # https://medium.com/@gareth.stretton/obsidian-do-almost-anything-really-with-system-commands-b496ffd0679c
 # https://medium.com/@gareth.stretton/obsidian-part-2-system-commands-cdc20836a2b8
-
 #───────────────────────────────────────────────────────────────────────────────
 
 pipe_cmd="$*"
-result="$(cat << EOF | ${pipe_cmd}
+result=$(cat <<EOF | $pipe_cmd
 ${selection}
 EOF
-)"
+)
 
 # paste via clipboard
 echo -n "$result" | pbcopy
