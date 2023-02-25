46cdc9a74408acf143f45e9fe5273b3638cf2c4f
🤖 automated <cron@job>
2023-02-25 13:09:11 +0100

    iMac Home (4)

--- karabiner/assets/complex_modifications/bla.sh
+++ karabiner/assets/complex_modifications/bla.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
-
 selection=$(osascript -e 'tell application "Finder" to return POSIX path of (selection as alias)')
-ext=${$selection##*.}
+ext=${selection##*.}
+if [[ "$ext" == "bkp" ]]; then
+	mv "$selection" "${selection:0:4}"	
+else
+	mv "$selection" "$selection.bkp"	
+fi
