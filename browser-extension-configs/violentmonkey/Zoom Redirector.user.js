3192cab70a37c8822d7405007abe5c6694cbcf67
🤖 automated <cron@job>
2023-02-27 10:18:27 +0100

    iMac Home (5)

--- browser-extension-configs/violentmonkey/Zoom Redirector.user.js
+++ browser-extension-configs/violentmonkey/Zoom Redirector.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         Zoom Redirector
 // @namespace    Violentmonkey Scripts
-// @version      0.1
+// @version      1.0
 // @description  Open directly in Zoom App instead of opening a tab
 // @author       pseudometa
 // @match        *://*.zoom.us/j/*
 // ==/UserScript==
 
-const url = document.URL
-	.replace (/https?:\/\/.*\.zoom\.us\/j\/(\w+)\?pwd=(\w+)$/,"zoommtg://zoom.us/join?confno=$1&pwd=$2");
+/* global document, window */
+const url = document.URL.replace(
+	/https?:\/\/.*\.zoom\.us\/j\/(\w+)\?pwd=(\w+)$/,
+	"zoommtg://zoom.us/join?confno=$1&pwd=$2",
+);
 
-window.location.href = (url);
+window.location.href = url;
