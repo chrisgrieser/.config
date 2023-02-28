82c4d2b10f8b17ae09621a270f94b2d32760eca3
🤖 automated <cron@job>
2023-02-28 12:37:07 +0100

    iMac Home (5)

---
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/open-at-github.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
+
+# shellcheck disable=2034
+GIT_OPTIONAL_LOCKS=0
+file="$1"
+cd "$(dirname "$file")" || exit
+if ! git rev-parse --is-inside-work-tree &>/dev/null ; then echo "$file is not ins a git repository." && exit 1 ; fi
+
+#───────────────────────────────────────────────────────────────────────────────
+
+BRANCH=$(git branch --show-current)
+ROOT_PATH=$(git rev-parse --show-toplevel)
+ROOT_LEN=${#ROOT_PATH}
+REMOTE_URL="$(git remote -v | head -n1 | cut -f2 | cut -d' ' -f1 | sed -e's/:/\//' -e 's/git@/https:\/\//' -e 's/\.git//')"
+PATH_IN_REPO=${file:$ROOT_LEN}
+PATH_IN_REPO=$(osascript -l JavaScript -e "encodeURIComponent('$PATH_IN_REPO')")
+
+URL="$REMOTE_URL/blob/$BRANCH/$PATH_IN_REPO"
+echo "$URL" | pbcopy
+open "$URL"
