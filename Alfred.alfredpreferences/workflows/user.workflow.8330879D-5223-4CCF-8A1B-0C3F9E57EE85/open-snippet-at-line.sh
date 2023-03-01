ab711aa6604b0b2d0a0c6bd4a2f60068d02694b2
🤖 automated <cron@job>
2023-03-01 18:26:32 +0100

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/user.workflow.8330879D-5223-4CCF-8A1B-0C3F9E57EE85/open-snippet-at-line.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.8330879D-5223-4CCF-8A1B-0C3F9E57EE85/open-snippet-at-line.sh
@@ -2,5 +2,7 @@
 
 snippet_file="$1"
 snippet="$2"
+line_number=$(grep --line-number --max-count=1 "\"$snippet\"" "$snippet_file" | cut -d: -f1)
 
-grep --line-number --max-count=1 "\"$snippet\"" "$snippet_file"
+# workaround for https://github.com/neovide/neovide/issues/1604
+open "$snippet_file" --env=LINE="$line_number"
