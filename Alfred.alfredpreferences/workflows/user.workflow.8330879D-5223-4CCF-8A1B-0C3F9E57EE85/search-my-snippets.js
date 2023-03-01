d1d0c8dc642672cead451a46a18cdf400354c985
🤖 automated <cron@job>
2023-03-01 17:46:32 +0100

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/user.workflow.8330879D-5223-4CCF-8A1B-0C3F9E57EE85/search-my-snippets.js
+++ Alfred.alfredpreferences/workflows/user.workflow.8330879D-5223-4CCF-8A1B-0C3F9E57EE85/search-my-snippets.js
@@ -9,22 +9,37 @@ function alfredMatcher(str) {
 	return [clean, camelCaseSeperated, str].join(" ") + " ";
 }
 
+function readFile(path) {
+	const fm = $.NSFileManager.defaultManager;
+	const data = fm.contentsAtPath(path);
+	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
+	return ObjC.unwrap(str);
+}
+
 //──────────────────────────────────────────────────────────────────────────────
 
 const snippetDir = $.getenv("snippetDir").replace(/^~/, app.pathTo("home folder"));
-const jsonArray = app.doShellScript(`find "${snippetDir}" -type f -name "*.json"`)
+const jsonArray = app
+	.doShellScript(`find "${snippetDir}" -type f -name "*.json"`)
 	.split("\r")
-	.map(fPath => {
-		const parts = fPath.split("/")
-		const fileName = parts.pop().slice(0, -5);
-		const 
-		
-		return {
-			title: item,
-			match: alfredMatcher(item),
-			subtitle: item,
-			arg: item,
-			uid: item,
-		};
+	.filter(path => !path.endsWith("package.json"))
+	.forEach(snippetFile => {
+		const fileName = snippetFile.split("/").pop().slice(0, -5);
+		const snippetJson = JSON.parse(readFile(snippetFile));
+
+		Object.keys(snippetJson).forEach(snippet => {
+			if (!snippet.prefix || !snippet.body) return;
+			let desc = fileName;
+			if (snippet.description) desc += snippet.description;
+			jsonArray.push({
+				title: snippet.prefix,
+				subtitle: desc,
+				match: alfredMatcher(fileName) + " " + alfredMatcher(snippet),
+				arg: `${snippetFile}/${snippet.prefix}`,
+				mods: { alt: { arg: snippet.body } },
+				uid: `${fileName}/${snippet}`,
+			});
+		});
 	});
+
 JSON.stringify({ items: jsonArray });
