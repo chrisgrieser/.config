76840802aaeb18422607896d8024cc8e27167430
🤖 automated <cron@job>
2023-03-11 12:18:42 +0100

    iMac Home (3)

--- nvim/after/ftplugin/yaml.lua
+++ nvim/after/ftplugin/yaml.lua
@@ -1,26 +1,8 @@
 require("config.utils")
 --------------------------------------------------------------------------------
 
-local function leaveVisualMode()
-	-- https://github.com/neovim/neovim/issues/17735#issuecomment-1068525617
-	local escKey = vim.api.nvim_replace_termcodes("<Esc>", false, true, true)
-	vim.api.nvim_feedkeys(escKey, "nx", false)
-end
-
--- use 2 spaces instead of tabs
 bo.shiftwidth = 2
 bo.tabstop = 2
 bo.softtabstop = 2
 bo.expandtab = true
 opt_local.listchars = "tab: >,multispace:·,leadmultispace: "
-
--- does not work perfectly yet though
-keymap("x", "<D-m>", function()
-	leaveVisualMode()
-	cmd([['<,'> !yq -P -o=json -I=0]])
-	cmd([[.s/\d\@<!"\d\@!//ge]]) -- remove quotes that do not surround a number https://vim.fandom.com/wiki/Regex_lookahead_and_lookbehind
-	cmd([[.s/:/: /ge]]) -- fix some stuff since this isn't real json
-	cmd([[.s/,/, /ge]])
-	cmd([[.s/\\\\/\\/ge]])
-	normal("kJ")
-end, { desc = "minify YAML", buffer = true })
