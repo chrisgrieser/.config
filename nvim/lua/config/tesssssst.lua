16ad0e2a5e6b26a72a3f8fd5645be9d1f7a4205e
🤖 automated <cron@job>
2023-03-13 16:54:34 +0100

    iMac Home (3)

--- nvim/lua/config/tesssssst.lua
+++
@@ -1,7 +0,0 @@
-
--- orangetree
--- iiiiiiiii
-----------------------
--- applereeeeeeeee
--- applerree
-
--- nvim/lua/funcs/quickfix.lua
+++ nvim/lua/funcs/quickfix.lua
@@ -13,16 +13,23 @@ end
 
 --------------------------------------------------------------------------------
 
--- statusline component
+-- statusline component, showing current and total quickfix item
 function M.counter()
-	local count = #vim.fn.getqflist()
-	if count == 0 then return "" end
-	return " " .. tostring(count)
+	local totalItems = #vim.fn.getqflist()
+	if totalItems == 0 then return "" end
+	local out = " "
+	if g.qfCount then out = out .. tostring(g.qfCount) .. "/" end
+	out = out .. tostring(totalItems)
+	return out
 end
 
 --------------------------------------------------------------------------------
 -- keymaps
 
+---delete the quickfixlist
+function M.deleteList() vim.cmd.cexpr("[]") end
+
+---goto next quickfix and wrap around
 function M.next()
 	if quickFixIsEmpty() then return end
 	if not g.qfCount then g.qfCount = 0 end -- initialize counter
@@ -36,9 +43,25 @@ function M.next()
 	end
 end
 
-function M.replacerWrapper()
+---goto previous quickfix and wrap around
+function M.previous()
 	if quickFixIsEmpty() then return end
-	if not require("replacer") then 
+	if not g.qfCount then g.qfCount = #(vim.fn.getqflist()) end -- initialize counter
+
+	local wentToPrevious = pcall(function() cmd([[silent cprevious]]) end)
+	if wentToPrevious then
+		g.qfCount = g.qfCount - 1
+	else
+		cmd([[silent clast]])
+		g.qfCount = #(vim.fn.getqflist())
+	end
+end
+
+---wrapper around replacer.nvim for convenience
+---@param closingKeys string[]? keys to save changes and close quickfix window
+function M.replacerWrapper(closingKeys)
+	if quickFixIsEmpty() then return end
+	if not require("replacer") then
 		vim.notify("replacer.nvim not installed.", vim.log.levels.ERROR)
 		return
 	end
@@ -46,11 +69,15 @@ function M.replacerWrapper()
 	-- if not yet in quickfix window, open it
 	if vim.bo.filetype ~= "qf" then cmd.copen() end
 
+	-- run replacer
 	require("replacer").run { rename_files = true }
-	for _, key in pairs { "<D-w>", "<D-s>", "q" } do
-		keymap("n", key, function()
-			cmd.write()
-			vim.notify(" Finished replacing.")
+
+	-- set closing keybindings for replacer.nvim
+	if not closingKeys or #closingKeys == 0 then return end
+	for _, key in pairs(closingKeys) do
+		vim.keymap.set("n", key, function()
+			vim.cmd.write()
+			vim.notify(" Finished replacing.", vim.log.levels.INFO)
 		end, { desc = " Finish replacing", buffer = true, nowait = true })
 	end
 end
