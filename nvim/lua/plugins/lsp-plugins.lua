3034cd894fa6772699f89dc7201cfd374a1be504
🤖 automated <cron@job>
2023-03-13 20:35:47 +0100

    iMac Home (4)

--- nvim/lua/plugins/lsp-plugins.lua
+++ nvim/lua/plugins/lsp-plugins.lua
@@ -1,11 +1,7 @@
--- lsp attach function
-
-local signatureConfig = {
-
-				floating_window = false,
-				hint_prefix = "﬍ ",
-				hint_scheme = "NonText", -- highlight group
-}
+-- INFO by lazyloading all this plugins and only setting up / attaching them in
+-- the lspAttach autocmd, they are only loaded when an LSP server with the
+-- supporting capatibility is loaded, maximing the amount of lazyloading
+--------------------------------------------------------------------------------
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
@@ -13,79 +9,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		local capabilities = client.server_capabilities
 
-		-- stylua: ignore
+		-- stylua: ignore start
 		if capabilities.inlayHintProvider then
-			require("lsp-inlayhints").on_attach(client, bufnr)
-		end
-
-		if capabilities.documentSymbolProvider and client.name ~= "cssls" then
-			require("nvim-navic").attach(client, bufnr)
-		end
-
-		if capabilities.signatureHelpProvider then
-			require "lsp_signature".on_attach(signatureConfig, bufnr)
-		end
-	end,
-})
-
---------------------------------------------------------------------------------
-
-return {
-	{ -- schemas for json-lsp
-		"b0o/SchemaStore.nvim",
-		lazy = true, -- loaded on jsonls setup
-	},
-	{ -- breadcrumbs for statusline/winbar
-		"SmiteshP/nvim-navic",
-		lazy = true, -- loaded when attaching to supporting lsp servers
-		config = function()
-			require("nvim-navic").setup {
-				icons = { Object = "ﴯ " },
-				separator = "  ",
-				depth_limit = 7,
-				depth_limit_indicator = "…",
-			}
-		end,
-	},
-	{
-		"folke/neodev.nvim", -- lsp for nvim-lua config
-		lazy = false,
-		config = function()
-			-- INFO this must come before lua LSP setup
-			require("neodev").setup {
-				library = { plugins = false },
-			}
-		end,
-	},
-	{
-		"ray-x/lsp_signature.nvim",
-		lazy = true,
-		config = function()
-			require("lsp_signature").setup {
-				floating_window = false,
-				hint_prefix = "﬍ ",
-				hint_scheme = "NonText", -- highlight group
-			}
-		end,
-	},
-	{
-		-- INFO only temporarily needed, until https://github.com/neovim/neovim/issues/18086
-		"lvimuser/lsp-inlayhints.nvim",
-		lazy = true, -- attaching to supporting lsp servers (see init function)
-		init = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				callback = function(args)
-					local bufnr = args.buf
-					local client = vim.lsp.get_client_by_id(args.data.client_id)
-					local capabilities = client.server_capabilities
-
-					if capabilities.inlayHintProvider then
-						require("lsp-inlayhints").on_attach(client, bufnr)
-					end
-				end,
-			})
-		end,
-		config = function()
 			require("lsp-inlayhints").setup {
 				inlay_hints = {
 					parameter_hints = {
@@ -104,12 +29,53 @@ return {
 					highlight = "NonText", -- highlight group
 				},
 			}
-		end,
+			require("lsp-inlayhints").on_attach(client, bufnr)
+		end
+		if capabilities.documentSymbolProvider and client.name ~= "cssls" then
+			require("nvim-navic").setup {
+				icons = { Object = "ﴯ " },
+				separator = "  ",
+				depth_limit = 7,
+				depth_limit_indicator = "…",
+			}
+			require("nvim-navic").attach(client, bufnr)
+		end
+		if capabilities.signatureHelpProvider then
+			require("lsp_signature").on_attach({
+				floating_window = false,
+				hint_prefix = "﬍ ",
+				hint_scheme = "NonText", -- highlight group
+			}, bufnr)
+		end
+		if capabilities.renameProvider then
+			require("inc_rename").setup()
+		end
+		-- stylua: ignore end
+	end,
+})
+
+--------------------------------------------------------------------------------
+
+return {
+	{ -- schemas for json-lsp
+		"b0o/SchemaStore.nvim",
+		lazy = true, -- loaded on jsonls setup
+	},
+	{ -- breadcrumbs for statusline/winbar
+		"SmiteshP/nvim-navic",
+		lazy = true, -- loaded when attaching to supporting lsp servers
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		lazy = true, -- loaded when attaching
+	},
+	{
+		-- INFO only temporarily needed, until https://github.com/neovim/neovim/issues/18086
+		"lvimuser/lsp-inlayhints.nvim",
+		lazy = true, -- loaded when attaching
 	},
 	{
 		"smjonas/inc-rename.nvim",
-		lazy = true, -- loaded when attaching to supporting lsp servers
-		-- event = "LspAttach",
-		config = function() require("inc_rename").setup {} end,
+		lazy = true, -- loaded when attaching
 	},
 }
--- nvim/lua/plugins/mason-and-lspconfig.lua
+++ nvim/lua/plugins/mason-and-lspconfig.lua
@@ -16,6 +16,118 @@ local lsp_servers = {
 
 --------------------------------------------------------------------------------
 
+local lspSettings = {}
+local lspFileTypes = {}
+
+-- https://github.com/LuaLS/lua-language-server/wiki/Annotations#annotations
+-- https://github.com/LuaLS/lua-language-server/wiki/Settings
+lspSettings.lua_ls = {
+	Lua = {
+		format = { enable = false }, -- using stylua instead. Also, sumneko-lsp-formatting has this weird bug where all folds are opened
+		completion = {
+			callSnippet = "Replace",
+			keywordSnippet = "Replace",
+			displayContext = 2,
+		},
+		-- libraries defined per-project via luarc.json location: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
+		diagnostics = {
+			disable = { "trailing-space" },
+		},
+		workspace = { checkThirdParty = false }, -- HACK https://github.com/sumneko/lua-language-server/issues/679#issuecomment-925524834
+		hint = {
+			enable = true,
+			setType = true,
+			paramName = "All",
+			paramType = true,
+			arrayIndex = "Disable",
+		},
+		telemetry = { enable = false },
+	},
+}
+
+-- https://github.com/sublimelsp/LSP-css/blob/master/LSP-css.sublime-settings
+lspSettings.cssls = {
+	css = {
+		lint = {
+			vendorPrefix = "ignore",
+			propertyIgnoredDueToDisplay = "error",
+			universalSelector = "ignore",
+			float = "ignore",
+			boxModel = "ignore",
+			-- since these would be duplication with stylelint
+			duplicateProperties = "ignore",
+			emptyRules = "warning",
+		},
+		colorDecorators = { enable = true }, -- not supported yet
+	},
+}
+
+local jsAndTsSettings = {
+	-- https://github.com/typescript-language-server/typescript-language-server#workspacedidchangeconfiguration
+	format = {}, -- not used, since taken care of by prettier
+	inlayHints = {
+		includeInlayEnumMemberValueHints = true,
+		includeInlayFunctionLikeReturnTypeHints = true,
+		includeInlayFunctionParameterTypeHints = true,
+		includeInlayParameterNameHints = "all", -- none | literals | all
+		includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+		includeInlayPropertyDeclarationTypeHints = true,
+		includeInlayVariableTypeHints = true,
+		includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+	},
+}
+
+lspSettings.tsserver = {
+	completions = { completeFunctionCalls = true },
+	diagnostics = {
+		-- https://github.com/microsoft/TypeScript/blob/master/src/compiler/diagnosticMessages.json
+		ignoredCode = {},
+	},
+	typescript = jsAndTsSettings,
+	javascript = jsAndTsSettings,
+}
+
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#eslint
+lspSettings.eslint = {
+	quiet = false, -- = include warnings
+	codeAction = {
+		disableRuleComment = { location = "sameLine" }, -- add ignore-comments on the same line
+	},
+}
+
+-- https://github.com/sublimelsp/LSP-json/blob/master/LSP-json.sublime-settings
+lspSettings.jsonls = {
+	json = {
+		validate = { enable = true },
+		format = { enable = true },
+		schemas = require("schemastore").json.schemas(),
+	},
+}
+
+-- https://github.com/redhat-developer/yaml-language-server#language-server-settings
+lspSettings.yamlls = {
+	yaml = { keyOrdering = false }, -- FIX mapKeyOrder
+}
+
+--------------------------------------------------------------------------------
+
+lspFileTypes.bashls = { "sh", "zsh", "bash" } -- force lsp to work with zsh
+lspFileTypes.emmet_ls = { "css", "scss", "html" }
+
+--------------------------------------------------------------------------------
+
+-- Enable snippet capability for completion (nvim_cmp)
+local lspCapabilities = vim.lsp.protocol.make_client_capabilities()
+lspCapabilities.textDocument.completion.completionItem.snippetSupport = true
+
+-- Enable folding (nvim-ufo)
+lspCapabilities.textDocument.foldingRange = {
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
+}
+
+--------------------------------------------------------------------------------
+
 return {
 	{
 		"williamboman/mason.nvim",
@@ -41,8 +153,30 @@ return {
 			}
 		end,
 	},
+	{ -- lsp for nvim-lua config
+		"folke/neodev.nvim",
+		-- INFO this must come before lua lsp-config setup
+		lazy = false,
+		init = function()
+			require("neodev").setup {
+				library = { plugins = false },
+			}
+		end,
+	},
 	{
 		"neovim/nvim-lspconfig",
+		lazy = false,
+		init = function()
+			-- configure all lsp servers
+			for _, lsp in pairs(lsp_servers) do
+				local config = {
+					capabilities = lspCapabilities,
+					settings = lspSettings[lsp], -- if no settings, will assign nil and therefore to nothing
+					filetypes = lspFileTypes[lsp],
+				}
+				require("lspconfig")[lsp].setup(config)
+			end
+		end,
 		config = function()
 			-- Border Styling
 			require("lspconfig.ui.windows").default_options.border = BorderStyle
