cfbd59dd617586698f21425b9492ab9e1f828b63
🤖 automated <cron@job>
2023-03-14 17:07:41 +0100

    iMac Home (2)

--- nvim/lua/funcs/quality-of-life.lua
+++ nvim/lua/funcs/quality-of-life.lua
@@ -60,9 +60,7 @@ end
 -- UNDO
 
 -- Save Open time
-vim.api.nvim_create_augroup("undoTimeMarker", {})
 vim.api.nvim_create_autocmd("BufReadPost", {
-	group = "undoTimeMarker",
 	callback = function() b.timeOpened = os.time() end,
 })
 
@@ -105,32 +103,6 @@ function M.overscroll(action)
 		usedCount = tonumber(actionCount) * usedCount
 	end
 	normal(tostring(usedCount) .. action)
-end
-
----toggle wrap, colorcolumn, and hjkl visual/logical maps in one go
-function M.toggleWrap()
-	local opts = { buffer = true }
-
-	local wrapOn = vim.opt_local.wrap:get()
-	if wrapOn then
-		vim.opt_local.wrap = false
-		vim.opt_local.colorcolumn = opt.colorcolumn:get()
-		vim.keymap.del({ "n", "x" }, "H", opts)
-		vim.keymap.del({ "n", "x" }, "L", opts)
-		vim.keymap.del({ "n", "x" }, "J", opts)
-		vim.keymap.del({ "n", "x" }, "K", opts)
-		vim.keymap.del({ "n", "x" }, "k", opts)
-		vim.keymap.del({ "n", "x" }, "j", opts)
-	else
-		vim.opt_local.wrap = true
-		vim.opt_local.colorcolumn = ""
-		vim.keymap.set({ "n", "x" }, "H", "g^", opts)
-		vim.keymap.set({ "n", "x" }, "L", "g$", opts)
-		vim.keymap.set({ "n", "x" }, "J", function() M.overscroll("6gj") end, opts)
-		vim.keymap.set({ "n", "x" }, "K", "6gk", opts)
-		vim.keymap.set({ "n", "x" }, "k", "gk", opts)
-		vim.keymap.set({ "n", "x" }, "j", function() M.overscroll("gj") end, opts)
-	end
 end
 
 --------------------------------------------------------------------------------
