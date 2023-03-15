335def60b34cb0e4c1e66ef5d2b544eabdda6a80
🤖 automated <cron@job>
2023-03-15 21:38:09 +0100

    iMac Home (1)

--- nvim/lua/funcs/word-switcher.lua
+++ nvim/lua/funcs/word-switcher.lua
@@ -1,27 +1,45 @@
-
--- INFO `false` indicates that the second element is not exchanged with the first
+-- INFO `false` as third list items indicates that the second element is *not* 
+-- exchanged with the first
 local generalWords = {
-	{ "true", "false" },
+	-- on/off
 	{ "on", "off" },
 	{ "yes", "no" },
+	{ "y", "n" },
 	{ "disable", "enable" },
 	{ "disabled", "enabled" },
+	{ "1", "0" },
+	{ "true", "false" },
 
-	{ "warn", "error" },
+	-- opposites
 	{ "show", "hide" },
 	{ "min", "max" },
-	{ "read", "write" },
+	{ "minimum", "maximum" },
+	{ "increase", "decrease" },
+	{ "increased", "decreased" },
 	{ "always", "never" },
-
 	{ "next", "previous" },
 	{ "inner", "outer" },
 	{ "before", "after" },
 	{ "low", "high" },
 	{ "dark", "light" },
+	{ "first", "last" },
+	{ "up", "down" },
+	{ "right", "left" },
+	{ "black", "white" },
+	{ "red", "blue" },
 
+	-- commonly switched between
+	{ "variable", "constant" },
+	{ "read", "write" },
+	{ "warn", "error" },
+	{ "and", "or" },
+
+	-- comparisons
+	{ "<", ">" },
 	{ "<=", ">=" },
 	{ "!=", "==" },
 
+	-- units
 	{ "years", "months", false },
 	{ "months", "weeks", false },
 	{ "weeks", "days", false },
@@ -35,15 +53,15 @@ local filetypeSpecificWords = {
 	css = {
 		{ "padding", "margin" },
 		{ "top", "bottom" },
-		{ "right", "left" },
 		{ "relative", "absolute" },
 		{ "width", "height" },
-		{ "black", "white" },
-		{ "red", "blue" },
+		-- most common css terms already included in general words
 	},
 	lua = {
+		{ "==", "~=" },
+		{ "nil", "{}" },
 		{ "if", "elseif", false },
-		{ "and", "or" },
+		{ "not", "", false },
 		{ "elseif", "else", false },
 		{ "else", "if", false },
 		{ "function", "local function", false },
@@ -55,16 +73,19 @@ local filetypeSpecificWords = {
 		{ "True", "False" },
 	},
 	javascript = {
+		{ "null", "undefined", false },
 		{ "if", "} else if", false },
 		{ "else", "else if", false },
-		{ "var", "const", false },
+		{ "var", "const", false }, -- don't switch back to var!
 		{ "const", "let" },
 		{ "map", "forEach" },
 		{ "replace", "replaceAll" },
 		{ "===", "!==" },
+		{ "&&", "||" },
+		{ "return", "break" },
+		{ "default", "case" }, -- switch-case statements
 	},
 	sh = {
-		{ "y", "n" },
 		{ "lt", "gt" },
 		{ "eq", "nq" },
 		{ "&&", "||" },
@@ -73,12 +94,13 @@ local filetypeSpecificWords = {
 		{ "else", "if", false },
 		{ "echo", "print" },
 		{ "exit", "return" },
-		{ "1", "0" },
 	},
+
 	-- filetypes to link to another filetype
 	typescript = "javascript",
 	bash = "sh",
 	zsh = "sh",
+	fish = "sh",
 }
 
 --------------------------------------------------------------------------------
@@ -89,12 +111,9 @@ local M = {}
 function M.switch()
 	local ft = vim.bo.filetype
 	local wordsToUse = {}
-	for _, v in pairs(generalWords) do
-		table.insert(wordsToUse, v)
-	end
 
 	if filetypeSpecificWords[ft] then
-		-- filetype inherits word by other filetype
+		-- filetype inherits words by other filetype
 		if filetypeSpecificWords[ft] == "string" then ft = tostring(filetypeSpecificWords[ft]) end
 
 		---@diagnostic disable-next-line: param-type-mismatch --- type match ensured above
@@ -103,13 +122,18 @@ function M.switch()
 		end
 	end
 
+	-- INFO general words added *after* the filetype-specific words, so that the latter get priority
+	for _, v in pairs(generalWords) do 
+		table.insert(wordsToUse, v)
+	end
+
 	-- remove word-delimiters for <cword>
 	local iskeywBefore = vim.opt.iskeyword:get()
 	vim.opt.iskeyword:remove { "_", "-", "." }
 
 	local cword = vim.fn.expand("<cword>")
 	local cBigword = vim.fn.expand("<cWORD>")
-	
+
 	local alphaNumericUnderCursor = cBigword:find("[%a%d]")
 	local word = alphaNumericUnderCursor and cword or cBigword
 
