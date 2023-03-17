70bcd9f47068477e0d93bbac095fbfa46927af77
🤖 automated <cron@job>
2023-03-17 14:09:50 +0100

    iMac Home (8)

--- hammerspoon/lua/reload-and-systemstart.lua
+++ hammerspoon/lua/reload-and-systemstart.lua
@@ -1,13 +1,4 @@
 require("lua.utils")
---------------------------------------------------------------------------------
--- EVAL
-
-
-
-
-
---------------------------------------------------------------------------------
--- RELOAD AND SYSTEM START
 
 -- `hammerspoon://hs-reload` for reloading via Build System
 local reloadIndicator = "/tmp/hs-is-reloading"
