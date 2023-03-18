5bad8e5c8970d753da227bccdc3749d4f1112041
🤖 automated <cron@job>
2023-03-18 21:28:29 +0100

    iMac Home (3)

--- nvim/lua/config/gui-settings.lua
+++ nvim/lua/config/gui-settings.lua
@@ -34,32 +34,33 @@ Keymap({ "n", "x", "i" }, "<D-->", function() g.neovide_scale_factor = g.neovide
 g.neovide_confirm_quit = false
 g.neovide_hide_mouse_when_typing = true
 g.neovide_remember_window_size = false -- done via --geometry in `neovide` call
-g.neovide_scroll_animation_length = 0.1
 
 -- Keymaps
 g.neovide_input_use_logo = true -- logo = `cmd` (on macOS)
 g.neovide_input_macos_alt_is_meta = true -- makes `opt` usable (on macOS)
 Keymap("i", "<M-.>", "…") -- needed when alt is turned into meta key
-Keymap("i", "<M-->", "–")
+Keymap("i", "<M-->", "–") -- en-dash
 
 -- Window Appearance
+g.neovide_refresh_rate = 100
 g.neovide_underline_automatic_scaling = true -- slightly unstable according to docs
 -- INFO: Transparency set in theme-config.lua
 
--- cursor
+-- cursor & scroll
 g.neovide_cursor_animation_length = 0.003
-g.neovide_cursor_trail_size = 0.5
+g.neovide_cursor_trail_size = 0.7
 g.neovide_cursor_unfocused_outline_width = 0.1
+g.neovide_scroll_animation_length = 0.1
 
-g.neovide_cursor_vfx_mode = "" -- railgun|torpedo|pixiedust|sonicboom|ripple|wireframe
+-- cursor animation
+g.neovide_cursor_vfx_mode = "railgun" -- railgun|torpedo|pixiedust|sonicboom|ripple|wireframe
 
-local particleModes = { "railgun", "torpedo", "pixiedust" }
-if vim.tbl_contains(particleModes, g.neovide_cursor_vfx_mode) then
-	g.neovide_cursor_vfx_particle_lifetime = 0.4
+if vim.tbl_contains({ "railgun", "torpedo", "pixiedust" }, g.neovide_cursor_vfx_mode) then
+	g.neovide_cursor_vfx_particle_lifetime = 0.5
 	g.neovide_cursor_vfx_particle_density = 20.0
 	g.neovide_cursor_vfx_particle_speed = 40.0
 	if g.neovide_cursor_vfx_mode == "railgun" then
-		g.neovide_cursor_vfx_particle_phase = 1.3 -- only railgun
-		g.neovide_cursor_vfx_particle_curl = 1.3 -- only railgun
+		g.neovide_cursor_vfx_particle_phase = 1.3 
+		g.neovide_cursor_vfx_particle_curl = 1.3 
 	end
 end
