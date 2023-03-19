ff2cf2e47177c98b9383958783022d210790c21f
🤖 automated <cron@job>
2023-03-19 19:31:31 +0100

    iMac Home (1)

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -605,9 +605,11 @@ Autocmd("FileType", {
 
 --------------------------------------------------------------------------------
 
+Keymap({"n", "o", "x"}, "w", "E", { desc = "w -> E" })
+
 -- Simple version of the delaytrain.nvim
--- CamelCaseMotion for e, w, and b
-for _, key in ipairs { "x", "h", "l", "e", "w", "b" } do
+-- CamelCaseMotion for e, and b
+for _, key in ipairs { "x", "h", "l", "e", "b" } do
 	local timeout = 3000
 	local maxUsage = 10
 
