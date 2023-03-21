c317bcc6752ddb42a171be0c9209eebef3d324ac
🤖 automated <cron@job>
2023-03-21 17:24:30 +0100

    mini Office (6)

--- sketchybar/weather.sh
+++ sketchybar/weather.sh
@@ -8,4 +8,20 @@ else
 	icon=$(echo "$weather" | cut -c1)
 	temp=$(echo "$weather" | cut -c2-)
 fi
+
+# replace emoji weather fonts with nerd fonts
+case "$icon" in
+"✨") icon=" " ;;
+"☁️") icon=" " ;;
+"🌫") icon="敖" ;;
+"🌧") icon=" " ;;
+"❄️") icon=" " ;;
+"🌦") icon=" " ;;
+"🌨") icon=" " ;;
+"⛅️") icon=" " ;;
+"☀️") icon=" " ;;
+"🌩") icon="朗" ;;
+"⛈") icon=" " ;;
+esac
+
 sketchybar --set "$NAME" icon="$icon" label="$temp"
