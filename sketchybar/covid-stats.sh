87b2bd9d0ba64f45f8016921a5d07d80c464216b
🤖 automated <cron@job>
2023-03-21 17:24:30 +0100

    mini Office (6)

--- sketchybar/covid-stats.sh
+++ sketchybar/covid-stats.sh
@@ -3,4 +3,4 @@
 covid_numbers=$(curl -s "https://api.corona-zahlen.org/germany" | yq -r '.weekIncidence' | cut -d. -f1)
 [[ "$covid_numbers" == "null" ]] && covid_numbers="–"
 
-sketchybar --set "$NAME" label="${covid_numbers}" icon=" " icon.font=""
+sketchybar --set "$NAME" label="${covid_numbers}" icon="󱎷 "
