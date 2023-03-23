b84f70d807833a155248efe2a3ff20e1471ec2e7
🤖 automated <cron@job>
2023-03-23 21:32:02 +0100

    iMac Mother (9)

---
+++ vivaldi/bootstrap-vivaldi.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+open -a "Vivaldi" "vivaldi://sync"
+#───────────────────────────────────────────────────────────────────────────────
+open -a "Vivaldi" "vivaldi://experiments"
+# TODO custom css
+
+#───────────────────────────────────────────────────────────────────────────────
+
+killall "Vivaldi"
+while pgrep -q "Vivaldi" ; do sleep 0.1; done
+
+sed -i '' \
+	's/"directory_upgrade":true/"directory_upgrade":true,"extensions_to_open":"torrent:zip:alfredworkflow:ics"/' \
+	"$HOME/Library/Application Support/Vivaldi/Default/Preferences"
+
+open -a "Vivaldi" "vivaldi://settings/appearance/"
+echo -n "$HOME/.config/vivaldi" | pbcopy
+
+# TODO paste folder & restart Vivaldi again
+
+#───────────────────────────────────────────────────────────────────────────────
+# TODO import settings from these
+open -a "Vivaldi" "chrome-extension://pncfbmialoiaghdehhbnbhkkgmjanfhe/pages/options.html"
+open -a "Vivaldi" "chrome-extension://bgnkhhnnamicmpeenaelnjfhikgbkllg/pages/options.html"
+
