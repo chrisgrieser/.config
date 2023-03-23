33d3287127813db18723e4485bbec135bf632f3a
🤖 automated <cron@job>
2023-03-23 14:10:24 +0100

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/github-utilities/clone-my-repo.sh
+++ Alfred.alfredpreferences/workflows/github-utilities/clone-my-repo.sh
@@ -3,8 +3,10 @@
 # use SSH instead of https
 url="$(echo "$*" | sed -E 's/https?:\/\/github.com\//git@github.com:/').git"
 reponame=$(echo "$*" | sed -E 's/.*\///')
+
 # shellcheck disable=2154
 target="${local_repo_folder/#\~/$HOME}"
+[[ ! -e "$target" ]] && mkdir -p "$target"
 
 cd "$target" || exit 1
 git clone --depth=1 "$url" || echo "Error"
