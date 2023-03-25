2651cab6cfc599af97d774067ef9cdef814d1f97
🤖 automated <cron@job>
2023-03-25 20:25:06 +0100

    iMac Mother (3)

--- sketchybar/weather-emoji-to-nerdfont.js
+++ sketchybar/weather-emoji-to-nerdfont.js
@@ -2,15 +2,15 @@
 // helper, cause replacing emojis via `sed` does not seem to work
 function run(argv) {
 	return argv.join("")
-		// .replace("✨", " ")
-		// .replace("☁️", " ")
-		// .replace("🌫", "敖")
-		// .replace("🌧", " ")
-		// .replace("❄️", " ")
-		// .replace("🌦", " ")
-		// .replace("🌨", " ")
-		// .replace("⛅️", " ")
-		// .replace("☀️", " ")
-		// .replace("🌩", "朗")
-		// .replace("⛈", " ");
+		.replace("✨", "")
+		.replace("☁️", "")
+		.replace("🌫", "敖")
+		.replace("🌧", "")
+		.replace("❄️", "")
+		.replace("🌦", "")
+		.replace("🌨", "")
+		.replace("⛅️", "")
+		.replace("☀️", "")
+		.replace("🌩", "朗")
+		.replace("⛈", "");
 }
