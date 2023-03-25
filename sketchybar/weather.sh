d392400f9e194db4b691fff27d6626d972fb87d0
🤖 automated <cron@job>
2023-03-25 20:05:06 +0100

    iMac Mother (4)

--- sketchybar/weather.sh
+++ sketchybar/weather.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env zsh
 
-weather=$(curl "https://wttr.in/Berlin?format=1" | tr -d "C +" | sed 's/-0/0/')
+weather=$(curl "https://wttr.in/Berlin?format=1")
 if [[ "$weather" =~ Unknown ]] || [[ "$weather" =~ Sorry ]] || [[ -z "$weather" ]]; then
 	icon=""
 	temperature="–"
 else
-	icon=$(echo "$weather" | cut -c1)
-	temperature=$(echo "$weather" | cut -c2-)
+	icon=$(echo "$weather" | cut -d" " -f1)
+	temperature=$(echo "$weather" | cut -c2- | tr -d "+C ")
+	# helper to replace the weather icons with their nerdfont equivalents, since
+	# replacing them via `sed` does not seem to work
+	icon=$(osascript -l JavaScript ./weather-emoji-to-nerdfont.js "$icon" | sed 's/^\s*//' | sed 's/\s*$//')
 fi
 
 sketchybar --set "$NAME" icon="$icon" label="$temperature"
