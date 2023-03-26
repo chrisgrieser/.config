817cc82c4771d795fb16f24767875bd8a923ab9c
🤖 automated <cron@job>
2023-03-26 22:07:44 +0200

    iMac Home (8)

--- Alfred.alfredpreferences/workflows/sidenote-tweaks/openurl-copy-delete-note.js
+++ Alfred.alfredpreferences/workflows/sidenote-tweaks/openurl-copy-delete-note.js
@@ -8,25 +8,24 @@ function writeToFile(file, text) {
 	const str = $.NSString.alloc.initWithUTF8String(text);
 	str.writeToFileAtomicallyEncodingError(file, true, $.NSUTF8StringEncoding, null);
 }
-
 //──────────────────────────────────────────────────────────────────────────────
 
-// get first URL
-const sidenotes = Application("SideNotes");
-const currentNote = sidenotes.currentNote();
-const content = currentNote.text();
-const url = content.match(/https?:\/\/[^\s]+/)[0];
+function run(argv) {
+	const mode = argv[0];
 
-// close sidenotes
-Application("System Events").keystroke("w", { using: ["command down"] });
+	// get content
+	const sidenotes = Application("SideNotes");
+	const currentNote = sidenotes.currentNote();
+	const content = currentNote.text();
 
-// open URL
-app.openLocation(url);
+	if (mode.includes("openurl")) {
 
-// delete note when only URL or only second line is URL
-const noteHasOnlyUrl = content === url;
-const secondLineOnlyUrl = content.split("\n")[1] === url;
-if (noteHasOnlyUrl || secondLineOnlyUrl) {
+	}
+	const url = content.match(/https?:\/\/[^\s]+/)[0];
+
+	// close sidenotes
+	Application("System Events").keystroke("w", { using: ["command down"] });
+
 	// Trash Note, but keep copy in trash folder
 	const maxNameLen = 50;
 	let safeTitle = sidenotes
@@ -37,4 +36,7 @@ if (noteHasOnlyUrl || secondLineOnlyUrl) {
 	const trashNotePath = `${app.pathTo("home folder")}/.Trash/${safeTitle}.txt`;
 	writeToFile(trashNotePath, content);
 	sidenotes.currentNote().delete();
+
+	// (optional) copy to clipboard
+	if (mode === "copy-delete") app.setTheClipboardTo(content);
 }
