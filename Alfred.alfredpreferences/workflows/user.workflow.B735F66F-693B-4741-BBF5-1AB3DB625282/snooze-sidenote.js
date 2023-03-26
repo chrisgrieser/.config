75f41ea311972dd1f00f45d1fefe75bc091b94cd
🤖 automated <cron@job>
2023-03-26 14:46:28 +0200

    iMac Mother (3)

---
+++ Alfred.alfredpreferences/workflows/user.workflow.B735F66F-693B-4741-BBF5-1AB3DB625282/snooze-sidenote.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env osascript -l JavaScript
+
+function run() {
+	const sidenotes = Application("Sidenotes");
+	const reminders = Application("Reminders");
+
+	const content = sidenotes.currentNote().text();
+	sidenotes.currentNote().delete();
+	sidenotes.vi
+
+	reminders.defaultList().make({ new: "reminder", withProperties: { name: content } });
+	// alternative method
+	// const newReminder = reminders.Reminder({ name: "Title for reminder", body: "Notes for the reminder" });
+	// reminders.lists.byName("List Name").reminders.push(newReminder);
+
+	return content; // direct return for notification
+}
