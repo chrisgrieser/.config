ba7918333a44c51164efbcc9b04981ee6ff3f8da
🤖 automated <cron@job>
2023-03-26 15:46:29 +0200

    iMac Mother (1)

--- Alfred.alfredpreferences/workflows/user.workflow.B735F66F-693B-4741-BBF5-1AB3DB625282/push-todays-reminders-to-sidenotes.js
+++ Alfred.alfredpreferences/workflows/user.workflow.B735F66F-693B-4741-BBF5-1AB3DB625282/push-todays-reminders-to-sidenotes.js
@@ -1,30 +1,36 @@
 #!/usr/bin/env osascript -l JavaScript
-// https://leancrew.com/all-this/2017/08/my-jxa-problem/
-// https://developer.apple.com/library/archive/releasenotes/InterapplicationCommunication/RN-JavaScriptForAutomation/Articles/OSX10-10.html#//apple_ref/doc/uid/TP40014508-CH109-SW10
-//──────────────────────────────────────────────────────────────────────────────
 
 ObjC.import("stdlib");
 const sidenotes = Application("Sidenotes");
-const remin = Application("Reminders");
+const reminders = Application("Reminders");
 
 const today = new Date();
 const folder = sidenotes.folders.byName($.getenv("new_note_folder"));
 
 //──────────────────────────────────────────────────────────────────────────────
 
-const todaysTasks = remin.defaultList().reminders.whose({ dueDate: { _lessThan: today } });
-console.log("todaysTasks:", todaysTasks.length);
+// https://leancrew.com/all-this/2017/08/my-jxa-problem/
+// https://developer.apple.com/library/archive/releasenotes/InterapplicationCommunication/RN-JavaScriptForAutomation/Articles/OSX10-10.html#//apple_ref/doc/uid/TP40014508-CH109-SW10
+const todaysTasks = reminders.defaultList().reminders.whose({ dueDate: { _lessThan: today } });
+const tasksNum = todaysTasks.length; // outside, since .delete() during loop changes the number
 
-todaysTasks.forEach(task => {
+// needs iterating for loop since JXA Record Array cannot be looped with `foreach` or `for in`
+for (let i = tasksNum - 1; i < 0; i--) {
+	const task = todaysTasks[i];
+
 	let newNoteContent = task.name();
 	if (task.body()) newNoteContent += "\n" + task.body();
+
 	sidenotes.createNote({
 		folder: folder,
 		text: newNoteContent,
 		ispath: false,
 	});
-});
+
+	task.delete();
+}
 
 //──────────────────────────────────────────────────────────────────────────────
 
-// todaysTasks.delete();
+// reminders.includeStandardAdditions = true;
+// reminders.quit();
