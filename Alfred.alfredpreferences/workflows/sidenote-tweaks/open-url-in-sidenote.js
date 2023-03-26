338572428ffc87d08978e59dcd1b67e7e085e07d
🤖 automated <cron@job>
2023-03-26 21:07:43 +0200

    iMac Home (8)

--- Alfred.alfredpreferences/workflows/sidenote-tweaks/open-url-in-sidenote.js
+++ Alfred.alfredpreferences/workflows/sidenote-tweaks/open-url-in-sidenote.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env osascript -l JavaScript
 
+// get first URL
 const sidenote = Application("SideNotes");
-
 const currentNote = sidenote.currentNote()
-const url = currentNote.text().match(/https?:\/\/[^\s]+/)[0];
-
+const content = currentNote.text()
+const url = content.match(/https?:\/\/[^\s]+/)[0];
+const noteHasOnlyUrl = content === url
 //──────────────────────────────────────────────────────────────────────────────
 
 // close sidenotes
 Application("System Events").keystroke("w", {using: ["command down"]});
 
+// open URL
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 app.openLocation(url);
+
+//──────────────────────────────────────────────────────────────────────────────
+
+if (noteHasOnlyUrl) currentNote.delete()
