10684eb814b11e4c729b9c2dfdd89f049111007e
🤖 automated <cron@job>
2023-03-27 16:12:50 +0200

    iMac Home (4)

---
+++ Alfred.alfredpreferences/workflows/sidenote-tweaks/move-office-sidenotes-to-base.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+//──────────────────────────────────────────────────────────────────────────────
+
+const officenotes = Application("SideNotes").folders.byName("Office").notes
+for (let i = 0; i <  <  <  < .length; i++) {	const element = o.length; i++) {	const element =  < .length; i++) {	const element = of.length; i++) {	const element =  <  < .length; i++) {	const element = o.length; i++) {	const element =  < .length; i++) {	const element = off.length; i++) {
+	const element =  <  <  < .length; i++) {	const element = o.length; i++) {	const element =  < .length; i++) {	const element = of.length; i++) {	const element =  <  < .length; i++) {	const element = o.length; i++) {	const element =  < .length; i++) {	const element = off[i];
+	
+}
+officenotes.forEach(note => {
+	note.delete()
+})
