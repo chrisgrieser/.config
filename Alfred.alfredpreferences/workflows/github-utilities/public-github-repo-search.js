43127714821cdc17826f21888ab80fab59a00045
🤖 automated <cron@job>
2023-03-28 16:47:59 +0200

    iMac Home (10)

--- Alfred.alfredpreferences/workflows/github-utilities/public-github-repo-search.js
+++ Alfred.alfredpreferences/workflows/github-utilities/public-github-repo-search.js
@@ -19,18 +19,26 @@ function run(argv) {
 		.items.filter(repo => !repo.fork && !repo.archived)
 		.map(repo => {
 			const name = repo.full_name.split("/")[1];
-			const stars = repo.stargazers_count ? `★ ${repo.stargazers_count}   ` : "";
+
 			const daysAgo = Math.ceil((new Date() - new Date(repo.updated_at)) / 1000 / 3600 / 24);
 			let updated =
 				daysAgo < 31 ? daysAgo.toString() + " days ago" : Math.ceil(daysAgo / 30).toString() + " months ago";
-
 			if (updated.startsWith("1 ")) updated = updated.replace("s ago", " ago"); // remove plural "s"
+
+			let subtitle = `★ ${repo.stargazers_count} – ${updated} – ${repo.description}`;
+			if (repo.description) subtitle += " – " + repo.description;
 
 			return {
 				title: name,
-				subtitle: stars + repo.description,
+				subtitle: subtitle,
 				match: alfredMatcher(name),
 				arg: repo.html_url,
+				mods: {
+					shift: {
+						subtitle: `⇧: Search Issues (${repo.open_issues} open)`,
+						arg: repo.full_name,
+					},
+				},
 			};
 		});
 	return JSON.stringify({ items: jsonArray });
