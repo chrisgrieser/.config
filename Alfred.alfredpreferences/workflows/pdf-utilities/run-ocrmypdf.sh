2c6f919aae8f13a86bf65f4046df694ab777fa09
🤖 automated <cron@job>
2023-03-29 13:18:41 +0200

    iMac Home (15)

--- Alfred.alfredpreferences/workflows/pdf-utilities/run-ocrmypdf.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/run-ocrmypdf.sh
@@ -9,6 +9,6 @@ out_path="${path_no_ext}_ocr.pdf"
 ocrmypdf --language="$lang" "$input_path" "$out_path"
 
 #───────────────────────────────────────────────────────────────────────────────
-
+# success sound & reveal file
 open -R "$out_path"
 afplay "/System/Library/Sounds/Blow.aiff" &
