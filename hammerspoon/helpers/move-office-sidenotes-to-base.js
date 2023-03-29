87c01c1ccbc34f2483db485bdb6daaef535598de
🤖 automated <cron@job>
2023-03-29 15:00:50 +0200

    iMac Home (3)

--- hammerspoon/helpers/move-office-sidenotes-to-base.js
+++ hammerspoon/helpers/move-office-sidenotes-to-base.js
@@ -8,7 +8,7 @@ const sidenotes = Application("SideNotes");
 const destination = sidenotes.folders.byName(toFolder);
 const notesToMove = sidenotes.folders.byName(fromFolder).notes;
 
-// loop backwards since deleting from array
+// loop backwards since deleting from array, forEach not supported by JXA array
 // creating and deleting note since there does not seem to be a moving function
 for (let i = notesToMove.length - 1; i >= 0; i--) {
 	const note = notesToMove[i];
