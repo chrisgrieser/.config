397c10dab54c0c54e28178617420659f57cbd284
🤖 automated <cron@job>
2023-03-30 18:39:55 +0200

    iMac Home (1)

--- nvim/lua/funcs/auto-closer.lua
+++ nvim/lua/funcs/auto-closer.lua
@@ -3,16 +3,16 @@ local ignoredFiletypes, retirementAgeMins
 
 -- https://neovim.io/doc/user/builtin.html#getbufinfo() iterate all buffer and close outdated, non-special buffers
 local function checkOutdatedBuffer()
-	local openBuffers
-	vim.schedule_wrap( function() openBuffers = vim.fn.getbufinfo { buflisted = 1 } end)()
-
-	if openBuffers == nil then
-		print("offenbuffers is nil")
-		return
-	end
+	local openBuffers = vim.fn.getbufinfo { buflisted = 1 }
 	for _, buf in pairs(openBuffers) do
-		local recentlyUsed = (os.time() - buf.lastused) > retirementAgeMins * 60
-		if not recentlyUsed then vim.api.nvim_buf_delete(buf.bufnr, { force = false, unload = false }) end
+		local usedSecsGo = os.time() - buf.lastused
+		local recentlyUsed = usedSecsGo < retirementAgeMins * 60
+		local bufFt = vim.api.nvim_buf_get_option(buf.bufnr)
+		if not recentlyUsed then
+			local filename = buf.name:gsub(".*/", "")
+			print("Closing Buffer" .. filename)
+			vim.api.nvim_buf_delete(buf.bufnr, { force = false, unload = false })
+		end
 	end
 end
 
@@ -25,7 +25,7 @@ function M.setup(opts)
 	-- setup timer https://neovim.io/doc/user/luvref.html#uv.new_timer()
 	local timer = vim.loop.new_timer()
 	if not timer then return end
-	timer:start(0, 5000, checkOutdatedBuffer)
+	timer:start(0, 5000, vim.schedule_wrap(checkOutdatedBuffer))
 end
 
 return M
