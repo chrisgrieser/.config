297825e40fb15db71b0e982dfa37a490f580d1ac
🤖 automated <cron@job>
2023-03-30 20:59:55 +0200

    iMac Home (1)

--- nvim/after/ftplugin/http.lua
+++ nvim/after/ftplugin/http.lua
@@ -2,9 +2,8 @@ require("config.utils")
 --------------------------------------------------------------------------------
 
 Keymap("n", "<leader>r", "<Plug>RestNvim", { desc = "Run Request under cursor", buffer = true })
+Keymap("n", "<D-r>", "<Plug>RestNvimPreview", { desc = "Preview request curl", buffer = true })
 Keymap("n", "<leader>la", "<Plug>RestNvimLast", { desc = "re-run the last request", buffer = true })
-
---------------------------------------------------------------------------------
 
 -- INFO https://github.com/rest-nvim/rest.nvim
 -- see test files here https://github.com/rest-nvim/rest.nvim/tree/main/tests
