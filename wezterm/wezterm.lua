8b34027bdb45ac2040ef841e171450cd30ed85cf
🤖 automated <cron@job>
2023-03-30 21:59:55 +0200

    iMac Home (4)

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm") -- Pull in the wezterm API
+local config = {} -- This table will hold the configuration.
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then config = wezterm.config_builder() end
+--------------------------------------------------------------------------------
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "AdventureTime"
+
+--------------------------------------------------------------------------------
+
+return config
