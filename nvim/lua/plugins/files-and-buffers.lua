b42dc99ed04b0e580520132b825f11f683980f4d
🤖 automated <cron@job>
2023-03-30 20:19:55 +0200

    iMac Home (3)

--- nvim/lua/plugins/files-and-buffers.lua
+++ nvim/lua/plugins/files-and-buffers.lua
@@ -21,15 +21,47 @@ return {
 			},
 		},
 	},
+	{ -- Cycle Buffers
+		"ghillb/cybu.nvim",
+		event = "BufEnter", -- cannot load on <Plug> key for whatever reason
+		dependencies = { "nvim-tree/nvim-web-devicons", "nvim-lua/plenary.nvim" },
+		opts = {
+			display_time = 1000,
+			position = {
+				anchor = "bottomcenter",
+				max_win_height = 12,
+				vertical_offset = 3,
+			},
+			style = {
+				border = BorderStyle,
+				padding = 7,
+				path = "tail",
+				hide_buffer_id = true,
+				highlights = {
+					current_buffer = "CursorLine",
+					adjacent_buffers = "Normal",
+				},
+			},
+			behavior = {
+				mode = {
+					default = {
+						switch = "immediate",
+						view = "paging",
+					},
+				},
+			},
+		},
+	},
 	{ -- auto-close inactive buffers
 		"chrisgrieser/nvim-retire-early",
 		dev = true,
 		event = "VeryLazy", -- delay does not hurt really
 		opts = {
-			retirementAgeMins = 10,
-			ignoredFiletypes = { "lazy" },
+			retirementAgeMins = 20,
+			ignoredFiletypes = {},
 			notificationOnAutoClose = true,
-			neverCloseAltFile = true,
+			ignoreAltFile = true,
+			ignoreModifiedBufs = false,
 		},
 	},
 	{ -- change cwd per project
