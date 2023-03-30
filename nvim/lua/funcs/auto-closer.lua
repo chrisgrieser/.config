b336e30dc8640216081abed182ff1aff3b8d7f6e
🤖 automated <cron@job>
2023-03-30 17:58:35 +0200

    iMac Home (5)

--- nvim/lua/funcs/auto-closer.lua
+++ nvim/lua/funcs/auto-closer.lua
@@ -1,22 +1,29 @@
--- https://neovim.io/doc/user/luvref.html#uv.new_timer()
 local M = {}
---------------------------------------------------------------------------------
+local ignoredFiletypes, retirementAgeMins
 
--- iterate all buffer and close outdated, non-special buffers
--- https://neovim.io/doc/user/builtin.html#getbufinfo()
+-- https://neovim.io/doc/user/builtin.html#getbufinfo() iterate all buffer and close outdated, non-special buffers
 local function checkOutdatedBuffer()
-	
+	local openBuffers = vim.fn.getbufinfo { buflisted = 1 }
+	for _, buf in pairs(openBuffers) do
+		local bufFt = "?" -- TODO figure out how to get filetype from buffer
+		local ignoredFt = vim.tbl_contains(ignoredFiletypes, bufFt)
+		local recentlyUsed = (os.time() - buf.lastused) > retirementAgeMins * 60
+		if not ignoredFt and not recentlyUsed then
+			vim.cmd.bdelete(buf.bufnr)
+		end
+	end
 end
 
---------------------------------------------------------------------------------
-function M.setup()
-	local intervallMin = 
+---@param opts table
+function M.setup(opts)
+	if not opts then opts = {} end
+	retirementAgeMins = opts.retirementAgeMins or 30
+	ignoredFiletypes = opts.ignoredFiletypes or {}
+
 	-- setup timer https://neovim.io/doc/user/luvref.html#uv.new_timer()
 	local timer = vim.loop.new_timer()
 	if not timer then return end
-	timer:start(1, 1000, checkOutdatedBuffer)
+	timer:start(0,  1000, checkOutdatedBuffer)
 end
-
---------------------------------------------------------------------------------
 
 return M
