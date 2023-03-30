6dcd69f07651f3f9b4ad8f8c36fd576919510218
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-03-30 08:26:17 +0200

    chore

---
+++ nvim/lua/funcs/comment-divider.lua
@@ -0,0 +1,44 @@
+local M = {}
+
+function M.commentHr()
+	---@diagnostic disable: param-type-mismatch
+	local linechar = "─"
+	local wasOnBlank = vim.fn.getline(".") == ""
+	local indent = vim.fn.indent(".")
+	local textwidth = vim.bo.textwidth
+	local comStr = vim.bo.commentstring
+	local ft = vim.bo.filetype
+	local comStrLength = #(comStr:gsub(" ?%%s ?", ""))
+
+	if comStr == "" then
+		vim.notify(" No commentstring for this filetype available.", vim.log.levels.WARN)
+		return
+	end
+	if comStr:find("-") then linechar = "-" end
+
+	local linelength = textwidth - indent - comStrLength
+	local fullLine = string.rep(linechar, linelength)
+	local hr = comStr:gsub(" ?%%s ?", fullLine)
+	if ft == "markdown" then hr = "---" end
+
+	local linesToAppend = { "", hr, "" }
+	if wasOnBlank then linesToAppend = { hr, "" } end
+
+	vim.fn.append(".", linesToAppend)
+
+	-- shorten if it was on blank line, since fn.indent() does not return indent
+	-- line would have if it has content
+	if wasOnBlank then
+		vim.cmd.normal{"j==", bang = true}
+		local hrIndent = vim.fn.indent(".")
+		-- cannot use simply :sub, since it assumes one-byte-size chars
+		local hrLine = vim.fn.getline(".") ---@diagnostic disable-next-line: assign-type-mismatch, undefined-field
+		hrLine = hrLine:gsub(linechar, "", hrIndent)
+		vim.fn.setline(".", hrLine)
+	else
+		vim.cmd.normal{"jj==", bang = true}
+	end
+	---@diagnostic enable: param-type-mismatch
+end
+
+return M
