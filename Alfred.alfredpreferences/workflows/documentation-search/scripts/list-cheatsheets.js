1c55806393b68046aa6088e1a87d0273f8f25057
🤖 automated <cron@job>
2023-03-31 13:49:04 +0200

    iMac Home (8)

--- Alfred.alfredpreferences/workflows/documentation-search/scripts/list-cheatsheets.js
+++ Alfred.alfredpreferences/workflows/documentation-search/scripts/list-cheatsheets.js
@@ -5,10 +5,11 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 const alfredMatcher = (str) => str.replace (/[-()/_.:]/g, " ") + " " + str + " ";
 
-// fill in here
+//──────────────────────────────────────────────────────────────────────────────
+
 const jsonArray = app.doShellScript("curl 'https://cheat.sh/:list'")
 	.split("\r")
-	.filter(l => !l.endsWith(":list") && !l.endsWith("/") && !l.startsWith(":"))
+	.filter(line => !line.endsWith(":list") && !line.endsWith("/") && !line.startsWith(":"))
 	.concat([":intro", ":styles-demo", ":styles", ":vim", ":random"])
 	.map(item => {
 		return {
