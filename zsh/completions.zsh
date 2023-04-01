0e9198f777d2afb062fd33c326f5d433c89f3f2a
🤖 automated <cron@job>
2023-04-01 20:30:49 +0200

    iMac Home (2)

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -1,5 +1,5 @@
 # good guide: https://thevaluable.dev/zsh-completion-guide-examples/
-#-------------------------------------------------------------------------------
+#───────────────────────────────────────────────────────────────────────────────
 
 # case insensitive path-completion - https://scriptingosx.com/2019/07/moving-to-zsh-part-5-completions/
 zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 
@@ -11,23 +11,9 @@ zstyle ':completion:*' menu select
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*:*:-command-:*:*' group-order alias builtins functions commands
 
-# Use vim keys in tab complete menu
-zmodload zsh/complist
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -M menuselect 'J' vi-forward-blank-word # next group
-bindkey -M menuselect 'K' vi-backward-blank-word # previous group
-bindkey -M menuselect '^[[Z' accept-and-infer-next-history # shift+tab: for directories, query for children
-bindkey -M menuselect ' ' accept-line
-bindkey -M menuselect '\e' send-break
-bindkey -M menuselect '^Z' undo
-bindkey -M menuselect '^L' clear-screen
-
 # coloring & messages
 zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}[%d]%f'
-zstyle ':completion:*:messages' format '%F{yellow}-- %d --%f'
+zstyle ':completion:*:messages' format '%F{blue}-- %d --%f'
 zstyle ':completion:*:warnings' format '%F{yellow}-- No completions found.%f'
 # shellcheck disable=SC2086,SC2296
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
@@ -40,13 +26,13 @@ zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 
 
 
 # only require one <tab> completion (the docs do not describe the actual
-# behavior of the setting) https://zsh.sourceforge.io/Doc/Release/Options.html#Completion-4
+# behavior of the setting well) https://zsh.sourceforge.io/Doc/Release/Options.html#Completion-4
 setopt MENU_COMPLETE
 
-#-------------------------------------------------------------------------------
+#───────────────────────────────────────────────────────────────────────────────
 # CLI-SPECIFIC COMPLETIONS
 
-# Homebrew Completions have to be added in .zprofile
+# Homebrew Completions added in .zprofile
 # https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
 
 # Lazy loading to decrease loading time:
