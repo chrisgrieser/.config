3db4bd219fd8989c040d21623fe53ff50d9b3c7a
🤖 automated <cron@job>
2023-04-01 17:30:49 +0200

    iMac Home (2)

--- utility-scripts/fork-public-repo.sh
+++ utility-scripts/fork-public-repo.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
 # INPUT
-echo "Enter Repo (format: owner/name)"
-read -r REPO
+echo "Paste GitHub URL"
+read -r github_url
 
-echo "Enter tag for neovimcraft"
+echo "Enter tag for neovimcraft (Check tags at: https://neovimcraft.com/)"
 read -r tag
 
 TEMP_DIR="$WD"
@@ -19,14 +19,17 @@ function commit_push_pr_view() {
 	local msg="$1"
 	git add .
 	git commit -m "$msg"
-	git push
 	gh pr create --fill # --fill = use commit ino
-	gh pr view
+
+	# open in web just to check
+	origin=$(git remote -v | grep origin | head -n1 | cut -d: -f2 | cut -d. -f1)
+	gh repo set-default "$origin"
+	gh pr view --web
 }
 
+repo
 plugin_name=$(echo "$REPO" | cut -d/ -f2)
 github_user=$(echo "$REPO" | cut -d/ -f1)
-github_link="https://github.com/$REPO"
 
 # desc can be inferred from github description (not using jq for portability)
 plugin_desc=$(curl -sL "https://api.github.com/repos/$REPO" | grep "description" | head -n1 | cut -d'"' -f4)
@@ -38,8 +41,9 @@ gh repo fork "neurosnap/neovimcraft" --clone=false # separate clone command for 
 gh repo clone "$github_user/neovimcraft" -- --depth=1
 cd "neovimcraft"
 
-# TODO add to json manually (skip annoying deno installation) 
-# https://github.com/neurosnap/neovimcraft/pull/290/files
+# to avoid the installation of `deno` and the interactive `make resource`,
+# enter the new plugin data automatically
+target="data/manual.json"
 to_add="},
     {
       \"type\": \"github\",
@@ -48,9 +52,15 @@ to_add="},
       \"tags\": [
         \"plugin\",
         \"$tag\",
-      ]"
+      ]
+    }
+  ]
+}"
 
-
+sed -i '' "$ d" "$target"
+sed -i '' "$ d" "$target"
+sed -i '' "$ d" "$target"
+echo -n "$to_add" >>"$target"
 
 commit_push_pr_view "Add $plugin_name"
 
@@ -58,7 +68,7 @@ commit_push_pr_view "Add $plugin_name"
 # AWESOME NEOVIM
 
 cd "$TEMP_DIR"
-gh repo fork "rockerBOO/awesome-neovim" --clone=false 
+gh repo fork "rockerBOO/awesome-neovim" --clone=false
 gh repo clone "$plugin_name/awesome-neovim" -- --depth=1
 cd "awesome-neovim"
 
@@ -73,7 +83,7 @@ commit_push_pr_view "Add \`$REPO\`"
 
 cd "$TEMP_DIR"
 
-gh repo fork "phaazon/this-week-in-neovim-contents" --clone=false 
+gh repo fork "phaazon/this-week-in-neovim-contents" --clone=false
 gh repo clone "$plugin_name/this-week-in-neovim-contents" -- --depth=1
 # INFO needs to switch to most recent branch :S
 
@@ -91,4 +101,3 @@ echo "Done. Now Opening Reddit…"
 sleep 0.1
 echo "$plugin_desc" | pbcopy
 open "https://www.reddit.com/r/neovim/submit?title=Introducing%3A$plugin_name"
-
