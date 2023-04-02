f1b9c49cdf249deec9b1e8442d4fda0ac50c506a
🤖 automated <cron@job>
2023-04-02 13:12:21 +0200

    iMac Home (1)

--- Alfred.alfredpreferences/workflows/sidenote-tweaks/search-sidenotes.js
+++ Alfred.alfredpreferences/workflows/sidenote-tweaks/search-sidenotes.js
@@ -11,21 +11,22 @@ function alfredMatcher(str) {
 // HACK since notes are not directly accessible via their id, but only from
 // inside a folder: `Application("SideNotes").folders.byId("35BE5A12-DAF4-44FD-AF7D-2689CBB14BF3").notes.byId("0776263A-77FA-41EF-808E-6266C77DBDF9")`
 // `Application("SideNotes").currentNote()` retrieves a note that way. This
-// necessitates iterating folders *and* notes to retrieve them by ID. However, 
+// necessitates iterating folders *and* notes to retrieve them by ID. However,
 // note objects have more properties like textFormatting, the `.text()` method
 // includes information on whether the note has an image, and methods like
 // `.delete()` are available
-function getNoteObj(noteId) {
+function getFlatNoteArray() {
+	const allNotes = [];
 	const sidenotes = Application("SideNotes");
 	const folders = sidenotes.folders;
 	for (let i = 0; i < folders.length; i++) {
 		const notesInFolder = folders[i].notes;
 		for (let j = 0; j < notesInFolder.length; j++) {
 			const note = notesInFolder[j];
-			if (note.id() === noteId) return note;
+			allNotes.push(note)
 		}
 	}
-	return false;
+	return allNotes;
 }
 
 const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
@@ -34,31 +35,29 @@ const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]
 
 function run(argv) {
 	const query = argv[0] ? argv[0].trim() : "";
-	const ignoredTitle = $.getenv("ignored_title");
 
-	const sidenotes = Application("SideNotes");
-	const results = sidenotes
-		.searchNotes(query)
-		.filter(item => item.title !== ignoredTitle)
-		.map(item => {
-			const noteObj = getNoteObj(item.identifier);
-			if (!noteObj) return false;
-			const content = noteObj.text();
-			console.log("content:", content);
+	const results = getFlatNoteArray()
+		.filter(item => item.title !== $.getenv("ignored_title"))
+		.map(noteObj => {
+			const fullText = noteObj.text();
+			const id = noteObj.id();
+			const title = noteObj.title();
+			const body = noteObj.content();
+			const secondLine = body.split("\n")[0];
+
 			let icon = "";
-
 			let type = noteObj.textFormatting();
-			if (type === "markdown" && content.match(/\[[x ]\]/)) type = "tasklist";
+			if (type === "markdown" && fullText.match(/\[[x ]\]/)) type = "tasklist";
 			if (type === "code") icon += "👨‍💻";
 			if (type === "tasklist") icon += "☑️ ";
 			if (type === "plain") icon += "📃";
 
-			if (content.includes("[img ")) icon += "🖼️ ";
-			const urls = content.match(urlRegex);
+			if (fullText.includes("[img ")) icon += "🖼️ ";
+			const urls = fullText.match(urlRegex);
 			let urlSubtitle = "⌘: ";
 			if (urls) {
 				icon += "🔗";
-				const isLinkOnlyNote = [item.title, item.details].includes(urls[0]);
+				const isLinkOnlyNote = (title + secondLine).includes(urls[0]);
 				if (isLinkOnlyNote) urlSubtitle += "🗑🔗 Delete & Open ";
 				else urlSubtitle += "🔗 Open ";
 				urlSubtitle += urls[0];
@@ -68,13 +67,13 @@ function run(argv) {
 
 			if (icon !== "") icon += " "; // padding
 			return {
-				title: item.title,
-				subtitle: icon + item.details.slice(0, 100),
-				match: alfredMatcher(item.title + item.details),
-				arg: item.identifier,
-				uid: item.identifier,
+				title: title,
+				subtitle: icon + secondLine,
+				match: alfredMatcher(title) + " " + alfredMatcher(body),
+				arg: id,
+				uid: id,
 				mods: {
-					alt: { arg: content },
+					alt: { arg: fullText },
 					cmd: {
 						subtitle: urlSubtitle,
 						valid: Boolean(urls),
