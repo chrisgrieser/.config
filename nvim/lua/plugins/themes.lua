3f5ca5f9a70e203c2ba0dd8ba0bcd37ed6ed3ff3
🤖 automated <cron@job>
2023-04-03 11:43:34 +0200

    iMac Home (4)

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -7,11 +7,11 @@ local g = vim.g
 -- - rest ignored
 -- - if only one theme, it's used for both light and dark
 local themes = {
-	"EdenEast/nightfox.nvim",
+	-- "EdenEast/nightfox.nvim",
+	{ "rose-pine/neovim", name = "rose-pine" },
 	-- "sainnhe/everforest",
 	-- "rebelot/kanagawa.nvim",
 	"glepnir/zephyr-nvim",
-	{"rose-pine/neovim", name = "rose-pine"},
 	-- "folke/tokyonight.nvim",
 	-- "NTBBloodbath/sweetie.nvim",
 	-- "nyoom-engineering/oxocarbon.nvim",
@@ -23,20 +23,21 @@ g.lightTransparency = 0.92
 
 --------------------------------------------------------------------------------
 
--- The purpose of this is not having to manage two lists, once with themes 
+-- The purpose of this is not having to manage two lists, once with themes
 -- to install and one for determining light/dark theme
 
 -- the light/dark values are used in config/theme-config.lua for properly
 -- setting up the themes
 
 ---convert lazy.nvim-plugin-spec or github-repo into theme name
----@param repoName string|table either github-repo or plugin-spec with github-repo as first item
+---@param repo string|table either github-repo or plugin-spec with github-repo as first item
 ---@nodiscard
 ---@return string name of the color scheme
-local function getName(repoName)
-	if type(repoName) == "table" then repoName =repoName[1] end
-	repoName = repoName:gsub(".*/", ""):gsub("[.%-]?nvim", ""):gsub("neovim%-?", "")
-	return repoName
+local function getName(repo)
+	-- either first item, or name-key
+	if type(repo) == "table" then repo = repo.name or repo[1] end
+	local name = repo:gsub(".*/", ""):gsub("[.%-]?nvim", ""):gsub("neovim%-?", "")
+	return name
 end
 
 g.lightTheme = getName(themes[1])
--- nvim/templates/skeleton.js
+++ nvim/templates/skeleton.js
@@ -3,10 +3,4 @@ ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 
-function alfredMatcher(str) {
-	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
-	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
-	return [clean, camelCaseSeperated, str].join(" ") + " ";
-}
-
 //──────────────────────────────────────────────────────────────────────────────
