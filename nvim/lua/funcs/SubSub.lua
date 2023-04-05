50b2e6725e12550690e32ffca4ecb8607834224b
🤖 automated <cron@job>
2023-04-05 23:31:43 +0200

    iMac Home (1)

--- nvim/lua/funcs/SubSub.lua
+++ nvim/lua/funcs/SubSub.lua
@@ -1,9 +1,52 @@
 local M = {}
+local delimiter, regexFlavor
+--------------------------------------------------------------------------------
+
+-- TODO replace these for example with `node` for js like regex?
+
+---function performing the actual string substitution, using lua's string.gsub
+---@param inputLines string[]
+---@param toSearch string
+---@param toReplace string
+---@param numOfReplacement integer|nil nil will perform all replacements
+---@param language "lua" currently only lua
+---@nodiscard
+---@return string[] outputLines
+local function useGsub(inputLines, toSearch, toReplace, numOfReplacement, language)
+	local outputLines = {}
+	if language == "lua" then
+		local occurrences = numOfReplacement or nil
+		for _, line in pairs(inputLines) do
+			local newLine = line:gsub(toSearch, toReplace, occurrences)
+			table.insert(outputLines, newLine)
+		end
+	end
+	return outputLines
+end
+
+---function performing a search, using lua's string.find
+---@param str string
+---@param toSearch string
+---@param fromIdx integer perform find from this index
+---@param language "lua" currently only lua
+---@nodiscard
+---@return integer startPos of match, nil if no match
+---@return integer endPos of match, nil if no match
+local function useFind(str, toSearch, fromIdx, language)
+	local startPos, endPos
+	if language == "lua" then
+		startPos, endPos = str:find(toSearch, fromIdx)
+	end
+	return startPos, endPos
+end
+
+--------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 ---process the parameters given in the user command (ranges, args, etc.)
 ---@param opts table
 ---@param curBufNum integer
+---@param delimitingStr string character delimiting search from replacement value
 ---@nodiscard
 ---@return integer start line of range
 ---@return integer end line of range
@@ -11,9 +54,9 @@ local M = {}
 ---@return string term to search
 ---@return string|nil replacement
 ---@return boolean whether to search first or all occurrences in line
-local function processParameters(opts, curBufNum)
+local function processParameters(opts, curBufNum, delimitingStr)
 	-- "trimempty" allows to leave out the first and third "/" from regular `:s`
-	local input = vim.split(opts.args, "/", { trimempty = true, plain = false })
+	local input = vim.split(opts.args, delimitingStr, { trimempty = true, plain = false })
 	local toSearch, toReplace, flags = input[1], input[2], input[3]
 	local singleRepl = (flags and flags:find("g")) == nil
 
@@ -23,35 +66,6 @@ local function processParameters(opts, curBufNum)
 	return line1, line2, bufferLines, toSearch, toReplace, singleRepl
 end
 
----function performing the actual string substitution, using lua's string.gsub
----@param inputLines string[]
----@param toSearch string
----@param toReplace string
----@param numOfReplacement integer|nil nil will perform all replacements
----@nodiscard
----@return string[] outputLines
-local function useGsub(inputLines, toSearch, toReplace, numOfReplacement)
-	local outputLines = {}
-	local occurrences = numOfReplacement or nil
-	for _, line in pairs(inputLines) do
-		local newLine = line:gsub(toSearch, toReplace, occurrences)
-		table.insert(outputLines, newLine)
-	end
-	return outputLines
-end
-
----function performing a search, using lua's string.find
----@param str string
----@param toSearch string
----@param fromIdx integer perform find from this index
----@nodiscard
----@return integer|nil startPos of match, nil if no match
----@return integer|nil startPos of match, nil if no match
-local function useFind(str, toSearch, fromIdx)
-	local startPos, endPos = str:find(toSearch, fromIdx)
-	return startPos, endPos
-end
-
 --------------------------------------------------------------------------------
 
 ---more complicated than just running gsub on each line, since the shift in
@@ -59,13 +73,13 @@ end
 ---@param opts table
 ---@param ns integer namespace id to use for highlights
 ---@param curBufNum integer buffer id
-local function previewAndHlReplacements(opts, ns, curBufNum)
-	local line1, line2, bufferLines, toSearch, toReplace, singleRepl = processParameters(opts, curBufNum)
+local function previewAndHighlightReplacements(opts, ns, curBufNum, delimitingStr)
+	local line1, line2, bufferLines, toSearch, toReplace, singleRepl = processParameters(opts, curBufNum, delimitingStr)
 	if not toReplace then return end
 
 	-- preview changes
 	local numOfReplacement = singleRepl and 1 or nil
-	local newBufferLines = useGsub(bufferLines, toSearch, toReplace, numOfReplacement)
+	local newBufferLines = useGsub(bufferLines, toSearch, toReplace, numOfReplacement, regexFlavor)
 	vim.api.nvim_buf_set_lines(curBufNum, line1 - 1, line2, false, newBufferLines)
 
 	-- add highlights
@@ -74,16 +88,18 @@ local function previewAndHlReplacements(opts, ns, curBufNum)
 
 		-- find all startPositions in the line
 		local startPositions = {}
-		for col in line:gmatch("()" .. toSearch) do
-			table.insert(startPositions, col)
-			if singleRepl then break end
+		local start = 0
+		while true do
+			start, _ = useFind(line, toSearch, start + 1, regexFlavor)
+			if not start then break end
+			table.insert(startPositions, start)
 		end
 
 		-- iterate matches in given line
 		local previousShift = 0
 		for ii, startPos in ipairs(startPositions) do
-			local _, endPos = line:find(toSearch, startPos)
-			local lineWithSomeSubs = (useGsub({ line }, toSearch, toReplace, ii))[1]
+			local _, endPos = useFind(line, toSearch, startPos, regexFlavor)
+			local lineWithSomeSubs = (useGsub({ line }, toSearch, toReplace, ii, regexFlavor))[1]
 			local diff = (#lineWithSomeSubs - #line)
 			startPos = startPos + previousShift
 			endPos = endPos + diff -- shift of end position due to replacement
@@ -97,14 +113,13 @@ end
 ---@param opts table
 ---@param ns integer namespace id to use for highlights
 ---@param curBufNum integer buffer id
-local function highlightSearches(opts, ns, curBufNum)
-	local line1, _, bufferLines, toSearch, _, _ = processParameters(opts, curBufNum)
+local function highlightSearches(opts, ns, curBufNum, delimitingStr)
+	local line1, _, bufferLines, toSearch, _, _ = processParameters(opts, curBufNum, delimitingStr)
 	for i, line in ipairs(bufferLines) do
 		-- only highlighting first match, since the g-flag can only be entered
 		-- when there is a substitution value
-		local startPos, endPos = line:find(toSearch)
-		if startPos then
-			-- stylua: ignore
+		local startPos, endPos = useFind(line, toSearch, 1, regexFlavor)
+		if startPos and endPos then
 			vim.api.nvim_buf_add_highlight(0, ns, "Substitute", line1 + i - 2, startPos - 1, endPos)
 		end
 	end
@@ -114,9 +129,9 @@ end
 
 ---the substitution to perform when the commandline is confirmed with <CR>
 ---@param opts table
-local function confirmSubstitution(opts)
+local function confirmSubstitution(opts, delimitingStr)
 	local curBufNum = vim.api.nvim_get_current_buf()
-	local line1, line2, bufferLines, toSearch, toReplace, singleRepl = processParameters(opts, curBufNum)
+	local line1, line2, bufferLines, toSearch, toReplace, singleRepl = processParameters(opts, curBufNum, delimitingStr)
 
 	if not toReplace then
 		vim.notify("No replacement value given, cannot perform substitution.", vim.log.levels.ERROR)
@@ -124,7 +139,7 @@ local function confirmSubstitution(opts)
 	end
 
 	local numOfReplacement = singleRepl and 1 or nil
-	local newBufferLines = useGsub(bufferLines, toSearch, toReplace, numOfReplacement)
+	local newBufferLines = useGsub(bufferLines, toSearch, toReplace, numOfReplacement, regexFlavor)
 	vim.api.nvim_buf_set_lines(curBufNum, line1 - 1, line2, false, newBufferLines)
 end
 
@@ -147,14 +162,18 @@ local function previewSubstitution(opts, ns, preview_buf)
 	if not hasReplacementValue then
 		highlightSearches(opts, ns, curBufNum)
 	else
-		previewAndHlReplacements(opts, ns, curBufNum)
+		previewAndHighlightReplacements(opts, ns, curBufNum)
 	end
 
 	return 2 -- return the value of the preview type
 end
 
+--------------------------------------------------------------------------------
+
+---@class opts
+
 -- adds the usercommand
-function M.setup()
+function M.setup(opts)
 	local commands = { "S", "SubSub" }
 	for _, cmd in pairs(commands) do
 		vim.api.nvim_create_user_command(cmd, confirmSubstitution, {
