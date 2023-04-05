0620031a26b61a36dcb4c97fb4ccbde8d0fb21bc
🤖 automated <cron@job>
2023-04-05 16:31:43 +0200

    iMac Home (3)

---
+++ nvim/lua/funcs/SubSub.lua
@@ -0,0 +1,98 @@
+local M = {}
+--------------------------------------------------------------------------------
+
+-- iterate lines and replace
+---@param lines string[]
+---@param toSearch string
+---@param toReplace string
+---@return string[]
+local function substituteLines(lines, toSearch, toReplace)
+	local newBufferLines = {}
+	for _, line in pairs(lines) do
+		local newLine = line:gsub(toSearch, toReplace) -- TODO different substitution engine here
+		table.insert(newBufferLines, newLine)
+	end
+	return newBufferLines
+end
+
+local function processParameters(opts)
+	
+end
+
+---main function running the substitution
+---@param opts table
+local function executeSubstitution(opts)
+	-- "trimempty" allows to leave out the first and third "/" from regular `:s`
+	-- "plain" required since special characters should only be processed at substitution
+	local input = vim.split(opts.args, "/", { trimempty = true, plain = true })
+
+	local toSearch = input[1]
+	local toReplace = input[2]
+	-- local flags = input[3] -- TODO flags
+	if not toReplace then
+		vim.notify("No replacement value given, cannot perform substitution.", vim.log.levels.ERROR)
+		return
+	end
+	local line1 = opts.line1
+	local line2 = opts.line2
+	local bufferLines = vim.api.nvim_buf_get_lines(0, line1 - 1, line2, false)
+
+	local newBufferLines = substituteLines(bufferLines, toSearch, toReplace)
+	vim.api.nvim_buf_set_lines(0, line1 - 1, line2, false, newBufferLines)
+end
+
+-- https://neovim.io/doc/user/map.html#%3Acommand-preview
+---@param opts table
+---@param ns number namespace for the highlight
+---@param preview_buf boolean true if inccommand=split. (Not implemented yet.)
+---@return integer?
+local function previewSubstitution(opts, ns, preview_buf)
+	if preview_buf then
+		vim.notify_once(
+			"SubSub does not support 'inccommand=split' yet. Please use 'inccommand=unplit'.",
+			vim.log.levels.WARN
+		)
+		return
+	end
+	local input = vim.split(opts.args, "/", { trimempty = true, plain = true })
+	local line1 = opts.line1
+	local line2 = opts.line2
+	local bufferLines = vim.api.nvim_buf_get_lines(0, line1 - 1, line2, false)
+	local toSearch = input[1]
+	local toReplace = input[2]
+
+	-- live preview the changes
+	if toReplace then
+		local newBufferLines = substituteLines(bufferLines, toSearch, toReplace)
+		vim.api.nvim_buf_set_lines(0, line1 - 1, line2, false, newBufferLines)
+	end
+
+	-- Highlights
+	-- if no replacement entered yet: highlight search term
+	-- if replacement entered: highlight replacement
+	for i, line in ipairs(bufferLines) do
+		local startIdx, endIdx = line:find(toSearch)
+		if startIdx then
+			-- TODO make this work with dynamic length of replacement
+			if toReplace then endIdx = startIdx + #toReplace - 1 end
+			vim.api.nvim_buf_add_highlight(0, ns, "Substitute", line1 + i - 2, startIdx - 1, endIdx)
+		end
+	end
+
+	return 2 -- Return the value of the preview type
+end
+
+-- adds the usercommand as ":S" and ":Substitute"
+function M.setup()
+	local commands = { "S", "Substitute" }
+	for _, cmd in pairs(commands) do
+		vim.api.nvim_create_user_command(cmd, executeSubstitution, {
+			nargs = "?",
+			range = "%", -- defaults to whole buffer
+			addr = "lines",
+			preview = previewSubstitution,
+		})
+	end
+end
+
+return M
