123243d84c28cdc4855542a7273ef886d7262478
🤖 automated <cron@job>
2023-04-05 14:51:43 +0200

    iMac Home (1)

--- nvim/lua/funcs/no-vim-regex.lua
+++ nvim/lua/funcs/no-vim-regex.lua
@@ -1,55 +1,63 @@
 local M = {}
 --------------------------------------------------------------------------------
 
--- TODO preview https://neovim.io/doc/user/map.html#%3Acommand-preview
 -- TODO ranges
+-- TODO flags
 
----main function running the substitution
----@param opts table
-local function substitute(opts)
-	local input = vim.split(opts.args, "/", { trimempty = true, plain = true })
-	local toSearch = input[1]
-	local toReplace = input[2]
-	local flags = input[3] -- TODO implement flags
-	if flags == nil then flags = {} end
-	local bufferLines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
-	-- iterate lines and replace
+--------------------------------------------------------------------------------
+
+
+-- iterate lines and replace
+local function substitute(lines, toSearch, toReplace)
 	local newBufferLines = {}
-	for _, line in pairs(bufferLines) do
+	for _, line in pairs(lines) do
 		local newLine = line:gsub(toSearch, toReplace)
 		table.insert(newBufferLines, newLine)
 	end
+	return newBufferLines
+end
 
+---main function running the substitution
+---@param opts table
+local function executeSubstitution(opts)
+	local input = vim.split(opts.args, "/", { trimempty = true, plain = true })
+	local toSearch = input[1]
+	local toReplace = input[2]
+	local bufferLines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+	local newBufferLines = substitute(bufferLines, toSearch, toReplace)
 	vim.api.nvim_buf_set_lines(0, 0, -1, false, newBufferLines)
 end
 
-local function previewFunc(opts, preview_ns, preview_buf)
-	if preview_buf
+
+-- https://neovim.io/doc/user/map.html#%3Acommand-preview
+local function previewFunc(opts, ns, preview_buf)
+	if preview_buf then
+		vim.notify("vim.opt.inccommand = 'split' has not been implemented yet", vim.log.levels.WARN)
+		return
+	end
+	local input = vim.split(opts.args, "/", { trimempty = true, plain = true })
+	local toSearch = input[1]
+	local toReplace = input[2]
+
 	local line1 = opts.line1
 	local line2 = opts.line2
-	local lines = vim.api.nvim_buf_get_lines(0, line1 - 1, line2, false)
-	for i, line in ipairs(lines) do
-		local start_idx, end_idx = string.find(line, "%s+$")
+	local bufferLines = vim.api.nvim_buf_get_lines(0, line1 - 1, line2, false)
+
+	substitute(bufferLines, opts)
+
+	for i, line in ipairs(bufferLines) do
+		local start_idx, end_idx = string.find(line, toSearch)
 		if start_idx then
-			-- Highlight the match
-			vim.api.nvim_buf_add_highlight(
-				0,
-				preview_ns,
-				"Substitute",
-				line1 + i - 2,
-				start_idx - 1,
-				end_idx
-			)
+			vim.api.nvim_buf_add_highlight(0, ns, "Substitute", line1 + i - 2, start_idx - 1, end_idx)
 		end
 	end
-	-- Return the value of the preview type
-	return 2
+	return 2 -- Return the value of the preview type
 end
 
 function M.setup()
-	vim.api.nvim_create_user_command("S", substitute, {
-		nargs = 1,
+	vim.api.nvim_create_user_command("S", executeSubstitution, {
+		nargs = "?",
 		range = "%",
 		addr = "lines",
 		preview = previewFunc,
