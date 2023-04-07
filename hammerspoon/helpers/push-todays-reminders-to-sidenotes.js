01587396c0b90f3a58a0c9be56041b123331199b
🤖 automated <cron@job>
2023-04-07 22:18:04 +0200

    iMac Home (1)

--- hammerspoon/helpers/push-todays-reminders-to-sidenotes.js
+++ hammerspoon/helpers/push-todays-reminders-to-sidenotes.js
@@ -13,24 +13,28 @@ const folder = sidenotes.folders.byName("Base");
 // https://developer.apple.com/library/archive/releasenotes/InterapplicationCommunication/RN-JavaScriptForAutomation/Articles/OSX10-10.html#//apple_ref/doc/uid/TP40014508-CH109-SW10
 const todaysTasks = reminders.defaultList().reminders.whose({ dueDate: { _lessThan: today } });
 
-// - needs iterating for loop since JXA Record Array cannot be looped with `foreach` or `for in`
-// - backwards, to not change the indices at loop runtime
-for (let i = todaysTasks.length - 1; i >= 0; i--) {
-	const task = todaysTasks[i];
+if (todaysTasks.length > 0) {
+	// - needs iterating for loop since JXA Record Array cannot be looped with `foreach` or `for in`
+	// - backwards, to not change the indices at loop runtime
+	for (let i = todaysTasks.length - 1; i >= 0; i--) {
+		const task = todaysTasks[i];
+		let newNoteContent = task.name();
+		if (task.body()) newNoteContent += "\n" + task.body();
 
-	let newNoteContent = task.name();
-	if (task.body()) newNoteContent += "\n" + task.body();
+		sidenotes.createNote({
+			folder: folder,
+			text: newNoteContent,
+			ispath: false,
+		});
+		task.delete();
+	}
 
-	sidenotes.createNote({
-		folder: folder,
-		text: newNoteContent,
-		ispath: false,
-	});
+	//──────────────────────────────────────────────────────────────────────────────
 
-	task.delete();
+	reminders.includeStandardAdditions = true;
+	reminders.quit();
+
+	// close sidenotes again
+	delay (0.05); /* eslint-disable-line no-magic-numbers */
+	Application("System Events").keystroke("w", { using: ["command down"] });
 }
-
-//──────────────────────────────────────────────────────────────────────────────
-
-reminders.includeStandardAdditions = true;
-reminders.quit();
