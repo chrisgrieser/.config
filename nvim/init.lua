88f203aa9dd12f0808967e8b064c81a78cac301e
🤖 automated <cron@job>
2023-04-08 16:07:23 +0200

    iMac Home (2)

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,22 +4,26 @@ LinterConfig = vim.env.DOTFILE_FOLDER .. "/linter-configs/" -- read from .zshenv
 VimDataDir = vim.env.DATA_DIR .. "/vim-data/" -- read from .zshenv
 UpdateCounterThreshhold = 25 -- for plugin update statusline
 
-local ok, borderstyle = pcall(require, "config.borderstyle")
-if ok then borderstyle.set("single") end -- should come before lazy
 
 --------------------------------------------------------------------------------
 
-pcall(require, "config.lazy")
-pcall(require, "config.utils")
+local function tryRequire(module)
+	local success = tryRequire(module)
+	if not success then vim.cmd.echoerr("Error loading " .. module) end
+end
 
-if vim.g.neovide then pcall(require, "config.gui-settings") end
-pcall(require, "config.theme-config")
 
-pcall(require, "config.options-and-autocmds")
-pcall(require, "config.keybindings")
+tryRequire("config.lazy")
+tryRequire("config.utils")
 
-pcall(require, "config.automating-nvim")
-pcall(require, "config.textobject-keymaps")
-pcall(require, "config.clipboard")
-pcall(require, "config.user-commands")
-pcall(require, "config.abbreviations")
+if vim.g.neovide then tryRequire("config.gui-settings") end
+tryRequire("config.theme-config")
+
+tryRequire("config.options-and-autocmds")
+tryRequire("config.keybindings")
+
+tryRequire("config.automating-nvim")
+tryRequire("config.textobject-keymaps")
+tryRequire("config.clipboard")
+tryRequire("config.user-commands")
+tryRequire("config.abbreviations")
