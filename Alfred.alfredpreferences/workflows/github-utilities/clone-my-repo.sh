f143ead0c1f8bd2cfe2ab86492d4f1f82beea44e
🤖 automated <cron@job>
2023-04-09 11:55:51 +0200

    iMac Home (1)

--- Alfred.alfredpreferences/workflows/github-utilities/clone-my-repo.sh
+++ Alfred.alfredpreferences/workflows/github-utilities/clone-my-repo.sh
@@ -7,13 +7,14 @@ reponame=$(echo "$*" | sed -E 's/.*\///')
 # shellcheck disable=2154
 target="${local_repo_folder/#\~/$HOME}"
 [[ ! -e "$target" ]] && mkdir -p "$target"
-repo_abs_path="$target/$reponame"
 
-
-echo -n "$repo_abs_path"
 cd "$target" || exit 1
-git clone --depth=1 "$url" || return 1
+if [[ -f "$reponame" ]] ; then
+	osascript -e 'display notification "" with title "⚠️ Repo already exists."'
+else
+	git clone --depth=1 "$url" || return 1
+fi
 
 # open in terminal via Alfred
-echo -n "$repo_abs_path"
+echo -n "$target/$reponame"
 
