5b726857555cb65713295f854be0f307d14b2d41
🤖 automated <cron@job>
2023-04-13 17:07:43 +0200

    iMac Home (36)

--- nvim/after/ftplugin/bib.lua
+++ nvim/after/ftplugin/bib.lua
@@ -5,9 +5,5 @@ local fn = vim.fn
 -- do not autowrap
 bo.formatoptions = bo.formatoptions:gsub("t", "")
 
--- when opening large files, start with some folds closed
-if fn.line("$") > 100 then
-	vim.defer_fn(function ()
-		require("ufo").closeFoldsWith(0) -- = fold level zero
-	end, 1)
-end
+-- when opening large files, start toplevel folds closed
+if fn.line("$") > 100 then vim.defer_fn(function() vim.cmd("%foldclose") end, 1) end
