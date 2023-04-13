48eeba8408554086eca1e17d3c8e5d0870fce49a
🤖 automated <cron@job>
2023-04-13 18:48:26 +0200

    iMac Home (4)

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -110,8 +110,8 @@ keymap("x", "-", "zn<Esc>/\\%V", { desc = "Search within selection" })
 keymap("n", "+", "*", { desc = "Search word under cursor" })
 keymap("x", "+", [["zy/\V<C-R>=getreg("@z")<CR><CR>]], { desc = "Visual star" })
 
-keymap("c", "<C-n>", "<C-g>", {desc = "Next Match (when inc. search)"})
-keymap("c", "<C-S-n>", "<C-t>", {desc = "Next Match (when inc. search)"})
+keymap("c", "<C-n>", "<C-g>", { desc = "Next Match (when inc. search)" })
+keymap("c", "<C-S-n>", "<C-t>", { desc = "Next Match (when inc. search)" })
 
 -- while searching: enable hlsearch -> https://www.reddit.com/r/neovim/comments/zc720y/comment/iyvcdf0/?context=3
 vim.on_key(function(char)
