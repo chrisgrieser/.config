bf2aab3a1d6b1e53eed31d024aade5a35b753110
🤖 automated <cron@job>
2023-04-14 12:48:25 +0200

    iMac Home (2)

--- nvim/lua/config/folding.lua
+++ nvim/lua/config/folding.lua
@@ -9,13 +9,17 @@ local u = require("config.utils")
 vim.opt.foldopen:remove { "search" } -- no auto-open when searching
 keymap("n", "-", "zn/", { desc = "/ & Pause Folds" })
 
+
 -- while searching: pause folds -> https://www.reddit.com/r/neovim/comments/zc720y/comment/iyvcdf0/?context=3
 vim.on_key(function(char)
 	local searchKeys = { "n", "N", "*", "#", "/", "?" }
 	local searchConfirmed = (fn.keytrans(char):upper() == "<CR>" and fn.getcmdtype():find("[/?]"))
 	if not (searchConfirmed or fn.mode() == "n") then return end
 	local searchKeyUsed = searchConfirmed or (vim.tbl_contains(searchKeys, fn.keytrans(char)))
-	if vim.opt.foldenable:get() == searchKeyUsed then vim.opt.foldenable = not searchKeyUsed end
+	local notInSync = vim.opt.foldenable:get() == searchKeyUsed
+	if notInSync then
+		vim.opt.foldenable = not searchKeyUsed
+	end
 end, vim.api.nvim_create_namespace("auto_pause_folds"))
 
 --------------------------------------------------------------------------------
