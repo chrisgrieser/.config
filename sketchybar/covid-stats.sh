aca6fb4a7e9a63a03a421b0cc6ac697fa2f8c03d
🤖 automated <cron@job>
2023-04-19 10:59:21 +0200

    iMac Home (2)

--- sketchybar/covid-stats.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-covid_numbers=$(curl -s "https://api.corona-zahlen.org/germany" | yq -r '.weekIncidence' | cut -d. -f1)
-[[ "$covid_numbers" == "null" ]] && covid_numbers="–"
-
-sketchybar --set "$NAME" label="${covid_numbers}" icon="󱎷 "
