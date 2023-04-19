22e2347325de10de56ea5d1f40c9fde9b9132e88
🤖 automated <cron@job>
2023-04-19 16:32:20 +0200

    iMac Home (2)

--- zsh/general_and_plugin_configs.zsh
+++ zsh/general_and_plugin_configs.zsh
@@ -1,5 +1,8 @@
 # shellcheck disable=SC2190
 
+#───────────────────────────────────────────────────────────────────────────────
+# GENERAL SETTINGS
+
 # sets English everywhere, so that programs behave predictably
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
@@ -10,10 +13,15 @@ setopt AUTO_CD              # pure directory = cd into it
 setopt INTERACTIVE_COMMENTS # comments in interactive mode (useful for copypasting)
 
 #───────────────────────────────────────────────────────────────────────────────
+# CLI SETTINGS
 
-# you should use
+# you-should-use
 export YSU_IGNORED_ALIASES=("bi" "bu") # due to homebrew Alfred workflow
 export YSU_MESSAGE_POSITION="after"
+
+# fd
+# HACK fixes for `fd` colors when using a light terminal bg https://github.com/sharkdp/fd/issues/1031#issuecomment-1325716744
+export LS_COLORS=''
 
 # fzf
 export FZF_DEFAULT_COMMAND='fd --hidden'
@@ -28,6 +36,7 @@ export _ZO_DATA_DIR="$DATA_DIR/zoxide/"
 eval "$(zoxide init --no-cmd zsh)" # needs to be placed after compinit
 
 #───────────────────────────────────────────────────────────────────────────────
+# ZSH PLUGIN SETTINGS
 
 # zsh syntax highlighting
 export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets regexp root)
