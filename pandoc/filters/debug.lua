a322c847bda1a55f18e0dde7a7110a92dc2efa5f
🤖 automated <cron@job>
2023-04-24 19:28:08 +0200

    iMac Home (2)

--- pandoc/filters/debug.lua
+++ pandoc/filters/debug.lua
@@ -1,9 +1,9 @@
-
 -- run in the terminal:
 -- touch t.md && pandoc -L debug.lua t.md && rm t.md
 
-function Pandoc(aaa)
-	local info = pandoc.cli.default_options.standalone
-	print(aaa[1])
+function Pandoc()
+	local opts = pandoc.cli.parse_options(arg)
+
+	print(opts)
 	os.exit(0)
 end
