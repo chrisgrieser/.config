02e8a793aae28601ab4e05b2f98bf9a80e0422be
🤖 automated <cron@job>
2023-04-25 18:26:47 +0200

    mini Office (5)

--- nvim/after/ftplugin/bib.lua
+++ nvim/after/ftplugin/bib.lua
@@ -5,5 +5,9 @@ local fn = vim.fn
 -- do not autowrap
 bo.formatoptions = bo.formatoptions:gsub("t", "")
 
+-- for some reason, bib files have no comment string defined, even though they
+-- do have comments?
+bo.commentstring = "% %s"
+
 -- when opening large files, start toplevel folds closed
 if fn.line("$") > 100 then vim.defer_fn(function() vim.cmd("%foldclose") end, 1) end
