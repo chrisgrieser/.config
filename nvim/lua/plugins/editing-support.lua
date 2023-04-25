a851168a8061af3e54d264c82f71ace137b84ab9
🤖 automated <cron@job>
2023-04-25 16:46:47 +0200

    mini Office (3)

--- nvim/lua/plugins/editing-support.lua
+++ nvim/lua/plugins/editing-support.lua
@@ -12,6 +12,7 @@ return {
 
 			require("nvim-autopairs").add_rules {
 				rule("<", ">", "lua"):with_pair(isNodeType("string")), -- useful for keymaps
+				rule("<", ">", "vim"):with_pair(), -- useful for keymaps
 				rule('\\"', '\\"', "json"):with_pair(), -- escaped double quotes
 				rule("*", "*", "markdown"):with_pair(), -- italics
 				rule("__", "__", "markdown"):with_pair(), -- bold
