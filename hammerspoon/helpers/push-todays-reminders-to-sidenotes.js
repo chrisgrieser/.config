93d342b02756bc08c7a9719a100ae21c29043e6d
🤖 automated <cron@job>
2023-04-26 11:29:23 +0200

    mini Office (1)

--- hammerspoon/helpers/push-todays-reminders-to-sidenotes.js
+++ hammerspoon/helpers/push-todays-reminders-to-sidenotes.js
@@ -8,6 +8,7 @@ function run() {
 
 	const today = new Date();
 	const folder = sidenotes.folders.byName("Base");
+	const delayMs = 0.05;
 
 	// https://leancrew.com/all-this/2017/08/my-jxa-problem/
 	// https://developer.apple.com/library/archive/releasenotes/InterapplicationCommunication/RN-JavaScriptForAutomation/Articles/OSX10-10.html#//apple_ref/doc/uid/TP40014508-CH109-SW10
@@ -15,7 +16,7 @@ function run() {
 
 	if (todaysTasks.length === 0) {
 		reminders.quit();
-		return
+		return;
 	}
 
 	// - needs iterating for loop since JXA Record Array cannot be looped with `foreach` or `for in`
@@ -33,9 +34,10 @@ function run() {
 		task.delete();
 	}
 
+	delay(delayMs);
 	reminders.quit();
 
 	// close sidenotes again
-	delay(0.05); /* eslint-disable-line no-magic-numbers */
+	delay(delayMs);
 	Application("System Events").keystroke("w", { using: ["command down"] });
 }
