307b29cf0ebba62f23f43305fb27ca6c209a8c0a
🤖 automated <cron@job>
2023-04-26 13:26:46 +0200

    mini Office (1)

--- hammerspoon/lua/visuals.lua
+++ hammerspoon/lua/visuals.lua
@@ -1,8 +1,8 @@
 local M = {}
 
+local env = require("lua.environment-vars")
 local u = require("lua.utils")
 local wu = require("lua.window-utils")
-local env = require("lua.environment-vars")
 --------------------------------------------------------------------------------
 
 -- https://www.hammerspoon.org/Spoons/RoundedCorners.html
@@ -15,9 +15,13 @@ if roundedCorners then roundedCorners:start() end
 function M.holeCover(arg)
 	if env.isProjector() then return end
 
-	if Cover1 then Cover1 = nil end
-	if Cover2 then Cover2 = nil end
-	if Cover3 then Cover3 = nil end
+	if CoverParts then
+		---@diagnostic disable-next-line: unused-local
+		for _, cover in pairs(CoverParts) do
+			cover = nil ---@diagnostic disable-line: unused-local
+		end
+		CoverParts = nil
+	end
 	if arg == "remove" then return end
 
 	local bgColor
@@ -28,31 +32,22 @@ function M.holeCover(arg)
 		bgColor = { red = 0.8, green = 0.8, blue = 0.8, alpha = 1 }
 	end
 
-local pseudoMaxCorner = wu.toTheSide.w + wu.toTheSide.x
+	local pseudoMaxCorner = wu.toTheSide.w + wu.toTheSide.x
 
 	-- three points, forming roughly a triangle
-	Cover1 = hs.drawing.rectangle { x = pseudoMaxCorner - 9, y = screen.h - 3, w = 18, h = 3 }
-	Cover2 = hs.drawing.rectangle { x = pseudoMaxCorner - 6, y = screen.h - 6, w = 12, h = 3 }
-	Cover3 = hs.drawing.rectangle { x = pseudoMaxCorner - 3, y = screen.h - 9, w = 6, h = 3 }
+	CoverParts = {}
+	CoverParts[1] = hs.drawing.rectangle { x = pseudoMaxCorner - 9, y = screen.h - 3, w = 18, h = 3 }
+	CoverParts[2] = hs.drawing.rectangle { x = pseudoMaxCorner - 6, y = screen.h - 6, w = 12, h = 3 }
+	CoverParts[3] = hs.drawing.rectangle { x = pseudoMaxCorner - 3, y = screen.h - 9, w = 6, h = 3 }
 
 	-- for some reason, these cannot be put into a function :/
-	Cover1:setFillColor(bgColor)
-	Cover1:sendToBack()
-	Cover1:setFill(true)
-	Cover1:setStrokeColor(bgColor)
-	Cover1:show()
-
-	Cover2:setFillColor(bgColor)
-	Cover2:sendToBack()
-	Cover2:setFill(true)
-	Cover2:setStrokeColor(bgColor)
-	Cover2:show()
-
-	Cover3:setFillColor(bgColor)
-	Cover3:sendToBack()
-	Cover3:setFill(true)
-	Cover3:setStrokeColor(bgColor)
-	Cover3:show()
+	for _, cover in pairs(CoverParts) do
+		cover:setFillColor(bgColor)
+		cover:sendToBack()
+		cover:setFill(true)
+		cover:setStrokeColor(bgColor)
+		cover:show()
+	end
 end
 
 --------------------------------------------------------------------------------
