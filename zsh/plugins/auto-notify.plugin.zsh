da454c99bfcee8e1f0e2af2316161f20cdbd1a9b
🤖 automated <cron@job>
2023-04-29 11:55:47 +0200

    iMac Home (4)

--- zsh/plugins/auto-notify.plugin.zsh
+++ zsh/plugins/auto-notify.plugin.zsh
@@ -42,12 +42,15 @@ function _auto_notify_message() {
     # Run using echo -e in order to make sure notify-send picks up new line
     local DEFAULT_TITLE="\"%command\" Completed"
     local DEFAULT_BODY="$(echo -e "Total time: %elapsed seconds\nExit code: %exit_code")"
+    local DEFAULT_SOUND="" # default = no sound
 
     local title="${AUTO_NOTIFY_TITLE:-$DEFAULT_TITLE}"
     local text="${AUTO_NOTIFY_BODY:-$DEFAULT_BODY}"
+    local sound="${AUTO_NOTIFY_SOUND:-$DEFAULT_SOUND}"
 
     title="$(_auto_notify_format "$title" "$command" "$elapsed" "$exit_code")"
     body="$(_auto_notify_format "$text" "$command" "$elapsed" "$exit_code")"
+    [[ -n "$sound" ]] && sound="sound name \"$sound\""
 
     if [[ "$platform" == "Linux" ]]; then
         local urgency="normal"
@@ -58,11 +61,7 @@ function _auto_notify_message() {
         fi
         notify-send "$title" "$body" --app-name=zsh "$transient" "--urgency=$urgency" "--expire-time=$AUTO_NOTIFY_EXPIRE_TIME"
     elif [[ "$platform" == "Darwin" ]]; then
-        osascript \
-          -e 'on run argv' \
-          -e 'display notification (item 1 of argv) with title (item 2 of argv)' \
-          -e 'end run' \
-          "$body" "$title"
+        osascript -e "display notification \"$body\" with title \"$title\""
     else
         printf "Unknown platform for sending notifications: $platform\n"
         printf "Please post an issue on gitub.com/MichaelAquilina/zsh-auto-notify/issues/\n"
