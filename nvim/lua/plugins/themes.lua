daf01afee43b5616a0efe9bddab52fa08db7fad9
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-05-09 17:02:35 +0200

    chore

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -7,15 +7,14 @@ local g = vim.g
 -- - rest ignored
 -- - if only one theme, it's used for both light and dark
 local themes = {
-	-- "EdenEast/nightfox.nvim",
-	{ "rose-pine/neovim", name = "rose-pine" },
+	"EdenEast/nightfox.nvim",
+	-- { "rose-pine/neovim", name = "rose-pine" },
 	-- "tanvirtin/monokai.nvim",
 	-- "rebelot/kanagawa.nvim",
 	-- "kvrohit/mellow.nvim",
 	"glepnir/zephyr-nvim",
 	-- "sainnhe/everforest",
 	-- "folke/tokyonight.nvim",
-	-- "NTBBloodbath/sweetie.nvim",
 	-- "nyoom-engineering/oxocarbon.nvim",
 	-- "savq/melange",
 }
