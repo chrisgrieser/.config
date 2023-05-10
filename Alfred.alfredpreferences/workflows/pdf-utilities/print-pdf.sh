91abf570a301a4c86fbc9b319a7ad58c595333af
🤖 automated <cron@job>
2023-05-10 12:12:39 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/pdf-utilities/print-pdf.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/print-pdf.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env zsh
-lpr "$*"
+
+INPUT="$*"
+ext=${INPUT##*.}
+if [[ "$ext" != "pdf" ]]; then
+	echo "⚠️ Selection not a PDF file"	
+	return 1
+fi
+
+# send to printer
+lpr "$INPUT"
+
+# open printers to see progress
 find "$HOME/Library/Printers" -name "*.app" -exec open "{}" \;
