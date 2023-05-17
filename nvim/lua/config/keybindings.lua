d7b43916d1600fe6877045e9d991e7d94e23ad02
🤖 automated <cron@job>
2023-05-17 20:37:45 +0200

    iMac Home (2)

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -74,8 +74,6 @@ end, { desc = "Clear Notifications" })
 
 --------------------------------------------------------------------------------
 -- MOTIONS
-keymap({ "n", "o", "x" }, "e", '<cmd>lua require("spider").motion("e")<CR>', { desc = "󱇪 e" })
-keymap({ "n", "o", "x" }, "b", '<cmd>lua require("spider").motion("b")<CR>', { desc = "󱇪 b" })
 
 -- HJKL behaves like hjkl, but bigger distance (best used with scroll offset)
 keymap({ "o", "x" }, "H", "^")
--- nvim/lua/plugins/motions-and-textobjects.lua
+++ nvim/lua/plugins/motions-and-textobjects.lua
@@ -12,7 +12,52 @@ return {
 	{ -- CamelCase Motion plus
 		"chrisgrieser/nvim-spider",
 		dev = true,
+		lazy = true,
+		init = function()
+			-- stylua: ignore start
+			vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "󱇫 e" })
+			vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "󱇫 b" })
+			-- stylua: ignore end
+		end,
+		opts = {
+			skipInsignificantPunctuation = true,
+		},
 	},
+
+{
+	"chrisgrieser/nvim-spider",
+	dev = true,
+	init = function()
+		vim.keymap.set(
+			{ "n", "o", "x" },
+			"w",
+			"<cmd>lua require('spider').motion('w')<CR>",
+			{ desc = "Spider-w" }
+		)
+		vim.keymap.set(
+			{ "n", "o", "x" },
+			"e",
+			"<cmd>lua require('spider').motion('e')<CR>",
+			{ desc = "Spider-e" }
+		)
+		vim.keymap.set(
+			{ "n", "o", "x" },
+			"b",
+			"<cmd>lua require('spider').motion('b')<CR>",
+			{ desc = "Spider-b" }
+		)
+		vim.keymap.set(
+			{ "n", "o", "x" },
+			"ge",
+			"<cmd>lua require('spider').motion('ge')<CR>",
+			{ desc = "Spider-ge" }
+		)
+	end,
+	opts = {
+		skipInsignificantPunctuation = true,
+	},
+},
+
 
 	-----------------------------------------------------------------------------
 
