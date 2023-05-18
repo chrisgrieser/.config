ad64cb40608b878ae1491a055a62e98b8bf19d4b
🤖 automated <cron@job>
2023-05-18 19:08:26 +0200

    iMac Home (5)

--- nvim/lua/plugins/motions-and-textobjects.lua
+++ nvim/lua/plugins/motions-and-textobjects.lua
@@ -24,41 +24,6 @@ return {
 		},
 	},
 
-{
-	"chrisgrieser/nvim-spider",
-	dev = true,
-	init = function()
-		vim.keymap.set(
-			{ "n", "o", "x" },
-			"w",
-			"<cmd>lua require('spider').motion('w')<CR>",
-			{ desc = "Spider-w" }
-		)
-		vim.keymap.set(
-			{ "n", "o", "x" },
-			"e",
-			"<cmd>lua require('spider').motion('e')<CR>",
-			{ desc = "Spider-e" }
-		)
-		vim.keymap.set(
-			{ "n", "o", "x" },
-			"b",
-			"<cmd>lua require('spider').motion('b')<CR>",
-			{ desc = "Spider-b" }
-		)
-		vim.keymap.set(
-			{ "n", "o", "x" },
-			"ge",
-			"<cmd>lua require('spider').motion('ge')<CR>",
-			{ desc = "Spider-ge" }
-		)
-	end,
-	opts = {
-		skipInsignificantPunctuation = true,
-	},
-},
-
-
 	-----------------------------------------------------------------------------
 
 	{ -- tons of text objects
