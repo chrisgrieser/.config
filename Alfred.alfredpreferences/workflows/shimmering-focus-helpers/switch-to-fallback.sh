bedd1894762fda8789e1d5ff8df1867d12fa4a50
🤖 automated <cron@job>
2023-05-23 17:47:49 +0200

    mini Office (4)

--- Alfred.alfredpreferences/workflows/shimmering-focus-helpers/switch-to-fallback.sh
+++ Alfred.alfredpreferences/workflows/shimmering-focus-helpers/switch-to-fallback.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # switch to the FALLBACK
-theme_folder="$VAULT_PATH/.obsidian/themes/Shimmering Focus/fallback.css"
+theme_folder="$VAULT_PATH/.obsidian/themes/Shimmering Focus/"
 ln -sf "$theme_folder/fallback.css" "$theme_folder/theme.css"
 
 # delete local folder
--- Alfred.alfredpreferences/workflows/shimmering-focus-helpers/switch-to-local-repo.sh
+++ Alfred.alfredpreferences/workflows/shimmering-focus-helpers/switch-to-local-repo.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env zsh
 # shellcheck disable=2154,2164
 
+theme_folder="$VAULT_PATH/.obsidian/themes/Shimmering Focus/"
 remote_ssh="git@github.com:chrisgrieser/shimmering-focus.git"
 target_path="${local_repo_folder/#\~/$HOME}"
 
+#───────────────────────────────────────────────────────────────────────────────
+
 [[ ! -d "$target_path" ]] && mkdir -p "$target_path"
 cd "$target_path"
+git clone --depth=1 "$remote_ssh"
+npm i # install dependencies (clean-css-cli)
 
-git clone --depth=1 "$remote_ssh" || return 1
+ln -sf "$local_repo_folder/shimmering-focus/source.css" "$theme_folder/theme.css"
