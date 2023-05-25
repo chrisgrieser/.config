962a4683c0892a75ea4e2ca24ea9370c3bda3762
🤖 automated <cron@job>
2023-05-25 17:34:07 +0200

    mini Office (1)

--- Alfred.alfredpreferences/workflows/shimmering-focus-helpers/switch-to-local-repo.sh
+++ Alfred.alfredpreferences/workflows/shimmering-focus-helpers/switch-to-local-repo.sh
@@ -9,6 +9,6 @@ remote_ssh="git@github.com:chrisgrieser/shimmering-focus.git"
 [[ ! -d "$target_path" ]] && mkdir -p "$target_path"
 cd "$LOCAL_REPOS"
 git clone --depth=1 "$remote_ssh"
-npm i # install dependencies (clean-css-cli)
+cd "shimmering-focus" && npm i &>/dev/null # install dependencies (clean-css-cli)
 
 ln -sf "$LOCAL_REPOS/shimmering-focus/source.css" "$theme_folder/theme.css"
