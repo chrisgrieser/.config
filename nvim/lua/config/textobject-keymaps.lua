d3bfd64131dc02b3cd4c41e3920758894550d528
🤖 automated <cron@job>
2023-05-25 16:34:05 +0200

    mini Office (14)

--- nvim/lua/config/textobject-keymaps.lua
+++ nvim/lua/config/textobject-keymaps.lua
@@ -88,6 +88,7 @@ keymap({ "x", "o" }, "gh", ":Gitsigns select_hunk<CR>", { desc = "󱡔 󰊢 hunk
 
 -- space: subword
 keymap({"o", "x"}, "<Space>", "<cmd>lua require('various-textobjs').subword(true)<CR>", { desc = "󱡔 inner subword textobj" })
+keymap({"o", "x"}, "aS", "<cmd>lua require('various-textobjs').subword(false)<CR>", { desc = "󱡔 outer subword textobj" })
 
 -- L: link
 keymap("o", "L", "<cmd>lua require('various-textobjs').url()<CR>", { desc = "󱡔 link textobj" })
