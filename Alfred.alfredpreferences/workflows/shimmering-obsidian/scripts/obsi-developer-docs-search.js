5c4e69271ede0da82081c874d7f282d76773fdaa
🤖 automated <cron@job>
2023-05-26 15:25:12 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/shimmering-obsidian/scripts/obsi-developer-docs-search.js
+++ Alfred.alfredpreferences/workflows/shimmering-obsidian/scripts/obsi-developer-docs-search.js
@@ -2,34 +2,32 @@
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
-const alfredMatcher = (/** @type {string} */ str) => str.replace (/[-()_/.]/g, " ") + " " + str + " ";
+const alfredMatcher = (/** @type {string} */ str) => str.replace(/[-()_/.]/g, " ") + " " + str + " ";
 
 //------------------------------------------------------------------------------
 
 const sourceURL = "https://api.github.com/repos/obsidianmd/obsidian-developer-docs/git/trees/main?recursive=1";
-const baseURL = "https://docs.obsidian.md/"
+const baseURL = "https://docs.obsidian.md";
 
 const workArray = JSON.parse(app.doShellScript(`curl -sL ${sourceURL}`))
-	.tree
-	.filter((/** @type {{ path: string; }} */ file) => file.path.startsWith("en/") && file.path.endsWith(".md"))
-	.map(file => {
-		const subsitePath = file.path.slice(5, -3);
+	.tree.filter((/** @type {{ path: string; }} */ file) => file.path.startsWith("en/") && file.path.endsWith(".md"))
+	.map((/** @type {{ path: string }} */ file) => {
+		const subsitePath = file.path.slice(3, -3);
 
-		const displayTitle = subsitePath
-			.replace(/.*\//, "") // show only file name
-			.replaceAll("-", " ");
+		const displayTitle = subsitePath.replace(/.*\//, ""); // show only file name
 
 		const category = subsitePath
 			.replace(/(.*)\/.*/, "$1") // only parent
-			.replaceAll ("/", " → ") // nicer tree
-			.replaceAll("-", " ");
+			.replaceAll("/", " → "); // nicer tree
+
+		const subsiteURL = subsitePath.replaceAll(" ", "+"); // obsidian publish uses `+`
 
 		return {
-			"title": displayTitle,
-			"subtitle": category,
-			"match": alfredMatcher (subsitePath),
-			"arg": `https://marcus.se.net/obsidian-plugin-docs/${subsitePath}`,
-			"uid": subsitePath,
+			title: displayTitle,
+			subtitle: category,
+			match: alfredMatcher(subsitePath),
+			arg: `${baseURL}/${subsiteURL}`,
+			uid: subsitePath,
 		};
 	});
 
