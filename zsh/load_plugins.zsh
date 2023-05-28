03143617e9bb910b0c21e5683d332740a5fb6065
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-05-28 08:11:50 +0200

    switch completion back to fzf-tab

--- zsh/load_plugins.zsh
+++ zsh/load_plugins.zsh
@@ -1,5 +1,9 @@
 # shellcheck disable=SC1090,SC1091,SC2292
 
+# Completions for Homebrew https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
+# REQUIRED must be loaded before completion setup
+FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+
 # activate completions, also needed for ZSH auto suggestions & completions
 # must be loaded before plugins
 autoload compinit -Uz +X && compinit
@@ -11,7 +15,7 @@ autoload compinit -Uz +X && compinit
 
 # "fzf-tab needs to be loaded after compinit, but before plugins which will wrap
 # widgets, such as zsh-autosuggestions or fast-syntax-highlighting"
-# source "$DOTFILE_FOLDER/zsh/plugins/fzf-tab/fzf-tab.plugin.zsh"
+source "$DOTFILE_FOLDER/zsh/plugins/fzf-tab/fzf-tab.plugin.zsh"
 
 # INFO `brew --prefix` ensures the right path is inserted on M1 as well as  non-M1 macs
 source "$(brew --prefix)/share/zsh-you-should-use/you-should-use.plugin.zsh"
@@ -28,4 +32,3 @@ source "$DOTFILE_FOLDER/zsh/plugins/magic_enter.zsh"
 # Starship
 eval "$(starship init zsh)"
 export STARSHIP_CONFIG=~/.config/starship/starship.toml
-[[ "$TERM" == "Warp" ]] && export STARSHIP_CONFIG=~/.config/starship/starship-warp.toml
