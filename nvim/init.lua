a9384826ae66f2d6c1d42fa95e92757623cd2648
🤖 automated <cron@job>
2023-05-31 15:40:33 +0200

    iMac Home (37)

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,25 +25,13 @@ safeRequire("config.lazy")
 if vim.fn.has("gui_running") then safeRequire("config.gui-settings") end
 safeRequire("config.theme-customization")
 
-local fn = vim.fn
--- auto-nohl -> https://www.reddit.com/r/neovim/comments/zc720y/comment/iyvcdf0/?context=3
-vim.on_key(function(char)
-	local searchKeys = { "n", "N", "*", "#" }
-	local searchConfirmed = (fn.keytrans(char) == "<CR>" and fn.getcmdtype():find("[/?]") ~= nil)
-	if not (searchConfirmed or fn.mode() == "n") then return end
-	local searchKeyUsed = searchConfirmed or (vim.tbl_contains(searchKeys, fn.keytrans(char)))
-	if vim.opt.hlsearch:get() ~= searchKeyUsed then vim.opt.hlsearch = searchKeyUsed end
-end, vim.api.nvim_create_namespace("auto_nohl"))
-
-
-
 safeRequire("config.options-and-autocmds")
 
--- safeRequire("config.keybindings")
--- safeRequire("config.textobject-keymaps")
+safeRequire("config.keybindings")
+safeRequire("config.textobject-keymaps")
 
--- safeRequire("config.folding")
--- safeRequire("config.clipboard")
+safeRequire("config.folding")
+safeRequire("config.clipboard")
 
--- safeRequire("config.user-commands")
--- safeRequire("config.abbreviations")
+safeRequire("config.user-commands")
+safeRequire("config.abbreviations")
