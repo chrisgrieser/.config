76911c627e93ec0749d7b53da5c765cf4adfdb41
🤖 automated <cron@job>
2023-06-01 20:17:34 +0200

    iMac Home (1)

--- sketchybar/sidenotes-count.sh
+++ sketchybar/sidenotes-count.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env zsh
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 
+#───────────────────────────────────────────────────────────────────────────────
+# CONFIG
 folder="Base"
+preview_cutoff=9
+
+#───────────────────────────────────────────────────────────────────────────────
+
 note_count=$(osascript -l JavaScript -e "
 	Application('SideNotes').folders.byName(\"$folder\").notes().length
 ")
@@ -17,7 +23,7 @@ fi
 
 # add preview of note to status bar (similar to the OneThing app)
 if [[ $(echo "$current_note" | wc -l) -eq 1 ]]; then
-	[[ ${#current_note} -lt 10 ]] && preview="$current_note" || preview="$(echo "$current_note" | cut -c-10)…"
+	[[ ${#current_note} -lt $preview_cutoff ]] && preview="$current_note" || preview="$(echo "$current_note" | cut -c-$preview_cutoff)…"
 	label="$label $preview"
 fi
 
