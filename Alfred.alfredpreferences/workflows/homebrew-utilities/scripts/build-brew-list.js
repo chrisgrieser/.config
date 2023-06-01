35c109f6f002cc1f7a3c96d6238a1b7d6d1db8a5
🤖 automated <cron@job>
2023-06-01 13:57:34 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/homebrew-utilities/scripts/build-brew-list.js
+++ Alfred.alfredpreferences/workflows/homebrew-utilities/scripts/build-brew-list.js
@@ -4,59 +4,68 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 const home = app.pathTo("home folder");
 
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @param {string} str */
 function alfredMatcher(str) {
 	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
 	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
 	return [clean, camelCaseSeperated, str].join(" ") + " ";
 }
 
+/** @param {string} path */
 function readFile(path) {
 	const data = $.NSFileManager.defaultManager.contentsAtPath(path);
 	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
 	return ObjC.unwrap(str);
 }
 
-const fileExists = filePath => Application("Finder").exists(Path(filePath));
+const fileExists = (/** @type {string} */ filePath) => Application("Finder").exists(Path(filePath));
 
 //──────────────────────────────────────────────────────────────────────────────
 
 // INFO https://formulae.brew.sh/docs/api/
 const jsonArray = [];
 
-const caskTxt = home + "/Library/Caches/Homebrew/api/cask_names.txt";
-const formulaTxt = home + "/Library/Caches/Homebrew/api/formula_names.txt";
-if (!fileExists(formulaTxt) || !fileExists(caskTxt)) app.doShellScript(`brew update`);
+// OFFLINE USAGE
+// const caskTxt = home + "/Library/Caches/Homebrew/api/cask_names.txt";
+// const formulaTxt = home + "/Library/Caches/Homebrew/api/formula_names.txt";
+// if (!(fileExists(formulaTxt) && fileExists(caskTxt))) app.doShellScript("brew update");
+// const casks = readFile(caskTxt).split("\n");
+// const formula = readFile(formulaTxt).split("\n");
 
-const casks = readFile(caskTxt).split("\n");
-const formula = readFile(formulaTxt).split("\n");
-let mackups = false;
+const caskApi = "https://formulae.brew.sh/api/cask.json"
+const formulaApi = "https://formulae.brew.sh/api/formula.json"
+const casks = JSON.parse(app.shellScript(`curl -s ${caskApi}`));
+const formulae = JSON.parse(app.shellScript(`curl -s ${formulaApi}`));
+
+let mackups;
 try {
-	mackups = app.doShellScript("export PATH=/usr/local/bin/:/opt/homebrew/bin/:$PATH ; mackup list")
-	if (mackups) mackups = mackups.split("\r").map(item => item.slice(3));
+	const mackupsStr = app.doShellScript("export PATH=/usr/local/bin/:/opt/homebrew/bin/:$PATH ; mackup list")
+	mackups = mackupsStr ? mackupsStr.split("\r").map((/** @type {string | any[]} */ item) => item.slice(3)) : [];
 } catch (error) {
 	console.log(error);
 }
 
 //──────────────────────────────────────────────────────────────────────────────
 
-casks.forEach(name => {
-	const mackupIcon = mackups && mackups.includes(name) ? " " + $.getenv("mackup_icon") : "";
+casks.forEach((cask) => {
+	const mackupIcon = mackups.includes(cask.name) ? " " + $.getenv("mackup_icon") : "";
+	const url = cask.homepage || "";
+	
 	jsonArray.push({
-		title: name + mackupIcon,
-		match: alfredMatcher(name),
-		subtitle: "cask",
-		arg: `${name} --cask`,
-		uid: name,
-	});
-});
-formula.forEach(name => {
-	const mackupIcon = mackups && mackups.includes(name) ? " " + $.getenv("mackup_icon") : "";
-	jsonArray.push({
-		title: name + mackupIcon,
-		match: alfredMatcher(name),
-		subtitle: "formula",
-		arg: `${name} --formula`,
-		uid: name,
+		title: cask.name + mackupIcon,
+		match: alfredMatcher(cask.name),
+		subtitle: `cask – ${cask.desc}`,
+		arg: `${cask.token} --cask`,
+		uid: cask.token,
+		mods: {
+			cmd: {
+				arg: url,
+				subtitle: `⌘: ${url}`,
+				valid: Boolean(url)
+			},
+		},
 	});
 });
 
