0ff7fa20590f04a2ec705856bb72517f49356b62
🤖 automated <cron@job>
2023-06-01 19:17:34 +0200

    iMac Home (3)

--- sketchybar/sidenotes-count.sh
+++ sketchybar/sidenotes-count.sh
@@ -5,7 +5,7 @@ folder="Base"
 count=$(osascript -l JavaScript -e "
 	Application('SideNotes').folders.byName(\"$folder\").notes().length
 ")
-unfinishedTasks=$(osascript -l JavaScript -e "
+unfinished_tasks=$(osascript -l JavaScript -e "
 	const notesInFolder = Application('SideNotes').folders.byName(\"$folder\").notes();
 	let totalTasks = 0;
 	for (let i = 0; i < notesInFolder.length; i++) {
@@ -15,16 +15,25 @@ unfinishedTasks=$(osascript -l JavaScript -e "
 	}
 	totalTasks; // direct return
 ")
+current_note=$(osascript -l JavaScript -e "
+	Application('SideNotes').currentNote().text()
+")
 
 if [[ $count -eq 0 ]]; then
 	icon=""
 	label=""
-elif [[ $unfinishedTasks -eq 0 ]]; then
+elif [[ $unfinished_tasks -eq 0 ]]; then
 	icon="󰛽"
 	label="$count"
 else
 	icon="󰛽"
-	label="$count ($unfinishedTasks 󰝣 )"
+	label="$count ($unfinished_tasks 󰝣 )"
+fi
+
+# add preview of note to status bar (similar to the OneThing app)
+if [[ $(echo "$current_note" | wc -l) -eq 1 ]]; then
+	[[ ${#current_note} -lt 15 ]] && short_text="$current_note" || short_text="$(echo "$current_note" | cut -c-15)…"
+	label="$label $short_text"
 fi
 
 sketchybar --set "$NAME" icon="$icon" label="$label"
