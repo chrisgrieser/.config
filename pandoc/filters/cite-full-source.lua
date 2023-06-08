511b2e02cddbfa2ff07fbad3280eda7307c92c0a
🤖 automated <cron@job>
2023-06-08 16:28:33 +0200

    mini Office (4)

--- pandoc/filters/cite-full-source.lua
+++ pandoc/filters/cite-full-source.lua
@@ -1,15 +1,14 @@
 -- https://stackoverflow.com/a/66605610
+--------------------------------------------------------------------------------
 
 local refs = {}
 
-local function store_refs (div)
-	local ref_id = div.identifier:match 'ref%-(.*)$'
-	if ref_id then
-		refs[ref_id] = div.content
-	end
+local function store_refs(div)
+	local ref_id = div.identifier:match("ref%-(.*)$")
+	if ref_id then refs[ref_id] = div.content end
 end
 
-local function replace_cite (cite)
+local function replace_cite(cite)
 	local citation = cite.citations[1]
 	if citation and refs[citation.id] and #cite.citations == 1 then
 		return pandoc.utils.blocks_to_inlines(refs[citation.id])
@@ -17,6 +16,6 @@ local function replace_cite (cite)
 end
 
 return {
-	{Div = store_refs},
-	{Cite = replace_cite},
+	{ Div = store_refs },
+	{ Cite = replace_cite },
 }
