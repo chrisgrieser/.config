c290592ffcbf4ef4251677b9dc3e941106e03d0b
🤖 automated <cron@job>
2023-06-08 17:48:33 +0200

    mini Office (4)

--- pandoc/filters/cite-full-source.lua
+++ pandoc/filters/cite-full-source.lua
@@ -9,9 +9,9 @@ local function store_refs(div)
 end
 
 local function replace_cite(cite)
-
-	local citekey = cite.citations[1]
-	if citekey and refs[citekey] and #cite.citations == 1 then
+	if #cite.citations ~= 1 then return end
+	local citekey = cite.citations[1].id
+	if citekey and refs[citekey] then
 		local full_reference = pandoc.utils.blocks_to_inlines(refs[citekey])
 		local prefix = { citekey .. " – "} -- prefix citekey in front of the citation
 		return prefix .. full_reference
