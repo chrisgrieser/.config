443e3f5035e51ffb1c646d222f2e4efb5914855f
🤖 automated <cron@job>
2023-06-08 20:38:07 +0200

    iMac Home (2)

--- nvim/after/ftplugin/bib.lua
+++ nvim/after/ftplugin/bib.lua
@@ -1,5 +1,4 @@
 local bo = vim.bo
-local fn = vim.fn
 --------------------------------------------------------------------------------
 
 -- do not autowrap
@@ -8,3 +7,23 @@ bo.formatoptions = bo.formatoptions:gsub("t", "")
 -- for some reason, bib files have no comment string defined, even though they
 -- do have comments?
 bo.commentstring = "% %s"
+
+--------------------------------------------------------------------------------
+
+local function checkCitekeysForDuplicates()
+	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+	local linesWithAt = vim.tbl_filter(function(line) return line:match("^@") end, lines)
+	local citekeys = vim.tbl_map(function(line) return line:match("^.-{(.*)") end, linesWithAt)
+	local citekeyCount = {}
+	for _, citekey in pairs(citekeys) do
+		if not citekeyCount[citekey] then
+			citekeyCount[citekey] = 1
+		else
+			citekeyCount[citekey] = citekeyCount[citekey] + 1
+		end
+	end
+	local duplicateCitekeys = vim.tbl_filter(function(citekey) return citekeyCount[citekey] > 1 end, citekeys)
+end
+
+-- run on entering a bibtex buffer
+checkCitekeysForDuplicates()
