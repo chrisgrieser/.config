5a3fed8f3cb611c864b346c3c597443bfacd0c7f
🤖 automated <cron@job>
2023-06-09 19:38:06 +0200

    iMac Home (1)

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,5 +40,7 @@ safeRequire("config.abbreviations")
 
 -- Load tip of the day after launching nvim
 local tip = vim.fn.system('curl -s "https://vtip.43z.one"')
-tip = "TIP\n" .. tip
-vim.defer_fn(function() vim.notify(tip, vim.log.levels.INFO, { timeout = 10000 }) end, 2000)
+vim.defer_fn(
+	function() vim.notify("  TIP\n" .. tip, vim.log.levels.INFO, { timeout = 10000 }) end,
+	2000
+)
