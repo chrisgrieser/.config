85e9fc5addabaddc3566941641605a44fd5d7ce5
🤖 automated <cron@job>
2023-06-09 20:58:06 +0200

    iMac Home (4)

---
+++ nvim/lua/config/Untitled.lua
@@ -0,0 +1,338 @@
+
+ local bla = { { 
+     bufnr = 9, 
+     changed = 0, 
+     changedtick = 4, 
+     hidden = 0, 
+     lastused = 1686336093, 
+     linecount = 867, 
+     listed = 1, 
+     lnum = 319, 
+     loaded = 1, 
+     name = "/Users/chrisgrieser/.config/nvim/lua/config/keybindings.lua", 
+     signs = { { 
+         group = "gitsigns_vimfn_signs_", 
+         id = 604, 
+         lnum = 604, 
+         name = "GitSignsDeleteDelete", 
+         priority = 6 
+       } }, 
+     variables = { 
+       __indent_blankline_active = true, 
+       __indent_blankline_ranges = { 
+         ["0"] = { { 295, 345 } } 
+       }, 
+       autopairs_keymaps = { "{", "}", "`", "[", "]", "<", "(", ")", "'", '"' }, 
+       changedtick = 4, 
+       cur_branch = "main", 
+       did_ftplugin = 1, 
+       did_indent = 1, 
+       editorconfig = {}, 
+       gitsigns_head = "main", 
+       gitsigns_status = "-2", 
+       gitsigns_status_dict = { 
+         added = 0, 
+         changed = 0, 
+         gitdir = "/Users/chrisgrieser/.config/.git", 
+         head = "main", 
+         removed = 2, 
+         root = "/Users/chrisgrieser/.config" 
+       }, 
+       harpoonMark = "", 
+       match_ignorecase = 0, 
+       match_midmap = { { "luaFunction", "return" } }, 
+       match_words = "\\<\\%(do\\|function\\|if\\)\\>:\\ 
+ \<\\%(return\\|else\\|elseif\\)\\>:\\<end\\>,\\<repeat\ 
+ \\>:\\<until\\>,\\%(--\\)\\=\\[\\(=*\\)\\[:]\\1],--\\[\ 
+ \\(=*\\)\\[:]\\1]", 
+       matchup_active_engines = { 
+         all = { "tree_sitter", "classic" }, 
+         delim_all = { "tree_sitter", "classic" }, 
+         delim_py = { "tree_sitter" }, 
+         delim_tex = { "classic" } 
+       }, 
+       matchup_delim_enabled = 1, 
+       matchup_delim_lists = { 
+         all = { 
+           regex = { { 
+               augments = vim.empty_dict(), 
+               close = ")", 
+               mid = "", 
+               mid_list = {}, 
+               open = "(" 
+             }, { 
+               augments = vim.empty_dict(), 
+               close = "}", 
+               mid = "", 
+               mid_list = {}, 
+               open = "{" 
+             }, { 
+               augments = vim.empty_dict(), 
+               close = "]", 
+               mid = "", 
+               mid_list = {}, 
+               open = "\\[" 
+             } }, 
+           regex_capture = { { 
+               aug_comp = vim.empty_dict(), 
+               close = ")", 
+               extra_info = { 
+                 has_zs = 0 
+               }, 
+               extra_list = { vim.empty_dict(), vim.empt 
+ ty_dict() }, 
+               grp_renu = vim.empty_dict(), 
+               has_zs = 0, 
+               mid = "", 
+               mid_list = {}, 
+               need_grp = vim.empty_dict(), 
+               open = "(" 
+             }, { 
+               aug_comp = vim.empty_dict(), 
+               close = "}", 
+               extra_info = { 
+                 has_zs = 0 
+               }, 
+               extra_list = { vim.empty_dict(), vim.empt 
+ ty_dict() }, 
+               grp_renu = vim.empty_dict(), 
+               has_zs = 0, 
+               mid = "", 
+               mid_list = {}, 
+               need_grp = vim.empty_dict(), 
+               open = "{" 
+             }, { 
+               aug_comp = vim.empty_dict(), 
+               close = "]", 
+               extra_info = { 
+                 has_zs = 0 
+               }, 
+               extra_list = { vim.empty_dict(), vim.empt 
+ ty_dict() }, 
+               grp_renu = vim.empty_dict(), 
+               has_zs = 0, 
+               mid = "", 
+               mid_list = {}, 
+               need_grp = vim.empty_dict(), 
+               open = "\\[" 
+             } } 
+         }, 
+         delim_all = { 
+           regex = { { 
+               augments = vim.empty_dict(), 
+               close = ")", 
+               mid = "", 
+               mid_list = {}, 
+               open = "(" 
+             }, { 
+               augments = vim.empty_dict(), 
+               close = "}", 
+               mid = "", 
+               mid_list = {}, 
+               open = "{" 
+             }, { 
+               augments = vim.empty_dict(), 
+               close = "]", 
+               mid = "", 
+               mid_list = {}, 
+               open = "\\[" 
+             } }, 
+           regex_capture = { { 
+               aug_comp = vim.empty_dict(), 
+               close = ")", 
+               extra_info = { 
+                 has_zs = 0 
+               }, 
+               extra_list = { vim.empty_dict(), vim.empt 
+ ty_dict() }, 
+               grp_renu = vim.empty_dict(), 
+               has_zs = 0, 
+               mid = "", 
+               mid_list = {}, 
+               need_grp = vim.empty_dict(), 
+               open = "(" 
+             }, { 
+               aug_comp = vim.empty_dict(), 
+               close = "}", 
+               extra_info = { 
+                 has_zs = 0 
+               }, 
+               extra_list = { vim.empty_dict(), vim.empt 
+ ty_dict() }, 
+               grp_renu = vim.empty_dict(), 
+               has_zs = 0, 
+               mid = "", 
+               mid_list = {}, 
+               need_grp = vim.empty_dict(), 
+               open = "{" 
+             }, { 
+               aug_comp = vim.empty_dict(), 
+               close = "]", 
+               extra_info = { 
+                 has_zs = 0 
+               }, 
+               extra_list = { vim.empty_dict(), vim.empt 
+ ty_dict() }, 
+               grp_renu = vim.empty_dict(), 
+               has_zs = 0, 
+               mid = "", 
+               mid_list = {}, 
+               need_grp = vim.empty_dict(), 
+               open = "\\[" 
+             } } 
+         }, 
+         delim_tex = { 
+           regex = { { 
+               augments = vim.empty_dict(), 
+               close = ")", 
+               mid = "", 
+               mid_list = {}, 
+               open = "(" 
+             }, { 
+               augments = vim.empty_dict(), 
+               close = "}", 
+               mid = "", 
+               mid_list = {}, 
+               open = "{" 
+             }, { 
+               augments = vim.empty_dict(), 
+               close = "]", 
+               mid = "", 
+               mid_list = {}, 
+               open = "\\[" 
+             } }, 
+           regex_capture = { { 
+               aug_comp = vim.empty_dict(), 
+               close = ")", 
+               extra_info = { 
+                 has_zs = 0 
+               }, 
+               extra_list = { vim.empty_dict(), vim.empt 
+ ty_dict() }, 
+               grp_renu = vim.empty_dict(), 
+               has_zs = 0, 
+               mid = "", 
+               mid_list = {}, 
+               need_grp = vim.empty_dict(), 
+               open = "(" 
+             }, { 
+               aug_comp = vim.empty_dict(), 
+               close = "}", 
+               extra_info = { 
+                 has_zs = 0 
+               }, 
+               extra_list = { vim.empty_dict(), vim.empt 
+ ty_dict() }, 
+               grp_renu = vim.empty_dict(), 
+               has_zs = 0, 
+               mid = "", 
+               mid_list = {}, 
+               need_grp = vim.empty_dict(), 
+               open = "{" 
+             }, { 
+               aug_comp = vim.empty_dict(), 
+               close = "]", 
+               extra_info = { 
+                 has_zs = 0 
+               }, 
+               extra_list = { vim.empty_dict(), vim.empt 
+ ty_dict() }, 
+               grp_renu = vim.empty_dict(), 
+               has_zs = 0, 
+               mid = "", 
+               mid_list = {}, 
+               need_grp = vim.empty_dict(), 
+               open = "\\[" 
+             } } 
+         } 
+       }, 
+       matchup_delim_re = { 
+         all = { 
+           _engine_info = { 
+             has_zs = { 
+               both = 0, 
+               both_all = 0, 
+               close = 0, 
+               mid = 0, 
+               open = 0, 
+               open_mid = 0 
+             } 
+           }, 
+           both = "\\m\\C\\%()\\|(\\|}\\|{\\|]\\|\\[\\)" 
+ ", 
+           both_all = "\\m\\C\\%()\\|(\\|}\\|{\\|]\\|\\[ 
+ [\\)", 
+           close = "\\m\\C\\%()\\|}\\|]\\)", 
+           mid = "", 
+           open = "\\m\\C\\%((\\|{\\|\\[\\)", 
+           open_mid = "\\m\\C\\%((\\|{\\|\\[\\)" 
+         }, 
+         delim_all = { 
+           _engine_info = { 
+             has_zs = { 
+               both = 0, 
+               both_all = 0, 
+               close = 0, 
+               mid = 0, 
+               open = 0, 
+               open_mid = 0 
+             } 
+           }, 
+           both = "\\m\\C\\%()\\|(\\|}\\|{\\|]\\|\\[\\)" 
+ ", 
+           both_all = "\\m\\C\\%()\\|(\\|}\\|{\\|]\\|\\[ 
+ [\\)", 
+           close = "\\m\\C\\%()\\|}\\|]\\)", 
+           mid = "", 
+           open = "\\m\\C\\%((\\|{\\|\\[\\)", 
+           open_mid = "\\m\\C\\%((\\|{\\|\\[\\)" 
+         }, 
+         delim_tex = { 
+           _engine_info = { 
+             has_zs = { 
+               both = 0, 
+               both_all = 0, 
+               close = 0, 
+               mid = 0, 
+               open = 0, 
+               open_mid = 0 
+             } 
+           }, 
+           both = "\\m\\C\\%()\\|(\\|}\\|{\\|]\\|\\[\\)" 
+ ", 
+           both_all = "\\m\\C\\%()\\|(\\|}\\|{\\|]\\|\\[ 
+ [\\)", 
+           close = "\\m\\C\\%()\\|}\\|]\\)", 
+           mid = "", 
+           open = "\\m\\C\\%((\\|{\\|\\[\\)", 
+           open_mid = "\\m\\C\\%((\\|{\\|\\[\\)" 
+         } 
+       }, 
+       matchup_delim_skip = "matchup#ts_syntax#skip_expr 
+ r(s:effline('.'),s:effcol('.'))", 
+       navic_awaiting_lsp_response = true, 
+       navic_client_id = 4, 
+       navic_client_name = "lua_ls", 
+       ["nvim-autopairs"] = 1, 
+       scrollview_search_cached = { 291, 292 }, 
+       scrollview_search_changedtick_cached = 3, 
+       scrollview_search_pattern = "extra", 
+       scrollview_search_pattern_cached = "extra", 
+       scrollview_signs_5_diagnostics = {}, 
+       scrollview_signs_64_search = {}, 
+       scrollview_signs_68_gitsigns = {}, 
+       scrollview_signs_69_gitsigns = {}, 
+       scrollview_signs_6_diagnostics = {}, 
+       scrollview_signs_70_gitsigns = { 604 }, 
+       scrollview_signs_7_diagnostics = {}, 
+       scrollview_signs_8_diagnostics = {}, 
+       timeOpened = 1686335809, 
+       ts_highlight = true, 
+       undo_ftplugin = "setlocal cms< com< def< fo< inex 
+ x< sua< | unlet! b:match_words b:match_ignorecase| unle 
+ et! b:match_midmap", 
+       undo_indent = "setlocal autoindent< indentexpr< i 
+ indentkeys<" 
+     }, 
+     windows = { 1000 } 
+   } } 
