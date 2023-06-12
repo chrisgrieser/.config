49117e004e4bd590e2f24ef37601ccd9a37fb8ea
🤖 automated <cron@job>
2023-06-12 19:22:39 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/neovim-utilities/scripts/search-nvim-plugins.js
+++ Alfred.alfredpreferences/workflows/neovim-utilities/scripts/search-nvim-plugins.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env osascript -l JavaScript
-/* eslint-disable no-magic-numbers */
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
-const alfredMatcher = str => str.replace(/[-/()_.:]/g, " ") + " " + str + " " + str.replace(/([A-Z])/g, " $1"); // match parts of CamelCase
+const alfredMatcher = (/** @type {string} */ str) => str.replace(/[-/()_.:]/g, " ") + " " + str + " " + str.replace(/([A-Z])/g, " $1"); // match parts of CamelCase
 
 //──────────────────────────────────────────────────────────────────────────────
 
@@ -11,18 +10,18 @@ const alfredMatcher = str => str.replace(/[-/()_.:]/g, " ") + " " + str + " " + 
 const neovimcraftURL = "https://nvim.sh/s";
 
 const installedPluginsPath = $.getenv("plugin_installation_path").replace(/^~/, app.pathTo("home folder"));
-// prettier-ignore
-const installedPlugins = app.doShellScript(
-	`find "${installedPluginsPath}" -path "*/.git" -type d -maxdepth 3 | while read -r line ; do
-		cd "$line"/..
-		git remote -v | head -n1
-	done`)
+const installedPlugins = app
+	.doShellScript(
+		`find "${installedPluginsPath}" -path "*/.git" -type d -maxdepth 3 |
+		while read -r line ; do
+			cd "$line"/..
+			git remote -v | head -n1
+		done`,
+	)
 	.split("\r")
-	.map(remote => {
-		return remote
-			.slice(26, -12)
-			.replaceAll(".git (fetch)", ""); // for lazy.nvim
-	})
+	.map((/** @type {string} */ remote) => {
+		return remote.slice(26, -12).replaceAll(".git (fetch)", ""); // for lazy.nvim
+	});
 
 //──────────────────────────────────────────────────────────────────────────────
 
@@ -30,7 +29,7 @@ const jsonArray = app
 	.doShellScript(`curl -sL '${neovimcraftURL}'`)
 	.split("\r")
 	.slice(2)
-	.map(line => {
+	.map((/** @type {string} */ line) => {
 		const parts = line.split(/ {2,}/);
 		const repo = parts[0];
 		const name = repo.split("/")[1];
@@ -38,9 +37,8 @@ const jsonArray = app
 		// subtitles
 		const stars = parts[1];
 		const openIssues = parts[2];
-		const daysAgo = Math.ceil((new Date() - new Date(parts[3])) / 1000 / 3600 / 24);
-		let updated =
-			daysAgo < 31 ? daysAgo.toString() + " days ago" : Math.ceil(daysAgo / 30).toString() + " months ago";
+		const daysAgo = Math.ceil((+new Date() - +new Date(parts[3])) / 1000 / 3600 / 24);
+		let updated = daysAgo < 31 ? daysAgo.toString() + " days ago" : Math.ceil(daysAgo / 30).toString() + " months ago";
 		if (updated.startsWith("1 ")) updated = updated.replace("s ago", " ago"); // remove plural "s"
 		const desc = parts[4] || "";
 		let subtitle = `★ ${stars} – ${updated}`;
