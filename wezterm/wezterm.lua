618ac5c2603f74e54bb1702eebf883ee26d0f2c3
🤖 automated <cron@job>
2023-06-14 15:00:27 +0200

    mini Office (2)

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -67,7 +67,7 @@ local keybindings = {
 	{ key = "k", mods = "CMD", action = act.ClearScrollback("ScrollbackAndViewport") },
 	{ key = "PageDown", mods = "", action = act.ScrollByPage(0.8) },
 	{ key = "PageUp", mods = "", action = act.ScrollByPage(-0.8) },
-	{ key = "Enter", mods = "SHIFT", action = act.ActivateTabRelative(1) },
+	{ key = "Enter", mods = "CTRL", action = act.ActivateTabRelative(1) },
 	{ key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) },
 	{ key = "v", mods = "CMD", action = actFun(autoQuotePastedUrls) },
 
