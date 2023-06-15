7ad459986c12615d49a877aa195a361fb14b62ac
🤖 automated <cron@job>
2023-06-15 15:30:22 +0200

    iMac Home (1)

--- Alfred.alfredpreferences/workflows/pass-cli-client/scripts/pass-cli-search.js
+++ Alfred.alfredpreferences/workflows/pass-cli-client/scripts/pass-cli-search.js
@@ -1,18 +1,23 @@
 #!/usr/bin/env osascript -l JavaScript
+
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+/** @param {string} str */
+function alfredMatcher(str) {
+	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
+	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
+	return [clean, camelCaseSeperated, str].join(" ");
+}
+const fileExists = (/** @type {string} */ filePath) => Application("Finder").exists(Path(filePath));
+const home = app.pathTo("home folder");
+
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @param {string[]} argv */
+// rome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
-	ObjC.import("stdlib");
-	const app = Application.currentApplication();
-	app.includeStandardAdditions = true;
-
-	function alfredMatcher(str) {
-		const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
-		const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
-		return [clean, camelCaseSeperated, str].join(" ");
-	}
-	const fileExists = filePath => Application("Finder").exists(Path(filePath));
-	const home = app.pathTo("home folder");
-
-	//──────────────────────────────────────────────────────────────────────────────
 	let passwordStore = argv[0]; // INFO password store location retrieved via .zshenv
 	if (passwordStore === "") passwordStore = home + "/.password-store";
 	const jsonArray = [];
@@ -26,9 +31,10 @@ function run(argv) {
 		return JSON.stringify({ items: jsonArray });
 	}
 
-	app.doShellScript(`cd "${passwordStore}" ; find . -name "*.gpg"`)
+	app
+		.doShellScript(`cd "${passwordStore}" ; find . -name "*.gpg"`)
 		.split("\r")
-		.forEach(gpgFile => {
+		.forEach((/** @type {string} */ gpgFile) => {
 			const id = gpgFile.slice(2, -4);
 			const parts = id.split("/");
 			const name = parts.pop();
@@ -43,24 +49,25 @@ function run(argv) {
 			});
 		});
 
-	app.doShellScript(`cd "${passwordStore}" ; find . -type d -not -path "*/.git*"`)
+	app
+		.doShellScript(`cd "${passwordStore}" ; find . -type d -not -path "*/.git*"`)
 		.split("\r")
 		.slice(1) // first entry removed (root)
-		.forEach(folder => {
-			folder = folder.slice(2); // remove leading "./"
+		.forEach((/** @type {string} */ folder) => {
 
 			jsonArray.push({
-				title: folder,
+				title: folder.slice(2),// remove leading "./"
 				icon: { path: "pw-folder.png" },
 				subtitle: "Create new entry in folder",
 				match: alfredMatcher(folder) + " new folder",
 				arg: "*" + folder,
 				uid: folder,
-				mods: { // disallow entry-actions
-					"cmd": { valid: false },
-					"shift": { valid: false },
+				mods: {
+					// disallow entry-actions
+					cmd: { valid: false },
+					shift: { valid: false },
 					"fn+cmd": { valid: false },
-					"ctrl": { valid: false },
+					ctrl: { valid: false },
 				},
 			});
 		});
