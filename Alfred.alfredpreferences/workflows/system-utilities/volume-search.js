bf3960e87da443c7e408a2e85bf28d1308f45889
🤖 automated <cron@job>
2023-06-16 15:30:21 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/system-utilities/volume-search.js
+++ Alfred.alfredpreferences/workflows/system-utilities/volume-search.js
@@ -3,43 +3,38 @@
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 
-const notToDisplay = [
-	"Macintosh HD",
-	"Samsung SSD 1TB",
-	"GoogleDrive",
-	"Recovery"
-];
+const notToDisplay = ["Macintosh HD", "Samsung SSD 1TB", "GoogleDrive", "Recovery"];
 
 //──────────────────────────────────────────────────────────────────────────────
 
-const volumes = app.doShellScript("ls /Volumes/")
+const volumes = app
+	.doShellScript("ls /Volumes/")
 	.split("\r")
 	.map((/** @type {string} */ vol) => {
-		if (!notToDisplay.includes(vol)) return {};
-		const diskSpace = app.doShellScript(`df -h | grep "${vol}" | tr -s " " | cut -d " " -f 2-5 | tr "i." "b," `).split(" ");
-		const spaceInfo =
-		"Total: " + diskSpace[0]
-		+ "   Available: " + diskSpace[2]
-		+ "   Used: " + diskSpace[1]
-		+ " (" + diskSpace[3] + ")";
-
-		return{
-			"title": "📂 " + vol,
-			"subtitle": spaceInfo,
-			"arg": "/Volumes/"+ vol
+		if (notToDisplay.includes(vol)) return {};
+		const space = app.doShellScript(`df -h | grep "${vol}" | tr -s " " | cut -d " " -f 2-5 | tr "i." "b," `).split(" ");
+		const spaceInfo = `Total: ${space[0]}   Available: ${space[2]}   Used: ${space[1]} (${space[3]})`;
+		return {
+			title: "📂 " + vol,
+			subtitle: spaceInfo,
+			arg: "/Volumes/" + vol,
 		};
 	});
 
+let rerunSecs = 5;
+
 if (volumes.length === 0) {
-	volumes.push ({
-		"title": "No mounted volume recognized.",
-		"subtitle": "Press [Esc] to abort.",
-		"arg": "no volume"
+	volumes.push({
+		title: "No mounted volume recognized.",
+		subtitle: "Press [Esc] to abort.",
+		arg: "no volume",
 	});
+	rerunSecs = 1; // quicker reruns when no volume found
 }
 
-
+/** @type function {} */
 JSON.stringify({
-	"rerun":
-	"items": volumeArray,
+	rerun: rerunSecs, // seconds (only 0.1 - 5)
+	items: volumes,
 });
+// JSON.stringify(out);
