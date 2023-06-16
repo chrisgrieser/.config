420476fe6e522629458a360a42962112051966af
🤖 automated <cron@job>
2023-06-16 13:30:21 +0200

    iMac Home (1)

--- Alfred.alfredpreferences/workflows/terminal-and-dotfiles/open-terminal.sh
+++ Alfred.alfredpreferences/workflows/terminal-and-dotfiles/open-terminal.sh
@@ -3,19 +3,11 @@ export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 
 FRONT_APP=$(osascript -e 'tell application "System Events" to set frontApp to (name of first process where it is frontmost)')
 if [[ "$FRONT_APP" =~ "Finder" ]]; then
-	dir_to_open=$(osascript -e '
-		tell application "Finder"
-			if (count windows) is not 0 then set pathToOpen to target of window 1 as alias
-			return POSIX path of pathToOpen
-		end tell
-	')
+	dir_to_open=$(osascript -e 'tell application "Finder" to return POSIX path of (target of window 1 as alias)')
 elif [[ "$FRONT_APP" =~ "neovide" ]]; then
 	# INFO requires vim.opt.titlestring='%{expand(\"%:p\")}'
 	win_title=$(osascript -e 'tell application "System Events" to tell process "neovide" to return name of front window')
 	dir_to_open=$(dirname "$win_title")
-else
-	# shellcheck disable=2016
-	dir_to_open='$WD' # defined in .zshenv
 fi
 
 #───────────────────────────────────────────────────────────────────────────────
@@ -27,5 +19,5 @@ fi
 
 open -a "WezTerm" # launch/activate
 while ! pgrep -xq "wezterm-gui"; do sleep 0.1; done
-sleep 0.1
-echo "cd \"$dir_to_open\"" | wezterm cli send-text --no-paste
+sleep 0.2
+[[ -n "$dir_to_open" ]] && echo "cd '$dir_to_open'" | wezterm cli send-text --no-paste
