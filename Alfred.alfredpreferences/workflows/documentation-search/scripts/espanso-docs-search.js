1827daf6cef61d7faf1e36976d9117d4b96a6030
🤖 automated <cron@job>
2023-06-18 07:42:58 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/documentation-search/scripts/espanso-docs-search.js
+++ Alfred.alfredpreferences/workflows/documentation-search/scripts/espanso-docs-search.js
@@ -2,16 +2,15 @@
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
-const alfredMatcher = (str) => str.replace (/[-()_./]/g, " ") + " " + str + " ";
-
-
+const alfredMatcher = (str) => str.replace(/[-()_./]/g, " ") + " " + str + " ";
 
 //──────────────────────────────────────────────────────────────────────────────
 
-const workArray = JSON.parse(app.doShellScript('curl -s "https://api.github.com/repos/espanso/website/git/trees/main?recursive=1"'))
-	.tree
-	.filter(file => /^docs\/.*\.mdx?$/.test(file.path) && !file.path.includes("/_"))
-	.map(file => {
+const workArray = JSON.parse(
+	app.doShellScript('curl -s "https://api.github.com/repos/espanso/website/git/trees/main?recursive=1"'),
+)
+	.tree.filter((file) => /^docs\/.*\.mdx?$/.test(file.path) && !file.path.includes("/_"))
+	.map((file) => {
 		const site = file.path
 			.slice(5) // remove "docs/"
 			.replace(/\.mdx?$/, ""); // remove file extension
@@ -21,11 +20,11 @@ const workArray = JSON.parse(app.doShellScript('curl -s "https://api.github.com/
 		const url = "https://espanso.org/docs/" + site;
 
 		return {
-			"title": displayTitle,
-			"subtitle": parentSite,
-			"match": alfredMatcher (site),
-			"arg": url,
-			"uid": site,
+			title: displayTitle,
+			subtitle: parentSite,
+			match: alfredMatcher(site),
+			arg: url,
+			uid: site,
 		};
 	});
 
