8b4f6248dff5919474153fc5341ba8439133bd5a
🤖 automated <cron@job>
2023-06-18 07:12:58 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/documentation-search/scripts/git-doc-search.js
+++ Alfred.alfredpreferences/workflows/documentation-search/scripts/git-doc-search.js
@@ -2,14 +2,29 @@
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
-const alfredMatcher = (str) => str.replace (/[-()_.]/g, " ") + " " + str + " ";
+
+const alfredMatcher = (/** @type {string} */ str) => str.replace(/[-()_.]/g, " ") + " " + str + " ";
 
 //──────────────────────────────────────────────────────────────────────────────
 
-const workArray = JSON.parse(app.doShellScript('curl -s "https://api.github.com/repos/git/git/git/trees/master?recursive=1"'))
-	.tree
-	.filter(file => file.path.startsWith("Documentation/") && !file.path.includes("/RelNotes/"))
-	.map(file => {
+const gitBookURL = "https://git-scm.com/book/en/v2";
+const referenceDocsURL = gitBookURL + "https://api.github.com/repos/git/git/git/trees/master?recursive=1";
+const ahrefRegex = /.*?href="(.*?)">(.*?)<.*/i;
+
+const gitBookPages = app
+	.doShellScript(`curl -s "${gitBookURL}"`)
+	.split("\r")
+	.map(item => {
+		
+		return item;
+	})
+
+const referenceDocs = JSON.parse(app.doShellScript(`curl -sL "${referenceDocsURL}"`))
+	.tree.filter(
+		(/** @type {{ path: string; }} */ file) =>
+			file.path.startsWith("Documentation/") && !file.path.includes("/RelNotes/"),
+	)
+	.map((/** @type {{ path: string; }} */ file) => {
 		const subsite = file.path
 			.slice(14) // eslint-disable-line no-magic-numbers
 			.replace(/\.\w+$/, ""); // remove extension
@@ -17,11 +32,11 @@ const workArray = JSON.parse(app.doShellScript('curl -s "https://api.github.com/
 		const displayTitle = subsite.replaceAll("-", " ");
 
 		return {
-			"title": displayTitle,
-			"match": alfredMatcher (subsite),
-			"arg": `https://git-scm.com/docs/${subsite}`,
-			"uid": subsite,
+			title: displayTitle,
+			match: alfredMatcher(subsite),
+			arg: `https://git-scm.com/docs/${subsite}`,
+			uid: subsite,
 		};
 	});
 
-JSON.stringify({ items: workArray });
+JSON.stringify({ items: [...referenceDocs, ...gitBookPages] });
