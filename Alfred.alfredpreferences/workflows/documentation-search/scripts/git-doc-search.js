2ea89d2db4bebe6021ed093e95e634e66b6c7f93
🤖 automated <cron@job>
2023-06-18 08:00:00 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/documentation-search/scripts/git-doc-search.js
+++ Alfred.alfredpreferences/workflows/documentation-search/scripts/git-doc-search.js
@@ -4,7 +4,7 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 
 const progitBookURL = "https://git-scm.com/book/en/v2"; // cannot use book's git repo since files do not match URLs
-const referenceDocsURL = "https://api.github.com/repos/git/git/git/trees/master?recursive=1";
+const referenceDocsURL = "https://git-scm.com/docs";
 const ahrefRegex = /.*?href="(.*?)">(.*?)<.*/i;
 
 //──────────────────────────────────────────────────────────────────────────────
@@ -14,9 +14,9 @@ const progitBookPages = app
 	.split("\r")
 	.slice(190, 600) // cut header/footer from html
 	.filter((line) => line.includes("a href")) // only links
-	.map((link) => {
-		const url = link.replace(ahrefRegex, "$1").replaceAll("%3F", "?");
-		const title = link.replace(ahrefRegex, "$2");
+	.map((line) => {
+		const url = line.replace(ahrefRegex, "$1").replaceAll("%3F", "?");
+		const title = line.replace(ahrefRegex, "$2");
 		if (!url) return {};
 		const subsite = url.slice(12, -title.length).replaceAll("-", " ");
 
@@ -28,24 +28,25 @@ const progitBookPages = app
 		};
 	});
 
-const referenceDocs = JSON.parse(app.doShellScript(`curl -sL "${referenceDocsURL}"`))
-	.tree.filter(
-		(/** @type {{ path: string; }} */ file) =>
-			file.path.startsWith("Documentation/") && !file.path.includes("/RelNotes/"),
-	)
-	.map((/** @type {{ path: string; }} */ file) => {
-		const subsite = file.path
-			.slice(14) // eslint-disable-line no-magic-numbers
-			.replace(/\.\w+$/, ""); // remove extension
 
-		const displayTitle = subsite.replaceAll("-", " ");
+const referenceDocs = app
+	.doShellScript(`curl -s "${referenceDocsURL}"`)
+	.split("\r")
+	.slice(250, 276) // cut header/footer from html
+	.filter((line) => line.includes("a href")) // only links
+	.map((line) => {
+		const url = line.replace(ahrefRegex, "$1");
+		const title = line.replace(ahrefRegex, "$2");
+		if (!url) return {};
+		const subsite = url.slice(6);
 
 		return {
-			title: displayTitle,
-			subtitle: "➡️",
-			arg: `https://git-scm.com/docs/${subsite}`,
-			uid: subsite,
+			title: title,
+			subtitle: "➡️ " + subsite,
+			arg: `https://git-scm.com/${url}`,
+			uid: url,
 		};
 	});
 
-JSON.stringify({ items: [...referenceDocs, ...progitBookPages] });
+// JSON.stringify({ items: [...referenceDocs, ...progitBookPages] });
+JSON.stringify({ items: referenceDocs });
