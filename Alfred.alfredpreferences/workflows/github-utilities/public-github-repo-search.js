9e343d8ce6b6570f8efb842a02506635cca0ceab
🤖 automated <cron@job>
2023-06-19 15:19:13 +0200

    iMac Home (46)

--- Alfred.alfredpreferences/workflows/github-utilities/public-github-repo-search.js
+++ Alfred.alfredpreferences/workflows/github-utilities/public-github-repo-search.js
@@ -1,32 +1,35 @@
 #!/usr/bin/env osascript -l JavaScript
-/* eslint-disable no-magic-numbers */
+
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+/** @param {string} str */
+function alfredMatcher(str) {
+	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
+	return [clean, str].join(" ") + " ";
+}
+
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @type {AlfredRun} */
+// rome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
-	ObjC.import("stdlib");
-	const app = Application.currentApplication();
-	app.includeStandardAdditions = true;
-
-	function alfredMatcher(str) {
-		const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
-		return [clean, str].join(" ") + " ";
-	}
-
-	//──────────────────────────────────────────────────────────────────────────────
-
 	const query = argv[0];
 	if (query.match(/^\s*$/)) return; // don't run for empty query
 	const apiURL = `https://api.github.com/search/repositories?q=${query}`;
 
 	const jsonArray = JSON.parse(app.doShellScript(`curl -sL "${apiURL}"`))
-		.items.filter(repo => !repo.fork && !repo.archived)
-		.map(repo => {
+		.items.filter((/** @type {{ fork: boolean; archived: boolean; }} */ repo) => !(repo.fork || repo.archived))
+		.map((/** @type {{ full_name: string; updated_at: string | number | Date; stargazers_count: any; description: string; html_url: any; open_issues: any; }} */ repo) => {
 			const name = repo.full_name.split("/")[1];
 
-			const daysAgo = Math.ceil((new Date() - new Date(repo.updated_at)) / 1000 / 3600 / 24);
+			const daysAgo = Math.ceil((+new Date() - +new Date(repo.updated_at)) / 1000 / 3600 / 24);
 			let updated =
 				daysAgo < 31 ? daysAgo.toString() + " days ago" : Math.ceil(daysAgo / 30).toString() + " months ago";
 			if (updated.startsWith("1 ")) updated = updated.replace("s ago", " ago"); // remove plural "s"
 
-			let subtitle = `★ ${repo.stargazers_count} – ${updated} – ${repo.description}`;
+			let subtitle = `★ ${repo.stargazers_count} – ${updated}`;
 			if (repo.description) subtitle += " – " + repo.description;
 
 			return {
