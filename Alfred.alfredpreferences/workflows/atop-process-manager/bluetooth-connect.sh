d530f3766f19f105d45b11c3c2a10c786d426089
🤖 automated <cron@job>
2023-06-19 12:58:54 +0200

    iMac Home (6)

--- Alfred.alfredpreferences/workflows/atop-process-manager/bluetooth-connect.sh
+++ Alfred.alfredpreferences/workflows/atop-process-manager/bluetooth-connect.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env zsh
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 
-address="$*"
-
-# toggle if `blueutil` is installed, open bluetooth settings otherwise
-if command -v blueutil &>/dev/null; then
-	if [[ $(blueutil --is-connected "$address") -eq 0 ]]; then
-		blueutil --connect "$address"
-	else
-		blueutil --disconnect "$address"
-	fi
-else
+# open bluetooth settings if blueutil is not installed
+if ! command -v blueutil &>/dev/null; then
 	open "x-apple.systempreferences:com.apple.BluetoothSettings"
+	return 0
+fi
+
+# toggle via `blueutil`
+address="$*"
+if [[ $(blueutil --is-connected "$address") -eq 0 ]]; then
+	blueutil --connect "$address"
+	echo "Connecting to $address"
+else
+	blueutil --disconnect "$address"
+	echo "Disconnecting to $address"
 fi
