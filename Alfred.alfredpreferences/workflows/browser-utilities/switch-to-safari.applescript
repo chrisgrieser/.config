2f3f64d2364b1dd659762f38ea09d65cd6bab6e2
🤖 automated <cron@job>
2023-06-19 18:19:13 +0200

    iMac Home (10)

--- Alfred.alfredpreferences/workflows/browser-utilities/switch-to-safari.applescript
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env osascript
-tell application "Brave Browser"
-	set currentTabUrl to URL of active tab of front window
-end tell
-
-tell application "Safari"
-	set the URL of the current tab of the front window to currentTabUrl
-	activate
-end tell
---
+++ Alfred.alfredpreferences/workflows/browser-utilities/switch-to-safari.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+//──────────────────────────────────────────────────────────────────────────────
+function browserTab() {
+	const frontmostAppName = Application("System Events").applicationProcesses.where({ frontmost: true }).name()[0];
+	const frontmostApp = Application(frontmostAppName);
+	const chromiumVariants = ["Google Chrome", "Chromium", "Opera", "Vivaldi", "Brave Browser", "Microsoft Edge", "Arc"];
+	let title, url;
+	if (chromiumVariants.some((appName) => frontmostAppName.startsWith(appName))) {
+		url = frontmostApp.windows[0].activeTab.url();
+		title = frontmostApp.windows[0].activeTab.name();
+	} else {
+		app.displayNotification("", { withTitle: "You need a supported browser as your frontmost app", subtitle: "" });
+		return;
+	}
+	return { url: url, title: title };
+}
+const url = browserTab().url;
+
+// open in Safari
+const safari = Application("Safari");
+safari.includeStandardAdditions = true;
+safari.openLocation(url);
