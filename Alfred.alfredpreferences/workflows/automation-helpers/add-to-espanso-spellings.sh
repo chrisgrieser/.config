7721a0582eb104eb818697e5d8539018856ab5f7
🤖 automated <cron@job>
2023-06-20 16:32:24 +0200

    mini Office (6)

--- Alfred.alfredpreferences/workflows/automation-helpers/add-to-espanso-spellings.sh
+++ Alfred.alfredpreferences/workflows/automation-helpers/add-to-espanso-spellings.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env zsh
 
+# CONFIG
 spell_config="$DOTFILE_FOLDER/espanso/match/spelling.yml"
+
+#───────────────────────────────────────────────────────────────────────────────
 
 wrong=$(echo "$*" | cut -d" " -f1)
 correct=$(echo "$*" | cut -d" " -f2)
 
-{ echo "  - trigger: $wrong" ;
-  echo "    replace: $correct" ;
-  echo "    propagate_casing: true" ;
-  echo "    word: true";        } >> "$spell_config"
+{
+	echo "  - trigger: $wrong"
+	echo "    replace: $correct"
+	echo "    propagate_casing: true"
+	echo "    word: true"
+} >>"$spell_config"
+
+# shellcheck disable=2154
+[[ "$open" == "false" ]] && return 0
+
+#───────────────────────────────────────────────────────────────────────────────
 
 LINE_NO=$(wc -l "$spell_config" | tr -s " " | cut -d" " -f2)
-
 # workaround for https://github.com/neovide/neovide/issues/1604
 # shellcheck disable=2086
 open "$spell_config" --env=LINE=$LINE_NO
-
