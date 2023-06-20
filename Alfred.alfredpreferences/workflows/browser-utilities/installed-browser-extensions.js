73cfcf3363c2de3f6a9630b95173b0b954b9e6cc
🤖 automated <cron@job>
2023-06-20 10:49:12 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/browser-utilities/installed-browser-extensions.js
+++ Alfred.alfredpreferences/workflows/browser-utilities/installed-browser-extensions.js
@@ -19,54 +19,58 @@ function readFile(path) {
 
 //──────────────────────────────────────────────────────────────────────────────
 
-const browserConfig = app.doShellScript("echo $BROWSER_CONFIG").trim(); // defined in .zshenv
-const extensionFolder = app.pathTo("home folder") + `/Library/Application Support/${browserConfig}/Default/Extensions`;
+/** @type {AlfredRun} */
+// rome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run(argv) {
+	const browserDefaultsPath = argv[0];
+	const extensionFolder =
+		app.pathTo("home folder") + `/Library/Application Support/${browserDefaultsPath}/Default/Extensions`;
 
-const jsonArray = app
-	.doShellScript(`find "${extensionFolder}" -name "manifest.json"`)
-	.split("\r")
-	.map(manifestPath => {
-		const root = manifestPath.slice(0, -13); /* eslint-disable-line no-magic-numbers */
-		const id = root.replace(/.*Extensions\/(\w+)\/.*/, "$1");
-		const manifest = JSON.parse(readFile(manifestPath));
+	const extensions = app
+		.doShellScript(`find "${extensionFolder}" -name "manifest.json"`)
+		.split("\r")
+		.map((manifestPath) => {
+			const root = manifestPath.slice(0, -13);
+			const id = root.replace(/.*Extensions\/(\w+)\/.*/, "$1");
+			const manifest = JSON.parse(readFile(manifestPath));
 
-		// determine name
-		let name = manifest.name;
-		if (name.startsWith("__MSG_") && manifest.short_name) name = manifest.short_name;
-		if (name.startsWith("__MSG_")) {
-			const messagesJson = JSON.parse(readFile(root + "_locales/en/messages.json"));
-			if (messagesJson.extensionName?.message) name = messagesJson.extensionName.message;
-			else if (messagesJson.name?.message) name = messagesJson.name.message;
-			else if (messagesJson.extName?.message) name = messagesJson.extName.message;
-			else "[name not found]";
-		}
+			// determine name
+			let name = manifest.name;
+			if (name.startsWith("__MSG_") && manifest.short_name) name = manifest.short_name;
+			if (name.startsWith("__MSG_")) {
+				const messagesJson = JSON.parse(readFile(root + "_locales/en/messages.json"));
+				if (messagesJson.extensionName?.message) name = messagesJson.extensionName.message;
+				else if (messagesJson.name?.message) name = messagesJson.name.message;
+				else if (messagesJson.extName?.message) name = messagesJson.extName.message;
+				else "[name not found]";
+			}
 
-		// determine options path
-		let optionsPath = "";
-		if (manifest.options_ui?.page) optionsPath = manifest.options_ui.page;
-		else if (manifest.options_page) optionsPath = manifest.options_page;
-		const optionsUrl = `chrome-extension://${id}/${optionsPath}`;
-		const webstoreUrl = `https://chrome.google.com/webstore/detail/${id}`;
-		const localFolder = extensionFolder + "/" + id;
+			// determine options path
+			let optionsPath = "";
+			if (manifest.options_ui?.page) optionsPath = manifest.options_ui.page;
+			else if (manifest.options_page) optionsPath = manifest.options_page;
+			const optionsUrl = `chrome-extension://${id}/${optionsPath}`;
+			const webstoreUrl = `https://chrome.google.com/webstore/detail/${id}`;
+			const localFolder = extensionFolder + "/" + id;
 
-		// emoji/icon
-		const emoji = optionsPath ? "" : " 🚫"; // indicate no options available
-		let icon = manifest.icons["128"] || manifest.icons["64"];
-		if (!icon) icon = manifest.icons["48"]; // old extensions
-		const iconPath = root + icon;
+			// emoji/icon
+			const emoji = optionsPath ? "" : " 🚫"; // indicate no options available
+			const icon = manifest.icons["128"] || manifest.icons["64"] || manifest.icons["48"];
+			const iconPath = root + icon;
 
-		return {
-			title: name + emoji,
-			match: alfredMatcher(name),
-			icon: { path: iconPath },
-			valid: optionsPath !== "",
-			arg: optionsUrl,
-			mods: {
-				alt: { arg: webstoreUrl },
-				cmd: { arg: webstoreUrl },
-				ctrl: { arg: localFolder },
-			},
-			uid: id,
-		};
-	});
-JSON.stringify({ items: jsonArray });
+			return {
+				title: name + emoji,
+				match: alfredMatcher(name),
+				icon: { path: iconPath },
+				valid: optionsPath !== "",
+				arg: optionsUrl,
+				uid: id,
+				mods: {
+					alt: { arg: webstoreUrl },
+					cmd: { arg: webstoreUrl },
+					ctrl: { arg: localFolder },
+				},
+			};
+		});
+	return JSON.stringify({ items: extensions });
+}
