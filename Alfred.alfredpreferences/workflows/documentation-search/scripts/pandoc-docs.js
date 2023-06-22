53a1c532797b1e8f09253609c1d6932035588e01
🤖 automated <cron@job>
2023-06-22 10:26:29 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/documentation-search/scripts/pandoc-docs.js
+++ Alfred.alfredpreferences/workflows/documentation-search/scripts/pandoc-docs.js
@@ -7,20 +7,33 @@ const pandocDocsUrl = "https://pandoc.org/MANUAL.html";
 
 //──────────────────────────────────────────────────────────────────────────────
 
+let breadcrumbs = [];
+
 const sectionsArr = app
 	.doShellScript(`curl -s "${pandocDocsUrl}"`)
-	.split(">") // split by tags, not lines, since html formatting breaks up some tags across lines
-	.filter((tag) => tag.includes("<span id=") || tag.includes("<section id=")) 
-	.map((tag) => {
-		tag = tag.replaceAll("\r", "")
-		// const [_, name, level] = 
-		// const name = tag.match(/id="(.*?)"\s*class="(.*?)"/)[0]
-		// const url = `${pandocDocsUrl}#${name}`
-		const url = ""
+	.split(">") // INFO split by tags, not lines, since html formatting breaks up some tags across lines
+	.filter((htmlTag) => (htmlTag.includes("<span id=") || htmlTag.includes("<section id=")) && !htmlTag.includes('id="cb'))
+	.map((htmlTag) => {
+		htmlTag = htmlTag.replaceAll("\r", "");
+		const [_, name, levelStr] = htmlTag.match(/id="(.*?)"\s*class="(.*?)"/);
+		const url = `${pandocDocsUrl}#${name}`;
+		let displayName = name.replace(/^option--/, "--").replace(/-\d$/, "").replace(/^extension-/, "Extension: ");
+		displayName = displayName.charAt(0).toUpperCase() + displayName.slice(1); // capitalize
+
+		// construct breadcrumbs
+		const lvl = levelStr.match(/\d/) ? parseInt(levelStr.match(/\d/)[0]) : null;
+		if (lvl) { // options do not get a section level
+			breadcrumbs[lvl - 1] = displayName; // set current level
+			breadcrumbs = breadcrumbs.slice(0, lvl); // delete lower section levels
+		}
+		const parentsBreadcrumbs = breadcrumbs
+			.slice(0, -1) // remove last element, since it's the name
+			.join(" > ") // separator
+			.replace(/ > $/, ""); // trailing separator appears when heading levels are skipped
 
 		return {
-			title: tag,
-			// subtitle: level,
+			title: displayName,
+			subtitle: parentsBreadcrumbs,
 			arg: url,
 			uid: url,
 		};
