e1aedb8cbc2a0e9f249a075f77d8bc9ea5f76279
🤖 automated <cron@job>
2023-06-24 11:04:20 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/finder-utilities/files-in-trash.js
+++ Alfred.alfredpreferences/workflows/finder-utilities/files-in-trash.js
@@ -16,18 +16,6 @@ const home = app.pathTo("home folder");
 const trashLocation1 = home + "/.Trash";
 const trashLocation2 = home + "/Library/Mobile Documents/com~apple~CloudDocs/.Trash";
 
-//──────────────────────────────────────────────────────────────────────────────
-
-const dateAddedToTrash = app
-	.doShellScript("mdls -name kMDItemFSName -name kMDItemDateAdded ~/.Trash/* | sed 'N;s/\\n/ /'")
-	.split("\r")
-	.filter(line => !line.includes("(null)") && !line.includes(": could not find"))
-	.map(line => {
-		// kMDItemDateAdded = 2021-08-31 00:52:46 +0000 kMD
-		const rawDate = line.slice(19, 44).replaceAll(" +", "+")
-		return new Date(rawDate);
-	})
-
 const trashedFile = app
 	.doShellScript(`find "${trashLocation1}" "${trashLocation2}" -maxdepth 1 -mindepth 1`)
 	.split("\r")
