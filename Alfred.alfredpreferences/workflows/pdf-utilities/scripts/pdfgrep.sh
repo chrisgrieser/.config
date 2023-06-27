e137e951535a9ca6d3c18989dbf8f84c5a822d56
🤖 automated <cron@job>
2023-06-27 12:00:42 +0200

    iMac Home (33)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pdfgrep.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pdfgrep.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 
+cache="$alfred_workflow_cache/pdfpath"
 pdfpath=$(osascript "./scripts/get-pdf-path.applescript")
-osascript -l JavaScript "$*" "$pdfpath" 
+
+# shellcheck disable=2154
+[[ ! -d "$alfred_workflow_cache" ]] && mkdir -p "$alfred_workflow_cache"
+echo -n "$pdfpath" > "$alfred_workflow_cache/pdfpath"
+
+osascript -l JavaScript "./scripts/pdfgrep.js" "$*" "$(cat "$alfred_workflow_cache/pdfpath")" 
