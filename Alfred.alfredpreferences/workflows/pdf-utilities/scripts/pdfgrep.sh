81384a2c498cf48a6804f22bec52f388d38de907
🤖 automated <cron@job>
2023-06-27 14:30:42 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pdfgrep.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pdfgrep.sh
@@ -2,5 +2,15 @@
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 # shellcheck disable=2154
 
-pdfpath=$(osascript "./scripts/get-pdf-path.applescript")
+# get filename from Highlights.app
+pdfpath=$(osascript -e '
+	tell application "System Events"
+		tell process "Highlights"
+			if (count of windows) > 0 then set frontWindow to name of front window
+		end tell
+	end tell
+	return text item 1 of frontWindow
+'
+
+
 osascript -l JavaScript "./scripts/pdfgrep.js" "$*" "$pdfpath" 
