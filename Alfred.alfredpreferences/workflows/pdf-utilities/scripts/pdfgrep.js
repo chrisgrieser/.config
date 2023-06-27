a2945a5aafbb40e5300823d9c2439e15c88e6385
🤖 automated <cron@job>
2023-06-27 12:00:42 +0200

    iMac Home (33)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pdfgrep.js
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pdfgrep.js
@@ -8,7 +8,11 @@ app.includeStandardAdditions = true;
 /** @type {AlfredRun} */
 // rome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv){
-	const [pattern, pdfPath] = argv;
+	let [pattern, pdfPath] = argv;
+	console.log("pdfPath:", pdfPath);
+	console.log("pattern:", pattern);
+	if (pattern.length < 3) pattern = "qua"
+
 
 	/** @type AlfredItem[] */
 	const searchHits = app.doShellScript(`pdfgrep --ignore-case --page-number "${pattern}" "${pdfPath}"`)
