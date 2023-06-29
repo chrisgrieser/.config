d27875621c766ac1c98a413e1f3d77be1e8fd2fe
🤖 automated <cron@job>
2023-06-29 07:43:42 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/z_search-utilities/run-searchlink-and-copy.sh
+++ Alfred.alfredpreferences/workflows/z_search-utilities/run-searchlink-and-copy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 
-query="$*!!" # "!!" makes searchlink just get the link
-link="$(automator -r -i "$query" ~/Library/Services/SearchLink.workflow | sed -n 2p | cut -d\" -f2)"
+if ! command -v ddgr &>/dev/null; then echo "ddgr not installed." && return 1; fi
+
+link=$(ddgr --num=1 --noprompt --expand --nocolor "$1" | sed -n '2p' | xargs)
 echo -n "$link" | pbcopy
-echo -n "$link" # for notification
+echo -n "$link" # notification via Alfred
+
