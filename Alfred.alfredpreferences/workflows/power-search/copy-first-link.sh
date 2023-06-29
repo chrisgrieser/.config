53e91c290584a3b64bb92faac8aff352a4f2e724
🤖 automated <cron@job>
2023-06-29 09:06:53 +0200

    iMac Home (11)

--- Alfred.alfredpreferences/workflows/power-search/copy-first-link.sh
+++ Alfred.alfredpreferences/workflows/power-search/copy-first-link.sh
@@ -4,7 +4,7 @@ export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 if ! command -v ddgr &>/dev/null; then echo "ddgr not installed." && return 1; fi
 
 query="$*"
-link=$(ddgr --num=1 --json --expand --nocolor "$query" | grep "url" | cut -d'"' -f4)
-echo -n "$link" | pbcopy
-echo -n "$link" # notification via Alfred
+url=$(ddgr --num=1 --json "$query" | grep "url" | cut -d'"' -f4)
+echo -n "$url" | pbcopy
+echo -n "$url" # notification via Alfred
 
