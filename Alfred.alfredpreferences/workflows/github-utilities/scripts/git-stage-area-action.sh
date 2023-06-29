3f4d31f8b80b192d5d90553debefb32826ca3271
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-06-29 23:03:29 +0200

    chore

--- Alfred.alfredpreferences/workflows/github-utilities/scripts/git-stage-area-action.sh
+++ Alfred.alfredpreferences/workflows/github-utilities/scripts/git-stage-area-action.sh
@@ -6,16 +6,16 @@ cd "$repo" || return 1
 
 if [[ "$mode" == "stage" ]]; then
 	git add "$file"
+elif [[ "$mode" == "unstage" ]]; then
+	git restore --staged "$file"
+elif [[ "$mode" == "reset" ]]; then
+	git reset "$file"
 elif [[ "$mode" == "reveal" ]]; then
 	open -R "$file"
 elif [[ "$mode" == "undo unstaged" ]]; then
 	git restore "$file"
 elif [[ "$mode" == "undo staged" ]]; then
 	git reset HEAD "$file"
-elif [[ "$mode" == "unstage file" ]]; then
-	git restore --staged "$file"
-elif [[ "$mode" == "unstage change" ]]; then
-	git restore "$file"
 fi
 
 echo "$file"
