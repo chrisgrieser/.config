2d0d2ff5177118eea839b153da22d2a285d09dee
🤖 automated <cron@job>
2023-06-30 18:50:48 +0200

    iMac Home (2)

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -173,6 +173,7 @@ local config = {
 	detect_password_input = false,
 
 	-- Start/Close
+	default_cwd = wt.home_dir .. "/Library/Mobile Documents/com~apple~CloudDocs/File Hub/",
 	quit_when_all_windows_are_closed = true,
 	window_close_confirmation = "NeverPrompt",
 
