4e60babc8e339aa550b85d4a4c1a05818914c366
🤖 automated <cron@job>
2023-07-02 08:35:27 +0200

    iMac Home (5)

--- Alfred.alfredpreferences/workflows/browser-utilities/skip-paywall.js
+++ Alfred.alfredpreferences/workflows/browser-utilities/skip-paywall.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env osascript -l JavaScript
 
-const app = Application.currentApplication()
+const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 
-const frontmostAppName = Application("System Events").applicationProcesses.where({ frontmost: true }).name()[0];
+const frontmostAppName = Application("System Events")
+	.applicationProcesses.where({ frontmost: true })
+	.name()[0];
 const frontmostApp = Application(frontmostAppName);
+frontmostApp.includeStandardAdditions = true;
+
+//──────────────────────────────────────────────────────────────────────────────
 
 function browserTab() {
-	const chromiumVariants = ["Google Chrome", "Chromium", "Opera", "Vivaldi", "Brave Browser", "Microsoft Edge", "Arc"];
+	// rome-ignore format: A
+	const chromiumVariants = [ "Google Chrome", "Chromium", "Opera", "Vivaldi", "Brave Browser", "Microsoft Edge", "Arc" ];
 	const webkitVariants = ["Safari", "Webkit"];
 	let title, url;
 	if (chromiumVariants.some((appName) => frontmostAppName.startsWith(appName))) {
@@ -17,16 +23,33 @@ function browserTab() {
 		url = frontmostApp.documents[0].url();
 		title = frontmostApp.documents[0].name();
 	} else {
-		app.displayNotification("", { withTitle: "You need a supported browser as your frontmost app", subtitle: "" });
-		return;
+		app.displayNotification("", {
+			withTitle: "You need a supported browser as your frontmost app",
+			subtitle: "",
+		});
+		return null;
 	}
 	return { url: url, title: title };
 }
 
-const url = browserTab().url;
-const title = browserTab().title;
-frontmostApp.includeStandardAdditions = true;
-frontmostApp.openLocation("https://12ft.io/" + url);
-frontmostApp.openLocation("https://www.spaywall.com/search/" + url);
-frontmostApp.openLocation("https://www.google.com/search?q=" + encodeURIComponent(title));
+//──────────────────────────────────────────────────────────────────────────────
 
+/** @type {AlfredRun} */
+// rome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run() {
+	const tab = browserTab();
+	if (!tab) return;
+
+	const url = browserTab().url;
+	const title = browserTab().title;
+
+	// Google to get there via the first link maybe
+	frontmostApp.openLocation("https://www.google.com/search?q=" + encodeURIComponent(title));
+
+	// Paywall skippers
+	frontmostApp.openLocation("https://12ft.io/" + url);
+	frontmostApp.openLocation("https://www.spaywall.com/search/" + url);
+
+	// Reader Mode, if supported
+	frontmostApp.menuBars[0].menuBarItems.byName("View").menus[0].menuItems.byName("Enter Reader Mode").click();
+}
