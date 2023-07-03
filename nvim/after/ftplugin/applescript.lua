639b110ef56abeabea5c365349624c5b3f30a56b
🤖 automated <cron@job>
2023-07-03 10:01:36 +0200

    iMac Home (5)

--- nvim/after/ftplugin/applescript.lua
+++ nvim/after/ftplugin/applescript.lua
@@ -1,6 +1,4 @@
 local cmd = vim.cmd
-local expand = vim.fn.expand
-local fn = vim.fn
 local keymap = vim.keymap.set
 local u = require("config.utils")
 --------------------------------------------------------------------------------
@@ -17,14 +15,4 @@ keymap({ "n", "i", "x" }, "<D-s>", function()
 	cmd.loadview(2)
 	cmd.write()
 end, { buffer = true, desc = "Save & Format" })
-
---------------------------------------------------------------------------------
-
--- Build
-keymap("n", "<leader>r", function()
-	cmd.update()
-	local output = fn.system(('osascript "%s"'):format(expand("%:p")))
-	local logLevel = vim.v.shell_error > 0 and u.error or u.trace
-	vim.notify(output, logLevel)
-end, { buffer = true, desc = " AppleScript run" })
 
