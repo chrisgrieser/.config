8320aab7ca44cb1a175dbf823ae951a0f698c4e6
🤖 automated <cron@job>
2023-07-05 00:28:08 +0200

    iMac Home (7)

--- Alfred.alfredpreferences/workflows/atop-process-manager/scripts/eject-volume.sh
+++ Alfred.alfredpreferences/workflows/atop-process-manager/scripts/eject-volume.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 volume="$*"
-stdout=$(diskutil eject "$volume" 2>&1)
+msg=$(diskutil eject "$volume" 2>&1)
 
 # notification via Alfred
 # shellcheck disable=2181
-if [[ "$?" -eq 0 ]]; then
+if [[ $? -eq 0 ]]; then
 	echo "✅ $volume ejected"
 else
-	echo "❌ $stdout"
+	echo "❌ $msg"
 fi
