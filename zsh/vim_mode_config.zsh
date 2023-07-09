9eb2868b7766ad877fbc062dba32aef1ebafd85d
🤖 automated <cron@job>
2023-07-09 11:42:34 +0200

    iMac Home (3)

--- zsh/vim_mode_config.zsh
+++ zsh/vim_mode_config.zsh
@@ -1,28 +1,58 @@
 # shellcheck disable=2034
 #───────────────────────────────────────────────────────────────────────────────
-# INFO
-# available vi-mode widgets: `bindkey -M vicmd`
-# zsh-vi-mode config: https://github.com/jeffreytse/zsh-vi-mode#configuration-function
-#───────────────────────────────────────────────────────────────────────────────
 
+# https://github.com/jeffreytse/zsh-vi-mode#configuration-function
 function zvm_config() {
-	ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT # Always starting with insert mode for each command line
+	# Always starting with insert mode for each command line
+	ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
+
+	# cause I'm slow
+	ZVM_KEYTIMEOUT=0.666
+
+	# cursor styling with blinking
 	ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BEAM
 	ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BLOCK
 	ZVM_OPPEND_MODE_CURSOR=$ZVM_CURSOR_BLINKING_UNDERLINE
+	ZVM_VISUAL_MODE_CURSOR=$ZVM_CURSOR_BLINKING_UNDERLINE
+	ZVM_VISUAL_LINE_MODE_CURSOR=$ZVM_CURSOR_BLINKING_UNDERLINE
 }
 
-function zvm_after_lazy_keybindings() {
-	# zvm_define_widget my_custom_widget
+#───────────────────────────────────────────────────────────────────────────────
 
+# shellcheck disable=1091
+# has to run *after* zvm_config, but *before* zvm_after_lazy_keybindings
+source "$(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
+
+#───────────────────────────────────────────────────────────────────────────────
+
+# available vi-mode widgets: `bindkey -M vicmd`
+# https://github.com/jeffreytse/zsh-vi-mode#custom-widgets-and-keybindings
+
+# yank to system clipboard – https://stackoverflow.com/a/37411340
+# equivalent to `set clipboard=unnamed` (but only for y)
+function vi-yank-pbcopy {
+	zle vi-yank
+	echo "$CUTBUFFER" | pbcopy
+}
+zle -N vi-yank-pbcopy
+
+# q in normal mode exists the Terminal
+function normal-mode-exit { exit; }
+zle -N normal-mode-exit
+
+function zvm_after_lazy_keybindings() {
 	bindkey -M vicmd 'L' vi-end-of-line
 	bindkey -M vicmd 'H' vi-first-non-blank
 	bindkey -M vicmd 'U' redo
 	bindkey -M vicmd 'M' vi-join
 
-	bindkey -M vicmd 'gg' vi-beginning-of-line # so gg does not go to the top of history
-	bindkey -M vicmd -s 'Y' 'y$'               # -s flag sends direct keystrokes, to allow for remappings
-	bindkey -M vicmd -s 'X' 'mz$"_x`z'         # Remove last character from line
+	bindkey -M vicmd 'gg' vi-beginning-of-line # so gg does not go to the top of history, which you never want
+	bindkey -M vicmd 'q' normal-mode-exit      # quicker quitting
+	bindkey -M vicmd 'y' vi-yank-pbcopy        # so it copies to the system clipboard
+
+	# -s flag sends direct keystrokes, to allow for remappings
+	bindkey -M vicmd -s 'Y' 'y$'
+	bindkey -M vicmd -s 'X' 'mz$"_x`z' # Remove last character from line
+	bindkey -M vicmd -s '^W' 'daw'     # HACK set via Karabiner to <S-Space>
 	bindkey -M vicmd -s ' ' 'ciw'
-	bindkey -M vicmd -s '^W' 'daw' # set via Karabiner to <S-Space>
 }
