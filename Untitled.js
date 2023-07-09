0a4ef3299905f8c45ec715b911c5054db41193dc
🤖 automated <cron@job>
2023-07-09 08:00:00 +0200

    iMac Home (4)

--- Untitled.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env osascript -l JavaScript
-ObjC.import("stdlib");
-const app = Application.currentApplication();
-app.includeStandardAdditions = true;
-
-//──────────────────────────────────────────────────────────────────────────────
-
-const bi = Application("Finder").exists("tttt");
-Application("Finder").ffffffff()
