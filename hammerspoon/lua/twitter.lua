9ce11df2b14a40a92eab84ad679c63c1a70b7b00
🤖 automated <cron@job>
2023-07-10 09:25:20 +0200

    iMac Home (1)

--- hammerspoon/lua/twitter.lua
+++ hammerspoon/lua/twitter.lua
@@ -84,12 +84,19 @@ end
 ---@param referenceWin hs.window
 local function showHideTickerApp(referenceWin)
 	local app = u.app(env.tickerApp)
-	if not app or not referenceWin or u.isFront("CleanShot X") then return end
+	if
+		not app
+		or not referenceWin
+		or referenceWin:title() == "Login" -- login window max sized and triggers this as well
+		or u.isFront("CleanShot X")
+	then
+		return
+	end
 
 	if wu.CheckSize(referenceWin, wu.pseudoMax) or wu.CheckSize(referenceWin, wu.centered) then
 		winToTheSide()
 	elseif wu.CheckSize(referenceWin, wu.maximized) then
-		u.notify("Hiding Ivory due to: ", referenceWin:title())
+		u.notify("Hiding Ivory due to:", referenceWin:title())
 		app:hide()
 	end
 end
