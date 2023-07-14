951a554dc0b97c0970777a3d13c8c5ffe2cf5a3a
🤖 automated <cron@job>
2023-07-14 14:22:40 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/alfred-bibtex-citation-picker/scripts/paste-full-bibliography.sh
+++ Alfred.alfredpreferences/workflows/alfred-bibtex-citation-picker/scripts/paste-full-bibliography.sh
@@ -1,20 +1,21 @@
 #!/bin/zsh
-# shellcheck disable=SC2154
 export PATH=/usr/local/bin:/opt/homebrew/bin/:$PATH
-
-CSL=apa-6th-edition.csl
-CITEKEY="$*"
-LIBRARY="${bibtex_library_path/#\~/$HOME}"
-DUMMYDOC="---
-nocite: "\@$CITEKEY"
----"
 
 if ! command -v pandoc &>/dev/null; then
 	echo -n "You need to install pandoc for this feature."
 	return 0
 fi
 
-echo -n "$DUMMYDOC" |
+CSL="./apa-6th-edition.csl"
+CITEKEY="$*"
+# shellcheck disable=SC2154
+LIBRARY="${bibtex_library_path/#\~/$HOME}"
+DUMMYDOC="---
+nocite: '@$CITEKEY'
+---"
+
+reference=$(echo -n "$DUMMYDOC" |
 	pandoc --citeproc --read=markdown --write=plain --wrap=none \
-	--csl="$CSL" --bibliography="$LIBRARY" |
-	tr -d "\n"
+	--csl="$CSL" --bibliography="$LIBRARY")
+
+echo -n "$reference"
