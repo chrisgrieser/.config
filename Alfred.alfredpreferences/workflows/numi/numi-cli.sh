852450ee05d7ba00aa94f24e0dd55479300f35a3
🤖 automated <cron@job>
2023-07-14 22:22:40 +0200

    iMac Home (24)

--- Alfred.alfredpreferences/workflows/numi/numi-cli.sh
+++ Alfred.alfredpreferences/workflows/numi/numi-cli.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env zsh
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
-if command -v numi-cli &>/dev/null; then
-	result=$(numi-cli "$1")
-	subtitle="$1"
-	arg="$result"
-else
+
+if ! command -v numi-cli &>/dev/null; then
 	result="numi-cli not installed"
 	subtitle="↵ : Copy \`brew install nikolaeu/numi/numi-cli\` to the clipboard"
 	arg="brew install nikolaeu/numi/numi-cli"
+else
+	# shellcheck disable=2154
+	result=$(numi-cli --precision="$precision" -- "$1") # using `--` so negative numbers work
+	subtitle="$1"
+	arg="$result"
 fi
 
 cat <<EOF
