21dea2b5d28cb6017df6fc5d9541e7d424f21537
🤖 automated <cron@job>
2023-07-19 12:12:01 +0200

    iMac Home (3)

--- nvim/lua/config/options-and-autocmds.lua
+++ nvim/lua/config/options-and-autocmds.lua
@@ -237,30 +237,6 @@ autocmd("FileType", {
 })
 
 --------------------------------------------------------------------------------
--- FOLDING
-
--- Remember folds and cursor
-local function remember(mode)
-	-- stylua: ignore
-	local ignoredFts = { "TelescopePrompt", "DressingSelect", "DressingInput", "toggleterm", "gitcommit", "replacer", "harpoon", "help", "qf" }
-	if vim.tbl_contains(ignoredFts, bo.filetype) or bo.buftype ~= "" or not bo.modifiable then return end
-
-	if mode == "save" then
-		cmd.mkview(1)
-	else
-		pcall(function() cmd.loadview(1) end) -- pcall, since new files have no view yet
-	end
-end
-autocmd("BufWinLeave", {
-	pattern = "?*", -- pattern required, otherwise does not trigger
-	callback = function() remember("save") end,
-})
-autocmd("BufWinEnter", {
-	pattern = "?*",
-	callback = function() remember("load") end,
-})
-
---------------------------------------------------------------------------------
 
 -- Add missing buffer names, e.g. for status bar
 autocmd("FileType", {
