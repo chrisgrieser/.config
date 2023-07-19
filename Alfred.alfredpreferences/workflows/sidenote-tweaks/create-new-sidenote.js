0acda06debb91f7abffb2e06ea40ee80a62e7b63
🤖 automated <cron@job>
2023-07-19 14:15:28 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/sidenote-tweaks/create-new-sidenote.js
+++ Alfred.alfredpreferences/workflows/sidenote-tweaks/create-new-sidenote.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env osascript -l JavaScript
 
-const app = Application.currentApplication()
+const app = Application.currentApplication();
 app.includeStandardAdditions = true;
-ObjC.import("stdlib")
+ObjC.import("stdlib");
 const fileExists = (/** @type {string} */ filePath) => Application("Finder").exists(Path(filePath));
 const sidenotes = Application("Sidenotes");
 
@@ -12,7 +12,7 @@ const sidenotes = Application("Sidenotes");
 // rome-ignore lint/correctness/noUnusedVariables: Alfred
 function run(argv) {
 	const input = argv[0];
-	const baseFolder = sidenotes.folders.whose({"name": $.getenv("base_folder")})[0]() 
+	const baseFolder = sidenotes.folders.byName($.getenv("base_folder"));
 
 	// without the folder field, uses the setting from SideNotes to determine new
 	// note location
