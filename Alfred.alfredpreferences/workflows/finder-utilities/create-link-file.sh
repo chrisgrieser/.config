2a9fe193fdd529e662ce337bcdbc0b8037d19a49
🤖 automated <cron@job>
2023-07-20 22:57:51 +0200

    iMac Home (10)

--- Alfred.alfredpreferences/workflows/finder-utilities/create-link-file.sh
+++ Alfred.alfredpreferences/workflows/finder-utilities/create-link-file.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env zsh
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 
+#───────────────────────────────────────────────────────────────────────────────
+
 url="$*"
 title=$(curl -sL "$url" | grep -o "<title>[^<]*" | cut -d'>' -f2- | tr -d ":/\\,")
+[[ -z "$title" ]] && title="Untitled"
 
-# shellcheck disable=2154
-link_file_path="${default_folder/#\~/$HOME}/$title.url"
+source "$HOME/.zshenv" # sources $WD
+link_file_path="$WD/$title.url"
 
-{
-	echo "[InternetShortcut]"
-	echo "URL=$url"
-	echo "IconIndex=0"
-} >>"$link_file_path"
+#───────────────────────────────────────────────────────────────────────────────
+
+echo "[InternetShortcut]
+URL=$url
+IconIndex=0" >>"$link_file_path"
 
 open -R "$link_file_path"
