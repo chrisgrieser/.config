3d9594c41114abadada84c890142c1e84538046a
🤖 automated <cron@job>
2023-07-20 23:27:51 +0200

    iMac Home (10)

--- Alfred.alfredpreferences/workflows/finder-utilities/browse-finder-win-in-vidir.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
-finderWin=$(osascript -e 'tell application "Finder" to return POSIX path of (target of window 1 as alias)')
-
-alacritty --working-directory="$finderWin" --command vidir
--- Alfred.alfredpreferences/workflows/finder-utilities/wd-search.js
+++ Alfred.alfredpreferences/workflows/finder-utilities/wd-search.js
@@ -4,34 +4,30 @@ ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 
-/** @param {string} str */
-function alfredMatcher(str) {
-	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
-	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
-	return [clean, camelCaseSeperated, str].join(" ") + " ";
+/** @type {AlfredRun} */
+// rome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run(argv) {
+	const defaultFolder = argv[0];
+
+	const workArray = app
+		.doShellScript(`cd "${defaultFolder}" && find . -not -name ".DS_Store" -mindepth 1 -maxdepth 1`)
+		.split("\r")
+		.map((item) => {
+			const itemPath = defaultFolder + item.slice(1);
+			const extension = item.split(".").pop();
+			const fileName = item.slice(2)
+
+			const iconToDisplay = { path: itemPath };
+			const imageExtensions = ["png", "jpg", "jpeg", "gif", "icns", "tiff", "heic", "pdf"];
+			if (!imageExtensions.includes(extension)) iconToDisplay.type = "fileicon";
+
+			return {
+				title: fileName,
+				icon: iconToDisplay,
+				type: "file:skipcheck",
+				arg: itemPath,
+			};
+		});
+
+	return JSON.stringify({ items: workArray });
 }
-
-//──────────────────────────────────────────────────────────────────────────────
-const defaultFolder = $.getenv("default_folder").replace(/^~/, app.pathTo("home folder"));
-
-const workArray = app
-	.doShellScript(`cd "${defaultFolder}" && find . -not -name ".DS_Store" -mindepth 1 -maxdepth 1`)
-	.split("\r")
-	.map(item => {
-		const itemPath = defaultFolder + item.slice(1);
-		const extension = item.split(".").pop();
-
-		const iconToDisplay = { path: itemPath };
-		const imageExtensions = ["png", "jpg", "jpeg", "gif", "icns", "tiff", "heic", "pdf"];
-		if (!imageExtensions.includes(extension)) iconToDisplay.type = "fileicon";
-
-		return {
-			title: item.slice(2),
-			match: alfredMatcher(item),
-			type: "file:skipcheck",
-			arg: itemPath,
-			icon: iconToDisplay,
-		};
-	});
-
-JSON.stringify({ items: workArray });
