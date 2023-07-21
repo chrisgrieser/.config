55b7361e2ff31ecbb6938c711736e94b258970cd
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-07-21 18:26:56 +0200

    chore

--- Alfred.alfredpreferences/workflows/terminal-and-dotfiles/get-wezterm-pwd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-
-path_to_open="$*"
-current_cwd=$(wezterm cli list --format json | grep "cwd" | cut -d'"' -f4 | sed 's/%20/ /g' | sed -E 's|/$||' | sed -E 's/^file:.*local//')
-
-echo -n "$path_to_open"
