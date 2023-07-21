1a84c5631134c0c71b2dad423792de70ea1c91aa
🤖 automated <cron@job>
2023-07-21 12:48:07 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/shimmering-focus-helpers/switch-to-local-repo.sh
+++ Alfred.alfredpreferences/workflows/shimmering-focus-helpers/switch-to-local-repo.sh
@@ -6,11 +6,9 @@ remote_ssh="git@github.com:chrisgrieser/shimmering-focus.git"
 
 #───────────────────────────────────────────────────────────────────────────────
 
-[[ ! -d "$LOCAL_REPOS" ]] && mkdir -p "$LOCAL_REPOS"
+[[ ! -d "$LOCAL_REPOS" ]] && mkdir -p "$LOCAL_REPOS" 
 cd "$LOCAL_REPOS"
-git clone --depth=1 --filter="blob:none" "$remote_ssh"
-cd "shimmering-focus" || return 1
-ln -sf "$LOCAL_REPOS/shimmering-focus/source.css" "$theme_folder/theme.css"
 
-#───────────────────────────────────────────────────────────────────────────────
+git clone --depth=1 --filter="blob:none" "$remote_ssh"
+ln -sf "$LOCAL_REPOS/shimmering-focus/source.css" "$theme_folder/theme.css"
 
