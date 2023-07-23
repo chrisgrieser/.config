4838fadcb17b10a31cf30314e76e14e40583bc95
🤖 automated <cron@job>
2023-07-23 11:41:17 +0200

    iMac Mother (15)

--- Alfred.alfredpreferences/workflows/finder-utilities/scripts/create-link-file.js
+++ Alfred.alfredpreferences/workflows/finder-utilities/scripts/create-link-file.js
@@ -10,6 +10,26 @@ function writeToFile(filepath, text) {
 	str.writeToFileAtomicallyEncodingError(filepath, true, $.NSUTF8StringEncoding, null);
 }
 
+function browserTab() {
+	const frontmostAppName = Application("System Events").applicationProcesses.where({ frontmost: true }).name()[0];
+	const frontmostApp = Application(frontmostAppName);
+	const chromiumVariants = ["Google Chrome", "Chromium", "Opera", "Vivaldi", "Brave Browser", "Microsoft Edge", "Arc"];
+	const webkitVariants = ["Safari", "Webkit"];
+	let title, url;
+	if (chromiumVariants.some((appName) => frontmostAppName.startsWith(appName))) {
+		url = frontmostApp.windows[0].activeTab.url();
+		title = frontmostApp.windows[0].activeTab.name();
+	} else if (webkitVariants.some((appName) => frontmostAppName.startsWith(appName))) {
+		url = frontmostApp.documents[0].url();
+		title = frontmostApp.documents[0].name();
+	} else {
+		app.displayNotification("", { withTitle: "You need a supported browser as your frontmost app", subtitle: "" });
+		return;
+	}
+	return { url: url, title: title };
+}
+
+
 //──────────────────────────────────────────────────────────────────────────────
 
 /** @type {AlfredRun} */
