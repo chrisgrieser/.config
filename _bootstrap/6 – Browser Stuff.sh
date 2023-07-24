ea667a3822ed56103a5f14025d05e2f509d5d74e
🤖 automated <cron@job>
2023-07-24 14:28:21 +0200

    iMac Mother (5)

---
+++ _bootstrap/6 – Browser Stuff.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
+#───────────────────────────────────────────────────────────────────────────────
+
+# Import / Change settings
+open "chrome-extension://pncfbmialoiaghdehhbnbhkkgmjanfhe/pages/options.html"
+open "chrome-extension://hfjbmagddngcpeloejdejnfgbamkjaeg/pages/options.html"
+open "chrome-extension://jinjaccalgkegednnccohejagnlnfdag/options/index.html#settings"
+open "chrome-extension://epmaefhielclhlnmjofcdapbeepkmggh/options.html"
+open "chrome-extension://bijpdibkloghppkbmhcklkogpjaenfkg/html/options.html"
+open "chrome-extension://bgnkhhnnamicmpeenaelnjfhikgbkllg/pages/options.html"
+open "chrome-extension://gnmdbogfankgjepgglmmfmbnimcmcjle/optionPage/optionPage.html"
+open "chrome-extension://mnjggcdmjocbbbhaepdhchncahnbgone/options/options.html#import"
+open "chrome-extension://oldceeleldhonbafppcapldpdifcinji/options/options.html"
+
+# toggle local file URLs here
+open "brave://extensions/?id=hfjbmagddngcpeloejdejnfgbamkjaeg"
