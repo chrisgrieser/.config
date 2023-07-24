96441077dcd3b57ffe2568a340d1877750e75b11
🤖 automated <cron@job>
2023-07-25 00:13:13 +0200

    iMac Mother (4)

--- Alfred.alfredpreferences/workflows/user.workflow.C689FBB4-7694-4C21-B793-B7A9E5050E1A/hackernews-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.C689FBB4-7694-4C21-B793-B7A9E5050E1A/hackernews-search.js
@@ -17,30 +17,38 @@ function httpRequest(url) {
 // rome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
 	const query = argv[0];
-	if (!query) {
-		return JSON.stringify({
-			items: [ { title: "Waiting for query…", valid: false } ],
+			items: [{ title: "Waiting for query…", valid: false }],
 		});
 	}
-	const apiUrl = "https://hn.algolia.com/api/v1/search?query=";
+	const searchApi = "https://hn.algolia.com/api/v1/search?query=";
 
 	/** @type AlfredItem[] */
-	const news = JSON.parse(httpRequest(apiUrl + query)).hits.map((hit) => {
-		const hackernewsUrl = "https://news.ycombinator.com/item?id=" + hit.objectID;
-		const externalUrl = hit.url || hit.story_url || hackernewsUrl;
-		const comments = hit.num_comments ? ` (${hit.num_comments})` : "";
-		const subtitle = `${hit.points}${comments}  ·  ${externalUrl}`;
+	const news = JSON.parse(httpRequest(searchApi + query)).hits.map(
+		(
+			/** @type {{ created_at: string; objectID: string | number; url: any; story_url: any; num_comments: any; points: any; title: any; story_title: any; }} */ hit,
+		) => {
+			const useDstillAi = $.getenv("USE_DSTILL_AI") === "1";
+			const year = hit.created_at.substring(0, 4);
+			const baseUrl = useDstillAi
+				? "https://dstill.ai/hackernews/item/"
+				: "https://news.ycombinator.com/item?id=";
+			const hackernewsUrl = baseUrl + hit.objectID;
+			const externalUrl = hit.url || hit.story_url || hackernewsUrl;
+			const comments = hit.num_comments ? `  |  •${hit.num_comments}` : "";
+			const subtitle = `▲${hit.points}${comments}  |  ${year}`;
+			const title = hit.title || hit.story_title;
 
-		return {
-			title: hit.title,
-			subtitle: subtitle,
-			arg: hackernewsUrl,
-			mods: {
-				cmd: {
-					arg: hackernewsUrl,
+			return {
+				title: title,
+				subtitle: subtitle,
+				arg: hackernewsUrl,
+				mods: {
+					cmd: {
+						arg: externalUrl,
+					},
 				},
-			},
-		};
-	});
+			};
+		},
+	);
 	return JSON.stringify({ items: news });
 }
