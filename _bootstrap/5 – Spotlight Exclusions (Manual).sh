3185ada6446a380400bf7c54dabff680ddf30892
🤖 automated <cron@job>
2023-07-26 21:56:36 +0200

    iMac Home (5)

---
+++ _bootstrap/5 – Spotlight Exclusions (Manual).sh
@@ -0,0 +1,6 @@
+# TODO add manually to ensure ignorance by Spotlight
+open -R "/Applications/Cisco"
+open -R "/Applications/Utilities"
+open -R "$DATA_DIR/vim-data"
+open -R "$DATA_DIR/vim-data"
+open "x-apple.systempreferences:com.apple.preference.speech"
