c4352b02eb0045b30645dd65a52e8ac25e1060ee
🤖 automated <cron@job>
2023-07-27 11:49:21 +0200

    mini Office (5)

--- _bootstrap/5 – Spotlight Exclusions (Manual).sh
+++ _bootstrap/5 – Spotlight Exclusions (Manual).sh
@@ -1,6 +1,11 @@
-# TODO add manually to ensure ignorance by Spotlight
-open -R "/Applications/Cisco"
-open -R "/Applications/Utilities"
-open -R "$DATA_DIR/vim-data"
-open -R "$DATA_DIR/vim-data"
+# INFO manually add to ensure ignorance by Spotlight
+
+function revealIfExistent() {
+	[[ -e "$1" ]] && open -R "$1"
+}
+
+revealIfExistent "/Applications/Cisco"
+revealIfExistent "/Applications/Utilities"
+revealIfExistent "$DATA_DIR/vim-data"
+revealIfExistent "$DATA_DIR/Backups"
 open "x-apple.systempreferences:com.apple.preference.speech"
