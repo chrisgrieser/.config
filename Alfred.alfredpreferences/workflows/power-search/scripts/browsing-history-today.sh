24db680794b107958a4da0aee141e61461220234
🤖 automated <cron@job>
2023-07-30 20:11:28 +0200

    iMac Home (7)

--- Alfred.alfredpreferences/workflows/power-search/scripts/browsing-history-today.sh
+++ Alfred.alfredpreferences/workflows/power-search/scripts/browsing-history-today.sh
@@ -2,8 +2,12 @@
 
 today=$(date +%Y-%m-%d)
 
-# `!` negates a sed match, q quits. 
+# `!` negates a sed match, q quits.
 # -> Effectivlely, this reads the file until the *last* occurrence of $today
 
-# shellcheck disable=2154
-sed "/$today/!q" "$log_location" | cut -d" " -f2-
+# shellcheck disable=2154 # alfred var
+if [[ -f "$log_location" ]]; then
+	sed "/$today/!q" "$log_location" | cut -d" " -f2-
+else
+	echo "No log file found at $log_location."
+fi
