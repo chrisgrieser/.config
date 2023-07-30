9d4fb00414768c89bc87ba6c2c5747171df3b74b
🤖 automated <cron@job>
2023-07-30 13:27:48 +0200

    iMac Home (15)

--- Alfred.alfredpreferences/workflows/terminal-and-dotfiles/open-scratch-file.sh
+++ Alfred.alfredpreferences/workflows/terminal-and-dotfiles/open-scratch-file.sh
@@ -3,13 +3,13 @@
 if pgrep -xq "neovide"; then
 	osascript -e 'tell application "Neovide" to activate'
 else
-	neovide --geometry=104x33 --notabs "$@" &
+	neovide --geometry=104x33 &
 	disown # https://stackoverflow.com/a/20338584/22114136
 	while ! pgrep -xq "neovide"; do sleep 0.1; done
-	sleep 0.1
+	sleep 0.7
 fi
 
 # https://neovim.io/doc/user/remote.html#remote.txt
 # new file, nowrite-file, paste from system clipboard
 nvim --server "/tmp/nvim_server.pipe" \
-	--remote-send '<cmd>enew | set buftype=nowrite | file Scratchpad<CR>"+p'
+	--remote-send '<cmd>enew | set buftype=nowrite | lua vim.api.nvim_buf_set_name(0, "Scratchpad")<CR>"+p'
