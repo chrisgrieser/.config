1c45dea2a9500487bcb81d3fd292d7137685335d
🤖 automated <cron@job>
2023-07-30 10:57:48 +0200

    iMac Home (10)

--- Alfred.alfredpreferences/workflows/automation-helpers/neovim-debugger.sh
+++ Alfred.alfredpreferences/workflows/automation-helpers/neovim-debugger.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env zsh
-# shellcheck disable=2009
 
 mode="$*"
 
 if [[ "$mode" == "killall" ]]; then
-	killall neovide nvim osascript language_server_macos_x64 language_server_macos_arm
+	killall neovide nvim osascript
 	osascript -e 'display notification "" with title "⚔️ Killed all nvim-related processes."'
 elif [[ "$mode" == "signal" ]]; then
 	nvim --server "/tmp/nvim_server.pipe" --remote-send "<cmd>echo 'ping'<CR>"
 elif [[ "$mode" == "report" ]]; then
-	# send to alfred large type
+	# shellcheck disable=2009
 	processes="$(ps cAo 'pid,ppid,state,rss=MEM,command' |
 		grep -E "COMMAND|osascript|neovide|nvim|language.server|rome$|node" | 
 		sed '/^$/d')"
-	if [[ -n "$processes" ]]; then
-		echo -n "$processes"
+	if [[ $(echo -n "$processes" | wc -l) -gt 1 ]]; then
+		echo -n "$processes" # send to alfred large type
 	else
 		osascript -e 'display notification "" with title "⚔️ No nvim processes found."'
 	fi
