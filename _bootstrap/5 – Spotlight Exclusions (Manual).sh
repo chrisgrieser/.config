a52f969b99f96a7024976eb5288346a4ff5e8d0d
🤖 automated <cron@job>
2023-07-31 18:03:24 +0200

    iMac Home (2)

--- _bootstrap/5 – Spotlight Exclusions (Manual).sh
+++ _bootstrap/5 – Spotlight Exclusions (Manual).sh
@@ -1,11 +1,13 @@
 # INFO manually add to ensure ignorance by Spotlight
 
-function revealIfExistent() {
-	[[ -e "$1" ]] && open -R "$1"
-}
+function revealIfExistent() { [[ -e "$1" ]] && open -R "$1"; }
+
+#───────────────────────────────────────────────────────────────────────────────
 
 revealIfExistent "/Applications/Cisco"
 revealIfExistent "/Applications/Utilities"
 revealIfExistent "$DATA_DIR/vim-data"
 revealIfExistent "$DATA_DIR/Backups"
+revealIfExistent "/Users/chrisgrieser/Library/Mobile Documents/com~apple~CloudDocs/PDFs"
+
 open "x-apple.systempreferences:com.apple.preference.speech"
