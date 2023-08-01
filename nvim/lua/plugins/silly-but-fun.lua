dc5d5ca261d9a310357b413d6d2b84d2b8ea7d2c
🤖 automated <cron@job>
2023-08-01 15:06:34 +0200

    iMac Home (2)

--- nvim/lua/plugins/silly-but-fun.lua
+++ nvim/lua/plugins/silly-but-fun.lua
@@ -6,4 +6,14 @@ return {
 			{ "<leader>Ö", function() require("duck").cook("💀") end, desc = "󰇥 Cook Duck" },
 		},
 	},
+	{ -- simple screensaver (zone.nvim is nicer, but unfortunately buggy)
+		"folke/drop.nvim",
+		event = "VeryLazy",
+		opts = {
+			theme = "summer", -- stars|summer|spring|xmas|snow|leaves
+			max = 100, -- maximum number of drops on the screen
+			interval = 100, -- updates in ms
+			screensaver = (1000 * 60) * 5, -- starting time in 5 minutes
+		},
+	},
 }
