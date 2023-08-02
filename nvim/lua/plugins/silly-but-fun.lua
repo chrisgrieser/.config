d3b3b28f3f3fef1d7f19b4084ae7d36fd43223de
🤖 automated <cron@job>
2023-08-02 09:53:34 +0200

    iMac Home (3)

--- nvim/lua/plugins/silly-but-fun.lua
+++ nvim/lua/plugins/silly-but-fun.lua
@@ -1,6 +1,7 @@
 return {
-	{ -- simple screensaver (zone.nvim is nicer, but unfortunately buggy)
+	{ -- simple screensaver
 		"folke/drop.nvim",
+		enabled = false,
 		event = "VeryLazy",
 		opts = {
 			theme = "summer", -- stars|summer|spring|xmas|snow|leaves
@@ -9,24 +10,19 @@ return {
 			screensaver = (1000 * 60) * 10, -- starting time in 5 minutes
 		},
 	},
+	{ -- nicer screensaver, but a bit buggy
+		"tamton-aquib/zone.nvim",
+		event = "VeryLazy",
+		opts = {
+			style = "epilepsy", -- vanish|epilepsy|treadmill|dvd|matrix
+			after = 60, -- seconds?
+		},
+	},
 	{ -- just a duck
 		"tamton-aquib/duck.nvim",
 		keys = {
 			{ "<leader>zd", function() require("duck").hatch("🦆", 5) end, desc = "󰇥 Hatch Duck" },
 			{ "<leader>zc", function() require("duck").cook("💀") end, desc = "󰇥 Cook Duck" },
-		},
-	},
-	-- TODO
-	-- implement timer & autocmds for making this a screensaver
-	-- https://github.com/folke/drop.nvim/blob/main/lua/drop/config.lua#L184-L188
-	-- https://github.com/folke/drop.nvim/blob/main/lua/drop/config.lua#L140-L155
-	{ -- impressive animations
-		"Eandrju/cellular-automaton.nvim",
-		keys = {
-			-- stylua: ignore
-			{ "<leader>zr", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "󰯉 Make it Rain" },
-			-- stylua: ignore
-			{ "<leader>zg", "<cmd>CellularAutomaton game_of_life<CR>", desc = "󰯉 Game of Life" },
 		},
 		config = function()
 			require("which-key").register { mode = { "n" }, ["<leader>z"] = { name = "󰯉 Zilly Fun Stuff" } }
