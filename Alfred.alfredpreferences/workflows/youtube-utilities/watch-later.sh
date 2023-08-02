d75d86578f2fa3af905df81f39a80d1c7e94b58a
🤖 automated <cron@job>
2023-08-02 08:23:34 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/youtube-utilities/watch-later.sh
+++ Alfred.alfredpreferences/workflows/youtube-utilities/watch-later.sh
@@ -1,24 +1,23 @@
 #!/bin/zsh
 # shellcheck disable=SC2154
 
+if ! command -v fileicon &>/dev/null; then print "\033[1;33mfileicon not installed.\033[0m" && return 1; fi
+
 URL="$*"
 YOUTUBE_ID=$(echo "$URL" | cut -d "=" -f2)
-TITLE=$(curl -s "$URL" | grep -o "<title>[^<]*" | cut -d'>' -f2- | tr "/:" "--" | sed -e 's/ - YouTube//' | sed -e 's/amp;//g' )
+TITLE=$(curl -s "$URL" | grep -o "<title>[^<]*" | cut -d'>' -f2- | tr "/:" "--" | sed -e 's/ - YouTube//' | sed -e 's/amp;//g')
 IMAGE_URL="https://img.youtube.com/vi/$YOUTUBE_ID/0.jpg"
 
-# if non-existing folder (e.g. different device) save in working directory instead
-youtube_link_folder="${youtube_link_folder/#\~/$HOME}"
-if [[ -d "$youtube_link_folder" ]] ; then
-	BOOKMARK_PATH="${youtube_link_folder/#\~/$HOME}"
-else
-	BOOKMARK_PATH="${working_directory/#\~/$HOME}"
-fi
-BOOKMARK_PATH="$BOOKMARK_PATH/$TITLE.url"
+[[ ! -d "$youtube_link_folder" ]] && 
+	destination="$youtube_link_folder" || 
+	destination="$fallback_folder"
 
-# shellcheck disable=SC2028
-echo "[InternetShortcut]\nURL=$URL\nIconIndex=0" > "$BOOKMARK_PATH"
-curl -sL "$IMAGE_URL" > temp.jpg
-fileicon -q set "$BOOKMARK_PATH" temp.jpg
+#───────────────────────────────────────────────────────────────────────────────
+
+destination="$destination/$TITLE.url"
+print "[InternetShortcut]\nURL=$URL\nIconIndex=0" >"$destination"
+curl -sL "$IMAGE_URL" >temp.jpg
+fileicon -q set "$destination" temp.jpg
 rm -f temp.jpg
 
 # return for notification
