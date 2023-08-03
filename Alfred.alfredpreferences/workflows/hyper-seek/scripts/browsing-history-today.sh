c6c23c80ba543436d7a4d8b1513b8d5f27be82ef
🤖 automated <cron@job>
2023-08-03 15:46:17 +0200

    iMac Home (1)

--- Alfred.alfredpreferences/workflows/hyper-seek/scripts/browsing-history-today.sh
+++ Alfred.alfredpreferences/workflows/hyper-seek/scripts/browsing-history-today.sh
@@ -3,11 +3,14 @@
 today=$(date +%Y-%m-%d)
 
 # `!` negates a sed match, q quits.
-# -> Effectivlely, this reads the file until the *last* occurrence of $today
+# -> Effectivlely, this reads the file until the first occurrence of a line that
+# is not $today
 
 # shellcheck disable=2154 # alfred var
 if [[ -f "$log_location" ]]; then
-	sed "/$today/!q" "$log_location" | cut -d" " -f2-
+	sed "/$today/!q" "$log_location" | # read until it's not today
+		sed '$d' |                        # remove last line
+		cut -d" " -f2-                    # remove date
 else
 	echo "No log file found at $log_location."
 fi
