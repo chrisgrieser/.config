f26a9748fbb98a6f629a4cceef2d973a0c061269
🤖 automated <cron@job>
2023-08-03 19:57:59 +0200

    iMac Home (8)

--- nvim/lua/config/options-and-autocmds.lua
+++ nvim/lua/config/options-and-autocmds.lua
@@ -15,7 +15,7 @@ local api = vim.api
 -- nvim server (RPC) https://neovim.io/doc/user/remote.html
 
 -- avoid duplicate server when opening nvim in the terminal as well
-if vim.fn.has("gui_running") then
+if vim.fn.has("gui_running") == 1 then
 	local removed = pcall(os.remove, "/tmp/nvim_server.pipe") -- FIX server sometimes not properly shut down
 	local delay = removed and 500 or 0
 	vim.defer_fn(function() vim.fn.serverstart("/tmp/nvim_server.pipe") end, delay)
