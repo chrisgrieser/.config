d06aadfe0ee6a7481d8892f4f25ee77568f543a9
🤖 automated <cron@job>
2023-08-03 13:46:18 +0200

    iMac Home (12)

---
+++ Alfred.alfredpreferences/workflows/user.workflow.36286BD7-7D3A-438A-B2CC-F4AEFCD8B3DB/browse-subreddit.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+//──────────────────────────────────────────────────────────────────────────────
+
+// INFO free API calls restricted to 10 per minute
+// https://support.reddithelp.com/hc/en-us/articles/16160319875092-Reddit-Data-API-Wiki
+
+/** @type {AlfredRun} */
+// rome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run() {
+	const subreddit = $.getenv("selected_subreddit");
+	// INFO yes, curl is blocked only until you change the user agent, lol
+	const curlCommand = `curl -H "User-Agent: Chrome/115.0.0.0" "https://www.reddit.com/r/${subreddit}/new.json"`;
+	const response = JSON.parse(app.doShellScript(curlCommand));
+
+	// mostly too many requests
+	if (response.error) {
+		return JSON.stringify({
+			items: [{ title: response.message, subtitle: response.error }],
+		});
+	}
+
+	/** @type AlfredItem[] */
+	const redditPosts = response.data.children.map((/** @type {{ data: any; }} */ data) => {
+		const item = data.data;
+		const comments = item.num_comments;
+		const category = item.link_flair_text ? `[${item.link_flair_text}]` : "";
+		const subtitle = `${item.score}↑  ${comments}●  ${category}`;
+
+		return {
+			title: item.title,
+			subtitle: subtitle,
+			arg: item.url,
+			mods: {
+				shift: {
+					valid: false,
+					subtitle: `author: ${item.author}`,
+				},
+			},
+		};
+	});
+	return JSON.stringify({ items: redditPosts });
+}
