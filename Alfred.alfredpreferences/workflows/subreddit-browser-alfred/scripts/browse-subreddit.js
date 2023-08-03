3c8cdacb5577325b93becb95981266b2470cbc2f
🤖 automated <cron@job>
2023-08-03 21:27:59 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/subreddit-browser-alfred/scripts/browse-subreddit.js
+++ Alfred.alfredpreferences/workflows/subreddit-browser-alfred/scripts/browse-subreddit.js
@@ -5,6 +5,42 @@ app.includeStandardAdditions = true;
 
 const fileExists = (/** @type {string} */ filePath) => Application("Finder").exists(Path(filePath));
 
+/** @param {string} path */
+function readFile(path) {
+	const data = $.NSFileManager.defaultManager.contentsAtPath(path);
+	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
+	return ObjC.unwrap(str);
+}
+
+/** @param {string} filepath @param {string} text */
+function writeToFile(filepath, text) {
+	const str = $.NSString.alloc.initWithUTF8String(text);
+	str.writeToFileAtomicallyEncodingError(filepath, true, $.NSUTF8StringEncoding, null);
+}
+
+/** @param {string} path */
+function cacheIsOutdated(path) {
+	const cacheObj = Application("System Events").aliases[path];
+	const cacheAgeMins = (+new Date() - cacheObj.creationDate()) / 1000 / 60;
+	const cacheAgeThreshold = parseInt($.getenv("cache_age_threshold")) || 15;
+	return cacheAgeMins > cacheAgeThreshold;
+}
+
+function ensureCacheFolderExists() {
+	const finder = Application("Finder");
+	const cacheDir = $.getenv("alfred_workflow_cache");
+	if (!finder.exists(Path(cacheDir))) {
+		console.log("Cache Dir does not exist and is created.");
+		const cacheDirBasename = $.getenv("alfred_workflow_bundleid");
+		const cacheDirParent = cacheDir.slice(0, -cacheDirBasename.length);
+		finder.make({
+			new: "folder",
+			at: Path(cacheDirParent),
+			withProperties: { name: cacheDirBasename },
+		});
+	}
+}
+
 //──────────────────────────────────────────────────────────────────────────────
 
 // INFO free API calls restricted to 10 per minute
@@ -13,20 +49,34 @@ const fileExists = (/** @type {string} */ filePath) => Application("Finder").exi
 /** @type {AlfredRun} */
 // rome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	const subredditName = $.getenv("selected_subreddit");
-	// INFO yes, curl is blocked only until you change the user agent, lol
-	const curlCommand = `curl -sL -H "User-Agent: Chrome/115.0.0.0" "https://www.reddit.com/r/${subredditName}/new.json"`;
-	const response = JSON.parse(app.doShellScript(curlCommand));
+	// search passed subreddit, or if called directly, the top subreddit
+	const topSubreddit = $.getenv("subreddits").split("\n")[0];
+	const subredditName =
+		$.NSProcessInfo.processInfo.environment.objectForKey("selected_subreddit").js || topSubreddit;
+
+	const subredditCache = `${$.getenv("alfred_workflow_cache") + subredditName}.json`;
+	let response = {};
+
+	if (!fileExists(subredditCache) || cacheIsOutdated(subredditCache)) {
+		console.log("Writing new cache for " + subredditName);
+
+		// INFO yes, curl is blocked only until you change the user agent, lol
+		const curlCommand = `curl -sL -H "User-Agent: Chrome/115.0.0.0" "https://www.reddit.com/r/${subredditName}/new.json"`;
+		const responseStr = app.doShellScript(curlCommand);
+		response = JSON.parse(responseStr);
+
+		if (response.error) {
+			return JSON.stringify({ items: [{ title: response.message, subtitle: response.error }] });
+		}
+		ensureCacheFolderExists()
+		writeToFile(subredditCache, responseStr);
+	} else {
+		console.log("Using existing cache for " + subredditName);
+		response = JSON.parse(readFile(subredditCache));
+	}
 
 	let iconPath = `${$.getenv("alfred_workflow_data")}/${subredditName}.png`;
 	if (!fileExists(iconPath)) iconPath = "icon.png"; // not cached
-
-	// e.g., too many requests
-	if (response.error) {
-		return JSON.stringify({
-			items: [{ title: response.message, subtitle: response.error }],
-		});
-	}
 
 	/** @type AlfredItem[] */
 	const redditPosts = response.data.children.map((/** @type {{ data: any; }} */ data) => {
