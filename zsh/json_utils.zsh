50aa7379b5b39091d5827312eee128d1a6dcdbeb
🤖 automated <cron@job>
2023-08-05 13:55:44 +0200

    iMac Home (1)

--- zsh/json_utils.zsh
+++ zsh/json_utils.zsh
@@ -12,40 +12,58 @@ function json2yaml() {
 }
 
 #───────────────────────────────────────────────────────────────────────────────
+
+# json [s]chema
+function jsons() {
+	file_name=$(basename "$1" .json)
+	quicktype --lang=schema --out="$file_name.json" "$1"
+}
+
 # json e[x]plore (via pager)
 function jsonx() {
 	if ! command -v fx &>/dev/null; then print "\033[1;33mfx not installed.\033[0m" && return 1; fi
 	if ! [[ "$TERM_PROGRAM" == "WezTerm" ]]; then echo "Not using WezTerm." && return 1; fi
-
 	local url="$*"
-	curl --silent "$url" --output "/tmp/fx-curl.json"
-	pane_id=$(wezterm cli spawn -- fx "/tmp/fx-curl.json")
-	wezterm cli set-tab-title --pane-id="$pane_id" "curl-fx"
+
+	curl --silent "$url" --output "/tmp/jsonx.json"
+	pane_id=$(wezterm cli spawn -- fx "/tmp/jsonx.json") # open in new wezterm tab
+	wezterm cli set-tab-title --pane-id="$pane_id" "json explore"
 }
 
 # json [t]ype (as typescript)
 function jsont() {
 	if ! command -v quicktype &>/dev/null; then print "\033[1;33mquicktype not installed.\033[0m" && return 1; fi
 	if ! command -v bat &>/dev/null; then print "\033[1;33mbat not installed.\033[0m" && return 1; fi
-	
+	if ! [[ "$TERM_PROGRAM" == "WezTerm" ]]; then echo "Not using WezTerm." && return 1; fi
 	local url="$*"
-	quicktype "$url" --lang=typescript --just-types | bat --language=typescript --style=plain
+
+	# insert URL at top
+	echo -e "// $url\n" >"/tmp/jsont.ts" 
+
+	# insert types
+	types=$(quicktype "$url" --lang=typescript --just-types)
+	echo "$types" >>"/tmp/jsont.ts"
+
+	# open in new wezterm tab
+	pane_id=$(wezterm cli spawn -- bat "/tmp/jsont.ts")
+	wezterm cli set-tab-title --pane-id="$pane_id" "json types"
 }
 
 # json [g]rep
 function jsong() {
 	if ! command -v fastgron &>/dev/null; then print "\033[1;33m fastgron not installed.\033[0m" && return 1; fi
 	if ! command -v fzf &>/dev/null; then print "\033[1;33m fzf not installed.\033[0m" && return 1; fi
-	if ! command -v fzf &>/dev/null; then print "\033[1;33m fzf not installed.\033[0m" && return 1; fi
 
-	if [[ $# -ne 2 ]]; then
-		print "\033[1;33mUsage: jsong <search_term> <url>\033[0m"
-		return 1
-	fi
+	local url="$1"
+	local query="$2"
+	selection=$(fastgron --color --no-newline "$url" |
+		cut -c5- | # #cut the leading "json"
+		fzf --ansi --no-sort --exact --query="$query" --info=inline)
 
-	local query="$1"
-	local url="$2"
+	# no selection made -> no exit 130
+	[[ -z "$selection" ]] && return 0 
 
-	fastgron --color "$url" | rg --color=never "$query" | fzf --ansi
+	# selection made -> copy
+	echo -n "$selection" | pbcopy
+	echo "Copied: $selection"
 }
-
