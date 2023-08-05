be83be56e88a93d14f175ab3b5a570217bcc68cb
🤖 automated <cron@job>
2023-08-06 00:26:33 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/homebrew-utilities/scripts/build-brew-list.js
+++ Alfred.alfredpreferences/workflows/homebrew-utilities/scripts/build-brew-list.js
@@ -23,13 +23,29 @@ const fileExists = (/** @type {string} */ filePath) => Application("Finder").exi
 
 //──────────────────────────────────────────────────────────────────────────────
 
+/** @typedef {object} Formula
+ * @property {string} name
+ * @property {string[]} dependencies
+ * @property {string} caveats
+ * @property {string} desc
+ * @property {string} homepage
+ */
+
+/** @typedef {object} Cask
+ * @property {string} token
+ * @property {string} desc
+ * @property {string} homepage
+ */
+
+//──────────────────────────────────────────────────────────────────────────────
+
 /** @type {AlfredRun} */
 // rome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-
 	// INFO https://formulae.brew.sh/docs/api/
 	// these files contain as payload the API response of casks and formulas; they
-	// are updated on each `brew update`
+	// are updated on each `brew update`. Since they are effectively caches,
+	// there is no need create caches on my own
 	const home = app.pathTo("home folder");
 	const caskJson = home + "/Library/Caches/Homebrew/api/cask.jws.json";
 	const formulaJson = home + "/Library/Caches/Homebrew/api/formula.jws.json";
@@ -47,43 +63,50 @@ function run() {
 	//───────────────────────────────────────────────────────────────────────────
 
 	/** @type{AlfredItem[]} */
-	const casks = JSON.parse(casksRaw).map(
-		( cask) => {
-			const name = cask.token;
-			const installedIcon = installedBrews.includes(name) ? " ✅" : "";
-			// const dependencies = cask.dependencies.length > 0 ? ` +${cask.dependencies.length} ` : "";
-			return {
-				title: name + installedIcon,
-				match: alfredMatcher(name) + cask.desc,
-				subtitle: `${caskIcon} ${cask.desc}`,
-				arg: `${name} --cask`,
-				mods: {
-					// PERF quicker to pass here than to call `brew home` on brew-id
-					cmd: { arg: cask.homepage }, 
+	const casks = JSON.parse(casksRaw).map((/** @type {Cask} */ cask) => {
+		const name = cask.token;
+		const installedIcon = installedBrews.includes(name) ? " ✅" : "";
+		return {
+			title: name + installedIcon,
+			match: alfredMatcher(name) + cask.desc,
+			subtitle: `${caskIcon} · ${cask.desc}`,
+			arg: `${name} --cask`,
+			mods: {
+				// PERF quicker to pass here than to call `brew home` on brew-id
+				cmd: {
+					subtitle: "⌘: " + cask.homepage,
+					arg: cask.homepage,
 				},
-				uid: name,
-			};
-		},
-	);
+			},
+			uid: name,
+		};
+	});
 
 	/** @type{AlfredItem[]} */
-	const formulas = JSON.parse(formulaRaw).map(
-		( formula) => {
-			const name = formula.name;
-			const installedIcon = installedBrews.includes(name) ? " ✅" : "";
-			const dependencies = formula.dependencies.length > 0 ? ` +${formula.dependencies.length} ` : "";
-			return {
-				title: name + installedIcon,
-				match: alfredMatcher(name) + formula.desc,
-				subtitle: `${formulaIcon} ${dependencies} ${formula.desc}`,
-				arg: `${name} --formula`,
-				mods: {
-					cmd: { arg: formula.homepage },
+	const formulas = JSON.parse(formulaRaw).map((/** @type {Formula} */ formula) => {
+		const name = formula.name;
+		const installedIcon = installedBrews.includes(name) ? " ✅" : "";
+		const dependencies = formula.dependencies.length > 0 ? ` +${formula.dependencies.length} ` : "";
+		const caveats = formula.caveats || "";
+		const caveatIcon = caveats ? " ℹ️ " : "";
+		return {
+			title: name + installedIcon,
+			match: alfredMatcher(name) + formula.desc,
+			subtitle: `${formulaIcon}${dependencies}${caveatIcon} · ${formula.desc}`,
+			arg: `${name} --formula`,
+			text: {
+				largetype: caveats,
+				copy: caveats,
+			},
+			mods: {
+				cmd: {
+					subtitle: "⌘: " + formula.homepage,
+					arg: formula.homepage,
 				},
-				uid: name,
-			};
-		},
-	);
+			},
+			uid: name,
+		};
+	});
 
 	console.log(`${formulas.length} formulas, ${casks.length} casks`);
 	return JSON.stringify({ items: [...casks, ...formulas] });
