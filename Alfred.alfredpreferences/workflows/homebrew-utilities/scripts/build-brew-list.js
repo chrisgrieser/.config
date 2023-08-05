ef5edf1934693ca9cd9219ccdf3bde9783bdb7ba
🤖 automated <cron@job>
2023-08-05 23:56:33 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/homebrew-utilities/scripts/build-brew-list.js
+++ Alfred.alfredpreferences/workflows/homebrew-utilities/scripts/build-brew-list.js
@@ -5,6 +5,11 @@ app.includeStandardAdditions = true;
 
 //──────────────────────────────────────────────────────────────────────────────
 
+const caskIcon = "🍺";
+const formulaIcon = "🐚";
+
+//──────────────────────────────────────────────────────────────────────────────
+
 const alfredMatcher = (/** @type {string} */ str) => str.replaceAll("-", " ") + " " + str + " ";
 
 /** @param {string} path */
@@ -18,60 +23,68 @@ const fileExists = (/** @type {string} */ filePath) => Application("Finder").exi
 
 //──────────────────────────────────────────────────────────────────────────────
 
-// INFO https://formulae.brew.sh/docs/api/
-const home = app.pathTo("home folder");
-const caskJson = home + "/Library/Caches/Homebrew/api/cask.jws.json";
-const formulaJson = home + "/Library/Caches/Homebrew/api/formula.jws.json";
-
-// PERF `ls` quicker than `brew list`
-const installedBrews = app
-	.doShellScript("ls -1 /opt/homebrew/Cellar ; ls -1 /opt/homebrew/Caskroom")
-	.split("\r");
-
-//──────────────────────────────────────────────────────────────────────────────
-
 /** @type {AlfredRun} */
 // rome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
+
+	// INFO https://formulae.brew.sh/docs/api/
+	// these files contain as payload the API response of casks and formulas; they
+	// are updated on each `brew update`
+	const home = app.pathTo("home folder");
+	const caskJson = home + "/Library/Caches/Homebrew/api/cask.jws.json";
+	const formulaJson = home + "/Library/Caches/Homebrew/api/formula.jws.json";
+
+	// PERF `ls` quicker than `brew list`
+	const installedBrews = app
+		.doShellScript("ls -1 /opt/homebrew/Cellar ; ls -1 /opt/homebrew/Caskroom")
+		.split("\r");
+
 	if (!fileExists(formulaJson) || !fileExists(caskJson)) app.doShellScript("brew update");
 
 	const casksRaw = JSON.parse(readFile(caskJson)).payload;
-
-	/** @type{AlfredItem[]} */
-	const casks = JSON.parse(casksRaw).map((cask) => {
-		const name = cask.token;
-		const installedIcon = installedBrews.includes(name) ? " ✅" : "";
-		return {
-			title: name + installedIcon,
-			match: alfredMatcher(name),
-			subtitle: `💈 ${cask.desc}`,
-			arg: `${name} --cask`,
-			mods: {
-				cmd: { arg: cask.homepage },
-			},
-			uid: name,
-		};
-	});
-
 	const formulaRaw = JSON.parse(readFile(formulaJson)).payload;
 
+	//───────────────────────────────────────────────────────────────────────────
+
 	/** @type{AlfredItem[]} */
-	const formulas = JSON.parse(formulaRaw).map((formula) => {
-		const name = formula.name;
-		const installedIcon = installedBrews.includes(name) ? " ✅" : "";
-		return {
-			title: name + installedIcon,
-			match: alfredMatcher(name),
-			subtitle: `🍺 ${formula.desc}`,
-			arg: `${name} --formula`,
-			mods: {
-				cmd: { arg: formula.homepage },
-			},
-			uid: name,
-		};
-	});
+	const casks = JSON.parse(casksRaw).map(
+		( cask) => {
+			const name = cask.token;
+			const installedIcon = installedBrews.includes(name) ? " ✅" : "";
+			// const dependencies = cask.dependencies.length > 0 ? ` +${cask.dependencies.length} ` : "";
+			return {
+				title: name + installedIcon,
+				match: alfredMatcher(name) + cask.desc,
+				subtitle: `${caskIcon} ${cask.desc}`,
+				arg: `${name} --cask`,
+				mods: {
+					// PERF quicker to pass here than to call `brew home` on brew-id
+					cmd: { arg: cask.homepage }, 
+				},
+				uid: name,
+			};
+		},
+	);
 
-	const all = [...casks, ...formulas];
+	/** @type{AlfredItem[]} */
+	const formulas = JSON.parse(formulaRaw).map(
+		( formula) => {
+			const name = formula.name;
+			const installedIcon = installedBrews.includes(name) ? " ✅" : "";
+			const dependencies = formula.dependencies.length > 0 ? ` +${formula.dependencies.length} ` : "";
+			return {
+				title: name + installedIcon,
+				match: alfredMatcher(name) + formula.desc,
+				subtitle: `${formulaIcon} ${dependencies} ${formula.desc}`,
+				arg: `${name} --formula`,
+				mods: {
+					cmd: { arg: formula.homepage },
+				},
+				uid: name,
+			};
+		},
+	);
 
-	return JSON.stringify({ items: all });
+	console.log(`${formulas.length} formulas, ${casks.length} casks`);
+	return JSON.stringify({ items: [...casks, ...formulas] });
 }
