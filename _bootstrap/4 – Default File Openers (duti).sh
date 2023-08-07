0475a5481e1ed63eafb0dbd91504ed752c975db6
🤖 automated <cron@job>
2023-08-07 14:23:10 +0200

    iMac Home (46)

--- _bootstrap/4 – Default File Openers (duti).sh
+++ _bootstrap/4 – Default File Openers (duti).sh
@@ -3,20 +3,17 @@
 #───────────────────────────────────────────────────────────────────────────────
 brew install duti
 
-open "$DOTFILE_FOLDER/nvim/mac-helper/Neovim.app" # open once to initialize
-
-# App Choice
-videoplayerID="com.colliderli.iina"
-editorID="com.apple.automator.Neovim"
-obsiOpenerID="com.apple.automator.Obsidian-Opener"
-browserID=$(osascript -e "id of app \"$BROWSER_APP\"") # set in zshenv
-mailID=$(osascript -e "id of app \"$MAIL_APP\"") # set in zshenv
+# open once to initialize
+open "$DOTFILE_FOLDER/nvim/mac-helper/Neovide Helper.app" 
+open "$DOTFILE_FOLDER/obsidian/Obsidian Helper.app" 
 
 # Obsidian
+obsiOpenerID="com.apple.automator.Obsidian-Helper"
 duti -s "$obsiOpenerID" md all
 duti -s "$obsiOpenerID" canvas all
 
 # video & mp3
+videoplayerID="com.colliderli.iina"
 duti -s "$videoplayerID" vid all
 duti -s "$videoplayerID" mp4 all
 duti -s "$videoplayerID" mp3 all
@@ -27,7 +24,7 @@ duti -s "$videoplayerID" mkv all
 duti -s "$videoplayerID" m4a all
 
 # code
-editorID="com.apple.automator.Neovim"
+editorID="com.apple.automator.Neovide-Helper"
 duti -s "$editorID" public.make-source all
 duti -s "$editorID" conf all
 duti -s "$editorID" fdignore all
@@ -62,10 +59,13 @@ duti -s "$editorID" vimrc all
 duti -s "$editorID" sketchybarrc all
 
 # Browser & Mail
+browserID=$(osascript -e "id of app \"$BROWSER_APP\"") # set in zshenv
 duti -s "$browserID" chrome-extension
 duti -s "$browserID" chrome
 duti -s "$browserID" webloc all # link files
 duti -s "$browserID" url all    # link files
+
+mailID=$(osascript -e "id of app \"$MAIL_APP\"") # set in zshenv
 duti -s "$mailID" mailto # = default mail client
 
 # Misc
