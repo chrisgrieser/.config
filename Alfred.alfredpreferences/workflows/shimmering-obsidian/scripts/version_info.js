eb5b5a37285437f84b00e523324e6dfe2acb384e
🤖 automated <cron@job>
2023-08-11 08:00:04 +0200

    iMac Home (12)

--- Alfred.alfredpreferences/workflows/shimmering-obsidian/scripts/version_info.js
+++ Alfred.alfredpreferences/workflows/shimmering-obsidian/scripts/version_info.js
@@ -24,92 +24,96 @@ function logger(str) {
 
 /** @type {AlfredRun} */
 // rome-ignore lint/correctness/noUnusedVariables: Alfred run
-function run(){
-// input parameters
+function run() {
+	// input parameters
 
-const appTempPath = app.pathTo("home folder") + "/Library/Application Support/obsidian/";
-let obsiVer;
-try {
-	obsiVer = app
-		.doShellScript("cd '" + appTempPath + "'; ls *.asar | grep -Eo '(\\d|\\.)*'")
-		.slice(0, -1)
-		.replaceAll("\n", ";"); // multiple .asars for alpha testers
-} catch {
-	obsiVer = ".asar file missing";
-}
-const macVer = app.doShellScript("sw_vers -productVersion");
-const dotObsidian = fileExists(vaultPath + "/.obsidian/") ? "exists" : "does NOT exist";
+	const appTempPath = app.pathTo("home folder") + "/Library/Application Support/obsidian/";
+	let obsiVer;
+	try {
+		obsiVer = app
+			.doShellScript("cd '" + appTempPath + "'; ls *.asar | grep -Eo '(\\d|\\.)*'")
+			.slice(0, -1)
+			.replaceAll("\n", ";"); // multiple .asars for alpha testers
+	} catch {
+		obsiVer = ".asar file missing";
+	}
+	const macVer = app.doShellScript("sw_vers -productVersion");
+	const dotObsidian = fileExists(vaultPath + "/.obsidian/") ? "exists" : "does NOT exist";
 
-const advancedUriJSON = vaultPath + "/.obsidian/plugins/obsidian-advanced-uri/manifest.json";
-const advancedUriVer = fileExists(advancedUriJSON)
-	? JSON.parse(readFile(advancedUriJSON)).version
-	: "Advanced URI plugin not installed.";
-const advancedUriVerOnline = onlineJSON(
-	"https://github.com/Vinzent03/obsidian-advanced-uri/releases/latest/download/manifest.json",
-).version;
+	const advancedUriJSON = vaultPath + "/.obsidian/plugins/obsidian-advanced-uri/manifest.json";
+	const advancedUriVer = fileExists(advancedUriJSON)
+		? JSON.parse(readFile(advancedUriJSON)).version
+		: "Advanced URI plugin not installed.";
+	const advancedUriVerOnline = onlineJSON(
+		"https://github.com/Vinzent03/obsidian-advanced-uri/releases/latest/download/manifest.json",
+	).version;
 
-const metadataExJSON = vaultPath + "/.obsidian/plugins/metadata-extractor/manifest.json";
-const metadataExVer = fileExists(metadataExJSON)
-	? JSON.parse(readFile(metadataExJSON)).version
-	: "Metadata Extractor plugin not installed.";
-const metadataExVerOnline = onlineJSON(
-	"https://github.com/kometenstaub/metadata-extractor/releases/latest/download/manifest.json",
-).version;
+	const metadataExJSON = vaultPath + "/.obsidian/plugins/metadata-extractor/manifest.json";
+	const metadataExVer = fileExists(metadataExJSON)
+		? JSON.parse(readFile(metadataExJSON)).version
+		: "Metadata Extractor plugin not installed.";
+	const metadataExVerOnline = onlineJSON(
+		"https://github.com/kometenstaub/metadata-extractor/releases/latest/download/manifest.json",
+	).version;
 
-const obsiOnlineInfo = onlineJSON(
-	"https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/desktop-releases.json",
-);
-const obsiVerOnline = obsiOnlineInfo.latestVersion;
-const obsiVerBetaOnline = obsiOnlineInfo.beta.latestVersion;
+	const obsiOnlineInfo = onlineJSON(
+		"https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/desktop-releases.json",
+	);
+	const obsiVerOnline = obsiOnlineInfo.latestVersion;
+	const obsiVerBetaOnline = obsiOnlineInfo.beta.latestVersion;
 
-const workflowVerOnline = onlineJSON("https://api.github.com/repos/chrisgrieser/shimmering-obsidian/tags")[0].name;
+	const workflowVerOnline = onlineJSON(
+		"https://api.github.com/repos/chrisgrieser/shimmering-obsidian/tags",
+	)[0].name;
 
-const workspaceData15 = fileExists(vaultPath + "/.obsidian/workspace");
-const workspaceData16 = fileExists(vaultPath + "/.obsidian/workspace.json");
+	const workspaceData15 = fileExists(vaultPath + "/.obsidian/workspace");
+	const workspaceData16 = fileExists(vaultPath + "/.obsidian/workspace.json");
 
-const numberOfJSONS = app.doShellScript(
-	"ls '" +
-		vaultPath +
-		'/.obsidian/plugins/metadata-extractor/\' | grep ".json" | grep -v "manifest" | grep -v "^data" | wc -l | tr -d " "',
-);
-const metadataJSON = vaultPath + "/.obsidian/plugins/metadata-extractor/metadata.json";
-const metadataStrLen = fileExists(metadataJSON) ? readFile(metadataJSON).length : "no metadata.json";
+	const numberOfJSONS = app.doShellScript(
+		"ls '" +
+			vaultPath +
+			'/.obsidian/plugins/metadata-extractor/\' | grep ".json" | grep -v "manifest" | grep -v "^data" | wc -l | tr -d " "',
+	);
+	const metadataJSON = vaultPath + "/.obsidian/plugins/metadata-extractor/metadata.json";
+	const metadataStrLen = fileExists(metadataJSON) ? readFile(metadataJSON).length : "no metadata.json";
 
-//──────────────────────────────────────────────────────────────────────────────
+	//──────────────────────────────────────────────────────────────────────────────
 
-logger("");
-logger("-------------------------------");
-logger("INTERNAL WORKFLOW CONFIGURATION");
-logger("Vault Path: " + vaultPath);
-logger(".obsidian: " + dotObsidian);
-logger("Metadata JSONs: " + numberOfJSONS + "/4");
-if (numberOfJSONS < 4) logger("Not all metadata not found. Please run `osetup` and retry.");
-logger("metadata.json String Length: " + metadataStrLen);
-logger("-------------------------------");
-logger("WORKSPACE DATA");
-if (workspaceData15) logger("'workspace' exists");
-if (workspaceData16) logger("'workspace.json' exists");
-if (!workspaceData15 && !workspaceData16) logger("none exists");
-logger("-------------------------------");
-logger("SYSTEM");
-logger("macOS: " + macVer);
-logger("Alfred: " + $.getenv("alfred_version"));
-logger("-------------------------------");
-logger("INSTALLED VERSION");
-logger("Obsidian: " + obsiVer);
-logger("This Workflow: " + $.getenv("alfred_workflow_version"));
-logger("Advanced URI Plugin: " + advancedUriVer);
-logger("Metadata Extractor: " + metadataExVer);
-logger("-------------------------------");
-logger("LATEST VERSION");
-logger(`Obsidian: ${obsiVerOnline} (Insider: ${obsiVerBetaOnline})`);
-logger("This Workflow: " + workflowVerOnline);
-logger("Advanced URI Plugin: " + advancedUriVerOnline);
-logger("Metadata Extractor: " + metadataExVerOnline);
-logger("-------------------------------");
+	logger("");
+	logger("-------------------------------");
+	logger("INTERNAL WORKFLOW CONFIGURATION");
+	logger("Vault Path: " + vaultPath);
+	logger(".obsidian: " + dotObsidian);
+	logger("Metadata JSONs: " + numberOfJSONS + "/4");
+	if (numberOfJSONS < 4) logger("Not all metadata not found. Please run `osetup` and retry.");
+	logger("metadata.json String Length: " + metadataStrLen);
+	logger("-------------------------------");
+	logger("WORKSPACE DATA");
+	if (workspaceData15) logger("'workspace' exists");
+	if (workspaceData16) logger("'workspace.json' exists");
+	if (!workspaceData15 && !workspaceData16) logger("none exists");
+	logger("-------------------------------");
+	logger("SYSTEM");
+	logger("macOS: " + macVer);
+	logger("Alfred: " + $.getenv("alfred_version"));
+	logger("-------------------------------");
+	logger("INSTALLED VERSION");
+	logger("Obsidian: " + obsiVer);
+	logger("This Workflow: " + $.getenv("alfred_workflow_version"));
+	logger("Advanced URI Plugin: " + advancedUriVer);
+	logger("Metadata Extractor: " + metadataExVer);
+	logger("-------------------------------");
+	logger("LATEST VERSION");
+	logger(`Obsidian: ${obsiVerOnline} (Insider: ${obsiVerBetaOnline})`);
+	logger("This Workflow: " + workflowVerOnline);
+	logger("Advanced URI Plugin: " + advancedUriVerOnline);
+	logger("Metadata Extractor: " + metadataExVerOnline);
+	logger("-------------------------------");
 
-// remove config
-Application("com.runningwithcrayons.Alfred").removeConfiguration("ObRunning", { inWorkflow: $.getenv("alfred_workflow_bundleid") });
+	// remove config
+	Application("com.runningwithcrayons.Alfred").removeConfiguration("ObRunning", {
+		inWorkflow: $.getenv("alfred_workflow_bundleid"),
+	});
 
-return output; // JXA direct return
+	return output; // JXA direct return
 }
