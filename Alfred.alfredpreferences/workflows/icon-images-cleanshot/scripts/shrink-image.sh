577417f8018148e9f9ab8de4ffad255a134a175a
🤖 automated <cron@job>
2023-08-11 18:07:58 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/icon-images-cleanshot/scripts/shrink-image.sh
+++ Alfred.alfredpreferences/workflows/icon-images-cleanshot/scripts/shrink-image.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env zsh
 
-image="$*"
-width=$(sips --getProperty pixelWidth "$image" | tail -n1 | cut -d: -f2 | tr -d " ")
+images=("$@")
 
-# scale only works for division, therefore dividing by 1 https://askubuntu.com/a/754356
-new_width=$(echo "scale = 0; $width * 0.7 / 1" | bc) 
+for image in "${images[@]}"; do
+	width=$(sips --getProperty pixelWidth "$image" | tail -n1 | cut -d: -f2 | tr -d " ")
 
-#───────────────────────────────────────────────────────────────────────────────
+	# scale only works for division, therefore dividing by 1 https://askubuntu.com/a/754356
+	new_width=$(echo "scale = 0; $width * 0.7 / 1" | bc)
 
-filename_no_ext=${image%.*}
-ext=${image##*.}
+	filename_no_ext=${image%.*}
+	ext=${image##*.}
 
-sips -Z "$new_width" "$image" --out "${filename_no_ext}_70%.$ext"
+	sips -Z "$new_width" "$image" --out "${filename_no_ext}_70%.$ext"
+done
