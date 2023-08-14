0f50169b104a480c639c8d1e719369a4de23c8c6
🤖 automated <cron@job>
2023-08-14 22:46:59 +0200

    iMac Home (58)

--- Alfred.alfredpreferences/workflows/mastodon-utils/create-new-tweet-mastodon.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-# INFO Browser app needs to be forced, since Twitter.app takes over twitter urls
-# (BROWSER_APP from .zshenv)
-open -a "$BROWSER_APP" "https://twitter.com/compose/tweet"
-open "https://pkm.social/publish"
