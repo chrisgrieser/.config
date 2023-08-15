6645c518a4c0fa9a9c0c985fa483b04b6b51638e
🤖 automated <cron@job>
2023-08-15 21:52:25 +0200

    iMac Home (5)

---
+++ nvim/after/ftplugin/bla.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+//──────────────────────────────────────────────────────────────────────────────
+
