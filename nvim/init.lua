7e00de9459b571ff1c7b1473ec6f4046611e7406
🤖 automated <cron@job>
2023-08-17 16:49:58 +0200

    mini Office (3)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
----try to require the module, and do not error when one of them cannot be
+---try to require the module, and do not error out when one of them cannot be
 ---loaded, but do notify if there was an error.
 ---@param module string module to load
 local function safeRequire(module)
@@ -25,9 +25,10 @@ vim.loader.enable() -- TODO will be nvim default in later versions
 
 safeRequire("config.lazy")
 if vim.fn.has("gui_running") == 1 then safeRequire("config.gui-settings") end
-
 safeRequire("config.theme-customization")
 safeRequire("config.options-and-autocmds")
+
+reOpen()
 
 safeRequire("config.keybindings")
 safeRequire("config.leader-keybindings")
@@ -35,6 +36,3 @@ safeRequire("config.textobject-keymaps")
 
 safeRequire("config.user-commands")
 safeRequire("config.abbreviations")
-
-reOpen()
-
