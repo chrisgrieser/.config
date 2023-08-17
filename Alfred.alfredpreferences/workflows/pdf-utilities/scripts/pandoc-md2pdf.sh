58b2a7f2e6d72830935daaa40ac096eae56540bb
🤖 automated <cron@job>
2023-08-17 17:45:42 +0200

    mini Office (6)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pandoc-md2pdf.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pandoc-md2pdf.sh
@@ -4,7 +4,9 @@
 # INFO pandoc's --data-dir defined in .zshenv
 #───────────────────────────────────────────────────────────────────────────────
 
+# guard clauses
 if ! command -v wkhtmltopdf &>/dev/null; then echo "wkhtmltopdf not installed." && return 1; fi
+if ! command -v pandoc &>/dev/null; then echo "pandoc not installed." && return 1; fi
 
 INPUT="$*"
 OUTPUT="${INPUT%.*}_CG.pdf"
@@ -14,6 +16,9 @@ if [[ "$ext" != "md" ]]; then
 	echo "⚠️ Selection not a Markdown file."
 	return 1
 fi
+
+#───────────────────────────────────────────────────────────────────────────────
+osascript -e 'display notification "" with title "⏳ Running pandoc…"'
 
 pandoc "$INPUT" --output="$OUTPUT" --defaults="md2pdf" 2>&1 &&
 	open -R "$OUTPUT"
--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/run-ocrmypdf.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/run-ocrmypdf.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env zsh
 # shellcheck disable=2154
 
+if ! command -v ocrmypdf &>/dev/null; then
+	print "ocrmypdf not installed." 
+	return 1
+fi
+
+#───────────────────────────────────────────────────────────────────────────────
 lang="$*"
 path_no_ext=${input_path%.*}
 out_path="${path_no_ext}_ocr.pdf"
+
+osascript -e 'display notification "" with title "⏳ Running OCRmyPDF"'
 
 ocrmypdf --language="$lang" "$input_path" "$out_path"
 
