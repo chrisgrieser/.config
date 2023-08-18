3a3481e270f342d93179b2364bc7d632fe9e12d6
🤖 automated <cron@job>
2023-08-18 17:32:42 +0200

    iMac Home (10)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/copy-text-from-pdf.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/copy-text-from-pdf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-if ! command -v pdftotext &>/dev/null; then printf "\033[1;33mpdftotext (poppler) not installed.\033[0m" && return 1; fi
+if ! command -v pdftotext &>/dev/null; then printf "pdftotext (poppler) not installed." && return 1; fi
 
 pdfpath="$1"
 pdftotext "$pdfpath" - | pbcopy
