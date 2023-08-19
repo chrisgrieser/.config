628c282938ab2b95a1fc11e913eeca7de661f876
🤖 automated <cron@job>
2023-08-19 17:12:01 +0200

    iMac Home (58)

--- Alfred.alfredpreferences/workflows/youtube-and-pip/scripts/pip-in-iina.sh
+++ Alfred.alfredpreferences/workflows/youtube-and-pip/scripts/pip-in-iina.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env zsh
-
 if ! command -v iina &>/dev/null; then print "⚠️ iina-cli not installed." && return 1; fi
 
 #───────────────────────────────────────────────────────────────────────────────
@@ -18,12 +17,10 @@ fi
 
 #───────────────────────────────────────────────────────────────────────────────
 
+# open in IINA and float on top
 iina --no-stdin "$URL"
-tell application "System Events" to tell process "IINA" 
-	set frontmost to true 
-	try 
-		click menu item "Exit Full Screen" of menu "Video" of menu bar 1
-		delay 0.4 
-	end try 
-	click menu item "Float on Top" of menu "Video" of menu bar 1 
-end tell
+osascript -e '
+	tell application "System Events" to tell process "IINA" 
+		set frontmost to true 
+		click menu item "Float on Top" of menu "Video" of menu bar 1 
+	end tell'
