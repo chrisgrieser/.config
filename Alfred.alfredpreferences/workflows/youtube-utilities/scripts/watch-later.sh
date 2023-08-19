60e77a3d1b176c3975ac0364a69c3c64cdd6ff19
🤖 automated <cron@job>
2023-08-19 16:42:00 +0200

    iMac Home (21)

--- Alfred.alfredpreferences/workflows/youtube-utilities/scripts/watch-later.sh
+++ Alfred.alfredpreferences/workflows/youtube-utilities/scripts/watch-later.sh
@@ -3,7 +3,24 @@
 
 if ! command -v fileicon &>/dev/null; then print "\033[1;33mfileicon not installed.\033[0m" && return 1; fi
 
-URL="$*"
+#───────────────────────────────────────────────────────────────────────────────
+# shellcheck disable=1091
+
+
+# cannot use JXA to get browser URL, since sometimes a PWA is frontmost
+source "$HOME/.zshenv" # get BROWSER_APP env var
+URL=$(osascript -e "tell application \"$BROWSER_APP\" 
+	return URL of active tab of front window
+end tell")
+
+if [[ -z "$URL" ]]; then
+	echo -n "❌ Tab could not be retrieved."
+	return 1
+elif [[ ! "$URL" =~ "youtu" ]]; then # to match youtu.de and youtube.com
+	echo -n "❌ Not a YouTube URL."
+	return 1
+fi
+
 YOUTUBE_ID=$(echo "$URL" | cut -d "=" -f2)
 TITLE=$(curl -s "$URL" | grep -o "<title>[^<]*" | cut -d'>' -f2- | tr "/:" "--" | sed -e 's/ - YouTube//' | sed -e 's/amp;//g')
 IMAGE_URL="https://img.youtube.com/vi/$YOUTUBE_ID/0.jpg"
@@ -21,4 +38,4 @@ fileicon -q set "$destination" temp.jpg
 rm -f temp.jpg
 
 # return for notification
-echo -n "$TITLE"
+echo -n "🎞 Saved: $TITLE"
