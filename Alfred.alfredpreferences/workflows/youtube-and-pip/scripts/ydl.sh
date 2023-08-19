a8005ac9dc12df441c50a3cc55eb2738e92d325c
🤖 automated <cron@job>
2023-08-19 23:33:35 +0200

    iMac Home (6)

--- Alfred.alfredpreferences/workflows/youtube-and-pip/scripts/ydl.sh
+++ Alfred.alfredpreferences/workflows/youtube-and-pip/scripts/ydl.sh
@@ -3,22 +3,22 @@
 
 CURRENT_TAB=$(osascript -l JavaScript "./scripts/get-url-from-browser.js" &)
 
-if [[ -z "$CURRENT_TAB" ]] ; then
+if [[ -z "$CURRENT_TAB" ]]; then
 	echo -n "❌ Tab could not be retrieved."
 	return 1
-elif ! cd "$download_location" ; then
+elif [[ ! -d "$download_location" ]]; then
 	echo -n "❌ Invalid Download Location."
 	return 1
 fi
 
 osascript -e 'display notification "" with title "⏳ Starting Download…"'
 
-msg=$(yt-dlp "$CURRENT_TAB" 2>&1)
+msg=$(cd "$download_location" && yt-dlp --quiet "$CURRENT_TAB")
 
 # shellcheck disable=2181
-if [[ $? -eq 0 ]] ; then
+if [[ $? -eq 0 ]]; then
 	echo -n "✅ Download finished."
-elif ! [[ "$(yt-dlp -U)" =~ "up to date" ]]; then
+elif ! [[ "$(yt-dlp --update)" =~ "up to date" ]]; then
 	echo -n "ℹ️ yt-dlp not up to date."
 	echo -n "brew update && brew upgrade yt-dlp" | pbcopy
 else
