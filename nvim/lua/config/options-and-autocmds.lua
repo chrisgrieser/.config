2e2759c392e97ed5c78537bb76fee1dfdebb418b
🤖 automated <cron@job>
2023-08-20 20:29:51 +0200

    iMac Home (2)

--- nvim/lua/config/options-and-autocmds.lua
+++ nvim/lua/config/options-and-autocmds.lua
@@ -64,7 +64,7 @@ opt.signcolumn = "yes:1"
 
 -- Wrapping
 opt.textwidth = 80
-opt.wrapmargin = 3 -- extra space since using a scrollbar plugin
+opt.wrapmargin = 4 -- extra space since using a scrollbar plugin
 opt.wrap = false
 opt.breakindent = false
 opt.linebreak = true -- do not break up full words on wrap
