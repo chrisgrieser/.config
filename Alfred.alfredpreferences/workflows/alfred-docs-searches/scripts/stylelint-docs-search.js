22ec0e18740e02fc18787b7acfa3b739e48757af
🤖 automated <cron@job>
2023-08-22 15:12:52 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/stylelint-docs-search.js
+++ Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/stylelint-docs-search.js
@@ -5,8 +5,8 @@ app.includeStandardAdditions = true;
 
 /** @param {string} str */
 function alfredMatcher(str) {
-	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
-	return [clean, str].join(" ");
+	const clean = str.replaceAll("-", " ");
+	return [clean, str].join(" ") + " ";
 }
 
 //──────────────────────────────────────────────────────────────────────────────
@@ -14,20 +14,29 @@ function alfredMatcher(str) {
 /** @type {AlfredRun} */
 // rome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-
 	const githubApi = "https://api.github.com/repos/stylelint/stylelint/git/trees/main?recursive=1";
-	const baseURL = "https://stylelint.io/user-guide/rules";
-	const docsPathRegex = /^lib\/rules\/(.*)\/README\.md$/i;
+	const baseUrlRules = "https://stylelint.io/user-guide";
+	const ruleRegex = /^lib\/rules\/(.*)\/README\.md$/;
+	const userGuideRegex = /^docs\/user-guide\/(.*)\.md$/;
 
-	const rulesArray = JSON.parse(app.doShellScript(`curl -s "${githubApi}"`))
-		.tree.filter((/** @type {{ path: string; }} */ file) => docsPathRegex.test(file.path))
+	const siteArr = JSON.parse(app.doShellScript(`curl -s "${githubApi}"`))
+		.tree.filter(
+			(/** @type {{ path: string; }} */ file) => ruleRegex.test(file.path) || userGuideRegex.test(file.path),
+		)
 		.map((/** @type {{ path: string; }} */ entry) => {
-			const subsite = entry.path.replace(docsPathRegex, "$1");
-			const category = "rules"
-			const url = `${baseURL}/${subsite}`;
+			const path = entry.path;
+			const isRule = path.startsWith("lib");
+			const subsite = isRule ? path.replace(ruleRegex, "$1") : path.replace(userGuideRegex, "$1");
+			const category = isRule ? "rules" : "user guide";
+			const url = isRule ? `${baseUrlRules}/rules/${subsite}` : `${baseUrlRules}/${subsite}/`;
+			let displayTitle = subsite;
+			if (!isRule) {
+				displayTitle = subsite.replaceAll("-", " ");
+				displayTitle = displayTitle.charAt(0).toUpperCase() + displayTitle.slice(1);
+			}
 
 			return {
-				title: subsite,
+				title: displayTitle,
 				subtitle: category,
 				match: alfredMatcher(subsite),
 				arg: url,
@@ -35,5 +44,5 @@ function run() {
 			};
 		});
 
-	return JSON.stringify({ items: rulesArray });
+	return JSON.stringify({ items: siteArr });
 }
