982b8ee64e19ae9a002ef584c6b04226a18ce271
🤖 automated <cron@job>
2023-08-22 17:42:52 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/shimmering-focus-helpers/switch-to-local-repo.sh
+++ Alfred.alfredpreferences/workflows/shimmering-focus-helpers/switch-to-local-repo.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env zsh
-# shellcheck disable=2154,2164
+# shellcheck disable=2154
 
 theme_folder="$VAULT_PATH/.obsidian/themes/Shimmering Focus/"
 remote_ssh="git@github.com:chrisgrieser/shimmering-focus.git"
 
 #───────────────────────────────────────────────────────────────────────────────
 
-[[ ! -d "$LOCAL_REPOS" ]] && mkdir -p "$LOCAL_REPOS" 
-cd "$LOCAL_REPOS"
+[[ ! -d "$LOCAL_REPOS" ]] && mkdir -p "$LOCAL_REPOS"
+cd "$LOCAL_REPOS" || return 1
 
-git clone --depth=1 --filter="blob:none" "$remote_ssh"
+# WARN depth=2 ensures that amending a shallow commit does not result in a
+# new commit without parent, effectively destroying git history (!!)
+git clone --depth=2 --filter="blob:none" "$remote_ssh"
+
+# switch symlink
 ln -sf "$LOCAL_REPOS/shimmering-focus/source.css" "$theme_folder/theme.css"
 
-# run in background, since dependencies can be run in the background 
-cd "$LOCAL_REPOS/shimmering-focus/" || return 1
-npm install --production &
+osascript -e '
+	tell application id "com.runningwithcrayons.Alfred" to run trigger "loop" in workflow "de.chris-grieser.shimmering-focus"
+'
 
+# - install only #production, since the dev-depdencies are on my machine globally
+# installed and only listed in the package.json for documentation purposes
+cd "./shimmering-focus/" || return 1
+npm install --omit=dev
