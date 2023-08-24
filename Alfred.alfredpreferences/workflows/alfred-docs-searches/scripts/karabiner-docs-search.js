68db47a018da28c0c636e3f1107c36f3d5944dc3
🤖 automated <cron@job>
2023-08-24 12:42:50 +0200

    iMac Home (7)

--- Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/karabiner-docs-search.js
+++ Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/karabiner-docs-search.js
@@ -13,19 +13,19 @@ const alfredMatcher = (/** @type {string} */ str) => str.replace(/[-()_./]/g, " 
 function run() {
 	const githubApi =
 		"https://api.github.com/repos/pqrs-org/gh-pages-karabiner-elements.pqrs.org/git/trees/main?recursive=1";
+	const baseUrl = "https://karabiner-elements.pqrs.org/docs/json";
 
-	const workArray = JSON.parse(app.doShellScript(`curl -s "${githubApi}`))
+	const workArray = JSON.parse(app.doShellScript(`curl -s "${githubApi}"`))
 		.tree.filter(
 			(/** @type {{ path: string; }} */ file) =>
 				file.path.startsWith("docs/docs/json/") && file.path.endsWith("/index.html"),
 		)
 		.map((/** @type {{ path: string }} */ file) => {
-			// eslint-disable-next-line no-magic-numbers
 			const site = file.path.slice(15, -11); // remove "docs/docs/json/" and "/index.html"
 			const parts = site.split("/");
 			const subsite = parts.pop();
 			const parentSite = parts.join("/") + "/";
-			const url = "https://karabiner-elements.pqrs.org/docs/json/" + site;
+			const url = `${baseUrl}/${site}/`;
 			return {
 				title: subsite,
 				match: alfredMatcher(site),
