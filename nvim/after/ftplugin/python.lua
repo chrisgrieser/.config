7ba83c198dd441208fac9c92f613b01139da4c18
🤖 automated <cron@job>
2023-08-27 08:42:46 +0200

    iMac Home (5)

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -8,20 +8,14 @@ bo.shiftwidth = 4
 bo.tabstop = 4
 bo.softtabstop = 4
 
-vim.opt_local.listchars:append { tab = "󰌒 " } 
-vim.opt_local.listchars:append { lead = " " } 
+vim.opt_local.listchars:append { tab = "󰌒 " }
+vim.opt_local.listchars:append { lead = " " }
 -- python inline comments are separated by two spaces via `black`, so multispace
 -- only adds noise when displaying the dots for them
-vim.opt_local.listchars:append { multispace = " " } 
-
--- extra trailing char
-vim.keymap.set("n", "<leader>:", "mzA :<Esc>`z", { desc = " : to EoL", buffer = true })
+vim.opt_local.listchars:append { multispace = " " }
 
 -- fix habits
 abbr("<buffer> true True")
 abbr("<buffer> false False")
 abbr("<buffer> // #")
 abbr("<buffer> -- #")
-abbr("<buffer> else else:")
-
---------------------------------------------------------------------------------
