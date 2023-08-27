a3449005f86858420354ca6f66c076e65e6877b3
🤖 automated <cron@job>
2023-08-27 10:12:46 +0200

    iMac Home (6)

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -2,6 +2,10 @@ local bo = vim.bo
 local abbr = vim.cmd.inoreabbrev
 --------------------------------------------------------------------------------
 
+require("config.utils").applyTemplateIfEmptyFile("py")
+
+--------------------------------------------------------------------------------
+
 -- python standard
 bo.expandtab = true
 bo.shiftwidth = 4
@@ -13,6 +17,8 @@ vim.opt_local.listchars:append { lead = " " }
 -- python inline comments are separated by two spaces via `black`, so multispace
 -- only adds noise when displaying the dots for them
 vim.opt_local.listchars:append { multispace = " " }
+
+--------------------------------------------------------------------------------
 
 -- fix habits
 abbr("<buffer> true True")
