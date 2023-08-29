61f4084fb4471c3da7d956cca00cb560519f8f17
🤖 automated <cron@job>
2023-08-29 22:03:53 +0200

    iMac Home (5)

---
+++ nvim/lua/my-efm/linters/stylelint.lua
@@ -0,0 +1,13 @@
+local fs = require("efmls-configs.fs")
+
+local linter = "stylelint"
+local args = "--quiet --no-color --formatter compact --stdin --stdin-filename ${INPUT}"
+local command = string.format("%s %s", fs.executable(linter, fs.Scope.NODE), args)
+
+return {
+	prefix = linter,
+	lintCommand = command,
+	lintStdin = true,
+	lintFormats = { "%.%#: line %l, col %c, %trror - %m", "%.%#: line %l, col %c, %tarning - %m" },
+	rootMarkers = { ".stylelintrc" },
+}
