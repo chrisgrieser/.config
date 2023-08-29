2aa89db9249fc62cdba12ea2586779e26698fedc
🤖 automated <cron@job>
2023-08-29 10:33:53 +0200

    iMac Home (10)

---
+++ nvim/queries/comment/highlights.scm
@@ -0,0 +1,63 @@
+; Modification of the original highlights to add more common tags
+
+; DOCS 
+; Supposedly, this adding this file to the after directory should be enough to
+; extend the query. https://github.com/nvim-treesitter/nvim-treesitter#adding-queries
+;
+; However, this is (currently) not the case. Instead it seems
+; only possible to completely override the query. Therefore, this file is a copy
+; of the comment highlights, modified to include some changes I like to have.
+; https://github.com/nvim-treesitter/nvim-treesitter/blob/master/queries/comment/highlights.scm
+; 
+;───────────────────────────────────────────────────────────────────────────────
+
+; FOOBAR TEST ERROR
+; NOTE XXX PERF
+; BUG
+; TODO HACK
+
+;───────────────────────────────────────────────────────────────────────────────
+
+(_) @spell
+
+((tag
+  (name) @text.todo @nospell
+  ("(" @punctuation.bracket (user) @constant ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#any-of? @text.todo "TODO" "WIP"))
+
+("text" @text.todo @nospell
+ (#any-of? @text.todo "TODO" "WIP"))
+
+((tag
+  (name) @text.note @nospell
+  ("(" @punctuation.bracket (user) @constant ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#any-of? @text.note "NOTE" "XXX" "INFO" "DOCS" "PERF" "TEST"))
+
+("text" @text.note @nospell
+ (#any-of? @text.note "NOTE" "XXX" "INFO" "DOCS" "PERF" "TEST"))
+
+((tag
+  (name) @text.warning @nospell
+  ("(" @punctuation.bracket (user) @constant ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#any-of? @text.warning "HACK" "WARNING" "WARN" "FIX"))
+
+("text" @text.warning @nospell
+ (#any-of? @text.warning "HACK" "WARNING" "WARN" "FIX"))
+
+((tag
+  (name) @text.danger @nospell
+  ("(" @punctuation.bracket (user) @constant ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#any-of? @text.danger "FIXME" "BUG" "ERROR"))
+
+("text" @text.danger @nospell
+ (#any-of? @text.danger "FIXME" "BUG" "ERROR"))
+
+; Issue number (#123)
+("text" @number
+ (#lua-match? @number "^#[0-9]+$"))
+
+((uri) @text.uri @nospell)
