5cb3e152d223f8d7a149f29298363ba76ca9aded
🤖 automated <cron@job>
2023-08-30 01:33:53 +0200

    iMac Home (9)

--- nvim/lua/my-efm/formatters/shellcheck.lua
+++ nvim/lua/my-efm/formatters/shellcheck.lua
@@ -2,13 +2,13 @@ local fs = require("efmls-configs.fs")
 
 local formatter = "shellcheck"
 
--- Using `git apply` is the officially recommended to use shellcheck for auto-fixing
+-- Using `git apply` is the officially recommended way for auto-fixing
 -- https://github.com/koalaman/shellcheck/issues/1220#issuecomment-594811243
-local args = "--shell=bash --format=diff - | sed 's/-$/${INPUT}/' | git apply"
+local args = '--shell=bash --format=diff ${INPUT} | git apply'
 
 local command = string.format("%s %s", fs.executable(formatter), args)
 
 return {
 	formatCommand = command,
-	formatStdin = true,
+	formatStdin = false, -- BUG not working when disabling stdin
 }
