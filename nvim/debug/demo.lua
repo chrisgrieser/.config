c3607cb1c12f72464e62c7c5d3fae70d2a1d0028
🤖 automated <cron@job>
2023-09-01 19:09:47 +0200

    iMac Home (11)

--- nvim/debug/demo.lua
+++ nvim/debug/demo.lua
@@ -1,14 +1,14 @@
-print("hi")
 
 local numbers = { 1, 2, 3, 4, 5, 6 }
 local usedNumbers = {}
 local sum = 10
+local temp
 
 for _, v in ipairs(numbers) do
 	sum = sum + v
 	sum = sum / 2
 	table.insert(usedNumbers, v * 2)
 
-	local temp = vim.inspect(usedNumbers)
+	temp = vim.inspect(usedNumbers)
 	print(temp)
 end
