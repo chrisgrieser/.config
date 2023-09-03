06c8e8d1f67b0ec1adfecbdc3fecbe485c629d74
🤖 automated <cron@job>
2023-09-03 14:07:51 +0200

    iMac Home (3)

--- nvim/lua/tool-configs/formatters/bibtex-tidy.lua
+++ nvim/lua/tool-configs/formatters/bibtex-tidy.lua
@@ -1,13 +1,25 @@
 local fs = require("efmls-configs.fs")
 
 local formatter = "bibtex-tidy"
-local args = ""
-local command = string.format('%s %s', fs.executable(formatter, fs.Scope.NODE), args)
-local rangeFilter = "${--range-start:charStart} ${--range-end:charEnd}"
-
+local args = {
+	"--tab",
+	"--curly",
+	"--strip-enclosing-braces",
+	"--enclosing-braces=title,journal,booktitle",
+	"--numeric",
+	"--months",
+	"--no-align",
+	"--encode-urls",
+	"--duplicates",
+	"--drop-all-caps",
+	"--sort-fields",
+	"--remove-empty-fields",
+	"--no-wrap",
+}
+local command =
+	string.format("%s --quiet %s", fs.executable(formatter, fs.Scope.NODE), table.concat(args, " "))
 
 return {
-	formatCommand = rangeFilter .. command,
+	formatCommand = command,
 	formatStdin = true,
-	formatCanRange = true,
 }
