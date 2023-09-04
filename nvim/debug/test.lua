c76276145d6c0963d51f42088431a49e9b0e5d70
🤖 automated <cron@job>
2023-09-04 12:07:50 +0200

    iMac Home (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,6 +1,33 @@
 
-vim.notify("Foobar", vim.log.levels.INFO, { title = "" })
+---@param lines string[]
+---@param max_width number
+---@return string[]
+local function customWrap(lines, max_width)
+	local function split_length(line, width)
+		local text = {}
+		local next_line
+		while true do
+			if #line == 0 then return text end
+			next_line, line = line:sub(1, width), line:sub(width)
+			text[#text + 1] = next_line
+		end
+	end
 
+	local wrappedLines = {}
+	for _, line in pairs(lines) do
+		local new_lines = split_length(line, max_width)
+		new_lines = new_lines
+		for _, nl in ipairs(new_lines) do
+			nl = nl:gsub("^%s*", ""):gsub("%s*$", "")
+			table.insert(wrappedLines, " " .. nl .. " ")
+		end
+	end
+
+	return wrappedLines
+end
+
+local out = customWrap({ "Lorem ipsum dolor sit amet" }, 10)
+vim.notify(table.concat(out, "\n"))
 
 --------------------------------------------------------------------------------
 
