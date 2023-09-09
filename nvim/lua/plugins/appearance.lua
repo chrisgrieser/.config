96c3a6f7021510102f0741549a4bb6da965638ee
🤖 automated <cron@job>
2023-09-09 23:57:33 +0200

    iMac Home (2)

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -216,10 +216,12 @@ return {
 		"stevearc/dressing.nvim",
 		init = function()
 			-- lazy load triggers
+			---@diagnostic disable-next-line: duplicate-set-field
 			vim.ui.select = function(...)
 				require("lazy").load { plugins = { "dressing.nvim" } }
 				return vim.ui.select(...)
 			end
+			---@diagnostic disable-next-line: duplicate-set-field
 			vim.ui.input = function(...)
 				require("lazy").load { plugins = { "dressing.nvim" } }
 				return vim.ui.input(...)
