34ba92b2e8fa54660ddddcd76f5ff6fe2f90c5b6
🤖 automated <cron@job>
2023-09-09 23:27:33 +0200

    iMac Home (2)

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -4,15 +4,6 @@ local abbr = vim.cmd.inoreabbrev
 
 require("config.utils").applyTemplateIfEmptyFile("py")
 
-vim.keymap.set(
-	"n",
-	"<localleader>v",
-	"<cmd>VenvSelect<CR>",
-	{ buffer = true, desc = "󱥒  VenvSelect" }
-)
-
---------------------------------------------------------------------------------
-
 -- python standard
 bo.expandtab = true
 bo.shiftwidth = 4
@@ -21,6 +12,7 @@ bo.softtabstop = 4
 
 vim.opt_local.listchars:append { tab = "󰌒 " }
 vim.opt_local.listchars:append { lead = " " }
+
 -- python inline comments are separated by two spaces via `black`, so multispace
 -- only adds noise when displaying the dots for them
 vim.opt_local.listchars:append { multispace = " " }
