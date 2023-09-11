2afe31a2bd17c13b2c9c7c14a298432bd52c3c7b
🤖 automated <cron@job>
2023-09-11 13:22:35 +0200

    iMac Home (1)

--- zsh/node_npm.zsh
+++ zsh/node_npm.zsh
@@ -2,6 +2,8 @@
 # https://nodejs.org/api/repl.html#repl_environment_variable_options
 export NODE_REPL_HISTORY=""
 
+#───────────────────────────────────────────────────────────────────────────────
+
 # instead of writing npm config to ~/.npmrc, they can also be defined as shell
 # environment variables https://docs.npmjs.com/cli/v9/using-npm/config#environment-variables
 
