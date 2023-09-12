f67f1ed1f387108df5c7d6206548e42cc9702c37
🤖 automated <cron@job>
2023-09-12 21:06:36 +0200

    mini Office (5)

--- Alfred.alfredpreferences/workflows/word-changer-spelling-anycomplete/scripts/synonym.js
+++ Alfred.alfredpreferences/workflows/word-changer-spelling-anycomplete/scripts/synonym.js
@@ -1,22 +1,24 @@
 #!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
 
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @type {AlfredRun} */
+// biome-ignore lint/correctness/noUnusedVariables: <explanation>
 function run(argv) {
-	ObjC.import("stdlib");
-	const app = Application.currentApplication();
-	app.includeStandardAdditions = true;
-	//───────────────────────────────────────────────────────────────────────────
-
 	const baseURL = "https://api.datamuse.com/words?rel_syn=";
-	const query = argv.join("");
-	
-	const jsonArray = JSON.parse(app.doShellScript(`curl -s '${baseURL}${query}'`))
-		.map(item => {
-			return {
-				"title": item.word,
-				"subtitle": item.score,
-				"arg": item.word,
-			};
-		});
+	const query = argv[0].trim();
 
-	return JSON.stringify({ items: jsonArray });
+	const response = app.doShellScript(`curl -s '${baseURL}${query}'`);
+	const synonyms = JSON.parse(response).map((/** @type {{ word: string; score: number; }} */ item) => {
+		return {
+			title: item.word,
+			subtitle: item.score,
+			arg: item.word,
+		};
+	});
+
+	return JSON.stringify({ items: synonyms });
 }
