f7c10bebf8578e45df1a79d11311c94dd9f32f11
🤖 automated <cron@job>
2023-09-14 16:05:29 +0200

    mini Office (11)

--- _bootstrap/4 – Default File Openers (duti).sh
+++ _bootstrap/4 – Default File Openers (duti).sh
@@ -26,7 +26,6 @@ duti -s "$videoplayerID" m4a all
 # code
 editorID="com.apple.automator.Neovide-Helper"
 duti -s "$editorID" public.unix-executable all 
-duti -s "$editorID" org.n8gray.scheme-source all # treesitter
 duti -s "$editorID" scm all # treesitter
 duti -s "$editorID" add all # vim spell file
 duti -s "$editorID" com.apple.traditional-mac-plain-text all
