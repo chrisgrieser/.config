7ca91d65cac65f8effe6f50d2c1394da69209c74
🤖 automated <cron@job>
2023-09-15 18:00:28 +0200

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,2 +1,5 @@
-
-vim.notify("demo", vim.log.levels.TRACE, {title = "foobar"})
+local toolsToKeep = {"stylua"}
+local tools = require("mason-registry").get_installed_packages()
+tools = vim.tbl_filter(function(t) return not vim.tbl_contains(t.spec.categories, "LSP") end, tools)
+tools = vim.tbl_map(function(t) return t.name end, tools)
+local toUninstall = vim.tbl_filter(function(t) return not vim.tbl_contains(toolsToKeep, t) end, tools)
