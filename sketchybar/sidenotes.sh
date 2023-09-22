cebb0f8b9170f49131913afd6c87ee84c880efa5
🤖 automated <cron@job>
2023-09-22 13:52:07 +0200

    mini Office (4)

--- sketchybar/sidenotes.sh
+++ sketchybar/sidenotes.sh
@@ -4,14 +4,12 @@ export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 #───────────────────────────────────────────────────────────────────────────────
 # CONFIG
 folder="Base"
-preview_cutoff=9
+preview_cutoff_chars=9
 
 #───────────────────────────────────────────────────────────────────────────────
 
-note_count=$(osascript -l JavaScript -e "
-	Application('SideNotes').folders.byName(\"$folder\").notes().length
-")
-current_note=$(osascript -l JavaScript -e " Application('SideNotes').currentNote().text()")
+note_count=$(osascript -l JavaScript -e "Application('SideNotes').folders.byName('$folder').notes().length")
+current_note=$(osascript -l JavaScript -e "Application('SideNotes').currentNote().text()")
 
 if [[ $note_count -eq 0 ]]; then
 	icon=""
@@ -23,7 +21,9 @@ fi
 
 # add preview of note to status bar (similar to the OneThing app)
 if [[ $(echo "$current_note" | wc -l) -eq 1 ]]; then
-	[[ ${#current_note} -lt $preview_cutoff ]] && preview="$current_note" || preview="$(echo "$current_note" | cut -c-$preview_cutoff)…"
+	preview="$(echo -n "$current_note" |
+		sed -e 's/\[ ] //' -e 's/- //' |
+		cut -c-$preview_cutoff_chars)…"
 	label="$label $preview"
 fi
 
