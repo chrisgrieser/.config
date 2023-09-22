8ac034597c3b72e4557f1323bdf4c9770768d33d
🤖 automated <cron@job>
2023-09-22 14:22:07 +0200

    mini Office (2)

--- nvim/after/ftplugin/json.lua
+++ nvim/after/ftplugin/json.lua
@@ -1,29 +1,8 @@
-local cmd = vim.cmd
-local keymap = vim.keymap.set
 local u = require("config.utils")
 --------------------------------------------------------------------------------
 
 -- hides quotes, making it more readable
-vim.opt_local.conceallevel = 2 
-
---------------------------------------------------------------------------------
-
-local function leaveVisualMode()
-	local escKey = vim.api.nvim_replace_termcodes("<Esc>", false, true, true)
-	vim.api.nvim_feedkeys(escKey, "nx", false)
-end
-
--- escape stuff properly for VS Code Style snippet
-keymap("n", "<localleader>e", function()
-	u.normal("'[v']") -- select last paste
-	leaveVisualMode() -- -> sets '<,'> marks
-	cmd([['<,'>s/\\/\\\\/ge]]) -- escape the escaping backslashes
-	cmd([['<,'>s/"/\\"/ge]]) -- escape the double quotes
-	cmd([['<,'>s/^\(\s*\)\(.*\)/\1"\2",/e]]) -- surround non-whitespace with quotes and comma
-	cmd([['>s/,$//e]]) -- remove trailing comma at last line
-	u.normal("gv=") -- auto-indent everything
-end, { desc = " Escape Code Snippet", buffer = true })
-
+vim.opt_local.conceallevel = 2
 
 --------------------------------------------------------------------------------
 
