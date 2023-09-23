ffd7b8759525d06b16dfc4d10a24914e9ce366a7
🤖 automated <cron@job>
2023-09-23 17:26:11 +0200

    iMac Mother (15)

--- Alfred.alfredpreferences/workflows/user.workflow.CF354046-C536-4644-9807-2D44D9AC7516/search-for-link.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-query="$*"
-
-# PERF `--noua` disables user agent & fetches faster (~10% faster according to hyperfine)
-url=$(ddgr --num=1 --noua --unsafe --json "$query" | grep "url" | cut -d'"' -f4)
-mdlink="[$query]($url)"
-
-echo -n "$mdlink"
