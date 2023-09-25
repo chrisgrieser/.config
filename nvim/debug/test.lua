bcfe249d261ed890b6069ff6c1a89b4d5459938a
🤖 automated <cron@job>
2023-09-25 12:30:43 +0200

    iMac Home (7)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,12 +1,3 @@
-local installedTools = require("mason-registry").get_installed_packages()
-local nonLspInstalled = vim.tbl_filter(
-	function(t) return t.name == "vale" end,
-	installedTools
-)
-
-
-blaa = {}
-nonLspInstalled[1]:uninstall()
-bla = {
-
-}
+vim.notify("one two three", vim.log.levels.INFO, {
+	title = "title",
+})
