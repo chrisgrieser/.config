7c6220f68d92a2a7151052793f631079bb129819
🤖 automated <cron@job>
2023-09-26 18:02:02 +0200

    mini Office (4)

--- Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/neovim-hard-restart.sh
+++ Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/neovim-hard-restart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 killall -9 neovide nvim language_server_macos_arm language_server_macos_x86 osascript
 delay 0.5
-echo "Restarting neovim."
-
+echo -n "Restarting neovim." # Alfred notification
+# INFO not opening neovide here, since using osascript is sometimes buggy
