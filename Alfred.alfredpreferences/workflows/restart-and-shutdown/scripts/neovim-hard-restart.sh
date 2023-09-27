7378b5af2f56b83ada9a0c1ab78174aee451adb4
🤖 automated <cron@job>
2023-09-27 13:52:32 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/neovim-hard-restart.sh
+++ Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/neovim-hard-restart.sh
@@ -2,5 +2,6 @@
 killall -9 neovide nvim language_server_macos_arm language_server_macos_x86 osascript
 delay 0.5
 echo -n "Force restarting neovim…" # Alfred notification
+open -a "Neovide" # config reopens last file if no arg
 
 # INFO not opening neovide here, since using `osascript` is sometimes buggy
