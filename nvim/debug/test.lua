7b7e0040bb440433e07ba96731d628310d2eedfb
🤖 automated <cron@job>
2023-09-28 14:54:55 +0200

    mini Office (1)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -4,12 +4,14 @@ staging blubb
 *Pushing*
 ]]
 
-
 vim.notify(text, vim.log.levels.INFO, {
 	title = "title",
 	on_open = function(win)
 		local buf = vim.api.nvim_win_get_buf(win)
-		vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
-		vim.api.nvim_win_set_option(win, "conceallevel", 2)
-	end
+		local winNs = 4
+		vim.api.nvim_win_set_hl_ns(win, winNs)
+		local lastLine = vim.api.nvim_buf_line_count(buf)
+
+		vim.api.nvim_buf_add_highlight(buf, winNs, "ErrorMsg", lastLine - 2, 0, -1)
+	end,
 })
