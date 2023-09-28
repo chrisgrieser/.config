e23f803ddf6feac92683112f61d2f20ff24d27c3
🤖 automated <cron@job>
2023-09-28 14:24:55 +0200

    mini Office (2)

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -11,7 +11,8 @@ optl.tabstop = 4
 require("funcs.quality-of-life").wrap("on")
 
 -- decrease line length without zen mode plugins
-vim.defer_fn(function() optl.signcolumn = "yes:9" end, 1)
+-- workaround, since vim.opt_local.signcolumn does not work well for some reason
+vim.api.nvim_win_set_option(0, "signcolumn", "yes:9")
 
 -- do not auto-wrap text
 optl.formatoptions:remove { "t", "c" }
--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,6 +1,15 @@
-vim.notify("one two three", vim.log.levels.INFO, {
+local text = [[
+staging blubb
+*refactor*(scope): test message
+*Pushing*
+]]
 
-		title = "title",
+
+vim.notify(text, vim.log.levels.INFO, {
+	title = "title",
+	on_open = function(win)
+		local buf = vim.api.nvim_win_get_buf(win)
+		vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+		vim.api.nvim_win_set_option(win, "conceallevel", 2)
+	end
 })
-
-require("notify.config").max_width()
