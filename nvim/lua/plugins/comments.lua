384152363a697c5ac5a53c7d9895f23ef7a36ad9
🤖 automated <cron@job>
2023-10-05 12:38:52 +0200

    mini Office (7)

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -23,6 +23,11 @@ return {
 				desc = " Function Annotation",
 			},
 			{
+				"qF",
+				function() require("neogen").generate { type = "file" } end,
+				desc = " File Annotation",
+			},
+			{
 				"qt",
 				function() require("neogen").generate { type = "type" } end,
 				desc = " Type Annotation",
