460eaae13fff5696cfd9b96af63ed076dd2028cb
🤖 automated <cron@job>
2023-10-07 14:42:09 +0200

    iMac Home (5)

--- nvim/lua/plugins/git-plugins.lua
+++ nvim/lua/plugins/git-plugins.lua
@@ -15,7 +15,7 @@ return {
 			{ "<leader>gi", function() require("tinygit").issuesAndPrs { state = "open" } end, desc = " Open Issues" },
 			{ "<leader>gI", function() require("tinygit").issuesAndPrs { state = "closed" } end, desc = " Closed Issues" },
 			{ "<leader>gu", function() require("tinygit").githubUrl() end, mode = { "n", "x" }, desc = " GitHub Link" },
-			{ "<leader>G", function() require("tinygit.pickaxe").pickaxeCurrentFile() end, desc = "󰊢 Git Pickaxe" },
+			{ "<leader>gd", function() require("tinygit.pickaxe").currentFileHistory() end, desc = "󰢷 File History" },
 			-- stylua: ignore end
 		},
 	},
@@ -50,62 +50,5 @@ return {
 			max_file_length = 12000, -- lines
 			preview_config = { border = require("config.utils").borderStyle },
 		},
-	},
-	{ -- diff / merge / file history
-		"sindrets/diffview.nvim",
-		dependencies = "nvim-lua/plenary.nvim",
-		cmd = { "DiffviewOpen", "DiffviewFileHistory" },
-		keys = {
-			{
-				"<leader>gd",
-				function()
-					vim.ui.input({ prompt = "󰢷 Git Pickaxe (empty = full history)" }, function(pickaxe)
-						if not pickaxe then return end
-
-						local query = pickaxe ~= "" and (" -G'%s'"):format(pickaxe) or ""
-						vim.cmd("DiffviewFileHistory %" .. query)
-						vim.cmd.wincmd("w") -- go directly to file window
-						vim.cmd.wincmd("|") -- maximize it
-
-						-- directly search for the term
-						if pickaxe ~= "" then vim.fn.execute("/" .. pickaxe, "silent!") end
-					end)
-				end,
-				desc = "󰊢 Pickaxe File History",
-			},
-			{
-				"<leader>gd",
-				":DiffviewFileHistory<CR><C-w>w<C-w>|", -- requires `:` for '<'> marks
-				mode = "x",
-				desc = "󰊢 Line History (Diffview)",
-			},
-		},
-		config = function() -- needs config, for access to diffview.actions in mappings
-			require("diffview").setup {
-				-- https://github.com/sindrets/diffview.nvim#configuration
-				enhanced_diff_hl = false, -- true = no red for deletes
-				show_help_hints = false,
-				file_history_panel = {
-					win_config = { height = 5 },
-				},
-				hooks = {
-					diff_buf_read = function()
-						-- set buffername, mostly for tabline (lualine)
-						pcall(function() vim.api.nvim_buf_set_name(0, "Diffview") end)
-					end,
-				},
-				keymaps = {
-					view = {
-						{ "n", "<D-w>", vim.cmd.tabclose, {} }, -- close tab instead of window
-						{ "n", "<S-CR>", function() vim.cmd.wincmd("w") end, {} }, -- consistent with general buffer switcher
-					},
-					file_history_panel = {
-						{ "n", "<D-w>", vim.cmd.tabclose, {} },
-						{ "n", "?", require("diffview.actions").help("file_history_panel"), {} },
-						{ "n", "<S-CR>", function() vim.cmd.wincmd("w") end, {} },
-					},
-				},
-			}
-		end,
 	},
 }
