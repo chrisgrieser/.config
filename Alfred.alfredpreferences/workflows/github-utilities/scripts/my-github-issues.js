3774ebcc463359f7ad777bf12903973896a1466d
🤖 automated <cron@job>
2023-10-11 20:57:58 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/github-utilities/scripts/my-github-issues.js
+++ Alfred.alfredpreferences/workflows/github-utilities/scripts/my-github-issues.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env osascript -l JavaScript
+
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+/** @param {string} str */
+function alfredMatcher(str) {
+	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
+	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
+	return [clean, camelCaseSeperated, str].join(" ");
+}
+
+//──────────────────────────────────────────────────────────────────────────────
+
 // biome-ignore lint/correctness/noUnusedVariables: alfred_run
 function run() {
-	ObjC.import("stdlib");
-	const app = Application.currentApplication();
-	app.includeStandardAdditions = true;
-
-	/** @param {string} str */
-	function alfredMatcher(str) {
-		const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
-		const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
-		return [clean, camelCaseSeperated, str].join(" ");
-	}
-
-	//──────────────────────────────────────────────────────────────────────────────
-
 	const resultsNumber = $.getenv("results_number");
 	const username = $.getenv("github_username");
 	const apiURL = `https://api.github.com/search/issues?q=involves:${username}&per_page=${resultsNumber}`;
 
-	const issues = JSON.parse(app.doShellScript(`curl -sL "${apiURL}"`)).items.map(item => {
+	const issues = JSON.parse(app.doShellScript(`curl -sL "${apiURL}"`)).items.map((item) => {
 		const issueAuthor = item.user.login;
 		const authoredByMe = issueAuthor === username;
 
@@ -29,8 +30,8 @@ function run() {
 		const comments = item.comments > 0 ? "💬 " + item.comments.toString() : "";
 
 		let icon = authoredByMe ? "🚩 " : "";
-		if (item.state === "open" && isPR) icon += "🟦 ";
-		else if (item.state === "closed" && isPR && merged) icon += "🟨 ";
+		if (item.state === "open" && isPR) icon += "🟩 ";
+		else if (item.state === "closed" && isPR && merged) icon += "🟪 ";
 		else if (item.state === "closed" && isPR && !merged) icon += "🟥 ";
 		else if (item.state === "closed" && !isPR) icon += "🟣 ";
 		else if (item.state === "open" && !isPR) icon += "🟢 ";
