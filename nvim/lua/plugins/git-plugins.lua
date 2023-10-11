a6d63543bfcdc849f71cb1a2aeed791a74ca466c
🤖 automated <cron@job>
2023-10-11 17:28:20 +0200

    mini Office (5)

--- nvim/lua/plugins/git-plugins.lua
+++ nvim/lua/plugins/git-plugins.lua
@@ -23,6 +23,7 @@ return {
 		"lewis6991/gitsigns.nvim",
 		event = "VeryLazy",
 		keys = {
+			{ "<leader>ga", ":Gitsigns stage_hunk<CR>", mode = "x", desc = "󰊢 Stage Selected Hunks" },
 			{ "<leader>gy", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "󰊢 Unstage Last Hunk" },
 			{ "<leader>gA", "<cmd>Gitsigns stage_buffer<CR>", desc = "󰊢 Add Buffer" },
 			{ "<leader>gv", "<cmd>Gitsigns preview_hunk<CR>", desc = "󰊢 Preview Hunk Diff" },
