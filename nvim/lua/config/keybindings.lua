f8bb9d8cac477d4c81abe730af619c2a277ddc33
🤖 automated <cron@job>
2023-10-12 19:25:45 +0200

    mini Office (3)

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -226,3 +226,4 @@ keymap("x", "<D-e>", "<Esc>`<i`<Esc>`>la`<Esc>", { desc = "  Inline Code" 
 keymap("i", "<D-e>", "``<Left>", { desc = "  Inline Code" })
 
 --------------------------------------------------------------------------------
+-- fsfsfs
