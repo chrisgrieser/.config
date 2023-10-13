749ccb943d886ae6e47ace31c65cec2efa3d91cd
🤖 automated <cron@job>
2023-10-13 17:57:55 +0200

    iMac Home (5)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,15 +1,2 @@
-local function myFunc() print(debug.getinfo(1, "n").name) end
-
-myFunc()
 
 
-local function myFunc() print(debug.getinfo(1, "n").name) end
-
-local function myFunc1() print(debug.getinfo(1, "n").name) end
-local function myFunc2() print(debug.getinfo(1, "n").name) end
-local function myFunc3() print(debug.getinfo(1, "n").name) end
-local function myFunc4() print(debug.getinfo(1, "n").name) end
-
-local function myFunc5() print(debug.getinfo(1, "n").name) end
-
-local function myFunc6() print(debug.getinfo(1, "n").name) end
