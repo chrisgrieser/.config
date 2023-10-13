f9f5a8a0e082945846ace966cc4b506e8f23071e
🤖 automated <cron@job>
2023-10-13 10:27:56 +0200

    iMac Home (6)

--- nvim/lua/plugins/bulk-processing.lua
+++ nvim/lua/plugins/bulk-processing.lua
@@ -1,47 +1,7 @@
 local u = require("config.utils")
-
 --------------------------------------------------------------------------------
 
 return {
-	{ -- Multi Cursor
-		"mg979/vim-visual-multi",
-		keys = {
-			{ "<D-j>", mode = { "n", "x" }, desc = "󰆿 Multi-Cursor (Cursor Word)" },
-			{ "<D-a>", mode = { "n", "x" }, desc = "󰆿 Multi-Cursor (All)" },
-			{ "<D-down>", desc = "󰆿 Multi-Cursor (Down)" },
-		},
-		init = function()
-			vim.g.VM_set_statusline = 0 -- already using my version via lualine component
-			vim.g.VM_show_warnings = 0
-			vim.g.VM_silent_exit = 1
-			vim.g.VM_quit_after_leaving_insert_mode = 1 -- can use "reselect last" to restore
-			-- DOCS https://github.com/mg979/vim-visual-multi/blob/master/doc/vm-mappings.txt
-			vim.g.VM_maps = {
-				-- Enter Visual-Multi-Mode
-				["Find Under"] = "<D-j>", -- select word under cursor
-				["Reselect Last"] = "gV",
-				["Visual Add"] = "<D-j>",
-				["Select All"] = "<D-a>",
-				["Visual All"] = "<D-a>",
-				["Add Cursor Down"] = "<D-down>",
-
-				-- Visual-Multi-Mode Mappings
-				["Find Next"] = "<D-j>",
-				["Skip Region"] = "n", -- [n]o & find next
-				["Find Operator"] = "s", -- operator, selects all regions found in textobj
-				["Motion $"] = "L", -- use my HL motions here as well
-				["Motion ^"] = "H",
-			}
-		end,
-		config = function()
-			u.addToLuaLine("tabline", "lualine_z", function()
-				if not vim.b.VM_Selection then return "" end ---@diagnostic disable-line: undefined-field
-				local cursors = vim.b.VM_Selection.Regions
-				if not cursors then return "" end
-				return "󰇀 Visual-Multi (" .. tostring(#cursors) .. ")"
-			end)
-		end,
-	},
 	{ -- editable quickfix list
 		"gabrielpoca/replacer.nvim",
 		opts = { rename_files = false },
