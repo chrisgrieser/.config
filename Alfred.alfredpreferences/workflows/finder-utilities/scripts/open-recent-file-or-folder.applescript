a56742d3acf8d049e590ad866abc507244e8ec77
🤖 automated <cron@job>
2023-10-18 14:30:33 +0200

    mini Office (4)

--- Alfred.alfredpreferences/workflows/finder-utilities/scripts/open-recent-file-or-folder.applescript
+++ Alfred.alfredpreferences/workflows/finder-utilities/scripts/open-recent-file-or-folder.applescript
@@ -1,14 +1,12 @@
 #!/usr/bin/env osascript
 on run argv
 
+	set theType to (system attribute "type")
 	set itemID to (item 1 of argv as number)
-	set type to (system attribute "type")
-
-	tell application "Finder" to launch -- ensure Finder runs
 
 	tell application "System Events" to tell process "Finder"
 		set frontmost to true
-		if (type is "file") then
+		if (theType is "file") then
 			click menu item itemID of menu of menu item "Recent Items" of menu "Apple" of menu bar 1
 		else
 			click menu item itemID of menu of menu item "Recent Folders" of menu "Go" of menu bar 1
--- Alfred.alfredpreferences/workflows/finder-utilities/scripts/select-recent-file-or-folder.js
+++ Alfred.alfredpreferences/workflows/finder-utilities/scripts/select-recent-file-or-folder.js
@@ -15,63 +15,64 @@ function alfredMatcher(str) {
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	const recentItems = app
-		.doShellScript(
-			`osascript -e 'tell application "System Events" to tell (process 1 where frontmost is true) to set allItems to every menu item of menu of menu item "Recent Items" of menu 1 of menu bar 1'`,
-		)
-		.split(", ")
-		.map((item) => item.slice(10).split(" of ")[0]);
+	const recentFiles = Application("System Events")
+		.processes.byName("Finder")
+		.menuBars[0].menuBarItems.byName("Apple")
+		.menus[0].menuItems.byName("Recent Items")
+		.menus[0].menuItems.name();
+	const recentFilesStart = recentFiles.indexOf("Documents") + 1;
+	const recentFilesEnd = recentFiles.indexOf("Servers") - 1;
 
-	const start = recentItems.indexOf("Documents") + 1;
-	console.log("start: " + start);
-	const end = recentItems.indexOf("Servers") - 1;
-
-	let id = 0;
-	const recentFolders = app
-		.doShellScript(
-			`osascript -e 'tell application "System Events" to tell (process "Finder") to set allItems to every menu item of menu of menu item "Recent Folders" of menu "Go" of menu bar 1'`,
-		)
-		.split(", ")
-		.slice(0, -2)
-		.map((item) => item.slice(10).split(" of ")[0])
-		.map((item) => {
-			id++;
-			return { name: item, id: id, type: "dir" };
-		});
-
-	id = 0;
-	const recentItemsMap = recentItems
-		.map((item) => {
-			// INFO workaround with id necessary, since only file names, but not file paths are
+	let menuId = -1;
+	const recentItemsMap = recentFiles
+		.slice(recentFilesStart, recentFilesEnd)
+		.filter((/** @type {string} */ item) => !item.includes("“"))
+		.map((/** @type {string} */ item) => {
+			// HACK workaround with id necessary, since only file names, but not file paths are
 			// saved in the menu, so that the IDs need to be used to emulate a click in
 			// the next applescript step
-			id++;
-			return { name: item, id: id, type: "file" };
-		})
-		.slice(start, end)
-		.filter((item) => !item.name.includes("“"));
+			menuId = menuId + 2;
+			return {
+				name: item,
+				menuId: recentFilesStart + menuId,
+				type: "file",
+			};
+		});
+
+	//───────────────────────────────────────────────────────────────────────────
+
+	menuId = 0;
+	const recentFolders = Application("System Events")
+		.processes.byName("Finder")
+		.menuBars[0].menuBarItems.byName("Go")
+		.menus[0].menuItems.byName("Recent Folders")
+		.menus[0].menuItems.name()
+		.slice(0, -2)
+		.map((/** @type {string} */ item) => {
+			menuId++;
+			return { name: item, menuId: menuId, type: "dir" };
+		});
 
 	/** @type {AlfredItem[]} */
-	const recentAll = [...recentFolders, ...recentItemsMap].map((item) => {
-		const revealID = item.type === "file" ? item.id : item.id + 1;
+	const recentAll = [...recentItemsMap, ...recentFolders].map((item) => {
 		let iconPath = "../../../_custom-filetype-icons/";
 		iconPath += item.type === "file" ? "blank.png" : "folder.png";
-		const subtitle = item.type === "file" ? "⌥: Reveal in Finder" : "❌ not available for folder";
+		const subtitle = item.type === "file" ? "⌥: Reveal in Finder" : "❌ Not for folder";
 
 		/** @type {AlfredItem} */
 		const alfredItem = {
 			title: item.name,
 			uid: item.name,
 			match: alfredMatcher(item.name),
+			icon: { path: iconPath },
+			arg: item.menuId.toString(),
 			mods: {
 				alt: {
-					arg: revealID.toString(),
-					valid: item.type === "f",
+					arg: item.menuId + 1,
+					valid: item.type === "file",
 					subtitle: subtitle,
 				},
 			},
-			icon: { path: iconPath },
-			arg: item.id.toString(),
 			variables: { type: item.type },
 		};
 		return alfredItem;
