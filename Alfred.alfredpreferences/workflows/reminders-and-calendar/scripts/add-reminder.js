4a74e455bb455c55c6140b00a991cd88c9ea3edc
🤖 automated <cron@job>
2023-10-19 23:06:39 +0200

    iMac Home (15)

--- Alfred.alfredpreferences/workflows/reminders-and-calendar/scripts/add-reminder.js
+++ Alfred.alfredpreferences/workflows/reminders-and-calendar/scripts/add-reminder.js
@@ -1,31 +1,27 @@
 #!/usr/bin/env osascript -l JavaScript
 ObjC.import("stdlib");
-const app = Application.currentApplication();
-app.includeStandardAdditions = true;
-
 //──────────────────────────────────────────────────────────────────────────────
 
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
 	const inDays = parseInt(argv[0])
-	console.log("🪚 inDays:", inDays);
 	const dueDate = new Date();
 	dueDate.setDate(dueDate.getDate() + inDays);
-	console.log("🪚 dueDate:", dueDate);
 
-	const text = $.getenv("reminderText");
-	const title = text.split("\n")[0];
-	const body = text.split("\n").slice(1).join("\n");
+	const lines = $.getenv("reminderText").split("\n");
+	const title = lines.shift();
+	const body = lines.join("\n");
 
 	const rem = Application("Reminders");
+
 	const newReminder = rem.Reminder({
 		name: title,
 		body: body,
-		allDayDueDate: (new Date()),
+		alldayDueDate: dueDate,
 	});
 	rem.defaultList().reminders.push(newReminder)
 
-	rem.quit()
+	// rem.quit()
 	return title; // Alfred notification
 }
