acb4d544c359ae437697e6d9e5c647b5fa6ff774
🤖 automated <cron@job>
2023-10-21 13:05:31 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/github-utilities/scripts/clone-repo.sh
+++ Alfred.alfredpreferences/workflows/github-utilities/scripts/clone-repo.sh
@@ -3,7 +3,8 @@
 #───────────────────────────────────────────────────────────────────────────────
 
 # use SSH instead of https
-url="$(echo "$*" | sed -E 's/https?:\/\/github.com\//git@github.com:/').git"
+full_repo=$(echo "$*" | cut -c20-)
+url="git@github.com:$full_repo.git"
 reponame=$(echo "$*" | sed -E 's/.*\///')
 
 [[ ! -e "$LOCAL_REPOS" ]] && mkdir -p "$LOCAL_REPOS"
@@ -11,23 +12,25 @@ cd "$LOCAL_REPOS" || exit 1
 
 if [[ -e "$reponame" ]]; then
 	osascript -e 'display notification "" with title "⚠️ Repo already exists."'
-	echo -n "$LOCAL_REPOS/$reponame" # Open in terminal via Alfred
-	return 0
 fi
 
 # WARN depth=2 ensures that amending a shallow commit does not result in a 
 # new commit without parent, effectively destroying git history (!!)
 git clone --depth=2 --filter="blob:none" "$url" || return 1
 
+#───────────────────────────────────────────────────────────────────────────────
+
+echo -n "$LOCAL_REPOS/$reponame" # Open in terminal via Alfred
+
 # shellcheck disable=2154 # set via previous script
 if [[ "$publicRepo" == "true" ]]; then
 	cd "$reponame" || return 1
 	gh repo fork --remote=false
 
-	git remote add my-fork "git@github.com:$github_username/$reponame.git"
+	# add my remote as SSH
+	git remote add upstream "git@github.com:$github_username/$reponame.git"
 
-	git checkout -b "feature"
-	git push --set-upstream my-fork "feature"
+	gh repo set-default "$full_repo" # where to send PRs
+	git checkout -b "feature" # send PR via branch for editing from maintainer
 fi
 
-echo -n "$LOCAL_REPOS/$reponame" # Open in terminal via Alfred
