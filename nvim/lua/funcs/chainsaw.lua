7d2d35101bc92cc8403a9c320394c159931e2f79
🤖 automated <cron@job>
2023-10-27 13:43:03 +0200

    iMac Home (1)

--- nvim/lua/funcs/chainsaw.lua
+++ nvim/lua/funcs/chainsaw.lua
@@ -2,7 +2,6 @@ local M = {}
 --------------------------------------------------------------------------------
 local fn = vim.fn
 local bo = vim.bo
-local cmd = vim.cmd
 
 ---runs :normal natively with bang
 local function normal(cmdStr) vim.cmd.normal { cmdStr, bang = true } end
@@ -13,20 +12,6 @@ local function noSupportNotif(funcname)
 	local msg = funcname .. " does not support " .. bo.filetype .. " yet."
 	vim.notify(msg, vim.log.levels.WARN, { title = "Chainsaw" })
 end
-
-local function isNvimLua()
-	return vim.fn.expand("%:p"):find("nvim")
-end
-
---------------------------------------------------------------------------------
-
--- CONFIG
-local config = {
-	marker = "🪚",
-	beepEmojis = { "🤖", "👽", "👾", "💣" }
-} 
-
---------------------------------------------------------------------------------
 
 ---in normal mode, returns word under cursor, in visual mode, returns selection
 ---@return string?
@@ -50,36 +35,71 @@ end
 ---@param text string
 local function append(text)
 	local ln = vim.api.nvim_win_get_cursor(0)[1]
+	local indent = vim.api.nvim_get_current_line():match("^%s*")
+	vim.api.nvim_buf_set_lines(0, ln, ln, false, { indent .. text })
+end
 
-	if vim.bo.ft == "python" then
-		local indent = vim.api.nvim_get_current_line():match("^%s*")
-		text = indent .. text
-		vim.api.nvim_buf_set_lines(0, ln, ln, false, { text })
-	else
-		vim.api.nvim_buf_set_lines(0, ln, ln, false, { text })
-		normal("j==")
-	end
+local function getFiletype()
+	local ft = bo.filetype
+	if ft == "lua" and vim.fn.expand("%:p"):find("nvim") then ft = "nvim_lua" end
+	return ft
 end
 
 --------------------------------------------------------------------------------
 
-function M.messageLog()
-	local ft = bo.filetype
-	local templateStr
+-- CONFIG
+local config = {
+	marker = "🪚",
+	beepEmojis = { "🤖", "👽", "👾", "💣" },
+	logStatements = {
+		beepLog = {
+			nvim_lua = 'vim.notify("%s beep %s")',
+			lua = 'print("%s beep %s")',
+			python = 'print("%s beep %s")',
+			javascript = 'console.log("%s beep %s");',
+			typescript = 'console.log("%s beep %s");',
+			sh = 'echo "%s beep %s"',
+			applescript = "beep -- %s",
+			css = "outline: 2px solid red !important; /* %s */",
+			scss = "outline: 2px solid red !important; /* %s */",
+		},
+		objectLog = {
+			nvim_lua = 'vim.notify("%s %s: " .. vim.inspect(%s))',
+			typescript = 'console.log("%s %s:", %s)',
+			javascript = 'console.log("%s %s:", JSON.stringify(%s))',
+		},
+		assertLog = {
+			lua = 'assert(%s, "%s %s")',
+			nvim_lua = 'assert(%s, "%s %s")',
+			python = 'assert %s, "%s %s"',
+		},
+		messageLog = {
+			lua = 'print("%s ")',
+			nvim_lua = 'vim.notify("%s ")', -- FIX for noice.nvim print-bug: https://github.com/folke/noice.nvim/issues/556
+			python = 'print("%s ")',
+			javascript = 'console.log("%s ");',
+			typescript = 'console.log("%s ");',
+			sh = 'echo "%s "',
+			applescript = 'log "%s "',
+		},
+		variableLog = {
+			lua = 'print("%s %s: ", %s)',
+			nvim_lua = 'vim.notify("%s %s: ".. tostring(%s))', -- FIX for noice.nvim print-bug: https://github.com/folke/noice.nvim/issues/556
+			python = 'print(f"%s {%s = }")',
+			javascript = 'console.log("%s %s:", %s);',
+			typescript = 'console.log("%s %s:", %s);',
+			sh = 'echo "%s %s: $%s"',
+			applescript = 'log "%s %s:" & %s',
+			css = "outline: 2px solid red !important; /* %s */",
+			scss = "outline: 2px solid red !important; /* %s */",
+		},
+	},
+}
 
-	if ft == "lua" then
-		templateStr = 'print("%s ")'
-		-- FIX for noice.nvim print-bug: https://github.com/folke/noice.nvim/issues/556
-		if isNvimLua() then templateStr = 'vim.notify("%s ")' end
-	elseif ft == "python" then
-		templateStr = 'print("%s ")'
-	elseif ft == "javascript" or ft == "typescript" then
-		templateStr = 'console.log("%s ");'
-	elseif ft == "zsh" or ft == "bash" or ft == "fish" or ft == "sh" then
-		templateStr = 'echo "%s "'
-	elseif ft == "applescript" then
-		templateStr = 'log "%s "'
-	else
+function M.messageLog()
+	local ft = getFiletype()
+	local templateStr = config.logStatements.messageLog[ft]
+	if not templateStr then
 		noSupportNotif("Message Log")
 		return
 	end
@@ -88,101 +108,61 @@ function M.messageLog()
 	append(logStatement)
 	-- goto insert mode at correct location
 	normal('f";') -- goto second `"`
-	cmd.startinsert()
+	vim.cmd.startinsert()
 end
 
 ---log statement for variable under cursor, similar to the 'turbo console log'
 ---VS Code plugin. Supported: lua, python, js/ts, zsh/bash/fish, and applescript
 function M.variableLog()
-	local varname = getVar()
-	local templateStr
-	local ft = bo.filetype
-
-	if ft == "lua" then
-		templateStr = 'print("%s %s: ".. %s)'
-		-- FIX for noice.nvim print-bug: https://github.com/folke/noice.nvim/issues/556
-		if isNvimLua() then templateStr = 'vim.notify("%s %s: " .. tostring(%s))' end
-	elseif ft == "python" then
-		templateStr = 'print(f"%s {%s = }")'
-	elseif ft == "javascript" or ft == "typescript" then
-		templateStr = 'console.log("%s %s:", %s);'
-	elseif ft == "zsh" or ft == "bash" or ft == "sh" then
-		templateStr = 'echo "%s %s: $%s"'
-	elseif ft == "applescript" then
-		templateStr = 'log "%s %s:" & %s'
-	elseif ft == "css" or ft == "scss" then
-		templateStr = "outline: 2px solid red !important; /* %s */"
-	else
+	local ft = getFiletype()
+	local templateStr = config.logStatements.variableLog[ft]
+	if not templateStr then
 		noSupportNotif("Variable Log")
 		return
 	end
 
+	local varname = getVar()
 	local logStatement = templateStr:format(config.marker, varname, varname)
 	append(logStatement)
 end
 
 function M.assertLog()
-	local templateStr
-	local varname = getVar()
-
-	local ft = bo.filetype
-	if ft == "lua" then
-		templateStr = 'assert(%s, "%s %s")'
-	elseif ft == "python" then
-		templateStr = 'assert %s, "%s %s"'
-	else
+	local ft = getFiletype()
+	local templateStr = config.logStatements.assertLog[ft]
+	if not templateStr then
 		noSupportNotif("Assert Log")
 		return
 	end
 
+	local varname = getVar()
 	local logStatement = templateStr:format(varname, config.marker, varname)
 	append(logStatement)
 	normal("f,") -- goto the comma to edit the condition
 end
 
 function M.objectLog()
-	local varname = getVar()
-	local templateStr
-	local ft = bo.filetype
-
-	if ft == "lua" and isNvimLua() then
-		templateStr = 'vim.notify("%s %s: " .. vim.inspect(%s))'
-	elseif ft == "javascript" then
-		templateStr = 'console.log("%s %s:", JSON.stringify(%s))'
-	else
+	local ft = getFiletype()
+	local templateStr = config.logStatements.objectLog[ft]
+	if not templateStr then
 		noSupportNotif("Object Log")
 		return
 	end
 
+	local varname = getVar()
 	local logStatement = templateStr:format(config.marker, varname, varname)
 	append(logStatement)
 end
 
----adds simple "beep" log statement to check whether conditionals have been
----triggered. Supported: lua, python, js/ts, zsh/bash/fish, and applescript
+---adds simple "beep" log statement to check whether conditionals have been triggered
 function M.beepLog()
-	local templateStr
-	local ft = bo.filetype
-	local randomEmoji = config.beepEmojis[math.random(1, #config.beepEmojis)]
-
-	if isNvimLua() and ft == "lua" then
-		-- FIX for noice.nvim print-bug: https://github.com/folke/noice.nvim/issues/556
-		templateStr = 'vim.notify("%s beep %s")'
-	elseif ft == "lua" or ft == "python" then
-		templateStr = 'print("%s beep %s")'
-	elseif ft == "javascript" or ft == "typescript" then
-		templateStr = 'console.log("%s beep %s");'
-	elseif ft == "zsh" or ft == "bash" or ft == "sh" then
-		templateStr = 'echo "%s beep %s"'
-	elseif ft == "applescript" then
-		templateStr = "beep -- %s"
-	elseif ft == "css" or ft == "scss" then
-		templateStr = "outline: 2px solid red !important; /* %s */"
-	else
+	local ft = getFiletype()
+	local templateStr = config.logStatements.beepLog[ft]
+	if not templateStr then
 		noSupportNotif("Beep Log")
 		return
 	end
 
+	local randomEmoji = config.beepEmojis[math.random(1, #config.beepEmojis)]
 	local logStatement = templateStr:format(config.marker, randomEmoji)
 	append(logStatement)
 end
@@ -207,20 +187,20 @@ function M.timeLog()
 			'print(f"%s: {durationSecs}s")',
 		}
 	elseif ft == "javascript" then
-		-- JXA does not support console.time()
+		-- not all JS engines support console.time()
 		start = { "const timelogStart = +new Date(); // %s" }
 		stop = {
 			"const durationSecs = (+new Date() - timelogStart) / 1000; // %s",
-			'console.log(`%s: ${durationSecs}s`);',
+			"console.log(`%s: ${durationSecs}s`);",
 		}
 	elseif ft == "typescript" then
 		start = { 'console.time("timelog"); // %s' }
 		stop = { 'console.timeEnd("timelog"); // %s' }
-	elseif ft == "bash" or ft == "zsh" or ft == "sh" or ft == "fish" then
-		start = { "timelogStart=$(date +%s) # %s" }
+	elseif ft == "bash" or ft == "zsh" or ft == "sh" then
+		start = { "timelogStart=$(date +%%s) # %s" }
 		stop = {
-			"timelogEnd=$(date +%s) && durationSecs = $((timelogEnd - timelogStart)) # %s",
-			'echo "%s time ${durationSecs}s"',
+			"timelogEnd=$(date +%%s) && durationSecs = $((timelogEnd - timelogStart)) # %s",
+			'echo "%s ${durationSecs}s"',
 		}
 	else
 		noSupportNotif("Time Log")
@@ -238,12 +218,6 @@ function M.debugLog()
 	local logStatement
 	local ft = bo.filetype
 
-	if ft == "javascript" or ft == "typescript" then
-		logStatement = "debugger; // %s"
-	elseif ft == "python" then
-		logStatement = "breakpoint()  # %s"
-	else
-		noSupportNotif("Debug Log")
 		return
 	end
 
@@ -256,16 +230,15 @@ function M.removeLogs()
 
 	-- escape for vim regex, in case `[]` are used in the marker
 	local toRemove = config.marker:gsub("%]", "\\]"):gsub("%[", "\\[")
-	cmd(("silent g/%s/d"):format(toRemove))
-	cmd.nohlsearch()
+	vim.cmd(("silent g/%s/d"):format(toRemove))
+	vim.cmd.nohlsearch()
 
 	local linesRemoved = numOfLinesBefore - vim.api.nvim_buf_line_count(0)
 	local msg = ("Removed %s log statements."):format(linesRemoved)
 	if linesRemoved == 1 then msg = msg:sub(1, -3) .. "." end -- 1 = singular
 	vim.notify(msg, vim.log.levels.INFO, { title = "Chainsaw" })
 
-	---@diagnostic disable-next-line: inject-field
-	vim.b.timelogStart = false -- reset timelog
+	vim.b.timelogStart = false ---@diagnostic disable-line: inject-field
 end
 
 --------------------------------------------------------------------------------
