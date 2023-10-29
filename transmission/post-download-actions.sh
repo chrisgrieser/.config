a6ee541387d54d59cbe9ace25010b42929a6e3c6
🤖 automated <cron@job>
2023-10-29 02:10:35 +0200

    iMac Home (2)

--- transmission/post-download-actions.sh
+++ transmission/post-download-actions.sh
@@ -1,27 +1,39 @@
 #!/usr/bin/env zsh
+# shellcheck disable=2164
 # DOCS https://github.com/transmission/transmission/blob/main/docs/Scripts.md#scripts
 #───────────────────────────────────────────────────────────────────────────────
 
 # GUARD
-command -v transmission-remote &>/dev/null || touch "./WARN transmission-remote not installed"
-cd "$HOME/Downloads/" || return 1
+download_folder="$(defaults read org.m0k.transmission DownloadFolder)"
+[[ -x "$(command -v transmission-remote)" ]] || touch "./WARN transmission-remote not installed"
+if [[ -d "$download_folder" ]]; then
+	cd "$download_folder"
+else
+	touch "./WARN Transmission DownloadFolder not found"
+	return 1
+fi
+
+#───────────────────────────────────────────────────────────────────────────────
 
 # delete clutter
 find . \( -name '*.txt' -or -name '*.nfo' -or -name '*.exe' -or -name '*.md' \
 	-or -name '*.jpg' -or -name '*.png' \) -delete
-find . -name "Sample" -print0 | xargs -0 rm -r # `-delete` doesn't work for directories
+# shellcheck disable=2038
+find . -name "Sample" | xargs rm -r # `-delete` doesn't work for directories
 
 # wait for files being fully moved & identify new folder
 sleep 0.5
-NEW_FOLDER="$(find . -mindepth 1 -type d -mtime -2m | head -n1)"
+new_folder="$(find . -depth 1 -type d -mtime -2m | head -n1)"
 
 # if single file, move up and remove directory
 sleep 0.5
-FILES_IN_FOLDER=$(find "$NEW_FOLDER" -depth 1 | wc -l | tr -d " ")
-if [[ $FILES_IN_FOLDER -eq 1 ]]; then
-	mv "$NEW_FOLDER"/* "$VIDEO_DIR"
-	rmdir "$NEW_FOLDER"
+files_in_folder=$(find "$new_folder" -depth 1 | wc -l | tr -d " ")
+if [[ $files_in_folder -eq 1 ]]; then
+	mv "$new_folder"/* "$download_folder"
+	rmdir "$new_folder"
 fi
+
+#───────────────────────────────────────────────────────────────────────────────
 
 # quit Transmission, if there are no other torrents active
 sleep 3 # time for new torrents to be initialized
