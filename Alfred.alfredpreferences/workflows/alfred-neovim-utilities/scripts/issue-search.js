7781163341428dd84473b4b786760ae440b0270c
🤖 automated <cron@job>
2023-10-29 18:06:47 +0100

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/alfred-neovim-utilities/scripts/issue-search.js
+++ Alfred.alfredpreferences/workflows/alfred-neovim-utilities/scripts/issue-search.js
@@ -18,40 +18,30 @@ function run() {
 	const repoID = $.getenv("repoID");
 	console.log("🪚 repoID:", repoID);
 
-	// GET PLUGIN ISSUES
 	// GitHub API only returns 100 results https://stackoverflow.com/questions/30656761/github-search-api-only-return-30-results
 	const issueAPIURL = `https://api.github.com/repos/${repoID}/issues?state=all&per_page=100`;
 	const issueJSON = JSON.parse(app.doShellScript(`curl -s "${issueAPIURL}"`))
 		.sort((/** @type {GithubIssue} */ x, /** @type {GithubIssue} */ y) => {
 			const a = x.state;
 			const b = y.state;
-			return a === b ? 0 : a < b ? 1 : -1; 
+			return a === b ? 0 : a < b ? 1 : -1;
 		})
 		.map((/** @type {GithubIssue} */ issue) => {
-			const title = issue.title;
 			const issueCreator = issue.user.login;
-
-			let state = "";
-			if (issue.state === "open") state += "🟢 ";
-			else state += "🟣 ";
-			if (title.toLowerCase().includes("request") || title.includes("FR")) state += "🙏 ";
-			if (title.toLowerCase().includes("suggestion")) state += "💡 ";
-			if (title.toLowerCase().includes("bug")) state += "🪲 ";
-			if (title.includes("?")) state += "❓ ";
-			let comments = "";
-			if (issue.comments !== "0") comments = "   💬 " + issue.comments;
+			const state = issue.state === "open" ? "🟢" : "🟣";
+			const comments = issue.comments === "0" ? "" : "💬 " + issue.comments;
 
 			const issueMatcher = [
 				issue.state,
-				alfredMatcher(title),
+				alfredMatcher(issue.title),
 				alfredMatcher(issueCreator),
 				"#" + issue.number,
 			].join(" ");
 
 			return {
-				title: state + title,
+				title: state + " " + issue.title,
 				match: issueMatcher,
-				subtitle: "#" + issue.number + " by " + issueCreator + comments,
+				subtitle: `#${issue.number} by ${issueCreator}   ${comments}`,
 				arg: issue.html_url,
 			};
 		});
