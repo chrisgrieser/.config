51f1bda1f6f6934a150b3280f78cd537479bbf9c
🤖 automated <cron@job>
2023-10-31 22:14:04 +0100

    mini Office (1)

--- Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/pre-shutdown.sh
+++ Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/pre-shutdown.sh
@@ -2,23 +2,23 @@
 
 cd "$HOME/.config" || return 1
 if [[ -n "$(git status --porcelain)" ]]; then
-	osascript -e 'display notification "🔵 Dotfiles" with title "🔁 Syncing…"'
+	osascript -e 'display notification "Dotfiles 🔵" with title "🔁 Syncing…"'
 	zsh "$HOME/.config/git-dotfile-sync.sh" &>/dev/null
 fi
 
 cd "$VAULT_PATH" || return 1
 if [[ -n "$(git status --porcelain)" ]]; then
-	osascript -e 'display notification "🟪 Vault" with title "🔁 Syncing…"'
+	osascript -e 'display notification "Vault 🟪" with title "🔁 Syncing…"'
 	zsh "$VAULT_PATH/Meta/git-vault-sync.sh" &>/dev/null
 fi
 
 cd "$PASSWORD_STORE_DIR" || return 1
 if [[ -n "$(git status --porcelain)" ]]; then
-	osascript -e 'display notification "🔑 Password Store" with title "🔁 Syncing…"'
+	osascript -e 'display notification "Password Store 🔑" with title "🔁 Syncing…"'
 	zsh "$PASSWORD_STORE_DIR/pass-sync.sh" &>/dev/null
 fi
 
 #───────────────────────────────────────────────────────────────────────────────
 
-# conditional for Alfred to prompt shutdown
+# for Alfred conditional to prompt shutdown
 echo -n "success" 
