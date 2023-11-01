7c5168012f9f731f54a2a56698db050f1dccf2ae
🤖 automated <cron@job>
2023-11-01 13:12:00 +0100

    iMac Home (4)

--- zsh/options_and_plugin_configs.zsh
+++ zsh/options_and_plugin_configs.zsh
@@ -34,8 +34,11 @@ export RIPGREP_CONFIG_PATH="$HOME/.config/rg/ripgrep-config"
 
 # zoxide
 export _ZO_DATA_DIR="$DATA_DIR/zoxide/"
-export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS --height=30%"
 eval "$(zoxide init --no-cmd zsh)" # needs to be placed after compinit
+export _ZO_FZF_OPTS="
+	$FZF_DEFAULT_OPTS --height=50% --with-nth=2.. --preview-window=right
+	--preview='eza {2} --icons --color=always --width=\$FZF_PREVIEW_COLUMNS'
+"
 
 # updates managed via homebrew https://cli.github.com/manual/gh_help_environment
 export GH_NO_UPDATE_NOTIFIER=1
