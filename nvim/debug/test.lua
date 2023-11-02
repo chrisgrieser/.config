fd93535d18d272a115f2468a5dfe65bc0555389f
🤖 automated <cron@job>
2023-11-02 17:57:46 +0100

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,18 +1,10 @@
 --------------------------------------------------------------------------------
---
--- local history = require("yanky.history").all()
--- local historyOfFt = vim.tbl_filter(function(item) return item.filetype == vim.bo.ft end, history)
--- local historyStrings = vim.tbl_map(function(item) return item.regcontents end, historyOfFt)
--- vim.notify(vim.inspect(historyOfFt[8]))
 
-local hist_type = ":"
-local seen_items = {}
-local items = {}
-for i = 1, vim.fn.histnr(hist_type) do
-	local item = vim.fn.histget(hist_type, -i)
-	if #item > 0 and not seen_items[item] then
-		seen_items[item] = true
-		items[#items + 1] = { label = item, dup = 0 }
-	end
-end
-vim.notify("🪚 items: " .. vim.inspect(items))
+local foobar_1
+local foobar_2
+local foobar_3
+
+local foobar_1
+local foobar_2
+local foobar_3
+
