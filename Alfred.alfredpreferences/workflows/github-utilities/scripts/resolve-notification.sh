9ddb4d1e75c81f8451826d26900ce5dbde1cc143
🤖 automated <cron@job>
2023-11-04 23:06:19 +0100

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/github-utilities/scripts/resolve-notification.sh
+++ Alfred.alfredpreferences/workflows/github-utilities/scripts/resolve-notification.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env zsh
 
-notification_url="$1"
+url="$1"
 
-# DOCS https://docs.github.com/en/rest/activity/notifications?apiVersion=2022-11-28#get-a-thread
-url=$(curl -sL -H "Accept: application/vnd.github+json" \
-	-H "Authorization: Bearer $GITHUB_TOKEN" \
-	-H "X-GitHub-Api-Version: 2022-11-28" \
-	"$notification_url" |
-	grep "html_url.*" | head -n1 | cut -d '"' -f 4 # skip `jq` dependency
-)
+# shellcheck disable=2154
+if [[ "$mode" == "direct-open" ]]; then
+	open "$url"
+elif [[ "$mode" == "mark-as-read" ]]; then
+	# DOCS https://docs.github.com/en/rest/activity/notifications?apiVersion=2022-11-28#mark-a-thread-as-read
+	thread_id="$1"
+	curl -sL \
+		-X PATCH \
+		-H "Accept: application/vnd.github+json" \
+		-H "Authorization: Bearer $GITHUB_TOKEN" \
+		-H "X-GitHub-Api-Version: 2022-11-28" \
+		"https://api.github.com/notifications/threads/$thread_id"
+else
+	# DOCS https://docs.github.com/en/rest/activity/notifications?apiVersion=2022-11-28#get-a-thread
+	url=$(
+		curl -sL -H "Accept: application/vnd.github+json" \
+			-H "Authorization: Bearer $GITHUB_TOKEN" \
+			-H "X-GitHub-Api-Version: 2022-11-28" \
+			"$thread_id" |
+			grep "html_url.*" | head -n1 | cut -d '"' -f 4 # skip `jq` dependency
+	)
 
-open "$url"
-		# if (notif.subject.url) {
-		# 	const idInRepo = notif.subject.url.match(/\d+$/);
-		# 	const lastCommentId =
-		# 		notif.subject.latest_comment_url && notif.subject.latest_comment_url !== notif.subject.url
-		# 			? "#issuecomment-" + notif.subject.latest_comment_url.match(/\d+$/)
-		# 			: "";
-		# 	const type = notif.subject.type === "PullRequest" ? "pull" : "issues"; //codespell-ignore
-		# 	url = `https://github.com/${notif.repository.full_name}/${type}/${idInRepo}${lastCommentId}`;
-		# } else {
-		# 	// discussions do not have a direct URL available :/
-		# 	url = "https://github.com/notifications";
-		# }
-		#
+	if [[ "$mode" == "open" ]]; then
+		open "$url"
+	elif [[ "$mode" == "copy" ]]; then
+		echo "$url" | pbcopy
+		echo "$url" # pass for Alfred notification
+	fi
+fi
