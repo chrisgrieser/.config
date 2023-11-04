bfaebdbe6aacd99622fafeb95b25e2e06e7de170
🤖 automated <cron@job>
2023-11-04 13:06:19 +0100

    iMac Home (8)

--- Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/search-man-pages.js
+++ Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/search-man-pages.js
@@ -16,32 +16,41 @@ function alfredMatcher(str) {
 
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
-function run() {
-	const baseURL = "https://man.cx/";
+function run(argv) {
+	// INFO using mankier due to its ability to search options https://www.mankier.com/about
+	const manPageSite = "https://www.mankier.com/1/";
 
+	// process Alfred args
+	const query = argv[0] || "";
+	const command = query.split(" ")[0];
+	const options = argv[0] ? query.split(" ").slice(1) : "";
+
+	// get list of all installed binaries
 	const binariesList = app
 		.doShellScript(
 			"echo $PATH | tr ':' '\n' | xargs -I {} find {} -maxdepth 1 -type f -or -type l -perm '++x'",
 		)
 		.split("\r")
-		.map((path) => path.replaceAll("//", "/"));
+		.filter((binary) => binary.includes(command));
 
 	/** @type{AlfredItem[]} */
 	const binariesArr = [...new Set(binariesList)] // only unique
-		.sort((a, b) => {
-			// sort homebrew installs first
-			if (a.includes("brew") && !b.includes("brew")) return -1;
-			if (!a.includes("brew") && b.includes("brew")) return 1;
-			return 0;
-		})
+		.sort((a, b) => a.length - b.length) // sort shorter ones to the top
 		.map((binary) => {
 			const cmd = binary.split("/").pop();
-			let icon = "";
-			if (binary.includes("brew")) icon += " 🍺";
+			const icon = binary.includes("brew") ? " 🍺" : "";
+			let url = manPageSite + cmd;
+			if (options) url += "#" + options;
 			return {
 				title: cmd + icon,
 				match: alfredMatcher(cmd),
-				arg: baseURL + cmd,
+				arg: url,
+				mods: {
+					cmd: {
+						arg: "man " + argv[0],
+						subtitle: "> man " + argv[0],
+					},
+				},
 				uid: cmd,
 			};
 		});
