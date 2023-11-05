a2c8548d808d9f567446ca9660a199d8e3f2e39b
🤖 automated <cron@job>
2023-11-05 18:06:18 +0100

    iMac Home (4)

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -1,4 +1,8 @@
 vim.opt_local.listchars:remove("multispace")
 
-vim.keymap.set("n", "q", vim.cmd.cquit, { buffer = true, desc = "Quit Commit", nowait = true })
-
+-- `nofile` gets applied to `DressingInput` buffers, but not the
+-- `COMMIT_EDITMSG` buffers. The condition thus ensures only commits valled from
+-- the terminal get this mapping, and not buffers like the one from nvim-tinygit
+if vim.bo.buftype ~= "nofile" then
+	vim.keymap.set("n", "q", vim.cmd.cquit, { buffer = true, desc = "Quit Commit", nowait = true })
+end
