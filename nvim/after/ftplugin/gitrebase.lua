a6f11980c49d78c8e53aee603ea9a198aad21dbb
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2023-11-05 10:24:31 +0100

    test

--- nvim/after/ftplugin/gitrebase.lua
+++ nvim/after/ftplugin/gitrebase.lua
@@ -1,5 +1,15 @@
 vim.opt_local.list = false
 
+-- add highlighting for commit messages
+vim.fn.matchadd("issueNumber", [[#\d\+]])
+vim.api.nvim_set_hl(0, "issueNumber", { link = "Number" })
+
+vim.fn.matchadd("mdInlineCode", [[`.\{-}`]]) -- .\{-} = non-greedy quantifier
+vim.api.nvim_set_hl(0, "mdInlineCode", { link = "@text.literal" })
+
+--------------------------------------------------------------------------------
+
+-- reminder about order
 vim.defer_fn(
 	function() vim.notify("top: past\nbottom: future", vim.log.levels.INFO, { title = "rebase" }) end,
 	1
