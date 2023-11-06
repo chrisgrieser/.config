00e23f5244b2b29ee81cfaac7471f44acb57027f
🤖 automated <cron@job>
2023-11-06 21:33:41 +0100

    iMac Home (5)

--- nvim/after/ftplugin/gitrebase.lua
+++ nvim/after/ftplugin/gitrebase.lua
@@ -1,10 +1,10 @@
 vim.opt_local.listchars:remove("multispace")
 
--- quit rebasing
+-- quit rebasing, `cquit` exits non-zero, ensuring rebase is not applied
 vim.keymap.set("n", "q", vim.cmd.cquit, { buffer = true, desc = "Quit Rebase", nowait = true })
 
 -- reminder about order
 vim.defer_fn(
-	function() vim.notify("top: past\nbottom: future", vim.log.levels.INFO, { title = "rebase" }) end,
+	function() vim.notify_once("top: past\nbottom: future", vim.log.levels.INFO, { title = "rebase" }) end,
 	1
 )
