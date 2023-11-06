b9dfd6f98ddcee82999388a711a2e172b81f1480
🤖 automated <cron@job>
2023-11-06 17:06:16 +0100

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,7 +1,13 @@
 local function fff() print("hi") end
 
-vim.lsp.buf.document_symbol {
-	on_list = function(items)
-		vim.notify(vim.inspect(items))
+local funcs = vim.lsp.buf.document_symbol {
+	on_list = function(response)
+		local funcsObjs = vim.tbl_filter(function(item) return item.kind == "Function" end, response.items)
+		local funcNames = vim.tbl_map(function(item)
+			return item.text:gsub("^%[Function%] ", "")
+		end, funcsObjs)
+		return funcNames
 	end,
 }
+	
+vim.notify("🪚 funcs: " .. vim.inspect(funcs))
