4dab673a9e658c7233daa48a5f0ee5f693a80116
🤖 automated <cron@job>
2023-11-07 16:25:37 +0100

    iMac Home (4)

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -237,4 +237,5 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.keymap.set("n", "q", cmd.close, { buffer = true, nowait = true, desc = "Quit" })
 	end,
 })
+
 --------------------------------------------------------------------------------
