d4e3da886ba16ded4f8cdaeff2bb8f82d8743d75
🤖 automated <cron@job>
2023-11-09 14:39:53 +0100

    iMac Home (5)

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -12,12 +12,24 @@ bo.tabstop = 4
 vim.opt_local.listchars:append { multispace = " " }
 
 --------------------------------------------------------------------------------
+-- auto-select project-local venv
+
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = "python",
+-- 	callback = function()
+-- 		vim.defer_fn(function()
+-- 			local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+-- 		end, 250)
+-- 	end,
+-- })
+
+--------------------------------------------------------------------------------
 
 -- fix habits
 u.ftAbbr("true", "True")
 u.ftAbbr("false", "False")
-u.ftAbbr("//", "#") 
-u.ftAbbr("--", "#") 
+u.ftAbbr("//", "#")
+u.ftAbbr("--", "#")
 u.ftAbbr("null", "None")
 u.ftAbbr("nil", "None")
 u.ftAbbr("none", "None")
