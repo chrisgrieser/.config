4571fbb9c9931e709ca715ac75a10090f5575bbe
🤖 automated <cron@job>
2023-11-09 15:39:53 +0100

    iMac Home (5)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,1 +1,4 @@
-vim.lsp.get_active_clients({name = "pyright"})
+local pythonPath = vim.lsp.get_active_clients({ name = "pyright" })[1].config.settings.python.pythonPath
+
+local venv = vim.fs.basename(vim.fs.dirname(vim.fs.dirname(pythonPath)))
+vim.notify(venv)
