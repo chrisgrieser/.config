1994e5afa78c4b3b980d039ce3a27a3ed6352d64
🤖 automated <cron@job>
2023-11-10 16:12:14 +0100

    iMac Home (3)

--- nvim/lua/plugins/python-plugins.lua
+++ nvim/lua/plugins/python-plugins.lua
@@ -5,16 +5,8 @@ return {
 	{ -- debugger preconfig for python
 		"mfussenegger/nvim-dap-python",
 		ft = "python",
-		config = function()
-			-- 1. use the debugypy installation by mason
-			-- 2. deactivate the annoying auto-opening the console by redirecting
-			-- to the internal console
-			local debugpyPythonPath = require("mason-registry")
-				.get_package("debugpy")
-				:get_install_path() .. "/venv/bin/python3"
-			require("dap-python").setup(debugpyPythonPath, { console = "internalConsole" })
-
-			-- Set virtual environment correctly
+		init = function()
+			-- Auto-set virtual environment correctly
 			vim.api.nvim_create_autocmd("FileType", {
 				pattern = "python",
 				callback = function()
@@ -23,6 +15,15 @@ return {
 					require("dap-python").resolve_python = function() return venv_python end
 				end,
 			})
+		end,
+		config = function()
+			-- 1. use the debugypy installation by mason
+			-- 2. deactivate the annoying auto-opening the console by redirecting
+			-- to the internal console
+			local debugpyPythonPath = require("mason-registry")
+				.get_package("debugpy")
+				:get_install_path() .. "/venv/bin/python3"
+			require("dap-python").setup(debugpyPythonPath, { console = "internalConsole" })
 		end,
 	},
 	{ -- import python modules action
