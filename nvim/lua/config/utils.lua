5013e366b3098d6533121f01190b8cbba379ff7a
🤖 automated <cron@job>
2023-11-12 14:12:11 +0100

    iMac Home (2)

--- nvim/lua/config/utils.lua
+++ nvim/lua/config/utils.lua
@@ -149,7 +149,7 @@ end
 M.textobjRemaps = {
 	c = "}", -- [c]urly brace
 	r = "]", -- [r]ectangular bracket
-	m = "W", -- [m]assive word
+	fsfsf = "f",
 	q = '"', -- [q]uote
 	z = "'", -- [z]ingle quote
 	e = "`", -- t[e]mplate string / inline cod[e]
