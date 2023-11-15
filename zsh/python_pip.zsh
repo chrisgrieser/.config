61ca0ec4756f4e236dc047b55cdc3454f8ea0aad
🤖 automated <cron@job>
2023-11-15 18:19:51 +0100

    iMac Home (5)

--- zsh/python_pip.zsh
+++ zsh/python_pip.zsh
@@ -1,7 +1,5 @@
 #!/usr/bin/env zsh
 
-#───────────────────────────────────────────────────────────────────────────────
-
 alias pu="pip uninstall"
 alias pi="pip install"
 alias pl="pip list --not-required"
@@ -80,21 +78,3 @@ function pip() {
 	fi
 }
 
-#───────────────────────────────────────────────────────────────────────────────
-# ANACONDA
-
-# Lazy-load conda environment, to improve performance and also to prevent conda
-# taking over the prompt until it is needed
-function conda {
-	unfunction "$0"
-	conda_prefix="$(brew --prefix)/anaconda3/bin" # change depending on where/hoow conda was installed
-
-	export PATH="$conda_prefix":$PATH
-	if [[ ! -x "$(command -v conda)" ]]; then print "\033[1;33mconda not installed.\033[0m" && return 1; fi
-
-	# setup snippet that `conda init zsh` adds to your `.zshrc`
-	__conda_setup="$("$conda_prefix/conda" 'shell.zsh' 'hook' 2> /dev/null)"
-	eval "$__conda_setup"
-
-	conda "$@"
-}
