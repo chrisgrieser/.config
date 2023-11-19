7c227ec7c90e3becaa9883245113c119d7fa1eec
🤖 automated <cron@job>
2023-11-19 23:20:39 +0100

    iMac Mother (2)

--- zsh/navigation.zsh
+++ zsh/navigation.zsh
@@ -16,6 +16,7 @@ function bookmark {
 }
 
 function unbookmark {
+	if [[ ! -x "$(command -v fzf)" ]]; then print "\e[1;33mfzf not installed.\e[0m" && return 1; fi
 	to_unbookmark=$(find "$bookmark_path" -type l | fzf --with-nth=-1 --delimiter="/")
 	[[ -n "$to_unbookmark" ]] && return 0 # aborted
 	rm "$to_unbookmark" && echo "Removed Bookmark: $(basename "$to_unbookmark")"
@@ -44,36 +45,35 @@ zle -N _grappling_hook
 bindkey "^O" _grappling_hook # bound to cmd+enter via wezterm
 
 #───────────────────────────────────────────────────────────────────────────────
+# SHORTHANDS
 
-# INFO leading space to ignore it in history due to HIST_IGNORE_SPACE
-alias b=" cd -"
-alias ..=" cd .."
-alias ...=" cd ../.."
-alias ....=" cd ../../.."
+alias b="cd -"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
 alias ..g='cd "$(git rev-parse --show-toplevel)"' # goto git root
+function mkcd { mkdir -p "$1" && cd "$1"; }       # mkdir + cd
 
 #───────────────────────────────────────────────────────────────────────────────
-
 # RECENT DIRS (via pushd & dirstack)
+
 setopt AUTO_PUSHD
 setopt PUSHD_IGNORE_DUPS
 export DIRSTACKSIZE=10
 
 function gr {
+	if [[ ! -x "$(command -v fzf)" ]]; then print "\e[1;33mfzf not installed.\e[0m" && return 1; fi
+	
 	local selected
 	selected=$(
-		dirs -pl | sed -E $'s|([^/]*/)|\e[0;38;5;245m\\1\e[0m|g' |
+		dirs -pl | sed -e '1d' -Ee $'s|([^/]*/)|\e[0;38;5;245m\\1\e[0m|g' |
 			fzf --query="$1" --no-sort --ansi \
 				--keep-right --with-nth=-2.. --delimiter="/" \
 				--preview-window="55%" --height="45%" \
 				--preview="printf '\e[7;38;5;245m\n{}\n\n\e[0m' ; eza {} --no-quotes --color=always --sort=newest --width=\$FZF_PREVIEW_COLUMNS"
 	)
-	[[ -d "$selected" ]] && cd "$selected"
+	[[ -z "$selected" ]] && return 0
+	cd "$selected"
 }
 
 #───────────────────────────────────────────────────────────────────────────────
-
-# mkdir + cd
-function mkcd {
-	mkdir -p "$1" && cd "$1"
-}
