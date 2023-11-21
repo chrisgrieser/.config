ee8a33cdf92c85aa37d22183558b61b464e9b9e8
🤖 automated <cron@job>
2023-11-21 22:36:38 +0100

    iMac Home (22)

--- Alfred.alfredpreferences/workflows/github-utilities/scripts/public-github-repo-search.js
+++ Alfred.alfredpreferences/workflows/github-utilities/scripts/public-github-repo-search.js
@@ -55,6 +55,9 @@ function relativeDate(absoluteDate) {
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
 	const query = argv[0] || "";
+
+
+
 	if (!query) return;
 	const apiURL = `https://api.github.com/search/repositories?q=${encodeURIComponent(query)}`;
 
