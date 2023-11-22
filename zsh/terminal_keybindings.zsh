7c71a59357313d5150a956969293dd223012ad85
🤖 automated <cron@job>
2023-11-22 23:24:39 +0100

    iMac Home (8)

--- zsh/terminal_keybindings.zsh
+++ zsh/terminal_keybindings.zsh
@@ -2,7 +2,7 @@
 # INFO BINDINGS FOR WIDGETS
 # - use `ctrl-v` and then a key combination to get the shell binding
 # - `bindkey -M main` to show existing keybinds
-# - some bindings with '^' are reserved (^M=enter, ^I=tab, ^[[Z = shift+tab)
+# - some bindings with '^' are reserved (^M=enter, ^I=tab)
 #───────────────────────────────────────────────────────────────────────────────
 # CUSTOM WIDGETS
 # https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/copybuffer/copybuffer.plugin.zsh
@@ -29,20 +29,15 @@ function zvm_after_init {
 	bindkey -M viins "^Z" undo            # cmd+z via wezterm
 	bindkey -M viins "^U" kill-whole-line # whole line, not part of the line
 
-	# DOCS https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html#Miscellaneous
-	# When confirming a command, search history for it and fill buffer with
-	# following command. e.g., using `<up><up><up><C-l><C-l><C-l>` will rerun
-	# three commands. Similar Alternative: accept-line-and-down-history
-	bindkey '^L' accept-and-infer-next-history
-
 	# zsh-history-substring-search
-	bindkey -M viins '^[[A' history-substring-search-up # up-arrow
+	bindkey -M viins '^[[A' history-substring-search-up   # up-arrow
 	bindkey -M viins '^[[B' history-substring-search-down # down-arrow
 }
 
 #───────────────────────────────────────────────────────────────────────────────
 
 # when typing backticks, escape & pair them
+# (useful for typing commit messages)
 function _autoEscapeBackTick {
 	LBUFFER+='\`'
 	RBUFFER+='\`'
