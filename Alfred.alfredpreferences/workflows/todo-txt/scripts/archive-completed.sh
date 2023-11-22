1abc71463a68fce596d536e7a27fc23bff6d5f1e
🤖 automated <cron@job>
2023-11-22 19:39:06 +0100

    iMac Home (9)

---
+++ Alfred.alfredpreferences/workflows/todo-txt/scripts/archive-completed.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+# shellcheck disable=2154
+
+# GUARD
+if [[ ! -f $donetxt_filepath ]]; then
+	echo -n "⚠️ Archive does not exist."
+	return 1
+fi
+
+to_be_archived_count=$(grep -c "^x " "$todotxt_filepath")
+
+# archive completed tasks
+grep "^x " "$todotxt_filepath" >>"$donetxt_filepath"
+
+# remove completed tasks
+grep -v "^x " "$todotxt_filepath" >"$todotxt_filepath.tmp"
+mv "$todotxt_filepath.tmp" "$todotxt_filepath"
+
+echo -n "$to_be_archived_count todos"
