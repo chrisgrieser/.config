7dbbea3569975a8100f217c17630d3c16a2b9653
🤖 automated <cron@job>
2023-12-01 13:45:08 +0100

    iMac Home (2)

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -3,9 +3,9 @@
 # DOCS https://zsh.sourceforge.io/Doc/Release/Options.html
 setopt HIST_IGNORE_DUPS
 setopt HIST_REDUCE_BLANKS
-setopt INC_APPEND_HISTORY
-setopt HIST_NO_STORE     # ignore history command itself for the history
-setopt HIST_IGNORE_SPACE # leading space is not added to the history
+setopt INC_APPEND_HISTORY # write immediately to history file
+setopt HIST_NO_STORE      # ignore history command itself for the history
+setopt HIST_IGNORE_SPACE  # cmds with leading space are not added to the history
 
 # DOCS https://zsh.sourceforge.io/Doc/Release/Parameters.html#Parameters-Used-By-The-Shell
 export HISTSIZE=3000
@@ -23,29 +23,32 @@ function lr() {
 
 # copies last command(s)
 function lc() {
-	local to_copy
-	if [[ $# -gt 0 ]] ; then
+	local to_copy cmd
+	if [[ $# -gt 0 ]]; then
 		to_copy=""
 		for arg in "$@"; do
-			to_copy="$to_copy$arg\n"
+			cmd=$(history -n -"$arg" -"$arg")
+			to_copy="$to_copy$cmd\n"
 		done
 	else
 		to_copy=$(history -n -1)
 	fi
-	print "\e[1;32mCopied:\e[0m "
+	print "\e[1;32mCopied:\e[0m"
 	echo -n "$to_copy"
 	echo -n "$to_copy" | pbcopy
 }
 
 # completions for it
 _lc () {
-	# shellcheck disable=2296 # valid in zsh
-	typeset -a recent_cmds=("${(f)"$(history -rn | sed "s/'/\\\'/g")"}") # lines to array
-	local expl
 
-	_values=({1..16})
+	local -a descriptions=()
+	history -rn | while IFS='' read -r value; do
+		arr1+=("$value")
+	done
+	# shellcheck disable=2296 # valid in zsh
+	local values=({1..16})
+	local expl
 	_description -V last-commands expl 'Last Commands'
-	# compadd "${expl[@]}" -Q -P"$'" -S"'" -- "${recent_cmds[@]}"
-	compadd "${expl[@]}" -Q -P"$'" -S"'" -d recent_cmds -a _values
+	compadd "${expl[@]}" -l -d "${descriptions[@]}" -a "${values"
 }
 compdef _lc lc
