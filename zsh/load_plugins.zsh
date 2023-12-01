d0651567cdc24b1c6cf6d94d95ad5727ea98f03a
🤖 automated <cron@job>
2023-12-01 12:45:08 +0100

    iMac Home (4)

--- zsh/load_plugins.zsh
+++ zsh/load_plugins.zsh
@@ -14,7 +14,6 @@ source "$HOMEBREW_PREFIX/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
 
 #───────────────────────────────────────────────────────────────────────────────
 
-source "$HOMEBREW_PREFIX/share/zsh-you-should-use/you-should-use.plugin.zsh"
 source "$HOMEBREW_PREFIX/share/zsh-autopair/autopair.zsh"
 source "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
