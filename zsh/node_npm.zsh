337337c3b05b48d632ee327c4d77f2a5ad7f7509
🤖 automated <cron@job>
2023-12-03 21:37:25 +0100

    iMac Home (4)

--- zsh/node_npm.zsh
+++ zsh/node_npm.zsh
@@ -2,19 +2,7 @@
 # https://nodejs.org/api/repl.html#repl_environment_variable_options
 export NODE_REPL_HISTORY=""
 
-#───────────────────────────────────────────────────────────────────────────────
-
-# instead of writing npm config to ~/.npmrc, they can also be defined as shell
+# INFO instead of writing npm config to ~/.npmrc, they can also be defined as shell
 # environment variables https://docs.npmjs.com/cli/v9/using-npm/config#environment-variables
 
-# disable funding reminder, has to be lowercase
-export npm_config_fund=false
-
-# lazy-load npm completions
-# DOCS https://docs.npmjs.com/cli/v10/commands/npm-completion
-function npm {
-	unfunction "$0"
-	# shellcheck disable=1090
-	source <(npm completion)
-	$0 "$@"
-}
+export npm_config_fund=false # disable funding reminder, has to be lowercase
