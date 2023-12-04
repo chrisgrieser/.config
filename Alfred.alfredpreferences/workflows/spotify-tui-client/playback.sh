7dfbbe09a870d29ddff70cf80dff87bbf9f68211
🤖 automated <cron@job>
2023-12-04 21:37:24 +0100

    iMac Home (15)

--- Alfred.alfredpreferences/workflows/spotify-tui-client/playback.sh
+++ Alfred.alfredpreferences/workflows/spotify-tui-client/playback.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env zsh
 # shellcheck disable=SC2086,SC2154
 
+playback_cmd=$1
+case $playback_cmd in
+"play-pause")
+	spotify_player playback --play-pause
+	;;
+"three")
+	echo "bar"
+	;;
+*)
+	echo "default"
+	;;
+esac
 [[ $1 == "play" ]] && shuffle="--shuffle"
-notification=$(spt playback --$1 $shuffle --format="$format" 2>&1)
+notification=$(spt playback --$playback_cmd $shuffle --format="$format" 2>&1)
 
 # shellcheck disable=SC2181
 if [[ "$?" != "0" ]]; then
