621e0cecc8c68292072d2d31881061fee2c55f94
🤖 automated <cron@job>
2023-12-10 02:09:16 +0100

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -2,17 +2,23 @@ local rootFiles = {
 	"info.plist",
 	".git",
 }
--- local parentDir = ".config"
-local function exists(path) return vim.loop.fs_stat(path) ~= nil end
+local ancestorDirs = { ".config" }
 
-local function getProjectRoot(startPath)
-	for _, file in ipairs(rootFiles) do
-		local path = startPath .. "/" .. file
-		if exists(path) then return path end
-	end
+local function getProjectRoot()
+	local function exists(path) return vim.loop.fs_stat(path) ~= nil end
+
+	local startPath = vim.fn.expand("%:p:h")
+	repeat
+		for _, file in ipairs(rootFiles) do
+			local path = startPath .. "/" .. file
+			if exists(path) then return vim.fs.dirname(path) end
+		end
+		for _, dir in ipairs(ancestorDirs) do
+			if vim.fs.basename(vim.fs.dirname(startPath)) == dir then return startPath end
+		end
+		startPath = vim.fs.dirname(startPath)
+	until startPath == "/"
 	return "no"
 end
 
-local root = getProjectRoot(vim.loop.cwd())
-
-print(root)
+vim.notify(getProjectRoot())
