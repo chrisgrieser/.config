37d2b0f5179c237e96b3fcf76f4d2b5fe2957513
🤖 automated <cron@job>
2023-12-10 11:13:53 +0100

    iMac Home (8)

--- nvim/after/ftplugin/bib.lua
+++ nvim/after/ftplugin/bib.lua
@@ -2,11 +2,11 @@ local bo = vim.bo
 local u = require("config.utils")
 --------------------------------------------------------------------------------
 
--- do not autowrap
-bo.formatoptions = bo.formatoptions:gsub("t", "")
+-- do not auto-wrap text
+vim.opt_local.formatoptions:remove("t")
 
 -- for some reason, bib files have no commentstring defined, even though they
--- do have comments?
+-- do have comments
 bo.commentstring = "% %s"
 
 --------------------------------------------------------------------------------
