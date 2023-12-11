f0877472de9ae74ccb3fa7478a177dabf7741be0
🤖 automated <cron@job>
2023-12-11 16:16:57 +0100

    mini Office (4)

--- Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/karabiner-docs-search.js
+++ Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/karabiner-docs-search.js
@@ -5,30 +5,35 @@ app.includeStandardAdditions = true;
 
 //──────────────────────────────────────────────────────────────────────────────
 
-const alfredMatcher = (/** @type {string} */ str) => str.replace(/[-()_./]/g, " ") + " " + str + " ";
+/** @param {string} str */
+function kebabToTitleCase(str) {
+	return str
+		.replaceAll("-", " ")
+		.replace(/\w+/g, (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+}
 
 //──────────────────────────────────────────────────────────────────────────────
+
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
 	const githubApi =
 		"https://api.github.com/repos/pqrs-org/gh-pages-karabiner-elements.pqrs.org/git/trees/main?recursive=1";
 	const baseUrl = "https://karabiner-elements.pqrs.org/docs";
-
 	const docsRegex = /^docs\/docs\/(.*)\/index\.html$/;
 
 	const workArray = JSON.parse(app.doShellScript(`curl -s "${githubApi}"`))
 		.tree.filter((/** @type {{ path: string; }} */ file) => docsRegex.test(file.path))
 		.map((/** @type {{ path: string }} */ file) => {
-			const site = file.path.slice(15, -11); // remove "docs/docs/json/" and "/index.html"
+			const site = file.path.replace(docsRegex, "$1");
 			const parts = site.split("/");
-			const subsite = parts.pop();
-			const parentSite = parts.join("/");
+			const subsite = kebabToTitleCase(parts.pop());
+			const parentSite = kebabToTitleCase(parts.join("  ·  "));
 			const url = `${baseUrl}/${site}/`;
 			return {
 				title: subsite,
-				match: alfredMatcher(site),
 				subtitle: parentSite,
+				match: `${subsite} ${parentSite}`,
 				arg: url,
 				uid: site,
 			};
