3c69bc97d49778d9dbc6425bd88a3a79c10fab6b
🤖 automated <cron@job>
2023-12-19 19:50:47 +0100

    iMac Home (7)

--- Alfred.alfredpreferences/workflows/ddgr-stuff/scripts/searchlink.sh
+++ Alfred.alfredpreferences/workflows/ddgr-stuff/scripts/searchlink.sh
@@ -3,11 +3,12 @@
 # INFO this scripts basically replaces https://brettterpstra.com/projects/searchlink/
 
 query="$*"
+extra_args=$([[ "$noua" == "1" ]] && echo "--noua" || echo "")
 
 # get URL
 # HACK adding `--noua` seems to fix the `[ERROR] HTTP Error 202: Accepted`
 # shellcheck disable=2086
-response=$(ddgr $ddgr_args --num=1 --json --reg="$region" "$query")
+response=$(ddgr --unsafe $extra_args --num=1 --json --reg="$region" "$query")
 url=$(echo "$response" | grep "url" | cut -d'"' -f4)
 mdlink="[$query]($url)"
 
