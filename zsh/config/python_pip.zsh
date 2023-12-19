17a3dbf51e26bf996cee846b738a2dbfb2e915d3
🤖 automated <cron@job>
2023-12-19 12:20:47 +0100

    iMac Home (10)

--- zsh/config/python_pip.zsh
+++ zsh/config/python_pip.zsh
@@ -2,7 +2,7 @@ alias pu="pip uninstall"
 alias pi="pip install"
 alias pl="pip list --not-required"
 alias py="python3"
-alias by="wezterm cli spawn -- bpython"
+alias bye="wezterm cli spawn -- bpython"
 
 #───────────────────────────────────────────────────────────────────────────────
 
