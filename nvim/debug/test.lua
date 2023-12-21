26fe00c4222c6fc7ade855e45b46bd07d49e45fc
🤖 automated <cron@job>
2023-12-21 01:01:07 +0100

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -4,7 +4,10 @@
 local anHTTPRequest = 222
 local anHttpRequest = 222
 
+-- vim.notify("🪚 anHTTPRequest: " .. tostring(anHTTPRequest))
+-- vim.notify("🪚 anHttpRequest: " .. tostring(anHttpRequest))
+-- ALL_UPPER_CASE_WORD = "%u%u+"
 
 
-vim.notify("🪚 anHTTPRequest: " .. tostring(anHTTPRequest))
-vim.notify("🪚 anHttpRequest: " .. tostring(anHttpRequest))
+vim.keymap.set("i", "<C-f>", "<Esc><cmd>lua require('spider').motion('w')<CR>i")
+vim.keymap.set("i", "<C-b>", "<Esc><cmd>lua require('spider').motion('b')<CR>i")
