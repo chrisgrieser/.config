9f13afb47b5a3a528f8bd9e59e7af9fba31f969b
🤖 automated <cron@job>
2023-12-21 13:00:56 +0100

    mini Office (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -4,10 +4,6 @@
 local anHTTPRequest = 222
 local anHttpRequest = 222
 
--- vim.notify("🪚 anHTTPRequest: " .. tostring(anHTTPRequest))
--- vim.notify("🪚 anHttpRequest: " .. tostring(anHttpRequest))
+vim.notify("🪚 anHTTPRequest: " .. tostring(anHTTPRequest))
+vim.notify("🪚 anHttpRequest: " .. tostring(anHttpRequest))
 -- ALL_UPPER_CASE_WORD = "%u%u+"
-
-
-vim.keymap.set("i", "<C-f>", "<Esc><cmd>lua require('spider').motion('w')<CR>i")
-vim.keymap.set("i", "<C-b>", "<Esc><cmd>lua require('spider').motion('b')<CR>i")
--- nvim/lua/plugins/motions-and-textobjects.lua
+++ nvim/lua/plugins/motions-and-textobjects.lua
@@ -23,6 +23,11 @@ return {
 	},
 	{ -- CamelCase Motion plus
 		"chrisgrieser/nvim-spider",
+		dependencies = {
+			"theHamsta/nvim_rocks",
+			build = "pip3 install --user hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
+			config = function() require("nvim_rocks").ensure_installed("luautf8") end,
+		},
 		keys = {
 			{
 				"e",
