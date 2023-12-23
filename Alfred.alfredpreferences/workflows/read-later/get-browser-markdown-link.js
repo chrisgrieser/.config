419ed6735e174af34ca7e46b7d0523827a5535b9
🤖 automated <cron@job>
2023-12-23 16:54:28 +0100

    iMac Mother (5)

--- Alfred.alfredpreferences/workflows/read-later/get-browser-markdown-link.js
+++ Alfred.alfredpreferences/workflows/read-later/get-browser-markdown-link.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env osascript -l JavaScript
-
+ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @param {string} filepath @param {string} text */
+function writeToFile(filepath, text) {
+	const str = $.NSString.alloc.initWithUTF8String(text);
+	str.writeToFileAtomicallyEncodingError(filepath, true, $.NSUTF8StringEncoding, null);
+}
+
+/** @param {string} path */
+function readFile(path) {
+	const data = $.NSFileManager.defaultManager.contentsAtPath(path);
+	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
+	return ObjC.unwrap(str);
+}
 
 function browserTab() {
 	const frontmostAppName = Application("System Events")
@@ -20,10 +34,14 @@ function browserTab() {
 	const webkitVariants = ["Safari", "Webkit"];
 	let title, url;
 	if (chromiumVariants.some((appName) => frontmostAppName.startsWith(appName))) {
+		// @ts-ignore
 		url = frontmostApp.windows[0].activeTab.url();
+		// @ts-ignore
 		title = frontmostApp.windows[0].activeTab.name();
 	} else if (webkitVariants.some((appName) => frontmostAppName.startsWith(appName))) {
+		// @ts-ignore
 		url = frontmostApp.documents[0].url();
+		// @ts-ignore
 		title = frontmostApp.documents[0].name();
 	} else {
 		app.displayNotification("", {
@@ -37,6 +55,17 @@ function browserTab() {
 
 //──────────────────────────────────────────────────────────────────────────────
 
-const { title, url } = browserTab();
-const mdLinkTask = `- [ ] [${title}](${url})`;
-mdLinkTask; // direct return
+/** @type {AlfredRun} */
+// biome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run() {
+	const { title, url } = browserTab();
+	const mdLinkTask = `- [ ] [${title}](${url})`;
+
+	// append
+	const filepath = $.getenv("read_later_file");
+	const currentFile = readFile(filepath)
+	const newFile = currentFile.trim() + "\n" + mdLinkTask;
+	writeToFile(filepath, newFile);
+
+	return title; // for Alfred notification
+}
