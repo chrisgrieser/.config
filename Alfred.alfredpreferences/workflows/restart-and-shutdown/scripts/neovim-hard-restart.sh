0f69380a0211d95127751f40263e37e0ebed1af8
🤖 automated <cron@job>
2023-12-24 18:51:19 +0100

    iMac Mother (6)

--- Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/neovim-hard-restart.sh
+++ Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/neovim-hard-restart.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 if killall -9 neovide nvim osascript "Automator Application Stub"; then
-	delay 0.3
+	sleep 0.2
 	echo -n "Force restarting neovim…" # Alfred notification
+	open -a "Neovide"
+	sleep 0.1
 	osascript -e 'tell application "Neovide" to activate' # `open -a` does not focus properly
 else
 	echo -n "Could not kill neovide." # Alfred notification
