5fc0827d201da2ba4398ba00412db913dd5f64a3
🤖 automated <cron@job>
2023-12-24 17:21:19 +0100

    iMac Mother (11)

--- nvim/debug/python test/test.py
+++ nvim/debug/python test/test.py
@@ -1,6 +1,28 @@
+"""B."""
 
 import re
+from time import time
 
-str = "hello world hotel"
 
-print(str)
+def ff(x: int) -> int:
+    """fsfsfsf."""
+    return x
+
+
+stri = "hello world hotel"
+stri = re.sub(r"^h", "XXX", stri)
+
+ooooo = {
+    "fff": 222,
+    "fffff": 222,
+} 
+
+print(ooooo["fff"])
+
+
+start = time()
+
+print(23 * ff(222))
+
+end = time()
+print(end - start)
