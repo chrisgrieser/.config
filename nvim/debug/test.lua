6dd757508a14cd6e84df4372e344847f7a43d90c
🤖 automated <cron@job>
2023-12-24 22:48:00 +0100

    iMac Mother (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,12 +1,5 @@
-local query = vim.treesitter.query.parse(
-	"markdown",
-	[[
-		(fenced_code_block) @codeblock
 
-		(block_quote_marker) @quote
-		(block_quote (paragraph (inline (block_continuation) @quote)))
-		(block_quote (paragraph (block_continuation) @quote))
-		(block_quote (block_continuation) @quote)
-	]]
-)
-vim.notify("🪚 query: " .. tostring(query))
+local bufPath = vim.api.nvim_buf_get_name(0)
+local rootFile = vim.fs.find({".git", "Makefile"}, { upward = true, path = bufPath })[1]
+local rootFolder = vim.fs.dirname(rootFile)
+vim.notify("🪚 rootFolder: " .. tostring(rootFolder))
