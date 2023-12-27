7f2d01d9a7de2824d2225ba2bcecde35ba14afd2
🤖 automated <cron@job>
2023-12-27 17:57:12 +0100

    iMac Home (10)

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -9,32 +9,35 @@ u.ftAbbr("!=", "~=")
 u.ftAbbr("!==", "~=")
 
 -- shorthands
-u.ftAbbr("tre", "then return end")
 u.ftAbbr("ree", "return end")
+u.ftAbbr("tree", "then return end")
 
 --------------------------------------------------------------------------------
 
 -- if in nvim dir, reload file/plugin, otherwise run `make`
 vim.keymap.set("n", "<leader>m", function()
 	vim.cmd("silent! update")
-	local isNvimConfig = vim.loop.cwd() == vim.fn.stdpath("config")
-	local filepath = vim.api.nvim_buf_get_name(0)
 
-	--- GUARD
+	-- :make
+	local isNvimConfig = vim.loop.cwd() == vim.fn.stdpath("config")
+	if not isNvimConfig then
+		vim.cmd.lmake()
+		return
+	end
+
+	-- reload nvim-file
+	local filepath = vim.api.nvim_buf_get_name(0)
 	if filepath:find("nvim/after/ftplugin/") then
 		u.notify("", "ftplugins cannot be reloaded. Just `:edit` buffer again.", "warn")
 		return
 	elseif filepath:find("nvim/lua/.*keymap") or filepath:find("nvim/lua/.*keybinding") then
 		u.notify("", "keymaps cannot be reloaded due to `map-unique`", "warn")
 		return
-	-- unload from lua cache (assuming that the pwd is ~/.config/nvim)
 	elseif isNvimConfig then
+		-- unload from lua cache (assuming that the pwd is ~/.config/nvim)
 		local packageName = vim.fn.expand("%:r"):gsub("lua/", ""):gsub("/", ".")
 		package.loaded[packageName] = nil
 		vim.cmd.source()
 		u.notify("Re-sourced", packageName)
-	-- run `make`
-	else
-		vim.cmd.lmake()
 	end
-end, { buffer = true, desc = " Reload /  Make" })
+end, { buffer = true, desc = " Reload /  Make" })
--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,29 +1,3 @@
-vim.opt_local.grepprg = "rg --vimgrep --no-column"
 
-vim.ui.input({
-	prompt = "Grep",
-	default = vim.fn.expand("<cword>"),
-}, function(input)
-	if not input then return end
-
-	vim.cmd("silent grep " .. input)
-	vim.cmd("copen 13") -- to preview results
-
-	local cmd = (":cdo s/%s//I"):format(input)
-	vim.api.nvim_feedkeys(cmd, "i", true)
-
-	-- position cursor
-	local left2 = vim.api.nvim_replace_termcodes("<Left><Left>", true, false, true)
-	vim.defer_fn(function() vim.api.nvim_feedkeys(left2, "i", false) end, 100)
-
-	vim.api.nvim_create_autocmd("CmdlineLeave", {
-		once = true,
-		callback = function()
-			vim.cmd.cclose()
-			vim.defer_fn(vim.cmd.cfirst, 1)
-		end,
-	})
-end)
-
--- MAYBE
--- MAYBE
+-- AAAAPP
+-- AAAAPP
