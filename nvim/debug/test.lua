a0b37c853e99f17f1458d104fedc9fb87671d00d
🤖 automated <cron@job>
2023-12-28 23:27:10 +0100

    iMac Home (4)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -6,7 +6,9 @@ local list = {
 	"four",
 	"five",
 } 
-local splitAt = 2
+local splitAt = 1
 
 local part1 = vim.list_slice(list, 1, splitAt)
+vim.notify("🪚 part1: " .. vim.inspect(part1))
 local part2 = vim.list_slice(list, splitAt + 1, #list)
+vim.notify("🪚 part2: " .. vim.inspect(part2))
