93d658b93c83d4004f349d8234bf5ceebbc25d56
🤖 automated <cron@job>
2023-12-31 19:08:30 +0100

    iMac Home (1)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,9 +1,12 @@
 
 -- NVIM-SCISSORS DEMO
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "python",
-	callback = function()
-		print("foobar")
-	end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = "python",
+-- 	callback = function()
+-- 		print("foobar")
+-- 	end,
+-- })
+
+local out = vim.split("foobar", "\n")
+vim.notify("🪚 out: " .. vim.inspect(out))
