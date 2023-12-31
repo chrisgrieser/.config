6c2593ba80c2f13ca4b64c4c96288b692c7366af
🤖 automated <cron@job>
2023-12-31 18:08:30 +0100

    iMac Home (6)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,28 +1,10 @@
--- vim.fn.matchadd("WarningMsg", [[\w\+\.lua:\d\+\ze:]]) -- \ze: lookahead
--- vim.fn.matchadd("WarningMsg", [[\w\+\.lua:\d\+\ze:]]) -- \ze: lookahead
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+vim.fn.matchadd("DiagnosticVirtualTextInfo", [[\${\d|.\{-}|}]]) 
 
--- .../nvim-data/lazy/nvim-chainsaw/lua/chainsaw/init.lua:31: attempt to call field 'normal' (a nil value)
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "noice",
-	callback = function(ctx)
-		local bufnr = ctx.buf
 
-		-- highlight name + line number
-		vim.api.nvim_buf_call(bufnr, function()
-			vim.fn.matchadd("WarningMsg", [[\w\+\.lua:\d\+\ze:]]) -- \ze: lookahead
-		end)
 
-		-- copy line number
-		vim.defer_fn(function()
-			if not vim.api.nvim_buf_is_valid(bufnr) then return end
-			local bufText = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false), "\n")
-			local lineNum = bufText:match("%w+%.lua:(%d+):")
-			if lineNum then vim.fn.setreg("+", lineNum) end
-		end, 1)
-	end,
-})
-
-local msg =
-	".../nvim-chainsaw/lua/chainsaw/init.lua:99: attempt to call field 'normal' (a nil value)\n"
-vim.notify(msg:rep(12))
+-- `${1|foo,bar|}`
