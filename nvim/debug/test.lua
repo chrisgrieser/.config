006eeeaed3b304acf45eb891cce0a6cc1031aa01
🤖 automated <cron@job>
2024-01-01 13:25:00 +0100

    iMac Home (1)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -4,4 +4,4 @@
 local record = vim.notify("test", vim.log.levels.INFO, { timeout = false })
 vim.notify("🪚 record: " .. vim.inspect(record))
 
-require("notify").dismiss({ silent = true })
+require("notify").dismiss({ id = 11 })
