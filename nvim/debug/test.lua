7cb1431b17630a9f54fc89fd687e91b1af75db02
🤖 automated <cron@job>
2024-01-01 12:55:00 +0100

    iMac Home (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,17 +1,7 @@
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
--- https://github.com/chrisgrieser/gitfred
--- https://github.com/chrisgrieser/gitfred
--- https://github.com/chrisgrieser/gitfred
 
-local urlPattern = require("various-textobjs.charwise-textobjs").urlPattern
+local record = vim.notify("test", vim.log.levels.INFO, { timeout = false })
+vim.notify("🪚 record: " .. vim.inspect(record))
 
-local bufText = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "\n")
-local urls = {}
-for url in bufText:gmatch(urlPattern) do
-	table.insert(urls, url)
-end
-
-vim.ui.select(urls, { prompt = "Select URL:" }, function (choice)
-	if choice then vim.fn.system { "open", choice } end
-end)
+require("notify")(record, vim.log.levels.INFO, { title = "test" }
