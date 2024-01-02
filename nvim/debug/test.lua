431a5105f10c94f5d9c4e18e5a1b017356fdb526
🤖 automated <cron@job>
2024-01-02 13:56:41 +0100

    iMac Home (4)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,3 +1,4 @@
 
-aut
-	local authorInitials = author:find("%s") and author:sub(1, 1) .. author:match("%s(%S)") or author:sub(1, 2)
+local author = "Chris Grieser fsfsf"
+local authorInitials = author:find("%s") and author:sub(1, 1) .. author:match("%s(%S)") or author:sub(1, 2)
+vim.notify("🪚 authorInitials: " .. tostring(authorInitials))
