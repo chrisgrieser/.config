66143eaaaeb7eda192c9f8108880f2e99570394f
🤖 automated <cron@job>
2024-01-06 18:30:20 +0100

    iMac Home (1)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -7,5 +7,9 @@ vim.keymap.set("n", "lhs", "rhs")
 vim.keymap.set("n", "lhs", "rhs", { desc = "description" })
 
 
-local a = "1 + 2222" .. "1111 + 2"
-local b = (1 + 2222) .. (1111 + 2)
+local a = "foobar"
+local c = "hello"
+local b = (("%s bla %%s bla %s"):format(a, c))
+vim.notify("🪚 b: " .. tostring(b))
+
+
