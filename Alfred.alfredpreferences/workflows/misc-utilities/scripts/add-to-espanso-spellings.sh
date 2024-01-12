ecd27dec46ee90c658f670018627e80067275bde
🤖 automated <cron@job>
2024-01-12 15:44:51 +0100

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/misc-utilities/scripts/add-to-espanso-spellings.sh
+++ Alfred.alfredpreferences/workflows/misc-utilities/scripts/add-to-espanso-spellings.sh
@@ -2,25 +2,15 @@
 
 # CONFIG
 spell_config="$HOME/.config/espanso/match/spelling.yml"
-
 #───────────────────────────────────────────────────────────────────────────────
 
 wrong=$(echo "$*" | cut -d" " -f1)
 correct=$(echo "$*" | cut -d" " -f2)
-
-{
-	echo "  - trigger: $wrong"
-	echo "    replace: $correct"
-	echo "    propagate_case: true"
-	echo "    word: true"
-} >>"$spell_config"
+new_line="  - { trigger: $wrong, replace: $correct, propagate_case: true, word: true }"
+echo "$new_line" >>"$spell_config"
 
 # shellcheck disable=2154
-[[ "$open" == "false" ]] && return 0
-
-#───────────────────────────────────────────────────────────────────────────────
-
-LINE_NO=$(wc -l "$spell_config" | tr -s " " | cut -d" " -f2)
-# workaround for https://github.com/neovide/neovide/issues/1604
-# shellcheck disable=2086
-open "$spell_config" --env=LINE=$LINE_NO
+if [[ "$open" == "true" ]]; then
+	line_no=$(wc -l "$spell_config" | tr -s " " | cut -d" " -f2)
+	open "$spell_config" --env=LINE="$line_no"
+fi
