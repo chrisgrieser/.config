2cb39ab0af5959bc2c1239cb4ffe50dc4cdee4bb
🤖 automated <cron@job>
2024-01-16 10:01:58 +0100

    iMac Home (4)

--- sketchybar/reminders-count.sh
+++ sketchybar/reminders-count.sh
@@ -13,9 +13,11 @@ fi
 # https://leancrew.com/all-this/2017/08/my-jxa-problem/
 # https://developer.apple.com/library/archive/releasenotes/InterapplicationCommunication/RN-JavaScriptForAutomation/Articles/OSX10-10.html#//apple_ref/doc/uid/TP40014508-CH109-SW10
 remindersToday=$(osascript -l JavaScript -e '
-	const rem = Application("Reminders");
-	const remindersToday = rem.defaultList().reminders.whose({
-		dueDate: { _lessThan: new Date() },
+	const today = new Date()
+	const tomorrow = new Date(today)
+	tomorrow.setDate(tomorrow.getDate() + 1)
+	const remindersToday = Application("Reminders").defaultList().reminders.whose({
+		dueDate: { _lessThan: tomorrow },
 		completed: false,
 	});
 	remindersToday.length;
