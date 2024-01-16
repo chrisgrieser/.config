fb2ed4c96aaea76690794a35bdfb8a64ba08c1a2
🤖 automated <cron@job>
2024-01-16 18:51:16 +0100

    mini Office (2)

--- sketchybar/reminders-count.sh
+++ sketchybar/reminders-count.sh
@@ -13,11 +13,10 @@ fi
 # https://leancrew.com/all-this/2017/08/my-jxa-problem/
 # https://developer.apple.com/library/archive/releasenotes/InterapplicationCommunication/RN-JavaScriptForAutomation/Articles/OSX10-10.html#//apple_ref/doc/uid/TP40014508-CH109-SW10
 remindersToday=$(osascript -l JavaScript -e '
-	const today = new Date()
-	const tomorrow = new Date(today)
-	tomorrow.setDate(tomorrow.getDate() + 1)
+	const endOfToday = new Date();
+	endOfToday.setHours(23, 59, 59, 0); // to include reminders in the afternoon
 	const remindersToday = Application("Reminders").defaultList().reminders.whose({
-		dueDate: { _lessThan: tomorrow },
+		dueDate: { _lessThan: endOfToday },
 		completed: false,
 	});
 	remindersToday.length;
