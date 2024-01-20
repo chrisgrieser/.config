d9ab82fb62778967b2bee27d514da6dfa7e52a7c
🤖 automated <cron@job>
2024-01-20 21:49:21 +0100

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/alfred-dev/scripts/open-cache-or-downloads.sh
+++ Alfred.alfredpreferences/workflows/alfred-dev/scripts/open-cache-or-downloads.sh
@@ -3,7 +3,8 @@
 cache_folder="$HOME/Library/Caches/com.runningwithcrayons.Alfred/Workflow Data"
 data_folder="$HOME/Library/Application Support/Alfred/Workflow Data"
 current_workflow_uid=$(sed -n "4p" "$HOME/Library/Application Support/Alfred/history.json" | cut -d'"' -f2)
-current_workflow_bundleid=$(grep -A1 "bundleid" "../$current_workflow_uid/info.plist" | tail -1 | cut -d">" -f2 | cut -d"<" -f1)
+current_workflow_bundleid=$(grep -A1 "<key>bundleid</key>" "../$current_workflow_uid/info.plist" |
+	tail -1 | cut -d">" -f2 | cut -d"<" -f1)
 
 which_one="$*"
 
@@ -12,4 +13,3 @@ if [[ "$which_one" == "cache" ]]; then
 elif [[ "$which_one" == "data" ]]; then
 	open "$data_folder/$current_workflow_bundleid" || open "$data_folder"
 fi
-
