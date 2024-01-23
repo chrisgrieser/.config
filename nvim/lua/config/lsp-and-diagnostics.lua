ed5b46d3c949cc21e6d89be97fe2fc22260564ac
🤖 automated <cron@job>
2024-01-23 15:11:24 +0100

    mini Office (2)

--- nvim/lua/config/lsp-and-diagnostics.lua
+++ nvim/lua/config/lsp-and-diagnostics.lua
@@ -34,11 +34,12 @@ vim.lsp.handlers["textDocument/rename"] = function(err, result, ctx, config) ---
 	})
 end
 
--- `vim.lsp.buf.definition` jumps to first definition
+-- `vim.lsp.buf.definition` jumps to first definition instead of filling quickfixlist
 -- https://www.reddit.com/r/neovim/comments/19cvgtp/comment/kj1yevl/
+local originalDefinitionHandler = vim.lsp.handlers["textDocument/definition"]
 vim.lsp.handlers["textDocument/definition"] = function(err, result, ctx, config)
 	if type(result) == "table" then result = { result[1] } end
-	vim.lsp.handlers["textDocument/definition"](err, result, ctx, config)
+	originalDefinitionHandler(err, result, ctx, config)
 end
 
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
