0a7454ebfa147518f5894243908cc2b1b7092551
🤖 automated <cron@job>
2024-01-29 21:35:05 +0100

    iMac Home (5)

--- Alfred.alfredpreferences/workflows/alfred-pass/scripts/pass-cli-search.js
+++ Alfred.alfredpreferences/workflows/alfred-pass/scripts/pass-cli-search.js
@@ -4,18 +4,15 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 //──────────────────────────────────────────────────────────────────────────────
 
-const fileExists = (/** @type {string} */ filePath) => Application("Finder").exists(Path(filePath));
-
-//──────────────────────────────────────────────────────────────────────────────
-
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	let passwordStore = app.doShellScript('source "$HOME/.zshenv" ; echo "$PASSWORD_STORE_DIR"');
-	if (passwordStore === "") passwordStore = app.pathTo("home folder") + "/.password-store";
-	console.log("👽 passwordStore:", passwordStore);
+	const passwordStore =
+		app.doShellScript('source "$HOME/.zshenv" ; echo "$PASSWORD_STORE_DIR"') ||
+		app.pathTo("home folder") + "/.password-store";
 
-	if (!fileExists(passwordStore)) {
+	// GUARD
+	if (!Application("Finder").exists(Path(passwordStore))) {
 		return JSON.stringify({
 			items: {
 				title: "⚠️ Password Store not found.",
@@ -27,7 +24,7 @@ function run() {
 
 	/** @type{AlfredItem[]} */
 	const passwords = app
-		.doShellScript(`cd "${passwordStore}" ; find . -name "*.gpg"`)
+		.doShellScript(`cd "${passwordStore}" ; find . -type f -name "*.gpg" -not -path "./.git*"`)
 		.split("\r")
 		.map((gpgFile) => {
 			const id = gpgFile.slice(2, -4);
@@ -52,54 +49,17 @@ function run() {
 		});
 
 	// new password
+	const disallowed = { subtitle: "🚫 Not possible for new password.", valid: false };
 	passwords.push({
-		
-	})
-
-	// let createNewPassword;
-	// if (passwordlist) {
-	// 	createNewPassword = false;
-	// 	for (const gpgFile of passwordlist.split("\r")) {
-	// 		const id = gpgFile.slice(2, -4);
-	// 		const parts = id.split("/");
-	// 		const name = parts.pop();
-	// 		const group = parts.join("/");
-	// 		const path = `${passwordStore}/${group}/${name}.gpg`;
-	//
-	// 		passwords.push({
-	// 			title: name,
-	// 			subtitle: group,
-	// 			arg: id,
-	// 			uid: id,
-	// 			mods: {
-	// 				// move id to variable for Alfred Script Filter
-	// 				shift: {
-	// 					variables: { entry: id },
-	// 					arg: "",
-	// 				},
-	// 				alt: { arg: path },
-	// 			},
-	// 		});
-	// 	}
-	// } else {
-	// 	createNewPassword = true;
-	// 	const cleanQuery = query.replace(/[/\\:]/, "-");
-	// 	const disallowed = { subtitle: "🚫 Not possible for new password.", valid: false };
-	// 	passwords.push({
-	// 		title: "🆕 " + query,
-	// 		subtitle: "Create new password",
-	// 		arg: cleanQuery,
-	// 		mods: {
-	// 			cmd: disallowed,
-	// 			shift: disallowed,
-	// 			alt: disallowed,
-	// 			ctrl: disallowed,
-	// 		},
-	// 	});
-	// }
-
-	return JSON.stringify({
-		// variables: { createNewPassword: createNewPassword }, // boolean passed as 1/0
-		items: passwords,
+		title: "Create New Password",
+		arg: "",
+		mods: {
+			cmd: disallowed,
+			shift: disallowed,
+			alt: disallowed,
+			ctrl: disallowed,
+		},
 	});
+
+	return JSON.stringify({ items: passwords });
 }
--- Alfred.alfredpreferences/workflows/alfred-pass/scripts/pw-generate.sh
+++ Alfred.alfredpreferences/workflows/alfred-pass/scripts/pw-generate.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env zsh
 
-[[ "$*" == "* root" ]] && folder="" || folder="$*/"
-entry_name=$(echo "$entry_name" | tr -d ":/\\") # remove illegal characters
+entry_name=$(echo "$*" | tr -d ":/\\") # remove illegal characters
 
 # shellcheck disable=2154
-if [[ "$generatePassword" -eq 1 ]]; then
-	pass generate --no-symbols "$folder$entry_name" &>/dev/null
+if [[ "$generatePassword" == "true" ]]; then
+	pass generate --no-symbols "$folder/$entry_name" &>/dev/null
 
 	# pass to Alfred for copying
-	echo -n "$(pass show "$folder$entry_name" | head -n1)"
+	echo -n "$(pass show "$folder/$entry_name" | head -n1)"
 else
 	# echo needed to skip confirmation, so not to pass password again
-	pbpaste | pass insert --echo "$folder$entry_name" &>/dev/null
-	open -R "$folder$entry_name" # show folder as form of confirmation
+	pbpaste | pass insert --echo "$folder/$entry_name" &>/dev/null
+	open -R "$folder/$entry_name" # show folder as form of confirmation
 fi
 
 # shellcheck disable=2154
