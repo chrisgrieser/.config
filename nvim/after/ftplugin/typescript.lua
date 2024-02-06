cee41a7e6bdb4bcbb78ef58c3fc276e184b59983
🤖 automated <cron@job>
2024-02-06 22:12:54 +0100

    iMac Home (2)

--- nvim/after/ftplugin/typescript.lua
+++ nvim/after/ftplugin/typescript.lua
@@ -3,5 +3,5 @@ local u = require("config.utils")
 
 -- fix my habits
 u.ftAbbr("cosnt", "const")
-u.ftAbbr("local", "const") 
-u.ftAbbr("--", "//") 
+u.ftAbbr("local", "const")
+u.ftAbbr("--", "//")
