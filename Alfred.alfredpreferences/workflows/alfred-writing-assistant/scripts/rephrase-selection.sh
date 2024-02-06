037916a17b43f64b5e70f3a9b1e95e613369ee2c
🤖 automated <cron@job>
2024-02-06 18:25:18 +0100

    mini Office (2)

--- Alfred.alfredpreferences/workflows/alfred-writing-assistant/scripts/rephrase-selection.sh
+++ Alfred.alfredpreferences/workflows/alfred-writing-assistant/scripts/rephrase-selection.sh
@@ -24,7 +24,7 @@ the_prompt=$(echo "$static_prompt $selection" | sed -e 's/"/\\"/g')
 
 # OPENAI API CALL
 # DOCS https://platform.openai.com/docs/api-reference/making-requests
-response=$(curl --silent --max-time=0.5 https://api.openai.com/v1/chat/completions \
+response=$(curl --silent --max-time 15 https://api.openai.com/v1/chat/completions \
 	-H "Content-Type: application/json" \
 	-H "Authorization: Bearer $apikey" \
 	-d "{ \"model\": \"$model\", \"messages\": [{\"role\": \"user\", \"content\": \"$the_prompt\"}], \"temperature\": $temperature }")
@@ -34,7 +34,7 @@ echo "$response" >&2
 
 # GUARD
 if [[ -z "$response" ]]; then
-	echo "ERROR: No response by OpenAI API."
+	echo "ERROR: Timeout, no response by OpenAI API."
 elif [[ "$response" =~ "error" || "$response" =~ "ERROR" ]]; then
 	# doing this avoids jq dependency
 	echo -n "ERROR: $(echo "$response" | grep '"message"' | cut -d'"' -f4)"
