c882b9a0748b7f3d981e616bca8ba17fe2b68ff7
🤖 automated <cron@job>
2024-02-08 23:47:58 +0100

    iMac Home (1)

--- nvim/after/ftplugin/typescript.lua
+++ nvim/after/ftplugin/typescript.lua
@@ -5,3 +5,19 @@ local u = require("config.utils")
 u.ftAbbr("cosnt", "const")
 u.ftAbbr("local", "const")
 u.ftAbbr("--", "//")
+u.ftAbbr("===", "==")
+u.ftAbbr("!==", "~=")
+
+--------------------------------------------------------------------------------
+
+vim.keymap.set("n", "<localleader><localleader>", function()
+	local toEvaluate
+	if vim.fn.mode() == "n" then
+		toEvaluate = vim.trim(vim.api.nvim_get_current_line())
+	else
+		u.normal('"zy')
+		toEvaluate = vim.fn.getreg("z")
+	end
+	local uri = "obsidian://advanced-uri?eval=" .. toEvaluate
+	vim.fn.system { "open", uri }
+end)
