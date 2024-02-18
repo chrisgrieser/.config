914b82779c50d0521b29ba218af280033196619e
🤖 automated <cron@job>
2024-02-18 13:51:49 +0100

    iMac Home (2)

--- zsh/config/keybindings_and_vim_mode.zsh
+++ zsh/config/keybindings_and_vim_mode.zsh
@@ -36,8 +36,31 @@ bindkey -M viins "^[[1;3C" forward-word
 
 #───────────────────────────────────────────────────────────────────────────────
 # VI MODE
+bindkey -v
 export KEYTIMEOUT=1 # no delay when pressing <Esc>
 
+# Change cursor shape for different vi modes.
+function zle-keymap-select {
+	if [[ ${KEYMAP} == vicmd ]] ||
+		[[ $1 = 'block' ]]; then
+		echo -ne '\e[1 q'
+	elif [[ ${KEYMAP} == main ]] ||
+		[[ ${KEYMAP} == viins ]] ||
+		[[ ${KEYMAP} = '' ]] ||
+		[[ $1 = 'beam' ]]; then
+		echo -ne '\e[5 q'
+	fi
+}
+zle -N zle-keymap-select
+
+zle-line-init() { echo -ne "\e[5 q"; }
+zle -N zle-line-init
+
+_fix_cursor() { echo -ne '\e[5 q'; }
+precmd_functions+=(_fix_cursor)
+
+#───────────────────────────────────────────────────────────────────────────────
+# VI MODE BINDINGS
 bindkey -M vicmd 'k' up-line # disable accidentally searching history
 
 bindkey -M vicmd 'L' vi-end-of-line
