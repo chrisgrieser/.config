d86fbcc8d1456346bf3d95a3bea79dfb65115022
🤖 automated <cron@job>
2024-02-22 14:47:30 +0100

    mini Office (1)

--- nvim/queries/comment/highlights.scm
+++ nvim/queries/comment/highlights.scm
@@ -29,6 +29,7 @@
 
 ;──────────────────────────────────────────────────────────────────────────────
 
+; not working with special characters in `here`, 
+; see https://github.com/stsewd/tree-sitter-comment/issues/34
 ("text" @markup.raw.markdown_inline
  (#match? @markup.raw.markdown_inline "`.+`"))
-; TODO #23232 ffsfsf `ObjCfsfsf` `fsfsffff` 
