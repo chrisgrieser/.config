7eb5a9a525ecc407dead67cb243c8d2114d3729b
🤖 automated <cron@job>
2024-02-25 20:18:23 +0100

    iMac Home (2)

--- nvim/lua/plugins/ai-plugins.lua
+++ nvim/lua/plugins/ai-plugins.lua
@@ -6,61 +6,81 @@ local u = require("config.utils")
 --------------------------------------------------------------------------------
 
 return {
-	-- AI Ghost-Text Suggestions
-	"Exafunction/codeium.vim",
-	event = "InsertEnter",
-	build = function()
-		-- symlink to enable syncing of API key
-		local symLinkFrom = vim.env.DATA_DIR .. "/private dotfiles/codium-api-key.json"
-		local symLinkTo = os.getenv("HOME") .. "/.codeium/config.json"
-		local fileExists = vim.loop.fs_stat(symLinkTo) ~= nil
-		if not fileExists then
-			pcall(vim.fn.mkdir, vim.fs.dirname(symLinkTo))
-			vim.loop.fs_symlink(symLinkFrom, symLinkTo)
-		end
-
-		-- FIX accumulated leftover binaries https://github.com/Exafunction/codeium.vim/issues/200
-		local bin_path = os.getenv("HOME") .. "/.codeium/bin"
-		local oldBinaries =
-			vim.fs.find("language_server_macos_arm", { limit = math.huge, path = bin_path })
-		table.remove(oldBinaries) -- remove last item (= current binary) from list to keep it
-		for _, binaryPath in pairs(oldBinaries) do
-			os.remove(binaryPath)
-			os.remove(vim.fs.dirname(binaryPath))
-		end
-	end,
-	keys = {
-		{
-			"<D-s>",
-			function() return vim.fn["codeium#Accept"]() end,
-			mode = "i",
-			expr = true,
-			silent = true,
-			desc = "󰚩 Accept Suggestion",
+	{
+		"Bryley/neoai.nvim",
+		dependencies = "MunifTanjim/nui.nvim",
+		cmd = {
+			"NeoAI",
+			"NeoAIOpen",
+			"NeoAIClose",
+			"NeoAIToggle",
+			"NeoAIContext",
+			"NeoAIContextOpen",
+			"NeoAIContextClose",
+			"NeoAIInject",
+			"NeoAIInjectCode",
+			"NeoAIInjectContext",
+			"NeoAIInjectContextCode",
 		},
-		{
-			"<leader>oa",
-			function()
-				vim.g.codeium_enabled = vim.g.codeium_enabled == false
-				local newState = vim.g.codeium_enabled and "enabled" or "disabled"
-				u.notify("Codium", "󰚩 Suggestions " .. newState .. ".")
-			end,
-			desc = "󰚩 Codium Suggestions",
-		},
+		opts = {},
 	},
-	config = function()
-		u.addToLuaLine("sections", "lualine_x", function()
-			-- only display activity
-			local status = vim.fn["codeium#GetStatusString"]()
-			if not status:find("%*") then return "" end
-			return "󰚩 …"
-		end)
+	{
+		-- AI Ghost-Text Suggestions
+		"Exafunction/codeium.vim",
+		event = "InsertEnter",
+		build = function()
+			-- symlink to enable syncing of API key
+			local symLinkFrom = vim.env.DATA_DIR .. "/private dotfiles/codium-api-key.json"
+			local symLinkTo = os.getenv("HOME") .. "/.codeium/config.json"
+			local fileExists = vim.loop.fs_stat(symLinkTo) ~= nil
+			if not fileExists then
+				pcall(vim.fn.mkdir, vim.fs.dirname(symLinkTo))
+				vim.loop.fs_symlink(symLinkFrom, symLinkTo)
+			end
 
-		vim.g.codeium_filetypes = {
-			TelescopePrompt = false,
-			DressingInput = false,
-			text = false, -- `pass` passwords are plaintext and should not be sent
-		}
-		vim.g.codeium_disable_bindings = 1
-	end,
+			-- FIX accumulated leftover binaries https://github.com/Exafunction/codeium.vim/issues/200
+			local bin_path = os.getenv("HOME") .. "/.codeium/bin"
+			local oldBinaries =
+				vim.fs.find("language_server_macos_arm", { limit = math.huge, path = bin_path })
+			table.remove(oldBinaries) -- remove last item (= current binary) from list to keep it
+			for _, binaryPath in pairs(oldBinaries) do
+				os.remove(binaryPath)
+				os.remove(vim.fs.dirname(binaryPath))
+			end
+		end,
+		keys = {
+			{
+				"<D-s>",
+				function() return vim.fn["codeium#Accept"]() end,
+				mode = "i",
+				expr = true,
+				silent = true,
+				desc = "󰚩 Accept Suggestion",
+			},
+			{
+				"<leader>oa",
+				function()
+					vim.g.codeium_enabled = vim.g.codeium_enabled == false
+					local newState = vim.g.codeium_enabled and "enabled" or "disabled"
+					u.notify("Codium", "󰚩 Suggestions " .. newState .. ".")
+				end,
+				desc = "󰚩 Codium Suggestions",
+			},
+		},
+		config = function()
+			u.addToLuaLine("sections", "lualine_x", function()
+				-- only display activity
+				local status = vim.fn["codeium#GetStatusString"]()
+				if not status:find("%*") then return "" end
+				return "󰚩 …"
+			end)
+
+			vim.g.codeium_filetypes = {
+				TelescopePrompt = false,
+				DressingInput = false,
+				text = false, -- `pass` passwords are plaintext and should not be sent
+			}
+			vim.g.codeium_disable_bindings = 1
+		end,
+	},
 }
--- zsh/plugins/magic_dashboard.zsh
+++ zsh/plugins/magic_dashboard.zsh
@@ -42,7 +42,7 @@ function _gitlog {
 				-e 's/tag: /  /g' \
 				-e 's/ -> /   /' \
 				-e 's/\* /· /' \
-				-Ee $'s/ (fix|refactor|build|ci|docs|feat|style|test|perf|chore|revert|break|improv|aider)(\\(.+\\)|!)?:/ \033[1;35m\\1\033[1;36m\\2\033[0;38;5;245m:\033[0m/' \
+				-Ee $'s/ (fix|refactor|build|ci|docs|feat|style|test|perf|chore|revert|break|improv|aider)(\\(.+\\))?(!?):/ \033[1;35m\\1\033[1;36m\\2\033[7;31m\\3\033[0;38;5;245m:\033[0m/' \
 				-Ee $'s/ (fixup|squash)!/\033[1;32m&\033[0m/g' \
 				-Ee $'s/`[^`]*`/\033[1;36m&\033[0m/g' \
 				-Ee $'s/#[0-9]+/\033[1;31m&\033[0m/g'
