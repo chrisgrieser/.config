3878f71d7f4bfff03a7864e6ce69f6c94148c1ec
🤖 automated <cron@job>
2024-02-26 21:36:05 +0100

    iMac Home (127)

--- Alfred.alfredpreferences/workflows/alfred-dev/scripts/update-alfred-preferences-icon.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-# Method described here does not work:
-# http://codefromabove.com/2015/03/programmatically-adding-an-icon-to-a-folder-or-file/
-
-prefs_path="/Applications/Alfred 5.app/Contents/Preferences/Alfred Preferences.app"
-icon_path="./Alfred Preferences.icns"
