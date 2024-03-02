0ace09b4a1471bdf78953d7e56102d6de2de85b0
🤖 automated <cron@job>
2024-03-02 11:39:18 +0100

    iMac Home (5)

---
+++ Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/devterms.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @param {string} url @return {string} */
+function httpRequest(url) {
+	const queryURL = $.NSURL.URLWithString(url);
+	const data = $.NSData.dataWithContentsOfURL(queryURL);
+	const requestStr = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding).js;
+	return requestStr;
+}
+
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @type {AlfredRun} */
+// biome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run(argv) {
+	const query = argv[0];
+	if (!query) {
+		return JSON.stringify({
+			items: [{ title: "Waiting for query…", valid: false }],
+		});
+	}
+
+	// DOCS https://devterms.io/api/docs#tag/default/get/api/v1/search
+	const apiURL = "https://devterms.io/api/v1/search?q=";
+	const response = JSON.parse(httpRequest(apiURL + encodeURIComponent(query)));
+
+	/** @type AlfredItem[] */
+	const terms = response.hits.map(
+		(/** @type {{ term: string; definition: string; example: string; id: string; }} */ hit) => {
+			return {
+				title: hit.term,
+				subtitle: `${hit.definition} · ${hit.example}`,
+				arg: "https://devterms.io/define/" + hit.term,
+				uid: hit.id,
+			};
+		},
+	);
+
+	return JSON.stringify({ items: terms });
+}
