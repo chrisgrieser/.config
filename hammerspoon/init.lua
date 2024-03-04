06529e3e757023a0c9baf3144551cc7557d949d2
🤖 automated <cron@job>
2024-03-04 03:45:47 +0100

    iMac Home (1)

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -32,13 +32,13 @@ safeRequire("lua.dark-mode")
 -- window management
 safeRequire("lua.window-utils")
 safeRequire("lua.layouts")
-safeRequire("lua.app-hider")
+-- safeRequire("lua.app-hider")
 safeRequire("lua.auto-quitter")
 
 -- system
 safeRequire("lua.console")
--- safeRequire("lua.cronjobs")
-safeRequire("lua.filesystem-watchers")
+safeRequire("lua.cronjobs")
+-- safeRequire("lua.filesystem-watchers")
 safeRequire("lua.repo-auto-sync")
 safeRequire("lua.hardware-periphery")
 safeRequire("lua.pageup-pagedown-scroll")
@@ -48,7 +48,7 @@ safeRequire("lua.vertical-split")
 -- app-specific
 safeRequire("lua.app-specific-behavior")
 safeRequire("lua.browser")
-safeRequire("lua.mastodon")
+-- safeRequire("lua.mastodon")
 
 -- reload function (should come last)
 safeRequire("lua.reload")
