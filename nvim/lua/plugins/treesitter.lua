4164ff949ae8de27c2ca422804fc71e38926ef5b
🤖 automated <cron@job>
2024-03-05 10:36:41 +0100

    iMac Home (1)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -10,6 +10,11 @@ return {
 
 		highlight = {
 			enable = true,
+			disable = function(lang, buf)
+				local max_filesize = 100 * 1024 -- 100 KB
+				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+				if ok and stats and stats.size > max_filesize then return true end
+			end,
 		},
 		indent = {
 			enable = true,
