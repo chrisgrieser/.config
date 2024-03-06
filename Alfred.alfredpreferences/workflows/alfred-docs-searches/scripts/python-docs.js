bc4c9cf2bbf137704471c2971d1d52dab44ea8c3
🤖 automated <cron@job>
2024-03-06 17:44:14 +0100

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/python-docs.js
+++ Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/python-docs.js
@@ -22,37 +22,37 @@ function readFile(path) {
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	// INFO HOW TO UPDATE SEARCH INDEX
-	// 1. Download it from https://docs.python.org/3/download.html
-	// 2. Frm the search, fetch the file `searchindex.js` and put it under the
-	// name below into this workflows `data` folder
-	// 3. Append the python version for documentation purposes
-	// TODO see how to automate this later
-	const searchIndexFile = "./data/searchindex_python-3.12.2.js";
+	// INFO 
+	// see the file `.data/HOWTO_update-python-searchindex` on how to update the index
+	const searchIndexFile = "./data/searchindex_python-3.12.2.json";
 
 	const pythonVersion = searchIndexFile.match(/\d\.\d+/)[0];
 	const baseUrl = `https://docs.python.org/${pythonVersion}/`;
-	// `slice` to remove the `Search.setIndex(…)` enclosing the json we want
-	const searchIndex = JSON.parse(readFile(searchIndexFile).slice(16, -1));
+	const searchIndex = JSON.parse(readFile(searchIndexFile));
 
 	const sites = [];
 	for (const [entryTitle, entry] of Object.entries(searchIndex.indexentries)) {
 		const [docId, documentTitle] = entry[0];
 		const doc = searchIndex.docnames[docId];
 		const url = baseUrl + doc + ".html#" + documentTitle;
-		const [_, title, area] = entryTitle.match(/(\S+)(?: \((.*)\))?/) || ["", "", ""];
+		const [full, area] = entryTitle.match(/ \((.+)\)$/) || [];
+		const title = area ? entryTitle.slice(0, -full.length) : entryTitle;
+
+		let matcher = alfredMatcher(title);
+		if (area) matcher += " " + alfredMatcher(area);
 
 		sites.push({
 			title: title,
-			match: alfredMatcher(title),
+			match: matcher,
 			subtitle: area,
 			arg: url,
+			uid: url,
 		});
 	}
 
 	return JSON.stringify({
 		items: sites,
 		// 1 week (using our offline index, so it's always up to date)
-		// cache: { seconds: 3600 * 24 * 7 },
+		cache: { seconds: 3600 * 24 * 7 },
 	});
 }
