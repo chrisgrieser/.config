04124cc5c351761b648957d0f300bcf9dde9d6fc
🤖 automated <cron@job>
2024-03-07 15:14:26 +0100

    iMac Mother (10)

--- Alfred.alfredpreferences/workflows/reminders-editor/scripts/edit-reminder.sh
+++ Alfred.alfredpreferences/workflows/reminders-editor/scripts/edit-reminder.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env zsh
-# shellcheck disable=2154 # Alfred variable
 
+title=$(echo "$*" | head -n1)
+body=$(echo "$*" | tail -n +2)
 
-
-
+# shellcheck disable=2154 # Alfred variables
 msg=$(reminders edit "$reminder_list" "$id" "$title" --notes "$body")
 echo "❗ $msg" >&2 # log msg in ALfred console
 
 echo -n "$title" # pass for notification
-
-reminders edit Default 1173144E-EF1C-4C52-BC50-9FDBC686B78F "hello world" --notes "ffffff"
