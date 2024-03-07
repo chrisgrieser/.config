e7749cc917da210790d84a56920b939018274271
🤖 automated <cron@job>
2024-03-07 14:44:25 +0100

    iMac Mother (10)

---
+++ Alfred.alfredpreferences/workflows/reminders-editor/scripts/select-reminder-time.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env osascript -l JavaScript
+
+/** @type {AlfredRun} */
+// biome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run(argv) {
+	const today = new Date();
+	const weekdayNum = today.getDay();
+
+	const daysUntilMonday = weekdayNum === 1 ? 7 : (1 - weekdayNum + 7) % 7;
+	const daysUntilWedday = weekdayNum === 3 ? 7 : (3 - weekdayNum + 7) % 7;
+	const daysUntilSunday = weekdayNum === 0 ? 7 : 7 - weekdayNum;
+
+	const query = argv[0];
+
+	const jsonArray = [
+		{
+			title: "Tomorrow",
+			variables: { selection: "Tomorrow" }, // label for notification
+			arg: 1,
+		},
+		{
+			title: "In 2 Days",
+			variables: { selection: "In 2 Days" },
+			arg: 2,
+		},
+		{
+			title: "In 7 days",
+			variables: { selection: "In 7 days" },
+			arg: 7,
+		},
+		{
+			title: "Next Monday",
+			variables: { selection: "Next Monday" },
+			arg: daysUntilMonday,
+		},
+		{
+			title: "Next Wednesday",
+			variables: { selection: "Next Wednesday" },
+			arg: daysUntilWedday,
+		},
+		{
+			title: "Next Sunday",
+			variables: { selection: "Next Sunday" },
+			arg: daysUntilSunday,
+		},
+		{
+			title: "In 2 weeks",
+			variables: { selection: "In 2 weeks" },
+			arg: 14,
+		},
+	];
+
+	return JSON.stringify({
+		variables: { reminderText: query },
+		items: jsonArray,
+	});
+}
--- Alfred.alfredpreferences/workflows/reminders-editor/scripts/select-reminders.js
+++ Alfred.alfredpreferences/workflows/reminders-editor/scripts/select-reminders.js
@@ -9,28 +9,27 @@ function run() {
 	const today = new Date().toISOString().slice(0, 10);
 	const list = $.getenv("reminder_list");
 
-	// index for usage with the reminder CLI not saved in the json
-	let index = 0;
-
 	/** @type AlfredItem[] */
 	const reminders = JSON.parse(
 		app.doShellScript(`reminders show "${list}" --due-date="${today}" --format="json"`),
 	).map((/** @type {{ title: string; notes: string; externalId: string; }} */ rem) => {
-		index++;
-		const { title, notes } = rem;
+		const { title, notes, externalId } = rem;
 		return {
 			title: title,
-			subtitle: notes.trim(),
-			arg: title + notes,
+			subtitle: (notes || "").trim(),
+			variables: { id: externalId },
+			arg: title, // complete
 			mods: {
-				cmd: { arg: index }, // complete
+				// edit
+				cmd: { arg: title + "\n" + notes },
 			},
 		};
 	});
 
 	// GUARD
-	if (reminders.length === 0)
+	if (reminders.length === 0) {
 		return JSON.stringify({ items: [{ title: "No reminders for today.", valid: false }] });
+	}
 
 	return JSON.stringify({ items: reminders });
 }
