f8f9d54b73aff511f41beb39abfdaee6ecca1634
🤖 automated <cron@job>
2024-03-10 10:46:36 +0100

    iMac Home (4)

--- + utility-scripts/multi-repo-updater.sh
+++ + utility-scripts/multi-repo-updater.sh
@@ -7,28 +7,45 @@ set -e # safe mode
 github_username="chrisgrieser"
 
 repos_to_update=$(gh repo list --limit=100 --no-archived --source --json="name" --jq=".[].name" |
-	grep "alfred")
+	grep -E "alfred|shimmering-obsidian")
 
 commit_msg="build: improved release action"
 
-function replacements_todo {
-	echo "placeholder"
-	# rm -v ./.github/workflows/build-and-release.yml
-	# cp -v "$WD/alfred-workflow-release.yml" ./.github/workflows
+function actions_in_repo {
+	# update release action
+	rm -v ./.github/workflows/alfred-workflow-release.yml
+	cp -v "$WD/alfred-workflow-release.yml" ./.github/workflows
 }
+
+#───────────────────────────────────────────────────────────────────────────────
+# confirmation that everything is correct
+print "\e[1;34mCommit Message:\e[0m"
+echo "$commit_msg"
+echo
+print "\e[1;34mRepos to Update:\e[0m"
+echo "$repos_to_update"
+echo
+print "\e[1;34mAction:\e[0m"
+declare -f actions_in_repo
+echo
+print "\e[1;34mProceed? (y/n)\e[0m"
+if [[ "$(read -rks)" != "y" ]]; then
+	echo "Aborted."
+	return 1
+fi
 
 #───────────────────────────────────────────────────────────────────────────────
 # clone each repo, do the replacements, and git add/commit/push
 while read -r repo; do
+	echo
 	echo "-----------------------------------------------------"
-	echo "Updating $repo…"
+	print "\e[1;34mUpdating $repo…\e[0m"
 	git clone --depth=2 "git@github.com:$github_username/$repo"
 	cd "$repo" || return 1
 
-	replacements_todo
+	actions_in_repo
 
 	git add --all && git commit -m "$commit_msg" && git push
 	cd ..
 	rm -rf "$repo"
-	echo
 done <<<"$repos_to_update"
