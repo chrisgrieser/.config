45cc4f2d27fc5cba2c0c9c12489a1b9a78379f87
🤖 automated <cron@job>
2024-03-11 17:19:03 +0100

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,5 +1,4 @@
-local builtins = vim.fs.find(
-	function(name) return name ~= "habamax.vim" end,
-	{ path = vim.env.VIMRUNTIME .. "/colors", limit = math.huge }
-)
-vim.notify("❗ builtins: " .. vim.inspect(builtins))
+local str1 = "origin  git@github.com:chrisgrieser/nvim-tinygit (fetch)"
+local str2 = "origin  https://github.com/chrisgrieser/nvim-tinygit (fetch)"
+local firstRemote = str2:match(":(%S+)") or str2:match("/(%S+)")
+vim.notify("❗ firstRemote: " .. tostring(firstRemote))
