31e58c6c23ba3209b98d24ba75ac7565426f0630
🤖 automated <cron@job>
2024-03-11 16:19:03 +0100

    iMac Home (2)

--- + utility-scripts/full-backup.sh
+++ + utility-scripts/full-backup.sh
@@ -1,40 +1,42 @@
 # CONFIG
-LOG_LOCATION="$DATA_DIR/Backups/backups-to-external-drives.log"
+log_location="$DATA_DIR/Backups/backups-to-external-drives.log"
 
 #───────────────────────────────────────────────────────────────────────────────
 # DETERMINE VOLUME
 
-# check continuously for a volume
-i=0
-while true; do
-	VOLUME_NAME="$(df -h | grep -io "\s/Volumes/.*" | cut -c2-)"
-	if [[ $(echo "$VOLUME_NAME" | wc -l) -gt 1 ]]; then
+echo "Searching for Volume… "
+for _ in {1..100}; do
+	printf "🬋"
+
+	volume_name="$(df -h | grep -io "\s/Volumes/.*" | cut -c2-)"
+	if [[ $(echo "$volume_name" | wc -l) -gt 1 ]]; then
 		print "\033[1;33mMore than one volume connected.\033[0m"
 		return 1
-	elif [[ -n "$VOLUME_NAME" ]]; then
+	elif [[ -n "$volume_name" ]]; then
 		break
 	fi
 
 	sleep 0.5
-	i=$((i + 1))
-	if [[ $i -gt 15 ]]; then
-		print "\033[1;33mNo Volume found.\033[0m"
-		return 1
-	fi
 done
-print "\033[1;34mBackup Volume: $VOLUME_NAME\033[0m"
+
+echo
+if [[ -z "$volume_name" ]]; then
+	print "\033[1;33mTimeout, no volume found.\033[0m"
+	return 1
+fi
+print "\033[1;34mBackup volume: $volume_name\033[0m"
 
 #───────────────────────────────────────────────────────────────────────────────
 # DETERMINE BACKUP DESTINATION
 
 # determine backup folder at destination
-DEVICE_NAME="$(scutil --get ComputerName)"
-BACKUP_DEST="$VOLUME_NAME/Backup_$DEVICE_NAME"
-mkdir -p "$BACKUP_DEST"
-cd "$BACKUP_DEST" || return 1
+device_name="$(scutil --get ComputerName)"
+backup_dest="$volume_name/Backup_$device_name"
+mkdir -p "$backup_dest"
+cd "$backup_dest" || return 1
 
 # Log (on the Mac)
-echo -n "Backup: $(date '+%Y-%m-%d %H:%M'), $VOLUME_NAME -- " >>"$LOG_LOCATION"
+echo -n "Backup: $(date '+%Y-%m-%d %H:%M'), $volume_name -- " >>"$log_location"
 
 #───────────────────────────────────────────────────────────────────────────────
 # HELPER FUNCTION
@@ -75,8 +77,8 @@ backup "$HOME/Library/Mobile Documents/com~apple~CloudDocs/" ./iCloud-Folder
 echo
 print "\033[1;34m─────────────────────────────────────────────────────────────────────────────\033[0m"
 echo
-if [[ -z "$errors" ]] ; then
-	print "\033[1;32mBackup on $VOLUME_NAME completed.\033[0m"
+if [[ -z "$errors" ]]; then
+	print "\033[1;32mBackup on $volume_name completed.\033[0m"
 else
 	print "\033[1;31m$errors\033[0m"
 fi
@@ -87,22 +89,22 @@ osascript -e 'display notification "" with title "Backup finished." sound name "
 
 # LOG BACKUP ACTIVITY
 # on Mac
-echo "completed: $(date '+%H:%M')" >>"$LOG_LOCATION"
+echo "completed: $(date '+%H:%M')" >>"$log_location"
 
 # at Backup Destination
 echo "Backup: $(date '+%Y-%m-%d %H:%M')" >>last_backup.log
 
 # Reminder for Next Backup in 14 days (idempotent, due to multiple backup disks)
 osascript -e '
-	set today to (current date) 
-	set inTwoWeeks to today + 14 * (60 * 60 * 24) 
-	tell application "Reminders" 
-		set theList to (default list) 
-		set backupReminders to (reminders of theList where name is "Backup" and completed is false and due date is greater than today) 
-		if (count of backupReminders) is 0 then 
-			tell theList to make new reminder with properties {name:"Backup", allday due date:inTwoWeeks} 
-		end if 
-		quit 
-	end tell 
+	set today to (current date)
+	set inTwoWeeks to today + 14 * (60 * 60 * 24)
+	tell application "Reminders"
+		set theList to (default list)
+		set backupReminders to (reminders of theList where name is "Backup" and completed is false and due date is greater than today)
+		if (count of backupReminders) is 0 then
+			tell theList to make new reminder with properties {name:"Backup", allday due date:inTwoWeeks}
+		end if
+		quit
+	end tell
 	count of backupReminders
 ' &>/dev/null
