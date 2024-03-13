fc6a8b346c563f74753bf4eabb6a7b3e642d043a
🤖 automated <cron@job>
2024-03-13 21:44:40 +0100

    iMac Home (6)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -2,3 +2,7 @@ local str1 = "origin  git@github.com:chrisgrieser/nvim-tinygit (fetch)"
 local str2 = "origin  https://github.com/chrisgrieser/nvim-tinygit (fetch)"
 local firstRemote = str2:match(":(%S+)") or str2:match("/(%S+)")
 vim.notify("❗ firstRemote: " .. tostring(firstRemote))
+
+
+vim.fn.winlayout()
+vim.api.nvim_win_get_position(0)
