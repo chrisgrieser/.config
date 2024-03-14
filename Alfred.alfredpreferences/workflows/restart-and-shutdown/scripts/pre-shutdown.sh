708ba09469a39b59b06130dc3a16fa982a500220
🤖 automated <cron@job>
2024-03-14 21:49:08 +0100

    iMac Home (1)

--- Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/pre-shutdown.sh
+++ Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/pre-shutdown.sh
@@ -2,27 +2,20 @@
 set -e
 #───────────────────────────────────────────────────────────────────────────────
 
-function ensure_sync {
-	local repo_path="$1"
-	local name="$2"
-
+while read -r line; do
+	name=$(echo "$line" | cut -d, -f1)
+	repo_path=$(echo "$line" | cut -d, -f2 | sed "s|^~|$HOME|")
+	icon=$(echo "$line" | cut -d, -f3)
 	cd "$repo_path"
 	if [[ -n "$(git status --porcelain)" ]]; then
-		osascript -e "display notification \"$name\" with title \"🔁 Syncing…\""
+		osascript -e "display notification \"$icon $name\" with title \"🔁 Syncing…\""
 		zsh ".sync-this-repo.sh" &>/dev/null
 	fi
 	if [[ -n "$(git status --porcelain)" ]]; then
-		echo "⚠️ $name not synced."
+		echo "⚠️ $icon $name not synced."
 		return 1
 	fi
-}
-
-#───────────────────────────────────────────────────────────────────────────────
-
-ensure_sync "$HOME/.config" "🔵 Dotfiles"
-ensure_sync "$VAULT_PATH" "🟪 Vault"
-ensure_sync "$PASSWORD_STORE_DIR" "🔑 Password Store"
-ensure_sync "$PHD_DATA_VAULT" "📗 PhD Data"
+done < "$HOME/.config/perma-repos.csv"
 
 # for Alfred conditional to prompt shutdown
 echo -n "success"
