0744b80ff43dcb3b8784604aa68da1694cad0b50
🤖 automated <cron@job>
2024-03-15 16:06:10 +0100

    iMac Home (1)

--- Alfred.alfredpreferences/workflows/reminders-editor/scripts/edit-reminder.sh
+++ Alfred.alfredpreferences/workflows/reminders-editor/scripts/edit-reminder.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 
 title=$(echo "$*" | head -n1)
+echo "❗ title: $title" >&2
 body=$(echo "$*" | tail -n +2)
+echo "❗ body: $body" >&2
 
 # shellcheck disable=2154 # Alfred variables
 msg=$(reminders edit "$reminder_list" "$id" "$title" --notes "$body")
