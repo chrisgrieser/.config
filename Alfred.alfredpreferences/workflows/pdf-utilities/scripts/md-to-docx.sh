7db748cff1991e3deda0f016bbec39aaf80b3e14
🤖 automated <cron@job>
2024-03-25 15:16:39 +0100

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/md-to-docx.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/md-to-docx.sh
@@ -14,28 +14,28 @@ word_file="${input_no_ext}_${date}_CG.docx"
 cd "$(dirname "$md_file")" || return 1
 
 pandoc "$md_file" --output="$word_file" --defaults="md2docx" 2>&1 || return 1
-rm "$word_file"
+rm "$md_file"
 
 #───────────────────────────────────────────────────────────────────────────────
 # `^l` is the manual line break token in MS Word
-# replace <br> with actual line break
+# -> replace <br> with actual line break
 [[ ! -e "$word_file" ]] && return 1
 open "$word_file"
 
 osascript -e '
-	tell application "Microsoft Word" 
+	tell application "Microsoft Word"
 		set i to 0
-		repeat until active document exists 
-			delay 0.1 
+		repeat until active document exists
+			delay 0.1
 			set i to i + 1
 			if i > 120 then return
-		end repeat 
-		activate 
-		
-		set myFind to find object of text object of active document 
-		execute find myFind find text "<br>" replace with "^l" replace replace all 
+		end repeat
+		activate
+
+		set myFind to find object of text object of active document
+		execute find myFind find text "<br>" replace with "^l" replace replace all
 
 		save active document
-	end tell 
+	end tell
 '
 
