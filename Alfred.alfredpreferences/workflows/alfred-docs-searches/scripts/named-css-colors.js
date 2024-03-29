201aa4cc8c294155fb6c17309ca20904413cea68
🤖 automated <cron@job>
2024-03-29 16:19:05 +0100

    iMac Home (7)

---
+++ Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/named-css-colors.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @param {string} path */
+function readFile(path) {
+	const data = $.NSFileManager.defaultManager.contentsAtPath(path);
+	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
+	return ObjC.unwrap(str);
+}
+
+//──────────────────────────────────────────────────────────────────────────────
+
+// biome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run() {
+	const jsonPath =
+		$.getenv("alfred_preferences") +
+		"/workflows/" +
+		$.getenv("alfred_workflow_uid") +
+		"/data/named-css-colors.json";
+	const colors = JSON.parse(readFile(jsonPath));
+
+	/** @type AlfredItem[] */
+	const items = [];
+	for (const [name, hex] of Object.entries(colors)) {
+		items.push({
+			title: name,
+			subtitle: hex,
+			arg: name,
+		});
+	}
+
+	return JSON.stringify({
+		items: items,
+		c,
+	});
+}
