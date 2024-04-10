2aa3ebd5ee7d52bf1b2454ccd38394c1899199d4
🤖 automated <cron@job>
2024-04-10 18:00:20 +0200

    iMac Home (1)

--- obsidian/vimrc-jscommands.js
+++ obsidian/vimrc-jscommands.js
@@ -31,28 +31,30 @@ function addYamlKey(key, value) {
 	// biome-ignore lint/correctness/noUndeclaredVariables: passed by vimrc plugin
 	const editor = view.editor;
 	const /** @type {string[]} */ lines = editor.getValue().split("\n");
-	const frontmatterEnd = lines.slice(1).findIndex((line) => line === "---");
-	if (frontmatterEnd === -1) {
+	const frontmatterEnd = lines.slice(1).findIndex((line) => line === "---") + 1;
+	if (frontmatterEnd === 0) {
 		// biome-ignore lint/correctness/noUndeclaredVariables: available via Obsidian API
 		new Notice("No frontmatter found.");
-		return 
-	};
+		return;
+	}
 
 	const stringifiedValue = typeof value === "string" ? `"${value}"` : value.toString();
 	const yamlLine = key + ": " + stringifiedValue;
 
 	const keyLnum = lines
-		.slice(0, frontmatterEnd) // only check frontmatter
+		.slice(0, frontmatterEnd + 1) // only check frontmatter
 		.findIndex((line) => line.startsWith(key + ":"));
+	let msg
 	if (keyLnum === -1) {
-		lines.splice(frontmatterEnd + 1, 0, yamlLine); // insert at frontmatter
+		lines.splice(frontmatterEnd, 0, yamlLine); // insert at frontmatter
+		msg = `Set property "${key}" to "${value}"`
 	} else {
 		lines[keyLnum] = yamlLine; // update existing key
 	}
 	editor.setValue(lines.join("\n"));
 
 	// biome-ignore lint/correctness/noUndeclaredVariables: available via Obsidian API
-	new Notice(`Set property "${key}" to "${value}"`);
+	new Notice(msg);
 }
 
 //──────────────────────────────────────────────────────────────────────────────
