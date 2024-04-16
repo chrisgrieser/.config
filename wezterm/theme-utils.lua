e5ff7213f110b2db11be5f311c71006cce7edb66
🤖 automated <cron@job>
2024-04-16 11:25:13 +0200

    mini Office (3)

--- wezterm/theme-utils.lua
+++ wezterm/theme-utils.lua
@@ -3,8 +3,8 @@ local wt = require("wezterm")
 --------------------------------------------------------------------------------
 
 ---selects the color scheme depending on Dark/Light Mode
----@param lightTheme string 
----@param darkTheme string 
+---@param lightTheme string
+---@param darkTheme string
 ---@nodiscard
 ---@return string name of the string to set in config.colorscheme
 function M.autoScheme(darkTheme, lightTheme)
@@ -14,13 +14,13 @@ function M.autoScheme(darkTheme, lightTheme)
 end
 
 ---selects the opacity depending on Dark/Light Mode
----@param lightOpacity number 
----@param darkOpacity number 
+---@param lightOpacity number
+---@param darkOpacity number
 ---@nodiscard
 ---@return integer Opacity to use
 function M.autoOpacity(darkOpacity, lightOpacity)
-	local currentMode = wt.gui.get_appearance()
-	local opacity = currentMode:find("Dark") and darkOpacity or lightOpacity
+	local isDarkMode = wt.gui.get_appearance():find("Dark")
+	local opacity = isDarkMode and darkOpacity or lightOpacity
 	return opacity
 end
 
@@ -28,23 +28,24 @@ end
 ---light mode
 function M.cycle(window, _)
 	local allSchemes = wt.color.get_builtin_schemes()
-	local currentMode = wt.gui.get_appearance()
-	local currentScheme = window:effective_config().color_scheme
 	local darkSchemes = {}
 	local lightSchemes = {}
-
 	for name, scheme in pairs(allSchemes) do
-		local bg = wt.color.parse(scheme.background) -- parse into a color object
-		local h, s, l, a = bg:hsla() ---@diagnostic disable-line: unused-local
-		if l < 0.45 then
-			table.insert(darkSchemes, name)
-		else
-			table.insert(lightSchemes, name)
+		if scheme.background then
+			local bg = wt.color.parse(scheme.background) -- parse into a color object
+			local h, s, l, a = bg:hsla() ---@diagnostic disable-line: unused-local
+			if l < 0.45 then
+				table.insert(darkSchemes, name)
+			else
+				table.insert(lightSchemes, name)
+			end
 		end
 	end
-	local schemesToSearch = currentMode:find("Dark") and darkSchemes or lightSchemes
 
-	for i = 1, #schemesToSearch, 1 do
+	local currentScheme = window:effective_config().color_scheme
+	local isDarkMode = wt.gui.get_appearance():find("Dark")
+	local schemesToSearch = isDarkMode and darkSchemes or lightSchemes
+	for i = 1, #schemesToSearch do
 		if schemesToSearch[i] == currentScheme then
 			local overrides = window:get_config_overrides() or {}
 			local nextScheme = schemesToSearch[i + 1]
