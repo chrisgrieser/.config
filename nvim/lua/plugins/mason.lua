c804ec5e6f799d04ee298ee31334bbe425733f0e
🤖 automated <cron@job>
2024-04-17 11:43:44 +0200

    iMac Home (29)

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,17 +1,17 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		external_dependencies = { "node", "python3.12" },
+		external_dependencies = { "node", "python3.12", "yq" },
 		keys = {
 			{ "<leader>pm", vim.cmd.Mason, desc = " Mason" },
 		},
 		opts = {
 			-- add my own local registry: https://github.com/mason-org/mason-registry/pull/3671#issuecomment-1851976705
 			-- also requires yq being available in the system
-			-- registries = {
-			-- 	"file:" .. vim.fn.stdpath("config") .. "/personal-mason-registry", -- needs to come first
-			-- 	"github:mason-org/mason-registry",
-			-- },
+			registries = {
+				"file:" .. vim.fn.stdpath("config") .. "/personal-mason-registry", -- needs to come first
+				"github:mason-org/mason-registry",
+			},
 			ui = {
 				border = vim.g.borderStyle,
 				height = 0.85,
