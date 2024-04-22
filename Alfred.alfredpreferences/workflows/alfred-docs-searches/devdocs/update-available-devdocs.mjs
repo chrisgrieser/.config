aaff2870680a3197433aa0c51c4fee99ef0e6b7d
🤖 automated <cron@job>
2024-04-22 10:08:42 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/alfred-docs-searches/devdocs/update-available-devdocs.mjs
+++ Alfred.alfredpreferences/workflows/alfred-docs-searches/devdocs/update-available-devdocs.mjs
@@ -50,13 +50,16 @@ async function run() {
 	let newXmlLines = [];
 	for (let i = 1; i <= popupNumber; i++) {
 		const label = i === 1 ? "Enabled devdocs" : "";
+		const number = i.toString().padStart(2, "0");
+
 		newXmlLines = newXmlLines.concat([
 			"<dict> <key>config</key> <dict> <key>default</key> <string></string> <key>pairs</key> <array>",
 			...infoPlistPopup,
-			`</array> </dict> <key>description</key> <string></string> <key>label</key> <string>${label}</string> <key>type</key> <string>popupbutton</string> <key>variable</key> <string>keyword_${i}</string> </dict>`,
+			`</array> </dict> <key>description</key> <string></string> <key>label</key> <string>${label}</string> <key>type</key> <string>popupbutton</string> <key>variable</key> <string>keyword_${number}</string> </dict>`,
 		]);
 	}
 
+	// {var:keyword_01}||{var:keyword_02}||{var:keyword_03}||{var:keyword_04}||{var:keyword_05}||{var:keyword_06}||{var:keyword_07}||{var:keyword_08}||{var:keyword_09}||{var:keyword_10}||{var:keyword_11}||{var:keyword_12}||{var:keyword_13}||{var:keyword_14}||{var:keyword_15}||{var:keyword_16}||{var:keyword_17}||{var:keyword_18}||{var:keyword_19}||{var:keyword_20}
 	const start = xmlLines.indexOf("\t<key>userconfigurationconfig</key>") + 2;
 	const end = xmlLines.indexOf("\t</array>", start);
 	xmlLines.splice(start, end - start, ...newXmlLines);
