4752a25d6a1ec6fc8ea8280f93f10ae7d588e576
🤖 automated <cron@job>
2024-04-28 10:25:38 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/reminders-editor/scripts/select-reminder-time.js
+++ Alfred.alfredpreferences/workflows/reminders-editor/scripts/select-reminder-time.js
@@ -1,71 +1,76 @@
 #!/usr/bin/env osascript -l JavaScript
 
+// CONFIG
+/** @type {Intl.DateTimeFormatOptions} */
+const format = { weekday: "short", day: "numeric", month: "long" };
+const lang = "en-GB";
+
+//───────────────────────────────────────────────────────────────────────────
+
+/** @param {number} inXDays */
+function getDisplayDate(inXDays) {
+	const date = new Date();
+	date.setDate(date.getDate() + inXDays);
+	return date.toLocaleDateString(lang, format);
+}
+
+/** @param {"Sunday"|"Monday"|"Tuesday"|"Wednesday"|"Thursday"|"Friday"|"Saturday"} weekday */
+function daysUntilNext(weekday) {
+	const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	const weekdayNum = weekdays.indexOf(weekday);
+	const weekdayNumToday = new Date().getDay();
+	const daysUntil = (weekdayNum - weekdayNumToday) % 7;
+	return daysUntil === 0 ? 7 : daysUntil;
+}
+
+//──────────────────────────────────────────────────────────────────────────────
+
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
-	const query = argv[0];
+	const query = argv[0] || "";
 
-	const dateFormat = {
-		weekday: "short",
-		day: "numeric",
-		month: "short",
-	};
-	const lang = "en-US";
-	//───────────────────────────────────────────────────────────────────────────
-
-	const today = new Date();
-
-	const tomorrow = new Date();
-	tomorrow.setDate(tomorrow.getDate() + 1);
-	const in2Days = new Date()
-	tomorrow.setDate(tomorrow.getDate() + 2);
-
-	const weekdayNum = today.getDay();
-	const daysUntilMonday = weekdayNum === 1 ? 7 : (1 - weekdayNum + 7) % 7;
-	const daysUntilWednesday = weekdayNum === 3 ? 7 : (3 - weekdayNum + 7) % 7;
-	const daysUntilSunday = weekdayNum === 0 ? 7 : 7 - weekdayNum;
-
-	const jsonArray = [
+	const dateChoices = [
 		{
 			title: "Tomorrow",
-			subtitle: tomorrow.toDateString(),
-			variables: { selectedDueDate: "Tomorrow" }, // label for notification
+			subtitle: getDisplayDate(1),
 			arg: 1,
 		},
 		{
 			title: "in 2 Days",
-			variables: { selectedDueDate: "in 2 Days" },
+			subtitle: getDisplayDate(2),
 			arg: 2,
 		},
 		{
 			title: "in 7 days",
-			variables: { selectedDueDate: "in 7 days" },
+			subtitle: getDisplayDate(7),
 			arg: 7,
 		},
 		{
 			title: "next Monday",
-			variables: { selectedDueDate: "next Monday" },
-			arg: daysUntilMonday,
+			subtitle: getDisplayDate(daysUntilNext("Monday")),
+			arg: daysUntilNext("Monday"),
 		},
 		{
-			title: "next Wednesday",
-			variables: { selectedDueDate: "next Wednesday" },
-			arg: daysUntilWednesday,
+			title: "next Tuesday",
+			subtitle: getDisplayDate(daysUntilNext("Tuesday")),
+			arg: daysUntilNext("Tuesday"),
 		},
 		{
-			title: "next Sunday",
-			variables: { selectedDueDate: "next Sunday" },
-			arg: daysUntilSunday,
+			title: "next Thursday",
+			subtitle: getDisplayDate(daysUntilNext("Thursday")),
+			arg: daysUntilNext("Thursday"),
 		},
 		{
 			title: "in 2 weeks",
-			variables: { selectedDueDate: "in 2 weeks" },
+			subtitle: getDisplayDate(14),
 			arg: 14,
 		},
 	];
+	
 
 	return JSON.stringify({
 		variables: { reminderText: query },
-		items: jsonArray,
+		items: dateChoices,
 	});
 }
