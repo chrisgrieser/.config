b53bfb83217ee8c701361a40aac4b7dc9a390ee3
🤖 automated <cron@job>
2024-04-28 09:55:38 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/reminders-editor/scripts/select-reminder-time.js
+++ Alfred.alfredpreferences/workflows/reminders-editor/scripts/select-reminder-time.js
@@ -3,18 +3,32 @@
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
-	const today = new Date();
-	const weekdayNum = today.getDay();
+	const query = argv[0];
 
+	const dateFormat = {
+		weekday: "short",
+		day: "numeric",
+		month: "short",
+	};
+	const lang = "en-US";
+	//───────────────────────────────────────────────────────────────────────────
+
+	const today = new Date();
+
+	const tomorrow = new Date();
+	tomorrow.setDate(tomorrow.getDate() + 1);
+	const in2Days = new Date()
+	tomorrow.setDate(tomorrow.getDate() + 2);
+
+	const weekdayNum = today.getDay();
 	const daysUntilMonday = weekdayNum === 1 ? 7 : (1 - weekdayNum + 7) % 7;
 	const daysUntilWednesday = weekdayNum === 3 ? 7 : (3 - weekdayNum + 7) % 7;
 	const daysUntilSunday = weekdayNum === 0 ? 7 : 7 - weekdayNum;
 
-	const query = argv[0];
-
 	const jsonArray = [
 		{
 			title: "Tomorrow",
+			subtitle: tomorrow.toDateString(),
 			variables: { selectedDueDate: "Tomorrow" }, // label for notification
 			arg: 1,
 		},
