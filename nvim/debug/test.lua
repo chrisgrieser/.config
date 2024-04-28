862dcd2356af08ce456a8d67ac465db09229cf02
🤖 automated <cron@job>
2024-04-28 10:55:38 +0200

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,1 +1,12 @@
-vim.cmd.cnoreabbrev("W w")
+vim.keymap.set("n", "W", function()
+	local pattern = "[%w_]+"
+	require("spider").motion("w", {
+		customPatterns = { pattern },
+	})
+end, { desc = "spider-W-motion" })
+
+--------------------------------------------------------------------------------
+
+local another_var_name = 42
+local my_var_name = another_var_name
+vim.notify("⭕ my_var_name: " .. tostring(my_var_name))
