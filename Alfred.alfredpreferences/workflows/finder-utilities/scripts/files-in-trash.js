bf98f6744fa753e21bacc53a712a2d8ab0cb0fbe
🤖 automated <cron@job>
2024-04-29 18:35:43 +0200

    iMac Home (1)

--- Alfred.alfredpreferences/workflows/finder-utilities/scripts/files-in-trash.js
+++ Alfred.alfredpreferences/workflows/finder-utilities/scripts/files-in-trash.js
@@ -2,15 +2,15 @@
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
+//──────────────────────────────────────────────────────────────────────────────
 
 /** @param {string} str */
-function alfredMatcher(str) {
-	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
-	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
-	return [clean, camelCaseSeperated, str].join(" ") + " ";
+function camelCaseMatch(str) {
+	const subwords = str.replace(/[-_./]/g, " ");
+	const fullword = str.replace(/[-_./]/g, "");
+	const camelCaseSeparated = str.replace(/([A-Z])/g, " ");
+	return [subwords, camelCaseSeparated, fullword, str].join(" ") + " ";
 }
-
-const fileExists = (/** @type {string} */ filePath) => Application("Finder").exists(Path(filePath));
 
 //──────────────────────────────────────────────────────────────────────────────
 
@@ -19,25 +19,23 @@ const fileExists = (/** @type {string} */ filePath) => Application("Finder").exi
 function run() {
 	const home = app.pathTo("home folder");
 	const trashLocation1 = home + "/.Trash";
-	const trashLocation2 = home + "/Library/Mobile Documents/com~apple~CloudDocs/.Trash";
-
-	let findCmd = `find "${trashLocation1}" "${trashLocation2}" -maxdepth 1 -mindepth 1`;
-	if (!fileExists(trashLocation2)) findCmd = `find "${trashLocation1}" -maxdepth 1 -mindepth 1`;
+	const trashLocation2 = home + "/Library/Mobile Documents/.Trash";
 
 	const trashedFile = app
-		.doShellScript(findCmd)
+		.doShellScript(`find "${trashLocation1}" "${trashLocation2}" -maxdepth 1 -mindepth 1`)
 		.split("\r")
 		.map((path) => {
-			const ext = path.split(".").pop();
+			const ext = path.split(".").pop() || "";
 			const filename = path.split("/").pop();
 
-			const iconToDisplay = { path: path };
 			const imageExtensions = ["png", "jpg", "jpeg", "gif", "icns", "tiff", "heic"];
-			if (!imageExtensions.includes(ext)) iconToDisplay.type = "fileicon";
+			const iconToDisplay = imageExtensions.includes(ext)
+				? { path: path }
+				: { path: path, type: "fileicon" };
 
 			return {
 				title: filename,
-				match: alfredMatcher(path),
+				match: camelCaseMatch(path),
 				icon: iconToDisplay,
 				type: "file:skipcheck",
 				arg: path,
