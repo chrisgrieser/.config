6cbefa1622ef4d06a1ffc463cf395432154919f4
🤖 automated <cron@job>
2024-05-01 18:21:43 +0200

    iMac Home (5)

--- Alfred.alfredpreferences/workflows/direct-bookmarks/scripts/direct-bookmarks.js
+++ Alfred.alfredpreferences/workflows/direct-bookmarks/scripts/direct-bookmarks.js
@@ -11,19 +11,72 @@ function readFile(path) {
 	return ObjC.unwrap(str);
 }
 
+/** @param {string} str */
+function alfredMatcher(str) {
+	const clean = str.replace(/[-()_.:#/\;,[\]]/g, " ");
+	return [clean, str].join(" ") + " ";
+}
+
+/** @typedef {Object} bookmark
+ * @property {number} date_added
+ * @property {number} date_last_used
+ * @property {number} guid
+ * @property {number} id
+ * @property {string} name
+ * @property {string} url
+ * @property {"url"|"folder"} type
+ * @property {bookmark[]?} children
+ */
+
 //──────────────────────────────────────────────────────────────────────────────
 
-function recursivlyGetBookmarks(child) {
-	if
+/**
+ * @param {bookmark} item
+ * @param {bookmark[]=} acc
+ * @return {bookmark[]} flattened
+ */
+function recursivelyGetBookmarks(item, acc) {
+	if (!acc) acc = [];
+	console.log("⭕ acc:", acc.length);
+	if (item.type === "url") {
+		acc.push(item);
+	} else if (item.type === "folder" && item.children) {
+		for (const child of item.children) {
+			const childBookmarks = recursivelyGetBookmarks(child, acc);
+			acc.push(...childBookmarks);
+		}
+	}
+	return acc;
 }
 
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	const bookmarkPath = ""
+	const bookmarkPath =
+		app.pathTo("home folder") +
+		"/Library/Application Support/BraveSoftware/Brave-Browser/Default/Bookmarks";
+	const bookmarkJson = JSON.parse(readFile(bookmarkPath)).roots;
 
-	const bookmarkJson = JSON.parse(readFile(bookmarkPath))
+	/** @type {bookmark[]} */
+	let allBookmarks = [];
+	// for (const key in bookmarkJson) {
+	// 	const bms = recursivelyGetBookmarks(bookmarkJson[key]);
+	// 	allBookmarks = allBookmarks.concat(bms);
+	// }
+	allBookmarks = recursivelyGetBookmarks(bookmarkJson.bookmark_bar);
 
+	const bookmarks = allBookmarks.map((bookmark) => {
+		const { name, url } = bookmark;
 
-	const bookmarks = []
-	return JSON.stringify({ items: bookmarks });
+		return {
+			title: name,
+			match: alfredMatcher(name),
+			subtitle: url,
+			arg: url,
+		};
+	});
+
+	return JSON.stringify({
+		items: bookmarks,
+		// cache: { seconds: 60 * 5, loosereload: true },
+	});
 }
