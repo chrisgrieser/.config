e39ba5155e3cb40c2ca52c8924c83d368698d141
🤖 automated <cron@job>
2024-05-06 19:52:19 +0200

    iMac Home (7)

--- Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/can-i-use.js
+++ Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/can-i-use.js
@@ -11,6 +11,14 @@ function httpRequest(url) {
 	return $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding).js;
 }
 
+/** @param {string} str */
+function camelCaseMatch(str) {
+	const subwords = str.replace(/[-_./]/g, " ");
+	const fullword = str.replace(/[-_./]/g, "");
+	const camelCaseSeparated = str.replace(/([A-Z])/g, " ");
+	return [subwords, camelCaseSeparated, fullword, str].join(" ") + " ";
+}
+
 //──────────────────────────────────────────────────────────────────────────────
 
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
@@ -21,18 +29,21 @@ function run() {
 
 	/** @type AlfredItem[] */
 	const entries = [];
-	for (const [_, value] of Object.entries(response.data)) {
-		const { title, categories } = value;
-		const categoryInfo = categories.join(", ");
+	for (const [key, value] of Object.entries(response.data)) {
+		const { title, categories, usage_perc_y } = value;
+		const subtitle = `${categories.join(", ")}   ·   ${usage_perc_y}%`;
+		const url = "https://caniuse.com/" + key;
+
 		entries.push({
 			title: title,
-			subtitle: categoryInfo,
-			arg: title,
+			match: camelCaseMatch(title),
+			subtitle: subtitle,
+			arg: url,
 		});
 	}
 
 	return JSON.stringify({
 		items: entries,
-		// cache: { seconds: 600, loosereload: true },
+		cache: { seconds: 3600 * 24, loosereload: true },
 	});
 }
