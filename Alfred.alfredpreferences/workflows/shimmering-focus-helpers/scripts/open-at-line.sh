e4c81463eda477397cf7952c03144d2d684d6f66
🤖 automated <cron@job>
2024-05-09 18:51:43 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/shimmering-focus-helpers/scripts/open-at-line.sh
+++ Alfred.alfredpreferences/workflows/shimmering-focus-helpers/scripts/open-at-line.sh
@@ -6,5 +6,5 @@ lineNo="$*"
 
 # workaround for https://github.com/neovide/neovide/issues/1604
 open "$sf_path"
-sleep 0.3
+sleep 0.8
 open "$sf_path" --env=LINE="$lineNo" # FIX not open at correct line
