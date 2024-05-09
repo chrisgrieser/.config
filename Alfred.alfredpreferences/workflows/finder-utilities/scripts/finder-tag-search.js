413ab42f4a6e91292e0ace79b4d4097ccb4afb9b
🤖 automated <cron@job>
2024-05-09 20:21:43 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/finder-utilities/scripts/finder-tag-search.js
+++ Alfred.alfredpreferences/workflows/finder-utilities/scripts/finder-tag-search.js
@@ -8,32 +8,37 @@ app.includeStandardAdditions = true;
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
 	const tagName = $.getenv("tag_to_search");
+	// https://www.alfredforum.com/topic/18041-advanced-search-using-tags-%C3%A0-la-finder/
+	const tagged = app.doShellScript(`mdfind "kMDItemUserTags == ${tagName}"`);
 
-	const taggedFiles = app
-		// https://www.alfredforum.com/topic/18041-advanced-search-using-tags-%C3%A0-la-finder/
-		.doShellScript(`mdfind "kMDItemUserTags == ${tagName}"`)
-		.split("\r")
-		.map((path) => {
-			const fileName = path.split("/").pop() || "";
-			const ext = fileName.split(".").pop() || "";
-			let parentFolder = path.split("/").slice(0, -1).join("/");
-			parentFolder = parentFolder
-				.replace(/\/Users\/\w+\/Library\/Mobile Documents\/com~apple~CloudDocs/, "☁️")
-				.replace(/\/Users\/\w+/, "~");
-
-			const iconToDisplay = ["png", "jpg", "jpeg", "gif", "icns", "tiff", "heic"].includes(ext)
-				? { type: "fileicon", path: path }
-				: { path: path };
-
-			return {
-				title: fileName + " " + $.getenv("tag_emoji"),
-				subtitle: parentFolder,
-				icon: iconToDisplay,
-				type: "file:skipcheck",
-				uid: path,
-				arg: path,
-			};
+	// GUARD
+	if (!tagged) {
+		return JSON.stringify({
+			items: [{ title: `No item with "${tagName}" not found` }],
 		});
+	}
+
+	const taggedFiles = tagged.split("\r").map((path) => {
+		const fileName = path.split("/").pop() || "";
+		const ext = fileName.split(".").pop() || "";
+		let parentFolder = path.split("/").slice(0, -1).join("/");
+		parentFolder = parentFolder
+			.replace(/\/Users\/\w+\/Library\/Mobile Documents\/com~apple~CloudDocs/, "☁️")
+			.replace(/\/Users\/\w+/, "~");
+
+		const iconToDisplay = ["png", "jpg", "jpeg", "gif", "icns", "tiff", "heic"].includes(ext)
+			? { type: "fileicon", path: path }
+			: { path: path };
+
+		return {
+			title: fileName + " " + $.getenv("tag_emoji"),
+			subtitle: parentFolder,
+			icon: iconToDisplay,
+			type: "file:skipcheck",
+			uid: path,
+			arg: path,
+		};
+	});
 
 	return JSON.stringify({ items: taggedFiles });
 }
