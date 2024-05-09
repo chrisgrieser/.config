c22b084a257a8a9b9f2e9a1e8461210df5c88971
🤖 automated <cron@job>
2024-05-09 19:21:43 +0200

    iMac Home (5)

--- Alfred.alfredpreferences/workflows/shimmering-focus-helpers/scripts/open-shimmering-focus.js
+++ Alfred.alfredpreferences/workflows/shimmering-focus-helpers/scripts/open-shimmering-focus.js
@@ -1,72 +1,16 @@
 #!/usr/bin/env osascript -l JavaScript
 ObjC.import("stdlib");
-const app = Application.currentApplication();
-app.includeStandardAdditions = true;
-//──────────────────────────────────────────────────────────────────────────────
-
-/** @param {string} path */
-function readFile(path) {
-	const fm = $.NSFileManager.defaultManager;
-	const data = fm.contentsAtPath(path);
-	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
-	return ObjC.unwrap(str);
-}
-
-const alfredMatcher = (/** @type {string} */ str) =>
-	str.replace(/[-()_:.]/g, " ") + " " + str + " ";
 
 const fileExists = (/** @type {string} */ filePath) => Application("Finder").exists(Path(filePath));
 
-//───────────────────────────────────────────────────────────────────────────
-
-/** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
 	const sfPath = $.getenv("local_repos") + "/shimmering-focus/theme.css";
+	const branch = $.getenv("branch_to_use");
 
-	//───────────────────────────────────────────────────────────────────────────
-	// REPO NEEDS TO BE CLONED
+	const item = fileExists(sfPath)
+		? { title: "Open theme.css", arg: "open" }
+		: { title: "Clone the Repo", subtitle: `Branch: ${branch}`, arg: "clone" };
 
-	if (!fileExists(sfPath)) {
-		const branch = $.getenv("branch_to_use");
-		return JSON.stringify({
-			items: [
-				{
-					title: "Clone the Repo",
-					subtitle: `Branch: ${branch}`,
-					arg: "clone",
-				},
-			],
-		});
-	}
-
-	//───────────────────────────────────────────────────────────────────────────
-	// SELECT NAVIGATION MARKER
-
-	let lineNum = 0;
-	const navigationMarkers = readFile(sfPath)
-		.split("\n")
-		.reduce((/** @type {AlfredItem[]} */ acc, line) => {
-			lineNum++;
-
-			// GUARD line is not marker
-			if (!line.startsWith("/* <") && !line.startsWith("  - # <<")) return acc;
-
-			const name = line
-				.replace(/ \*\/$/, "") // comment-ending
-				.replace(/^\/\* *<+ ?/, "") // comment-beginning
-				.replace(/^ {2}- # ?<+ ?/, ""); // YAML-comment
-
-			name.toString
-			acc.push({
-				title: name,
-				subtitle: lineNum.toString(),
-				match: alfredMatcher(name),
-				uid: name, // not lineNum, since the lineNum can change
-				arg: lineNum,
-			});
-			return acc;
-		}, []);
-
-	return JSON.stringify({ items: navigationMarkers });
+	return JSON.stringify({ items: [item] });
 }
