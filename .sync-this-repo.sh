1a80953d34de205ab0902676d654672ee56a069b
🤖 automated <cron@job>
2024-05-10 21:07:35 +0200

    iMac Home (3)

--- .sync-this-repo.sh
+++ .sync-this-repo.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")" || exit 1 # go to location of this script, i.e. going into 
 #───────────────────────────────────────────────────────────────────────────────
 
 files_changed="$(git status --porcelain | wc -l | tr -d ' ')"
+[[ $files_changed -eq 0 ]] && exit 0
 device_name=$(scutil --get ComputerName | cut -d" " -f2-)
 commit_msg="$device_name ($files_changed)"
 
@@ -12,7 +13,7 @@ commit_msg="$device_name ($files_changed)"
 # changed, the push will fail
 i=0
 while true; do
-	git add --all && git commit -m "$commit_msg" --author="🤖 automated<cron@job>" 2>&1 || exit 1
+	git add --all && git commit -m "$commit_msg" --author="🤖 automated<cron@job>" || exit 1
 	sleep 0.5 # prevent "Cannot rebase on multiple branches"
 	git pull && git push && exit 0
 	sleep 1
