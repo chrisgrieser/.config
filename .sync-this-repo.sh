7f4567fb18cf4fefe878404af6ef6dae23587991
🤖 automated <cron@job>
2024-05-10 20:20:02 +0200

    iMac Home (2)

--- .sync-this-repo.sh
+++ .sync-this-repo.sh
@@ -10,16 +10,12 @@ commit_msg="$device_name ($files_changed)"
 
 # loop git add-commit-pull-push, since when between add and push files have been
 # changed, the push will fail
-# 0: once push/pull is successful
-# 1: after multiple failed attempts to push/pull
-# 2: pre-commit hook failed
 i=0
 while true; do
-	git add --all && git commit -m "$commit_msg" --author="🤖 automated<cron@job>" 2>&1
-	[[ $? -eq 2 ]] && exit 2 # pre-commit failed
-	sleep 0.5                # prevent "Cannot rebase on multiple branches"
+	git add --all && git commit -m "$commit_msg" --author="🤖 automated<cron@job>" 2>&1 || exit 1
+	sleep 0.5 # prevent "Cannot rebase on multiple branches"
 	git pull && git push && exit 0
-	sleep 1
+	sleep 2
 	i=$((i + 1))
 	[[ $i -gt 3 ]] && exit 1
 done
