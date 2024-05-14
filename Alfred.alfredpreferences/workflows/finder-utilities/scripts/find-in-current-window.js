e6c4041c702651f9b9f060b898dfb755b923f561
🤖 automated <cron@job>
2024-05-14 11:03:52 +0200

    iMac Home (16)

--- Alfred.alfredpreferences/workflows/finder-utilities/scripts/find-in-current-window.js
+++ Alfred.alfredpreferences/workflows/finder-utilities/scripts/find-in-current-window.js
@@ -6,12 +6,15 @@ app.includeStandardAdditions = true;
 
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	const finderWinPath = decodeURIComponent(Application("Finder").insertionLocation().url().slice(7))
+	const finderWinPath = decodeURIComponent(
+		Application("Finder").insertionLocation().url().slice(7),
+	);
 
-	const itemsInWindow = app.doShellScript(`find "${finderWinPath}" -maxdepth 1 -mindepth 1`)
+	const itemsInWindow = app
+		.doShellScript(`find "${finderWinPath}" -maxdepth 1 -mindepth 1`)
 		.split("\r")
 		.map((absPath) => {
-			if (absPath.endsWith(".DS_Store")) return {}
+			if (absPath.endsWith(".DS_Store")) return {};
 			const name = absPath.split("/").pop();
 
 			return {
