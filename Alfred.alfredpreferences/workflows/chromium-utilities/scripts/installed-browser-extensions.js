be91dd089f0c58e9173a7a0ba1934ed344e64aa4
🤖 automated <cron@job>
2024-05-14 21:14:40 +0200

    iMac Home (6)

--- Alfred.alfredpreferences/workflows/chromium-utilities/scripts/installed-browser-extensions.js
+++ Alfred.alfredpreferences/workflows/chromium-utilities/scripts/installed-browser-extensions.js
@@ -18,93 +18,81 @@ function readFile(path) {
 	return ObjC.unwrap(str);
 }
 
-const fileExists = (/** @type {string} */ filePath) => Application("Finder").exists(Path(filePath));
-
 //──────────────────────────────────────────────────────────────────────────────
 
 /** @type {Record<string, string>} */
 const specialAnchors = {
+	"uBlock Origin": "#1p-filters.html",
 	"AdGuard AdBlocker": "#user-filter",
 	// biome-ignore lint/style/useNamingConvention: not set by me
 	Violentmonkey: "#scripts",
-	"uBlock Origin": "#1p-filters.html",
 };
-
-/** @type {Record<string, string>} */
-const browserPathMap = {
-	brave: "BraveSoftware/Brave-Browser",
-	chrome: "Google/Chrome",
-};
-
-//──────────────────────────────────────────────────────────────────────────────
 
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	const defaultsPath = browserPathMap[$.getenv("browser")];
-	const extensionFolder =
-		app.pathTo("home folder") + `/Library/Application Support/${defaultsPath}/Default/Extensions`;
+	const browser = $.getenv("browser");
+	const extensionPath = JSON.parse(readFile("./data/browser-vars.json")).extensionPath[browser];
 
 	// GUARD
-	if (!fileExists(extensionFolder)) {
-		const browserName = defaultsPath.split("/")[1];
+	const extensionManifests = app
+		.doShellScript(`find "${extensionPath}" -name "manifest.json"`)
+		.split("\r");
+	if (extensionManifests.length === 0) {
 		return JSON.stringify({
-			items: [{ title: `No extensions found for ${browserName}.` }],
+			items: [{ title: `No extensions found for ${browser}.` }],
 		});
 	}
 
-	const extensions = app
-		.doShellScript(`find "${extensionFolder}" -name "manifest.json"`)
-		.split("\r")
-		.map((manifestPath) => {
-			const root = manifestPath.slice(0, -13);
-			const id = root.replace(/.*Extensions\/(\w+)\/.*/, "$1");
-			const manifest = JSON.parse(readFile(manifestPath));
+	const extensions = extensionManifests.map((manifestPath) => {
+		const root = manifestPath.slice(0, -13);
+		const id = root.replace(/.*Extensions\/(\w+)\/.*/, "$1");
+		const manifest = JSON.parse(readFile(manifestPath));
 
-			// determine name
-			let name = manifest.name;
-			if (name.startsWith("__MSG_") && manifest.short_name) name = manifest.short_name;
-			if (name.startsWith("__MSG_")) {
-				const msg = JSON.parse(readFile(root + "_locales/en/messages.json"));
-				name =
-					msg.extensionName?.message ||
-					msg.name?.message ||
-					msg.extName?.message ||
-					msg.appName?.message ||
-					"[name not found]";
-			}
+		// determine name
+		let name = manifest.name;
+		if (name.startsWith("__MSG_") && manifest.short_name) name = manifest.short_name;
+		if (name.startsWith("__MSG_")) {
+			const msg = JSON.parse(readFile(root + "_locales/en/messages.json"));
+			name =
+				msg.extensionName?.message ||
+				msg.name?.message ||
+				msg.extName?.message ||
+				msg.appName?.message ||
+				"[name not found]";
+		}
 
-			// determine options path
-			let optionsPath = manifest.options_ui?.page || manifest.options_page || "";
+		// determine options path
+		let optionsPath = manifest.options_ui?.page || manifest.options_page || "";
 
-			// SPECIAL Stylus
-			if (name === "Stylus") optionsPath = "manage.html";
+		// SPECIAL Stylus
+		if (name === "Stylus") optionsPath = "manage.html";
 
-			// SPECIAL anchor for AdGuard, directly go to user rules
-			const anchor = specialAnchors[name] || "";
+		// SPECIAL anchor for AdGuard, directly go to user rules
+		const anchor = specialAnchors[name] || "";
 
-			const optionsUrl = `chrome-extension://${id}/${optionsPath}${anchor}`;
-			const webstoreUrl = `https://chrome.google.com/webstore/detail/${id}`;
-			const localFolder = extensionFolder + "/" + id;
+		const optionsUrl = `chrome-extension://${id}/${optionsPath}${anchor}`;
+		const webstoreUrl = `https://chrome.google.com/webstore/detail/${id}`;
+		const localFolder = extensionPath + "/" + id;
 
-			// emoji/icon
-			const emoji = optionsPath ? "" : " 🚫"; // indicate no options available
-			const icon = manifest.icons["128"] || manifest.icons["64"] || manifest.icons["48"];
-			const iconPath = root + icon;
+		// emoji/icon
+		const emoji = optionsPath ? "" : " 🚫"; // indicate no options available
+		const icon = manifest.icons["128"] || manifest.icons["64"] || manifest.icons["48"];
+		const iconPath = root + icon;
 
-			return {
-				title: name + emoji,
-				match: alfredMatcher(name),
-				icon: { path: iconPath },
-				valid: optionsPath !== "",
-				arg: optionsUrl,
-				uid: id,
-				mods: {
-					alt: { arg: webstoreUrl },
-					cmd: { arg: webstoreUrl },
-					fn: { arg: localFolder },
-				},
-			};
-		});
+		return {
+			title: name + emoji,
+			match: alfredMatcher(name),
+			icon: { path: iconPath },
+			valid: optionsPath !== "",
+			arg: optionsUrl,
+			uid: id,
+			mods: {
+				alt: { arg: webstoreUrl },
+				cmd: { arg: webstoreUrl },
+				fn: { arg: localFolder },
+			},
+		};
+	});
 	return JSON.stringify({
 		items: extensions,
 		cache: { seconds: 600, loosereload: true },
