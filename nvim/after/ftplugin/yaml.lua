0fd76ea16b77f29859d8d81cdcb1f1b393438220
🤖 automated <cron@job>
2024-05-16 15:11:14 +0200

    mini Office (3)

--- nvim/after/ftplugin/yaml.lua
+++ nvim/after/ftplugin/yaml.lua
@@ -4,24 +4,24 @@ vim.opt_local.expandtab = true
 
 --------------------------------------------------------------------------------
 
--- emulate truthy rule: https://yamllint.readthedocs.io/en/stable/rules.html#module-yamllint.rules.truthy
+-- TRUTHY RULE: https://yamllint.readthedocs.io/en/stable/rules.html#module-yamllint.rules.truthy
 -- implementing myself, since I do not want to install a linter just for one rule
 
 local function checkForYamlTruthy()
 	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
-
-	for _, line in pairs(lines) do
-		local truthy = line:find("[^\"']")
+	local msg = {}
+	for lnum = 1, #lines do
+		local line = lines[lnum]
+		local truthy = line:match(" yes$") or line:match(" no$")
 		if truthy then
-			if not citekeyCount[citekey] then
-				citekeyCount[citekey] = 1
-			else
-				duplCitekeys = duplCitekeys .. "\n" .. "- " .. citekey
-			end
+			table.insert(msg, ("%s: %q"):format(lnum, vim.trim(line)))
+			-- go to first occurrence
+			if #msg == 1 then vim.api.nvim_win_set_cursor(0, { lnum, #line }) end
 		end
 	end
-
-	vim.notify(lnum, vim.log.levels.WARN, { title = "Truthy" })
+	if #msg > 0 then
+		vim.notify(table.concat(msg, "\n"), vim.log.levels.WARN, { title = "Truthy Warning" })
+	end
 end
 
 -- run on entering a bibtex buffer
