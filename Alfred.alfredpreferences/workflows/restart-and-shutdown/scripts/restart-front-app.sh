1ff6d4653bb8822a7a2cd03d31b86ceffe3e9db9
🤖 automated <cron@job>
2024-05-17 12:45:37 +0200

    iMac Home (6)

--- Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/restart-front-app.sh
+++ Alfred.alfredpreferences/workflows/restart-and-shutdown/scripts/restart-front-app.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env zsh
 
-#───────────────────────────────────────────────────────────────────────────────
-# REGULAR RESTART
-
 FRONT_APP=$(osascript -e 'tell application "System Events" to return name of first process whose frontmost is true')
-if [[ "$FRONT_APP" != "neovide" ]]; then
+if [[ "$FRONT_APP" == "neovide" ]]; then
+	echo -n "Could not find frontmost app" # Alfred notification
+else
 	killall "$FRONT_APP"
-
-	# wait for 2.0 secs
-	i=0
-	while pgrep -xq "$FRONT_APP"; do
-		i=$((i + 1))
-		sleep 0.1
-		if [[ $i -gt 20 ]]; then
-			echo -n "Could not quit $FRONT_APP" # Alfred notification
-			return 1
-		fi
-	done
-	sleep 0.2
-
-	[[ "$FRONT_APP" == "wezterm-gui" ]] && FRONT_APP="WezTerm"
-	open -a "$FRONT_APP"
-	return
 fi
+
+
+# wait for 2.0 secs
+i=0
+while pgrep -xq "$FRONT_APP"; do
+	i=$((i + 1))
+	sleep 0.1
+	if [[ $i -gt 20 ]]; then
+		echo -n "Could not quit $FRONT_APP" # Alfred notification
+		return 1
+	fi
+done
+
+#───────────────────────────────────────────────────────────────────────────────
+
+[[ "$FRONT_APP" == "wezterm-gui" ]] && FRONT_APP="WezTerm"
+sleep 0.2
+open -a "$FRONT_APP"
+sleep 0.2
+open -a "$FRONT_APP"
