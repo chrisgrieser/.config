2638be5e74a011f65488ee28fc1825a4584fcfe5
🤖 automated <cron@job>
2024-05-20 13:15:33 +0200

    iMac Home (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,3 +1,7 @@
 -- BOB
 
-vim.notify("test", vim.log.levels.INFO, { title = "test" })
+vim.notify("test", vim.log.levels.INFO, {
+	title = "test",
+	animate = false,
+	on_open = function(winnr) vim.api.nvim_win_set_width(winnr, 80) end,
+})
