4720003374b9f4d0eac3f3f95cd8ff1676193547
🤖 automated <cron@job>
2024-05-20 23:45:32 +0200

    iMac Home (6)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,6 +1,2 @@
-local usedWithinMins = 15 -- CONFIG
-local recentBufs = vim.iter(vim.fn.getbufinfo { buflisted = 1 })
-	:filter(function(buf) return os.time() - buf.lastused < usedWithinMins * 60 end)
-	:map(function(buf) return buf.bufnr end)
-	:totable()
-vim.notify("⭕ recentBufs: " .. vim.inspect(recentBufs))
+-- vim.deprecate
+vim.deprecate("flllls,", "alt", "1.0")
