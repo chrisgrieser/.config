532cff40d92bbf9691bae7fdfda2685b8bb7a4ec
🤖 automated <cron@job>
2024-05-21 13:52:10 +0200

    mini Office (3)

--- nvim/lua/config/neovide-gui-settings.lua
+++ nvim/lua/config/neovide-gui-settings.lua
@@ -3,19 +3,22 @@
 if not vim.g.neovide then return end -- GUARD
 
 local g = vim.g
-local host = vim.fn.hostname()
-local isAtOffice = (host:find("mini") or host:find("eduroam") or host:find("fak1")) ~= nil
+local host = vim.uv.os_gethostname()
+local isAtOffice = (host:find("eduroam") or host:find("tu%-berlin")) ~= nil
 local isAtMother = host:find("Mother")
 --------------------------------------------------------------------------------
 
 -- SIZE & FONT
 if isAtMother then
+	g.neovide_scale_factor = 1.05
 	g.neovide_padding_top = 4
 	g.neovide_padding_left = 6
 elseif isAtOffice then
+	g.neovide_scale_factor = 1.05
 	g.neovide_padding_top = 0
-	g.neovide_padding_left = 4
+	g.neovide_padding_left = 2
 else
+	g.neovide_scale_factor = 1
 	g.neovide_padding_top = 15
 	g.neovide_padding_left = 7
 end
@@ -26,19 +29,19 @@ g.neovide_input_macos_option_key_is_meta = "none" -- so `{@~` etc. can be used
 
 -- Appearance
 g.neovide_remember_window_size = true
-g.neovide_transparency = 0.91
+g.neovide_hide_mouse_when_typing = true
+g.neovide_transparency = 0.92
 g.neovide_refresh_rate = isAtMother and 30 or 50
 vim.opt.linespace = -2 -- less line height
 
 --------------------------------------------------------------------------------
 -- CURSOR
 vim.opt.guicursor = {
-	"i-ci-c:ver25", -- INFO with noice.nvim, the guicursor cannot be styled in the cmdline https://github.com/folke/noice.nvim/issues/552
+	"i-ci-c:ver25", -- INFO with noice.nvim, guicursor does not apply to the cmdline https://github.com/folke/noice.nvim/issues/552
 	"n-sm:block",
 	"r-cr-o-v:hor10",
 	"a:blinkwait200-blinkoff350-blinkon550",
 }
-g.neovide_hide_mouse_when_typing = true
 
 g.neovide_cursor_animation_length = 0.01
 g.neovide_cursor_trail_size = 0.9
