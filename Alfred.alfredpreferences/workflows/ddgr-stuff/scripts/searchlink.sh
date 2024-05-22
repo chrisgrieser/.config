72387d73c263781b26362600bfbff1716cdfaf02
🤖 automated <cron@job>
2024-05-22 23:40:52 +0200

    iMac Home (1)

--- Alfred.alfredpreferences/workflows/ddgr-stuff/scripts/searchlink.sh
+++ Alfred.alfredpreferences/workflows/ddgr-stuff/scripts/searchlink.sh
@@ -7,18 +7,15 @@ query_enc=$(osascript -l JavaScript -e "encodeURIComponent('$query')")
 region=${region:-"us-en"} # Alfred variable
 ddgr_html_url="https://html.duckduckgo.com/html?q=$query_enc&kl=$region"
 
-response=$(curl --silent "$ddgr_html_url")
-if [[ -z "$response" ]]; then
-	echo -n "No response from DuckDuckGo."
-	return 1
-fi
 first_result=$(
-	echo "$response" |
+	curl --silent "$ddgr_html_url" |
 		grep --after-context=1 --max-count=1 "result__url" |
 		tail -n1 |
 		sed -E 's/^ *| *$//g'
 )
-first_result_url="http://$first_result"
 
-mdlink="[$query]($first_result_url)"
-echo -n "$mdlink" # paste via Alfred
+if [[ -z "$first_result" ]]; then
+	echo -n "No response from DuckDuckGo."
+else
+	echo -n "[$query](http://$first_result)" # paste via Alfred
+fi
