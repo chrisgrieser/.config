368b5aa9f1d1261f39cf3e7bc20684149d8fff38
🤖 automated <cron@job>
2024-05-25 12:55:10 +0200

    iMac Mother (3)

--- hammerspoon/lua/app-specific-behavior.lua
+++ hammerspoon/lua/app-specific-behavior.lua
@@ -15,6 +15,7 @@ M.aw_spotify = aw.new(function(appName, eventType)
 		or (env.isAtOffice or env.isProjector())
 		or not (hs.fnutils.contains(env.videoAndAudioApps, appName))
 		or not (eventType == aw.launched or eventType == aw.terminated)
+		or u.betweenTime(22, 7)
 	then
 		return
 	end
