a7b38bf2ebc8ada894ef138f5d5b959807eacbfb
🤖 automated <cron@job>
2024-05-31 14:57:20 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/youtube-utilities/scripts/ydl.sh
+++ Alfred.alfredpreferences/workflows/youtube-utilities/scripts/ydl.sh
@@ -1,29 +1,35 @@
 #!/bin/zsh
+# shellcheck disable=2154 # $browser_app set in Alfred settings
+
+function notify {
+	./notificator --title "yt-dlp" --message "$1" --subtitle "$2"
+}
+
+#───────────────────────────────────────────────────────────────────────────────
 
 # cannot use JXA to get browser URL, since sometimes a PWA is frontmost
-# shellcheck disable=2154 # $browser_app set in Alfred settings
 url=$(osascript -e "tell application \"$browser_app\" to return URL of active tab of front window")
 
 if [[ -z "$url" ]]; then
-	echo -n "❌ Tab could not be retrieved."
+	./notificator --title "yt-dlp" --message "❌ Tab could not be retrieved."
 	return 1
 elif [[ ! -d "$download_location" ]]; then
-	echo -n "❌ Invalid Download Location."
+	./notificator --title "yt-dlp" --message "❌ Invalid Download Location."
 	return 1
 fi
 
 #───────────────────────────────────────────────────────────────────────────────
 # DOWNLOAD
-./notificator --title "yt-dlp" --message "⏳ Starting Download…" --subtitle "$url"
+notify "yt-dlp" "⏳ Starting Download…"
 msg=$(cd "$download_location" && yt-dlp --quiet "$url")
 success=$?
 
 if [[ $success -eq 0 ]]; then
-	echo -n "✅ Download finished."
+	notify "✅ Download finished."
 elif ! [[ "$(yt-dlp --update)" =~ "up to date" ]]; then
-	echo -n "ℹ️  yt-dlp not up to date."
+	notify "ℹ️  yt-dlp not up to date."
 	echo -n "brew update && brew upgrade yt-dlp" | pbcopy
 else
 	afplay "/System/Library/Sounds/Basso.aiff" &
-	echo -n "❌ $msg"
+	notify "❌ $msg"
 fi
