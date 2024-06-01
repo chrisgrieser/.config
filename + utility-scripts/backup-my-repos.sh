d63f5221e01f4a7dcfb70c7f5473038c83d02d93
🤖 automated <cron@job>
2024-06-01 19:20:31 +0200

    iMac Home (2)

--- + utility-scripts/backup-my-repos.sh
+++ + utility-scripts/backup-my-repos.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env zsh
-export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
-# INFO
-# - Backup all PUBLIC NON-FORK repos owned by the specified github user as zip archive.
-# - Requires `yq`.
-# - Due to github API restrictions, only a maximum of 100 repos are downloaded.
-#───────────────────────────────────────────────────────────────────────────────
 
 # CONFIG
+# ignores perma-repos since they are already locally backed up
 backup_location="$DATA_DIR/Backups/My Repos"
 max_number_of_bkps=4
+ignore_repos=$(cut -d, -f2 "$HOME/.config/perma-repos.csv" | cut -c2- |
+	tr "\n" "|" | sed -e "s/.$//") # construct regex pattern for `grep`
 
 #───────────────────────────────────────────────────────────────────────────────
 
+set -e
+
 # get all repos
-repos=$(gh repo list --limit=200 --no-archived --source --json=" nameWithOwner" --jq=".[].nameWithOwner")
+repos=$(gh repo list --limit=200 --no-archived --source --json="nameWithOwner" --jq=".[].nameWithOwner" |
+	grep --extended-regexp --invert-match "$ignore_repos")
 repos_count=$(echo "$repos" | wc -l | tr -d " ")
 if [[ repos_count -ge 100 ]]; then
 	print "\e[1;33mMore than 100 repos. Unclear whether GitHub API allows for so many listings, so need to manually check that all repos are included.\e[0m"
@@ -38,9 +38,8 @@ zip -r --quiet "../$archive_name" . || return 1
 #───────────────────────────────────────────────────────────────────────────────
 # restrict number of backups
 cd "$backup_location" || return 1
-actual_number=$((max_number_of_bkps + 1))
 # shellcheck disable=2012
-ls -t | tail -n +$actual_number | tr '\n' '\0' | xargs -0 rm
+ls -t | tail -n +$((max_number_of_bkps + 1)) | tr '\n' '\0' | xargs -0 rm
 
 # confirm and remove leftover folders
 rm -rf "$backup_location/temp"
