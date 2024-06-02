ae3d80d8e43caf9b6519e2f3e730f33227e2c70c
🤖 automated <cron@job>
2024-06-02 12:20:30 +0200

    iMac Home (4)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,3 +1,13 @@
-local out = "test"
-local recipes = vim.split(out, " ")
-vim.notify("⭕ recipes: " .. vim.inspect(recipes))
+---@param str string
+---@param filePath string
+---@return string|nil -- error message
+local function overwriteFile(filePath, str)
+	local file, _ = io.open(filePath, "w")
+	if not file then return end
+	file:write(str)
+	file:close()
+end
+
+vim.uv.fs_mkdir("./debug/ffff", 493)
+
+overwriteFile("./debug/ffff/test.txt", "hello")
