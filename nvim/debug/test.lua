bad3b6142543f078041e613456d87f405e830b1c
🤖 automated <cron@job>
2024-06-05 18:19:53 +0200

    iMac Home (5)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,11 +1,4 @@
-local msg = "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit."
-
-vim.notify(msg, vim.log.levels.INFO, {
-	title = "Foobar",
-	timeout = false,
-})
-vim.notify(msg, vim.log.levels.INFO, {
-	timeout = false,
-})
 
 
+-- fooooooooooobar foobar
+-- foobar fooooooooooobar
