6d6b753e0d35234ed07bb723054b1c36a3f9d13f
🤖 automated <cron@job>
2024-06-05 12:49:53 +0200

    iMac Home (4)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,13 +1,3 @@
--- vim.ui.input({
--- 	prompt = "Hello World",
--- 	footer = "foobar",
--- }, function(text) vim.notify(text or "") end)
-
-vim.ui.select({ "one", "two" }, {
-	prompt = "Hello World",
-	footer = "foobar",
-	kind = "codeaction",
-}, function(selection)
-	if not selection then return end
-	vim.notify(selection)
-end)
+require("telescope.builtin").colorscheme {
+	ignore_builtins = true,
+}
