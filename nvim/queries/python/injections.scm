7ac1ef0d3d79baf802a56bccaa3a72e603008430
🤖 automated <cron@job>
2024-06-07 00:10:56 +0200

    iMac Home (3)

--- nvim/queries/python/injections.scm
+++ nvim/queries/python/injections.scm
@@ -2,11 +2,9 @@
 
 ; SOURCE https://github.com/Dronakurl/injectme.nvim
 ;───────────────────────────────────────────────────────────────────────────────
-
-;rst_for_docstring
-; (function_definition
-;   (block
-;     (expression_statement
-;       (string
-;         (string_content) @injection.content (#set! injection.language "rst")))))
-;
+rst_for_docstring (function_definition
+  (block
+    (expression_statement
+      (string
+        (string_content) @injection.content
+        (#set! injection.language "rst")))))
