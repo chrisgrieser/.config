9003aab1bc518ea432d4e3ce955ba7057940ecf9
🤖 automated <cron@job>
2024-06-07 12:40:55 +0200

    iMac Home (9)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,11 +1,3 @@
-local win = vim.api.nvim_open_win(0, true, {
-	relative = "editor",
-	row = 0,
-	col = 80,
-	width = 20,
-	height = 10,
-	title = "test",
-	border = "single",
-})
-
-vim.api.nvim_win_set_config(win, { title = "foobar" })
+require("lualine.components.diff.git_diff").update_diff_args()
+require("lualine.components.diff.git_diff").update_git_diff()
+require('lualine').refresh()
