606f20b160df72f200ada98802c371ca1639c0df
🤖 automated <cron@job>
2024-06-07 13:10:55 +0200

    iMac Home (4)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,3 +1,3 @@
 require("lualine.components.diff.git_diff").update_diff_args()
-require("lualine.components.diff.git_diff").update_git_diff()
-require('lualine').refresh()
+
+-- require("lualine.components.diff.git_diff").update_git_diff()
