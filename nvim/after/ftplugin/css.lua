2594e5e1af7ff7003c4885cc2262dc3fb05f9e5d
🤖 automated <cron@job>
2024-06-08 18:32:13 +0200

    iMac Home (3)

--- nvim/after/ftplugin/css.lua
+++ nvim/after/ftplugin/css.lua
@@ -1,5 +1,7 @@
 vim.bo.commentstring = "/* %s */"
 
+--------------------------------------------------------------------------------
+
 -- toggle !important (useful for debugging selectors)
 vim.keymap.set("n", "<leader>i", function()
 	local line = vim.api.nvim_get_current_line()
@@ -10,13 +12,3 @@ vim.keymap.set("n", "<leader>i", function()
 	end
 	vim.api.nvim_set_current_line(line)
 end, { buffer = true, desc = " Toggle !important", nowait = true })
-
--- workaround for `opt.exrc` not working with neovide
--- for security reasons, restricted to `~/repos/shimmering-focus`
-vim.defer_fn(function()
-	local u = require("config.utils")
-	local exrc = vim.fs.normalize("~/repos/shimmering-focus/.nvim.lua")
-	if vim.uv.cwd() == vim.fs.dirname(exrc) and u.fileExists(exrc) then
-		vim.cmd.source(exrc)
-	end
-end, 200)
