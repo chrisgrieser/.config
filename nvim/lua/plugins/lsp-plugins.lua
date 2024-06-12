3c9fd15e4a0d72161dc4b791e7a6a13798026b84
🤖 automated <cron@job>
2024-06-12 18:04:31 +0200

    iMac Home (7)

--- nvim/lua/plugins/lsp-plugins.lua
+++ nvim/lua/plugins/lsp-plugins.lua
@@ -2,6 +2,30 @@ local u = require("config.utils")
 --------------------------------------------------------------------------------
 
 return {
+	{
+		"ldelossa/litee.nvim",
+		event = "VeryLazy",
+		opts = {
+			notify = { enabled = false },
+			panel = {
+				orientation = "bottom",
+				panel_size = 10,
+			},
+		},
+		config = function(_, opts) require("litee.lib").setup(opts) end,
+	},
+
+	{
+		"ldelossa/litee-calltree.nvim",
+		dependencies = "ldelossa/litee.nvim",
+		event = "VeryLazy",
+		opts = {
+			on_open = "panel",
+			map_resize_keys = false,
+		},
+		config = function(_, opts) require("litee.calltree").setup(opts) end,
+	},
+	-----------------------------------------------------------------------------
 	{ -- nvim lua typings
 		"folke/lazydev.nvim",
 		ft = "lua",
