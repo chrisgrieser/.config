eaca35ea725991637552934fe29c9429f66b8858
🤖 automated <cron@job>
2024-06-12 20:34:31 +0200

    iMac Home (1)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,6 +1,8 @@
-local msg = "Lorem ipsum dolor sit amet, officia excepteur. Lorem ipsum dolor sit amet, officia excepteur"
+local ffff
+local msg = "Lorem local dolor sit amet, officia excepteur. Lorem ipsum dolor sit amet, officia excepteur"
 vim.notify(msg, vim.log.levels.INFO, {
 	title = "Lorem ipsum",
 	timeout = false,
 })
 
+vim.fn.matchadd("ErrorMsg", "\\s*local")
