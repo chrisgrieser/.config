54fdd585695295ea8961577068050c3ab5f2fd55
🤖 automated <cron@job>
2024-06-12 10:29:08 +0200

    iMac Home (2)

--- zsh/plugins/magic_dashboard.zsh
+++ zsh/plugins/magic_dashboard.zsh
@@ -71,7 +71,8 @@ function _list_files_here {
 }
 
 function _gitstatus {
-	git add --intent-to-add --all # so new files show up in `git diff`
+	# so git picks up new files
+	git ls-files --others --exclude-standard | xargs -I {} git add --intent-to-add {} &>/dev/null
 
 	if [[ -n "$(git status --porcelain)" ]]; then
 		local max_gitstatus_lines=${MAGIC_DASHBOARD_GITSTATUS_LINES:-12}
