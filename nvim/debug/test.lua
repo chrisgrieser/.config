5241bed0a1920943ea27ccd0e2c56fbee100dadb
🤖 automated <cron@job>
2024-06-12 21:34:31 +0200

    iMac Home (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,9 +1,17 @@
-local ffff
-local msg = "Lorem local dolor sit amet, officia excepteur. Lorem ipsum dolor sit amet, officia excepteur"
-vim.notify(msg, vim.log.levels.INFO, {
-	title = "Lorem ipsum",
-	timeout = false,
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("DressingInput", { clear = true }),
+	pattern = "DressingInput",
+	callback = function(ctx)
+		local win = vim.api.nvim_get_current_win()
+		local startWidth = vim.api.nvim_win_get_width(win)
+		local pad = 3
+		vim.api.nvim_create_autocmd("TextChangedI", {
+			buffer = ctx.buf,
+			callback = function()
+				local lineLength = #vim.api.nvim_get_current_line() + pad
+				local width = vim.api.nvim_win_get_width(win)
+				if lineLength > width then vim.api.nvim_win_set_width(win, lineLength + pad) end
+			end,
+		})
+	end,
 })
-
-vim.fn.matchadd("ErrorMsg", "\\s*local")
--- test: fffff
