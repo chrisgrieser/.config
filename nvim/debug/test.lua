0ddd2aed1c898d3038ebe447cb1259e24c8728ed
🤖 automated <cron@job>
2024-06-17 15:01:11 +0200

    iMac Home (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,7 +1,9 @@
 --------------------------------------------------------------------------------
 
 
--- local str = "\n"
-local str = "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+local str = ""
+vim.notify(str, vim.log.levels.INFO, { title = "test" })
+
+str = "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
 vim.notify(str, vim.log.levels.INFO, { title = "test" })
 
