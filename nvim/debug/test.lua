078414ba636e104207b65c2a69883afcc67c189c
🤖 automated <cron@job>
2024-06-18 16:01:09 +0200

    iMac Home (6)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -4,25 +4,9 @@ function _G.myFunc(motionType)
 		return "g@"
 	end
 
-	-- `] and `[ contain the start and end of the selection
-	local startLn, startCol = unpack(vim.api.nvim_buf_get_mark(0, "["))
-	local endLn, endCol = unpack(vim.api.nvim_buf_get_mark(0, "]"))
-	if motionType == "char" then
-		local lines = vim.api.nvim_buf_get_text(0, startLn - 1, startCol, endLn - 1, endCol + 1, {})
-		local text = table.concat(lines, "\n")
-		vim.notify(vim.inspect(vim.fn.luaeval(text)))
-	elseif motionType == "line" then
-		local lines = vim.api.nvim_buf_get_lines(0, startLn - 1, endLn, false)
-		local tmpfile = os.tmpname()
-
-		local file, _ = io.open(tmpfile, "w")
-		if not file then return end
-		file:write(table.concat(lines, "\n"))
-		file:close()
-
-		local result = vim.system({ "nvim", "-l", tmpfile }):wait()
-		local out = (result.stdout or "") .. (result.stderr or "") -- nvim writes to stderr
-		vim.notify(out, vim.log.levels[result.code == 0 and "INFO" or "ERROR"])
-	end
+	print(motionType)
 end
-vim.keymap.set("n", "gt", _G.myFunc, { expr = true })
+
+print("lllllbllbblll")
+
+
