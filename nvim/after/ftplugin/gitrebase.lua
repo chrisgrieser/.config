31ed7dd597c27aba3ceb9708f2b6268a434522d4
🤖 automated <cron@job>
2024-06-19 01:01:09 +0200

    iMac Home (12)

--- nvim/after/ftplugin/gitrebase.lua
+++ nvim/after/ftplugin/gitrebase.lua
@@ -1,12 +1,14 @@
 vim.opt_local.listchars:remove("multispace")
 
---------------------------------------------------------------------------------
-
-vim.keymap.set("n", "<CR>", vim.cmd.wq, { buffer = true, desc = "Confirm" })
-
--- quting with error code = aborting commit
-vim.keymap.set("n", "q", vim.cmd.cquit, { buffer = true, nowait = true })
-
--- Cycle Rebase Action
+-- KEYMAPS
+vim.keymap.set("n", "<CR>", vim.cmd.wq, { desc = "Confirm", buffer = true })
+vim.keymap.set("n", "q", vim.cmd.cquit, { desc = "Abort", buffer = true, nowait = true })
 vim.keymap.set("n", "<Tab>", "<C-a>", { desc = "Cycle Action", buffer = true, remap = true })
 vim.keymap.set("n", "<S-Tab>", "<C-x>", { desc = "Cycle Action", buffer = true, remap = true })
+
+-- HIGHLIGHTING
+-- apply to whole window, but sinc that window is closed anyway, it's not a problem
+local ok, tinygit = pcall(require, "tinygit.shared.utils")
+if ok and tinygit then tinygit.commitMsgHighlighting() end
+
+vim.fn.matchadd("NonText", [[^drop .*]])
