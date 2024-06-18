262522d9a0af158bb506a29cfd824d0583c73cb8
🤖 automated <cron@job>
2024-06-19 01:01:09 +0200

    iMac Home (12)

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -57,9 +57,9 @@ keymap("n", "U", "<cmd>silent redo<CR>zv")
 keymap("i", "<D-z>", "<C-o>u", { desc = "󰜊 Undo" }) -- also needed for Alfred killring plugin
 
 -- emulate some basic commands from `vim-abolish`
-foobar("n", "crt", "mzguiwgUl`z", { desc = "󰬴 Titlecase" })
-foobar("n", "cru", "mzgUiw`z", { desc = "󰬴 lowercase" })
-foobar("n", "crl", "mzguiw`z", { desc = "󰬴 UPPERCASE" })
+keymap("n", "crt", "mzguiwgUl`z", { desc = "󰬴 Titlecase" })
+keymap("n", "cru", "mzgUiw`z", { desc = "󰬴 lowercase" })
+keymap("n", "crl", "mzguiw`z", { desc = "󰬴 UPPERCASE" })
 
 keymap("n", "~", '<cmd>lua require("funcs.nano-plugins").betterTilde()<CR>', { desc = "better ~" })
 
