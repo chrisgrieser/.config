418e3e35d07a0e30f418dded9522895774281d7e
🤖 automated <cron@job>
2024-06-19 13:01:08 +0200

    iMac Home (5)

---
+++ Alfred.alfredpreferences/workflows/shimmering-obsidian/scripts/change-vault-setting.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @type {AlfredRun} */
+// biome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run(argv) {
+	const vaultPath = argv[0];
+
+
+
+	return
+}
