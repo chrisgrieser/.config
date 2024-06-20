c287c4604c109cd8cf4c60e026d299a57d09769b
🤖 automated <cron@job>
2024-06-20 21:01:06 +0200

    iMac Home (4)

--- nvim/lua/plugins/files-and-buffers.lua
+++ nvim/lua/plugins/files-and-buffers.lua
@@ -2,20 +2,22 @@ return {
 	{ -- convenience file operations
 		"chrisgrieser/nvim-genghis",
 		external_dependencies = "macos-trash",
-		cmd = "Genghis",
 		dependencies = "stevearc/dressing.nvim",
+		cmd = "Genghis",
 		keys = {
 			-- stylua: ignore start
 			{"<C-p>", function() require("genghis").copyFilepath() end, desc = "󰞇 Copy path" },
 			{"<C-t>", function() require("genghis").copyRelativePath() end, desc = "󰞇 Copy relative path" },
 			{"<C-n>", function() require("genghis").copyFilename() end, desc = "󰞇 Copy filename" },
 			{"<C-r>", function() require("genghis").renameFile() end, desc = "󰞇 Rename file" },
-			{"<D-M>", function() require("genghis").moveToFolderInCwd() end, desc = "󰞇 Move file" },
-			{"<leader>x", function() require("genghis").chmodx() end, desc = "󰞇 chmod +x" },
-			{"<C-d>", function() require("genghis").duplicateFile() end, desc = "󰞇 Duplicate file" },
-			{"<D-BS>", function() require("genghis").trashFile() end, desc = "󰞇 Move file to trash" },
+
 			{"<D-n>", function() require("genghis").createNewFile() end, desc = "󰞇 Create new file" },
+			{"<C-d>", function() require("genghis").duplicateFile() end, desc = "󰞇 Duplicate file" },
+			{"<D-M>", function() require("genghis").moveToFolderInCwd() end, desc = "󰞇 Move file" },
 			{"<leader>fx", function() require("genghis").moveSelectionToNewFile() end, mode = "x", desc = "󰞇 Selection to new file" },
+
+			{"<D-BS>", function() require("genghis").trashFile() end, desc = "󰞇 Move file to trash" },
+			{"<leader>x", function() require("genghis").chmodx() end, desc = "󰞇 chmod +x" },
 			-- stylua: ignore end
 		},
 	},
