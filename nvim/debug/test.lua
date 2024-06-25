b4101994e48fe47e4d32d4fc5cd8921c902be473
🤖 automated <cron@job>
2024-06-25 16:15:17 +0200

    mini Office (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,20 +1,20 @@
 local specRoot = require("lazy.core.config").options.spec.import
 local specPath = vim.fn.stdpath("config") .. "/lua/" .. specRoot
-
-local handler = vim.loop.fs_scandir(specPath)
+local handler = vim.loop.fs_scandir(specPath) or nil
 if not handler then return end
+
 local specFiles = {}
 repeat
-	local file, type = vim.loop.fs_scandir_next(handler)
-	if type == "file" and file then
-		local module = specRoot .. "." .. file:gsub("%.lua$", "")
-		local M = require(module)
-		if type(M[1]) == "table" then
-			vim.list_extend(specFiles, M)
-		else
-			table.insert(specFiles, M)
-		end
+	local file, kind = vim.loop.fs_scandir_next(handler)
+	if kind == "file" and file then
+		local moduleName = file:gsub("%.lua$", "")
+		local module = require(specRoot .. "." ..moduleName)
+		if type(module[1]) == "string" then module = { module } end
+		local plugins = vim.iter(module)
+			:map(function(plugin) return { repo = plugin[1], module = moduleName } end)
+			:totable()
+		vim.list_extend(specFiles, plugins)
 	end
 until not file
 
-vim.notify("👾 flat: " .. vim.inspect(specFiles))
+vim.notify("👾 specFiles: " .. vim.inspect(specFiles))
