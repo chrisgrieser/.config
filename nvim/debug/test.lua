d04d5209c6f8b7201a192711b7ab6cdbde53c9b8
🤖 automated <cron@job>
2024-06-29 16:59:03 +0200

    iMac Home (287)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,6 +1,4 @@
 local cc = require("tinygit.config").config.commitMsg.conventionalCommits.keywords
 local regex = [[\v(]] .. table.concat(cc, "|") .. [[)(\(.{-}\))?\ze: ]]
 vim.notify("👾 regex: " .. tostring(regex))
-vim.fn.matchadd("Title", regex)
 
--- fix: blaaaa
