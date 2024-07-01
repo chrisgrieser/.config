9a5b105c47b03612731937f1e31b34c5fc23f299
🤖 automated <cron@job>
2024-07-01 19:29:01 +0200

    iMac Home (1)

---
+++ nvim/debug/test.lua
@@ -0,0 +1,23 @@
+local M = {}
+local inlayHintNs = vim.api.nvim_create_namespace("lspEndhints")
+--------------------------------------------------------------------------------
+
+---@param userConfig? LspEndhints.config
+function M.setup(userConfig)
+	require("lsp-endhints.config").setup(userConfig)
+	require("lsp-endhints.override-handler")(inlayHintNs)
+<<<<<<< HEAD
+
+	local config = require("eol-lsp-hints.config").config
+	if config.autoEnableHints then autoEnableHints() end
+||||||| parent of c36ed92 (refactor: auto-enable as separate module)
+
+	local config = require("lsp-endhints.config").config
+	if config.autoEnableHints then autoEnableHints() end
+=======
+	require("lsp-endhints.auto-enable")(inlayHintNs)
+>>>>>>> c36ed92 (refactor: auto-enable as separate module)
+end
+
+--------------------------------------------------------------------------------
+return M
