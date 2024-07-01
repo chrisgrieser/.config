11ff094f4816fd37d9526aaba558a8c1ac9df325
🤖 automated <cron@job>
2024-07-01 13:17:29 +0200

    iMac Home (1)

--- nvim/after/ftplugin/gitrebase.lua
+++ nvim/after/ftplugin/gitrebase.lua
@@ -1,10 +1,20 @@
 vim.opt_local.listchars:remove("multispace")
 
+local function bufKeymap(mode, key, cmd, opts)
+	opts = vim.tbl_extend("force", { buffer = true, silent = true, nowait = true }, opts or {})
+	vim.keymap.set(mode, key, cmd, opts)
+end
+
 -- KEYMAPS
-vim.keymap.set("n", "<CR>", "ZZ", { desc = "Confirm", buffer = true })
-vim.keymap.set("n", "q", vim.cmd.cquit, { desc = "Abort", buffer = true, nowait = true })
-vim.keymap.set("n", "<Tab>", "<C-a>", { desc = "Cycle Action", buffer = true, remap = true })
-vim.keymap.set("n", "<S-Tab>", "<C-x>", { desc = "Cycle Action", buffer = true, remap = true })
+bufKeymap("n", "<CR>", "ZZ", { desc = "Confirm" })
+bufKeymap("n", "q", vim.cmd.cquit, { desc = "Abort" })
+bufKeymap("n", "<Tab>", "<C-a>", { desc = "Cycle Action" })
+
+-- leave out auto-formatting via `==`, since buggy
+bufKeymap("n", "<Down>", [[<cmd>. move +1<CR>]], { desc = "󰜮 Move line down" })
+bufKeymap("n", "<Up>", [[<cmd>. move -2<CR>]], { desc = "󰜷 Move line up" })
+bufKeymap("x", "<Down>", [[:move '>+1<CR>gv]], { desc = "󰜮 Move selection down" })
+bufKeymap("x", "<Up>", [[:move '<-2<CR>gv]], { desc = "󰜷 Move selection up" })
 
 -- HIGHLIGHTING
 -- apply to whole window, but sinc that window is closed anyway, it's not a problem
