6d19fdd3495deb0085661742b11412be56b988f1
🤖 automated <cron@job>
2024-07-01 16:59:01 +0200

    iMac Home (3)

--- sketchybar/homebrew-update-tracker.sh
+++ sketchybar/homebrew-update-tracker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 outdated=$(brew outdated --quiet | wc -l | tr -d " ")
-threshold=30
+threshold=20
 
 # only show menubar item above threshold
 if [[ $outdated -gt $threshold ]]; then
