bab6284d8bbf27056447d5c07127b94d2f34f4ed
🤖 automated <cron@job>
2024-07-01 10:59:01 +0200

    iMac Home (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,1 +1,8 @@
-require("funcs.eol-inlayhints")
+local cc = require("tinygit.config").config.commitMsg.conventionalCommits.keywords
+local ccRegex = [[\v(]] .. table.concat(cc, "|") .. [[)(\(.{-}\))?!?\ze: ]]
+vim.fn.matchadd("Title", ccRegex)
+
+-- fix: ffffff
+-- fix(scope): fffffff
+-- fix(scope)!: ffffff
+-- fix!: ffffff
