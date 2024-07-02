edc3eecff70385d2dcf7e4303d5afe534863e528
🤖 automated <cron@job>
2024-07-02 14:53:30 +0200

    mini Office (1)

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -2,7 +2,7 @@ local keymap = require("config.utils").bufKeymap
 --------------------------------------------------------------------------------
 
 -- options
-vim.opt_local.listchars:remove("multispace")
+vim.opt_local.listchars:remove("multispace") -- spacing in comments
 
 -- spelling
 vim.opt_local.spell = true
@@ -12,5 +12,9 @@ keymap("n", "gE", "[s")
 
 -- utility keymap
 keymap("i", "<Tab>", "<End>")
-keymap("n", "<CR>", "ZZ", { desc = "Confirm" })
-keymap("n", "q", vim.cmd.cquit, { desc = "Abort" }) -- quitting with error = aborting commit
+
+-- condition prevents mapping `DressingInput`, which already has its own mappings
+if vim.bo.buftype ~= "nofile" then
+	keymap("n", "<CR>", "ZZ", { desc = "Confirm" }) -- quitting with saving = committing
+	keymap("n", "q", vim.cmd.cquit, { desc = "Abort" }) -- quitting with error = aborting commit
+end
