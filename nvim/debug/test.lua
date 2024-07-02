7c63cb69a85a155352dc77fbf7020e11d4ad3505
🤖 automated <cron@job>
2024-07-02 19:28:59 +0200

    iMac Home (1)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,15 +1,13 @@
-local M = {}
-local inlayHintNs = vim.api.nvim_create_namespace("lspEndhints")
---------------------------------------------------------------------------------
-
----@param userConfig? LspEndhints.config
-function M.setup(userConfig)
-	require("lsp-endhints.config").setup(userConfig)
-	require("lsp-endhints.override-handler")(inlayHintNs)
-
-	local config = require("eol-lsp-hints.config").config
-	if config.autoEnableHints then autoEnableHints() end
-end
-
---------------------------------------------------------------------------------
-return M
+-- local function toSmallCap(str)
+-- 	local small = "󰫮󰫯󰫰󰫱󰫲󰫳󰫴󰫵󰫶󰫷󰫸󰫹󰫺󰫻󰫼󰫽󰫾󰫿󰬀󰬁󰬂󰬃󰬄󰬅󰬆󰬇"
+-- 	local alphabet = "abcdefghijklmnopqrstuvwxyz"
+-- 	for i = 1, #alphabet do
+-- 		local lowerUpper = alphabet:sub(i, i) .. alphabet:sub(i, i):upper()
+-- 		str = str:gsub("[" .. lowerUpper .. "]", small:sub(i, i))
+-- 	end
+-- 	return str
+-- end
+--
+-- local input = "foobar"
+-- local output = toSmallCap(input)
+-- vim.notify("👾 output: " .. tostring(output))
