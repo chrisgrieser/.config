82afaebd403479e50d15c262026a7f473c85fa0e
🤖 automated <cron@job>
2024-07-06 08:48:25 +0200

    iMac Home (4)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,2 +1,10 @@
-print("Hello World")
-print("Hello World 2")
+local path = "/Users/chrisgrieser/.config/nvim/lua/plugins/noice-and-notification.lua"
+local name = vim.fs.basename(path)
+vim.notify("👾 name: " .. tostring(name))
+local maxLength = 25 --CONFIG
+if #name > maxLength then name = vim.trim(name:sub(1, maxLength)) .. "…" end
+local _, devicons = pcall(require, "nvim-web-devicons")
+local extension = name:match("%w+$")
+local icon = devicons.get_icon(name, extension) or devicons.get_icon(name, vim.vim.bo.ft)
+local out = icon .. " " .. name
+vim.notify("👾 out: " .. vim.inspect(out))
