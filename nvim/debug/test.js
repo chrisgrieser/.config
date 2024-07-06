6a98fe95b6acf6bda07b6c0b227b70ce81862410
🤖 automated <cron@job>
2024-07-06 14:48:25 +0200

    iMac Home (4)

--- nvim/debug/test.js
+++
@@ -1,6 +0,0 @@
-const vim = { bo: { filetype: "js" } };
-
-function hello() {
-	const ft = vim.bo.filetype;
-	const other_ft = vim.bo.filetype;
-}
--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,8 +1,11 @@
--- DOCS https://github.com/nvim-treesitter/nvim-treesitter#adding-queries
+-- add favicons to links in comments
+-- https://github.com/nvim-treesitter/nvim-treesitter#adding-queries
 -- https://stackoverflow.com/questions/78714013/flutter-web-shows-a-gray-screen-in-hosting-only-without-any-error-in-debug-or-re
+-- https://new.reddit.com/r/neovim/comments/1dw4ha2/remote_development/
+-- https://discord.com/channels/686053708261228577/1056113012999720980
 
-local function hello()
-	m.bo
-	local ft = vim.bo.filetype
-	local other_ft = vim.vim.bo.filetype
-end
+-- but not to urls elsewhere
+local myUrl = "https://github.com/nvim-treesitter/nvim-treesitter#adding-queries"
+local myUrl2 = "https://new.reddit.com/r/neovim/comments/1dw4ha2/remote_development/"
+
+
