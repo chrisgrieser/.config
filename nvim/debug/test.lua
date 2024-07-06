742d5d08feb68502f95590d6a676e84e8b2db3d1
🤖 automated <cron@job>
2024-07-06 18:17:26 +0200

    iMac Home (1)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,24 +1,15 @@
+local ns = vim.api.nvim_create_namespace("personalDiagnostics")
+local bufnr = bufnr or 0
 
-local M = {}
-local inlayHintNs = vim.api.nvim_create_namespace("lspEndhints")
---------------------------------------------------------------------------------
-
----@param userConfig? LspEndhints.config
-function M.setup(userConfig)
-	require("lsp-endhints.config").setup(userConfig)
-	require("lsp-endhints.override-handler")(inlayHintNs)
-<<<<<<< HEAD
-
-	local config = require("eol-lsp-hints.config").config
-	if config.autoEnableHints then autoEnableHints() end
-||||||| parent of c36ed92 (refactor: auto-enable as separate module)
-
-	local config = require("lsp-endhints.config").config
-	if config.autoEnableHints then autoEnableHints() end
-=======
-	require("lsp-endhints.auto-enable")(inlayHintNs)
->>>>>>> c36ed92 (refactor: auto-enable as separate module)
-end
+---@type vim.Diagnostic[]
+local diags = {
+	{
+		lnum = 1,
+		col = 1,
+		message = "test",
+	},
+}
 
 --------------------------------------------------------------------------------
-return M
+
+vim.diagnostic.set(ns, bufnr, diags)
