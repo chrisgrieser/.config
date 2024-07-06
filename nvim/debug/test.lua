5f1ae2836af9104afae7d06557de6b19dbfacd72
🤖 automated <cron@job>
2024-07-06 09:18:25 +0200

    iMac Home (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,10 +1,20 @@
-local path = "/Users/chrisgrieser/.config/nvim/lua/plugins/noice-and-notification.lua"
-local name = vim.fs.basename(path)
-vim.notify("👾 name: " .. tostring(name))
-local maxLength = 25 --CONFIG
-if #name > maxLength then name = vim.trim(name:sub(1, maxLength)) .. "…" end
-local _, devicons = pcall(require, "nvim-web-devicons")
-local extension = name:match("%w+$")
-local icon = devicons.get_icon(name, extension) or devicons.get_icon(name, vim.vim.bo.ft)
-local out = icon .. " " .. name
-vim.notify("👾 out: " .. vim.inspect(out))
+
+-- DOCS https://github.com/nvim-treesitter/nvim-treesitter#adding-queries
+
+local query = vim.treesitter.query.parse("comment", "(uri) @string.special.url")
+
+local parser = vim.treesitter.get_parser(0)
+vim.notify("👾 parser: " .. vim.inspect(parser))
+
+
+
+
+vim.treesitter.get_parser(0):for_each_tree(function(tstree, tree)
+	local root = tstree:root()
+	local iter = query:iter_captures(root, 0)
+
+	for _, node, _ in iter do
+		local text = vim.treesitter.get_node_text(node, 0)
+		vim.notify("👾 text: " .. vim.inspect(text))
+	end
+end)
