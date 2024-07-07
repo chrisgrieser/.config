ce261c43f9e77bc1630d92466bf303e6b0936e3b
🤖 automated <cron@job>
2024-07-07 14:47:14 +0200

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,10 +1,10 @@
 
--- local path = "/Users/chrisgrieser/.config/nvim/snippets/basic/lua.json"
--- require("snippets.utils").reload_file(path)
-
+local path = "/Users/chrisgrieser/.config/nvim/snippets/basic/lua.json"
+require("snippets.utils").reload_file(path)
 
 --------------------------------------------------------------------------------
 
-local snip = Snippets.loaded_snippets
+local snip = Snippets.loaded_snippets["read file"]
 vim.notify("👾 snip: " .. vim.inspect(snip))
+
 
