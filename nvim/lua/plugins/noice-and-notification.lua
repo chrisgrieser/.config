3a21c243671f6f494092f1e0f273dc2f63ceb830
🤖 automated <cron@job>
2024-07-08 17:17:12 +0200

    iMac Home (2)

--- nvim/lua/plugins/noice-and-notification.lua
+++ nvim/lua/plugins/noice-and-notification.lua
@@ -127,12 +127,7 @@ return {
 				popup = {
 					border = { style = vim.g.borderStyle },
 					size = { width = 90, height = 25 },
-					win_options = {
-						scrolloff = 8,
-						wrap = true,
-						foldenable = true,
-						concealcursor = "ncv",
-					},
+					win_options = { scrolloff = 8, wrap = true, concealcursor = "ncv" },
 					close = { keys = { "q", "<D-w>", "<D-9>", "<D-0>" } },
 					format = { "{message}" }
 				},
