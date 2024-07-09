7d38f0126239dde976ceefe5ca29c318542d51e6
🤖 automated <cron@job>
2024-07-09 10:47:11 +0200

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,3 +1,6 @@
-
-local b = "aa" + "fff"
+local hunk = "@@ -123,1 +999,3 @@"
+local _, lnum, size1, size2 = hunk:match("@@ %-(%d+),?(%d*) %+(%d+),?(%d*) @@")
+vim.notify("👾 size2: " .. tostring(size2))
+vim.notify("👾 size1: " .. tostring(size1))
+vim.notify("👾 lnum: " .. tostring(lnum))
 
