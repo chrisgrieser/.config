6003cf07b2d3177b29e6e61f77b23c8309ff162a
🤖 automated <cron@job>
2024-07-10 17:19:11 +0200

    mini Office (1)

--- nvim/after/ftplugin/javascript.lua
+++ nvim/after/ftplugin/javascript.lua
@@ -1,15 +1,36 @@
+local u = require("config.utils")
+--------------------------------------------------------------------------------
+
 vim.bo.commentstring = "// %s" -- add space
 
 --------------------------------------------------------------------------------
 
 -- ABBREVIATIONS
-local abbr = require("config.utils").bufAbbrev
 
-abbr("cosnt", "const")
-abbr("local", "const")
-abbr("--", "//")
-abbr("~=", "!==")
-abbr("elseif", "else if")
-abbr("()", "() =>") -- quicker arrow function
+u.abbr("cosnt", "const")
+u.abbr("local", "const")
+u.abbr("--", "//")
+u.abbr("~=", "!==")
+u.abbr("elseif", "else if")
+u.abbr("()", "() =>") -- quicker arrow function
 
 --------------------------------------------------------------------------------
+
+---open the next regex at https://regex101.com/
+u.bufKeymap("n", "g/", function()
+	vim.cmd.TSTextobjectSelect("@regex.outer")
+	u.normal('"zy')
+
+	local regex, flags = vim.fn.getreg("z"):match("/(.*)/(%l*)")
+	local data = {
+		regex = regex,
+		flags = flags,
+		substitution = vim.api.nvim_get_current_line():match('%.replace ?%(/.*/.*, ?"(.-)"'),
+		delimiter = "/",
+		flavor = "javascript",
+		testString = "",
+	}
+
+	vim.cmd.TSTextobjectSelect("@regex.inner") -- reselect for easier pasting
+	require("rip-substitute.open-at-regex101").open(data)
+end, { desc = " Open in regex101" })
