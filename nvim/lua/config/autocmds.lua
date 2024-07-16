ca154c60561e7bc85a9200792b8ff4e4a12b1f83
🤖 automated <cron@job>
2024-07-16 19:41:12 +0200

    iMac Home (4)

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -98,6 +98,28 @@ vim.api.nvim_create_autocmd("FocusGained", {
 
 --------------------------------------------------------------------------------
 
+---@param mode? "set"|"clear"
+local function searchCountIndicator(mode)
+	local countNs = vim.api.nvim_create_namespace("searchCounter")
+	vim.api.nvim_buf_clear_namespace(0, countNs, 0, -1)
+	if mode == "clear" then return end
+
+	local signColumnPlusScrollbarWidth = 2 + 3 -- CONFIG
+	local row = vim.api.nvim_win_get_cursor(0)[1]
+	local count = vim.fn.searchcount()
+	if count.total == 0 then return end
+	local text = (" %s/%s "):format(count.current, count.total)
+	local line = vim.api.nvim_get_current_line():gsub("\t", (" "):rep(vim.bo.shiftwidth)) -- ffff
+	local lineFull = #line + signColumnPlusScrollbarWidth >= vim.api.nvim_win_get_width(0)
+	local margin = { (" "):rep(lineFull and signColumnPlusScrollbarWidth or 0), "None" }
+
+	vim.api.nvim_buf_set_extmark(0, countNs, row - 1, 0, {
+		virt_text = { { text, "IncSearch" }, margin },
+		virt_text_pos = lineFull and "right_align" or "eol",
+		priority = 200, -- so it comes in front of lsp-endhints
+	})
+end
+
 -- AUTO-NOHL & INLINE SEARCH COUNT
 vim.on_key(function(char)
 	local key = vim.fn.keytrans(char)
@@ -111,32 +133,13 @@ vim.on_key(function(char)
 	-- works for RHS, therefore no need to consider remaps
 	local searchMovement = vim.tbl_contains({ "n", "N", "*", "#" }, key)
 
-	local countNs = vim.api.nvim_create_namespace("searchCounter")
-	vim.api.nvim_buf_clear_namespace(0, countNs, 0, -1)
+	searchCountIndicator("clear")
 
 	if (searchCancelled or not searchMovement) and not searchConfirmed then
 		vim.opt.hlsearch = false
 	elseif searchMovement or searchConfirmed or searchStarted then
 		vim.opt.hlsearch = true
-
-		-- CONFIG
-		local signColumnPlusScrollbarWidth = 2 + 3
-
-		vim.defer_fn(function()
-			local row = vim.api.nvim_win_get_cursor(0)[1]
-			local count = vim.fn.searchcount()
-			if count.total == 0 then return end
-			local text = (" %s/%s "):format(count.current, count.total)
-			local line = vim.api.nvim_get_current_line():gsub("\t", (" "):rep(vim.bo.shiftwidth)) -- ffff
-			local lineFull = #line + signColumnPlusScrollbarWidth >= vim.api.nvim_win_get_width(0)
-			local margin = { (" "):rep(lineFull and signColumnPlusScrollbarWidth or 0), "None" }
-
-			vim.api.nvim_buf_set_extmark(0, countNs, row - 1, 0, {
-				virt_text = { { text, "IncSearch" }, margin },
-				virt_text_pos = lineFull and "right_align" or "eol",
-				priority = 200, -- so it comes in front of lsp-endhints
-			})
-		end, 1)
+		vim.defer_fn(searchCountIndicator, 1)
 	end
 end, vim.api.nvim_create_namespace("autoNohlAndSearchCount"))
 
@@ -243,8 +246,8 @@ vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
 			{},
 			vim.schedule_wrap(function(out)
 				local noConflicts = out.code == 0
-				local noGitRepo = vim.startswith(out.stdout, "warning: Not a git repository")
-				if noConflicts or noGitRepo then return end
+				local notGitRepo = vim.startswith(out.stdout, "warning: Not a git repository")
+				if noConflicts or notGitRepo then return end
 
 				local ns = vim.api.nvim_create_namespace("conflictMarkers")
 				local firstConflict
