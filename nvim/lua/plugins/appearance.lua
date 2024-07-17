bf2b204f66be18a809d89702c467b0b25365fdd6
🤖 automated <cron@job>
2024-07-17 19:11:56 +0200

    iMac Mother (8)

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -52,24 +52,7 @@ return {
 				fat_headlines = false,
 				bullets = false,
 			},
-			yaml = { codeblock_highlight = "CodeBlock" },
 		},
-		config = function(_, opts)
-			-- add background to injections, see `ftplugin/yaml/injections.scm`
-			-- related BUG https://github.com/lukas-reineke/headlines.nvim/issues/85
-			opts.yaml.query = vim.treesitter.query.parse(
-				"yaml",
-				[[
-					(block_mapping_pair
-						key: (flow_node) @_run
-						(#any-of? @_run "run" "shell_command" "cmd")
-						value: (block_node
-							(block_scalar) @codeblock
-							(#offset! @codeblock 1 1 1 0)))
-				]]
-			)
-			require("headlines").setup(opts)
-		end,
 	},
 	{ -- color previews & color picker
 		"uga-rosa/ccc.nvim",
