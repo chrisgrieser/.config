955107f004fdab151f86a45c32d5dc1bf055d394
🤖 automated <cron@job>
2024-07-19 11:23:41 +0200

    iMac Mother (107)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,7 +1,5 @@
-local ns = vim.api.nvim_create_namespace("test")
-vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
-local lnum = 2
-
-vim.api.nvim_buf_set_extmark(0, ns, lnum, 0, {
-	line_hl_group = "DiffAdd",
-})
+local function hello()
+	
+	local ft = vim.bo.filetype
+	local other_ft = vim.bo.filetype
+end
