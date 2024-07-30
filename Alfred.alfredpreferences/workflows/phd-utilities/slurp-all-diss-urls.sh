6aa627fe6be7c004d3c5c4aa8fccd663667c1715
🤖 automated <cron@job>
2024-07-30 12:19:46 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/phd-utilities/slurp-all-diss-urls.sh
+++ Alfred.alfredpreferences/workflows/phd-utilities/slurp-all-diss-urls.sh
@@ -2,4 +2,26 @@
 
 # CONFIG
 list_name="Diss URLs"
-reminders show "$list_name" --format=json 
+
+#───────────────────────────────────────────────────────────────────────────────
+set -e
+
+# start Obsidian
+open -a "Obsidian"
+sleep 1
+
+# Download
+urls=$(reminders show "$list_name" --format=json | yq '.[].title' --input-format=json)
+echo "$urls" | xargs -I {} open "obsidian://slurp?vault=phd-data-analysis&url={}"
+echo "$urls" | pbcopy
+
+# show inbox
+open "$HOME/phd-data-analysis/Data/_ inbox"
+open "$HOME/phd-data-analysis/Data/articles"
+
+# complete
+url_count=$(echo "$urls" | wc -l)
+
+for ((i = 0; i <= ; i++)); do
+   
+done
