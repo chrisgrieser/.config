e92f717d6a7d358b74702d83840ebfa8e44ad5bf
🤖 automated <cron@job>
2024-07-30 12:49:46 +0200

    iMac Home (5)

--- Alfred.alfredpreferences/workflows/phd-utilities/slurp-all-diss-urls.sh
+++ Alfred.alfredpreferences/workflows/phd-utilities/slurp-all-diss-urls.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env zsh
-
-# CONFIG
-list_name="Diss URLs"
-
-#───────────────────────────────────────────────────────────────────────────────
+# shellcheck disable=2154 # Alfred vars
 set -e
 
-# start Obsidian
-open -a "Obsidian"
-sleep 1
+#───────────────────────────────────────────────────────────────────────────────
+
+# start Obsidian (to ensure all URIs are accepted)
+if ! pgrep -xq "Obsidian"; then
+	open -a "Obsidian"
+	sleep 1
+fi
 
 # Download
-urls=$(reminders show "$list_name" --format=json | yq '.[].title' --input-format=json)
-echo "$urls" | xargs -I {} open "obsidian://slurp?vault=phd-data-analysis&url={}"
+urls=$(reminders show "$slurp_list" --format=json | yq '.[].title' --input-format=json)
+echo "$urls" | xargs -I {} open "obsidian://slurp?vault=${slurp_vault}&url={}"
 echo "$urls" | pbcopy
 
 # show inbox
-open "$HOME/phd-data-analysis/Data/_ inbox"
 open "$HOME/phd-data-analysis/Data/articles"
+open "$HOME/phd-data-analysis/Data/_ inbox"
 
-# complete
+# complete all (backwards, as indexes are shifted)
 url_count=$(echo "$urls" | wc -l)
-
-for ((i = 0; i <= ; i++)); do
-   
+for ((i = url_count - 1; i >= 0; i--)); do
+	reminders complete "$slurp_list" $i
 done
