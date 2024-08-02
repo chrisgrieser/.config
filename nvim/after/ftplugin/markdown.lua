129a58a4599ab9405265e17a5dbf1a3b5f47d4a1
🤖 automated <cron@job>
2024-08-02 21:42:34 +0200

    iMac Home (4)

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,4 @@
-local keymap = vim.keymap.set
+local keymap = require("config.utils").bufKeymap
 local optl = vim.opt_local
 --------------------------------------------------------------------------------
 
@@ -98,46 +98,39 @@ end, { desc = " Preview", buffer = true })
 -- HEADINGS
 
 -- Jump to next/prev heading
-keymap("n", "<C-j>", [[/^#\+ .*<CR>]], { desc = " Next Heading", buffer = true, silent = true })
-keymap("n", "<C-k>", [[?^#\+ .*<CR>]], { desc = " Prev Heading", buffer = true, silent = true })
+keymap("n", "<C-j>", [[/^#\+ .*<CR>]], { desc = " Next Heading" })
+keymap("n", "<C-k>", [[?^#\+ .*<CR>]], { desc = " Prev Heading" })
 
 --------------------------------------------------------------------------------
 -- MARKDOWN-SPECIFIC KEYMAPS
 
-keymap("n", "<leader>x", "mzI- [ ] <Esc>`z", { desc = " Add Task", buffer = true })
+keymap("n", "<leader>x", "mzI- [ ] <Esc>`z", { desc = " Add Task" })
 
 -- Format Table
-keymap(
-	"n",
-	"<leader>ft",
-	"vip:!pandoc --to=gfm<CR>",
-	{ desc = " Format Table under Cursor", buffer = true, silent = true }
-)
+keymap("n", "<leader>ft", "vip:!pandoc --to=gfm<CR>", { desc = " Format Table under Cursor" })
 
 -- convert md image to html image
 keymap("n", "<leader>fi", function()
 	local line = vim.api.nvim_get_current_line()
 	local htmlImage = line:gsub("!%[(.-)%]%((.-)%)", '<img src="%2" alt="%1" width=70%%>')
 	vim.api.nvim_set_current_line(htmlImage)
-end, { desc = " MD image to <img>", buffer = true })
+end, { desc = " MD image to <img>" })
 
 --------------------------------------------------------------------------------
 -- GUI KEYBINDINGS
 
 -- cmd+u: markdown bullet
-keymap("n", "<D-u>", "mzI- <Esc>`z", { desc = "• Bullet List", buffer = true })
+keymap("n", "<D-u>", "mzI- <Esc>`z", { desc = "• Bullet List" })
 
 -- cmd+k: markdown link
-keymap("n", "<D-k>", "bi[<Esc>ea]()<Esc>hp", { desc = " Link", buffer = true })
-keymap("x", "<D-k>", "<Esc>`<i[<Esc>`>la]()<Esc>hp", { desc = " Link", buffer = true })
-keymap("i", "<D-k>", "[]()<Left><Left><Left>", { desc = " Link", buffer = true })
+keymap("n", "<D-k>", "bi[<Esc>ea]()<Esc>hp", { desc = " Link" })
+keymap("x", "<D-k>", "<Esc>`<i[<Esc>`>la]()<Esc>hp", { desc = " Link" })
+keymap("i", "<D-k>", "[]()<Left><Left><Left>", { desc = " Link" })
 
 -- cmd+b: bold
-keymap("n", "<D-b>", "bi**<Esc>ea**<Esc>", { desc = " Bold", buffer = true })
-keymap("x", "<D-b>", "<Esc>`<i**<Esc>`>lla**<Esc>", { desc = " Bold", buffer = true })
-keymap("i", "<D-b>", "****<Left><Left>", { desc = " Bold", buffer = true })
+keymap("n", "<D-b>", "bi**<Esc>ea**<Esc>", { desc = " Bold" })
+keymap("i", "<D-b>", "****<Left><Left>", { desc = " Bold" })
 
 -- cmd+i: italics
-keymap("n", "<D-i>", "bi*<Esc>ea*<Esc>", { desc = " Italics", buffer = true })
-keymap("x", "<D-i>", "<Esc>`<i*<Esc>`>la*<Esc>", { desc = " Italics", buffer = true })
-keymap("i", "<D-i>", "**<Left>", { desc = " Italics", buffer = true })
+keymap("n", "<D-i>", "bi*<Esc>ea*<Esc>", { desc = " Italics" })
+keymap("i", "<D-i>", "**<Left>", { desc = " Italics" })
