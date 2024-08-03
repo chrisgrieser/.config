9a7fdf0b850782d3a23624b35935531db64d9a12
🤖 automated <cron@job>
2024-08-03 12:42:33 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/finder-utilities/scripts/recently-changed-in-home.js
+++ Alfred.alfredpreferences/workflows/finder-utilities/scripts/recently-changed-in-home.js
@@ -4,32 +4,44 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 //──────────────────────────────────────────────────────────────────────────────
 
+/** @param {string} str */
+function alfredMatcher(str) {
+	const clean = str.replace(/[-_().:#;,[\]'"]/g, " ");
+	return [clean, str].join(" ") + " ";
+}
+
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
 	// CONFIG
 	const folderToSearch = app.pathTo("home folder");
 	const maxItems = 10;
+	// INFO `fd` does not allow to sort results by recency, thus using `rg` instead
 	const rgCmd =
 		"rg --no-config --files --sortr=modified --glob='!/Library/' --glob='!*.photoslibrary'";
 
-	/** @type AlfredItem[] */
+	/** @type {AlfredItem[]} */
 	const recentlyChanged = app
-		.doShellScript(`cd "${folderToSearch}" && ${rgCmd}`)
+		.doShellScript(`cd '${folderToSearch}' && ${rgCmd}`)
 		.split("\r")
 		.slice(0, maxItems)
 		.map((relPath) => {
 			const [_, parent, name] = relPath.match(/(.*\/)(.*\/?)/) || [];
+			const absPath = folderToSearch + "/" + relPath;
 
 			/** @type {AlfredItem} */
 			const item = {
 				title: name,
 				subtitle: parent.slice(0, -1),
-				arg: folderToSearch + "/" + relPath,
+				arg: absPath,
 				type: "file:skipcheck",
+				match: alfredMatcher(name),
+				icon: { type: "fileicon", path: absPath },
 			};
 			return item;
 		});
-	console.log("🖨️ recentlyChanged:", JSON.stringify(recentlyChanged));
 
 	return JSON.stringify({ items: recentlyChanged });
 }
