f052d9cf0345b211f62fa90322ec148bcb91a9cd
🤖 automated <cron@job>
2024-08-08 12:13:13 +0200

    mini Office (4)

--- nvim/lua/plugins/editing-support.lua
+++ nvim/lua/plugins/editing-support.lua
@@ -139,52 +139,6 @@ return {
 			{ "<", ">", ft = { "lua" }, cond = function(fn) return fn.in_string() end },
 		},
 	},
-	{ -- better `:substitute`
-		"chrisgrieser/nvim-rip-substitute",
-		cmd = "RipSubstitute",
-		keys = {
-			{
-				"<leader>fs",
-				function() require("rip-substitute").sub() end,
-				mode = { "n", "x" },
-				desc = " substitute",
-			},
-			{
-				"<leader>fc",
-				function() require("rip-substitute").rememberCursorWord() end,
-				mode = { "n", "x" },
-				desc = " remember cword",
-			},
-		},
-		opts = {
-			popupWin = {
-				border = vim.g.borderStyle,
-				hideSearchReplaceLabels = true,
-			},
-			prefill = {
-				startInReplaceLineIfPrefill = true,
-			},
-			keymaps = {
-				insertModeConfirm = "<CR>",
-			},
-			editingBehavior = {
-				autoCaptureGroups = true,
-			},
-		},
-	},
-	{ -- refactoring utilities
-		"ThePrimeagen/refactoring.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
-		opts = { show_success_message = true },
-		keys = {
-			-- stylua: ignore start
-			{ "<leader>fi", function() require("refactoring").refactor("Inline Variable") end, mode = { "n", "x" }, desc = "󱗘 Inline Var" },
-			{ "<leader>fe", function() require("refactoring").refactor("Extract Variable") end, mode = "x", desc = "󱗘 Extract Var" },
-			{ "<leader>fu", function() require("refactoring").refactor("Extract Function") end, mode = "x", desc = "󱗘 Extract Func" },
-			{ "<leader>fU", function() require("refactoring").refactor("Extract Function To File") end, mode = "x", desc = "󱗘 Extract Func to File" },
-			-- stylua: ignore end
-		},
-	},
 	{ -- substitute & duplicate operator
 		"echasnovski/mini.operators",
 		keys = {
@@ -392,7 +346,7 @@ return {
 			require("treesj").setup(opts)
 		end,
 	},
-	{
+	{ -- quick adding log statements
 		"chrisgrieser/nvim-chainsaw",
 		init = function() vim.g.whichkeyAddGroup("<leader>l", "󰐪 Log") end,
 		opts = {
