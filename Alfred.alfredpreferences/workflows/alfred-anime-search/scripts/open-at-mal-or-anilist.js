e74fb14aba3675a12b04ec8d832dbf92aa8944e9
🤖 automated <cron@job>
2024-08-09 11:51:22 +0200

    iMac Home (4)

---
+++ Alfred.alfredpreferences/workflows/alfred-anime-search/scripts/open-at-mal-or-anilist.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @type {AlfredRun} */
+// biome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run(argv) {
+	const malId = argv[0];
+}
