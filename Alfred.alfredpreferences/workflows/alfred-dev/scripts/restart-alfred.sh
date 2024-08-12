47e18125866feb7e03967ac90aa5f14cfe534967
🤖 automated <cron@job>
2024-08-12 14:47:46 +0200

    iMac Home (6)

--- Alfred.alfredpreferences/workflows/alfred-dev/scripts/restart-alfred.sh
+++ Alfred.alfredpreferences/workflows/alfred-dev/scripts/restart-alfred.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env zsh
 # INFO this script cannot be called directly from Alfred, since it would
 # terminate itself. It must be called as a disowned process.
+#───────────────────────────────────────────────────────────────────────────────
+
 osascript -e 'display notification "" with title "Restarting Alfred…"'
+
+# Restart Alfred
 killall -9 "Alfred"
 while pgrep -xq "Alfred"; do sleep 0.1; done
 sleep 0.1
 open -a "Alfred 5"
 osascript -e 'tell application id "com.runningwithcrayons.Alfred" to search'
+
+# Restart Alfred Preferences (if open)
+if pgrep -xq "Alfred Preferences"; then
+	killall -9 "Alfred Preferences"
+	while pgrep -xq "AlfredPreferences"; do sleep 0.1; done
+	sleep 0.1
+	open -a "Alfred Preferences"
+fi
