6088281fde789f0b909f6ebf0ddab8ef15cd3586
🤖 automated <cron@job>
2024-08-17 09:28:34 +0200

    iMac Home (8)

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -321,7 +321,6 @@ keymap({ "n", "x" }, "x", '"_x')
 keymap({ "n", "x" }, "c", '"_c')
 keymap("n", "C", '"_C')
 keymap("x", "p", "P", { desc = " Paste w/o switching with register" })
-keymap("n", "P", "mzA <Esc>p`z", { desc = " Sticky Paste at EoL" })
 
 keymap("n", "dd", function()
 	if vim.api.nvim_get_current_line():find("^%s*$") then return '"_dd' end
