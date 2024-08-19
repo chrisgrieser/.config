60e0545a0ff4d97e46dc7abd492e22244334b40f
🤖 automated <cron@job>
2024-08-19 11:55:06 +0200

    iMac Home (2)

--- .sync-this-repo.sh
+++ .sync-this-repo.sh
@@ -6,15 +6,9 @@ cd "$(dirname "$0")" || exit 1 # go to location of this script, i.e. going into 
 
 # ADD & COMMIT
 files_changed="$(git status --porcelain | wc -l | tr -d ' ')"
-if [[ $files_changed -eq 0 ]]; then
-	echo "No changes."
-	exit 0
-fi
-
 device_name=$(scutil --get ComputerName | cut -d" " -f2-)
-commit_msg="$device_name ($files_changed)"
-git add --all
-git commit --message="$commit_msg" --author="🤖 automated<cron@job>"
+git add --all &&
+	git commit --message="$device_name ($files_changed)" --author="🤖 automated<cron@job>"
 
 #───────────────────────────────────────────────────────────────────────────────
 # PULL & PUSH
@@ -22,7 +16,7 @@ git commit --message="$commit_msg" --author="🤖 automated<cron@job>"
 # loop git add-commit-pull-push, since when between add and push files have been
 # changed, the push will fail
 i=0
-sleep 2 # prevent "Cannot rebase on multiple branches"
+sleep 1 # prevent "Cannot rebase on multiple branches"
 while true; do
 	git pull --no-progress && git push --no-progress && exit 0
 	sleep 2
