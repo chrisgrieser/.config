77a0f53dcb2893d1654b0ff3715cbae39f89020f
🤖 automated <cron@job>
2024-08-21 16:09:04 +0200

    iMac Home (4)

--- obsidian/vimrc/obsidian-vimrc.vim
+++ obsidian/vimrc/obsidian-vimrc.vim
@@ -343,16 +343,6 @@ nnoremap ,ph :openDynamicHighlightsSettings
 exmap cycleColorscheme jsfile Meta/vimrc-jsfile.js { cycleColorscheme() }
 nnoremap ,pc :cycleColorscheme
 
-" Open this vimrc file
-exmap openVimrc jscommand { view.app.openWithDefaultApp("Meta/obsidian-vimrc.vim"); }
-nnoremap g, :openVimrc
-
-" Workspace
-exmap loadWorkspace jsfile Meta/vimrc-jsfile.js { workspace("load", "Basic") }
-nnoremap ,w :loadWorkspace
-exmap saveWorkspace jsfile Meta/vimrc-jsfile.js { workspace("save", "Basic") }
-nnoremap ,W :saveWorkspace
-
 "───────────────────────────────────────────────────────────────────────────────
 " VISUAL MODE
 
--- obsidian/vimrc/vimrc-jsfile.js
+++ obsidian/vimrc/vimrc-jsfile.js
@@ -131,18 +131,6 @@ function openDynamicHighlightsSettings() {
 	}, 100);
 }
 
-/** @param {"load"|"save"} mode @param {string} workspaceName */
-async function workspace(mode, workspaceName) {
-	const workspacePlugin = view.app.internalPlugins.plugins.workspaces;
-	await workspacePlugin.enable();
-
-	if (mode === "load") workspacePlugin.instance.loadWorkspace(workspaceName);
-	else if (mode === "save") workspacePlugin.instance.saveWorkspace(workspaceName);
-
-	new Notice(`${mode === "load" ? "Loaded" : "Saved"} workspace "${workspaceName}".`);
-	setTimeout(() => workspacePlugin.disable(), 3000);
-}
-
 //──────────────────────────────────────────────────────────────────────────────
 
 /** @param {"next"|"prev"} which */
