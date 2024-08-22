ea120f75fa041af29ac83594e923cfba8e069206
🤖 automated <cron@job>
2024-08-22 20:36:59 +0200

    iMac Home (3)

---
+++ .bootstrap/1a_perma-repos.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+cut -d, -f2 "$HOME/.config/perma-repos.csv" |
+	grep -vE ".config|.password-store" | # already cloned in previous step
+	sed "s|^~|$HOME|" |
+	while read -r repo; do
+		location="$(dirname "$repo")"
+		mkdir -p "$location"
+		cd "$location" || return 1
+		git clone --depth=1 git@github.com:chrisgrieser/"$(basename "$repo")"
+		echo
+	done
