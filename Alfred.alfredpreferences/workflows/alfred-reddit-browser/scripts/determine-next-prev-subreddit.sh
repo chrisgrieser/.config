cbf744c145881cc962a60b81bb4daab5869bbee2
🤖 automated <cron@job>
2024-08-22 14:39:52 +0200

    mini Office (10)

--- Alfred.alfredpreferences/workflows/alfred-reddit-browser/scripts/determine-next-prev-subreddit.sh
+++ Alfred.alfredpreferences/workflows/alfred-reddit-browser/scripts/determine-next-prev-subreddit.sh
@@ -3,10 +3,15 @@
 
 direction="$1"
 cur_subreddit=$(cat "$alfred_workflow_cache/current_subreddit")
-list_of_subreddits=${subreddits//r\//}
+list_of_subreddits=$(echo "$subreddits" |
+	sed -E 's|^/?r/||') # can be r/ or /r/ https://www.alfredforum.com/topic/20813-reddit-browser/page/2/#comment-114645
+
+#───────────────────────────────────────────────────────────────────────────────
 
 if [[ "$direction" == "next" ]]; then
-	next_subreddit=$(echo "$list_of_subreddits" | grep -A1 "$cur_subreddit" | tail -n1)
+	next_subreddit=$(echo "$list_of_subreddits" |
+		grep --after-context=1 --extended-regexp "^$cur_subreddit$" |
+		tail -n1)
 
 	# if already last subreddit, go back to first subreddit
 	if [[ "$next_subreddit" == "$cur_subreddit" && "$add_hackernews" == "0" ]]; then
@@ -14,7 +19,9 @@ if [[ "$direction" == "next" ]]; then
 	fi
 
 elif [[ "$direction" == "prev" ]]; then
-	next_subreddit=$(echo "$list_of_subreddits" | grep -B1 "$cur_subreddit" | head -n1)
+	next_subreddit=$(echo "$list_of_subreddits" |
+		grep --before-context=1 --extended-regexp "^$cur_subreddit$" |
+		head -n1)
 
 	# if already first subreddit, go back to last subreddit
 	if [[ "$next_subreddit" == "$cur_subreddit" && "$add_hackernews" == "0" ]]; then
@@ -22,9 +29,12 @@ elif [[ "$direction" == "prev" ]]; then
 	fi
 fi
 
+# in case user has only one subreddit & hackernews enabled
 if [[ "$next_subreddit" == "$cur_subreddit" && "$add_hackernews" == "1" ]]; then
 	next_subreddit="hackernews"
 fi
 
+#───────────────────────────────────────────────────────────────────────────────
+
 # save for Alfred-loop
-echo -n "$next_subreddit" >"$alfred_workflow_cache/current_subreddit"
+echo -n "$next_subreddit" > "$alfred_workflow_cache/current_subreddit"
