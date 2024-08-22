f3eff0df49aa8073812b5dfa2e0e277386256d2c
🤖 automated <cron@job>
2024-08-22 17:39:52 +0200

    mini Office (7)

--- nvim/debug/repro.lua
+++ nvim/debug/repro.lua
@@ -1,8 +1,25 @@
+-- INFO run via: `nvim -u minimal-config.lua -- foobar.js`
+--------------------------------------------------------------------------------
 local spec = {
 	{
 		"uga-rosa/ccc.nvim",
 		opts = {
-			highlighter = { auto_enable = true },
+			highlighter = {
+				auto_enable = true,
+				lsp = false,
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function() require("lspconfig").cssls.setup {} end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim", opts = true },
+		opts = {
+			ensure_installed = { "css-lsp" },
+			run_on_start = true,
 		},
 	},
 }
@@ -10,3 +27,4 @@ local spec = {
 vim.env.LAZY_STDPATH = "/tmp/nvim-repro"
 load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
 require("lazy.minit").repro { spec = spec }
+
