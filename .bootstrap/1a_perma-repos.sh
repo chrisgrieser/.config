dbee18986c6ea6d0483da933ea2573b84bdccb68
🤖 automated <cron@job>
2024-08-24 22:15:31 +0200

    iMac Home (7)

--- .bootstrap/1a_perma-repos.sh
+++ .bootstrap/1a_perma-repos.sh
@@ -10,3 +10,6 @@ cut -d, -f2 "$HOME/.config/perma-repos.csv" |
 		git clone --depth=1 git@github.com:chrisgrieser/"$(basename "$repo")"
 		echo
 	done
+
+# contains info on all the vaults, so they do not have to be added manually
+cp -f ./obsidian.json "$HOME/Library/Application Support/obsidian/obsidian.json"
