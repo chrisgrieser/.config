2f9fc77a85081766c7338ff21b7ec20d46a99f69
🤖 automated <cron@job>
2024-08-24 20:15:31 +0200

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/obsidian-utils/scripts/vault-search.js
+++ Alfred.alfredpreferences/workflows/obsidian-utils/scripts/vault-search.js
@@ -15,7 +15,8 @@ function alfredMatcher(str) {
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	const shellCmd = `mdfind -onlyin "${$.getenv("vault_path")}" "*"`;
+	const vaultPath = $.getenv("vault_path");
+	const shellCmd = `mdfind -onlyin "${vaultPath}" -name ".md"`;
 
 	const items = app
 		.doShellScript(shellCmd)
@@ -23,13 +24,14 @@ function run() {
 		.map((absPath) => {
 			const parts = absPath.split("/");
 			const name = parts.pop() || "";
-			const parent = parts.join("/");
+			const parent = parts.join("/").slice(vaultPath.length + 1);
 			const obsidianUri = "obsidian://open?path=" + encodeURIComponent(absPath);
 
 			return {
 				title: name,
 				subtitle: "▸ " + parent,
 				arg: absPath,
+				uid: absPath,
 				variables: { uri: obsidianUri },
 				quicklookurl: absPath,
 				type: "file:skipcheck",
@@ -38,5 +40,8 @@ function run() {
 			};
 		});
 
-	return JSON.stringify({ items: items });
+	return JSON.stringify({
+		items: items,
+		cache: { seconds: 600, loosereload: true },
+	});
 }
