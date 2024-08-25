1e831758246bb8f558b0f24d6dd3d498b8a3b597
🤖 automated <cron@job>
2024-08-25 13:48:03 +0200

    iMac Home (2)

--- obsidian/startup-actions/main.js
+++ obsidian/startup-actions/main.js
@@ -2,7 +2,6 @@
 const obsidian = require("obsidian");
 //──────────────────────────────────────────────────────────────────────────────
 
-// CONFIG
 const config = {
 	opacity: {
 		light: 0.92,
@@ -12,20 +11,39 @@ const config = {
 
 //──────────────────────────────────────────────────────────────────────────────
 
+function setOpacity() {
+	const isDarkMode = document.querySelector("body.theme-dark");
+	const opacityValue = config.opacity[isDarkMode ? "dark" : "light"];
+	electronWindow.setOpacity(opacityValue);
+}
+
+class ExampleModal extends obsidian.FuzzySuggestModal {
+	getItems() {
+		return [
+			{ name: "Item 1", description: "Item 1 description" },
+			{ name: "Item 2", description: "Item 2 description" },
+		];
+	}
+
+	getItemText(plugin) {
+		return plugin.name;
+	}
+
+	onChooseItem(plugin, _event) {
+		new Notice(`Selected ${plugin.name}`);
+	}
+}
+
 class StartupActionsPlugin extends obsidian.Plugin {
 	onload() {
 		console.log(this.manifest.name + " loaded.");
+		const app = this.app;
 
 		// OPACITY, depending on dark/light mode
-		this.registerEvent(
-			this.app.workspace.on("css-change", () => {
-				const isDarkMode = document.querySelector("body.theme-dark");
-				const opacityValue = config.opacity[isDarkMode ? "dark" : "light"];
-				electronWindow.setOpacity(opacityValue);
-			}),
-		);
+		setOpacity();
+		this.registerEvent(this.app.workspace.on("css-change", () => setOpacity()));
 
-		// Plugin reload URI
+		// URI to reload a plugin
 		this.registerObsidianProtocolHandler("reload-plugin", async (uriParams) => {
 			const pluginId = uriParams?.id;
 			if (pluginId) {
@@ -33,6 +51,14 @@ class StartupActionsPlugin extends obsidian.Plugin {
 				await app.plugins.enablePlugin(pluginId);
 				new Notice(`Reloaded ${pluginId}.`);
 			}
+		});
+
+		this.addCommand({
+			id: "example-modal",
+			name: "Open Example Modal",
+			callback: () => {
+				new ExampleModal(app).open();
+			},
 		});
 	}
 }
