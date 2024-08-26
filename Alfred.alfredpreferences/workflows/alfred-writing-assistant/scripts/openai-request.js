d7b920a5469574a5eb57de80fa5720c1f209b146
🤖 automated <cron@job>
2024-08-26 14:45:49 +0200

    mini Office (1)

--- Alfred.alfredpreferences/workflows/alfred-writing-assistant/scripts/openai-request.js
+++ Alfred.alfredpreferences/workflows/alfred-writing-assistant/scripts/openai-request.js
@@ -4,27 +4,40 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 //──────────────────────────────────────────────────────────────────────────────
 
+/** @param {string} filepath @param {string} text */
+function writeToFile(filepath, text) {
+	const str = $.NSString.alloc.initWithUTF8String(text);
+	str.writeToFileAtomicallyEncodingError(filepath, true, $.NSUTF8StringEncoding, null);
+}
+
+//──────────────────────────────────────────────────────────────────────────────
+
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
 	const selection = argv[0]?.trim();
 	if (!selection) return "ERROR: No selection.";
+	const dataCache = $.getenv("alfre");
+
 	const apiKey =
 		$.NSProcessInfo.processInfo.environment.objectForKey("alfred_apikey").js ||
 		app.doShellScript('source "$HOME/.zshenv"; echo "$OPENAI_API_KEY"');
-	const prompt = $.getenv("static_prompt") + selection;
-	const temp = Number.parseInt($.getenv("temperature")) / 10;
+	const prompt = $.getenv("static_prompt") + " " + selection;
+	const temperature = Number.parseInt($.getenv("temperature")) / 10;
 
 	const data = {
 		model: $.getenv("openai_model"),
 		messages: [{ role: "user", content: prompt }],
-		temperature: temp,
+		temperature: temperature,
 	};
+	// write to file as send request via `--data-binary` to avoid more escaping issues
+	writeToFile(dataCache, JSON.stringify(data));
+
 	const response = app.doShellScript(
 		`curl --max-time 15 https://api.openai.com/v1/chat/completions \
 		-H 'Content-Type: application/json' \
 		-H 'Authorization: Bearer ${apiKey}' \
-		-d '${JSON.stringify(data)}' `,
+		--data-binary @'${dataCache}' `,
 	);
 	if (!response) return "ERROR: No response from OpenAI API.";
 	const obj = JSON.parse(response);
