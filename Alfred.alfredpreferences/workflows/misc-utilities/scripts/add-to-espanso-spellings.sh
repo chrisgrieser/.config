ed9f6988ad353817783c13d27b715b0649049609
🤖 automated <cron@job>
2024-08-27 17:48:01 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/misc-utilities/scripts/add-to-espanso-spellings.sh
+++ Alfred.alfredpreferences/workflows/misc-utilities/scripts/add-to-espanso-spellings.sh
@@ -6,11 +6,8 @@ spell_config="$HOME/.config/espanso/match/spelling.yml"
 
 wrong=$(echo "$*" | cut -d" " -f1)
 correct=$(echo "$*" | cut -d" " -f2)
-new_line="  - { replace: $correct, trigger: $wrong, propagate_case: true }"
+new_line="  - { trigger: $wrong, replace: $correct, propagate_case: true }"
 echo "$new_line" >>"$spell_config"
 
 # shellcheck disable=2154
-if [[ "$open" == "true" ]]; then
-	line_no=$(wc -l "$spell_config" | tr -s " " | cut -d" " -f2)
-	open "$spell_config" --env=LINE="$line_no"
-fi
+[[ "$open" == "true" ]] && open "$spell_config"
