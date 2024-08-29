bd9c15b9bbf5d903fbd98f8fca9d308d78ac0b48
🤖 automated <cron@job>
2024-08-29 14:13:31 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/alfred-pass/scripts/clear-clipboard.sh
+++ Alfred.alfredpreferences/workflows/alfred-pass/scripts/clear-clipboard.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env zsh
 # INFO password clearing is implemented via this script rather than using pass'
 # built-in functionality because the built-in functionality requires using `pass
-# --clip` which saves the password to in a non-transiently, meaning it is
-# readable in Alfred's clipboard history. Not using `pass --clip` however means
-# that the automatic password clearance after after PASSWORD_STORE_CLIP_TIME
-# seconds does not take place and therefore needs to be implemented manually
-# here.
+# --clip` which saves the password a non-transiently, meaning it is readable in
+# Alfred's clipboard history. Not using `pass --clip` however means that the
+# automatic password clearance after after PASSWORD_STORE_CLIP_TIME seconds does
+# not take place and therefore needs to be implemented manually here.
 
 #───────────────────────────────────────────────────────────────────────────────
 
-# read from .zshenv, default value 45s
-delay=${PASSWORD_STORE_CLIP_TIME-45}
-pw="$*"
+password="$*"
 
+delay=${PASSWORD_STORE_CLIP_TIME-45} # read from .zshenv, default value 45s
 sleep "$delay"
 
-# clear the clipboard only if it is currently in the clipboard
-if [[ "$pw" == "$(pbpaste)" ]]; then
+# clear the clipboard only if it is currently in the clipboard, to not override
+# other clipboard content
+if [[ "$password" == "$(pbpaste)" ]]; then
 	pbcopy < /dev/null
 	echo -n "cleared" # triggers Alfred notification
 fi
