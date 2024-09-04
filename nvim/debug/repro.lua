46efb46da035bedf0c6b669d108633b5b077ff87
🤖 automated <cron@job>
2024-09-04 14:20:32 +0200

    iMac Home (3)

--- nvim/debug/repro.lua
+++ nvim/debug/repro.lua
@@ -1,24 +1,6 @@
 -- INFO run via: `nvim -u minimal-config.lua -- foobar.js`
 --------------------------------------------------------------------------------
 local spec = {
-	{
-		"williamboman/mason.nvim",
-		opts = {},
-		dependencies = {
-			{
-				"WhoIsSethDaniel/mason-tool-installer.nvim",
-				opts = {
-					ensure_installed = { "lua_ls" },
-					run_on_start = true,
-				},
-			},
-			"williamboman/mason-lspconfig.nvim",
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function() require("lspconfig").lua_ls.setup {} end,
-	},
 }
 --------------------------------------------------------------------------------
 vim.env.LAZY_STDPATH = "/tmp/nvim-repro"
