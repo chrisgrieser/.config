aaa74802bf7cccb2c2d5f1c7826915a218db0fca
🤖 automated <cron@job>
2024-09-12 15:05:32 +0200

    mini Office (60)

--- obsidian/clipper/utils/routing.js
+++
@@ -1,8 +0,0 @@
-export function updateUrl(section, templateId) {
-    let url = `${window.location.pathname}?section=${section}`;
-    if (templateId) {
-        url += `&template=${templateId}`;
-    }
-    window.history.pushState({}, '', url);
-}
-//# sourceMappingURL=routing.js.map
