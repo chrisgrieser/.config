f97357b51ed716488249a32da36c6d1360481ef7
🤖 automated <cron@job>
2024-09-17 11:30:33 +0200

    iMac Home (3)

--- nvim/lua/plugins/editing-support.lua
+++ nvim/lua/plugins/editing-support.lua
@@ -370,15 +370,16 @@ return {
 		keys = {
 			-- stylua: ignore start
 			{"<leader>ll", function() require("chainsaw").variableLog() end, mode = {"n", "x"}, desc = "󰐪 variable" },
-			{"<leader>lo", function() require("chainsaw").objectLog() end, mode = {"n", "x"}, desc = "󰐪 object" },
+			{"<leader>lo", function() require("chainsaw").objectLog() end, mode = {"n", "x"}, desc = "⬠ object" },
 			{"<leader>la", function() require("chainsaw").assertLog() end, mode = {"n", "x"}, desc = "󰐪 assert" },
-			{"<leader>lt", function() require("chainsaw").typeLog() end, mode = {"n", "x"}, desc = "󰐪 type" },
-			{"<leader>lm", function() require("chainsaw").messageLog() end, desc = "󰐪 message" },
-			{"<leader>lb", function() require("chainsaw").beepLog() end, desc = "󰐪 beep" },
-			{"<leader>l1", function() require("chainsaw").timeLog() end, desc = "󰐪 time" },
-			{"<leader>ld", function() require("chainsaw").debugLog() end, desc = "󰐪 debugger" },
-			{"<leader>ls", function() require("chainsaw").stacktraceLog() end, desc = "󰐪 stacktrace" },
-			{"<leader>lk", function() require("chainsaw").clearLog() end, desc = "󰐪 clear" },
+			{"<leader>lt", function() require("chainsaw").typeLog() end, mode = {"n", "x"}, desc = "⬠ type" },
+			{"<leader>lm", function() require("chainsaw").messageLog() end, desc = "󰍡 message" },
+			{"<leader>lb", function() require("chainsaw").beepLog() end, desc = "󰱨 beep" },
+			{"<leader>lB", function() require("chainsaw").bell() end, desc = "󰂚 bell" },
+			{"<leader>l1", function() require("chainsaw").timeLog() end, desc = "󱎫 time" },
+			{"<leader>ld", function() require("chainsaw").debugLog() end, desc = "󰃤 debugger" },
+			{"<leader>ls", function() require("chainsaw").stacktraceLog() end, desc = " stacktrace" },
+			{"<leader>lk", function() require("chainsaw").clearLog() end, desc = "󰐪 󰅗 clear" },
 
 			{"<leader>lr", function() require("chainsaw").removeLogs() end, desc = "󰐪 󰅗 remove logs" },
 			-- stylua: ignore end
