8a6792c3643f72d508bad606320c4865088f8d17
🤖 automated <cron@job>
2024-09-19 16:19:10 +0200

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,1 +1,20 @@
-vim.system({"osascript", "-e", "beep"}) -- 🖨️
+
+local timelogStart1 = os.clock() -- 🖨️
+print("hi")
+print("hi")
+print("hi")
+
+vim.notify(("#1 🖨️: %.3fs"):format(os.clock() - timelogStart1))
+
+print("hi")
+print("hi")
+
+local timelogStart2 = os.clock() -- 🖨️
+
+print("hi")
+print("hi")
+print("hi")
+print("hi")
+
+
+vim.notify(("#2 🖨️: %.3fs"):format(os.clock() - timelogStart2))
