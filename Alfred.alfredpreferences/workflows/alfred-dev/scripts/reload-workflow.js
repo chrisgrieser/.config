71c0b5d1b0348629ccbc2f63114e8d7c841f6280
🤖 automated <cron@job>
2024-09-22 16:22:23 +0200

    iMac Home (13)

--- Alfred.alfredpreferences/workflows/alfred-dev/scripts/reload-workflow.js
+++ Alfred.alfredpreferences/workflows/alfred-dev/scripts/reload-workflow.js
@@ -9,10 +9,19 @@ app.includeStandardAdditions = true;
 function run(argv) {
 	const workflowPath = argv[0].trim();
 	const workflowUid = workflowPath.split("/").pop();
-	if (!workflowUid) return "Error reloading workflow " + workflowPath;
-	if (workflowUid === $.getenv("alfred_workflow_uid")) return "This workflow cannot reload itself";
 
-	const alfredApp = Application("com.runningwithcrayons.Alfred");
-	alfredApp.reloadWorkflow(workflowUid);
-	return "Reloaded: " + workflowUid; // pass back for notification
+	// GUARD
+	if (!workflowUid) return "⚠️ Error reloading workflow: " + workflowPath;
+	if (workflowUid === $.getenv("alfred_workflow_uid")) {
+		return "⚠️ Alfred Workflow Devtools cannot reload itself.";
+	}
+
+	// CAVEAT cannot delete the cache of a non-active workflow, since we can only
+	// get the uid
+
+	// reload
+	Application("com.runningwithcrayons.Alfred").reloadWorkflow(workflowUid);
+
+	// alfred notification
+	return "Reloaded & deleted cache: " + workflowUid; 
 }
