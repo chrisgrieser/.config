799ebbbf871c1a6efa67431639d374e888ccdd2e
🤖 automated <cron@job>
2024-09-24 14:02:54 +0200

    mini Office (6)

--- Alfred.alfredpreferences/workflows/japanese/scripts/jisho.js
+++ Alfred.alfredpreferences/workflows/japanese/scripts/jisho.js
@@ -13,6 +13,12 @@ function httpRequest(url) {
 
 //──────────────────────────────────────────────────────────────────────────────
 
+// CONFIG
+const readmoreIcon = "(…)";
+const commonSymbol = "🅲"; // ᴄᴏᴍᴍᴏɴ, 🅲 ,🅒
+
+//──────────────────────────────────────────────────────────────────────────────
+
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
@@ -20,7 +26,6 @@ function run(argv) {
 	const displayJlpt = $.getenv("display_jlpt") === "1";
 	const displayWanikani = $.getenv("display_wanikani") === "1";
 	const openAt = $.getenv("open_at");
-	const readmoreIcon = "  (...)";
 
 	// DOCS the API is undocumented, but some info is delivered in this thread:
 	// https://jisho.org/forum/54fefc1f6e73340b1f160000-is-there-any-kind-of-search-api
@@ -34,16 +39,16 @@ function run(argv) {
 	const items = response.data.map((item) => {
 		const { japanese, senses, is_common, jlpt, tags } = item;
 
+		// basic
 		const kanji = japanese[0].word;
 		const kana = japanese[0].reading;
 		const japWord = kanji || kana || "ERROR: Neither kanji nor kana found.";
 		const japDisplay = kanji && kana ? `${kanji} 【${kana}】` : japWord;
 		const engWord = senses.map((sense) => sense.english_definitions[0]).join(", ");
-		const url = openAt + japWord;
-		const readMoreLink = senses.find((sense) => sense.links.length > 0)?.links[0];
 
 		// properties
 		const properties = [];
+		if (is_common) properties.push(commonSymbol + " ");
 		if (jlpt && displayJlpt) {
 			const level = jlpt.map((j) => j.replace("jlpt-", "")).join(" ");
 			properties.push(level);
@@ -53,13 +58,16 @@ function run(argv) {
 			properties.push(level);
 		}
 		const propertiesDisplay = properties.join(" ").toUpperCase();
+
+		// more
+		const url = openAt + japWord;
+		const readMoreLink = senses.find((sense) => sense.links.length > 0)?.links[0];
 		const csvLine = [engWord, kana || "", kanji || ""].join(";");
 
 		/** @type {AlfredItem} */
 		const alfredItem = {
 			title: japDisplay + "   " + propertiesDisplay,
-			icon: is_common ? { path: "./icon-common.png" } : {},
-			subtitle: engWord + (readMoreLink ? " " + readmoreIcon : ""),
+			subtitle: engWord + (readMoreLink ? "  " + readmoreIcon : ""),
 			arg: url,
 			quicklookurl: url,
 			mods: {
