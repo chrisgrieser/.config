db0f76cc7e11bca3db8029ab3542e2c5af4f0195
🤖 automated <cron@job>
2024-09-26 13:59:31 +0200

    iMac Home (2)

--- zsh/config/python_pip.zsh
+++ zsh/config/python_pip.zsh
@@ -16,6 +16,7 @@ function pip {
 alias pu="pip uninstall"
 alias pi="pip install"
 alias pl="pip list --not-required"
+alias py="python3"
 alias v="toggle_venv"
 
 #───────────────────────────────────────────────────────────────────────────────
