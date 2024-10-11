7dbcab528dfaf1ee70168c0425cef0cb5e216a14
🤖 automated <cron@job>
2024-10-11 19:10:26 +0200

    iMac Home (8)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pandoc-md2pdf.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pandoc-md2pdf.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env zsh
-#───────────────────────────────────────────────────────────────────────────────
-# INFO pandoc's --data-dir defined in .zshenv
-#───────────────────────────────────────────────────────────────────────────────
 
 md_file="$*"
 if [[ ! "$md_file" =~ .*\.md ]]; then
 	echo "⚠️ Not a markdown file"
 	return 1
 fi
+pdf_file="${md_file%\.md}_CG.pdf"
 
-pdf_file="${md_file%.*}_CG.pdf"
-pandoc "$md_file" --output="$pdf_file" --defaults="md2pdf" 2>&1 &&
-	open -R "$pdf_file"
+# so `--resource-path` is set
+cd "$(dirname "$md_file")" || return 1
+
+# INFO pandoc's --data-dir for the `defaults` file defined in .zshenv
+pandoc "$md_file" --output="$pdf_file" --defaults="md2pdf" 2>&1 || return 1
+open -R "$pdf_file"
+
+#───────────────────────────────────────────────────────────────────────────────
