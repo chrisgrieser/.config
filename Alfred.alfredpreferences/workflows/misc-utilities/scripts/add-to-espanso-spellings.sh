e7c4b9f2a25e1020d55278b89d1c0e5fbfd6b3a0
🤖 automated <cron@job>
2024-10-15 19:13:14 +0200

    mini Office (2)

--- Alfred.alfredpreferences/workflows/misc-utilities/scripts/add-to-espanso-spellings.sh
+++ Alfred.alfredpreferences/workflows/misc-utilities/scripts/add-to-espanso-spellings.sh
@@ -6,7 +6,7 @@ spell_config="$HOME/.config/espanso/match/spelling.yml"
 
 wrong=$(echo "$*" | cut -d" " -f1)
 correct=$(echo "$*" | cut -d" " -f2)
-new_line="  - { trigger: $wrong, replace: $correct, propagate_case: true }"
+new_line="  - { trigger: $wrong, replace: $correct, propagate_case: true, word: true }"
 echo "$new_line" >>"$spell_config"
 
 # shellcheck disable=2154
