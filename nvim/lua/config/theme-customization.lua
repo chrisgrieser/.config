a245b1136b5eb4296231dff6b335961a0eef2fa1
🤖 automated <cron@job>
2024-10-16 20:52:54 +0200

    iMac Home (5)

--- nvim/lua/config/theme-customization.lua
+++ nvim/lua/config/theme-customization.lua
@@ -92,7 +92,6 @@ function M.themeModifications()
 		setHl("Italic", { italic = true })
 		-- broken when switching themes
 		setHl("TelescopeSelection", { link = "Visual" })
-		setHl("BlinkCmpKind", { link = "Special" })
 	elseif theme == "bluloco" then
 		setHl("@keyword.return", { fg = "#d42781", bold = true })
 		revertedTodoComments()
