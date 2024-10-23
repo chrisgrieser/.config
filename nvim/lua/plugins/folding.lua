02f60b67c2a1c865ccdb05ea56c899914fd1c003
🤖 automated <cron@job>
2024-10-23 13:35:18 +0200

    mini Office (1)

--- nvim/lua/plugins/folding.lua
+++ nvim/lua/plugins/folding.lua
@@ -9,18 +9,18 @@ return {
 		dependencies = "kevinhwang91/promise-async",
 		event = "UIEnter", -- needed for folds to load in time and comments being closed
 		keys = {
-			-- stylua: ignore start
 			{ "zm", function() require("ufo").closeAllFolds() end, desc = "󱃄 Close All Folds" },
-			{ "zr", function()
-				require("ufo").openFoldsExceptKinds { "comment", "imports" }
-				vim.opt.scrolloff = vim.g.baseScrolloff -- fix scrolloff setting sometimes being off
-			end, desc = "󱃄 Open All Regular Folds" },
-			{ "zR", function() require("ufo").openFoldsExceptKinds {} end, desc = "󱃄 Open All Folds" },
+			{
+				"zr",
+				function()
+					require("ufo").openFoldsExceptKinds { "comment", "imports" }
+					vim.opt.scrolloff = vim.g.baseScrolloff -- fix scrolloff setting sometimes being off
+				end,
+				desc = "󱃄 Open Regular Folds",
+			},
 			{ "z1", function() require("ufo").closeFoldsWith(1) end, desc = "󱃄 Close L1 Folds" },
 			{ "z2", function() require("ufo").closeFoldsWith(2) end, desc = "󱃄 Close L2 Folds" },
 			{ "z3", function() require("ufo").closeFoldsWith(3) end, desc = "󱃄 Close L3 Folds" },
-			{ "z4", function() require("ufo").closeFoldsWith(4) end, desc = "󱃄 Close L4 Folds" },
-			-- stylua: ignore end
 		},
 		init = function()
 			-- INFO fold commands usually change the foldlevel, which fixes folds, e.g.
@@ -32,20 +32,17 @@ return {
 			vim.opt.foldlevelstart = 99
 		end,
 		opts = {
+			-- when opening the buffer, close these fold kinds
+			-- use `:UfoInspect` to get available fold kinds from the LSP
+			close_fold_kinds_for_ft = { default = { "imports", "comment" } },
+			open_fold_hl_timeout = 800,
+			-- INFO some filetypes only allow indent, some only LSP, some only treesitter.
+			-- ufo only accepts two kinds as priority, see https://github.com/kevinhwang91/nvim-ufo/issues/256
 			provider_selector = function(_, ft, _)
-				-- INFO some filetypes only allow indent, some only LSP, some only
-				-- treesitter. However, ufo only accepts two kinds as priority,
-				-- therefore making this function necessary :/
 				local lspWithOutFolding = { "markdown", "zsh", "css", "html", "python", "json" }
 				if vim.tbl_contains(lspWithOutFolding, ft) then return { "treesitter", "indent" } end
 				return { "lsp", "indent" }
 			end,
-			-- when opening the buffer, close these fold kinds
-			-- use `:UfoInspect` to get available fold kinds from the LSP
-			close_fold_kinds_for_ft = {
-				default = { "imports", "comment" },
-			},
-			open_fold_hl_timeout = 800,
 			-- show folds with number of folded lines instead of just the icon
 			fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
 				local hlgroup = "NonText"
