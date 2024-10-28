c444f3614fa2d1d9e255a3e3efa0b001560a2e92
🤖 automated <cron@job>
2024-10-28 19:09:55 +0100

    mini Office (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,6 +1,2 @@
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-require("lspconfig").css_ls.setup({
-	capabilities = capabilities
-})
-
+-- GUARD 
+require('blink.cmp').sources.reload()
