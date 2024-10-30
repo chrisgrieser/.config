149936613c2675d4066eea81d1d67a1acf7d7358
🤖 automated <cron@job>
2024-10-30 18:54:45 +0100

    iMac Home (7)

--- sketchybar/network.sh
+++ sketchybar/network.sh
@@ -1,20 +1,37 @@
 #!/usr/bin/env zsh
 
-# - INFO `upload` would be $6
-# - HACK `netstat` only streams stdout, so using `awk`'s `exit` to return 1st value
-download_kb=$(netstat -w1 | awk '/[0-9]/ {print int($3/1024) ; exit }')
-unit="k"
+# CONFIG
+threshold_kb=50
 
-# only show when more than 50kb
-if [[ $download_kb -lt 50 ]]; then
+#───────────────────────────────────────────────────────────────────────────────
+
+# HACK `netstat` only streams stdout, so using exiting `awk` to return 1st value
+network=$(netstat -w1 | awk '/[0-9]/ {print int($3/1024) "," int($6/1024) ; exit }')
+download_kb=$(echo "$network" | cut -d',' -f1)
+upload_kb=$(echo "$network" | cut -d',' -f2)
+
+download=$download_kb
+upload=$upload_kb
+download_unit="k"
+upload_unit="k"
+
+# GUARD only show when above threshold
+if [[ $download -lt $threshold_kb && $upload -lt $threshold_kb ]]; then
 	sketchybar --set "$NAME" drawing=false
 	return 0
 fi
 
-# switch to Mb when more than 1024kb
-if [[ $download_kb -gt 1024 ]]; then
-	download_kb=$(echo "scale = 1; $download_kb / 1024" | bc)
-	unit="M"
+# Formatting
+if [[ $download -gt 1024 ]]; then
+	download=$(echo "scale = 1; $download / 1024" | bc)
+	download_unit="M"
 fi
+if [[ $upload -gt 1024 ]]; then
+	upload=$(echo "scale = 1; $upload / 1024" | bc)
+	upload_unit="M"
+fi
+[[ $download_kb -ge $threshold_kb ]] && download_display="⏷${download}${download_unit}"
+[[ $upload_kb -ge $threshold_kb ]] && upload_display="⏶${upload}${upload_unit}"
 
-sketchybar --set "$NAME" label="${download_kb}${unit}" drawing=true
+# Display
+sketchybar --set "$NAME" label="${download_display}  ${upload_display}" drawing=true
