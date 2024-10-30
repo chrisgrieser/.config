833733d93d733ac6894e65e887a4978ca766888a
🤖 automated <cron@job>
2024-10-30 16:34:44 +0100

    iMac Home (3)

--- sketchybar/cpu.sh
+++ sketchybar/cpu.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
 cpu_usage=$(top -F -n0 -l1 -s0 | grep "^CPU" | awk '{ print int($3 + $5) }')
+cpu_fraction=$(echo "scale = 2; $cpu_usage / 100" | bc)
 
-sketchybar --set "$NAME" label="$cpu_usage%"
+
+
+sketchybar --set "$NAME" label="$cpu_usage%" \
+	--push "$NAME" "$cpu_fraction"
--- sketchybar/network.sh
+++ sketchybar/network.sh
@@ -4,5 +4,18 @@
 # - HACK `netstat` only allows streaming output, we use `awk`'s `exit` to
 # - return the first value.
 download_kb=$(netstat -w1 | awk '/[0-9]/ {print int($3/1024) ; exit }')
+unit="k"
 
-sketchybar --set "$NAME" label="${download_kb}󰬒"
+# only show when more than 10kb
+if [[ $download_kb -lt 10 ]]; then
+	sketchybar --set "$NAME" drawing=false
+	return 0
+fi
+
+# switch to Mb when more than 1024kb
+if [[ $download_kb -gt 1024 ]]; then
+	download_kb=$((download_kb / 1024))
+	unit="M"
+fi
+
+sketchybar --set "$NAME" label="${download_kb}${unit}" drawing=true
