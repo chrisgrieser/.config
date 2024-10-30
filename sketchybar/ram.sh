85eb18faa9b8f4c1c4fb636c8f1ea8fa1b0a6d91
🤖 automated <cron@job>
2024-10-30 17:04:44 +0100

    iMac Home (7)

--- sketchybar/ram.sh
+++ sketchybar/ram.sh
@@ -3,4 +3,4 @@
 memory_free=$(memory_pressure | tail -n1 | grep --only-matching '[0-9.]*')
 memory_usage=$((100 - memory_free))
 
-sketchybar --set "$NAME" label="$memory_usage%"
+sketchybar --set "$NAME" label="$memory_usage%" drawing=true
