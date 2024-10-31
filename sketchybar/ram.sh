7555ed22b80ee20eace9df8854eede72ce582762
🤖 automated <cron@job>
2024-10-31 14:52:25 +0100

    mini Office (2)

--- sketchybar/ram.sh
+++ sketchybar/ram.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env zsh
 
+# CONFIG
+icons=("󰪞" "󰪟" "󰪠" "󰪡" "󰪢" "󰪣" "󰪤" "󰪥")
+
+#───────────────────────────────────────────────────────────────────────────────
+
 memory_free=$(memory_pressure | tail -n1 | grep --only-matching '[0-9.]*')
 memory_usage=$((100 - memory_free))
+idx=$((${#icons} * memory_usage / 100))
 
-sketchybar --set "$NAME" label="$memory_usage%" drawing=true
+sketchybar --set "$NAME" icon="${icons[$idx]}" drawing=true
