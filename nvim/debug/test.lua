d2370f4d7687107f827d45bbe2f0e8026c86b67c
🤖 automated <cron@job>
2024-11-03 14:40:15 +0100

    iMac Home (5)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,2 +1,8 @@
 -- local out = require("lspconfig").util.available_servers()
-vim.notify("🖨️ out: " .. vim.inspect(out))
+
+local allOps = {}
+vim.list_extend(allOps, vim.tbl_keys(require("genghis.operations.file")))
+vim.list_extend(allOps, vim.tbl_keys(require("genghis.operations.copy")))
+vim.list_extend(allOps, vim.tbl_keys(require("genghis.operations.other")))
+
+vim.notify("🖨️ allOps: " .. vim.inspect(allOps))
