33847a8d81bff590a7b147d000a61f4905aab4e9
🤖 automated <cron@job>
2024-11-04 17:01:02 +0100

    iMac Home (3)

--- sketchybar/cpu.sh
+++ sketchybar/cpu.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+# CPU values from `top` more reliable than from `ps` https://stackoverflow.com/questions/30855440/how-to-get-cpu-utilization-in-in-terminal-mac
 cpu_usage=$(top -F -n0 -l1 -s0 | grep "^CPU" | awk '{ print int($3 + $5) }')
 cpu_fraction=$(echo "scale = 2; $cpu_usage / 100" | bc)
 
