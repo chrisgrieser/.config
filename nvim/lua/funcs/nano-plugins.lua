fdadda6d69f97f2fc8f3790fb8a1654b64fdf575
🤖 automated <cron@job>
2024-11-06 14:08:17 +0100

    iMac Home (2)

--- nvim/lua/funcs/nano-plugins.lua
+++ nvim/lua/funcs/nano-plugins.lua
@@ -193,7 +193,7 @@ function M.gotoMostChangedFile()
 		return
 	end
 
-	-- identify most changed file
+	-- identify file with most changes
 	local targetFile
 	local mostChanges = 0
 	vim.iter(changedFiles):each(function(line)
@@ -219,35 +219,7 @@ function M.gotoMostChangedFile()
 end
 --------------------------------------------------------------------------------
 
----@alias Position { character: integer, line: integer }
-
----@param refs Position[]
 ---@param dir? "next"|"prev" default: "next"
----@return Position
-local function findNextRef(refs, dir)
-	local curLine, curCol = unpack(vim.api.nvim_win_get_cursor(0))
-	curLine = curLine - 1 -- vim is 1-indexed
-
-	table.sort(refs, function(a, b) return a.line < b.line end)
-	local iter = dir == "prev" and vim.iter(refs):rev() or vim.iter(refs)
-
-	local nextRef = iter
-		:filter(function(ref)
-			if dir == "prev" then
-				return ref.line < curLine or (ref.line == curLine and ref.character < curCol)
-			else
-				return ref.line > curLine or (ref.line == curLine and ref.character > curCol)
-			end
-		end)
-		:nth(1)
-
-	-- if no reference found, loop around
-	if not nextRef then nextRef = dir == "prev" and refs[#refs] or refs[1] end
-	return nextRef
-end
-
----@param dir? "next"|"prev" default: "next"
----@return boolean|nil noReferenceFound for error
 function M.nextReference(dir)
 	local params = vim.lsp.util.make_position_params()
 
@@ -255,39 +227,40 @@ function M.nextReference(dir)
 	-- opposed to `textDocument/references` which searches the entire workspace.
 	-- Since we jump only in the current file, the former is enough.
 	vim.lsp.buf_request(0, "textDocument/documentHighlight", params, function(err, refs, _, _)
+		-- GUARD
 		if err then
-			vim.notify("LSP Error: " .. err.message, vim.log.levels.ERROR, { title = "LSP Jump" })
-			return nil
+			vim.notify("LSP Error: " .. err.message, vim.log.levels.ERROR, { title = "LSP Reference" })
+			return
 		end
-		if not refs or vim.tbl_isempty(refs) then return false end
+		if not refs or vim.tbl_isempty(refs) then
+			vim.notify("No references found.", nil, { title = "LSP Reference" })
+			return
+		end
 
-		-- we only need the start position
-		refs = vim.tbl_map(function(ref) return ref.range.start end, refs) ---@cast refs Position[]
+		local curLine, curCol = unpack(vim.api.nvim_win_get_cursor(0))
+		curLine = curLine - 1 -- vim is 1-indexed
+
+		-- prepare refs
+		refs = vim.tbl_map(function(ref) return ref.range.start end, refs)
 		table.sort(refs, function(a, b) return a.line < b.line end)
+		local refsIter = dir == "prev" and vim.iter(refs):rev() or vim.iter(refs)
 
+		-- get next reference
+		local nextRef = refsIter
+			:filter(function(ref)
+				if dir == "prev" then
+					return ref.line < curLine or (ref.line == curLine and ref.character < curCol)
+				else
+					return ref.line > curLine or (ref.line == curLine and ref.character > curCol)
+				end
+			end)
+			:nth(1)
+		-- if not found, loop around
+		if not nextRef then nextRef = dir == "prev" and refs[#refs] or refs[1] end
 
-	local curLine, curCol = unpack(vim.api.nvim_win_get_cursor(0))
-	curLine = curLine - 1 -- vim is 1-indexed
-
-	local iter = dir == "prev" and vim.iter(refs):rev() or vim.iter(refs)
-
-	local nextRef = iter
-		:filter(function(ref)
-			if dir == "prev" then
-				return ref.line < curLine or (ref.line == curLine and ref.character < curCol)
-			else
-				return ref.line > curLine or (ref.line == curLine and ref.character > curCol)
-			end
-		end)
-		:nth(1)
-
-	-- if no reference found, loop around
-	if not nextRef then nextRef = dir == "prev" and refs[#refs] or refs[1] end
-
-		local nextRef = findNextRef(refs, dir)
+		-- jump
 		vim.api.nvim_win_set_cursor(0, { nextRef.line + 1, nextRef.character })
-		vim.cmd.normal { "zv", bang = true } -- open folds if inside a fold
-		return true
+		vim.cmd.normal { "zv", bang = true } -- open folds
 	end)
 end
 
