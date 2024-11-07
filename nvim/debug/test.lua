5be86f0d16eb65c9179ebdb2538dfbdbd562a0fe
🤖 automated <cron@job>
2024-11-07 15:04:05 +0100

    mini Office (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,8 +1,3 @@
 -- local out = require("lspconfig").util.available_servers()
 
-local allOps = {}
-vim.list_extend(allOps, vim.tbl_keys(require("genghis.operations.file")))
-vim.list_extend(allOps, vim.tbl_keys(require("genghis.operations.copy")))
-vim.list_extend(allOps, vim.tbl_keys(require("genghis.operations.other")))
-
-vim.notify("🖨️ allOps: " .. vim.inspect(allOps))
+vim.notify("fff", vim.log.levels.WARN, {})
