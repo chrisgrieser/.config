9d51cd40393dacc6e21816df8f8c4e77ef8974ab
🤖 automated <cron@job>
2024-11-12 19:38:16 +0100

    mini Office (2)

--- nvim/lua/plugins/noice-and-notification.lua
+++ nvim/lua/plugins/noice-and-notification.lua
@@ -96,7 +96,7 @@ return {
 				notify_jump = true,
 				modes = { "n" },
 			},
-
+			-- DOCS https://github.com/folke/snacks.nvim/blob/main/docs/notifier.md#️-config
 			styles = {
 				notification = {
 					wo = { wrap = true, winblend = 0 },
