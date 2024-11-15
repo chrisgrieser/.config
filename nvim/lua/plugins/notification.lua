a9cd66131d2043d3c23291e544b39f670c3c482d
🤖 automated <cron@job>
2024-11-15 18:44:14 +0100

    iMac Home (4)

--- nvim/lua/plugins/notification.lua
+++ nvim/lua/plugins/notification.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("FileType", {
 	desc = "User: Highlight filepaths and error codes in noice/snacks notifications.",
-	pattern = { "noice", "snacks_notif", "snacks_win" },
+	pattern = { "noice", "snacks_notif" },
 	callback = function(ctx)
 		vim.defer_fn(function()
 			vim.api.nvim_buf_call(ctx.buf, function()
@@ -14,81 +14,95 @@ vim.api.nvim_create_autocmd("FileType", {
 --------------------------------------------------------------------------------
 
 return {
-	"folke/snacks.nvim",
-	event = "VeryLazy",
-	keys = {
+	{ -- Message & Command System Overhaul
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = "MunifTanjim/nui.nvim",
+		opts = {
+			notify = { merge = true },
+			lsp = {
+				progress = { enabled = false }, -- using my own statusline component instead
+				signature = { enabled = false }, -- using `lsp_signature.nvim` instead
+			},
+		},
+	},
+	{
+		"folke/snacks.nvim",
+		event = "VeryLazy",
+		keys = {
 		-- stylua: ignore start
 		{ "<Esc>", function() require("snacks").notifier.hide() end, desc = "󰎟 Dismiss notifications" },
 		{ "ö", function() require("snacks").words.jump(1, true) end, desc = "󰒕 Next reference" },
 		{ "Ö", function() require("snacks").words.jump(-1, true) end, desc = "󰒕 Prev reference" },
-		-- stylua: ignore end
-		{
-			desc = "󰎟 Notification history",
-			"<D-0>",
-			function()
-				local lines = {}
-				vim
-					.iter(require("snacks").notifier.get_history())
-					:rev() -- last notification on top
-					:each(function(notif)
-						local msg = vim.split(notif.msg, "\n")
-						local icon = notif.level ~= "info" and notif.icon .. " " or ""
-						msg[1] = "[" .. icon .. notif.title .. "] " .. msg[1]
-						vim.list_extend(lines, msg)
-					end)
-				local bufnr = vim.api.nvim_create_buf(false, true)
-				vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-				require("snacks").win {
-					position = "bottom",
-					ft = "markdown",
-					buf = bufnr,
-					height = 0.5,
-				}
-			end,
+			-- stylua: ignore end
+			{
+				desc = "󰎟 Notification history",
+				"<D-0>",
+				function()
+					local lines = {}
+					vim
+						.iter(require("snacks").notifier.get_history())
+						:rev() -- last notification on top
+						:each(function(notif)
+							local msg = vim.split(notif.msg, "\n")
+							local icon = notif.level ~= "info" and notif.icon .. " " or ""
+							msg[1] = "[" .. icon .. notif.title .. "] " .. msg[1]
+							vim.list_extend(lines, msg)
+						end)
+					local bufnr = vim.api.nvim_create_buf(false, true)
+					vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+					require("snacks").win {
+						position = "bottom",
+						ft = "markdown",
+						buf = bufnr,
+						height = 0.5,
+					}
+				end,
+			},
+			{
+				desc = "󰎟 Last notification",
+				"<D-9>",
+				function()
+					local history = require("snacks").notifier.get_history()
+					local last = history[#history]
+					if not last then return end
+					local bufnr = vim.api.nvim_create_buf(false, true)
+					vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, vim.split(last.msg, "\n"))
+					local title = vim.trim((last.icon or "") .. " " .. (last.title or ""))
+					require("snacks").win {
+						position = "float",
+						ft = "markdown",
+						buf = bufnr,
+						height = 0.75,
+						width = 0.75,
+						title = vim.trim(title) ~= "" and " " .. title .. " " or nil,
+					}
+				end,
+			},
 		},
-		{
-			desc = "󰎟 Last notification",
-			"<D-9>",
-			function()
-				local history = require("snacks").notifier.get_history()
-				local last = history[#history]
-				if not last then return end
-				local bufnr = vim.api.nvim_create_buf(false, true)
-				vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, vim.split(last.msg, "\n"))
-				local title = vim.trim((last.icon or "") .. " " .. (last.title or ""))
-				require("snacks").win {
-					position = "float",
-					ft = "markdown",
-					buf = bufnr,
-					height = 0.75,
-					width = 0.75,
-					title = vim.trim(title) ~= "" and title or nil,
-				}
-			end,
-		},
-	},
-	opts = {
-		words = {
-			notify_jump = true,
-			modes = { "n" },
-			debounce = 300,
-		},
-		-- DOCS https://github.com/folke/snacks.nvim/blob/main/docs/notifier.md#%EF%B8%8F-config
-		notifier = {
-			timeout = 6000,
-			width = { min = 20, max = 0.5 },
-			height = { min = 1, max = 0.4 },
-			icons = { error = "", warn = "", info = "", debug = "", trace = "󰓘" },
-			top_down = false,
-		},
-		win = {
-			keys = { q = "close", ["<Esc>"] = "close", ["<D-0>"] = "close", ["<D-9>"] = "close" },
-			border = vim.g.borderStyle,
-		},
-		styles = {
-			notification = {
+		opts = {
+			words = {
+				notify_jump = true,
+				modes = { "n" },
+				debounce = 300,
+			},
+			-- DOCS https://github.com/folke/snacks.nvim/blob/main/docs/notifier.md#%EF%B8%8F-config
+			notifier = {
+				timeout = 6000,
+				width = { min = 20, max = 0.5 },
+				height = { min = 1, max = 0.4 },
+				icons = { error = "", warn = "", info = "", debug = "", trace = "󰓘" },
+				top_down = false,
+			},
+			win = {
+				keys = { q = "close", ["<Esc>"] = "close", ["<D-0>"] = "close", ["<D-9>"] = "close" },
 				border = vim.g.borderStyle,
-				wo = { wrap = true, winblend = 0 },
+			},
+			styles = {
+				notification = {
+					border = vim.g.borderStyle,
+					wo = { wrap = true, winblend = 0 },
+				},
 			},
 		},
 	},
