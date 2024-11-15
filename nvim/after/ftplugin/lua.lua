0ce0700a5540151acfd1f939d87c675b2e255990
🤖 automated <cron@job>
2024-11-15 15:44:14 +0100

    iMac Home (7)

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -12,48 +12,8 @@ abbr("=~", "~=") -- shell uses `=~`
 abbr("===", "==")
 
 --------------------------------------------------------------------------------
--- REQUIRE MODULE FROM CWD
-
--- lightweight version of telescope-import.nvim import (just for lua)
-bkeymap("n", "<leader>ci", function()
-	local regex = [[local (\w+) = require\(["'](.*?)["']\)(\.[\w.]*)?]]
-	local rgArgs = { "rg", "--no-config", "--only-matching", "--no-filename", regex }
-	local rgResult = vim.system(rgArgs):wait()
-	assert(rgResult.code == 0, rgResult.stderr)
-	local matches = vim.split(rgResult.stdout, "\n", { trimempty = true })
-	table.sort(matches)
-	local uniqMatches = vim.fn.uniq(matches)
-	local isAtBlank = vim.api.nvim_get_current_line():match("^%s*$")
-
-	vim.api.nvim_create_autocmd("FileType", {
-		desc = "User (buffer-specific): Set filetype to `lua` for `TelescopeResults`",
-		pattern = "TelescopeResults",
-		once = true,
-		callback = function(ctx)
-			vim.api.nvim_set_option_value("filetype", "lua", { buf = ctx.buf })
-			-- make discernible as the results are now colored
-			local ns = vim.api.nvim_create_namespace("telescope-import")
-			vim.api.nvim_win_set_hl_ns(0, ns)
-			vim.api.nvim_set_hl(ns, "TelescopeMatching", { reverse = true })
-		end,
-	})
-
-	vim.ui.select(uniqMatches, { prompt = " require" }, function(selection)
-		if not selection then return end
-		if isAtBlank then
-			vim.api.nvim_set_current_line(selection)
-			vim.cmd.normal { "==", bang = true }
-		else
-			local lnum = vim.api.nvim_win_get_cursor(0)[1]
-			vim.api.nvim_buf_set_lines(0, lnum, lnum, false, { selection })
-			vim.cmd.normal { "j==", bang = true }
-		end
-	end)
-end, { desc = " Import module" })
-
---------------------------------------------------------------------------------
-
-bkeymap("n", "<leader>rf", function()
+-- turn regular string into formatted/template string
+bkeymap("n", "<leader>ft", function()
 	local node = vim.treesitter.get_node()
 	if not node then return end
 	local strNode
@@ -73,13 +33,13 @@ bkeymap("n", "<leader>rf", function()
 
 	vim.api.nvim_win_set_cursor(0, { row + 1, endCol + 10 })
 	vim.cmd.startinsert()
-end, { desc = " Formatted String" })
+end, { desc = " Template String" })
 
 --------------------------------------------------------------------------------
 
 -- auto-comma for tables
 vim.api.nvim_create_autocmd("TextChangedI", {
-	desc = "User (buffer-specific): Set filetype to lua for TelescopeResults",
+	desc = "User (buffer-specific): auto-comma for tables",
 	buffer = 0,
 	group = vim.api.nvim_create_augroup("lua-autocomma", { clear = true }),
 	callback = function()
@@ -90,3 +50,52 @@ vim.api.nvim_create_autocmd("TextChangedI", {
 		end
 	end,
 })
+
+--------------------------------------------------------------------------------
+-- REQUIRE MODULE FROM CWD
+
+-- lightweight version of telescope-import.nvim import (just for lua)
+-- REQUIRED `ripgrep`
+bkeymap("n", "<leader>ci", function()
+	local isAtBlank = vim.api.nvim_get_current_line():match("^%s*$")
+
+	local regex = [[local (\w+) = require\(["'](.*?)["']\)(\.[\w.]*)?]]
+	local rgArgs = { "rg", "--no-config", "--only-matching", "--no-filename", regex }
+	local rgResult = vim.system(rgArgs):wait()
+	assert(rgResult.code == 0, rgResult.stderr)
+	local matches = vim.split(rgResult.stdout, "\n", { trimempty = true })
+
+	table.sort(matches)
+	local uniqMatches = vim.fn.uniq(matches) ---@cast uniqMatches string[]
+	table.sort(uniqMatches, function(a, b)
+		local varnameA = a:match("local (%S+) ?=")
+		local varnameB = b:match("local (%S+) ?=")
+		return #varnameA < #varnameB
+	end)
+
+	vim.api.nvim_create_autocmd("FileType", {
+		desc = "User (buffer-specific): Set filetype to `lua` for `TelescopeResults`",
+		pattern = "TelescopeResults",
+		once = true,
+		callback = function(ctx)
+			vim.bo[ctx.buf].filetype = "lua"
+			-- make discernible as the results are now colored
+			local ns = vim.api.nvim_create_namespace("telescope-import")
+			vim.api.nvim_win_set_hl_ns(0, ns)
+			vim.api.nvim_set_hl(ns, "TelescopeMatching", { reverse = true })
+		end,
+	})
+
+	vim.ui.select(uniqMatches, { prompt = " require" }, function(selection)
+		if not selection then return end
+		local lnum = vim.api.nvim_win_get_cursor(0)[1]
+		if isAtBlank then
+			vim.api.nvim_set_current_line(selection)
+			vim.cmd.normal { "==", bang = true }
+		else
+			vim.api.nvim_buf_set_lines(0, lnum, lnum, false, { selection })
+			vim.cmd.normal { "j==", bang = true }
+		end
+	end)
+end, { desc = " Import module" })
+
