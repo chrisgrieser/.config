65d48676bfd7d18fda00f23d659d2fe49a5298f4
🤖 automated <cron@job>
2024-11-15 13:44:14 +0100

    iMac Home (4)

--- nvim/lua/config/leader-keybindings.lua
+++ nvim/lua/config/leader-keybindings.lua
@@ -144,14 +144,6 @@ for _, key in pairs(trailChars) do
 	keymap("n", "<leader>" .. key, ("mzA%s%s<Esc>`z"):format(pad, key))
 end
 
--- JUST
-keymap(
-	"n",
-	"<leader>j",
-	function() require("funcs.justice").just() end,
-	{ desc = " Select just recipe" }
-)
-
 --------------------------------------------------------------------------------
 -- OPTION TOGGLING
 
