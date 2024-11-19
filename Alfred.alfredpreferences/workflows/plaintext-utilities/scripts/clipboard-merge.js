38aa94bc8e61a068bc6df520f7b77b69f4bf187a
🤖 automated <cron@job>
2024-11-19 13:31:08 +0100

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/plaintext-utilities/scripts/clipboard-merge.js
+++ Alfred.alfredpreferences/workflows/plaintext-utilities/scripts/clipboard-merge.js
@@ -4,13 +4,32 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 //──────────────────────────────────────────────────────────────────────────────
 
+/** @type {Record<string, string>} */
+const availableSeparators = {
+	commaWithSpace: ", ",
+	linebreak: "\n",
+	space: " ",
+	semicolon: ";",
+};
+
+//──────────────────────────────────────────────────────────────────────────────
+
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
+	const sep = availableSeparators[$.getenv("separator")];
+
+	/** @type {AlfredItem[]} */
 	const clipbHistory = [];
 	for (let i = 0; i <= 20; i++) {
-		clipbHistory[i] = $.getenv(`cb${i}`);
+		const text = $.getenv(`cb${i}`);
+		const merged = clipbHistory.map((item) => item.title).join(sep) + sep + text;
+
+		clipbHistory[i] = {
+			title: text,
+			subtitle: `${i + 1} items`,
+			arg: merged,
+		};
 	}
 
+	return JSON.stringify({ items: clipbHistory });
 }
-
-scri
