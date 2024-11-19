22d111553a3928a1e90b7d51b1b86444c2251ceb
🤖 automated <cron@job>
2024-11-19 14:31:08 +0100

    iMac Home (7)

--- Alfred.alfredpreferences/workflows/alfred-reminders-companion/scripts/increase-priority.sh
+++ Alfred.alfredpreferences/workflows/alfred-reminders-companion/scripts/increase-priority.sh
@@ -14,19 +14,3 @@ else
 fi
 
 echo -n "$title" # pass for notification
-
-#───────────────────────────────────────────────────────────────────────────────
-# JXA for adding a reminder (not used since slower)
-
-# osascript -l JavaScript -e "
-# 	const tomorrow = new ;
-# 	tomorrow.setDate(tomorrow.getDate() + 1);
-# 	const rem = Application('Reminders');
-# 	const newReminder = rem.Reminder({
-# 		name: '$title',
-# 		body: '$body',
-# 		alldayDueDate: tomorrow,
-# 	});
-# 	rem.lists.byName('$reminder_list').reminders.push(newReminder);
-# 	rem.quit();
-# " &> /dev/null
