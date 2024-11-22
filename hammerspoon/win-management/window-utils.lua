03a7055c7044185cf4fd9c56a2e1ead730d277aa
🤖 automated <cron@job>
2024-11-22 19:42:24 +0100

    iMac Home (3)

--- hammerspoon/win-management/window-utils.lua
+++ hammerspoon/win-management/window-utils.lua
@@ -9,12 +9,11 @@ M.pseudoMax = { x = 0.184, y = 0, w = 0.817, h = 1 }
 M.middleHalf = { x = 0.184, y = 0, w = 0.6, h = 1 }
 
 -- negative x to hide useless sidebar
-M.toTheSide = hs.geometry.rect(-92, 54, 446, 1026)
+M.toTheSide = hs.geometry.rect(-90, 54, 444, 1026)
 if env.isAtMother then M.toTheSide = hs.geometry.rect(-78, 54, 387, 890) end
 if env.isAtOffice then M.toTheSide = hs.geometry.rect(-90, 54, 466, 1100) end
 
 --------------------------------------------------------------------------------
--- WINDOW MOVEMENT
 
 ---@param win hs.window
 ---@param relSize hs.geometry
