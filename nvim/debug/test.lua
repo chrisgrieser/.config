c0805a6ee603f3934e5d1835e57e57f35a983ec5
🤖 automated <cron@job>
2024-11-24 15:49:32 +0100

    iMac Home (1)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,2 +1,18 @@
-local out = os.date("%H:%M:%S")
-print(out)
+local class = {
+	field = nil,
+	new = function(self, o) -- https://www.lua.org/pil/16.1.html
+		o = o or {}
+		setmetatable(o, self)
+		self.__index = self
+		return o
+	end,
+}
+
+local a = Account:new()
+a:withdraw(10)
+a:deposit(212)
+local b = Account:new()
+b:withdraw(10)
+b:deposit(1)
+vim.notify(vim.inspect(b.balance), nil, { title = "🖨️ b.balance", ft = "lua" })
+vim.notify(vim.inspect(a.balance), nil, { title = "🖨️ a.balance", ft = "lua" })
