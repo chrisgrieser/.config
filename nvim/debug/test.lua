29000a2acfbf740974fc4f744cd0facb72d0e75e
🤖 automated <cron@job>
2024-11-26 00:46:39 +0100

    iMac Home (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,4 +1,8 @@
 
 local ns = vim.api.nvim_create_namespace("chainsaw.markers")
 local extmarks = vim.api.nvim_buf_get_extmarks(0, ns, 0, -1, { details = true })
+vim.notify("🖨️ 🔵")
+vim.notify("🖨️ 🟩")
+vim.notify("🖨️ ⭐")
+vim.notify(vim.inspect(extmarks), nil, { title = "🖨️ extmarks", ft = "lua" })
 
