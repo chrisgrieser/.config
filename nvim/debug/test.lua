df8181dec6ad49d1d046ebfde6fe940cb4410678
🤖 automated <cron@job>
2024-11-25 15:23:18 +0100

    iMac Home (4)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,6 +1,7 @@
 
-local aaa = ("foobar %s foo"):format()
+local aaa = ("foo  bar %s foo"):format()
+local aaa = "foo  bar  foo"
 
 if true then
-	local aaa = "foobar  foo"
+	local aaa = ("foobar  foo%s"):format()
 end
