ae8f903aac0cec1b83116807f5156cfcea852f56
🤖 automated <cron@job>
2024-11-27 22:16:36 +0100

    iMac Home (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,3 +1,7 @@
 local a, b = pcall(vim.treesitter.query.parse, "lua", [[((return_statement) @keyword.return)]])
 vim.notify(vim.inspect(a), nil, { title = "🪚 a", ft = "lua" })
 vim.notify(vim.inspect(b), nil, { title = "🪚 b", ft = "lua" })
+
+
+local b = "./"
+
