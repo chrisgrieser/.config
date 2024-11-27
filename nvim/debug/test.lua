3c85e5992495cdae573662ce9185091c9a9435fa
🤖 automated <cron@job>
2024-11-27 15:46:37 +0100

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,19 +1,14 @@
-
-local function getAllReturnStatemetns()
-	local bufnr = 0
-	local query = vim.treesitter.query.parse("lua", [[
-		((return_statement) @keyword.return)
-	]])
-
-	-- local rootTree = vim.treesitter.get_node():tree():root()
+local function getAllReturnStatements(bufnr)
+	bufnr = bufnr or 0
+	local query = vim.treesitter.query.parse("lua", [[ ((return_statement) @keyword.return) ]])
 	local rootTree = vim.treesitter.get_parser(bufnr):parse()[1]:root()
-	local rows = vim.iter(query:iter_captures(rootTree, bufnr)):map(function (_, node, _)
-		local row = node:start()
-		return row
-	end)
+	local nodes = query:iter_captures(rootTree, bufnr)
+	local rows = vim
+		.iter(nodes) -- iterator over nodes
+		:map(function(_, node, _) return node:start() + 1 end)
+		:totable()
 	return rows
 end
 
-local total = getAllReturnStatemetns()
+local total = getAllReturnStatements()
 vim.notify(vim.inspect(total), nil, { title = "🪚 total", ft = "lua" })
-
