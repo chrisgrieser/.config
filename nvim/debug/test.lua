fbaf9ccaf8de513f85cc6dec434278eb6a464bae
🤖 automated <cron@job>
2024-11-28 12:39:12 +0100

    mini Office (3)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,7 +1,3 @@
-local a, b = pcall(vim.treesitter.query.parse, "lua", [[((return_statement) @keyword.return)]])
-vim.notify(vim.inspect(a), nil, { title = "🪚 a", ft = "lua" })
 vim.notify(vim.inspect(b), nil, { title = "🪚 b", ft = "lua" })
 
-
-local b = "./"
 
