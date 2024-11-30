b25f1fd98775a7c852e436584f3f51fe8eccca6a
🤖 automated <cron@job>
2024-11-30 13:26:32 +0100

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,52 +1,6 @@
--- based on https://stackoverflow.com/a/10459129/22114136
--- CAVEAT prints the 1st variable in the caller's scope that has the given value
----@param varValue any
-local function printVariable(varValue)
-	local varname
-
-	-- varname: Check caller's scope
-	for stackLvl = 1, math.huge do
-		local localName, localValue = debug.getlocal(2, stackLvl, 1)
-		if not localName then break end
-		if vim.deep_equal(localValue, varValue) then varname = localName end
-	end
-
-	-- varnme: Check global scope
-	if not varname then
-		for globalName, globalValue in pairs(_G) do
-			if vim.deep_equal(globalValue, varValue) then varname = globalName end
-		end
-	end
-
-	-- line number of print statemetn
-	local caller = debug.getinfo(1, "S")
-	for stackLvl = 2, 10 do
-		local info = debug.getinfo(stackLvl, "S")
-		if
-			info
-			and info.source ~= caller.source
-			and info.what ~= "C"
-			and info.source ~= "lua"
-			and info.source ~= "@" .. (vim.env.MYVIMRC or "")
-		then
-			caller = info
-			break
-		end
-	end
-	local lnum = caller.currentline
-
-	-- notify, with settings for snacks.nvim/nvim-notify
-	local title = ("%s (L%d)"):format(varname or "unknown", lnum)
-	local icon = "󰹈"
-	if package.loaded["notify"] then title = vim.trim(icon .. " " .. title) end
-	vim.notify(
-		vim.inspect(varValue),
-		vim.log.levels.DEBUG,
-		{ title = title, icon = icon, ft = "lua" }
-	)
+local bb = 200
+local function foobar()
+	Chainsaw(bb) -- 🪚
 end
 
---------------------------------------------------------------------------------
-
-local b = 20
-printVariable(b)
+foobar()
--- nvim/lua/plugin-configs/editing-support.lua
+++ nvim/lua/plugin-configs/editing-support.lua
@@ -207,6 +207,9 @@ return {
 		"chrisgrieser/nvim-chainsaw",
 		init = function() vim.g.whichkeyAddGroup("<leader>l", "󰐪 Log") end,
 		opts = {
+			logtypes = {
+				variableLog = { specialNvimDebug = true },
+			},
 			logStatements = {
 				-- not using any marker
 				assertLog = { lua = 'assert({{var}}, "")' },
@@ -231,7 +234,7 @@ return {
 			vim.g.lualine_add("sections", "lualine_x", {
 				require("chainsaw.visuals.statusline").countInBuffer,
 				color = "lualine_x_diagnostics_info_normal", -- only lualine item has also correct bg-color
-				padding = { left = 0, right = 1  },
+				padding = { left = 0, right = 1 },
 			}, "after")
 		end,
 		keys = {
