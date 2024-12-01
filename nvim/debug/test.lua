f0ca2a5d8db9c69382389594b595540f808bdefb
🤖 automated <cron@job>
2024-12-01 15:26:31 +0100

    iMac Home (2)

--- nvim/debug/test.lua
+++ nvim/debug/test.lua
@@ -1,18 +1,6 @@
-local function mydebug()
-	local acc = {}
-	for level = 2, math.huge do
-		local info = debug.getinfo(level, "Sln")
-		if not info then break end
-		local source = info.source:gsub("^@", "")
-		local line = "- " .. (info.name or vim.fs.basename(info.source)) .. ":" .. info.currentline
-		table.insert(acc, line)
+local function tst()
+	local a = {}
+	for level = 2, 20 do
+		table.insert(a, level)
 	end
-	local msg = table.concat(acc, "\n")
-	vim.notify(msg, vim.log.levels.DEBUG)
 end
-
---------------------------------------------------------------------------------
-
-local function one() mydebug() end
-local function two() one() end
-two()
