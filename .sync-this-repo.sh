eb7cffc21f10ac0e07b64ddf8e418d40f8619219
🤖 automated <cron@job>
2024-12-03 21:50:01 +0100

    iMac Home (1)

--- .sync-this-repo.sh
+++ .sync-this-repo.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env zsh
-
 export GIT_OPTIONAL_LOCKS=0    # prevent unnecessary lock files
 cd "$(dirname "$0")" || return 1 # go to location of this script, i.e. going into the git repo
+
 #───────────────────────────────────────────────────────────────────────────────
-
 # ADD & COMMIT
-files_changed="$(git status --porcelain | wc -l | tr -d ' ')"
-device_name=$(scutil --get ComputerName | cut -d" " -f2-)
+# do not exit when no changes, since there could still be changes to pull
 
+device_name=$(scutil --get ComputerName | cut -d" " -f2-)
+files_changed="$(git status --porcelain | wc -l | tr -d ' ')"
 if [[ $files_changed -gt 0 ]] ; then
-	# do not exit when no changes, since there could still be changes to pull
 	git add --all
 	git commit --message="$device_name ($files_changed)" --author="🤖 automated<cron@job>"
 fi
 
 #───────────────────────────────────────────────────────────────────────────────
 # PULL & PUSH
-
 # loop git add-commit-pull-push, since when between add and push files have been
 # changed, the push will fail
+
 i=0
 sleep 1.5 # prevent "Cannot rebase on multiple branches"
 while true; do
