e2ad98015ce9abb4d142970d5b05bb4fd16c68ea
🤖 automated <cron@job>
2024-12-04 18:17:32 +0100

    iMac Home (2)

--- nvim/queries/lua/highlights.scm
+++ nvim/queries/lua/highlights.scm
@@ -5,3 +5,8 @@
 ; - hammerspon: `hs`
 ((identifier) @namespace.builtin
               (#any-of? @namespace.builtin "vim" "hs"))
+
+; break and continue statements should get same styling as return statements
+((break_statement) @keyword.return)
+((identifier) @keyword.return
+              (#any-of? @keyword.return "continue"))
