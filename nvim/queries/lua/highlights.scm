f8924f67530f6a1b7bc4b3a7bad68bfc47ed383a
🤖 automated <cron@job>
2024-12-04 18:47:32 +0100

    iMac Home (1)

--- nvim/queries/lua/highlights.scm
+++ nvim/queries/lua/highlights.scm
@@ -8,5 +8,3 @@
 
 ; break and continue statements should get same styling as return statements
 ((break_statement) @keyword.return)
-((identifier) @keyword.return
-              (#any-of? @keyword.return "continue"))
