f26030098e7cf1438e3af5829038280bb6456b2e
🤖 automated <cron@job>
2024-12-05 12:38:47 +0100

    iMac Home (6)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pandoc-md2pdf.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pandoc-md2pdf.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env zsh
+set -e
+
+# CONFIG
+pdf_file="${*%\.md}_$(date +%Y-%m-%d)_CG.pdf"
+#───────────────────────────────────────────────────────────────────────────────
 
 md_file="$*"
 if [[ ! "$md_file" =~ .*\.md ]]; then
 	echo "⚠️ Not a markdown file"
 	return 1
 fi
-pdf_file="${md_file%\.md}_$(date +%Y-%m-%d)_CG.pdf"
 
-# so `--resource-path` is set
-cd "$(dirname "$md_file")" || return 1
+cd "$(dirname "$md_file")" # so `--resource-path` is set
 
-# INFO pandoc's --data-dir for the `defaults` file defined in .zshenv
-pandoc "$md_file" --output="$pdf_file" --defaults="md2pdf" 2>&1 || return 1
+#───────────────────────────────────────────────────────────────────────────────
+
+# INFO 
+# 1. pandoc's --data-dir for the `defaults` file defined in .zshenv
+# 2. Using headless chromium browser to dependency on pdf-engine for pandoc, and
+#    also since they tend to be not as customizable (e.g., font size not working)
+pandoc "$md_file" --output="/tmp/temp.html" --defaults="md2html" 2>&1
+
+'/Applications/Brave Browser.app/Contents/MacOS/Brave Browser' --headless \
+	--no-pdf-header-footer --print-to-pdf="$pdf_file" "/tmp/temp.html" 2>&1
+
 open -R "$pdf_file"
+
+rm -f "/tmp/temp.html"
 
 #───────────────────────────────────────────────────────────────────────────────
