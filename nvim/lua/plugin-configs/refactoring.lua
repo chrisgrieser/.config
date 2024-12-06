bd24ed355636a2d1f0e44f5c45ac121001d64835
🤖 automated <cron@job>
2024-12-06 21:08:46 +0100

    iMac Home (2)

--- nvim/lua/plugin-configs/refactoring.lua
+++ nvim/lua/plugin-configs/refactoring.lua
@@ -28,11 +28,32 @@ return {
 		dependencies = "nvim-lua/plenary.nvim",
 		opts = { show_success_message = true },
 		keys = {
-			-- stylua: ignore start
-			{ "<leader>fi", function() require("refactoring").refactor("Inline Variable") end, mode = { "n", "x" }, desc = "󱗘 Inline variable" },
-			{ "<leader>fe", function() require("refactoring").refactor("Extract Variable") end, mode = "x", desc = "󱗘 Extract variable" },
-			{ "<leader>fu", function() require("refactoring").refactor("Extract Function") end, mode = "x", desc = "󱗘 Extract function" },
-			-- stylua: ignore end
+			{
+				"<leader>fi",
+				function() require("refactoring").refactor("Inline Variable") end,
+				mode = { "n", "x" },
+				desc = "󱗘 Inline variable",
+			},
+			{
+				"<leader>fe",
+				function()
+					vim.cmd.normal { "viW", bang = true }
+					require("refactoring").refactor("Extract Variable")
+				end,
+				desc = "󱗘 Extract cursorWORD as variable",
+			},
+			{
+				"<leader>fe",
+				function() require("refactoring").refactor("Extract Variable") end,
+				mode = "x",
+				desc = "󱗘 Extract selection as variable",
+			},
+			{
+				"<leader>fu",
+				function() require("refactoring").refactor("Extract Function") end,
+				mode = "x",
+				desc = "󱗘 Extract function",
+			},
 		},
 	},
 }
