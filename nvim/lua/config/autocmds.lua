919c6ec46503d8234bca0544be8863e45c5ecad4
🤖 automated <cron@job>
2024-12-07 13:48:41 +0100

    iMac Home (3)

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -38,38 +38,29 @@ vim.api.nvim_create_autocmd("FocusLost", {
 
 --------------------------------------------------------------------------------
 -- AUTO-SAVE
-vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged", "BufLeave", "FocusLost" }, {
 	desc = "User: Auto-save",
 	callback = function(ctx)
-		local bufnr, bufname = ctx.buf, ctx.file
+		local saveInstantly = ctx.event == "FocusLost" or ctx.event == "BufLeave"
+		local bufnr = ctx.buf
 		local bo, b = vim.bo[bufnr], vim.b[bufnr]
-		if bo.buftype ~= "" or not bo.modifiable or b.autoSaveQueued then return end
+		local bufname = ctx.file
+		if bo.buftype ~= "" or bo.ft == "gitcommit" or bo.readonly then return end
+		if b.saveQueued and not saveInstantly then return end
 
-		b.autoSaveQueued = true
+		b.saveQueued = true
 		vim.defer_fn(function()
-			b.autoSaveQueued = false
+			if not vim.api.nvim_buf_is_valid(bufnr) then return end
 
 			vim.api.nvim_buf_call(bufnr, function()
-				if not vim.api.nvim_buf_is_valid(bufnr) then return end
 				-- `noautocmd` prevents weird cursor movement
 				-- saving with explicit name prevents issues when changing `cwd`
 				local vimCmd = ("silent noautocmd lockmarks update! %q"):format(bufname)
 				vim.cmd(vimCmd)
 			end)
-		end, 2000)
-	end,
-})
-
--- AUTO-FORMAT
--- formatting on `BufLeave` appears to be buggy
-vim.api.nvim_create_autocmd("FocusLost", {
-	desc = "User: Auto-format on `FocusLost`",
-	callback = function(ctx)
-		if not vim.api.nvim_buf_is_valid(ctx.buf) then return end
-		local bo = vim.bo[ctx.buf]
-		if bo.buftype ~= "" or not bo.modifiable then return end
-
-		require("personal-plugins.misc").formatWithFallback { async = true, bufnr = ctx.buf }
+			b.saveQueued = false
+		end, saveInstantly and 0 or 2000)
 	end,
 })
 
