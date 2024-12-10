e49553a4d38a993157e87119fad9224da5259406
🤖 automated <cron@job>
2024-12-10 11:21:53 +0100

    mini Office (5)

--- nvim/after/ftplugin/help.lua
+++ nvim/after/ftplugin/help.lua
@@ -1,5 +1,9 @@
 local bkeymap = require("config.utils").bufKeymap
 --------------------------------------------------------------------------------
 
-bkeymap("n", "q", vim.cmd.bdelete, { desc = "Quit" })
+-- `:bwipeout` so it isn't saved in oldfiles
+bkeymap("n", "q", vim.cmd.bwipeout, { desc = "Quit" })
+
 bkeymap("n", "gs", "gO", { remap = true }) -- `gO` opens the heading-selection in vim help files
+
+vim.diagnostic.enable(false, { bufnr = 0 })
