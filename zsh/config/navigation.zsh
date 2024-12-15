2084b55b04782e4b9b80231c6d9d595851aa7dca
🤖 automated <cron@job>
2024-12-15 12:17:21 +0100

    iMac Home (1)

--- zsh/config/navigation.zsh
+++ zsh/config/navigation.zsh
@@ -6,11 +6,12 @@
 # OPTIONS
 
 # make all directories in these folders available as `cd` targets from anywhere
-export CDPATH="$HOME/Developer:$HOME/Vaults"
+export CDPATH="$HOME/Desktop/:$HOME/Developer/:$HOME/Vaults"
 
 setopt CD_SILENT   # don't echo the directory after `cd`
 setopt CHASE_LINKS # follow symlinks when they are `cd` target
-# not using `AUTO_CD`, since my `tab`-mapping in `completion.zsh` is more flexible
+setopt AUTO_CD     # `cd` to directories without typing `cd`
+setopt AUTO_PUSHD  # push to directory stack on cd (used for own `gr` function)
 
 # POST-DIRECTORY-CHANGE-HOOK (use `cd -q` to suppress this hook)
 function chpwd {
@@ -40,8 +41,6 @@ bindkey '^O' _goto_desktop # remapped to cmd+enter in wezterm keybindings
 
 #───────────────────────────────────────────────────────────────────────────────
 # RECENT DIRS
-# - the list from `dirs` is already populated by zsh-autocomplete, so we do not
-#   need to use `AUTO_PUSHD` etc to populate it
 # - the zsh builtin `cdr` completes based on a number as argument, so the
 #   completions are not searched, which is why we are using this setup of our own
 
@@ -49,6 +48,9 @@ function gr {
 	local goto=${1:-"$OLDPWD"}
 	cd "$goto" || return 1
 }
+
+# on startup, OLDPWD is not set, thus we set it ourselves
+OLDPWD=$(dirs -p | sed -n '2p' | sed "s|^~|$HOME|")
 
 _gr() {
 	[[ $CURRENT -ne 2 ]] && return # only complete first word
