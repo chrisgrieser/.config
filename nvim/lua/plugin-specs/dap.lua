da7afcdce8a1dd19ae7cf7b9895028850cb5e0e8
🤖 automated <cron@job>
2024-12-15 18:47:20 +0100

    iMac Home (3)

--- nvim/lua/plugin-specs/dap.lua
+++ nvim/lua/plugin-specs/dap.lua
@@ -1,16 +1,16 @@
 return {
 	"mfussenegger/nvim-dap",
-	dependencies = { "rcarriga/nvim-dap-ui", "theHamsta/nvim-dap-virtual-text" },
+
+	-- as dependency, since this one we want to auto-load via listeners
+	dependencies = "theHamsta/nvim-dap-virtual-text",
+
 	keys = {
 		{ "7", function() require("dap").continue() end, desc = " Continue" },
 		{ "8", function() require("dap").toggle_breakpoint() end, desc = " Toggle breakpoint" },
-		{
-			"<leader>dc",
-			function() require("dap").clear_breakpoints() end,
-			desc = " Clear breakpoints",
-		},
+		-- stylua: ignore
+		{ "<leader>dc", function() require("dap").clear_breakpoints() end, desc = " Clear breakpoints" },
 		{ "<leader>dr", function() require("dap").restart() end, desc = " Restart" },
-		{ "<leader>dt", function() require("dap").terminate() end, desc = " Terminate" },
+		{ "<leader>dt", function() require("dap").terminate() end, desc = " Terminate" },
 	},
 	init = function() vim.g.whichkeyAddSpec { "<leader>d", group = "󰃤 Debugger" } end,
 	config = function()
@@ -37,20 +37,23 @@ return {
 			function()
 				local breakpoints = require("dap.breakpoints").get()
 				if #breakpoints == 0 then return "" end
-				local thisBuf = breakpoints[vim.api.nvim_get_current_buf()]
 				local allBufs = 0
 				for _, bp in pairs(breakpoints) do
 					allBufs = allBufs + #bp
 				end
+				local thisBuf = #(breakpoints[vim.api.nvim_get_current_buf()] or {})
+
+				local countStr = (thisBuf == allBufs) and thisBuf or thisBuf .. "/" .. allBufs
 				local icon = vim.fn.sign_getdefined("DapBreakpoint")[1].text
-				return (" %s %d/%d"):format(icon, #thisBuf, allBufs)
+				return icon .. countStr
 			end,
 		}, "before")
+
 		-- status
 		vim.g.lualineAdd("tabline", "lualine_z", function()
-			local dapStatus = require("dap").status()
-			if dapStatus == "" then return "" end
-			return "󰃤 " .. dapStatus
+			local status = require("dap").status()
+			if status == "" then return "" end
+			return "󰃤 " .. status
 		end)
 	end,
 }
