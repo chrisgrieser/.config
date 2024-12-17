c7cc5497c80c83eba871ec83ebc38d3fab4bad7a
🤖 automated <cron@job>
2024-12-17 15:47:05 +0100

    mini Office (2)

--- Alfred.alfredpreferences/workflows/alfred-atop/scripts/volume-search.js
+++ Alfred.alfredpreferences/workflows/alfred-atop/scripts/volume-search.js
@@ -3,6 +3,9 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 //──────────────────────────────────────────────────────────────────────────────
 
+// special volumes to ignore
+const ignoreVolumes = ["Recovery"];
+
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
 	let rerunSecs = 4; // slow reruns as default
@@ -11,18 +14,20 @@ function run() {
 	const volumes = app
 		.doShellScript("df -HY") // -H: human readable sizes (base10), -Y: filesystem format
 		.split("\r")
-		.reduce((volumes, line) => line.includes(" /Volumes/"))
-			if (!line.includes(" /Volumes/")
+		.reduce((/** @type {AlfredItem[]} */ volumes, line) => {
+			if (!line.includes(" /Volumes/")) return volumes;
 			// quicker reruns when volume stats unavailable
-			if (vol.includes("unavailable")) rerunSecs = 0.5;
+			if (line.includes("unavailable")) rerunSecs = 0.5;
 
-			const info = vol
+			const info = line
 				.split(/\s+/)
 				.map((value) => value.replaceAll("unavailable", "…").replace(/(\d[GTMk]$)/, "$1b"));
 
 			const [_, format, total, used, available, share] = info;
 			const path = info.slice(9).join(" ");
 			const name = path.replace("/Volumes/", "");
+
+			if (ignoreVolumes.includes(name)) return volumes;
 
 			const subtitle = `『${format}』   Total: ${total}   Available: ${available}   Used: ${used} (${share})`;
 			volumes.push({
@@ -31,7 +36,8 @@ function run() {
 				subtitle: subtitle,
 				arg: path,
 			});
-		}, {});
+			return volumes;
+		}, []);
 
 	// No Volume found
 	if (volumes.length === 0) {
