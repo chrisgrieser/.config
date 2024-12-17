1f9e7f73faed3158270727a3ddf2285cb8920f5e
🤖 automated <cron@job>
2024-12-17 19:39:06 +0100

    mini Office (8)

--- nvim/lua/plugin-specs/genghis.lua
+++ nvim/lua/plugin-specs/genghis.lua
@@ -7,20 +7,23 @@ return {
 		{"<leader>yr", function() require("genghis").copyRelativePath() end, desc = "󰝰 Relative path" },
 		{"<leader>yn", function() require("genghis").copyFilename() end, desc = "󰈔 Name of file" },
 		{"<leader>yp", function() require("genghis").copyDirectoryPath() end, desc = "󰝰 Parent path" },
-		{"<leader>yf", function() require("genghis").copyFileItself() end, desc = "󰈔 File (macOS)" },
+		{"<leader>yf", function() require("genghis").copyFileItself() end, desc = "󱉥 File (macOS)" },
 		-- stylua: ignore end
 
 		-- stylua: ignore
-		{ "<D-l>", function() require("genghis").showInSystemExplorer() end, desc = "󰀶 Reveal in Finder" },
 		{ "<leader>fr", function() require("genghis").renameFile() end, desc = "󰑕 Rename" },
-		{ "<leader>fn", function() require("genghis").createNewFile() end, desc = " New" },
-		{ "<leader>fd", function() require("genghis").duplicateFile() end, desc = " Duplicate" },
+		{ "<leader>fn", function() require("genghis").createNewFile() end, desc = "󰝒 New" },
+		{ "<leader>fw", function() require("genghis").duplicateFile() end, desc = " Duplicate" },
 		{ "<leader>fm", function() require("genghis").moveToFolderInCwd() end, desc = "󰪹 Move" },
-		{ "<leader>f<BS>", function() require("genghis").trashFile() end, desc = "󰩹 Trash" },
+		{ "<leader>fd", function() require("genghis").trashFile() end, desc = "󰩹 Delete" },
 
 		-- stylua: ignore
 		{ "<leader>rx", function() require("genghis").moveSelectionToNewFile() end, mode = "x", desc = " Selection to new file" },
-
+		{
+			"<D-l>",
+			function() require("genghis").showInSystemExplorer() end,
+			desc = "󰀶 Reveal in Finder",
+		},
 		{ "<leader>ex", function() require("genghis").chmodx() end, desc = "󰒃 chmod +x" },
 	},
 }
