9be996ba3a6fc18bc87f310e5407e28d3e5afe7e
🤖 automated <cron@job>
2024-12-18 23:47:16 +0100

    iMac Home (10)

--- nvim/lua/plugin-specs/oil.lua
+++ nvim/lua/plugin-specs/oil.lua
@@ -39,8 +39,8 @@ return {
 		float = {
 			border = vim.g.borderStyle,
 			override = function(conf)
-				local height = 0.9
-				local width = 0.7
+				local height = 0.85
+				local width = 0.6
 				conf.row = math.floor((1 - height) / 2 * vim.o.lines)
 				conf.col = math.floor((1 - width) / 2 * vim.o.columns)
 				conf.height = math.floor(vim.o.lines * height)
@@ -54,8 +54,8 @@ return {
 				local bufnr = vim.api.nvim_win_get_buf(winid)
 				local absPath = vim.api.nvim_buf_get_name(bufnr):gsub("^oil://", "")
 				local cwd = vim.uv.cwd() or ""
-				local relPath = "." .. absPath:gsub("^oil://", ""):sub(#cwd + 1)
-				local title = relPath ~= "" and relPath or absPath:gsub(vim.env.HOME, "~")
+				local relPath = "." .. absPath:sub(#cwd + 1)
+				local title = vim.startswith(absPath, cwd) and relPath or absPath:gsub(vim.env.HOME, "~")
 				return " " .. title .. " "
 			end,
 		},
