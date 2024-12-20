3d579418fcb96ae827d22a8e2f8722d721453e1d
🤖 automated <cron@job>
2024-12-20 14:47:13 +0100

    iMac Home (4)

--- nvim/debug/scratchf.lua
+++
@@ -1,6 +0,0 @@
-
-
-local mr = require("mason-registry")
-local names = mr.get_installed_package_names()
-
-Chainsaw(names) -- 🪚
--- transmission/post-download-actions.sh
+++ transmission/post-download-actions.sh
@@ -11,14 +11,14 @@ find . -type directory -name "Sample" -exec rm -r {} + # Folders with content do
 sleep 1
 
 # UNNEST IF SINGLE FILE
-find . -mindepth 1 -type d | while read -r folder; do
+find . -mindepth 1 -type directory | while read -r folder; do
 	files_in_folder=$(find "$folder" -depth 1 | wc -l | tr -d " ")
 	[[ $files_in_folder -eq 1 ]] && mv "$folder"/* "$TR_TORRENT_DIR"
 	rmdir "$folder" # only deletes empty folders
 done
 
 # QUIT TRANSMISSION, IF NO OTHER ACTIVE TORRENTS
-sleep 3 # time for new torrents to be initialized
+sleep 4 # time for new torrents to be initialized
 incomplete_dir=$(defaults read org.m0k.transmission IncompleteDownloadFolder)
-in_progress=$(find "$incomplete_dir" -mindepth 1 -type directory -not -path "**/TV**")
-[[ -z "$in_progress" ]] && killall "Transmission"
+active_torrents=$(find "$incomplete_dir" -mindepth 1 -type directory -not -path "**/TV**")
+[[ -z "$active_torrents" ]] && killall "Transmission"
