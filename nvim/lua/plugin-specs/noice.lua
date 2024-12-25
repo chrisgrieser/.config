cad379b4d39570fe9ab102cd2dfaf3fc0d7b6e19
🤖 automated <cron@job>
2024-12-25 22:17:06 +0100

    iMac Home (2)

--- nvim/lua/plugin-specs/noice.lua
+++ nvim/lua/plugin-specs/noice.lua
@@ -111,6 +111,11 @@ return {
 		lsp = {
 			progress = { enabled = false }, -- using my own
 			signature = { enabled = false }, -- using lsp_signature.nvim
+			hover = { enabled = false },
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+			},
 		},
 	},
 }
