4d642650d4ec434e2eaa64a10295e1c44a7009c7
🤖 automated <cron@job>
2024-12-30 12:54:13 +0100

    iMac Home (4)

--- nvim/queries/yaml/highlights.scm
+++ nvim/queries/yaml/highlights.scm
@@ -1,10 +1,8 @@
 ;extends
 
 ; PENDING https://github.com/nvim-treesitter/nvim-treesitter/pull/7512
-
 ; FIX for yaml's norway problem https://www.bram.us/2022/01/11/yaml-the-norway-problem/
 ; see also https://yamllint.readthedocs.io/en/stable/rules.html#module-yamllint.rules.truthy
 ((string_scalar) @boolean
   (#any-of? @boolean
-    "yes" "no" "YES" "NO" "Yes" "No" "y" "n" "Y" "N" "on" "off" "ON" "OFF" "On" "Off" "TRUE" "FALSE"
-    "True" "False"))
+    "yes" "no" "YES" "NO" "Yes" "No" "on" "off" "ON" "OFF" "On" "Off" "TRUE" "FALSE" "True" "False"))
