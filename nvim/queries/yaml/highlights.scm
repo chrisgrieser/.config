3aa341839ae2998097248aac5524f2c2e8b0e55f
🤖 automated <cron@job>
2024-12-31 13:12:24 +0100

    iMac Home (3)

--- nvim/queries/yaml/highlights.scm
+++ nvim/queries/yaml/highlights.scm
@@ -3,11 +3,22 @@
 ; PENDING https://github.com/nvim-treesitter/nvim-treesitter/pull/7512
 ; FIX for yaml's norway problem https://www.bram.us/2022/01/11/yaml-the-norway-problem/
 ; see also https://yamllint.readthedocs.io/en/stable/rules.html#module-yamllint.rules.truthy
+;───────────────────────────────────────────────────────────────────────────────
+(block_mapping_pair
+  value: (block_node
+    (block_sequence
+      (block_sequence_item
+        (flow_node
+          (plain_scalar
+            (string_scalar) @boolean
+            (#any-of? @boolean
+              "yes" "no" "YES" "NO" "Yes" "No" "on" "off" "ON" "OFF" "On" "Off" "TRUE" "FALSE"
+              "True" "False")))))))
 
 (block_mapping_pair
-  key: (flow_node) @_run
-  (#any-of? @_run "shell_command" "cmd"))
-
-((string_scalar) @boolean
-  (#any-of? @boolean
-    "yes" "no" "YES" "NO" "Yes" "No" "on" "off" "ON" "OFF" "On" "Off" "TRUE" "FALSE" "True" "False"))
+  value: (flow_node
+    (plain_scalar
+      (string_scalar) @boolean
+      (#any-of? @boolean
+        "yes" "no" "YES" "NO" "Yes" "No" "on" "off" "ON" "OFF" "On" "Off" "TRUE" "FALSE" "True"
+        "False"))))
