f7cb7bef21400ed631d6ad540fb04f1a074e0007
🤖 automated <cron@job>
2025-01-04 13:30:49 +0100

    iMac Mother (4)

--- hammerspoon/apps/browser.lua
+++ hammerspoon/apps/browser.lua
@@ -42,17 +42,15 @@ end):start()
 
 --------------------------------------------------------------------------------
 -- BOOKMARKS SYNCED TO CHROME BOOKMARKS
--- (so Alfred can pick up the Bookmarks without extra keyword)
+-- so Alfred can pick up the Bookmarks without extra keyword
+
+-- INFO The pathwatcher is triggered by changes of the *target*, while this
+-- function touches the *symlink itself* due to `-h`. Thus, there is no need to
+-- configure the symlink target here.
 
 local chromeBookmarks = os.getenv("HOME")
 	.. "/Library/Application Support/Google/Chrome/Default/Bookmarks"
-
-local function touchSymlink()
-	-- `-h` touches the symlink itself instead of its target.
-	-- The pathwatcher *trigger* is triggered by changes of the target, while
-	-- this function touches the symlink itself.
-	hs.execute(("touch -h %q"):format(chromeBookmarks))
-end
+local function touchSymlink() hs.execute(("touch -h %q"):format(chromeBookmarks)) end
 
 -- sync on system start & when bookmarks are changed
 if u.isSystemStart() then touchSymlink() end
