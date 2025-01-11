95429e714fd68c29c3656ed8ad07cf8b61509c47
🤖 automated <cron@job>
2025-01-11 21:52:09 +0100

    iMac Home (1)

--- nvim/lua/lspconfig/configs/gh_actions_ls.lua
+++ nvim/lua/lspconfig/configs/gh_actions_ls.lua
@@ -1,21 +1,21 @@
-local util = require 'lspconfig.util'
+local util = require("lspconfig.util")
 
 return {
-  default_config = {
-    cmd = { 'gh-actions-language-server', '--stdio' },
-    filetypes = { 'yaml.github' },
-    root_dir = util.root_pattern('.github'),
-    single_file_support = true,
-    capabilities = {
-      workspace = {
-        didChangeWorkspaceFolders = {
-          dynamicRegistration = true,
-        },
-      },
-    },
-  },
-  docs = {
-    description = [[
+	default_config = {
+		cmd = { "gh-actions-language-server", "--stdio" },
+		filetypes = { "yaml.github" },
+		root_dir = util.root_pattern(".github"),
+		single_file_support = true,
+		capabilities = {
+			workspace = {
+				didChangeWorkspaceFolders = {
+					dynamicRegistration = true,
+				},
+			},
+		},
+	},
+	docs = {
+		description = [[
 https://github.com/lttb/gh-actions-language-server
 
 Language server for GitHub Actions.
@@ -26,5 +26,5 @@ Language server for GitHub Actions.
 npm install -g gh-actions-language-server
 ```
 ]],
-  },
+	},
 }
