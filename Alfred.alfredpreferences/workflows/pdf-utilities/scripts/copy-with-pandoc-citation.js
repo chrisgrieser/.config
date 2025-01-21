514c1c2800acc9d91a4e815476adc02c1caba1a0
🤖 automated <cron@job>
2025-01-21 15:38:38 +0100

    mini Office (2)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/copy-with-pandoc-citation.js
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/copy-with-pandoc-citation.js
@@ -4,19 +4,18 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 //──────────────────────────────────────────────────────────────────────────────
 
-// REQUIRED
-// this script assumes that PDF files are have the format `{citekey}_{title}.pdf`
-
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
 	const selection = (argv[0] || "")
 		.replace(/[\n\r]/g, " ") // remove line breaks
 		.replaceAll("- ", "") // remove hyphenation
-		.trim()
+		.replace(/["’]/g, "'") // consistent single quotes
+		.trim();
 
 	const pdfWinTitle = Application("System Events").processes.Highlights?.windows[0]?.name();
 	// e.g.: "YlijokiMantyla2003_Conflicting Time Perspectives in Academic Work.pdf – Page 1 of 24"
+	// REQUIRED assumes that PDF files are have the format `{citekey}_{title}.pdf`
 	const [_, citekey, currentPage] = pdfWinTitle.match(/(.*?)_.* Page (\d+) of \d+/);
 	const pageInPdf = Number.parseInt(currentPage || "0");
 
@@ -29,8 +28,10 @@ function run(argv) {
 	// e.g.: pages = {55--78},
 	const firstTruePage = Number.parseInt(entry.match(/pages ?= ?\{(\d+)-+\d+\},/)?.[1] || "0");
 	const trueCurrentPage = pageInPdf + firstTruePage - 1;
-	const citation = `${citekey}, p. ${trueCurrentPage}`
+	const citation = `${citekey}, p. ${trueCurrentPage}`;
+
 	const toCopy = selection ? `"${selection}" [${citation}]` : `[${citation}]`;
 	app.setTheClipboardTo(toCopy);
+
 	return citation; // for Alfred notification
 }
