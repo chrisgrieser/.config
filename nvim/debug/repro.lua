b7598fb215b8a144f89a4a23fa93c72010eab73e
🤖 automated <cron@job>
2025-01-21 14:48:04 +0100

    mini Office (1)

--- nvim/debug/repro.lua
+++ nvim/debug/repro.lua
@@ -10,8 +10,4 @@ load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/ma
 require("lazy.minit").repro { spec = {} }
 --------------------------------------------------------------------------------
 
--- FIX broken `:Inspect` https://github.com/neovim/neovim/issues/31675
--- can be removed on the version after 0.10.3
-vim.hl = vim.highlight
-
 vim.cmd.colorscheme("tokyonight-day")
