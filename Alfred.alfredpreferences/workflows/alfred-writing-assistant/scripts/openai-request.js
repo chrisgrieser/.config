7129e17267c0b7054ffff70fab8e0a7c7676c1d2
🤖 automated <cron@job>
2025-02-05 17:06:02 +0100

    iMac Home (3)

--- Alfred.alfredpreferences/workflows/alfred-writing-assistant/scripts/openai-request.js
+++ Alfred.alfredpreferences/workflows/alfred-writing-assistant/scripts/openai-request.js
@@ -10,20 +10,6 @@ function writeToFile(filepath, text) {
 	str.writeToFileAtomicallyEncodingError(filepath, true, $.NSUTF8StringEncoding, null);
 }
 
-function ensureCacheFolderExists() {
-	const finder = Application("Finder");
-	const cacheDir = $.getenv("alfred_workflow_cache");
-	if (!finder.exists(Path(cacheDir))) {
-		const cacheDirBasename = $.getenv("alfred_workflow_bundleid");
-		const cacheDirParent = cacheDir.slice(0, -cacheDirBasename.length);
-		finder.make({
-			new: "folder",
-			at: Path(cacheDirParent),
-			withProperties: { name: cacheDirBasename },
-		});
-	}
-}
-
 //──────────────────────────────────────────────────────────────────────────────
 
 /** @type {AlfredRun} */
@@ -31,24 +17,29 @@ function ensureCacheFolderExists() {
 function run(argv) {
 	const selection = argv[0]?.trim();
 	if (!selection) return "ERROR: No selection.";
-	ensureCacheFolderExists();
-	const dataCache = $.getenv("alfred_workflow_cache") + "/request-data.json";
 
 	const apiKey =
 		$.NSProcessInfo.processInfo.environment.objectForKey("alfred_apikey").js ||
-		app.doShellScript('source "$HOME/.zshenv"; echo "$OPENAI_API_KEY"');
+		app.doShellScript('source "$HOME/.zshenv"; echo "$OPENAI_API_KEY"').trim();
 	const prompt = $.getenv("static_prompt") + " " + selection;
+	// Needs division by 10, since Alfred workflow config does not allow setting
+	// decimal values in its number sliders.
 	const temperature = Number.parseInt($.getenv("temperature")) / 10;
+	const frequencyPenalty = Number.parseInt($.getenv("frequency_penalty")) / 10;
 
 	const data = {
 		model: $.getenv("openai_model"),
 		messages: [{ role: "user", content: prompt }],
 		temperature: temperature,
+		// biome-ignore lint/style/useNamingConvention: not defined by me
+		frequency_penalty: frequencyPenalty,
 	};
 
-	// write to file as send request via `--data-binary` to avoid more escaping issues
-	// DOCS https://platform.openai.com/docs/api-reference/chat
+	// write to file as send request via `--data-binary` to avoid escaping issues
+	const dataCache = $.getenv("alfred_workflow_cache") + "/request-data.json";
 	writeToFile(dataCache, JSON.stringify(data));
+
+	// DOCS https://platform.openai.com/docs/api-reference/chat
 	const response = app.doShellScript(
 		`curl --max-time 15 https://api.openai.com/v1/chat/completions \
 		-H 'Content-Type: application/json' \
