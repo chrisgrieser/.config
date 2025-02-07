e34575ba470e4305f84ba14394cbc1556e859300
🤖 automated <cron@job>
2025-02-07 14:17:32 +0100

    mini Office (4)

--- Alfred.alfredpreferences/workflows/alfred-atop/scripts/select-dns-server.js
+++ Alfred.alfredpreferences/workflows/alfred-atop/scripts/select-dns-server.js
@@ -28,6 +28,30 @@ function run() {
 			arg: ["223.5.5.5", "223.6.6.6"],
 			variables: { server_name: "AliDNS" },
 		},
+		{
+			title: "Mullvad",
+			subtitle: "194.242.2.4 & 194.242.2.3",
+			arg: ["194.242.2.4", "194.242.2.3"],
+			variables: { server_name: "Mullvad" },
+		},
+		{
+			title: "DNS0",
+			subtitle: "193.110.81.0 & 185.253.5.0",
+			arg: ["193.110.81.0", "185.253.5.0"],
+			variables: { server_name: "DNS0" },
+		},
+		{
+			title: "Quad9",
+			subtitle: "9.9.9.9 & 149.112.112.112",
+			arg: ["9.9.9.9", "149.112.112.112"],
+			variables: { server_name: "Quad9" },
+		},
+		{
+			title: "AdGuard",
+			subtitle: "94.140.14.14 & 94.140.15.15",
+			arg: ["94.140.14.14", "94.140.15.15"],
+			variables: { server_name: "AdGuard" },
+		},
 	];
 
 	// get current DNS
@@ -37,10 +61,14 @@ function run() {
 		.replace("*", ""); // asterisk is used to mark disabled services
 	const currentDns = app
 		.doShellScript(`networksetup -getdnsservers "${serviceName}"`)
-		.split("\r")[0]
+		.split("\r")[0];
 	if (currentDns === "8.8.8.8") selectableDns[0].title = "✅ Google";
 	else if (currentDns === "1.1.1.1") selectableDns[1].title = "✅ Cloudflare";
 	else if (currentDns === "223.5.5.5") selectableDns[2].title = "✅ AliDNS";
+	else if (currentDns === "194.242.2.4") selectableDns[3].title = "✅ Mullvad";
+	else if (currentDns === "193.110.81.0") selectableDns[4].title = "✅ DNS0";
+	else if (currentDns === "9.9.9.9") selectableDns[5].title = "✅ quad9";
+	else if (currentDns === "94.140.14.14") selectableDns[6].title = "✅ AdGuard";
 
 	return JSON.stringify({ items: selectableDns });
 }
