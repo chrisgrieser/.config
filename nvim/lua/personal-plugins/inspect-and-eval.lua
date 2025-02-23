c3a3ab9db8a5db344763be1c9804d368e4c5d9fd
🤖 automated <cron@job>
2025-02-23 12:00:06 +0100

    iMac Home (3)

--- nvim/lua/personal-plugins/inspect-and-eval.lua
+++ nvim/lua/personal-plugins/inspect-and-eval.lua
@@ -40,52 +40,6 @@ function M.bufferInfo()
 	vim.notify(table.concat(out, "\n"), vim.log.levels.DEBUG, opts)
 end
 
-function M.nodeAtCursor()
-	local config = { hlDuration = 1500, hlGroup = "Search", maxChildren = 4 }
-
-	local ok, node = pcall(vim.treesitter.get_node)
-	if not (ok and node) then
-		vim.notify("No node under cursor", vim.log.levels.DEBUG, { icon = "" })
-		return
-	end
-
-	-- node info
-	local parent = node:parent() and node:parent():type() or "."
-	local tree = { parent, "└── " .. node:type() .. " (this node)" }
-	for childIdx = 1, config.maxChildren do
-		local child = node:child(childIdx)
-		if not child then break end
-		table.insert(tree, ("      ├── %s"):format(child:type()))
-	end
-	tree[#tree] = tree[#tree]:gsub("├", "└")
-	local msg = table.concat(tree, "\n")
-	vim.notify(msg, vim.log.levels.DEBUG, { icon = "", title = "Node at cursor" })
-
-	-- highlight the full node
-	local startRow, startCol = node:start()
-	local endRow, endCol = node:end_()
-	local ns = vim.api.nvim_create_namespace("node-highlight")
-	if startRow == endRow then
-		vim.api.nvim_buf_add_highlight(0, ns, config.hlGroup, startRow, startCol, endCol)
-	else
-		vim.api.nvim_buf_add_highlight(0, ns, config.hlGroup, startRow, startCol, -1)
-		local lnum = startRow + 1
-		while lnum < endRow do
-			vim.api.nvim_buf_add_highlight(0, ns, config.hlGroup, lnum, 0, -1)
-			lnum = lnum + 1
-		end
-		vim.api.nvim_buf_add_highlight(0, ns, config.hlGroup, endRow, 0, endCol)
-	end
-	vim.defer_fn(function() vim.api.nvim_buf_clear_namespace(0, ns, 0, -1) end, config.hlDuration)
-
-	-- FIX on_key being triggered by `n` key (only needed for my personal config)
-	-- see https://www.reddit.com/r/neovim/comments/1h051ht/comment/m01r7ju/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1
-	vim.defer_fn(function()
-		local countNs = vim.api.nvim_create_namespace("searchCounter")
-		vim.api.nvim_buf_clear_namespace(0, countNs, 0, -1)
-	end, 1)
-end
-
 function M.lspCapabilities()
 	local clients = vim.lsp.get_clients { bufnr = 0 }
 	if #clients == 0 then
@@ -127,31 +81,6 @@ function M.evalNvimLua()
 		vim.cmd.normal { '"zy', bang = true }
 		eval(vim.fn.getreg("z"))
 	end
-end
-
-function M.runFile()
-	vim.cmd("silent update")
-	local filepath = vim.api.nvim_buf_get_name(0)
-	local ft = vim.bo.filetype
-	if ft == "lua" and filepath:find("nvim") then
-		vim.cmd.source()
-		return
-	end
-
-	local cmd
-	if ft == "zsh" or ft == "bash" or ft == "sh" then
-		cmd = ft
-	else
-		local msg = ("Filetype %q not supported."):format(ft)
-		vim.notify(msg, vim.log.levels.WARN, { title = "Run", icon = "󰜎" })
-		return
-	end
-
-	vim.system({ cmd, filepath }, {}, function(out)
-		local msg = vim.trim(out.stdout .. "\n" .. out.stderr)
-		local lvl = out.code == 0 and "INFO" or "ERROR"
-		vim.notify(msg, vim.log.levels[lvl], { title = "Run", icon = "󰜎" })
-	end)
 end
 
 --------------------------------------------------------------------------------
