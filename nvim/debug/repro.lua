f2ee709b0faf9964b99a2009615fe8d8daae5ba7
🤖 automated <cron@job>
2025-03-02 14:19:04 +0100

    iMac Home (1)

--- nvim/debug/repro.lua
+++ nvim/debug/repro.lua
@@ -1,13 +1,20 @@
 -- save as `minimal-config.lua`
 -- run via: `nvim -u minimal-config.lua`
 --------------------------------------------------------------------------------
-local spec = {
+{
+    'chrisgrieser/nvim-spider',
+    opts = {
+      skipInsignificantPunctuation = false,
+    },
+    keys = {
+      { 'w', "<cmd>lua require('spider').motion('w')<CR>", mode = { 'n', 'o', 'x' } },
+      { 'e', "<cmd>lua require('spider').motion('e')<CR>", mode = { 'n', 'o', 'x' } },
+      { 'b', "<cmd>lua require('spider').motion('b')<CR>", mode = { 'n', 'o', 'x' } },
+    },
 }
 
 --------------------------------------------------------------------------------
 vim.env.LAZY_STDPATH = "/tmp/nvim-repro"
 load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
-require("lazy.minit").repro { spec = {} }
+require("lazy.minit").repro { spec = spec }
 --------------------------------------------------------------------------------
-
-vim.cmd.colorscheme("tokyonight-day")
