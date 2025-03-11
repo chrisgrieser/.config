f8b16398f5d542bc45672ce7e65fdfa321b246e7
🤖 automated <cron@job>
2025-03-11 19:22:45 +0100

    mini Office (1)

--- nvim/lua/personal-plugins/misc.lua
+++ nvim/lua/personal-plugins/misc.lua
@@ -101,7 +101,7 @@ function M.toggleOrIncrement()
 	if newWord then
 		vim.cmd.normal { '"_ciw' .. newWord, bang = true }
 	else
-		-- needs :execute to escape `<C-a>`
+		-- needs `:execute` to escape `<C-a>`
 		vim.cmd.execute('"normal! ' .. vim.v.count1 .. '\\<C-a>"')
 	end
 end
