160dca6fdaa0de648a8c7044cc67909fb237b996
🤖 automated <cron@job>
2025-04-03 11:51:05 +0200

    iMac Home (3)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -175,9 +175,9 @@ do
 
 	vim.api.nvim_create_autocmd("LspAttach", {
 		desc = "User: Set LSP folding if client supports it",
-		callback = function(args)
-			local client = vim.lsp.get_client_by_id(args.data.client_id)
-			if client and client:supports_method("textDocument/foldingRange") then
+		callback = function(ctx)
+			local client = assert(vim.lsp.get_client_by_id(ctx.data.client_id))
+			if client:supports_method("textDocument/foldingRange") then
 				local win = vim.api.nvim_get_current_win()
 				vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
 			end
