e2be45af8f5a92e88318c5fae059b0ea8a922d3f
🤖 automated <cron@job>
2025-04-07 16:08:16 +0200

    iMac Home (7)

--- nvim/after/ftplugin/swift.lua
+++ nvim/after/ftplugin/swift.lua
@@ -1,2 +1,9 @@
 -- formatters prescribe comments being separated by two spaces
 vim.opt_local.listchars:append { multispace = " " }
+
+--------------------------------------------------------------------------------
+
+local abbr = require("config.utils").bufAbbrev
+abbr("--", "//")
+abbr("local", "let")
+abbr("const", "let")
