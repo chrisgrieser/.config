fb23a838af3f867f0314de648b9e77de79e614e2
🤖 automated <cron@job>
2025-04-11 15:26:38 +0200

    iMac Home (8)

--- hammerspoon/win-management/sketchybar.lua
+++ hammerspoon/win-management/sketchybar.lua
@@ -5,7 +5,6 @@ local M = {} -- persist from garbage collector
 
 local env = require("meta.environment")
 local u = require("meta.utils")
-local wu = require("win-management.window-utils")
 
 local aw = hs.application.watcher
 --------------------------------------------------------------------------------
@@ -14,19 +13,18 @@ local aw = hs.application.watcher
 local dontTriggerHidingOtherApps = { "Alfred", "CleanShot X", "IINA", "pinentry-mac", "Catch" }
 
 M.aw_maxWindows = aw.new(function(appName, event, app)
-	if appName == "Hammerspoon" then return end -- lots of dummy windows
+	if appName == "Hammerspoon" then return end -- never hide the hammerspoon console
 	if event ~= aw.deactivated or env.isProjector() or u.isFront(dontTriggerHidingOtherApps) then
 		return
 	end
 
-	local hasCoveringWin = hs.fnutils.some(app:allWindows(), function(win)
-		local maximized = wu.winHasSize(win, hs.layout.maximized)
-		local leftHalf = wu.winHasSize(win, hs.layout.left50)
-		return maximized or leftHalf
-	end)
-	local hasLikelyCoveringWin = #app:allWindows() > 2
-
-	if hasCoveringWin or hasLikelyCoveringWin then app:hide() end
+	for _, win in pairs(app:allWindows()) do
+		local coversLeftTopCorner = win:frame().x == 0 and win:frame().y == 0
+		if coversLeftTopCorner then
+			app:hide()
+			return
+		end
+	end
 end):start()
 
 --------------------------------------------------------------------------------
