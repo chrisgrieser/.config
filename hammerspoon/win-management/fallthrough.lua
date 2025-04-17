8f3cb484542d172c9d87e7a5e4a1f71453bc67a4
🤖 automated <cron@job>
2025-04-17 14:53:33 +0200

    mini Office (1)

--- hammerspoon/win-management/fallthrough.lua
+++ hammerspoon/win-management/fallthrough.lua
@@ -1,27 +1,35 @@
+-- prevent keeping focus when closing a window
+--------------------------------------------------------------------------------
+
 local M = {}
+
+local u = require("meta.utils")
 
 local wf = hs.window.filter
 local aw = hs.application.watcher
 --------------------------------------------------------------------------------
 
--- prevent keeping focus when closing a window
+-- CONFIG
 local fallthroughApps = { "Finder", "Brave Browser", "Obsidian" }
 
 local function fallthrough()
-	local frontApp = hs.application.frontmostApplication()
-	local isFront = hs.fnutils.contains(fallthroughApps, frontApp:name())
-	local noWins = #(frontApp:allWindows()) == 0
-	if isFront and noWins then
-		local allWins = hs.window.orderedWindows() -- all visible windows in order
-		for _, win in ipairs(allWins) do
-			local winFromFallThroughApp =
-				hs.fnutils.contains(fallthroughApps, win:application():name()) ---@diagnostic disable-line: undefined-field
-			if win:isStandard() and not winFromFallThroughApp then
-				win:focus() -- hiding fallthrough-app does not work, must focus next win
-				return
+	-- deferring to ensure windows are already switched/created
+	u.defer(0.1, function()
+		local frontApp = hs.application.frontmostApplication()
+		local isFront = hs.fnutils.contains(fallthroughApps, frontApp:name())
+		local noWins = #(frontApp:allWindows()) == 0
+		if isFront and noWins then
+			local allWins = hs.window.orderedWindows() -- all visible windows in order
+			for _, win in ipairs(allWins) do
+				local winFromFallThroughApp =
+					hs.fnutils.contains(fallthroughApps, win:application():name()) ---@diagnostic disable-line: undefined-field
+				if win:isStandard() and not winFromFallThroughApp then
+					win:focus() -- hiding fallthrough-app does not work, must focus next win
+					return
+				end
 			end
 		end
-	end
+	end)
 end
 
 M.wf_fallthrough = wf
@@ -30,9 +38,6 @@ M.wf_fallthrough = wf
 	:subscribe(wf.windowDestroyed, fallthrough)
 
 M.aw_fallthrough = aw.new(function(name, event, _app)
-	-- deactivation of an app
-	if event == aw.deactivated or event == aw.terminated then fallthrough() end
-
 	-- activation of an fallthrough app
 	if event == aw.activated and hs.fnutils.contains(fallthroughApps, name) then fallthrough() end
 end):start()
