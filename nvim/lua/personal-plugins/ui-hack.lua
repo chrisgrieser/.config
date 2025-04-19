5aa5cf51ba1c1f95bc594c64f59f22161b9f56a2
🤖 automated <cron@job>
2025-04-19 15:07:41 +0200

    iMac Home (3)

--- nvim/lua/personal-plugins/ui-hack.lua
+++ nvim/lua/personal-plugins/ui-hack.lua
@@ -1,13 +1,14 @@
 -- INFO redirects cmdline messages to `vim.notify`, silencing `Press Enter to
--- continue` prompts even with `cmdheight=0`.
+-- continue` prompts, even with `cmdheight=0`.
 -- REQUIRED a plugin that shows `vim.notify` outside of the cmdline, such as
 -- `nvim-notify`, `snacks.notifier`, or `mini.notify`.
 --------------------------------------------------------------------------------
 
 local config = {
-	msgKind = {
+	msgKind = { -- existing kinds: https://neovim.io/doc/user/ui.html#ui-messages
 		ignore = {
 			"search_cmd",
+			"return_prompt",
 		},
 		mini = { -- more minimal style when using `snacks.notifier`
 			"bufwrite",
@@ -24,34 +25,36 @@ local config = {
 local ns = vim.api.nvim_create_namespace("ui-hack")
 
 local function attach()
-	---@diagnostic disable-next-line: redundant-parameter
+	---@diagnostic disable-next-line: redundant-parameter incomplete annotation from nvim core
 	vim.ui_attach(ns, { ext_messages = true }, function(event, ...)
 		if event == "msg_history_show" then
 			local msg = "`:messages` is neither supported nor needed anymore. "
-				.. "Just use your notification plugin's history to see past messages."
+				.. "Simply use the history command of your notification plugin to see past messages."
 			vim.notify(msg, vim.log.levels.WARN)
 		end
 		if event ~= "msg_show" then return end
 
-		local kind, content, _replaceLast, _history = ...
-		if kind == "return_prompt" then
+		local kind, content, _replaceLast, _history = ... -- for `msg_show` only https://neovim.io/doc/user/ui.html#ui-messages
+		if kind == "return_prompt" then -- we're still being blocked, so we need to feedkey `<CR>` 
 			local esc = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
 			vim.api.nvim_feedkeys(esc, "n", false)
-			return
 		end
 		if vim.list_contains(config.msgKind.ignore, kind) then return end
 
 		local text = vim.iter(content):fold("", function(acc, chunk) return acc .. chunk[2] end)
-		local notifOpts = { title = kind, icon = config.notification.icon, ft = "text" }
-		if vim.list_contains(config.msgKind.mini, kind) and package.loaded["snacks"] then
-			notifOpts.style = "minimal"
-			if notifOpts.icon then notifOpts.icon = " " .. notifOpts.icon .. " " end
-		end
+		text = vim.trim(text):gsub("^(E%d+):", "[%1]") -- colorize error code when using `snacks`
+
 		local level = vim.log.levels.INFO
 		if kind == "emsg" then level = vim.log.levels.ERROR end
 		if kind == "wmsg" then level = vim.log.levels.WARN end
 
-		vim.schedule(function() vim.notify(vim.trim(text), level, notifOpts) end)
+		local notifOpts = { title = kind, icon = config.notification.icon }
+		if vim.list_contains(config.msgKind.mini, kind) and package.loaded["snacks"] then
+			notifOpts.style = "minimal"
+			if notifOpts.icon then notifOpts.icon = " " .. notifOpts.icon .. " " end
+		end
+
+		vim.schedule(function() vim.notify(text, level, notifOpts) end)
 	end)
 end
 
