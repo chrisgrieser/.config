2db77dda25c8135671e34dcb5da83f5f82062ab5
🤖 automated <cron@job>
2025-04-19 15:37:41 +0200

    iMac Home (4)

--- nvim/lua/personal-plugins/ui-hack.lua
+++ nvim/lua/personal-plugins/ui-hack.lua
@@ -1,5 +1,6 @@
--- INFO redirects cmdline messages to `vim.notify`, silencing `Press Enter to
--- continue` prompts, even with `cmdheight=0`.
+-- INFO This snippet redirects cmdline messages to `vim.notify`, silencing
+-- `Press Enter to continue` prompts, even with `cmdheight=0`.
+
 -- REQUIRED a plugin that shows `vim.notify` outside of the cmdline, such as
 -- `nvim-notify`, `snacks.notifier`, or `mini.notify`.
 --------------------------------------------------------------------------------
@@ -10,7 +11,7 @@ local config = {
 			"search_cmd",
 			"return_prompt",
 		},
-		mini = { -- more minimal style when using `snacks.notifier`
+		mini = { -- more minimal style when `snacks.notifier` is used
 			"bufwrite",
 			"undo",
 		},
@@ -27,34 +28,41 @@ local ns = vim.api.nvim_create_namespace("ui-hack")
 local function attach()
 	---@diagnostic disable-next-line: redundant-parameter incomplete annotation from nvim core
 	vim.ui_attach(ns, { ext_messages = true }, function(event, ...)
+		-- only affect `msg_show` events
 		if event == "msg_history_show" then
-			local msg = "`:messages` is neither supported nor needed anymore. "
+			local msg = "`:messages` is not, supported but t is also not needed anymore. "
 				.. "Simply use the history command of your notification plugin to see past messages."
 			vim.notify(msg, vim.log.levels.WARN)
 		end
 		if event ~= "msg_show" then return end
 
-		local kind, content, _replaceLast, _history = ... -- for `msg_show` only https://neovim.io/doc/user/ui.html#ui-messages
-		if kind == "return_prompt" then -- we're still being blocked, so we need to feedkey `<CR>` 
+		-- ignore & deal with "press enter to continue" prompts
+		local kind, content, _replace, _history = ... -- for `msg_show` only https://neovim.io/doc/user/ui.html#ui-messages
+		if kind == "return_prompt" then -- we're still being blocked, so we need to feedkey `<CR>`
 			local esc = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
 			vim.api.nvim_feedkeys(esc, "n", false)
 		end
 		if vim.list_contains(config.msgKind.ignore, kind) then return end
 
+		-- notification text and options
 		local text = vim.iter(content):fold("", function(acc, chunk) return acc .. chunk[2] end)
 		text = vim.trim(text):gsub("^(E%d+):", "[%1]") -- colorize error code when using `snacks`
 
 		local level = vim.log.levels.INFO
-		if kind == "emsg" then level = vim.log.levels.ERROR end
-		if kind == "wmsg" then level = vim.log.levels.WARN end
-
-		local notifOpts = { title = kind, icon = config.notification.icon }
-		if vim.list_contains(config.msgKind.mini, kind) and package.loaded["snacks"] then
-			notifOpts.style = "minimal"
-			if notifOpts.icon then notifOpts.icon = " " .. notifOpts.icon .. " " end
+		if kind == "emsg" or vim.endswith(kind, "error") or vim.endswith(kind, "err") then
+			level = vim.log.levels.ERROR
+		elseif kind == "wmsg" then
+			level = vim.log.levels.WARN
 		end
 
-		vim.schedule(function() vim.notify(text, level, notifOpts) end)
+		local opts = { title = kind, icon = config.notification.icon }
+		if vim.list_contains(config.msgKind.mini, kind) and package.loaded["snacks"] then
+			opts.style = "minimal"
+			if opts.icon then opts.icon = " " .. opts.icon .. " " end
+		end
+		if vim.startswith(kind, "lua_") then opts.ft = "lua" end
+
+		vim.schedule(function() vim.notify(text, level, opts) end)
 	end)
 end
 
@@ -62,7 +70,7 @@ local function detach() vim.ui_detach(ns) end
 
 --------------------------------------------------------------------------------
 
-attach() -- init
+attach() -- initialize
 local group = vim.api.nvim_create_augroup("ui-hack", { clear = true })
 vim.api.nvim_create_autocmd("CmdlineEnter", { group = group, callback = detach })
 vim.api.nvim_create_autocmd("CmdlineLeave", { group = group, callback = attach })
