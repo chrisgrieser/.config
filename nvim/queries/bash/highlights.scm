be6dafe9fa2a0c9591e76d9b319b30a466c3a333
🤖 automated <cron@job>
2025-04-22 19:54:36 +0200

    iMac Home (6)

--- nvim/queries/bash/highlights.scm
+++ nvim/queries/bash/highlights.scm
@@ -2,6 +2,5 @@
 
 ; highlight `return` and `exit` like return statements in other languages with
 ; `@keyword.return`
-
 ((word) @keyword.return
-        (#any-of? @keyword.return "return" "exit"))
+  (#any-of? @keyword.return "return" "exit" "break"))
--- nvim/queries/swift/highlights.scm
+++ nvim/queries/swift/highlights.scm
@@ -2,5 +2,4 @@
 
 ; `break` and `return` statements with custom highlight, higher priority than
 ; LSP semantic tokens (using 125) https://neovim.io/doc/user/treesitter.html#treesitter-highlight-priority
-((break_statement) @keyword.return (#set! priority 200))
 ("return" @keyword.return (#set! priority 200))
