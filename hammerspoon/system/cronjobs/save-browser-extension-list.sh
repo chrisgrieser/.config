0738c87f14108d4f3a75a1a6873b8092a0364753
🤖 automated <cron@job>
2025-04-29 22:33:14 +0200

    iMac Home (9)

--- hammerspoon/system/cronjobs/save-browser-extension-list.sh
+++ hammerspoon/system/cronjobs/save-browser-extension-list.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env zsh
-# shellcheck disable=2010
 
+# CONFIG
+backup_file="$HOME/.config/+ browser-extension-configs/list-of-extensions.txt"
+
+#───────────────────────────────────────────────────────────────────────────────
+
+mkdir -p "$(dirname "$backup_file")"
+
+# shellcheck disable=2010
 ls "$HOME/Library/Application Support/BraveSoftware/Brave-Browser/Default/Extensions/" |
 	grep -v "Temp" |
 	sed "s|^|https://chrome.google.com/webstore/detail/|" \
-		> "$HOME/.config/.installed-apps-and-packages/browser-extensions.txt"
+		> "$backup_file"
