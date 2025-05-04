f3b39102139af747587af2041180febad5b82a25
🤖 automated <cron@job>
2025-05-04 13:54:03 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/alfred-pass/scripts/copy-pw.sh
+++ Alfred.alfredpreferences/workflows/alfred-pass/scripts/copy-pw.sh
@@ -5,5 +5,11 @@ gpgconf --kill gpg-agent
 
 # ensure gpg key is unlocked, then passing the entry
 entry="$*"
-pass show "$entry" &> /dev/null
-echo -n "$entry"
+export GPG_TTY="" ; pass show "$entry" >/dev/null 2>&1
+
+# shellcheck disable=2181
+if [[ $? -eq 0 ]]; then
+    echo "Password store is unlocked"
+else
+    echo "Password store is locked"
+fi
