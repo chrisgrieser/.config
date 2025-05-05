0bdaebd9ed2290621d4e3e017e8bd4de74aa2804
🤖 automated <cron@job>
2025-05-05 20:54:01 +0200

    iMac Home (1)

--- zsh/config/options.zsh
+++ zsh/config/options.zsh
@@ -9,19 +9,6 @@ setopt GLOB_DOTS            # glob includes dotfiles
 setopt PIPE_FAIL            # tracability: exit if pipeline failed
 setopt NO_BANG_HIST         # don't expand `!`
 
-# colorized 127 error code
-function command_not_found_handler() {
-	print "\e[1;33mCommand not found: \e[1;31m$1\e[0m"
-	return 127
-}
-
-# auto-escape special characters when pasting URLs
-autoload -U url-quote-magic bracketed-paste-magic
-zle -N self-insert url-quote-magic
-zle -N bracketed-paste bracketed-paste-magic
-
-#───────────────────────────────────────────────────────────────────────────────
-
 # HISTORY
 setopt HIST_IGNORE_DUPS
 setopt HIST_REDUCE_BLANKS
@@ -34,10 +21,9 @@ export SAVEHIST=$HISTSIZE
 # don't save in `$ZDOTDIR` as it's in my public dotfile repo
 export HISTFILE="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Dotfolder/zsh_history.zsh"
 
-#───────────────────────────────────────────────────────────────────────────────
 # LANGUAGE
 
-# sets English everywhere, fixes encoding issues
+# set English everywhere, fixes encoding issues
 export LANG="en_US.UTF-8"
 export LC_ALL="$LANG"
 export LC_CTYPE="$LANG"
