d10bda5a0397f578162eb26e4d8aab69683b6247
🤖 automated <cron@job>
2025-05-07 23:35:17 +0200

    iMac Home (2)

--- nvim/lua/plugin-specs/dap-python.lua
+++ nvim/lua/plugin-specs/dap-python.lua
@@ -3,12 +3,14 @@ return {
 	"mfussenegger/nvim-dap-python",
 	dependencies = "mfussenegger/nvim-dap",
 	keys = {
-		-- so it's loaded when used in a python file
+		-- so using `dap` in a python file loads `nvim-dap-python`
 		{ "7", function() require("dap").continue() end, ft = "python", desc = " Continue (py)" },
 	},
 	config = function()
-		-- 1. use the debugypy installation by mason
+		-- 1. use the `debugypy` installation by mason
 		-- 2. deactivate auto-opening the console by redirecting to internal console
+
+		-- TODO change to share dir, PENDING https://github.com/mason-org/mason-registry/issues/9981
 		local debugpyPythonPath = vim.env.MASON .. "/packages/debugpy/venv/bin/python3"
 		require("dap-python").setup(debugpyPythonPath, { console = "internalConsole" })
 	end,
