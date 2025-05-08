cbb5ff814c495ffb35db06baf724bb8365890829
🤖 automated <cron@job>
2025-05-08 14:09:48 +0200

    iMac Home (6)

--- Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/manual-docs-searches/biome-docs-search.js
+++ Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/manual-docs-searches/biome-docs-search.js
@@ -10,6 +10,13 @@ function alfredMatcher(str) {
 	return [camelCaseSeparated, str].join(" ") + " ";
 }
 
+/** @param {string} url @return {string} */
+function httpRequest(url) {
+	const queryURL = $.NSURL.URLWithString(url);
+	const data = $.NSData.dataWithContentsOfURL(queryURL);
+	return $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding).js;
+}
+
 //──────────────────────────────────────────────────────────────────────────────
 
 /** @type {AlfredRun} */
@@ -17,52 +24,53 @@ function alfredMatcher(str) {
 function run() {
 	const docsUrl = "https://api.github.com/repos/biomejs/website/git/trees/main?recursive=1";
 	const baseUrl = "https://biomejs.dev";
-	const docPathRegex = /^src\/content\/docs\/(.*)\.mdx?$/i;
 
-	const workArray = JSON.parse(app.doShellScript(`curl -sL "${docsUrl}"`)).tree.map(
-		(/** @type {{ path: string; }} */ entry) => {
-			const path = entry.path;
+	// `\w{3,}` excludes translations of the docs, which are in folders like
+	// `/ja/` or `/zh-CN/` https://github.com/biomejs/website/tree/main/src/content/docs
+	const docPathRegex = /^src\/content\/docs\/(\w{3,}.*)\.mdx?$/;
 
-			// GUARD
-			const translatedDocs =
-				path.includes("/zh-cn/") || path.includes("/ja/") || path.includes("/pt-br/");
-			const isDocsSite = docPathRegex.test(path) && !path.endsWith("404.md");
-			if (translatedDocs || !isDocsSite) return {};
+	const response = JSON.parse(httpRequest(docsUrl));
+	if (!response) return JSON.stringify({ items: [{ title: "Could not load.", valid: false }] });
 
-			const subsite = path.replace(docPathRegex, "$1");
-			const parts = subsite.split("/");
-			let title = parts.pop() || "??";
-			let category = parts.join("/");
-			let url = `${baseUrl}/${subsite}`;
+	const workArray = response.tree.map((/** @type {{ path: string; }} */ entry) => {
+		const path = entry.path;
+		const [_, subsite] = path.match(docPathRegex) || [];
 
-			if (subsite.endsWith("index")) {
-				title = category;
-				category = "";
-				url = `${baseUrl}/${subsite.slice(0, -5)}`;
-			}
+		if (!subsite) return {};
+		if (path.endsWith("404.md")) return {};
 
-			if (category.endsWith("rules")) {
-				// camelCase to conform to rule-casing the user expects
-				title = title.replace(/-\w/g, (match) => match.slice(-1).toUpperCase());
-			} else {
-				// capitalize
-				title = title.replaceAll("-", " ");
-				title = title.charAt(0).toUpperCase() + title.slice(1); // capitalize
-			}
+		const parts = subsite.split("/");
+		let title = parts.pop() || "??";
+		let category = parts.join("/");
+		let url = `${baseUrl}/${subsite}`;
 
-			return {
-				title: title,
-				subtitle: category,
-				match: alfredMatcher(title),
-				arg: url,
-				mods: {
-					cmd: { arg: title }, // copy entry
-				},
-				quicklookurl: url,
-				uid: title,
-			};
-		},
-	);
+		if (subsite.endsWith("index")) {
+			title = category;
+			category = "";
+			url = `${baseUrl}/${subsite.slice(0, -5)}`;
+		}
+
+		if (category.endsWith("rules")) {
+			// camelCase to conform to rule-casing the user expects
+			title = title.replace(/-\w/g, (match) => match.slice(-1).toUpperCase());
+		} else {
+			// capitalize
+			title = title.replaceAll("-", " ");
+			title = title.charAt(0).toUpperCase() + title.slice(1); // capitalize
+		}
+
+		return {
+			title: title,
+			subtitle: category,
+			match: alfredMatcher(title),
+			arg: url,
+			mods: {
+				cmd: { arg: title }, // copy entry
+			},
+			quicklookurl: url,
+			uid: title,
+		};
+	});
 
 	return JSON.stringify({
 		items: workArray,
