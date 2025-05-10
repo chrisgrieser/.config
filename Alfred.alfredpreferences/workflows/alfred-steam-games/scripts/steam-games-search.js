8a863e9bb3fbfbc172d804714473abfab53fb637
🤖 automated <cron@job>
2025-05-10 14:15:09 +0200

    iMac Home (5)

--- Alfred.alfredpreferences/workflows/alfred-steam-games/scripts/steam-games-search.js
+++ Alfred.alfredpreferences/workflows/alfred-steam-games/scripts/steam-games-search.js
@@ -14,26 +14,25 @@ function httpRequest(url) {
 function ensureCacheFolderExists() {
 	const finder = Application("Finder");
 	const cacheDir = $.getenv("alfred_workflow_cache");
-	if (!finder.exists(Path(cacheDir))) {
-		console.log("Cache directory does not exist and is created.");
-		const cacheDirBasename = $.getenv("alfred_workflow_bundleid");
-		const cacheDirParent = cacheDir.slice(0, -cacheDirBasename.length);
-		finder.make({
-			new: "folder",
-			at: Path(cacheDirParent),
-			withProperties: { name: cacheDirBasename },
-		});
-	}
+	if (finder.exists(Path(cacheDir))) return;
+	console.log("Cache directory does not exist and is created.");
+	const cacheDirBasename = $.getenv("alfred_workflow_bundleid");
+	const cacheDirParent = cacheDir.slice(0, -cacheDirBasename.length);
+	finder.make({
+		new: "folder",
+		at: Path(cacheDirParent),
+		withProperties: { name: cacheDirBasename },
+	});
 }
 
 /** @param {string} path */
 function cacheIsOutdated(path) {
-	const cacheAgeThresholdHours = 24; // CONFIG
+	const cacheAgeThresholdDays = Number.parseInt($.getenv("cache_refresh_days"));
 	ensureCacheFolderExists();
 	const cacheObj = Application("System Events").aliases[path];
 	if (!cacheObj.exists()) return true;
-	const cacheAgeHours = (Date.now() - +cacheObj.creationDate()) / 1000 / 60 / 60;
-	return cacheAgeHours > cacheAgeThresholdHours;
+	const cacheAgeDays = (Date.now() - +cacheObj.creationDate()) / 1000 / 60 / 60 / 24;
+	return cacheAgeDays > cacheAgeThresholdDays;
 }
 
 /** @param {string} filepath @param {string} text */
@@ -59,14 +58,14 @@ function readFile(path) {
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
-	const query = argv[0] || "";
+	const query = (argv[0] || "").toLowerCase();
 
 	// PERF since Alfred cannot handle too many items.
 	const minLength = 4;
 	if (query.length < minLength) {
 		return JSON.stringify({
 			items: [
-				{ title: "Query must be at least " + minLength + " characters long", valid: false },
+				{ title: "Query must be at least " + minLength + " characters long.", valid: false },
 			],
 		});
 	}
@@ -85,15 +84,35 @@ function run(argv) {
 		response = readFile(gameListCachePath);
 	}
 
+	// list of games installed locally
+	const manifestsPath =
+		app.pathTo("home folder") + "/Library/Application Support/Steam/steamapps/";
+	const installedAppids = app
+		.doShellScript(`find "${manifestsPath}" -type f -name "*.acf" -mindepth 1 -maxdepth 1`)
+		.split("\r")
+		.map((line) => {
+			const [_, appid] = line.match(/appmanifest_(\d+)\.acf$/) || ["", ""];
+			return Number.parseInt(appid);
+		});
+
 	// ITEMS
 	const list = JSON.parse(response).applist.apps;
-
-	const games = list.reduce((/** @type {AlfredItem[]} */ acc, /** @type {SteamGame} */ game) => {
-		if (game.name.toLowerCase().includes(query)) {
-			acc.push({ title: game.name, arg: game.appid });
-		}
-		return acc;
-	}, []);
+	const games = list
+		.reduce((/** @type {AlfredItem[]} */ acc, /** @type {SteamGame} */ game) => {
+			const matchesQuery = game.name.toLowerCase().includes(query);
+			if (matchesQuery) {
+				const emoji = installedAppids.includes(game.appid) ? " ✅" : "";
+				acc.push({
+					title: game.name + emoji,
+					arg: game.appid,
+				});
+			}
+			return acc;
+		}, [])
+		.sort( // sort by name length
+			(/** @type {AlfredItem} */ a, /** @type {AlfredItem} */ b) =>
+				a.title.length - b.title.length,
+		);
 
 	return JSON.stringify({ items: games });
 }
