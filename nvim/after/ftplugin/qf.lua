bbf29d79b28eb0c65cbc8ef397a52568c640d7ff
🤖 automated <cron@job>
2025-05-14 14:43:35 +0200

    mini Office (4)

--- nvim/after/ftplugin/qf.lua
+++ nvim/after/ftplugin/qf.lua
@@ -2,6 +2,11 @@ local bkeymap = require("config.utils").bufKeymap
 --------------------------------------------------------------------------------
 
 bkeymap("n", "q", vim.cmd.close, { desc = " Close" })
+
+-- keep <CR> behavior of going to entry, even if <CR> is mapped to something else otherwise
+bkeymap("n", "<CR>", "<CR>")
+
+-- delete entry under cursor from quickfix
 bkeymap("n", "dd", function()
 	local lnum = vim.api.nvim_win_get_cursor(0)[1]
 
@@ -10,8 +15,34 @@ bkeymap("n", "dd", function()
 	vim.fn.setqflist(qf.items, "r") -- "r" = replace = overwrite
 	vim.fn.setqflist({}, "a", { title = qf.title }) -- preserve title of qflist
 
-	vim.api.nvim_win_set_cursor(0, { lnum, 0 })
+	vim.api.nvim_win_set_cursor(0, { math.min(#qf.items, lnum), 0 })
 end, { desc = " Remove quickfix entry" })
 
--- keep <CR> behavior of going to entry, even if <CR> is mapped to something else otherwise
-bkeymap("n", "<CR>", "<CR>")
+
+--------------------------------------------------------------------------------
+
+function _G.myQuickfixText(info)
+	if info.quickfix == 0 then return {} end -- for loclist, use default
+
+	local qf = vim.fn.getqflist { items = true, qfbufnr = true }
+	local ns = vim.api.nvim_create_namespace("qflist")
+	local function highlight(ln, startCol, endCol, hlGroup)
+		vim.hl.range(qf.qfbufnr, ns, hlGroup, { ln - 1, startCol }, { ln, endCol })
+	end
+
+	local lines = {}
+	for ln, item in ipairs(qf.items) do
+		local lnumStr = item.lnum .. " "
+		local filename = vim.fs.basename(vim.api.nvim_buf_get_name(item.bufnr)) .. ":"
+		table.insert(lines, filename .. lnumStr .. vim.trim(item.text))
+
+		vim.schedule(function()
+			highlight(ln, 0, #filename, "qfFileName")
+			highlight(ln, #filename, #filename + #lnumStr, "qfLineNr")
+			highlight(ln, #filename + #lnumStr, -1, "qfText")
+		end)
+	end
+
+	return lines
+end
+vim.opt.quickfixtextfunc = "v:lua.myQuickfixText"
