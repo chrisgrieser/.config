79cbfd17c9e767f653d17b5c18d67428a0d3196a
🤖 automated <cron@job>
2025-06-03 20:27:15 +0200

    iMac Home (1)

--- pandoc/filters/remove-markdown-tasks.lua
+++ pandoc/filters/remove-markdown-tasks.lua
@@ -1,17 +1,16 @@
+-- DOCS https://pandoc.org/lua-filters.html#type-bulletlist
+
 function BulletList(items)
-	local new_items = {}
-
-	for _, item in pairs(items) do
-		if item and type(item) ~= "function" then
-			local para = item[1]
-			local content = pandoc.utils.stringify(para)
-
-			-- Remove task items that start with a checkbox
-			if not content:match("^%s*%[.?%]%s") then table.insert(new_items, item) end
+	local notTasks = {}
+	for _, item in ipairs(items.content) do
+		local first = item[1] -- not sure yet why needed, probably for nested lists
+		if first and first.t == "Plain" then
+			local content = pandoc.utils.stringify(first)
+			local isTask = content:find("☒") or content:find("☐")
+			if not isTask then table.insert(notTasks, first) end
+		else
+			table.insert(notTasks, first)
 		end
 	end
-
-	if #new_items == 0 then return nil end
-
-	return pandoc.BulletList(new_items)
+	return pandoc.BulletList(notTasks)
 end
