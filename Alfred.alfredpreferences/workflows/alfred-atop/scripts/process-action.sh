3b7341a3b139ff5b685b20fa82c95d204524d284
🤖 automated <cron@job>
2025-06-06 11:57:12 +0200

    iMac Home (1)

--- Alfred.alfredpreferences/workflows/alfred-atop/scripts/process-action.sh
+++ Alfred.alfredpreferences/workflows/alfred-atop/scripts/process-action.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=2154
 
 pid="$*"
-name=$(ps -p "$*" -co 'command=')
+name=$(ps -p "$*" -co "command=")
 
 #───────────────────────────────────────────────────────────────────────────────
 
@@ -33,12 +33,12 @@ elif [[ "$mode" == "force killall" ]]; then
 	killall -9 -- "$name" 
 	msg="Force killed all processes with name"
 elif [[ "$mode" == "copy process path" ]]; then
-	process_path=$(ps -p "$pid" -o comm=)
+	process_path=$(ps -p "$pid" -o "command=")
 	if [[ -z "$process_path" ]]; then
 		msg="⚠️ Could not find process path."
 	else
 		echo -n "$process_path" | pbcopy
-		msg="✅ Copied process path \"$process_path\""
+		msg="✅ Copied process path: \"$process_path\""
 	fi
 	echo -n "$msg" # Alfred notification
 	return 0
