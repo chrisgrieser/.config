ebe8eb5890bde8d11169fe98253d69d704e5e6a4
🤖 automated <cron@job>
2025-06-11 12:23:24 +0200

    mini Office (2)

--- hammerspoon/apps/spotify.lua
+++ hammerspoon/apps/spotify.lua
@@ -7,9 +7,9 @@ local aw = hs.application.watcher
 
 -- auto-pause/resume Spotify on launch/quit of apps with sound
 M.aw_spotify = aw.new(function(appName, eventType)
+	if not env.isAtHome then return end
 	if
 		not u.screenIsUnlocked()
-		or not env.isAtHome
 		or env.isProjector()
 		or not (hs.fnutils.contains(u.videoAndAudioApps, appName))
 		or not (eventType == aw.launched or eventType == aw.terminated)
@@ -18,6 +18,7 @@ M.aw_spotify = aw.new(function(appName, eventType)
 	end
 	if M.spotify_task and M.spotify_task:isRunning() then M.spotify_task:terminate() end
 
+	-- using Alexa virtual trigger since it's more reliable than `spotify_player`
 	local action = eventType == aw.launched and "pause" or "play"
 	local alexaTrigger = os.getenv("HOME")
 		.. "/Library/Mobile Documents/com~apple~CloudDocs/Dotfolder/alexa-virtual-trigger"
