6f1096b53f4a17584adbda0315eeca3cbfc51e47
🤖 automated <cron@job>
2025-06-23 16:02:23 +0200

    mini Office (1)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pandoc-md2docx.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pandoc-md2docx.sh
@@ -4,7 +4,8 @@ md_file="$*"
 #───────────────────────────────────────────────────────────────────────────────
 
 # CONFIG
-word_file="${md_file%\.md}_$(date +%Y-%m-%d)_CG.docx"
+output_location="$HOME/Desktop/"
+word_file="$output_location/${md_file%\.md}_$(date +%Y-%m-%d)_CG.docx"
 
 #───────────────────────────────────────────────────────────────────────────────
 # PREPARE
@@ -21,33 +22,10 @@ fi
 #───────────────────────────────────────────────────────────────────────────────
 # PANDOC
 
-cd "$(dirname "$md_file")" # so `--resource-path` is set
+cd "$(dirname "$md_file")" # so `--resource-path` works correctly is set
 
 # INFO pandoc's --data-dir for the `defaults` file defined in .zshenv
 pandoc "$md_file" --output="$word_file" --defaults="md2docx" 2>&1
 
 open -R "$word_file"
 open "$word_file"
-
-#───────────────────────────────────────────────────────────────────────────────
-# INSERT LINE BREAKS IN TABLES
-# replace `¶` with `^l`, which is the line break token in MS Word
-# INFO pandoc does not support line breaks in tables https://pandoc.org/MANUAL.html#extension-pipe_tables
-# REQUIRED longform compile step that turns "<br>" into "¶"
-
-osascript -e '
-	tell application "Microsoft Word"
-		set i to 0
-		repeat until active document exists
-			delay 0.1
-			set i to i + 1
-			if i > 120 then return
-		end repeat
-		activate
-
-		set myFind to find object of text object of active document
-		execute find myFind find text "¶" replace with "^l" replace replace all
-
-		save active document
-	end tell
-' &> /dev/null
