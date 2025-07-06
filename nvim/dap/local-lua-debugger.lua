76c6526394faee0ef06d3dc78aa1bdfd9dab6c90
🤖 automated <cron@job>
2025-07-06 21:33:03 +0200

    iMac Home (3)

--- nvim/dap/local-lua-debugger.lua
+++ nvim/dap/local-lua-debugger.lua
@@ -1,12 +1,17 @@
--- DOCS https://codeberg.org/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#local-lua-debugger-vscode
+-- DOCS 
+-- https://codeberg.org/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#local-lua-debugger-vscode
+-- https://github.com/tomblind/local-lua-debugger-vscode
 --------------------------------------------------------------------------------
 
-local adapterDir = vim.env.MASON .. "/packages/local-lua-debugger-vscode"
+local adapterDir = vim.env.MASON .. "/share/local-lua-debugger-vscode"
+local luaVersion = "luajit" ---@type "luajit"|"lua5.4" -- luajit used by nvim, lua5.4 by hammerspoon
+
+--------------------------------------------------------------------------------
 
 require("dap").adapters["local-lua"] = {
 	type = "executable",
 	command = "node",
-	args = { adapterDir .. "/extension/extension/debugAdapter.js" }, -- SIC 2x extension
+	args = { adapterDir .. "/extension/debugAdapter.js" },
 	enrich_config = function(config, on_config)
 		if not config["extensionPath"] then
 			local c = vim.deepcopy(config)
@@ -20,12 +25,12 @@ require("dap").adapters["local-lua"] = {
 
 require("dap").configurations.lua = {
 	{
-		name = "Launch file",
+		name = ("local-lua-debugger: Launch file (%s)"):format(luaVersion),
 		type = "local-lua", -- match with `dap.adapters["local-lua"]`
 		request = "launch",
 		cwd = "${workspaceFolder}",
 		program = {
-			lua = "luajit",
+			lua = luaVersion,
 			file = "${file}",
 		},
 		args = {},
