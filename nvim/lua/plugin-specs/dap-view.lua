a26d01e6930bd4bcf526fadb250888cefaf32bd1
🤖 automated <cron@job>
2025-07-06 08:33:04 +0200

    iMac Home (8)

---
+++ nvim/lua/plugin-specs/dap-view.lua
@@ -0,0 +1,30 @@
+-- DOCS https://igorlfs.github.io/nvim-dap-view/configuration
+-- Alternative: https://github.com/miroshQa/debugmaster.nvim
+--------------------------------------------------------------------------------
+
+return {
+	"igorlfs/nvim-dap-view",
+	dependencies = "mfussenegger/nvim-dap",
+	keys = {
+		{ "<leader>du", function() require("dap-view").toggle() end, desc = "󱂬 Toggle UI" },
+		{ "<leader>db", function() require("dap-view").toggle() end, desc = "󱂬 Toggle UI" },
+	},
+
+	---@module "dap-view"
+	---@type dapview.Config
+	opts = {
+		auto_toggle = false,
+		winbar = {
+			-- stylua: ignore
+			sections = { "scopes", "breakpoints", "watches", "exceptions", "threads", "repl" },
+			default_section = "scopes",
+		},
+		windows = { height = 12, position = "below" },
+	},
+	config = function(_, opts)
+		require("dap-view").setup(opts)
+
+		-- AUTO-CLOSE THE UI
+		require("dap").listeners.after.disconnect["dap-view"] = require("dap-view").close
+	end,
+}
