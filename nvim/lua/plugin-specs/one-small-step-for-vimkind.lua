0668c1db7a6eac1e9cc6bfc6f0aa3cb7d78cc5c6
🤖 automated <cron@job>
2025-07-08 12:31:23 +0200

    mini Office (5)

--- nvim/lua/plugin-specs/one-small-step-for-vimkind.lua
+++ nvim/lua/plugin-specs/one-small-step-for-vimkind.lua
@@ -1,17 +1,28 @@
+-- DOCS https://github.com/jbyuki/one-small-step-for-vimkind/
+--------------------------------------------------------------------------------
+
 return {
 	"jbyuki/one-small-step-for-vimkind",
-	commit = "d9f8325", -- PENDING https://github.com/jbyuki/one-small-step-for-vimkind/issues/81
+	commit = "9895b68",
 
-	dependencies = "mfussenegger/nvim-dap", -- loads dap config as well
-	config = function() end,
+
+	config = function()
+		require("dap").adapters.nlua = function(callback, config)
+			callback {
+				type = "server",
+				host = config.host or "127.0.0.1",
+				port = config.port or 8086,
+			}
+		end
+	end,
 	keys = {
 		{
 			"7",
 			function()
 				if require("dap").status() == "" then
-					-- INFO this debugger the only one that needs manual starting, other
+					-- this debugger the only one that needs manual starting, other
 					-- debuggers start with `continue` by themselves
-					require("osv").run_this() -- starts single file
+					require("osv").run_this()
 				else
 					require("dap").continue()
 				end
