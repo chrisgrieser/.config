a2e7b69c4384c7e046f866dd2df5ab03604d7b39
🤖 automated <cron@job>
2025-07-12 13:30:29 +0200

    iMac Home (4)

--- Alfred.alfredpreferences/workflows/alfred-neovim-utilities/scripts/search-nvim-plugins.js
+++ Alfred.alfredpreferences/workflows/alfred-neovim-utilities/scripts/search-nvim-plugins.js
@@ -18,23 +18,66 @@ function httpRequest(url) {
 	return $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding).js;
 }
 
-// *   [smjonas/inc-rename.nvim (⭐601)](https://github.com/smjonas/inc-rename.nvim) - Provides an incremental LSP rename command based on Neovim's command-preview feature.
-const mdLinkRegex = /\[(.+?) \(⭐(.+?)\)\]\((.+?)\) - (.*)/;
-
 const fileExists = (/** @type {string} */ filePath) => Application("Finder").exists(Path(filePath));
+
+/** @param {string|Date} date @return {string} relative date */
+function relativeDate(date) {
+	const absDate = typeof date === "string" ? new Date(date) : date;
+	const deltaMins = (Date.now() - absDate.getTime()) / 1000 / 60;
+	/** @type {"year"|"month"|"week"|"day"|"hour"|"minute"} */
+	let unit;
+	let delta;
+	if (deltaMins < 60) {
+		unit = "minute";
+		delta = Math.floor(deltaMins);
+	} else if (deltaMins < 60 * 24) {
+		unit = "hour";
+		delta = Math.floor(deltaMins / 60);
+	} else if (deltaMins < 60 * 24 * 7) {
+		unit = "day";
+		delta = Math.floor(deltaMins / 60 / 24);
+	} else if (deltaMins < 60 * 24 * 7 * 4) {
+		unit = "week";
+		delta = Math.floor(deltaMins / 60 / 24 / 7);
+	} else if (deltaMins < 60 * 24 * 7 * 4 * 12) {
+		unit = "month";
+		delta = Math.floor(deltaMins / 60 / 24 / 7 / 4);
+	} else {
+		unit = "year";
+		delta = Math.floor(deltaMins / 60 / 24 / 7 / 4 / 12);
+	}
+	const formatter = new Intl.RelativeTimeFormat("en", { style: "narrow", numeric: "auto" });
+	return formatter.format(-delta, unit);
+}
 
 //──────────────────────────────────────────────────────────────────────────────
 
-// INFO Searching awesome-neovim instead of neovimcraft or dotfyle, since the
-// the latter two only scrape awesome-neovim anyway
+// INFO Using the crawler result of `store.nvim`, since it is it includes more
+// plugins that awesome-neovim, and neovimcraft and dotfyle only include plugins
+// that are in the awesome-neovim
+const storeNvimList =
+	"https://gist.githubusercontent.com/alex-popov-tech/93dcd3ce38cbc7a0b3245b9b59b56c9b/raw/a4859fe73ddda67e4fb86a5cd7bc30e9889cb787/store.nvim-repos.json";
+
+/** @typedef {Object} StoreNvimRepo
+ * @property {string} full_name
+ * @property {string} description
+ * @property {string} homepage
+ * @property {string} html_url
+ * @property {number} stargazers_count
+ * @property {number} watchers_count
+ * @property {number} fork_count
+ * @property {string} updated_at - ISO‑8601 timestamp
+ * @property {string[]} topics
+ */
+
+//──────────────────────────────────────────────────────────────────────────────
 
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
 	// determine local plugins
 	const pluginInstallPath = $.getenv("plugin_installation_path");
-	/** @type {string[]} */
-	let installedPlugins = [];
+	let /** @type {string[]} */ installedPlugins = [];
 	if (fileExists(pluginInstallPath)) {
 		const shellCmd = `cd "${pluginInstallPath}" && grep --only-matching --no-filename --max-count=1 "http.*" ./*/.git/config`;
 		installedPlugins = app
@@ -46,38 +89,29 @@ function run() {
 			});
 	}
 
-	// Using `trackawesomelist` over the raw markdown, as it includes star count
-	const awesomeNeovimList =
-		"https://raw.githubusercontent.com/trackawesomelist/trackawesomelist/main/content/rockerBOO/awesome-neovim/readme/README.md";
+	const pluginsArr = JSON.parse(httpRequest(storeNvimList)).repositories.map(
+		(/** @type {StoreNvimRepo} */ repo) => {
+			const { full_name, description, html_url, stargazers_count, updated_at } = repo;
+			const [author, name] = full_name.split("/");
+			const installedIcon = installedPlugins.includes(full_name) ? " ✅" : "";
+			const updated = relativeDate(updated_at);
+			const subtitle = ["⭐ " + stargazers_count, author, updated, description].join("  ·  ");
 
-	/** @type {AlfredItem[]} */
-	const pluginsArr = (httpRequest(awesomeNeovimList)
-		.split("\n")
-		.reduce((/** @type {AlfredItem[]} */acc, /** @type {string} */ line) => {
-			if (!line.startsWith("*   [") || !line.includes("/")) return acc;
-
-			const [_, repo, stars, url, desc] = line.match(mdLinkRegex) || [];
-			if (!repo || !url) return {};
-			const [author, name] = repo.split("/");
-			if (!name) return {};
-			const displayName = name.replaceAll("\\_", "_");
-			const installedIcon = installedPlugins.includes(repo) ? " ✅" : "";
-			const subtitle = ["⭐ " + stars, author, desc].join("  ·  ");
-
-			acc.push({
-				title: displayName + installedIcon,
-				match: alfredMatcher(repo),
+			return {
+				title: name + installedIcon,
+				match: alfredMatcher(full_name),
 				subtitle: subtitle,
-				arg: url,
+				arg: html_url,
 				mods: {
 					cmd: { arg: repo },
 				},
-				quicklookurl: url,
+				quicklookurl: html_url,
 				uid: repo,
-			});
-			return acc
-		}, []));
+			};
+		},
+	);
 
+	console.log("plugin count:", pluginsArr.length);
 	return JSON.stringify({
 		items: pluginsArr,
 		cache: { seconds: 300, loosereload: true }, // faster, to update install icons
