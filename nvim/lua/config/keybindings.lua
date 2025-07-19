9bc82b40786788f91b43e6f0876e6b2b4e38f7c7
🤖 automated <cron@job>
2025-07-19 13:20:16 +0200

    iMac Home (2)

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -321,11 +321,6 @@ do
 	keymap("n", "<PageUp>", function() scrollLspWin(-5) end, { desc = "↑ Scroll LSP win" })
 end
 
-keymap("n", "<leader>H", function()
-	local peek = require("personal-plugins.misc")
-	peek.peek_definition_code("@function.outer", "textobjects")
-end, { desc = " Peek function" })
-
 --------------------------------------------------------------------------------
 
 -- Open first URL in file
