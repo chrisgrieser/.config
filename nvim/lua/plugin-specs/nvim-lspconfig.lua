1113900664d4d4403dca831247ad383edea4de7f
🤖 automated <cron@job>
2025-07-20 11:50:14 +0200

    iMac Home (2)

--- nvim/lua/plugin-specs/nvim-lspconfig.lua
+++ nvim/lua/plugin-specs/nvim-lspconfig.lua
@@ -3,7 +3,7 @@ return {
 
 	-- No need to load the plugin—since we just want its configs, adding the
 	-- it to the `runtimepath` is enough.
-	lazy = true,
+	lazy = false,
 	init = function()
 		local lspConfigPath = require("lazy.core.config").options.root .. "/nvim-lspconfig"
 
