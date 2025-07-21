36fb315b00bca5f229411f29435643b0a232a857
🤖 automated <cron@job>
2025-07-21 11:20:13 +0200

    iMac Home (3)

--- nvim/after/ftplugin/bib.lua
+++ nvim/after/ftplugin/bib.lua
@@ -10,18 +10,44 @@ vim.opt_local.formatoptions:append { r = true }
 
 -- since treesitter has not symbol support for bibtex, we just use a small
 -- function to search the buffer for citekeys
-vim.keymap.set("n", "gs", function()
-	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-	local citekeys = vim.iter(lines)
-		:filter(function(line) return line:find("^@") end)
-		:map(function(line) return line:match("^@.*{(.*),") end)
-		:totable()
-	vim.ui.select(citekeys, {
-		prompt = "Select citekey:",
-		format_item = function(citekey) return "@" .. citekey end,
-		kind = "bibtex.citekey-search",
-	}, function(citekey)
-		if not citekey then return end
-		vim.fn.search(citekey .. ",")
-	end)
-end, { buffer = true })
+vim.defer_fn(function()
+	vim.keymap.set("n", "gs", function()
+		local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+		local citekeys = vim.iter(lines)
+			:filter(function(line) return line:find("^@") end)
+			:map(function(line) return line:match("^@.*{(.*),") end)
+			:totable()
+		vim.ui.select(citekeys, {
+			prompt = "Select citekey:",
+			format_item = function(citekey) return "@" .. citekey end,
+			kind = "bibtex.citekey-search",
+		}, function(citekey)
+			if not citekey then return end
+			vim.fn.search(citekey .. ",")
+		end)
+	end, { buffer = true })
+end, 1)
+
+--------------------------------------------------------------------------------
+
+---checks a .bib file for duplicate citekeys and reports them via `vim.notify`
+---when any are found. Does nothing, if there are no duplicate citekeys.
+local function checkForDuplicateCitekeys()
+	local duplCitekeys = ""
+	local citekeysInFile = {}
+
+	for _, line in pairs(vim.api.nvim_buf_get_lines(0, 0, -1, true)) do
+		local citekey = line:match("^@.-{(.*),")
+		if citekey then
+			if not citekeysInFile[citekey] then
+				citekeysInFile[citekey] = 1
+			else
+				duplCitekeys = duplCitekeys .. "\n" .. "- " .. citekey
+			end
+		end
+	end
+	if duplCitekeys ~= "" then
+		vim.notify(duplCitekeys, vim.log.levels.WARN, { title = "Duplicate Citekeys" })
+	end
+end
+checkForDuplicateCitekeys()
