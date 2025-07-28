8430dacd356db6421767f68ecb18820ba566c8d1
🤖 automated <cron@job>
2025-07-28 12:13:04 +0200

    iMac Home (6)

--- Alfred.alfredpreferences/workflows/alfred-atop/scripts/switch-dns.sh
+++ Alfred.alfredpreferences/workflows/alfred-atop/scripts/switch-dns.sh
@@ -4,15 +4,18 @@ dns_address_1="$1"
 dns_address_2="$2"
 
 # switch on CURRENT available network services to prevent unnecessary change
-scutil --nwi | awk -F': ' '/Network interfaces/ {print $2;}' | sed 's/ /\n/g' | while read interface; do
-	# only change the Ethernet interfaces
-	if [[ $interface == en* ]]; then
-		networksetup -listnetworkserviceorder | awk "/Device: $interface)/{print a}{a=\$0}" | sed 's/([0-9]*)//g' | while read networkservicename; do
-			if [[ -z "$dns_address_1" && -z "$dns_address_2" ]]; then
-				networksetup -setdnsservers $networkservicename empty
-			else
-				networksetup -setdnsservers $networkservicename "$dns_address_1" "$dns_address_2"
-			fi
-		done
-	fi
-done
+scutil --nwi |
+	awk -F': ' '/Network interfaces/ {print $2;}' |
+	sed 's/ /\n/g' |
+	while read -r interface; do
+		networksetup -listnetworkserviceorder |
+			awk "/Device: $interface)/{print a}{a=\$0}" |
+			sed 's/([0-9]*)//g' |
+			while read -r networkservicename; do
+				if [[ -z "$dns_address_1" && -z "$dns_address_2" ]]; then
+					networksetup -setdnsservers "$networkservicename" empty
+				else
+					networksetup -setdnsservers "$networkservicename" "$dns_address_1" "$dns_address_2"
+				fi
+			done
+	done
