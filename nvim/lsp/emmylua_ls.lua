c3b32572bed246fb80b9242bdacb7dc51be2fb56
🤖 automated <cron@job>
2025-08-01 17:51:27 +0200

    iMac Home (2)

---
+++ nvim/lsp/emmylua_ls.lua
@@ -0,0 +1,29 @@
+-- DOCS https://github.com/EmmyLuaLs/emmylua-analyzer-rust/blob/main/docs/config/emmyrc_json_EN.md
+--------------------------------------------------------------------------------
+-- TODO replace even lazydev.nvim? https://www.reddit.com/r/neovim/comments/1mdtr4g/emmylua_ls_is_supersnappy/
+
+return {
+	on_attach = function(client)
+		-- disable formatting in favor of `stylua`
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
+		-- FIX folds too much on kind `comment`
+		client.server_capabilities.foldingRangeProvider = false
+	end,
+	settings = {
+		Lua = {
+			completion = { postfix = "." }, -- useful for `table.insert` and the like
+			signature = { detailSignatureHelper = true },
+			diagnostics = {
+				disable = {
+					"type-not-found", -- PENDING https://github.com/folke/lazydev.nvim/issues/86
+				},
+			},
+			strict = {
+				requirePath = true,
+				typeCall = true,
+				arrayIndex = false, -- too strict, checks for sparse arrays everywhere
+			},
+		},
+	},
+}
