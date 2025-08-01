f9cf62bf36933d0b93fc941d196768d627e7a7ed
🤖 automated <cron@job>
2025-08-01 12:51:04 +0200

    iMac Home (3)

--- nvim/lsp/biome.lua
+++ nvim/lsp/biome.lua
@@ -1,3 +1,7 @@
+-- FIX
+-- `biome` does not attach on the first buffer of startup, due to the `root_dir`
+-- function by nvim-lspconfig using `cwd`, which is running before auto-rooting
+-- automcmds become active. Simply using the root markers fixes that.
 return {
-	workspace_required = false,
+	root_markers = { "biome.json", "biome.jsonc" },
 }
