47aadb7f27c698f84440f9239f6e321e44710e66
🤖 automated <cron@job>
2025-08-02 22:51:25 +0200

    iMac Home (16)

--- Alfred.alfredpreferences/workflows/misc-utilities/scripts/save-diaglogue-front-finder-win.js
+++ Alfred.alfredpreferences/workflows/misc-utilities/scripts/save-diaglogue-front-finder-win.js
@@ -7,9 +7,8 @@ app.includeStandardAdditions = true;
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	const se = Application("System Events");
-
 	try {
+		const se = Application("System Events");
 		const selection = Application("Finder").selection()[0];
 		const frontWin = Application("Finder").insertionLocation();
 		const path = decodeURIComponent((selection || frontWin).url().slice(7));
@@ -19,7 +18,7 @@ function run() {
 		delay(0.2);
 
 		se.keystroke("v", { using: ["command down"] });
-		delay(0.1);
+		delay(0.2);
 		se.keyCode(36); // return-key
 	} catch (_error) {
 		return "No Finder window open or no selection.";
