2c8ade8b8daf6ac83f0614adfd7fb49ef6678142
🤖 automated <cron@job>
2025-08-02 11:21:26 +0200

    iMac Home (1)

--- nvim/lua/plugin-specs/justice.lua
+++ nvim/lua/plugin-specs/justice.lua
@@ -4,16 +4,18 @@ return {
 		{ "<leader>j", function() require("justice").select() end, desc = "󰖷 Just" },
 	},
 	opts = {
-		highlights = {
-			quickSelect = "StandingOut",
-		},
-		keymaps = {
-			runFirstRecipe = "j", -- <leader>jj = run first recipe
-			dontUseForQuickKey = { "-", "_" },
-		},
 		recipes = {
 			terminal = {
 				name = { "release" }, -- my release scripts usually require version numbers as input
+			},
+		},
+		window = {
+			highlights = {
+				quickSelect = "StandingOut",
+			},
+			keymaps = {
+				runFirstRecipe = "j", -- <leader>jj = run first recipe
+				dontUseForQuickKey = { "-", "_" },
 			},
 		},
 	},
