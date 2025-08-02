ad3f0476dcce7291efb60630856ce5ad5f25b083
🤖 automated <cron@job>
2025-08-02 14:51:26 +0200

    iMac Home (8)

--- nvim/lua/plugin-specs/lazydev-or-luvit-types.lua
+++ nvim/lua/plugin-specs/lazydev-or-luvit-types.lua
@@ -1,24 +1,21 @@
-local lazydev = {
-	"folke/lazydev.nvim",
-	ft = "lua",
-	opts = {
-		library = {
-			-- Load luvit types when the "word" `vim.uv` is found
-			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+return {
+	{
+		"folke/lazydev.nvim",
+		cond = not vim.g.use_emmylua,
+		ft = "lua",
+		opts = {
+			library = {
+				-- Load luvit types when the "word" `vim.uv` is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 
-			-- global debugging function `Chainsaw`
-			{ path = "nvim-chainsaw/lua/chainsaw/nvim-debug.lua", words = { "Chainsaw" } },
+				-- global debugging function `Chainsaw`
+				{ path = "nvim-chainsaw/lua/chainsaw/nvim-debug.lua", words = { "Chainsaw" } },
+			},
 		},
 	},
+	{
+		"Bilal2453/luvit-meta",
+		lazy = false, -- only need it locally for emmylua to have vim.uv typing
+		cond = vim.g.use_emmylua,
+	},
 }
-
-local luvitTypes = {
-	"Bilal2453/luvit-meta",
-	lazy = false, -- only need it locally for emmylua to have vim.uv typing
-}
-
-if vim.g.use_emmylua then
-	return luvitTypes
-else
-	return lazydev
-end
