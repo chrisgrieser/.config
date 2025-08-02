93fb64550dbe32a2f0020bd0652628e2eb32bb03
🤖 automated <cron@job>
2025-08-02 15:21:26 +0200

    iMac Home (3)

--- nvim/lua/plugin-specs/lsp-endhints.lua
+++ nvim/lua/plugin-specs/lsp-endhints.lua
@@ -4,6 +4,10 @@ return {
 	keys = {
 		{ "<leader>oh", function() require("lsp-endhints").toggle() end, desc = "󰑀 Endhints" },
 	},
+	config = function(_, opts)
+		-- FIX for emmylua_ls not loading on startup
+		vim.defer_fn(function() require("lsp-endhints").setup(opts) end, 100)
+	end,
 	opts = {
 		label = {
 			sameKindSeparator = " ",
