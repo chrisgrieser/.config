65d1e71df3f7fef03b556775bd6134e9dc2c9c38
🤖 automated <cron@job>
2025-08-02 11:51:30 +0200

    iMac Home (2)

--- Alfred.alfredpreferences/workflows/misc-utilities/scripts/searchlink.sh
+++ Alfred.alfredpreferences/workflows/misc-utilities/scripts/searchlink.sh
@@ -4,11 +4,11 @@
 
 query="$1"
 query_enc=$(osascript -l JavaScript -e "encodeURIComponent('$query')")
-region=${region:-"us-en"} # Alfred variable
-ddgr_html_url="https://html.duckduckgo.com/html?q=$query_enc&kl=$region"
+ddgr_html_url="https://html.duckduckgo.com/html?kl=us-en&q=$query_enc"
+echo "🪚 ddgr_html_url: $ddgr_html_url" >&2
 
 first_result=$(
-	curl --silent "$ddgr_html_url" |
+	curl --user-agent="searchlink" --silent "$ddgr_html_url" |
 		grep --after-context=1 --max-count=1 "result__url" |
 		tail -n1 |
 		sed -E 's/^ *| *$//g'
