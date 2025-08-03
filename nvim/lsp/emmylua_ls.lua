a49dd1ad08823ac302f9dbaf2ac960523965f0f4
🤖 automated <cron@job>
2025-08-03 15:51:25 +0200

    iMac Home (1)

--- nvim/lsp/emmylua_ls.lua
+++ nvim/lsp/emmylua_ls.lua
@@ -7,38 +7,23 @@ return {
 		client.server_capabilities.documentFormattingProvider = false
 		client.server_capabilities.documentRangeFormattingProvider = false
 	end,
-	-- WARN setting any settings makes emmylua break half the time
-	-- https://github.com/EmmyLuaLs/emmylua-analyzer-rust/issues/678
-	-- settings = {
-	-- 	Lua = {
-	-- 		diagnostics = {
-	-- 			disable = { "unnecessary-if" }, -- buggy rule
-	-- 		},
-	-- 		completion = {
-	-- 			callSnippet = true,
-	-- 		},
-	-- 		signature = {
-	-- 			detailSignatureHelper = true,
-	-- 		},
-	-- 		strict = {
-	-- 			requirePath = true,
-	-- 			typeCall = true,
-	-- 		},
-	-- 	},
-	-- },
+	settings = {
+		Lua = {
+			diagnostics = {
+				disable = {
+					"unnecessary-if", -- buggy rule
+				},
+			},
+			completion = {
+				callSnippet = true,
+			},
+			signature = {
+				detailSignatureHelper = true,
+			},
+			strict = {
+				requirePath = true,
+				typeCall = true,
+			},
+		},
+	},
 }
-
---------------------------------------------------------------------------------
-
--- -- reproduction for https://github.com/EmmyLuaLs/emmylua-analyzer-rust/issues/678
--- return {
--- 	cmd = { "emmylua_ls" },
--- 	filetypes = { "lua" },
--- 	root_markers = {
--- 		".luarc.json",
--- 		".emmyrc.json",
--- 		".luacheckrc",
--- 		".git",
--- 	},
--- 	workspace_required = false,
--- }
