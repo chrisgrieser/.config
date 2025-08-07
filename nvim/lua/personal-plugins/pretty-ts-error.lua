fc2890cb803f6a0d68f04a0fc347725f9fb65e4e
🤖 automated <cron@job>
2025-08-07 14:44:18 +0200

    Home (3 files)

--- nvim/lua/personal-plugins/pretty-ts-error.lua
+++ nvim/lua/personal-plugins/pretty-ts-error.lua
@@ -1,16 +1,17 @@
 local M = {}
 --------------------------------------------------------------------------------
 
--- requ
+-- REQUIREMENTS
+-- * `ts_ls` LSP
+-- * nvim 0.10+
+-- * (recommended) `biome` or `prettier` for formatted codeblocks
+-- * (recommended) `TSInstall markdown markdown_inline` for better highlighting
+-- * (recommended) `render-markdown.nvim` or `markview.nvim` for better highlighting
 
-function M.prettyTsError()
-	local notifyOpts = { icon = "", title = "ts_ls" }
-	local diag = vim.diagnostic.get_next()
-	if not diag or diag.source ~= "typescript" then
-		vim.notify("No diagnostic found.", vim.log.levels.WARN, notifyOpts)
-		return
-	end
+--------------------------------------------------------------------------------
 
+---@param diag vim.Diagnostic
+local function show(diag)
 	-- split diagnostic into codeblocks
 	-- example: `Type '{ title: number; subtitle: string; mods: { cmd: { arg: string; }; ctrl: { arg: string; }; }; arg: string; variables: { address: string; url: string; coordinates: string; }; }[]' is not assignable to type 'AlfredItem[]'.`
 	local msg = diag
@@ -50,15 +51,40 @@ function M.prettyTsError()
 	local _bufnr, winid = vim.lsp.util.open_floating_preview(lines, "markdown", {
 		close_events = { "CursorMoved", "CursorMovedI", "BufHidden", "LspDetach" },
 		wrap = false,
+
 		title = title,
 		height = #lines,
-		focus = true,
 	})
 	-- FIX value above being ignored
 	vim.api.nvim_win_set_config(winid, {
 		title = title,
 		height = #lines,
 	})
+end
+
+--------------------------------------------------------------------------------
+
+function M.select()
+	-- get diagnostics
+	local row = vim.api.nvim_win_get_cursor(0)[1] - 1
+	local diags = vim.diagnostic.get(0, { lnum = row })
+	diags = vim.tbl_filter(function(d) return d.source == "typescript" end, diags)
+
+	if #diags == 0 then
+		vim.notify("No diagnostic found.", vim.log.levels.WARN, { icon = "", title = "ts_ls" })
+		return
+	elseif #diags == 1 then
+		show(diags[1]) -- auto-select when only one
+		return
+	end
+
+	vim.ui.select(diags, {
+		prompt = " Select diagnostic: ",
+		format_item = function(d) return d.message:sub(0, 30) end,
+	}, function(selection)
+		if not selection then return end
+		show(selection)
+	end)
 end
 
 --------------------------------------------------------------------------------
