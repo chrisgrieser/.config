f0d2b2f1aba0a34b6b342fc73e3423e71beb616c
🤖 automated <cron@job>
2025-08-07 09:44:18 +0200

    Home (9 files)

--- nvim/lsp/ts_ls.lua
+++ nvim/lsp/ts_ls.lua
@@ -9,27 +9,13 @@ local config = {
 		},
 	},
 	handlers = {
-		["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
-			if result.diagnostics == nil then return end
+		["textDocument/publishDiagnostics"] = function(_, result, ctx, _config)
+			-- remove "File is a CommonJS module; it may be converted to an ES module."
+			result.diagnostics = vim.iter(result.diagnostics)
+				:filter(function(diag) return diag.code ~= 80001 end)
+				:totable()
 
-			-- ignore some tsserver diagnostics
-			local idx = 1
-			while idx <= #result.diagnostics do
-				local entry = result.diagnostics[idx]
-
-				local formatter = require("format-ts-errors")[entry.code]
-				entry.message = formatter and formatter(entry.message) or entry.message
-
-				-- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
-				if entry.code == 80001 then
-					-- { message = "File is a CommonJS module; it may be converted to an ES module.", }
-					table.remove(result.diagnostics, idx)
-				else
-					idx = idx + 1
-				end
-			end
-
-			vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+			vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx)
 		end,
 	},
 	settings = {
--- nvim/lua/plugin-specs/format-ts-errors.lua
+++ nvim/lua/plugin-specs/format-ts-errors.lua
@@ -1,7 +1,24 @@
----@module "lazy.core.specs"
+---@module "lazy.types"
 ---@type LazyPluginSpec
 return {
 	"davidosomething/format-ts-errors.nvim",
-	ft = { "typescript", "javascript" },
-	opts = {},
+	opts = {
+		add_markdown = true,
+		start_indent_level = 0,
+	},
+	config = function (_, opts)
+		require("format-ts-errors").setup(opts)
+
+		vim.lsp.config("ts_ls", {
+			handlers = {
+				["textDocument/publishDiagnostics"] = function() 
+					:map(function(diag)
+					local formatter = require("format-ts-errors")[diag.code]
+					if formatter then diag.message = formatter(diag.message) end
+					return diag
+				end)
+				end,
+			}
+		})
+	end
 }
