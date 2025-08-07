0da531a31e36e30f7d7f20ca053420c9215ba220
🤖 automated <cron@job>
2025-08-07 18:19:36 +0200

    Home (nvim)

--- nvim/lua/plugin-specs/colorschemes.lua
+++ nvim/lua/plugin-specs/colorschemes.lua
@@ -1,10 +1,16 @@
--- for separation placed in a different folder
 local darkScheme = require("colorschemes." .. vim.g.darkColor)
 local lightScheme = require("colorschemes." .. vim.g.lightColor)
+--------------------------------------------------------------------------------
 
 -- disable the auto-activation of the colorscheme that is not needed on startup
-local macOSMode = vim.system({ "defaults", "read", "-g", "AppleInterfaceStyle" }):wait()
-local bg = (macOSMode.stdout or ""):find("Dark") and "dark" or "light"
+local bg
+if vim.g.neovide and jit.os == "OSX" then -- neovide too slow in setting `background`
+	local macOSMode = vim.system({ "defaults", "read", "-g", "AppleInterfaceStyle" }):wait()
+	bg = (macOSMode.stdout or ""):find("Dark") and "dark" or "light"
+else
+	bg = vim.o.background
+end
+
 if bg == "dark" then
 	lightScheme.config = nil
 	lightScheme.priority = nil
