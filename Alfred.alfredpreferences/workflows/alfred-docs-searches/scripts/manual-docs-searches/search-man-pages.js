0294c4945430a54f52583e64012f3b9e7f6ab042
🤖 automated <cron@job>
2025-08-08 08:49:35 +0200

    Home (scripts/manual-docs-searches/search-man-pages.js)

--- Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/manual-docs-searches/search-man-pages.js
+++ Alfred.alfredpreferences/workflows/alfred-docs-searches/scripts/manual-docs-searches/search-man-pages.js
@@ -17,6 +17,15 @@ function alfredMatcher(str) {
 	const squeezed = str.replace(/[-_]/g, "");
 	return [clean, squeezed, str].join(" ") + " ";
 }
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @typedef {Object} MankierPage
+ * @property {string} name
+ * @property {string} section
+ * @property {string} description
+ * @property {boolean} is_alias
+ * @property {string} url
+ */
 
 //──────────────────────────────────────────────────────────────────────────────
 
@@ -41,14 +50,16 @@ function run(argv) {
 		"echo $PATH | tr ':' '\n' | xargs -I {} find {} -mindepth 1 -maxdepth 1 -type f -or -type l -perm '++x' | xargs basename";
 	const installedBinaries = app.doShellScript(shellCmd).split("\r");
 
+	const response = httpRequest(apiUrl + firstWord);
+	const /** @type{MankierPage[]} */ results = JSON.parse(response).results;
+
 	/** @type{AlfredItem[]} */
-	const manPages = JSON.parse(httpRequest(apiUrl + firstWord)).results.map(
-		(
-			/** @type {{ name: string; section: string; description: string; is_alias: boolean; url: string; }} */ result,
-		) => {
+	const manPages = results
+		.map((result) => {
 			const cmd = result.is_alias ? result.url.split("/").at(-1) || "" : result.name;
+			const isInstalled = installedBinaries.includes(cmd);
 			const aliasSuffix = result.is_alias ? " (alias)" : "";
-			const icon = installedBinaries.includes(cmd) ? " ✅" : "";
+			const icon = isInstalled ? " ✅" : "";
 			let matcher = alfredMatcher(cmd);
 			if (result.is_alias) matcher += alfredMatcher(result.name);
 
@@ -57,14 +68,20 @@ function run(argv) {
 				subtitle: `(${result.section})  ${result.description}`,
 				match: matcher,
 				uid: cmd,
-				arg: remainingQuery, // pass to next script filter
-				variables: {
-					cmd: cmd,
-					section: result.section,
+				mods: {
+					cmd: { valid: false, subtitle: "⛔ not supported" },
+					alt: { valid: false, subtitle: "⛔ not supported" },
 				},
+				arg: remainingQuery, // pass to next script filter
+				variables: { cmd: cmd, section: result.section }, // next script filter
+				isInstalled: isInstalled, // just for sorting, not for Alfred
 			};
-		},
-	);
+		})
+		.sort((a, b) => {
+			if (a.isInstalled && !b.isInstalled) return -1;
+			if (!a.isInstalled && b.isInstalled) return 1;
+			return 0;
+		});
 
 	return JSON.stringify({ items: manPages });
 }
