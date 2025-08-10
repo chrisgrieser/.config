0400ae92625cbcad83b90a8638b5836af7a07e01
🤖 automated <cron@job>
2025-08-10 10:40:57 +0200

    Home (Alfred.alfredpreferences/workflows/openai-utils/)

--- Alfred.alfredpreferences/workflows/openai-utils/scripts/openai-costs.js
+++ Alfred.alfredpreferences/workflows/openai-utils/scripts/openai-costs.js
@@ -13,31 +13,49 @@ function run() {
 	firstDayCurMonth.setHours(0, 0, 0, 0);
 	const startTimestamp = firstDayCurMonth.getTime() / 1000; // OpenAI uses seconds
 	const dayOfMonth = new Date().getDate();
+	let nextPage = "";
+	const dataAcc = [];
 
-	// DOCS https://platform.openai.com/docs/api-reference/usage/costs
-	const url =
-		"https://api.openai.com/v1/organization/costs" +
-		`?start_time=${startTimestamp}` +
-		`&limit=${dayOfMonth}`;
+	while (true) {
+		// DOCS https://platform.openai.com/docs/api-reference/usage/costs
+		const url =
+			"https://api.openai.com/v1/organization/costs" +
+			`?start_time=${startTimestamp}` +
+			`&limit=${dayOfMonth}` +
+			`&page=${nextPage}`;
 
-	const curlCmd = `curl --request GET --url "${url}" \
+		const curlCmd = `curl --request GET --url "${url}" \
 		--header 'Content-Type: application/json' \
 		--header 'Authorization: Bearer ${apiKey}'`;
-	const response = app.doShellScript(curlCmd);
-	if (!response) return JSON.stringify({ items: [{ title: "Error: No results", valid: false }] });
-	const data = JSON.parse(response);
-	if (data.error) {
-		return JSON.stringify({
-			items: [{ title: "Error: " + data.error.message, valid: false }],
-		});
+		const response = app.doShellScript(curlCmd);
+		if (!response)
+			return JSON.stringify({ items: [{ title: "Error: No results", valid: false }] });
+		const data = JSON.parse(response);
+		if (data.error) {
+			return JSON.stringify({
+				items: [{ title: "Error: " + data.error.message, valid: false }],
+			});
+		}
+
+		dataAcc.push(...data.data);
+		nextPage = data.next_page;
+		if (!data.has_more) break;
 	}
 
-	const totalCost = data.data.reduce((total, day) => {
+	const totalCost = dataAcc.reduce((total, day) => {
+		// @ts-expect-error too long to add all that
 		const costOfDay = day.results.reduce((sum, result) => sum + result.amount?.value || 0, 0);
 		return total + costOfDay;
 	}, 0);
 
-	return JSON.stringify({
-		items: [{ title: `Total cost: ${totalCost.toFixed(2)}$`, valid: false }],
-	});
+	//───────────────────────────────────────────────────────────────────────────
+
+	const today = new Date();
+	const monthlyBudget = Number.parseFloat($.getenv("openai_monthly_budget"));
+	const nameOfMonth = today.toLocaleString("default", { month: "long" });
+	const daysInMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
+	const daysRemainingThisMonth = daysInMonth - today.getDate() + 1; // +1 for today itself
+
+	// Output for Alfred
+	return `$${totalCost.toFixed(2)} / $${monthlyBudget.toFixed(2)}\n${nameOfMonth} budget, resets in ${daysRemainingThisMonth} days`;
 }
