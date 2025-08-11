9f773f50ab6569a37f97716f545031572bf2d0c1
🤖 automated <cron@job>
2025-08-11 19:18:20 +0200

    Home (Alfred.alfredpreferences/workflows/misc-utilities/scripts/)

--- Alfred.alfredpreferences/workflows/misc-utilities/scripts/create-link-file.sh
+++ Alfred.alfredpreferences/workflows/misc-utilities/scripts/create-link-file.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env zsh
 
-title="$(echo "$1" | cut -d$'\t' -f1 | tr "$€§*#?!:;.,'\"\{}" "-")"
-url="$(echo "$1" | cut -d$'\t' -f2)"
-filepath="$HOME/Desktop/$title.webloc"
+title="$(echo "$1" | cut -d$'\t' -f1 | cut -c1-50 | tr "$€§*#?!:;.,'\"\{}" "-")"
+url="$(echo "$1" | cut -d$'\t' -f2 | sed 's/&/&amp;/g')" # `&` invalid in xml
 
-# multiline string:
-cat > "$filepath" <<EOF
+finder_path=$(osascript -l "JavaScript" -e 'decodeURIComponent(Application("Finder").insertionLocation().url()?.slice(7) || "")')
+default_path="$HOME/Desktop"
+target_path="${finder_path:-$default_path}"
+filepath="$target_path/$title.webloc"
+
+#───────────────────────────────────────────────────────────────────────────────
+
+cat > "$filepath" << EOF
 <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" \
-    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-    <key>URL</key>
-    <string>$url</string>
+	<key>URL</key>
+	<string>${url}</string>
 </dict>
 </plist>
 EOF
