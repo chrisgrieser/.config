faa354ef3440c8cb80798ae5e9ec7812ff349478
🤖 automated <cron@job>
2025-08-22 08:24:46 +0200

    Home (Alfred.alfredpreferences/workflows/misc-utilities/)

---
+++ Alfred.alfredpreferences/workflows/misc-utilities/scripts/toggle-lock.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+filepath="$1"
+
+# Get only the flags field from ls -lO
+# shellcheck disable=2012
+flags=$(ls -lO "$filepath" | awk '{print $5}')
+name=$(basename "$filepath")
+
+if [[ "$flags" == *"uchg"* ]]; then
+	chflags -h nouchg "$filepath"
+	alfred_msg="Unlocked $name"
+else
+	chflags -h uchg "$filepath"
+	alfred_msg="Locked $name"
+fi
+
+echo "$alfred_msg"
