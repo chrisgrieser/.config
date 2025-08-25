75b7f41a0b36e626bf94741c2aebff28c0bf592f
🤖 automated <cron@job>
2025-08-25 11:46:02 +0200

    Home (18 files)

--- nvim/lsp/biome.lua
+++ nvim/lsp/biome.lua
@@ -3,6 +3,4 @@ return {
 	-- Do not require a `package.json` like in nvim-lspconfig default.
 	root_markers = { "biome.json", "biome.jsonc" },
 	root_dir = false, ---@diagnostic disable-line: assign-type-mismatch
-
-	workspace_required = false, -- to use biome's json formatter outside js_projects
 }
--- nvim/lua/plugin-specs/tooling/conform.lua
+++ nvim/lua/plugin-specs/tooling/conform.lua
@@ -21,17 +21,23 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			markdown = { "markdownlint", "markdown-toc", "injected" },
-			python = { "ruff_fix" },
-			typescript = { "ts_remove_unused_imports", "ts_update_imports" },
+			python = { "ruff_fix", "ruff_organize_imports" },
+			typescript = { "ts_update_imports", "biome-organize-imports" },
 			zsh = { "shell_home", "shellcheck" },
+			json = { lsp_format = "prefer", "jq" }, -- prefer `biome`, then `jq`
 
 			-- fallback, used when no formatters are defined and no LSP is available
 			_ = { "trim_whitespace", "trim_newlines", "squeeze_blanks" },
 		},
 		formatters = {
-			injected = { ignore_errors = true },
+			injected = {
+				ignore_errors = true,
+				lang_to_formatters = {
+					json = { "jq" },
+				},
+			},
 			shell_home = {
-				format = function(_, _, _, callback)
+				format = function(_self, _ctx, _lines, callback)
 					vim.cmd([[% s_/Users/\w\+/_$HOME/_e]]) -- replace `/Users/…` with `$HOME/`
 					callback()
 				end,
@@ -40,7 +46,7 @@ return {
 				args = "'$FILENAME' --format=diff --shell=bash | patch -p1 '$FILENAME'",
 			},
 			ts_update_imports = {
-				format = function(_, _, _, callback)
+				format = function(_self, _ctx, _lines, callback)
 					vim.lsp.buf.code_action {
 						---@diagnostic disable-next-line: missing-fields, assign-type-mismatch
 						context = { only = { "source.addMissingImports.ts" } },
