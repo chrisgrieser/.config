9b0bbfb3a0a228706da539d4d8496c89c12453f5
🤖 automated <cron@job>
2025-08-27 11:14:33 +0200

    Home (3 files)

--- hammerspoon/system/japanese.lua
+++ hammerspoon/system/japanese.lua
@@ -4,7 +4,7 @@ local M = {}
 -- TOGGLE LANGUAGE
 -- not done via Karabiner, since setting input method there is buggy: https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/to/select-input-source/
 
-hs.hotkey.bind({ "option" }, "space", function()
+hs.hotkey.bind({ "option", "control" }, "j", function()
 	-- Japanese defined by METHOD, Latin languages by LAYOUT
 	local isJapanese = hs.keycodes.currentMethod() ~= nil
 	if isJapanese then
