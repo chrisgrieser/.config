51560865ce5c7cb270aaed57db1850d561846f7f
🤖 automated <cron@job>
2025-09-01 09:25:10 +0200

    Home (3 files)

--- Alfred.alfredpreferences/workflows/misc-utilities/scripts/toggle-hidden.sh
+++ Alfred.alfredpreferences/workflows/misc-utilities/scripts/toggle-hidden.sh
@@ -2,16 +2,16 @@
 
 filepath="$1"
 
-# Get only the flags field from ls -lO
+# Get only the flags field from `ls -lO`
 # shellcheck disable=2012
 flags=$(ls -lO "$filepath" | awk '{print $5}')
 name=$(basename "$filepath")
 
-if [[ "$flags" == *"uchg"* ]]; then
-	chflags -h nouchg "$filepath"
+if [[ "$flags" == *"hidden"* ]]; then
+	chflags -h nohidden "$filepath"
 	alfred_msg="🔒 Unlocked \"$name\""
 else
-	chflags -h uchg "$filepath"
+	chflags -h hidden "$filepath"
 	alfred_msg="🔒 Locked \"$name\""
 fi
 
