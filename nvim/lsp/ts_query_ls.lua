b6200a33dcdad171919b96ebf4577efde0614ea3
🤖 automated <cron@job>
2025-09-07 13:06:44 +0200

    Home (3 files)

--- nvim/lsp/ts_query_ls.lua
+++ nvim/lsp/ts_query_ls.lua
@@ -1,7 +1,9 @@
 ---@type vim.lsp.Config
 return {
-	-- FIX for https://github.com/ribru17/ts_query_ls/issues/233
-	settings = {
-		parser_install_directories = { require("nvim-treesitter.config").get_install_dir("") .. "/parser" },
+	init_options = {
+		-- use the custom directory set in the treesitter config
+		parser_install_directories = {
+			require("nvim-treesitter.config").get_install_dir("") .. "/parser",
+		},
 	},
 }
