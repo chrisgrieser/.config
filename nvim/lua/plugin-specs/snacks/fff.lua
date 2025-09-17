26fec633330dbd0afd6f1e73ec9c511fce05c8c6
🤖 automated <cron@job>
2025-09-17 14:39:22 +0200

    Office (2 files)

--- nvim/lua/plugin-specs/snacks/fff.lua
+++ nvim/lua/plugin-specs/snacks/fff.lua
@@ -1,16 +1,11 @@
 ---@module "lazy.types"
 ---@type LazyPluginSpec
 return {
-	'dmtrKovalenko/fff.nvim',
+	"dmtrKovalenko/fff.nvim",
 	build = function() require("fff.download").download_or_build_binary() end,
-	opts = {
-	},
+	opts = {},
 	lazy = false,
 	keys = {
-	  {
-	    "ff",
-	    function() require('fff').find_files() end,
-	    desc = 'FFFind files',
-	  }
-	}
+		{ "gO", function() require("fff").find_files() end, desc = "FFFind files" },
+	},
 }
