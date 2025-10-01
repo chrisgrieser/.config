c7bf6195d7aa867e62696bb6176d0c0d5751b343
🤖 automated <cron@job>
2025-10-01 13:59:10 +0200

    Office (2 files)

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -90,17 +90,16 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged", "BufLeave", "FocusLo
 	desc = "User: Auto-save",
 	callback = function(ctx)
 		local saveInstantly = ctx.event == "FocusLost" or ctx.event == "BufLeave"
-		local bufnr = ctx.buf
-		local bo, b = vim.bo[bufnr], vim.b[bufnr]
+		local bo, b = vim.bo[ctx.buf], vim.b[ctx.buf]
 		local bufname = ctx.file
 		if bo.buftype ~= "" or bo.ft == "gitcommit" or bo.readonly then return end
 		if b.saveQueued and not saveInstantly then return end
 
 		b.saveQueued = true
 		vim.defer_fn(function()
-			if not vim.api.nvim_buf_is_valid(bufnr) then return end
+			if not vim.api.nvim_buf_is_valid(ctx.buf) then return end
 
-			vim.api.nvim_buf_call(bufnr, function()
+			vim.api.nvim_buf_call(ctx.buf, function()
 				-- saving with explicit name prevents issues when changing `cwd`
 				-- `:update!` suppresses "The file has been changed since reading it!!!"
 				local vimCmd = ("silent! noautocmd lockmarks update! %q"):format(bufname)
