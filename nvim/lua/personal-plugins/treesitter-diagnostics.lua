c3a5578823da3bc533a89ea80ff0f8005bc5cb93
🤖 automated <cron@job>
2025-10-23 13:02:01 +0200

    Office (nvim/lua/personal-plugins/treesitter-diagnostics.lua)

--- nvim/lua/personal-plugins/treesitter-diagnostics.lua
+++ nvim/lua/personal-plugins/treesitter-diagnostics.lua
@@ -10,7 +10,7 @@ local ignore = {
 --------------------------------------------------------------------------------
 
 --- language-independent query for syntax errors and missing elements
-local error_query = vim.treesitter.query.parse("query", "[(ERROR)(MISSING)] @a")
+local errorQuery = vim.treesitter.query.parse("query", "[(ERROR)(MISSING)] @a")
 local ns = vim.api.nvim_create_namespace("treesitter.diagnostics")
 
 --- @param args vim.api.keyset.create_autocmd.callback_args
@@ -27,35 +27,33 @@ local function diagnose(args)
 
 	parser:parse(false, function(_, trees)
 		if not trees then return end
+
 		parser:for_each_tree(function(tree, ltree)
-			local lang = ltree:lang()
-			if vim.list_contains(ignore.parsers, lang) then return end
-			-- only process trees containing errors
-			if tree:root():has_error() then
-				for _, node in error_query:iter_captures(tree:root(), args.buf) do
-					local lnum, col, end_lnum, end_col = node:range()
+			if vim.list_contains(ignore.parsers, ltree:lang()) or not tree:root():has_error() then
+				return
+			end
 
-					-- collapse nested syntax errors that occur at the exact same position
-					local parent = node:parent()
-					if parent and parent:type() == "ERROR" and parent:range() == node:range() then
-						goto continue
-					end
+			for _, node in errorQuery:iter_captures(tree:root(), args.buf) do
+				local lnum, col, endLnum, endCol = node:range()
 
+				-- collapse nested syntax errors that occur at the exact same position
+				local parent = node:parent()
+				if parent and parent:type() == "ERROR" and parent:range() == node:range() then
 					-- clamp large syntax error ranges to just the line to reduce noise
-					if end_lnum > lnum then
-						end_lnum = lnum + 1
-						end_col = 0
+					if endLnum > lnum then
+						endLnum = lnum + 1
+						endCol = 0
 					end
 
 					--- @type vim.Diagnostic
 					local diag = {
 						source = "treesitter",
 						lnum = lnum,
-						end_lnum = end_lnum,
+						end_lnum = endLnum,
 						col = col,
-						end_col = end_col,
+						end_col = endCol,
 						message = "",
-						code = lang .. "-styntax",
+						code = ltree:lang() .. "-syntax",
 						bufnr = args.buf,
 						namespace = ns,
 						severity = vim.diagnostic.severity.ERROR,
@@ -65,9 +63,9 @@ local function diagnose(args)
 					-- add context to the error using sibling and parent nodes
 					local previous = node:prev_sibling()
 					if previous and previous:type() ~= "ERROR" then
-						local previous_type = previous:named() and previous:type()
-							or string.format("`%s`", previous:type())
-						diag.message = diag.message .. " after " .. previous_type
+						local previousType = previous:named() and previous:type()
+							or ("`%s`").format(previous:type())
+						diag.message = diag.message .. " after " .. previousType
 					end
 
 					if
@@ -79,7 +77,6 @@ local function diagnose(args)
 					end
 
 					table.insert(diagnostics, diag)
-					::continue::
 				end
 			end
 		end)
