85348977fdc4ecf9dcf5e0d695c7e1ab838f268d
🤖 automated <cron@job>
2025-11-01 15:38:22 +0100

    Home (8 files)

--- nvim/lsp/biome.lua
+++ nvim/lsp/biome.lua
@@ -2,10 +2,10 @@
 return {
 	root_markers = {
 		-- Do not require a `package.json` like in nvim-lspconfig default.
-		"biome.json",
-		"biome.jsonc",
 		"jsconfig.json",
 		"tsconfig.json",
+		"biome.json",
+		"biome.jsonc",
 		".obsidian/snippets/*.css",
 	},
 	root_dir = false, ---@diagnostic disable-line: assign-type-mismatch
--- obsidian/time-tracking-helper/main.js
+++ obsidian/time-tracking-helper/main.js
@@ -6,53 +6,55 @@ function lineToReport(plugin, editor) {
 	const cursor = editor.getCursor("from");
 	const currentLine = editor.getLine(cursor.line);
 
-	// example line: 9:00-11:00 Arbeit, 12:00-15:30 Orga
-	const timePattern = /(\d{1,2}[:.]\d{2})-(\d{1,2}[:.]\d{2}) ([\wÄÖÜäöü]+)/;
+	// example line: 9:00-11:00 Arbeit, 12:00-15:30 Orga, 15:30-17:00 Arbeit
+	const timePattern = /(\d{1,2}[.:]\d{2})-(\d{1,2}[.:]\d{2}) ([\wÄÖÜäöü]+)/;
 
 	// parse activities
 	const activities =
 		currentLine.match(new RegExp(timePattern, "g"))?.reduce((acc, activity) => {
 			const [_, start, end, type] = activity.match(timePattern);
-			let [startH, startM] = start.split(/[:.]/).map(Number);
-			const [endH, endM] = end.split(/[:.]/).map(Number);
+			let [startH, startM] = start.split(/[.:]/).map(Number);
+			const [endH, endM] = end.split(/[.:]/).map(Number);
 			if (startH > endH) startH -= 24; // time beyond midnight
 
-			const duration = endH * 60 + endM - (startH * 60 + startM);
-			acc[type] = (acc[type] ?? 0) + duration;
+			const durationHours = (endH * 60 + endM - (startH * 60 + startM)) / 60;
+			acc[type] = (acc[type] ?? 0) + durationHours;
 			return acc;
 		}, {}) ?? false;
 	if (!activities) {
-		// biome-ignore lint/correctness/noUndeclaredVariables: not declarable
 		new Notice("Paragraph does not contain any activities in the expected format.");
 		return;
 	}
 
 	// add to frontmatter
-	const totalMins = 0;
-	const activeFile = ""
+	const activeFile = app.workspace.getActiveFile();
 	app.fileManager.processFrontMatter(activeFile, (fm) => {
-		fm["open-tasks"] = openTasks > 0 ? openTasks : undefined;
+		for (const [type, hours] of Object.entries(activities)) {
+			fm[type] = hours;
+		}
 	});
 
 	// praise notification 💪
+	const totalHours = Object.values(activities).reduce((acc, hours) => acc + hours, 0);
 	const praiseThresholdHours = 6;
-	if (totalMins > 60 * praiseThresholdHours) {
-		const msg = `Ganze ${prettyHours(totalMins)} heute!\n\nChris ist stolz auf Dich! 💪`;
+	if (totalHours > praiseThresholdHours) {
+		const msg = `Ganze ${totalHours}h heute!\n\nChris ist stolz auf Dich! 💪`; // typos: ignore-line
 		new Notice(msg, 10_000);
 	}
 }
 
 //──────────────────────────────────────────────────────────────────────────────
 
+// biome-ignore lint/correctness/noUndeclaredDependencies: okay, since only mini-plugin
 class TimeTrackingHelperPlugin extends require("obsidian").Plugin {
 	onload() {
 		console.info(this.manifest.name + " loaded.");
 
 		this.addCommand({
-			id: "line-to-report",
-			name: "Convert line to report",
+			id: "line-to-properties",
+			name: "Convert line to properties",
 			icon: "clipboard-clock", // for mobile
-			editorCallback: (editor) => lineToReport(editor),
+			editorCallback: (editor) => lineToReport(this, editor),
 		});
 	}
 }
