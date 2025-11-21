50a2b8f8d7f385a99fda35441aed00d0e884cf01
🤖 automated <cron@job>
2025-11-21 12:03:53 +0100

    Home (Alfred.alfredpreferences/workflows/)

--- Alfred.alfredpreferences/workflows/icon-images/scripts/annotate-last-screenshot.sh
+++ Alfred.alfredpreferences/workflows/icon-images/scripts/annotate-last-screenshot.sh
@@ -3,7 +3,7 @@
 # shellcheck disable=SC2154 # Alfred variable
 loc="$screenshot_folder"
 
-# shellcheck disable=SC2012 # special charrs not to be expected here
+# shellcheck disable=SC2012 # special chars not to be expected here
 last_screenshot=$(ls -t "$loc" | head -n1)
 if [[ -z "$last_screenshot" ]]; then
 	echo "⚠️ No screenshots found."
