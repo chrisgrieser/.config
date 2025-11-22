9deb59af9f7085f1bd759d62fb08217cdb8d499d
🤖 automated <cron@job>
2025-11-22 12:33:51 +0100

    Home (nvim/lua/plugin-specs/leetcode.lua)

--- nvim/lua/plugin-specs/leetcode.lua
+++ nvim/lua/plugin-specs/leetcode.lua
@@ -1,18 +1,31 @@
 return {
 	"kawre/leetcode.nvim",
 	lazy = vim.fn.argv(0, -1) ~= "leetcode.nvim", -- start via `nvim "leetcode.nvim"`
-	cmd = "Leet",
 
 	dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
 	config = function(_, opts)
 		vim.g.whichkeyAddSpec { "<leader>x", group = "󰹂 Leetcode" }
 		require("leetcode").setup(opts)
 
-		local oldNotify = vim.notify
-		vim.notify = function(msg, level, opts)
-			if msg:find("leetcode.nvim") then return end
-			oldNotify(msg, level, opts)
-		end
+		-- FIX snacks notifications not being replacing
+		vim.defer_fn(function()
+			local oldNotify = vim.notify
+			vim.notify = function(msg, level, o) ---@diagnostic disable-line: duplicate-set-field intentional overwrite
+				if o.title == "leetcode.nvim" then o.id = "leetcode" end
+				oldNotify(msg, level, o)
+			end
+		end, 1000) -- defer for notification plugin
+
+		-- FIX missing `nowait`
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "leetcode.nvim",
+			callback = function(_ctx)
+				vim.defer_fn(
+					function() vim.keymap.set("n", "q", vim.cmd.quit, { nowait = true, buffer = true }) end,
+					100
+				)
+			end,
+		})
 	end,
 	keys = {
 		-- https://github.com/kawre/leetcode.nvim#-commands
@@ -23,16 +36,11 @@ return {
 		{ "<leader>xp", "<cmd>Leet list difficulty=easy<CR>", desc = " Problem list" },
 		{ "<leader>xh", "<cmd>Leet info<CR>", desc = " Hints & similar problems" },
 		{ "<leader>xm", "<cmd>Leet menu<CR>", desc = "󰹯 Leetcode menu" },
-		{ "q", "q", ft = "leetcode.nvim", remap = true, nowait = true }, -- fix missing `nowait`
 	},
 	opts = {
 		lang = "javascript", -- https://github.com/kawre/leetcode.nvim#lang
 		storage = {
 			home = vim.g.iCloudSync .. "/leetcode",
-		},
-		description = {
-			position = "left",
-			width = "40%",
 		},
 	},
 }
