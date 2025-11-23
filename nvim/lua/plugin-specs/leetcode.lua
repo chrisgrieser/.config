a7dd460e3ceb8c9e84816d8f26a6303a111b1a1e
🤖 automated <cron@job>
2025-11-23 12:03:50 +0100

    Home (nvim/)

--- nvim/lua/plugin-specs/leetcode.lua
+++ nvim/lua/plugin-specs/leetcode.lua
@@ -11,6 +11,7 @@ return {
 		vim.defer_fn(function()
 			local oldNotify = vim.notify
 			vim.notify = function(msg, level, o) ---@diagnostic disable-line: duplicate-set-field intentional overwrite
+				if not o then o = {} end
 				if o.title == "leetcode.nvim" then o.id = "leetcode" end
 				oldNotify(msg, level, o)
 			end
@@ -19,11 +20,11 @@ return {
 		-- FIX missing `nowait`
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "leetcode.nvim",
-			callback = function(_ctx)
-				vim.defer_fn(
-					function() vim.keymap.set("n", "q", vim.cmd.quit, { nowait = true, buffer = true }) end,
-					100
-				)
+			callback = function(ctx)
+				local function set()
+					vim.keymap.set("n", "q", vim.cmd.quit, { nowait = true, buffer = ctx.buf })
+				end
+				vim.defer_fn(set, 100) -- defer to overwrite the leetcode.nvim's keymap
 			end,
 		})
 	end,
@@ -33,7 +34,8 @@ return {
 		{ "<leader>xd", "<cmd>Leet desc<CR>", desc = " Toggle problem desc" },
 		{ "<leader>xs", "<cmd>Leet submit<CR>", desc = " Submit" },
 		{ "<leader>xo", "<cmd>Leet open<CR>", desc = "󰖟 Open in browser" },
-		{ "<leader>xp", "<cmd>Leet list difficulty=easy<CR>", desc = " Problem list" },
+		{ "<leader>xp", "<cmd>Leet list difficulty=easy<CR>", desc = " Problem list (easy)" },
+		{ "<leader>xr", "<cmd>Leet random difficulty=easy<CR>", desc = " Random problem (easy)" },
 		{ "<leader>xh", "<cmd>Leet info<CR>", desc = " Hints & similar problems" },
 		{ "<leader>xm", "<cmd>Leet menu<CR>", desc = "󰹯 Leetcode menu" },
 	},
@@ -41,6 +43,10 @@ return {
 		lang = "typescript", -- https://github.com/kawre/leetcode.nvim#lang
 		storage = {
 			home = vim.g.iCloudSync .. "/leetcode",
+		},
+		description = {
+			position = "left",
+			width = "60%",
 		},
 	},
 }
