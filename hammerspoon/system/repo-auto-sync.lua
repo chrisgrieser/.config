5236033066b35b1cb7a537ff899cf730d85187d8
🤖 automated <cron@job>
2025-11-24 18:34:16 +0100

    Office (hammerspoon/system/repo-auto-sync.lua)

--- hammerspoon/system/repo-auto-sync.lua
+++ hammerspoon/system/repo-auto-sync.lua
@@ -2,6 +2,7 @@ local config = {
 	syncIntervalMins = 30,
 	-- csv-format: "location,icon"
 	permaReposPath = os.getenv("HOME") .. "/.config/perma-repos.csv",
+	syncScriptAtLocation = ".sync-this-repo.sh",
 }
 
 --------------------------------------------------------------------------------
@@ -11,105 +12,98 @@ local M = {} -- persist from garbage collector
 local env = require("meta.environment")
 local u = require("meta.utils")
 
----@param msg string
-local function notify(msg) hs.notify.show("Hammerspoon", "", msg) end
-
----@alias Repo { icon: string, location: string }
---------------------------------------------------------------------------------
-
-M.reposToSync = {} ---@type Repo[]
-
 --------------------------------------------------------------------------------
 -- SYNC IMPLEMENTATION
 
-M.finishedSyncing = {} ---@type Repo[]
-M.task_sync = {} ---@type table<string, hs.task>
+---@alias Repo { icon: string, location: string }
 
----@async
----@param repo Repo
-local function syncOneRepo(repo)
-	local syncScriptPath = repo.location:gsub("~", os.getenv("HOME") or "") .. "/.sync-this-repo.sh"
-	if not u.isExecutable(syncScriptPath) then return end
+---@param silent? "silent"
+local function syncAllGitRepos(silent)
+	-- reset
+	M.reposToSync = {} ---@type Repo[]
+	M.finishedSyncing = {} ---@type Repo[]
+	M.task_sync = {} ---@type table<string, hs.task>
 
-	M.task_sync[repo.location] = hs.task
-		.new(syncScriptPath, function(exitCode, stdout, stderr)
-			if exitCode == 0 then
-				table.insert(M.finishedSyncing, repo)
-			else
-				local output = (stdout .. "\n" .. stderr):gsub("^%s+", ""):gsub("%s+$", "")
-				local msg = ("⚠️️ %s %s Sync: %s"):format(repo.icon, repo.location, output)
-				print(msg)
-				hs.alert(msg, 5)
-				notify(msg)
-			end
-		end)
-		:start()
-end
-
----@nodiscard
----@return string stillSyncingIcons
-local function repoSyncsInProgress()
-	local stillSyncingIcons = ""
-	for _, repo in pairs(M.reposToSync) do
-		local isStillSyncing = M.task_sync[repo.location] and M.task_sync[repo.location]:isRunning()
-		if isStillSyncing then stillSyncingIcons = stillSyncingIcons .. repo.icon end
+	-- get all repos to sync
+	for line in io.lines(config.permaReposPath) do
+		local location, icon, _ = line:match("^([^,]+),([^,]+)")
+		assert(location and icon, "invalid repo line: " .. line)
+		table.insert(M.reposToSync, { location = location, icon = icon })
 	end
-	return stillSyncingIcons
-end
 
----@param notifyOnSuccess boolean set to false for regularly occurring syncs
-local function syncAllGitRepos(notifyOnSuccess)
+	-- helper func
+	local function repoSyncsInProgress() ---@return string stillSyncingIcons
+		local stillSyncingIcons = ""
+		for _, repo in pairs(M.reposToSync) do
+			local isStillSyncing = M.task_sync[repo.location]
+				and M.task_sync[repo.location]:isRunning()
+			if isStillSyncing then stillSyncingIcons = stillSyncingIcons .. repo.icon end
+		end
+		return stillSyncingIcons
+	end
+
 	-- GUARD
 	local hasInternetAccess = hs.network.reachability.internet():statusString():find("R") ---@diagnostic disable-line: undefined-field
 	if not hasInternetAccess then
 		u.notify("🛜⛔ No internet connection.")
 		return
-	elseif repoSyncsInProgress() ~= "" then
-		u.notify("🔁 Sync still in progress: " .. repoSyncsInProgress())
+	end
+	local stillInProgress = repoSyncsInProgress()
+	if stillInProgress ~= "" then
+		u.notify("🔁 Sync still in progress: " .. stillInProgress)
 		return
 	end
 
-	-- get repos from perma-repos.csv
-	M.reposToSync = {} -- reset
-	for line in io.lines(config.permaReposPath) do
-		local location, icon, _ = line:match("^(.-),(.-)")
-		if not (location and icon) then return end
-		table.insert(M.reposToSync, { location = location, icon = icon })
+	-- sync them
+	for _, repo in pairs(M.reposToSync) do
+		local syncScriptPath = repo.location:gsub("~", os.getenv("HOME") or "")
+			.. "/"
+			.. config.syncScriptAtLocation
+		assert(u.isExecutable(syncScriptPath), "no sync script found at " .. syncScriptPath)
+
+		M.task_sync[repo.location] = hs.task
+			.new(syncScriptPath, function(exitCode, stdout, stderr)
+				if exitCode == 0 then
+					table.insert(M.finishedSyncing, repo)
+				else
+					local output = (stdout .. "\n" .. stderr):gsub("^%s+", ""):gsub("%s+$", "")
+					local msg = ("⚠️️ %s %s Sync: %s"):format(repo.icon, repo.location, output)
+					print(msg)
+					hs.alert(msg, 5)
+				end
+			end)
+			:start()
 	end
 
-	-- sync them all
-	for _, repo in pairs(M.reposToSync) do
-		syncOneRepo(repo)
-	end
+	-- notify when done
 	M.timer_AllSyncs = hs.timer
 		.waitUntil(function() return repoSyncsInProgress() == "" end, function()
 			local syncedIcons = hs.fnutils.map(M.finishedSyncing, function(r) return r.icon end) or {}
 			local msg = "🔁 Sync done: " .. table.concat(syncedIcons)
 			print(msg)
-			if notifyOnSuccess then notify(msg) end
-			M.finishedSyncing = {} -- reset
+			if not silent then hs.notify.show("Hammerspoon", "", msg) end
 		end)
 		:start()
 end
 
 --------------------------------------------------------------------------------
--- WHEN TO SYNC
+-- SYNC TRIGGERS
 
--- 1. on systemstart
-if u.isSystemStart() then syncAllGitRepos(true) end
+-- 1. systemstart
+if u.isSystemStart() then syncAllGitRepos() end
 
 -- 2. every x minutes
 M.timer_repoSync = hs.timer
 	.doEvery(config.syncIntervalMins * 60, function()
 		local idleMins = hs.host.idleTime() / 60
-		if idleMins < config.syncIntervalMins then syncAllGitRepos(false) end
+		if idleMins < config.syncIntervalMins then syncAllGitRepos("silent") end
 	end)
 	:start()
 
 -- 3. manually via Alfred: `hammerspoon://sync-repos`
 hs.urlevent.bind("sync-repos", function()
 	u.notify("🔁 Starting sync…")
-	syncAllGitRepos(true)
+	syncAllGitRepos()
 end)
 
 -- 4. when going to sleep or when unlocking
@@ -125,7 +119,7 @@ M.caff_SleepWatcherForRepoSync = c.new(function(event)
 		or event == c.systemDidWake
 	then
 		u.defer(2, function()
-			syncAllGitRepos(true)
+			syncAllGitRepos()
 			M.recentlyTriggered = true
 			u.defer(7, function() M.recentlyTriggered = false end)
 		end)
