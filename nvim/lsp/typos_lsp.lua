6b1c985062a8a338d061c93f4257242b6193b173
🤖 automated <cron@job>
2025-11-24 16:18:01 +0100

    Office (8 files)

--- nvim/lsp/typos_lsp.lua
+++ nvim/lsp/typos_lsp.lua
@@ -7,9 +7,4 @@ return {
 		diagnosticSeverity = "Hint",
 		config = vim.fn.stdpath("config") .. "/lsp/typos_lsp_global_config.toml",
 	},
-	root_dir = function(bufnr, on_dir)
-		if require("config.utils").isObsidianOrNotesOrIcloud(bufnr) then return end
-		local rootMarkers = { "typos.toml", "_typos.toml", ".typos.toml" }
-		on_dir(vim.fs.root(bufnr, rootMarkers))
-	end,
 }
