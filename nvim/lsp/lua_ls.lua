e85a094185b226689fb408dadba09b591670015f
🤖 automated <cron@job>
2025-11-25 21:34:02 +0100

    Home (nvim/lsp/lua_ls.lua)

--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -7,6 +7,15 @@ return {
 		-- disable formatting in favor of `stylua`
 		client.server_capabilities.documentFormattingProvider = false
 		client.server_capabilities.documentRangeFormattingProvider = false
+		Chainsaw(client) -- 🪚
+
+		-- add globals for nvim-plugins
+		if client.root_dir:find("nvim") then
+			client.config.settings.Lua.diagnostics.globals = { ---@diagnostic disable-line: undefined-field
+				"vim", -- when working on nvim plugins that lack a `.luarc.json`
+				"Chainsaw", -- nvim-chainsaw debugging function
+			}
+		end
 	end,
 	settings = {
 		Lua = {
@@ -26,7 +35,7 @@ return {
 					-- (no loss of diagnostic, `unused-local` still informs about these functions)
 					"unused-function",
 				},
-				globals = { "vim" }, -- when working on nvim plugins that lack a `.luarc.json`
+				-- globals = { "vim" }, -- when working on nvim plugins that lack a `.luarc.json`
 			},
 			codeLens = {
 				enable = true, -- requires `vim.lsp.codelens.refresh` autocmd
