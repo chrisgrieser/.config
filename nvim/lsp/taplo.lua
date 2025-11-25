e823c4ffdea9be301cdda405833b0aa106bf3f2e
🤖 automated <cron@job>
2025-11-25 11:34:02 +0100

    Home (nvim/lsp/taplo.lua)

--- nvim/lsp/taplo.lua
+++
@@ -1,8 +0,0 @@
----@type vim.lsp.Config
-return {
-	on_attach = function(client)
-		-- disable formatting in favor of `tombi`
-		client.server_capabilities.documentFormattingProvider = false
-		client.server_capabilities.documentRangeFormattingProvider = false
-	end,
-}
