bdb6a713d21187e27e801d490f9ad9ae5e518472
🤖 automated <cron@job>
2025-11-26 15:43:37 +0100

    Office (sketchybar/)

--- sketchybar/components/no_internet.sh
+++ sketchybar/components/no_internet.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
 test_site="www.google.com"
-draw=$(curl --fail --max-time 3 "$test_site" && echo "false" || echo "true")
+draw=$(curl --fail --max-time 3 "$test_site" &> /dev/null && echo "false" || echo "true")
 sketchybar --set "$NAME" drawing="$draw"
