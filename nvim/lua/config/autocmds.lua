6f332512a0137fdd5ff4788ac086c3cc086a2c55
🤖 automated <cron@job>
2025-11-30 23:03:17 +0100

    Home (nvim/lua/)

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -313,7 +313,7 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
 
 			-- read template
 			local file = io.open(templatePath, "r")
-			if not (file) then return end
+			if not file then return end
 			local content = file:read("*a")
 			vim.snippet.expand(content) -- expands placeholders like `$0`
 
@@ -544,4 +544,28 @@ if jit.os == "OSX" then
 	})
 end
 
---------------------------------------------------------------------------------
+---READABLE LINE LENGTH IN MARKDOWN---------------------------------------------
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	desc = "User: readable line length on markdown",
+	callback = function(ctx)
+		local isMarkdown = vim.bo[ctx.buf].filetype == "markdown"
+		local pseudoWin = vim.g.readableLineLengthWin
+
+		if isMarkdown and not pseudoWin then
+			local bufnr = vim.api.nvim_create_buf(false, true)
+			local winid = vim.api.nvim_open_win(bufnr, false, {
+				split = "right",
+				width = vim.o.columns - vim.o.textwidth + 1,
+				style = "minimal",
+			})
+			vim.wo[winid].winblend = 50
+			vim.wo[winid].winfixbuf = true
+			vim.bo[bufnr].modifiable = false
+			vim.g.readableLineLengthWin = winid
+		elseif not isMarkdown and pseudoWin then
+			vim.api.nvim_win_close(pseudoWin, true)
+			vim.g.readableLineLengthWin = nil
+		end
+	end,
+})
