863204161685e1e401e9c35d6404ef60699d3863
🤖 automated <cron@job>
2025-12-01 13:03:17 +0100

    Home (nvim/)

--- nvim/queries/markdown_inline/textobjects.scm
+++ nvim/queries/markdown_inline/textobjects.scm
@@ -1,14 +1,18 @@
 ; extends
 
-; custom text objects for `nvim-treesitter-textobjects`
+; mdlink
 (inline_link) @mdlink.outer
 
 (link_text) @mdlink.inner
 
-(emphasis) @emphasis.outer
-
 ;-------------------------------------------------------------------------------
+; emphasis
 ((strong_emphasis) @emphasis.inner
-  (#lua-match? @emphasis.inner "^[a-z]+$"))
+  (#offset! @emphasis.inner 0 2 0 -2))
+
+((emphasis) @emphasis.inner
+  (#offset! @emphasis.inner 0 1 0 -1))
 
 (strong_emphasis) @emphasis.outer
+
+(emphasis) @emphasis.outer
