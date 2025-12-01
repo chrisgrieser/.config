9d96a0a2aab89393617a6f6978c6b8df8e604961
🤖 automated <cron@job>
2025-12-01 12:03:17 +0100

    Home (nvim/)

---
+++ nvim/lua/plugin-specs/virt-column.lua
@@ -0,0 +1,9 @@
+-- DOCS https://github.com/lukas-reineke/virt-column.nvim/blob/master/doc/virt-column.txt
+
+return {
+	"lukas-reineke/virt-column.nvim",
+	event = "VeryLazy",
+	opts = {
+		highlight = "NonText",
+	},
+}
