ccda439bb4b97631e9c3f73c2504ccffa7c278ec
🤖 automated <cron@job>
2025-12-04 15:23:27 +0100

    Office (11 files)

--- Alfred.alfredpreferences/workflows/icon-images/scripts/svg-tech-icons.js
+++ Alfred.alfredpreferences/workflows/icon-images/scripts/svg-tech-icons.js
@@ -16,42 +16,34 @@ function httpRequest(url) {
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	const apiUrl1 = "https://api.github.com/repos/vscode-icons/vscode-icons/git/trees/master?recursive=1"
-	const apiUrl2 = "https://api.github.com/repos/material-extensions/vscode-material-icon-theme/git/trees/main?recursive=1"
-
-	const response1 = httpRequest(apiUrl1);
-	if (!response1) return JSON.stringify({ items: [{ title: "Error: No results", valid: false }] });
-	const tree1 = JSON.parse(response1).tree;
+	// CONFIG
+	const iconRepos = {
+		vscode: { name: "vscode-icons/vscode-icons", branch: "master" },
+		material: { name: "material-extensions/vscode-material-icon-theme", branch: "main" },
+	};
+	//---------------------------------------------------------------------------
 	/** @type {AlfredItem[]} */
-	const items1 = tree1.flatMap((/** @type {{path: string}} */ item) => {
-		const [_, name] = item.path.match(/^icons\/(.*)\.svg$/) || []
-		if (!name) return [];
-		const url = `https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/${name}.svg`;
-		return {
-			title: name,
-			subtitle: "vscode-icons",
-			arg: url,
-		};
-	});
+	const items = [];
 
-	const response2 = httpRequest(apiUrl1);
-	if (!response2) return JSON.stringify({ items: [{ title: "Error: No results", valid: false }] });
-	const tree2 = JSON.parse(response2).tree;
-	/** @type {AlfredItem[]} */
-	const items2 = tree2.flatMap((/** @type {{path: string}} */ item) => {
-		const [_, name] = item.path.match(/^icons\/(.*)\.svg$/) || []
-		if (!name) return [];
-		const url = `https://raw.githubusercontent.com/material-extensions/vscode-material-icon-theme/master/icons/${name}.svg`;
-		return {
-			title: name,
-			subtitle: "material-icons",
-			arg: url,
-		};
-	});
+	for (const [iconGroup, repo] of Object.entries(iconRepos)) {
+		const apiUrl = `https://api.github.com/repos/${repo.name}/git/trees/${repo.branch}?recursive=1`;
+		const response = httpRequest(apiUrl);
+		const tree = JSON.parse(response).tree;
+		tree.forEach((/** @type {{path: string}} */ item) => {
+			const [_, filetype] = item.path.match(/^icons\/(.*)\.svg$/) || [];
+			if (!filetype) return;
+			const displayName = filetype.replaceAll("file_type_", "");
+			const url = `https://raw.githubusercontent.com/${repo.name}/${repo.branch}/icons/${filetype}.svg`;
+			items.push({
+				title: displayName,
+				subtitle: iconGroup,
+				arg: url,
+			});
+		});
+	}
 
 	return JSON.stringify({
-		items: [...items1, items2],
-		// cache: { seconds: 60 * 60 * 24 * 7, loosereload: true }, // 1 week
+		items: items,
+		cache: { seconds: 60 * 60 * 24 * 7, loosereload: true }, // 1 week
 	});
 }
-
