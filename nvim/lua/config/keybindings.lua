da272aed0def0dabea0c53ff3fa7cfb50a9b8c53
🤖 automated <cron@job>
2025-12-06 22:22:06 +0100

    Home (nvim/)

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -246,7 +246,7 @@ do
 		desc = "User: Yankring",
 		callback = function()
 			if vim.v.event.operator == "y" then
-				for i = 9, 1, -1 do -- shift all numbered registers.
+				for i = 9, 1, -1 do -- shift all numbered registers
 					vim.fn.setreg(tostring(i), vim.fn.getreg(tostring(i - 1)))
 				end
 			end
