4c0cef20b207750c888e1ca7666bc5d3688d28ce
🤖 automated <cron@job>
2025-12-08 01:07:36 +0100

    Home (18 files)

--- wezterm/wezterm-keymaps.lua
+++ wezterm/wezterm-keymaps.lua
@@ -11,90 +11,14 @@ local actFun = wt.action_callback
 
 M.keys = {
 	---ZSH BRIDGE----------------------------------------------------------------
-	-- cmd+enter -> ctrl+o -> cycle common directories
+	-- cmd+enter -> ctrl+o -> cycle common directories (configured in zshrc)
 	{ key = "Enter", mods = "CMD", action = act.SendKey { key = "o", mods = "CTRL" } },
 
-	-- cmd+l -> ctrl+l -> open current directory
+	-- cmd+l -> ctrl+l -> reveal current directory in Finder (configured in zshrc)
 	{ key = "l", mods = "CMD", action = act.SendKey { key = "l", mods = "CTRL" } },
 
-	-- REMAP VI MODE (cannot use `bindkey` for `<S-Space>`)
+	-- shift-space -> daw -> delete a word (in vi-mode)
 	{ key = "Space", mods = "SHIFT", action = act.SendString("daw") },
-
-	---META----------------------------------------------------------------------
-	{ -- cmd+, -> open the config file
-		key = ",",
-		mods = "CMD",
-		action = actFun(function() wt.open_with(wt.config_file) end),
-	},
-	-- FIX works with `send_composed_key_when_right_alt_is_pressed = true`
-	-- but expects another character, so this mapping fixes it
-	{ key = "n", mods = "ALT", action = act.SendString("~") },
-
-	-- Theme Cycler
-	{ key = "t", mods = "ALT", action = actFun(require("theme-cycler").cycle) },
-
-	---WEZTERM-------------------------------------------------------------------
-	{ key = "p", mods = "CMD", action = act.ActivateCommandPalette },
-	-- { key = "f", mods = "CMD", action = act.Search("CurrentSelectionOrEmptyString") },
-
-	-- vertical split
-	{ key = "v", mods = "CTRL", action = act.SplitHorizontal }, -- SIC, called horizontal by wezterm
-
-	-- closes panes, tabs, and windows
-	{ key = "w", mods = "CMD", action = act.CloseCurrentPane { confirm = false } },
-
-	-- scroll-to-prompt, requires shell integration: https://wezfurlong.org/wezterm/config/lua/keyassignment/ScrollToPrompt.html
-	{ key = "k", mods = "CTRL", action = act.ScrollToPrompt(-1) },
-	{ key = "j", mods = "CTRL", action = act.ScrollToPrompt(1) },
-
-	{ -- cycles panes, then tabs, then windows
-		key = "Enter",
-		mods = "CTRL",
-		action = wt.action_callback(function(win, pane)
-			local paneCount = #pane:tab():panes()
-			local tabCount = #win:mux_window():tabs()
-			if paneCount > 1 then
-				win:perform_action(act.ActivatePaneDirection("Next"), pane)
-			elseif tabCount > 1 then
-				win:perform_action(act.ActivateTabRelative(1), pane)
-			else
-				win:perform_action(act.ActivateTabRelative(1), pane)
-			end
-		end),
-	},
-
-	{
-		key = "PageUp",
-		action = wt.action_callback(function(win, pane)
-			-- if TUI (such as fullscreen fzf), send key to TUI,
-			-- otherwise scroll by page https://github.com/wez/wezterm/discussions/4101
-			if pane:is_alt_screen_active() then
-				win:perform_action(act.SendKey { key = "PageUp" }, pane)
-			else
-				win:perform_action(act.ScrollByPage(-0.9), pane)
-			end
-		end),
-	},
-	{
-		key = "PageDown",
-		action = wt.action_callback(function(win, pane)
-			if pane:is_alt_screen_active() then
-				win:perform_action(act.SendKey { key = "PageDown" }, pane)
-			else
-				win:perform_action(act.ScrollByPage(0.9), pane)
-			end
-		end),
-	},
-
-	{ -- for adding inline code to a commit, hotkey consistent with GitHub
-		key = "e",
-		mods = "CMD",
-		action = act.Multiple {
-			act.SendString([[\`\`]]),
-			act.SendKey { key = "LeftArrow" },
-			act.SendKey { key = "LeftArrow" },
-		},
-	},
 
 	{ -- insert line-break https://unix.stackexchange.com/a/80820
 		key = "Enter",
@@ -105,10 +29,64 @@ M.keys = {
 		},
 	},
 
-	-----------------------------------------------------------------------------
-	-- HINT MODE (= Quick Select) -- https://wezfurlong.org/wezterm/quickselect.html
-	-- cmd+y -> copy text
-	{ key = "y", mods = "CMD", action = act.QuickSelect },
+	---META----------------------------------------------------------------------
+	-- cmd+, -> open the config file
+	{ key = ",", mods = "CMD", action = actFun(function() wt.open_with(wt.config_file) end) },
+
+	{ key = "t", mods = "ALT", action = actFun(require("theme-cycler").cycle) },
+
+	-- FIX works with `send_composed_key_when_right_alt_is_pressed = true`
+	-- but expects another character, so this mapping fixes it
+	-- { key = "n", mods = "ALT", action = act.SendString("~") },
+
+	-- Theme Cycler
+
+	---WEZTERM-------------------------------------------------------------------
+	{ key = "p", mods = "CMD", action = act.ActivateCommandPalette },
+	{ key = "v", mods = "CTRL", action = act.SplitHorizontal }, -- SIC actually vertical
+	{ key = "w", mods = "CMD", action = act.CloseCurrentPane { confirm = false } }, -- pane, then tab
+
+	-- scroll-to-prompt, requires shell integration: https://wezfurlong.org/wezterm/config/lua/keyassignment/ScrollToPrompt.html
+	{ key = "k", mods = "CTRL", action = act.ScrollToPrompt(-1) },
+	{ key = "j", mods = "CTRL", action = act.ScrollToPrompt(1) },
+
+	{ -- cycles panes, then tabs, then windows
+		key = "Enter",
+		mods = "CTRL",
+		action = wt.action_callback(function(win, pane)
+			local hasSplit = #pane:tab():panes() > 1
+			local action = hasSplit and act.ActivatePaneDirection("Next") or act.ActivateTabRelative(1)
+			win:perform_action(action, pane)
+		end),
+	},
+	{
+		key = "PageUp",
+		action = wt.action_callback(function(win, pane)
+			-- send key to fullscreen-TUI (like fzf), otherwise scroll https://github.com/wez/wezterm/discussions/4101
+			local action = pane:is_alt_screen_active() and act.SendKey { key = "PageUp" }
+				or act.ScrollByPage(-0.9)
+			win:perform_action(action, pane)
+		end),
+	},
+	{
+		key = "PageDown",
+		action = wt.action_callback(function(win, pane)
+			local action = pane:is_alt_screen_active() and act.SendKey { key = "PageDown" }
+				or act.ScrollByPage(0.9)
+			win:perform_action(action, pane)
+		end),
+	},
+	{ -- cmd+e -> escaped backticks and (and move cursor into it)
+		key = "e",
+		mods = "CMD",
+		action = act.Multiple {
+			act.SendString([[\`\`]]),
+			act.SendKey { key = "LeftArrow" },
+			act.SendKey { key = "LeftArrow" },
+		},
+	},
+	---HINT MODE (QUICK SELECT)--------------------------------------------------
+	{ key = "y", mods = "CMD", action = act.QuickSelect }, -- https://wezfurlong.org/wezterm/quickselect.html
 
 	{ -- cmd+shift+u -> open URL (like `f` in vimium)
 		key = "u",
@@ -116,22 +94,19 @@ M.keys = {
 		action = act.QuickSelectArgs {
 			patterns = { [[https?://[^\]",' ]+\w]] },
 			label = "Open URL",
-			action = actFun(function(window, pane)
-				local url = window:get_selection_text_for_pane(pane)
+			action = actFun(function(win, pane)
+				local url = win:get_selection_text_for_pane(pane)
 				wt.open_with(url)
 			end),
 		},
 	},
 }
 
---------------------------------------------------------------------------------
-
--- https://wezterm.org/scrollback.html#configurable-search-mode-key-assignments
-M.searchModeKeys = {
-	-- cmd+g -> next/previous match, consistent with macOS behavior
-	{ key = "g", mods = "CMD", action = act.CopyMode("NextMatch") },
-	{ key = "g", mods = "CMD|SHIFT", action = act.CopyMode("PriorMatch") },
-}
+---SEARCH MODE------------------------------------------------------------------
+-- cmd+g -> next/previous match, consistent with macOS behavior
+M.searchKeys = wt.gui.default_key_tables().search_mode -- https://wezterm.org/scrollback.html#configurable-search-mode-key-assignments
+table.insert(M.searchKeys, { key = "g", mods = "CMD", action = act.CopyMode("NextMatch") })
+table.insert(M.searchKeys, { key = "g", mods = "CMD|SHIFT", action = act.CopyMode("PriorMatch") })
 
 --------------------------------------------------------------------------------
 return M
