a94d54ed293d0f867d5a853032b798d12400cb65
🤖 automated <cron@job>
2025-12-17 15:20:44 +0100

    Home (karabiner/)

--- karabiner/build-karabiner-config.js
+++ karabiner/build-karabiner-config.js
@@ -26,36 +26,37 @@ function run(argv) {
 	const profileToUse = argv[0] || "Default"; // CONFIG
 	const home = app.pathTo("home folder");
 	const karabinerJson = home + "/.config/karabiner/karabiner.json";
-	const customRulesJson = home + "/.config/karabiner/assets/complex_modifications/";
+	const customRulesDir = home + "/.config/karabiner/assets/complex_modifications/";
 
 	// GUARD yq is installed
 	const yqNotInstalled = app.doShellScript("command -v yq || echo 'false'") === "false";
 	if (yqNotInstalled) return "󱎘 yq is not installed.";
 
+	// 1. convert yaml to json
 	// using `explode` to expand anchors & aliases: https://mikefarah.gitbook.io/yq/operators/anchor-and-alias-operators#explode-alias-and-anchor
+	const tempDir = app.doShellScript("mktemp -d");
 	app.doShellScript(`
 		export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
-		cd "$HOME/.config/karabiner/assets/complex_modifications/" || exit 1
+		cd "${customRulesDir}" || exit 1
 		for f in *.yaml ; do
 			f=$(basename "$f" .yaml)
-			yq --output-format=json 'explode(.)' "$f.yaml" > "$f.json"
+			yq --output-format=json 'explode(.)' "$f.yaml" > "${tempDir}/$f.json"
 		done
 	`);
 
-	// compile new rules
+	// 2. merge jsons
+	/** @type {unknown[]} */
 	const customRules = [];
-	const ruleFile = app.doShellScript(`ls "${customRulesJson}" | grep ".json"`).split("\r");
+	const ruleFile = app.doShellScript(`ls "${tempDir}" | grep ".json"`).split("\r");
 	for (const fileName of ruleFile) {
-		const filePath = customRulesJson + fileName;
+		const filePath = tempDir + "/" + fileName;
 		const ruleSet = JSON.parse(readFile(filePath))?.rules;
 		if (!ruleSet) return "󱎘 Parsing issue";
-		for (const rule of ruleSet) {
-			customRules.push(rule);
-		}
+		ruleSet.forEach((/** @type {unknown} */ rule) => customRules.push(rule));
 		app.doShellScript(`rm "${filePath}"`); // delete leftover JSON
 	}
 
-	// insert new rules into karabiner config
+	// 3. insert new rules into karabiner config
 	const complexRules = JSON.parse(readFile(karabinerJson));
 	const profileIdx = complexRules.profiles.findIndex(
 		(/** @type {{ name: string; }} */ profile) => profile.name === profileToUse,
