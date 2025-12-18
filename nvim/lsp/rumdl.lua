ed4a7e662435a7a715d67ad122b929e8157898d4
🤖 automated <cron@job>
2025-12-18 18:17:12 +0100

    Office (2 files)

--- nvim/lsp/rumdl.lua
+++ nvim/lsp/rumdl.lua
@@ -1,9 +1,9 @@
--- DOCS https://github.com/rvben/rumdl#configuration
+-- DOCS https://github.com/rvben/rumdl#global-configuration
 --------------------------------------------------------------------------------
 
 ---@type vim.lsp.Config
 return {
-	root_markers = {
+	root_markers = { -- https://github.com/rvben/rumdl#configuration-discovery
 		"rumdl.toml",
 		".rumdl.toml",
 		"pyproject.toml",
