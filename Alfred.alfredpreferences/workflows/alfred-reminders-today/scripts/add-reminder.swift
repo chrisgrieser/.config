ab8e709ebbc904c7d58c00c41c880463907ecd5c
🤖 automated <cron@job>
2025-12-23 16:46:34 +0100

    Home (2 files)

--- Alfred.alfredpreferences/workflows/alfred-reminders-today/scripts/add-reminder.swift
+++ Alfred.alfredpreferences/workflows/alfred-reminders-today/scripts/add-reminder.swift
@@ -114,7 +114,7 @@ func fetchWebsiteTitle(from string: String) async throws -> String? {
 	let (data, _) = try await URLSession.shared.data(from: url)
 	guard let html = String(data: data, encoding: .utf8) else { return nil }
 
-	let regex = try! Regex("<title>(.*?)</title>")
+	let regex = try! Regex("<title.*?>(.*?)</title>")
 	if let match = try? regex.firstMatch(in: html) {
 		return String(match.output[1].substring!)
 	}
