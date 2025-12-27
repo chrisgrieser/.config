07463ff9c5d05c09edbe2006714a1cad6f361076
🤖 automated <cron@job>
2025-12-27 16:14:06 +0100

    Mother (nvim/after/ftplugin/just.lua)

--- nvim/after/ftplugin/just.lua
+++ nvim/after/ftplugin/just.lua
@@ -1,9 +1,13 @@
 -- standard used by `just --fmt`
-vim.defer_fn(function() vim.opt_local.expandtab = true end, 1)
+vim.schedule(function()
+	vim.opt_local.expandtab = true
+	vim.opt_local.shiftwidth = 4
+end)
 
 --------------------------------------------------------------------------------
+local bkeymap = require("config.utils").bufKeymap
 
-require("config.utils").bufKeymap("n", "<leader>ll", function()
+bkeymap("n", "<leader>ll", function()
 	local out = vim.system({ "just", "--evaluate" }):wait().stdout or "Error"
 	vim.notify(vim.trim(out), nil, { title = "just --evaluate", ft = "just", icon = "󱁤" })
 end, { desc = "󰖷 just --evaluate" })
