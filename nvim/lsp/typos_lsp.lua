7582b0ac8fe08b3a03ecf655f367c6bfae3a16cf
🤖 automated <cron@job>
2025-12-28 11:48:42 +0100

    Home (nvim/lsp/typos_lsp.lua)

--- nvim/lsp/typos_lsp.lua
+++ nvim/lsp/typos_lsp.lua
@@ -4,7 +4,7 @@
 ---@type vim.lsp.Config
 return {
 	init_options = {
-		diagnosticSeverity = "Hint",
+		diagnosticSeverity = "Warning",
 		config = vim.fn.stdpath("config") .. "/lsp/typos_lsp_global_config.toml",
 	},
 }
