068d35fe4c438ee99bef01739836dac8b404c0d2
🤖 automated <cron@job>
2026-01-04 18:24:04 +0100

    Home (nvim/)

--- nvim/lua/personal-plugins/marks.lua
+++ nvim/lua/personal-plugins/marks.lua
@@ -1,14 +1,13 @@
--- INFO Simple wrapper around vim's builtin mark functionality with some extras.
--- * shows marks in the signcolumn
--- * command to set a mark
--- * command to delete all marks
+-- Simple wrapper around vim's builtin mark functionality, adding the features:
+-- * marks are shown in the signcolumn
+-- * command to set/unset a mark
+-- * command to cycle through marks
 --------------------------------------------------------------------------------
 
 local config = {
 	marks = { "A", "B" },
 	signHlGroup = "StandingOut",
 	signIcons = { A = "󰬈", B = "󰬉" },
-	notifyIcon = "󰃀",
 }
 
 --------------------------------------------------------------------------------
@@ -17,36 +16,36 @@ local M = {}
 local ns = vim.api.nvim_create_namespace("mark-signs")
 
 ---@param msg string
----@param level? "info"|"warn"|"error"
-local function notify(msg, level)
-	local lvl = level and level:upper() or "INFO"
-	vim.notify(msg, vim.log.levels[lvl], { title = "Marks", icon = config.notifyIcon })
+---@param lvl? "info"|"warn"|"error"
+local function notify(msg, lvl)
+	if not lvl then lvl = "info" end
+	vim.notify(msg, vim.log.levels[lvl:upper()], { title = "Marks", icon = "󰃀" })
 end
 
 ---@param mark string
 ---@return boolean
 local function cursorIsAtMark(mark)
-	local row, _col, bufnr, path = unpack(vim.api.nvim_get_mark(mark, {}))
+	local row, _, bufnr, path = unpack(vim.api.nvim_get_mark(mark, {}))
 	if path == nil or path == "" then return false end -- mark not set
 	local cursorRow = vim.api.nvim_win_get_cursor(0)[1]
 	local currentBuf = vim.api.nvim_get_current_buf()
-	return cursorRow == row and currentBuf == bufnr -- do not check for col
+	return cursorRow == row and currentBuf == bufnr -- intentionally don't check for col
 end
 
 ---@param mark string
 local function setSignForMark(mark)
-	local row, _col, bufnr, path = unpack(vim.api.nvim_get_mark(mark, {}))
+	local row, _, bufnr, path = unpack(vim.api.nvim_get_mark(mark, {}))
 	if path == nil or path == "" then return end -- mark not set
 
-	local function setExtmark(buf, line)
+	local function setExtmark(buf, line, ma)
 		vim.api.nvim_buf_set_extmark(buf, ns, line - 1, 0, {
-			sign_text = config.signIcons[mark] or mark,
+			sign_text = config.signIcons[ma] or ma,
 			sign_hl_group = config.signHlGroup,
 		})
 	end
 
 	if bufnr ~= 0 then
-		setExtmark(bufnr, row)
+		setExtmark(bufnr, row, mark)
 		return
 	end
 
@@ -55,7 +54,7 @@ local function setSignForMark(mark)
 		desc = "User(once): Add signs for mark " .. mark,
 		callback = function(ctx)
 			if ctx.file == path then
-				setExtmark(ctx.buf, row)
+				setExtmark(ctx.buf, row, mark)
 				return true -- delete this autocmd
 			end
 		end,
@@ -65,7 +64,7 @@ end
 ---if cursor is at a mark, delete it, otherwise set it
 ---@param mark string
 local function setUnsetMark(mark)
-	local mrow, _mcol, bufnr, _path = unpack(vim.api.nvim_get_mark(mark, {}))
+	local mrow, _, bufnr, _ = unpack(vim.api.nvim_get_mark(mark, {}))
 	if mrow > 0 then vim.api.nvim_buf_clear_namespace(bufnr, ns, mrow - 1, mrow) end -- delete old sign
 
 	if cursorIsAtMark(mark) then
@@ -88,7 +87,7 @@ function M.cycleMarks()
 		notify("No mark has been set.")
 		return
 	elseif #marksSet == 1 and cursorIsAtMark(marksSet[1]) then
-		notify(("[%s] is the only mark set."):format(marksSet[1]))
+		notify(("Already at the only mark [%s]."):format(marksSet[1]))
 		return
 	end
 
@@ -103,14 +102,14 @@ function M.cycleMarks()
 
 	-- goto next mark
 	local row, col, bufnr, path = unpack(vim.api.nvim_get_mark(nextMark, {}))
-	local markInUnopenedFile = bufnr == 0
+	local markInUnopenedFile = bufnr == 0 and path ~= ""
 	if markInUnopenedFile then
 		vim.cmd.edit(path)
 	else
 		vim.api.nvim_set_current_buf(bufnr)
 	end
-	local success = pcall(vim.api.nvim_win_set_cursor, 0, { row, col })
-	if success then
+	local markExists = path ~= "" and pcall(vim.api.nvim_win_set_cursor, 0, { row, col })
+	if markExists then
 		vim.cmd.normal { "zv", bang = true } -- open folds at cursor
 	else
 		notify(("[%s] not valid anymore"):format(nextMark), "warn")
