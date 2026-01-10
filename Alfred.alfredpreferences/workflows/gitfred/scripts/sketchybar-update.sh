712c96528054378c67f15acbd417bf6524f91492
🤖 automated <cron@job>
2026-01-10 12:01:03 +0100

    Home (2 files)

--- Alfred.alfredpreferences/workflows/gitfred/scripts/sketchybar-update.sh
+++ Alfred.alfredpreferences/workflows/gitfred/scripts/sketchybar-update.sh
@@ -5,6 +5,6 @@ if [[ ! -x "$(command -v sketchybar)" ]]; then
 	echo "Could not update sketchybar component: sketchybar not installed." >&2
 	return 1
 else
-	sleep 4 # wait for sync
+	sleep 3 # wait for sync
 	sketchybar --trigger "$sketchybar_trigger_name"
 fi
