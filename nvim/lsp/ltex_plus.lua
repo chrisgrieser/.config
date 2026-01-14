696c2d87d18cffffc5f7dd935ca3632e90c0dd71
🤖 automated <cron@job>
2026-01-14 16:10:58 +0100

    Office (nvim/lsp/ltex_plus.lua)

--- nvim/lsp/ltex_plus.lua
+++ nvim/lsp/ltex_plus.lua
@@ -7,7 +7,6 @@ return {
 	root_dir = function(bufnr, on_dir)
 		-- do not load in specific repos (there is no `ltexignore` to do this)
 		local pathsToIgnore = {
-			vim.env.HOME .. "/Library/Mobile Documents/", -- anything in iCloud
 			vim.env.HOME .. "/phd-data-analysis/",
 			vim.g.notesDir,
 		}
@@ -20,18 +19,17 @@ return {
 	end,
 	settings = {
 		ltex = {
-			language = "en-US", -- also file via yaml header like `lang: de-DE` https://ltex-plus.github.io/ltex-plus/advanced-usage.html#set-language-in-markdown-with-yaml-front-matter
+			language = "auto", -- also per-file via yaml header: `lang: de-DE` https://ltex-plus.github.io/ltex-plus/advanced-usage.html#set-language-in-markdown-with-yaml-front-matter
 			diagnosticSeverity = { default = "warning" },
 			disabledRules = {
-				["en-US"] = {
+				["en"] = {
 					"MORFOLOGIK_RULE_EN_US", -- spellcheck done via Harper instead
 					"EN_QUOTES", -- don't expect smart quotes
 					"WHITESPACE_RULE", -- too many false positives
 					"UPPERCASE_SENTENCE_START", -- done via Harper
 					"CONSECUTIVE_SPACES", -- done by Harper & rumdl
 				},
-				["de-DE"] = {
-					"GERMAN_SPELLER_RULE", -- too many false positives
+				["de"] = {
 					"ABKUERZUNG_LEERZEICHEN", -- not needed
 					"TYPOGRAFISCHE_ANFUEHRUNGSZEICHEN", -- don't expect smart quotes
 				},
