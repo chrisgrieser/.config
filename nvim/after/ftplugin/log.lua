a22b682ca85bb607dcc85e03e9eaa78509427403
🤖 automated <cron@job>
2026-01-30 10:50:24 +0100

    Home (nvim/after/ftplugin/log.lua)

--- nvim/after/ftplugin/log.lua
+++ nvim/after/ftplugin/log.lua
@@ -1,6 +1,6 @@
 -- Go to tail of the log
-if vim.b.already_scrolled_logfile then return end
 vim.defer_fn(function()
+	if vim.b.already_scrolled_logfile then return end
 	vim.cmd.normal { "G", bang = true }
 	vim.b.already_scrolled_logfile = true
 end, 1)
