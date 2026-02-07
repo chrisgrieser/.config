87cce9b4d606dcf26f3560883b303d57858326b9
🤖 automated <cron@job>
2026-02-07 17:47:06 +0100

    Home (2 files)

--- Alfred.alfredpreferences/workflows/misc-utilities/scripts/copy-most-recent-steam-game-icon.sh
+++ Alfred.alfredpreferences/workflows/misc-utilities/scripts/copy-most-recent-steam-game-icon.sh
@@ -2,11 +2,19 @@
 steam_dir="$HOME/Library/Application Support/Steam/steamapps/common/"
 icon=$(find "$steam_dir" -path "*.app/Contents/Resources/*.icns" -mmin -60 | head -n1)
 [[ -f "$icon" ]] || exit 1
-osascript -e "tell application \"Finder\" to set the clipboard to (POSIX file \"$icon\")"
 
 game_dir="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Apps/Games/"
 game_shortcut=$(find "$game_dir" -maxdepth 1 -name "*.app" -mmin -60 | head -n1)
+[[ -f "$game_shortcut" ]] || exit 1
+
+#-------------------------------------------------------------------------------
+
+# copy icon
+osascript -e "tell application \"Finder\" to set the clipboard to (POSIX file \"$icon\")"
+
+# paste icon
 open -R "$game_shortcut" # reveal in Finder
+sleep 0.1
 osascript -e '
 	tell application "System Events" 
 		keystroke "i" using {command down}
