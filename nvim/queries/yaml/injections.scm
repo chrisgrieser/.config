7ae6ca2e9033a0459a16452ca01549e0a7cec4f6
🤖 automated <cron@job>
2026-02-11 10:21:47 +0100

    Office (2 files)

--- nvim/queries/yaml/injections.scm
+++ nvim/queries/yaml/injections.scm
@@ -3,53 +3,59 @@
 ; INFO injected languages require the TS parser name, not the vim filetype name,
 ; so `bash` works, but not `sh`.
 ;───────────────────────────────────────────────────────────────────────────────
-; INJECT BASH FOR:
-; - `shell_command` karabiner elements config
-; - `cmd` espanso
+; INJECT BASH for `shell_command` in karabiner elements configs
 ; adapted from: https://github.com/nvim-treesitter/nvim-treesitter/blob/master/queries/yaml/injections.scm
 ;───────────────────────────────────────────────────────────────────────────────
 (block_mapping_pair
   key: (flow_node) @_run
-  (#any-of? @_run "shell_command" "cmd")
+  (#eq? @_run "shell_command")
   value: (block_node
-           (block_scalar) @injection.content
-           (#set! injection.language "bash")
-           (#offset! @injection.content 0 1 0 0)))
+    (block_scalar) @injection.content
+    (#set! injection.language "bash")
+    (#offset! @injection.content 0 1 0 0)))
 
 (block_mapping_pair
   key: (flow_node) @_run
-  (#any-of? @_run "shell_command" "cmd")
+  (#eq? @_run "shell_command")
   value: (flow_node
-           (plain_scalar
-             (string_scalar) @injection.content)
-           (#set! injection.language "bash")))
+    (plain_scalar
+      (string_scalar) @injection.content)
+    (#set! injection.language "bash")))
+
+(flow_pair
+  key: (flow_node) @_run
+  (#eq? @_run "shell_command")
+  value: (flow_node
+    (plain_scalar
+      (string_scalar) @injection.content)
+    (#set! injection.language "bash")))
 
 (block_mapping_pair
   key: (flow_node) @_run
-  (#any-of? @_run "shell_command" "cmd")
+  (#eq? @_run "shell_command")
   value: (block_node
-           (block_scalar) @injection.content
-           (#set! injection.language "bash")
-           (#offset! @injection.content 0 1 0 0)))
+    (block_scalar) @injection.content
+    (#set! injection.language "bash")
+    (#offset! @injection.content 0 1 0 0)))
 
 (block_mapping_pair
   key: (flow_node) @_run
-  (#any-of? @_run "shell_command" "cmd")
+  (#eq? @_run "shell_command")
   value: (block_node
-           (block_sequence
-             (block_sequence_item
-               (flow_node
-                 (plain_scalar
-                   (string_scalar) @injection.content))
-               (#set! injection.language "bash")))))
+    (block_sequence
+      (block_sequence_item
+        (flow_node
+          (plain_scalar
+            (string_scalar) @injection.content))
+        (#set! injection.language "bash")))))
 
 (block_mapping_pair
   key: (flow_node) @_run
-  (#any-of? @_run "shell_command" "cmd")
+  (#eq? @_run "shell_command")
   value: (block_node
-           (block_sequence
-             (block_sequence_item
-               (block_node
-                 (block_scalar) @injection.content
-                 (#set! injection.language "bash")
-                 (#offset! @injection.content 0 1 0 0))))))
+    (block_sequence
+      (block_sequence_item
+        (block_node
+          (block_scalar) @injection.content
+          (#set! injection.language "bash")
+          (#offset! @injection.content 0 1 0 0))))))
