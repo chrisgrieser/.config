bbac934ba49d366bda866ec3a0e513022f8c857d
🤖 automated <cron@job>
2026-02-12 12:35:55 +0100

    Home (2 files)

--- Alfred.alfredpreferences/workflows/alfred-atop/scripts/select-dns-server.js
+++ Alfred.alfredpreferences/workflows/alfred-atop/scripts/select-dns-server.js
@@ -2,28 +2,29 @@
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
-//──────────────────────────────────────────────────────────────────────────────
+//------------------------------------------------------------------------------
+
+const dnsChoices = [
+	{ name: "Google", addresses: ["8.8.8.8", "8.8.4.4"] },
+	{ name: "Cloudflare", addresses: ["1.1.1.1", "1.0.0.1"] },
+	{ name: "AliDNS", addresses: ["223.5.5.5", "223.6.6.6"] },
+	{ name: "Mullvad", addresses: ["194.242.2.4", "194.242.2.3"] },
+	{ name: "DNS0", addresses: ["193.110.81.0", "185.253.5.0"] },
+	{ name: "Quad9", addresses: ["9.9.9.9", "149.112.112.112"] },
+	{ name: "AdGuard", addresses: ["94.140.14.14", "94.140.15.15"] },
+];
+
+//------------------------------------------------------------------------------
 
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	const selectableDns = [
-		{ name: "Google", addresses: ["8.8.8.8", "8.8.4.4"] },
-		{ name: "Cloudflare", addresses: ["1.1.1.1", "1.0.0.1"] },
-		{ name: "AliDNS", addresses: ["223.5.5.5", "223.6.6.6"] },
-		{ name: "Mullvad", addresses: ["194.242.2.4", "194.242.2.3"] },
-		{ name: "DNS0", addresses: ["193.110.81.0", "185.253.5.0"] },
-		{ name: "Quad9", addresses: ["9.9.9.9", "149.112.112.112"] },
-		{ name: "AdGuard", addresses: ["94.140.14.14", "94.140.15.15"] },
-	];
-
-	const dnsAlfredItems = selectableDns.map((dns) => {
-		return {
-			title: dns.name,
-			subtitle: dns.addresses.join("  ▪  "),
-			arg: dns.addresses,
-			variables: { serverName: dns.name }, // for Alfred notification
-		};
-	});
+	// transform list of DNS servers into Alfred items
+	const dnsAlfredItems = dnsChoices.map((dns) => ({
+		title: dns.name,
+		subtitle: dns.addresses.join("  ▪  "),
+		arg: dns.addresses,
+		variables: { serverName: dns.name }, // for Alfred notification
+	}));
 	dnsAlfredItems.push({
 		title: "Clear DNS setting",
 		subtitle: "will use the DNS provided by the router",
@@ -31,13 +32,11 @@ function run() {
 		variables: { serverName: "DNS provided by router" },
 	});
 
-	//───────────────────────────────────────────────────────────────────────────
-
-	// find first enabled service
+	// add icon to currently used DNS
 	const serviceName = app
 		.doShellScript("networksetup -listallnetworkservices")
 		.split("\r")
-		.find((line) => !line.includes("*")); // asterisk is used to mark disabled services
+		.find((line) => !line.includes("*")); // skip "asterisk (*) denotes disabled services"
 	const currentDns = app
 		.doShellScript(`networksetup -getdnsservers "${serviceName}"`)
 		.split("\r")[0];
