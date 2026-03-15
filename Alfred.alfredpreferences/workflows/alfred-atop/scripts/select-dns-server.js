d97a245d75f952ae8446cc6a781bb1248a86bc6b
🤖 automated <cron@job>
2026-03-15 14:12:42 +0100

    Home (Alfred.alfredpreferences/workflows/alfred-atop/)

--- Alfred.alfredpreferences/workflows/alfred-atop/scripts/select-dns-server.js
+++ Alfred.alfredpreferences/workflows/alfred-atop/scripts/select-dns-server.js
@@ -4,29 +4,24 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 //------------------------------------------------------------------------------
 
-const dnsChoices = [
-	{ name: "Google", addresses: ["8.8.8.8", "8.8.4.4"] },
-	{ name: "Cloudflare", addresses: ["1.1.1.1", "1.0.0.1"] },
-	{ name: "AliDNS", addresses: ["223.5.5.5", "223.6.6.6"] },
-	{ name: "Mullvad", addresses: ["194.242.2.4", "194.242.2.3"] },
-	{ name: "DNS0", addresses: ["193.110.81.0", "185.253.5.0"] },
-	{ name: "Quad9", addresses: ["9.9.9.9", "149.112.112.112"] },
-	{ name: "AdGuard", addresses: ["94.140.14.14", "94.140.15.15"] },
-];
-
-//------------------------------------------------------------------------------
-
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
+	const dnsChoices = $.getenv("dns_choices")
+		.split("\n")
+		.map((line) => {
+			const [name, address1, address2] = line.trim().split(/\s*,\s*/);
+			if (!name || !address1 || !address2) return {};
+			return {
+				title: name,
+				subtitle: address1 + "  ▪  " + address2,
+				arg: [address1, address2],
+				variables: { serverName: name }, // for Alfred notification
+			};
+		});
+
 	// transform list of DNS servers into Alfred items
-	const dnsAlfredItems = dnsChoices.map((dns) => ({
-		title: dns.name,
-		subtitle: dns.addresses.join("  ▪  "),
-		arg: dns.addresses,
-		variables: { serverName: dns.name }, // for Alfred notification
-	}));
-	dnsAlfredItems.push({
-		title: "Clear DNS setting",
+	dnsChoices.push({
+		title: "❌ Clear DNS setting",
 		subtitle: "will use the DNS provided by the router",
 		arg: ["", ""],
 		variables: { serverName: "DNS provided by router" },
@@ -40,9 +35,9 @@ function run() {
 	const currentDns = app
 		.doShellScript(`networksetup -getdnsservers "${serviceName}"`)
 		.split("\r")[0];
-	for (const dns of dnsAlfredItems) {
+	for (const dns of dnsChoices) {
 		if (dns.arg[0] === currentDns) dns.title += " ✅";
 	}
 
-	return JSON.stringify({ items: dnsAlfredItems });
+	return JSON.stringify({ items: dnsChoices });
 }
