fcfe90b9e09e905305de1e524b52fbad0024d6c8
🤖 automated <cron@job>
2026-03-16 14:16:35 +0100

    Home (5 files)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/headless-md2pdf.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/headless-md2pdf.sh
@@ -18,10 +18,10 @@ cd "$(dirname "$md_file")" # so `--resource-path` is set
 #───────────────────────────────────────────────────────────────────────────────
 
 # 1. pandoc's --data-dir for the `defaults` file defined in .zshenv
-# 2. Using headless chromium browser to dependency on pdf-engine for pandoc, and
-#    also since they tend to be not as customizable (e.g., font size not working)
+# 2. Using headless chromium browser to avoid dependency on pdf-engine for
+# pandoc, and also since they tend to be not as customizable
+# (e.g., font size not working)
 pandoc "$md_file" --output="/tmp/temp.html" --defaults="md2html" 2>&1
-"$headless_browser" --headless \
-	--no-pdf-header-footer --print-to-pdf="$pdf_file" "/tmp/temp.html"
+"$headless_browser" --headless --no-pdf-header-footer --print-to-pdf="$pdf_file" "/tmp/temp.html"
 open -R "$pdf_file"
 rm -f "/tmp/temp.html"
