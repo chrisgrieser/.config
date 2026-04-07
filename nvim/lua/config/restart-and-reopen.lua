ba8049953cc34b3c9eb456da141cca6693df3b19
🤖 automated <cron@job>
2026-04-07 17:06:04 +0200

    Office (nvim/)

--- nvim/lua/config/restart-and-reopen.lua
+++ nvim/lua/config/restart-and-reopen.lua
@@ -1,62 +1,32 @@
-local tempfile = "/tmp/neovide-restart"
+local restartSession = "/tmp/restart.vim"
 --------------------------------------------------------------------------------
 
 Keymap {
 	"<D-C-r>",
 	function()
-		-- FIX
-		-- 1. `vim.g.neovide` not set initially during `:restart`
-		-- 2. wrong position loading after restart
-		-- 3. wrong background
-		if vim.g.neovide then
-			local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-			local filepath = vim.api.nvim_buf_get_name(0)
-			local line = table.concat({ filepath, row, col, vim.o.background }, ":")
-			vim.fn.writefile({ line }, tempfile)
-		end
-
 		vim.cmd("silent! update")
-		vim.fn.serverstop(vim.g.serverAddress)
+		vim.cmd.mksession { restartSession, bang = true }
 		vim.cmd.restart()
 	end,
 	desc = " Save & restart",
 	mode = { "n", "x", "i" },
 }
 
---------------------------------------------------------------------------------
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = vim.schedule_wrap(function()
+		local isRestarting = vim.uv.fs_stat(restartSession) ~= nil
+		local wasNotOpenedWithArgs = vim.fn.argc(-1) == 0
 
-local restoreFunc
-local isRestarting = vim.uv.fs_stat(tempfile) ~= nil
-
-if isRestarting then
-	-- FIX #1 `vim.g.neovide` not set initially
-	vim.g.neovide = true
-
-	-- FIX #2 wrong position & background
-	local content = vim.fn.readfile(tempfile)[1]
-	local prevFile, row, col, background = unpack(vim.split(content, ":"))
-	vim.o.background = background
-	local reopenPrevPosition = function()
-		vim.cmd.edit(prevFile)
-		vim.api.nvim_win_set_cursor(0, { tonumber(row), tonumber(col) })
-		vim.cmd.normal { "zH", bang = true } -- fully scroll to the left
-	end
-	restoreFunc = reopenPrevPosition
-
-	pcall(os.remove, tempfile)
-else
-	local reopenLastFile = function()
-		local wasOpenedWithArgs = vim.fn.argc(-1) > 0
-		if wasOpenedWithArgs then return end
-		local toOpen = vim.iter(vim.v.oldfiles):find(function(file)
-			local notGitCommitMsg = vim.fs.basename(file) ~= "COMMIT_EDITMSG"
-			local exists = vim.uv.fs_stat(file) ~= nil
-			return exists and notGitCommitMsg
-		end)
-		if toOpen then vim.cmd.edit(toOpen) end
-	end
-	restoreFunc = reopenLastFile
-end
-
---------------------------------------------------------------------------------
-vim.api.nvim_create_autocmd("VimEnter", { callback = vim.schedule_wrap(restoreFunc) })
+		if isRestarting then
+			vim.cmd.source(restartSession)
+			pcall(os.remove, restartSession)
+		elseif wasNotOpenedWithArgs then
+			local lastFile = vim.iter(vim.v.oldfiles):find(function(file)
+				local notGitCommitMsg = vim.fs.basename(file) ~= "COMMIT_EDITMSG"
+				local exists = vim.uv.fs_stat(file) ~= nil
+				return exists and notGitCommitMsg
+			end)
+			if lastFile then vim.cmd.edit(lastFile) end
+		end
+	end),
+})
