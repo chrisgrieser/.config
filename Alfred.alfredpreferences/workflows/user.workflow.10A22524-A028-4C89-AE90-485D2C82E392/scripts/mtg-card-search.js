f3f502c93c45a4ec3f08b9c0be36489178d6635d
🤖 automated <cron@job>
2026-04-10 15:47:46 +0200

    Home (user.workflow.10A22524-A028-4C89-AE90-485D2C82E392/)

--- Alfred.alfredpreferences/workflows/user.workflow.10A22524-A028-4C89-AE90-485D2C82E392/scripts/mtg-card-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.10A22524-A028-4C89-AE90-485D2C82E392/scripts/mtg-card-search.js
@@ -19,33 +19,68 @@ function httpRequest(url) {
  * @property {string} type_line instant, sorcery, etc
  * @property {string} oracle_text the card body
  * @property {"common"|"uncommon"|"rare"|"mythic"} rarity
- * @property {string[]} colors
+ * @property {("U"|"W"|"B"|"R"|"G")[]?} colors
  * @property {{cardmarket: string, tcgplayer: string}} purchase_uris
- * @property {{usd: string, eur: string}} prices
+ * @property {{usd: string, eur: string}?} prices
+ * @property {{small: string, normal: string, large: string, png: string}?} image_uris
  */
 
 //──────────────────────────────────────────────────────────────────────────────
+
+const colorMap = {
+	// biome-ignore-start lint/style/useNamingConvention: not useful here
+	U: "blue",
+	W: "white",
+	B: "black",
+	R: "red",
+	G: "green",
+}; // biome-ignore-end lint/style/useNamingConvention: not useful here
+
+/**
+ * @param {string} title
+ * @param {string?} [subtitle]
+ */
+function errorItem(title, subtitle) {
+	return JSON.stringify({ items: [{ title: title, subtitle: subtitle || "", valid: false }] });
+}
+
+//------------------------------------------------------------------------------
 
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
 	const query = argv[0];
+	if (!query) return errorItem("Search for card…", "Supports Scryfall search syntax.");
 
 	// DOCS https://scryfall.com/docs/api/cards/search
 	const apiUrl = "https://api.scryfall.com/cards/search?q=" + encodeURIComponent(query);
 	const response = httpRequest(apiUrl);
-	if (!response) return JSON.stringify({ items: [{ title: "Error: No results", valid: false }] });
+	if (!response) return errorItem("No response from Scryfall", "Try again later");
 
-	const data = JSON.parse(response).data;
-	if (!data.length) return JSON.stringify({ items: [{ title: "No cards found", valid: false }] });
+	const cardData = JSON.parse(response).data;
+	if (cardData.length === 0) return errorItem("No cards found.");
 
 	/** @type {AlfredItem[]} */
-	const items = data.map((/** @type {{name: ScryfallCard}} */ card) => {
+	const items = cardData.map((/** @type {ScryfallCard} */ card) => {
+		let color = card.colors?.[0] ? colorMap[card.colors[0]] : "colorless";
+		if (card.colors && card.colors.length > 1) color = "multi";
+
+		const purchaseUrl = card.purchase_uris?.cardmarket;
+		const price = card.prices?.eur ? card.prices.eur + "€" : "";
+		const image = card.image_uris?.png;
+		const subtitle = [card.mana_cost, `${card.type_line}`, price].filter(Boolean).join("    ");
+
 		return {
 			title: card.name,
-			subtitle: "",
-			icon: { path: "scryfall-logo.png" },
-			arg: card.name,
+			subtitle: subtitle,
+			icon: { path: `./mtg-icons/${color}.png` },
+			arg: card.scryfall_uri,
+			quicklookurl: image,
+			mods: {
+				cmd: { arg: purchaseUrl },
+				opt: { arg: card.scryfall_uri }, // copy scryfall url
+				ctrl: { arg: image }, // copy card image
+			},
 		};
 	});
 
