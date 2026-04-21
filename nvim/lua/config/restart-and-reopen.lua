45c7ef247c09a4864886d779230cf3b08853bae3
🤖 automated <cron@job>
2026-04-21 13:28:36 +0200

    Office (nvim/lua/config/restart-and-reopen.lua)

--- nvim/lua/config/restart-and-reopen.lua
+++ nvim/lua/config/restart-and-reopen.lua
@@ -1,13 +1,13 @@
 if not vim.g.neovide then return end
 --------------------------------------------------------------------------------
-local restartSession = "/tmp/restart.vim"
+local restartSessionFile = "/tmp/restart.vim"
 --------------------------------------------------------------------------------
 
 Keymap {
 	"<D-C-r>",
 	function()
 		vim.cmd("silent! update")
-		vim.cmd.mksession { restartSession, bang = true }
+		vim.cmd.mksession { restartSessionFile, bang = true }
 		vim.cmd.restart()
 	end,
 	desc = " Save & restart",
@@ -16,12 +16,12 @@ Keymap {
 
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = vim.schedule_wrap(function()
-		local isRestarting = vim.uv.fs_stat(restartSession) ~= nil
+		local isRestarting = vim.uv.fs_stat(restartSessionFile) ~= nil
 		local notOpenedWithArgs = vim.fn.argc(-1) == 0
 
 		if isRestarting then
-			vim.cmd.source(restartSession)
-			pcall(os.remove, restartSession)
+			vim.cmd.source(restartSessionFile)
+			pcall(os.remove, restartSessionFile)
 		elseif notOpenedWithArgs then
 			local lastFile = vim.iter(vim.v.oldfiles):find(function(file)
 				local notGitCommitMsg = vim.fs.basename(file) ~= "COMMIT_EDITMSG"
