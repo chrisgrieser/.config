34ee4c6037c54a80b7ad0f4726df2f6e604c0050
🤖 automated <cron@job>
2026-04-21 12:58:36 +0200

    Office (nvim/)

--- nvim/lua/plugin-specs/refactoring.lua
+++ nvim/lua/plugin-specs/refactoring.lua
@@ -1,37 +1,32 @@
 vim.pack.add {
-	-- WARN plenary.nvim is deprecated https://github.com/ThePrimeagen/refactoring.nvim/issues/518
-	"https://github.com/nvim-lua/plenary.nvim",
-	"https://github.com/nvim-treesitter/nvim-treesitter",
-	"https://github.com/ThePrimeagen/refactoring.nvim",
+	"https://github.com/lewis6991/async.nvim",
+	"https://github.com/theprimeagen/refactoring.nvim",
 }
 --------------------------------------------------------------------------------
 
 Keymap {
 	"<leader>ri",
-	function() return require("refactoring").refactor("Inline Variable") end,
+	function() return require("refactoring").inline_var() end,
 	mode = { "n", "x" },
 	expr = true,
 	desc = "󰫧 Inline variable",
 }
 Keymap {
 	"<leader>re",
-	function() return require("refactoring").refactor("Extract Variable") end,
+	function() return require("refactoring").extract_var() end,
 	mode = "x",
 	expr = true,
 	desc = "󰫧 Extract selection as variable",
 }
 Keymap {
 	"<leader>rf",
-	function() return require("refactoring").refactor("Extract Function") end,
+	function() return require("refactoring").extract_func() end,
 	mode = "x",
 	expr = true,
 	desc = " Extract function",
 }
 --------------------------------------------------------------------------------
 
--- lazy-load, since it eagerly loads all modules
-vim.defer_fn(function()
-	require("refactoring").setup {
-		show_success_message = true,
-	}
-end, 500)
+require("refactoring").setup {
+	show_success_message = true,
+}
