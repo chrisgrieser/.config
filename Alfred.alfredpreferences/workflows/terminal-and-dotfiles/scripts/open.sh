8cf579213f4d00c9d08b69c47ca52f17b9695381
🤖 automated <cron@job>
2026-04-25 21:00:04 +0200

    Home (2 files)

--- Alfred.alfredpreferences/workflows/terminal-and-dotfiles/scripts/open.sh
+++ Alfred.alfredpreferences/workflows/terminal-and-dotfiles/scripts/open.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env zsh
+filepath=$1
+#-------------------------------------------------------------------------------
 
-# open "$1"
-
+# open "$filepath"
 # PENDING https://github.com/neovide/neovide/issues/3482
+
+#-------------------------------------------------------------------------------
+
+# open directory in Finder
+if [[ -d "$filepath" ]]; then
+	open "$filepath"
+	return 0
+fi
+
+# open file in Neovide
 if ! pgrep -xq "neovide"; then
 	open -a "Neovide"
 	while ! pgrep -xq "neovide"; do sleep 0.1; done
 	sleep 0.6
 fi
 open -a "Neovide" # focus it
-nvim --server '/tmp/nvim_server.pipe' --remote-send "<cmd>edit $1<CR>"
+nvim --server '/tmp/nvim_server.pipe' --remote-send "<cmd>edit $filepath<CR>"
