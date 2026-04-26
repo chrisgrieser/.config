4a81b3ecdbbe75dd03d64fd105b2ada278fbda71
🤖 automated <cron@job>
2026-04-26 14:06:37 +0200

    Home (hammerspoon/appearance/hole-cover.lua)

--- hammerspoon/appearance/hole-cover.lua
+++ hammerspoon/appearance/hole-cover.lua
@@ -28,19 +28,19 @@ function M.update()
 		or { red = 0.88, green = 0.88, blue = 0.88, alpha = 1 }
 
 	local height = 20
-	M.cover_bottom = hs
+	local menubarHeight = 30
+	M.cover_top = hs
 		.canvas
-		.new({ x = 0, y = screen.h - height, w = screen.w, h = height }) --[[@as hs.canvas]]
+		.new({ x = 0, y = menubarHeight, w = screen.w, h = height }) --[[@as hs.canvas]]
 		:appendElements({
 			{ type = "rectangle", action = "fill", fillColor = bgColor },
 		}) --[[@as hs.canvas]]
 		:sendToBack() --[[@as hs.canvas]]
 		:show()
 
-	local menubarHeight = 30
-	M.cover_top = hs
+	M.cover_bottom = hs
 		.canvas
-		.new({ x = 0, y = menubarHeight, w = screen.w, h = height }) --[[@as hs.canvas]]
+		.new({ x = 0, y = screen.h - height, w = screen.w, h = height }) --[[@as hs.canvas]]
 		:appendElements({
 			{ type = "rectangle", action = "fill", fillColor = bgColor },
 		}) --[[@as hs.canvas]]
