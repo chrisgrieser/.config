137788df0944064ab929ff97ce2ac9d636a8e7ac
🤖 automated <cron@job>
2026-06-16 16:37:29 +0200

    Home (nvim/lua/config-for-pass.lua)

--- nvim/lua/config-for-pass.lua
+++ nvim/lua/config-for-pass.lua
@@ -10,8 +10,8 @@ vim.keymap.set("n", "H", "0^")
 vim.keymap.set("n", "ss", "VP", { desc = "Substitute line" })
 vim.keymap.set("n", "S", "v$hP", { desc = "Substitute to EoL" })
 
-vim.keymap.set("n", "<Space>", "ciw")
-vim.keymap.set("n", "<S-Space>", "daw")
+vim.keymap.set("n", "<Space>", '"_ciw')
+vim.keymap.set("n", "<S-Space>", '"_daw')
 
 --------------------------------------------------------------------------------
 
