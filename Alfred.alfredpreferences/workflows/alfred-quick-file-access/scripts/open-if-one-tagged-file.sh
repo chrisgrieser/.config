421e4dc4466e97a681aed57ee83bf748557e5e89
🤖 automated <cron@job>
2026-06-25 14:40:48 +0200

    Home (Alfred.alfredpreferences/)

--- Alfred.alfredpreferences/workflows/alfred-quick-file-access/scripts/open-if-one-tagged-file.sh
+++ Alfred.alfredpreferences/workflows/alfred-quick-file-access/scripts/open-if-one-tagged-file.sh
@@ -2,7 +2,10 @@
 
 # shellcheck disable=2154 # Alfred variable
 tagged_files=$(mdfind "kMDItemUserTags == '$tag_to_search'" | grep --invert-match "/.Trash/")
-count=$(echo "$tagged_files" | wc -l | tr -d " ")
-
-[[ "$count" -eq 1 ]] && open "$tagged_files"
-echo -n "$count" # for Alfred
+if [[ -z "$tagged_files" ]]; then
+	echo -n "0" # subsequent Alfred script filter will display "no files" message
+else
+	count=$(echo "$tagged_files" | wc -l | tr -d " ")
+	[[ "$count" -eq 1 ]] && open "$tagged_files"
+	echo -n "$count" # for Alfred
+fi
