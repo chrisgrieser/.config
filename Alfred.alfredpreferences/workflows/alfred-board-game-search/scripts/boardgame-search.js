4e3de7a5471268878ddcec5228f4610e63cce4c5
🤖 automated <cron@job>
2026-07-18 17:27:16 +0200

    Home (Alfred.alfredpreferences/workflows/alfred-board-game-search/)

--- Alfred.alfredpreferences/workflows/alfred-board-game-search/scripts/boardgame-search.js
+++ Alfred.alfredpreferences/workflows/alfred-board-game-search/scripts/boardgame-search.js
@@ -23,14 +23,20 @@ const fileExists = (/** @type {string} */ filePath) => Application("Finder").exi
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
 	const csvPath = $.getenv("bgg_csv_path");
+	const maxGames = Number.parseInt($.getenv("max_games")) * 10_000;
 
 	if (!fileExists(csvPath))
 		return JSON.stringify({
 			items: [
 				{
 					title: "No CSV database found.",
-					subtitle: "⏎: Open workflow configuration and follow the Setup instructions.",
-					arg: "navigateto/workflows>workflow>alfred-board-game-search>userconfig>bgg_csv_path",
+					subtitle: "⏎: Open workflow configuration and follow the setup instructions.",
+					arg: "alfredpreferences://navigateto/workflows>workflow>alfred-board-game-search>userconfig>bgg_csv_path",
+				},
+				{
+					title: "",
+					subtitle: "⏎: Download the CSV database from BGG",
+					arg: "https://boardgamegeek.com/data_dumps/bg_ranks",
 				},
 			],
 		});
@@ -38,16 +44,30 @@ function run() {
 	const csv = readFile(csvPath);
 
 	/** @type {AlfredItem[]} */
-	const items = csv.split("\n").map((line) => {
-		const [bggId, name] = line.split(",");
+	const games = csv
+		.split("\n")
+		.slice(1, maxGames + 1) // skip header and limit games for performance reasons
+		.map((line) => {
+			// id,name,yearpublished,rank,bayesaverage,average,usersrated,is_expansion,abstracts_rank,cgs_rank,childrensgames_rank,familygames_rank,partygames_rank,strategygames_rank,thematic_rank,wargames_rank
+			// 224517,"Brass: Birmingham",2018,1,8.3918,8.56131,59277,0,,,,,,1,,
+			const [bggId, name, year, rank, _bavg, score, _rating, isExpansion] = line.split(",");
 
-		const url = "https://boardgamegeek.com/boardgame/" + bggId;
-		return {
-			title: name,
-			subtitle: "",
-			arg: name,
-		};
+			const displayName =
+				(name || "?").replace(/^"|"$/g, "") + (isExpansion === "1" ? " 🧩" : "");
+			const displayRank = rank === "0" ? "" : " #" + rank;
+			const displayScore = Number.parseFloat(score).toFixed(1);
+			const subtitle = `${year}     ${displayScore}`;
+
+			return {
+				title: displayName + displayRank,
+				subtitle: subtitle,
+				arg: "https://boardgamegeek.com/boardgame/" + bggId,
+			};
+		});
+	console.log("# games:", games.length);
+
+	return JSON.stringify({
+		items: games,
+		cache: { seconds: 3600 * 24 * 7, loosereload: true },
 	});
-
-	return JSON.stringify({ items: items });
 }
