c8ff6d3e7d13bef186b62618795faac79c2791f4
🤖 automated <cron@job>
2026-07-27 10:43:00 +0200

    Home (2 files)

--- hammerspoon/appearance/hole-cover.lua
+++ hammerspoon/appearance/hole-cover.lua
@@ -7,45 +7,63 @@ local M = {}
 local roundedCorner = hs.loadSpoon("RoundedCorners") -- https://www.hammerspoon.org/Spoons/RoundedCorners.html
 if roundedCorner then
 	roundedCorner.radius = 15 -- higher for macOS Tahoe
+	roundedCorner.allScreens = true
 	roundedCorner:start()
 end
 
 ---BOTTOM/TOP OF THE SCREEN ----------------------------------------------------
+
+M.cover_top = {}
+M.cover_bottom = {}
+
 function M.update()
-	if M.cover_bottom then
-		M.cover_bottom:delete() ---@diagnostic disable-line: undefined-field
-		M.cover_bottom = nil
-	end
-	if M.cover_top then
-		M.cover_top:delete() ---@diagnostic disable-line: undefined-field
-		M.cover_top = nil
-	end
-
-	local screen = hs.screen.mainScreen():fullFrame()
-
-	local bgColor = require("meta.utils").isDarkMode()
-			and { red = 0.1, green = 0.1, blue = 0.1, alpha = 1 }
-		or { red = 0.88, green = 0.88, blue = 0.88, alpha = 1 }
-
+	-- CONFIG
 	local height = 20
 	local menubarHeight = 30
-	M.cover_top = hs
-		.canvas
-		.new({ x = 0, y = menubarHeight, w = screen.w, h = height }) --[[@as hs.canvas]]
-		:appendElements({
-			{ type = "rectangle", action = "fill", fillColor = bgColor },
-		}) --[[@as hs.canvas]]
-		:sendToBack() --[[@as hs.canvas]]
-		:show()
 
-	M.cover_bottom = hs
-		.canvas
-		.new({ x = 0, y = screen.h - height, w = screen.w, h = height }) --[[@as hs.canvas]]
-		:appendElements({
-			{ type = "rectangle", action = "fill", fillColor = bgColor },
-		}) --[[@as hs.canvas]]
-		:sendToBack() --[[@as hs.canvas]]
-		:show()
+	for i = 1, #M.cover_top do
+		if M.cover_top[i] then
+			M.cover_top[i]:delete() ---@diagnostic disable-line: undefined-field
+			M.cover_top[i] = nil
+		end
+	end
+	for i = 1, #M.cover_bottom do
+		if M.cover_bottom[i] then
+			M.cover_bottom[i]:delete() ---@diagnostic disable-line: undefined-field
+			M.cover_bottom[i] = nil
+		end
+	end
+
+	-----------------------------------------------------------------------------
+
+	local screens = hs.screen.allScreens() --[[@as hs.screen[]]
+	for i, screen in ipairs(screens) do
+		local frame = screen:fullFrame()
+		local bgColor = require("meta.utils").isDarkMode()
+				and { red = 0.1, green = 0.1, blue = 0.1, alpha = 1 }
+			or { red = 0.88, green = 0.88, blue = 0.88, alpha = 1 }
+
+		M.cover_top[i] = hs
+			.canvas
+			.new({ x = 0, y = menubarHeight, w = frame.w, h = height }) --[[@as hs.canvas]]
+			:appendElements({
+				{ type = "rectangle", action = "fill", fillColor = bgColor },
+			}) --[[@as hs.canvas]]
+			:sendToBack() --[[@as hs.canvas]]
+			:show()
+
+		local isMainScreen = hs.screen.mainScreen():id() == screen:id()
+		if isMainScreen then
+			M.cover_bottom[i] = hs
+				.canvas
+				.new({ x = 0, y = frame.h - height, w = frame.w, h = height }) --[[@as hs.canvas]]
+				:appendElements({
+					{ type = "rectangle", action = "fill", fillColor = bgColor },
+				}) --[[@as hs.canvas]]
+				:sendToBack() --[[@as hs.canvas]]
+				:show()
+		end
+	end
 end
 M.update() -- initialize
 
