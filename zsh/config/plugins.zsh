54a3371e77823e65300d3f4786375852fed5d0ea
🤖 automated <cron@job>
2026-07-28 12:35:01 +0200

    Home (zsh/config/plugins.zsh)

--- zsh/config/plugins.zsh
+++ zsh/config/plugins.zsh
@@ -6,41 +6,42 @@
 # needs to be run *before* compinit/zsh-autocomplete
 export FPATH="$ZDOTDIR/completions:$HOMEBREW_PREFIX/share/zsh/site-functions:$FPATH"
 
+#-------------------------------------------------------------------------------
+
 # ZSH-AUTOCOMPLETE
 # also loads compinit stuff, therefore has to be loaded *before* most plugins
-# source "$HOMEBREW_PREFIX/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
+source "$HOMEBREW_PREFIX/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
 
 # INFO not needed when using zsh-autocomplete
 # autoload compinit -Uz +X && compinit
 # [[ $(uname -p) == "i386" ]] && compaudit | xargs chmod g-w # FIX for Intel Mac, https://github.com/zsh-users/zsh-completions/issues/433#issuecomment-629539004
+
+# ZSH-AUTOSUGGESTIONS
+# https://github.com/zsh-users/zsh-autosuggestions#configuration
+source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+export ZSH_AUTOSUGGEST_HISTORY_IGNORE="?(#c50,)" # ignores long history items
+export ZSH_AUTOSUGGEST_STRATEGY=(history)
+export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=35
+# do not accept autosuggestion when using vim's `A`
+export ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=("${ZSH_AUTOSUGGEST_ACCEPT_WIDGETS[@]/vi-add-eol/}")
 
 #-------------------------------------------------------------------------------
 
 # ZSH-AUTOPAIR
 source "$HOMEBREW_PREFIX/share/zsh-autopair/autopair.zsh"
 
+# ZSH-SYNTAX-HIGHLIGHTING
 # DOCS https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/regexp.md
 source "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets regexp)
 # shellcheck disable=2034 # variable is used in other config files
 typeset -A ZSH_HIGHLIGHT_REGEXP # just declaration, my custom highlights are defined in other files
 
-# ZSH-AUTOSUGGESTIONS
-# https://github.com/zsh-users/zsh-autosuggestions#configuration
-# source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-# export ZSH_AUTOSUGGEST_HISTORY_IGNORE="?(#c50,)" # ignores long history items
-# export ZSH_AUTOSUGGEST_STRATEGY=(history)
-# export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=35
-# # do not accept autosuggestion when using vim's `A`
-# export ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=("${ZSH_AUTOSUGGEST_ACCEPT_WIDGETS[@]/vi-add-eol/}")
-
 # ZSH-HISTORY-SUBSTRING-SEARCH
 # (must be loaded *after* zsh-syntax-highlighting)
 source "$HOMEBREW_PREFIX/share/zsh-history-substring-search/zsh-history-substring-search.zsh"
 bindkey '^[[A' history-substring-search-up   # arrow up
 bindkey '^[[B' history-substring-search-down # arrow down
-
-#-------------------------------------------------------------------------------
 
 # SEMANTIC PROMPTS (WEZTERM) https://wezfurlong.org/wezterm/shell-integration
 [[ "$TERM_PROGRAM" == "WezTerm" ]] && source "$ZDOTDIR/../wezterm/semantic_prompts.zsh"
