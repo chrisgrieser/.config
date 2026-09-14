da256f43dd443c6cd936e8570a4c57cd473971ba
🤖 automated <cron@job>
2026-09-14 20:57:12 +0200

    Home (2 files)

--- Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pandoc_md-to-docx.sh
+++ Alfred.alfredpreferences/workflows/pdf-utilities/scripts/pandoc_md-to-docx.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env zsh
 set -e
-md_file="$*"
-#-------------------------------------------------------------------------------
 
-output_location="$HOME/Desktop/"
-word_file="$output_location/$(basename "$md_file" ".md")_CG.docx"
+md_file="$*"
+output_location=$(dirname "$md_file")
+word_file="$output_location/$(basename "$md_file" ".md").docx"
 
 if [[ -f "$word_file" ]]; then
 	rm -f "$word_file"
 	osascript -e 'tell application "Microsoft Word" to close every window' &> /dev/null
 fi
 
-# INFO pandoc's --data-dir for the `defaults` file defined in .zshenv
-pandoc "$md_file" --output="$word_file" --defaults="md2docx" 2>&1
+#-------------------------------------------------------------------------------
+
+pandoc "$md_file" --output="$word_file" --verbose --fail-if-warnings=true 2>&1
 
 open -R "$word_file"
 open "$word_file"
