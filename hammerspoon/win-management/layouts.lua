f6017606bc5a3947e90fbe88393bd4d2ac08b013
🤖 automated <cron@job>
2026-09-23 00:56:44 +0200

    Home (2 files)

--- hammerspoon/win-management/layouts.lua
+++ hammerspoon/win-management/layouts.lua
@@ -192,7 +192,7 @@ local config = {
 local doEvery = hs.timer.doEvery
 M.sleepTimer = doEvery(config.checkIntervalMins * 60, function()
 	local userActive = (hs.host.idleTime() / 60) < config.triggerAfterMins
-	if userActive or not env.hasProjector() then return end
+	if userActive or not env.hasProjector() and U.screenIsUnlocked() then return end
 
 	-- inform user about upcoming sleep
 	local timeToReactSecs = config.timeToReactSecs
